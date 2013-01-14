4aafd2deff13f43bce1e231ac1fe574533796302
slush <info@bitcoin.cz>
2013-01-14 14:44:11 +0100

    Stub for protect_call tests

---
+++ tests/test_protect_call.py
@@ -0,0 +1,30 @@
+import unittest
+import common
+
+from bitkeylib import proto
+
+class TestProtectCall(common.BitkeyTest):
+    def test_features(self):
+        features = self.bitkey.call(proto.Initialize(session_id=self.bitkey.session_id))
+        
+        # Result is the same as reported by BitkeyClient class
+        self.assertEqual(features, self.bitkey.features)
+         
+    def test_ping(self):
+        ping = self.bitkey.call(proto.Ping(message='ahoj!'))
+        
+        # Ping results in Success(message='Ahoj!')
+        self.assertEqual(ping, proto.Success(message='ahoj!'))
+        
+    def test_uuid(self):
+        uuid1 = self.bitkey.get_uuid()
+        uuid2 = self.bitkey.get_uuid()
+        
+        # UUID must be longer than 10 characters
+        self.assertGreater(len(uuid1), 10)
+        
+        # Every resulf of UUID must be the same
+        self.assertEqual(uuid1, uuid2)
+
+if __name__ == '__main__':
+    unittest.main()
