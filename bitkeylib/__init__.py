5db17b3a6d565489b14b3fce071cda03dff2f0b2
slush <info@bitcoin.cz>
2013-09-01 02:46:18 +0200

    Proper loading of trezor_pb2.py

--- bitkeylib/__init__.py
+++ bitkeylib/__init__.py
@@ -1,6 +1,6 @@
 try:
-    import bitkey_pb2 as proto
+    import trezor_pb2 as proto
 except ImportError:
-    print "bitkey_pb2.py not found. Please run /protobuf/build.sh to generate it!"
+    print "trezor_pb2.py not found. Please run /protobuf/build.sh to generate it!"
     import sys
     sys.exit()
