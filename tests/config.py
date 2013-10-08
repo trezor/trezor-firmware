42419a129af78d79113474dcf182794d67936fcd
slush0 <info@bitcoin.cz>
2013-10-08 20:34:38 +0200

    Profiles for pipe transport / USB hid

--- tests/config.py
+++ tests/config.py
@@ -5,17 +5,22 @@ from trezorlib.transport_pipe import PipeTransport
 from trezorlib.transport_hid import HidTransport
 from trezorlib.transport_socket import SocketTransportClient
 
-TRANSPORT = PipeTransport
-TRANSPORT_ARGS = ('../../trezor-emu/pipe.trezor', False)
+use_pipe = True
+if use_pipe:
 
-#TRANSPORT = HidTransport
-#TRANSPORT_ARGS = ('0x10c4:0xea80:000868D3', False)
+    TRANSPORT = PipeTransport
+    TRANSPORT_ARGS = ('../../trezor-emu/pipe.trezor', False)
 
-#TRANSPORT = SocketTransportClient
-#TRANSPORT_ARGS = ('trezor.dyn:3000', False)
+    DEBUG_TRANSPORT = PipeTransport
+    DEBUG_TRANSPORT_ARGS = ('../../trezor-emu/pipe.trezor_debug', False)
 
-DEBUG_TRANSPORT = PipeTransport
-DEBUG_TRANSPORT_ARGS = ('../../trezor-emu/pipe.trezor_debug', False)
+else:
+
+    TRANSPORT = HidTransport
+    TRANSPORT_ARGS = ('0x10c4:0xea80:000868D3', False)
+
+    DEBUG_TRANSPORT = SocketTransportClient
+    DEBUG_TRANSPORT_ARGS = ('trezor.bo:2000', False)
 
 #DEBUG_TRANSPORT = SocketTransportClient
 #DEBUG_TRANSPORT_ARGS = ('trezor.dyn:2000', False)
