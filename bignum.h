52485b9008cf2f70e4f9dcb0da48bbce46ca8631
Pavol Rusnak <stick@gk2.sk>
2013-10-08 18:54:21 +0200

    define BN_PRINT if not defined

--- bignum.h
+++ bignum.h
@@ -91,6 +91,10 @@ void bn_substract_noprime(const bignum256 *a, const bignum256 *b, bignum256 *res
 
 void bn_divmod58(bignum256 *a, uint32_t *r);
 
+#ifndef BN_PRINT
+#define BN_PRINT 0
+#endif
+
 #if BN_PRINT
 void bn_print(const bignum256 *a);
 void bn_print_raw(const bignum256 *a);
