150c770e4eea21729c3c9cbf0f433070314bf966
Pavol Rusnak <stick@gk2.sk>
2013-11-25 15:37:49 +0100

    xprvnode -> hdnode, add hdnode_from_pub function

--- bip32.c
+++ bip32.c
@@ -5,7 +5,19 @@
 #include "ecdsa.h"
 #include "bip32.h"
 
-void xprv_from_seed(uint8_t *seed, int seed_len, XprvNode *out)
+void hdnode_from_pub(uint32_t version, uint32_t depth, uint32_t fingerprint, uint32_t child_num, uint8_t *chain_code, uint8_t *public_key, HDNode *out)
+{
+	out->version = version;
+	out->depth = depth;
+	out->fingerprint = fingerprint;
+	out->child_num = child_num;
+	memcpy(out->chain_code, chain_code, 32);
+	memcpy(out->public_key, public_key, 33);
+	memset(out->private_key, 0, 32);
+	hdnode_fill_address(out, 0x00);
+}
+
+void hdnode_from_seed(uint8_t *seed, int seed_len, HDNode *out)
 {
 	out->version = 0x0488ADE4; // main-net
 	out->depth = 0;
@@ -14,19 +26,20 @@ void xprv_from_seed(uint8_t *seed, int seed_len, XprvNode *out)
 	// this can be done because private_key[32] and chain_code[32]
 	// form a continuous 64 byte block in the memory
 	hmac_sha512((uint8_t *)"Bitcoin seed", 12, seed, seed_len, out->private_key);
-	xprv_fill_public(out);
+	hdnode_fill_public_key(out);
+	hdnode_fill_address(out, 0x00);
 }
 
-void xprv_descent(XprvNode *inout, uint32_t i)
+void hdnode_descent(HDNode *inout, uint32_t i)
 {
 	uint8_t data[1 + 32 + 4];
 	bignum256 a, b;
 
-	if (i & 0x80000000) {
+	if (i & 0x80000000) { // private derivation
 		data[0] = 0;
 		memcpy(data + 1, inout->private_key, 32);
-	} else {
-		ecdsa_get_public_key33(inout->private_key, data);
+	} else { // public derivation
+		memcpy(data, inout->public_key, 33);
 	}
 	write_be(data + 33, i);
 
@@ -43,12 +56,16 @@ void xprv_descent(XprvNode *inout, uint32_t i)
 	inout->child_num = i;
 	bn_write_be(&a, inout->private_key);
 
-	xprv_fill_public(inout);
+	hdnode_fill_public_key(inout);
+	hdnode_fill_address(inout, 0x00);
 }
 
-void xprv_fill_public(XprvNode *xprv)
+void hdnode_fill_public_key(HDNode *xprv)
 {
-	const uint8_t version = 0x00;
 	ecdsa_get_public_key33(xprv->private_key, xprv->public_key);
+}
+
+void hdnode_fill_address(HDNode *xprv, uint8_t version)
+{
 	ecdsa_get_address(xprv->public_key, version, xprv->address);
 }
--- bip32.h
+++ bip32.h
@@ -12,14 +12,18 @@ typedef struct {
 	uint8_t chain_code[32];  // form a continuous 64 byte block
 	uint8_t public_key[33];
 	char address[35];
-} XprvNode;
+} HDNode;
 
-void xprv_from_seed(uint8_t *seed, int seed_len, XprvNode *out);
+void hdnode_from_pub(uint32_t version, uint32_t depth, uint32_t fingerprint, uint32_t child_num, uint8_t *chain_code, uint8_t *public_key, HDNode *out);
 
-#define xprv_descent_prime(X, I) xprv_descent((X), ((I) | 0x80000000))
+void hdnode_from_seed(uint8_t *seed, int seed_len, HDNode *out);
 
-void xprv_descent(XprvNode *inout, uint32_t i);
+#define hdnode_descent_prime(X, I) hdnode_descent((X), ((I) | 0x80000000))
 
-void xprv_fill_public(XprvNode *xprv);
+void hdnode_descent(HDNode *inout, uint32_t i);
+
+void hdnode_fill_public_key(HDNode *xprv);
+
+void hdnode_fill_address(HDNode *xprv, uint8_t version);
 
 #endif
