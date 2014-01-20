733a4e979cafa1a3d3b4aab71fe720220570074a
Pavol Rusnak <stick@gk2.sk>
2014-01-20 13:21:49 +0100

    adapt config.py to new api

--- tests/config.py
+++ tests/config.py
@@ -5,22 +5,38 @@ from trezorlib.transport_pipe import PipeTransport
 from trezorlib.transport_hid import HidTransport
 from trezorlib.transport_socket import SocketTransportClient
 
-use_pipe = True
-if use_pipe:
+use_real = True
+use_pipe = False
+
+if use_real:
+
+    devices = HidTransport.enumerate()
+    TRANSPORT = HidTransport
+    TRANSPORT_ARGS = (devices[0], )
+    TRANSPORT_KWARGS = {'debug_link': False}
+
+    DEBUG_TRANSPORT = HidTransport
+    DEBUG_TRANSPORT_ARGS = (devices[0], )
+    DEBUG_TRANSPORT_KWARGS = {'debug_link': True}
+
+elif use_pipe:
 
     TRANSPORT = PipeTransport
     TRANSPORT_ARGS = ('../../trezor-emu/pipe.trezor', False)
+    TRANSPORT_KWARGS = {}
 
     DEBUG_TRANSPORT = PipeTransport
     DEBUG_TRANSPORT_ARGS = ('../../trezor-emu/pipe.trezor_debug', False)
+    DEBUG_TRANSPORT_KWARGS = {}
 
 else:
 
+    devices = HidTransport.enumerate()
     TRANSPORT = HidTransport
-    TRANSPORT_ARGS = ('0x10c4:0xea80:000868D3', False)
+    TRANSPORT_ARGS = (devices[0], )
+    TRANSPORT_KWARGS = {'debug_link': False}
 
     DEBUG_TRANSPORT = SocketTransportClient
-    DEBUG_TRANSPORT_ARGS = ('trezor.bo:2000', False)
-
-#DEBUG_TRANSPORT = SocketTransportClient
-#DEBUG_TRANSPORT_ARGS = ('trezor.dyn:2000', False)
+    DEBUG_TRANSPORT_ARGS = ('trezor.bo:2000', )
+#    DEBUG_TRANSPORT_ARGS = ('trezor.dyn:2000')
+    DEBUG_TRANSPORT_KWARGS = {}
