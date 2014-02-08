33d4fc9f2409526391079221965efa86ffeb09f3
Pavol Rusnak <stick@gk2.sk>
2014-02-08 17:53:32 +0100

    autodetect type of device in config

--- tests/config.py
+++ tests/config.py
@@ -5,38 +5,30 @@ from trezorlib.transport_pipe import PipeTransport
 from trezorlib.transport_hid import HidTransport
 from trezorlib.transport_socket import SocketTransportClient
 
-use_real = True
-use_pipe = False
+devices = HidTransport.enumerate()
 
-if use_real:
-
-    devices = HidTransport.enumerate()
-    TRANSPORT = HidTransport
-    TRANSPORT_ARGS = (devices[0],)
-    TRANSPORT_KWARGS = {'debug_link': False}
-
-    DEBUG_TRANSPORT = HidTransport
-    DEBUG_TRANSPORT_ARGS = (devices[0],)
-    DEBUG_TRANSPORT_KWARGS = {'debug_link': True}
-
-elif use_pipe:
-
+if len(devices) > 0:
+    if devices[0][1] != None:
+        print 'Using TREZOR'
+        TRANSPORT = HidTransport
+        TRANSPORT_ARGS = (devices[0],)
+        TRANSPORT_KWARGS = {'debug_link': False}
+        DEBUG_TRANSPORT = HidTransport
+        DEBUG_TRANSPORT_ARGS = (devices[0],)
+        DEBUG_TRANSPORT_KWARGS = {'debug_link': True}
+    else:
+        print 'Using Raspberry Pi'
+        TRANSPORT = HidTransport
+        TRANSPORT_ARGS = (devices[0],)
+        TRANSPORT_KWARGS = {'debug_link': False}
+        DEBUG_TRANSPORT = SocketTransportClient
+        DEBUG_TRANSPORT_ARGS = ('trezor.bo:2000',)
+        DEBUG_TRANSPORT_KWARGS = {}
+else:
+    print 'Using Emulator'
     TRANSPORT = PipeTransport
     TRANSPORT_ARGS = ('../../trezor-emu/pipe.trezor', False)
     TRANSPORT_KWARGS = {}
-
     DEBUG_TRANSPORT = PipeTransport
     DEBUG_TRANSPORT_ARGS = ('../../trezor-emu/pipe.trezor_debug', False)
-    DEBUG_TRANSPORT_KWARGS = {}
-
-else:
-
-    devices = HidTransport.enumerate()
-    TRANSPORT = HidTransport
-    TRANSPORT_ARGS = (devices[0],)
-    TRANSPORT_KWARGS = {'debug_link': False}
-
-    DEBUG_TRANSPORT = SocketTransportClient
-    DEBUG_TRANSPORT_ARGS = ('trezor.bo:2000', )
-#    DEBUG_TRANSPORT_ARGS = ('trezor.dyn:2000')
     DEBUG_TRANSPORT_KWARGS = {}
