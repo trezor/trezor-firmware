ca14d1a995315fc30efa387a1923ad229e485f93
slush0 <slush@satoshilabs.com>
2014-02-21 02:33:23 +0100

    Renamed unit tests for better consistency

---
+++ tests/test_msg_getentropy.py
@@ -0,0 +1,31 @@
+import unittest
+import common
+import math
+
+import trezorlib.messages_pb2 as proto
+import trezorlib.types_pb2 as proto_types
+
+def entropy(data):
+    counts = {}
+    for c in data:
+        if c in counts:
+            counts[c] += 1
+        else:
+            counts[c] = 1
+    e = 0
+    for _, v in counts.iteritems():
+        p = 1.0 * v / len(data)
+        e -= p * math.log(p, 256)
+    return e
+
+class TestEntropy(common.TrezorTest):
+
+    def test_entropy(self):
+        for l in [0, 1, 2, 3, 4, 5, 8, 9, 16, 17, 32, 33, 64, 65, 128, 129, 256, 257, 512, 513, 1024]:
+            self.client.set_expected_responses([proto.ButtonRequest(code=proto_types.ButtonRequest_Other), proto.Entropy()])
+            ent = self.client.get_entropy(l)
+            self.assertTrue(len(ent) >= l)
+            print 'entropy = ', entropy(ent)
+
+if __name__ == '__main__':
+    unittest.main()
