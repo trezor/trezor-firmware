f6845089d5392a36cce9e580b103433ba7452a33
slush0 <slush@satoshilabs.com>
2014-02-21 02:32:09 +0100

    Added utf8 tests to sign/verify messages

---
+++ tests/test_msg_signmessage.py
@@ -0,0 +1,43 @@
+import unittest
+import common
+import binascii
+
+from trezorlib.client import CallException
+
+class TestMsgSignmessage(common.TrezorTest):
+
+    def test_sign(self):
+        self.setup_mnemonic_nopin_nopassphrase()
+        sig = self.client.sign_message('Bitcoin', [0], "This is an example of a signed message.")
+        self.assertEqual(sig.address, '14LmW5k4ssUrtbAB4255zdqv3b4w1TuX9e')
+        self.assertEqual(binascii.hexlify(sig.signature), '209e23edf0e4e47ff1dec27f32cd78c50e74ef018ee8a6adf35ae17c7a9b0dd96f48b493fd7dbab03efb6f439c6383c9523b3bbc5f1a7d158a6af90ab154e9be80')
+
+    def test_sign_testnet(self):
+        self.setup_mnemonic_nopin_nopassphrase()
+        sig = self.client.sign_message('Testnet', [0], "This is an example of a signed message.")
+
+        self.assertEqual(sig.address, 'mirio8q3gtv7fhdnmb3TpZ4EuafdzSs7zL')
+        self.assertEqual(binascii.hexlify(sig.signature), '209e23edf0e4e47ff1dec27f32cd78c50e74ef018ee8a6adf35ae17c7a9b0dd96f48b493fd7dbab03efb6f439c6383c9523b3bbc5f1a7d158a6af90ab154e9be80')
+
+    def test_sign_utf(self):
+        self.setup_mnemonic_nopin_nopassphrase()
+
+        words_nfkd = u'Pr\u030ci\u0301s\u030cerne\u030c z\u030clut\u030couc\u030cky\u0301 ku\u030an\u030c u\u0301pe\u030cl d\u030ca\u0301belske\u0301 o\u0301dy za\u0301ker\u030cny\u0301 uc\u030cen\u030c be\u030cz\u030ci\u0301 pode\u0301l zo\u0301ny u\u0301lu\u030a'
+        words_nfc = u'P\u0159\xed\u0161ern\u011b \u017elu\u0165ou\u010dk\xfd k\u016f\u0148 \xfap\u011bl \u010f\xe1belsk\xe9 \xf3dy z\xe1ke\u0159n\xfd u\u010de\u0148 b\u011b\u017e\xed pod\xe9l z\xf3ny \xfal\u016f'
+
+        sig_nfkd = self.client.sign_message('Bitcoin', [0], words_nfkd)
+        self.assertEqual(sig_nfkd.address, '14LmW5k4ssUrtbAB4255zdqv3b4w1TuX9e')
+        self.assertEqual(binascii.hexlify(sig_nfkd.signature), '1fd0ec02ed8da8df23e7fe9e680e7867cc290312fe1c970749d8306ddad1a1eda4e39588e4ec2b6a22dda4ec4f562f06e91129eea9a844a7193812de82d47c496b')
+
+        sig_nfc = self.client.sign_message('Bitcoin', [0], words_nfc)
+        self.assertEqual(sig_nfc.address, '14LmW5k4ssUrtbAB4255zdqv3b4w1TuX9e')
+        self.assertEqual(binascii.hexlify(sig_nfc.signature), '1fd0ec02ed8da8df23e7fe9e680e7867cc290312fe1c970749d8306ddad1a1eda4e39588e4ec2b6a22dda4ec4f562f06e91129eea9a844a7193812de82d47c496b')
+
+    def test_too_long(self):
+        self.setup_mnemonic_nopin_nopassphrase()
+
+        # Message cannot be longer than 255 bytes
+        self.assertRaises(CallException, self.client.sign_message, 'Bitcoin', [0], '1' * 256)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_msg_verifymessage.py
@@ -0,0 +1,94 @@
+import unittest
+import common
+import binascii
+import base64
+
+from trezorlib.client import CallException
+
+class TestMsgVerifymessage(common.TrezorTest):
+    def test_too_long(self):
+        self.setup_mnemonic_nopin_nopassphrase()
+
+        ret = self.client.verify_message('1JwSSubhmg6iPtRjtyqhUYYH7bZg3Lfy1T',
+            binascii.unhexlify('1ba77e01a9e17ba158b962cfef5f13dfed676ffc2b4bada24e58f784458b52b97421470d001d53d5880cf5e10e76f02be3e80bf21e18398cbd41e8c3b4af74c8c2'),
+            '1' * 256
+        )
+        self.assertFalse(ret)
+
+    def test_message_testnet(self):
+        sig = base64.b64decode('IFP/nvQalDo9lWCI7kScOzRkz/fiiScdkw7tFAKPoGbl6S8AY3wEws43s2gR57AfwZP8/8y7+F+wvGK9phQghN4=')
+        ret = self.client.verify_message('moRDikgmxcpouFtqnKnVVzLYgkDD2gQ3sk', sig, 'Ahoj')
+
+        self.assertTrue(ret)
+
+    def test_message_verify(self):
+        # uncompressed pubkey - OK
+        res = self.client.verify_message(
+            '1JwSSubhmg6iPtRjtyqhUYYH7bZg3Lfy1T',
+            binascii.unhexlify('1ba77e01a9e17ba158b962cfef5f13dfed676ffc2b4bada24e58f784458b52b97421470d001d53d5880cf5e10e76f02be3e80bf21e18398cbd41e8c3b4af74c8c2'),
+            'This is an example of a signed message.'
+        )
+        self.assertTrue(res)
+
+        # uncompressed pubkey - FAIL
+        res = self.client.verify_message(
+            '1JwSSubhmg6iPtRjtyqhUYYH7bZg3Lfy1T',
+            binascii.unhexlify('1ba77e01a9e17ba158b96200000000dfed676ffc2b4bada24e58f784458b52b97421470d001d53d5880cf5e10e76f02be3e80bf21e18398cbd41e8c3b4af74c8c2'),
+            'This is an example of a signed message.'
+        )
+        self.assertFalse(res)
+
+        # compressed pubkey - OK
+        res = self.client.verify_message(
+            '1C7zdTfnkzmr13HfA2vNm5SJYRK6nEKyq8',
+            binascii.unhexlify('1f44e3e461f7ca9f57c472ce1a28214df1de1dadefb6551a32d1907b80c74d5a1fbfd6daaba12dd8cb06699ce3f6941fbe0f3957b5802d13076181046e741eaaaf'),
+            'This is an example of a signed message.')
+        self.assertTrue(res)
+
+        # compressed pubkey - FAIL
+        res = self.client.verify_message(
+            '1C7zdTfnkzmr13HfA2vNm5SJYRK6nEKyq8',
+            binascii.unhexlify('1f44e3e461f7ca9f57c472000000004df1de1dadefb6551a32d1907b80c74d5a1fbfd6daaba12dd8cb06699ce3f6941fbe0f3957b5802d13076181046e741eaaaf'),
+            'This is an example of a signed message.'
+        )
+        self.assertFalse(res)
+
+        # trezor pubkey - OK
+        res = self.client.verify_message(
+            '14LmW5k4ssUrtbAB4255zdqv3b4w1TuX9e',
+            binascii.unhexlify('209e23edf0e4e47ff1dec27f32cd78c50e74ef018ee8a6adf35ae17c7a9b0dd96f48b493fd7dbab03efb6f439c6383c9523b3bbc5f1a7d158a6af90ab154e9be80'),
+            'This is an example of a signed message.'
+        )
+        self.assertTrue(res)
+
+        # trezor pubkey - FAIL
+        res = self.client.verify_message(
+            '14LmW5k4ssUrtbAB4255zdqv3b4w1TuX9e',
+            binascii.unhexlify('209e23edf0e4e47ff1de000002cd78c50e74ef018ee8a6adf35ae17c7a9b0dd96f48b493fd7dbab03efb6f439c6383c9523b3bbc5f1a7d158a6af90ab154e9be80'),
+            'This is an example of a signed message.'
+        )
+        self.assertFalse(res)
+
+    def test_verify_utf(self):
+        self.setup_mnemonic_nopin_nopassphrase()
+
+        words_nfkd = u'Pr\u030ci\u0301s\u030cerne\u030c z\u030clut\u030couc\u030cky\u0301 ku\u030an\u030c u\u0301pe\u030cl d\u030ca\u0301belske\u0301 o\u0301dy za\u0301ker\u030cny\u0301 uc\u030cen\u030c be\u030cz\u030ci\u0301 pode\u0301l zo\u0301ny u\u0301lu\u030a'
+        words_nfc = u'P\u0159\xed\u0161ern\u011b \u017elu\u0165ou\u010dk\xfd k\u016f\u0148 \xfap\u011bl \u010f\xe1belsk\xe9 \xf3dy z\xe1ke\u0159n\xfd u\u010de\u0148 b\u011b\u017e\xed pod\xe9l z\xf3ny \xfal\u016f'
+
+        res_nfkd = self.client.verify_message(
+            '14LmW5k4ssUrtbAB4255zdqv3b4w1TuX9e',
+            binascii.unhexlify('1fd0ec02ed8da8df23e7fe9e680e7867cc290312fe1c970749d8306ddad1a1eda4e39588e4ec2b6a22dda4ec4f562f06e91129eea9a844a7193812de82d47c496b'),
+            words_nfkd
+        )
+
+        res_nfc = self.client.verify_message(
+            '14LmW5k4ssUrtbAB4255zdqv3b4w1TuX9e',
+            binascii.unhexlify('1fd0ec02ed8da8df23e7fe9e680e7867cc290312fe1c970749d8306ddad1a1eda4e39588e4ec2b6a22dda4ec4f562f06e91129eea9a844a7193812de82d47c496b'),
+            words_nfc
+        )
+
+        self.assertTrue(res_nfkd)
+        self.assertTrue(res_nfc)
+        
+if __name__ == '__main__':
+    unittest.main()
