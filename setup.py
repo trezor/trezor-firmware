d6dd3c13818d9a2fd93ec372da74aa0e2474219c
Pavol Rusnak <stick@gk2.sk>
2014-07-21 18:53:08 +0200

    use pure setuptools in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-from distribute_setup import use_setuptools
-use_setuptools()
-
 from setuptools import setup, find_packages
 from os.path import dirname, join
 
@@ -12,7 +9,24 @@ setup(
     author_email='info@bitcointrezor.com',
     description='Python library for handling TREZOR hardware bitcoin wallet',
     long_description=open(join(here, 'README.rst')).read(),
-    packages=find_packages(),
+    py_modules=[
+        'trezorlib.ckd_public',
+        'trezorlib.client',
+        'trezorlib.debuglink',
+        'trezorlib.mapping',
+        'trezorlib.messages_pb2',
+        'trezorlib.protobuf_json',
+        'trezorlib.qt.pinmatrix',
+        'trezorlib.tools',
+        'trezorlib.transport',
+        'trezorlib.transport_fake',
+        'trezorlib.transport_hid',
+        'trezorlib.transport_pipe',
+        'trezorlib.transport_serial',
+        'trezorlib.transport_socket',
+        'trezorlib.tx_api',
+        'trezorlib.types_pb2',
+    ],
     test_suite='tests',
     dependency_links=['https://github.com/trezor/python-mnemonic/archive/master.zip#egg=mnemonic-0.8'],
     install_requires=['ecdsa>=0.9', 'protobuf', 'mnemonic>=0.8', 'hidapi>=0.7.99'],
