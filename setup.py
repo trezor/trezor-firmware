c2de56eb460b93030ff72ba0b479fe57b0227de5
Pavol Rusnak <stick@gk2.sk>
2014-07-30 21:04:34 +0200

    add trezorlib/qt/__init__.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='trezor',
-    version='0.5.1',
+    version='0.5.2',
     author='Bitcoin TREZOR',
     author_email='info@bitcointrezor.com',
     description='Python library for communicating with TREZOR Bitcoin Hardware Wallet',
