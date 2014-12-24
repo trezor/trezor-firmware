280310c8a01084667d4f5c431c6cf3fc365b993b
Pavol Rusnak <stick@gk2.sk>
2014-12-24 01:26:39 +0100

    add xpubaddrgen utility

---
+++ tools/xpubaddrgen.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <bip32.h>
+#include <ecdsa.h>
+
+void process_job(uint32_t jobid, const char *xpub, uint32_t change, uint32_t from, uint32_t to)
+{
+	HDNode node, child;
+	if (change > 1 || to <= from || hdnode_deserialize(xpub, &node) != 0) {
+		printf("%d error\n", jobid);
+		return;
+	}
+	hdnode_public_ckd(&node, change);
+	uint32_t i;
+	char address[36];
+	for (i = from; i < to; i++) {
+		memcpy(&child, &node, sizeof(HDNode));
+		hdnode_public_ckd(&child, i);
+		ecdsa_get_address(child.public_key, 0, address, sizeof(address));
+		printf("%d %d %s\n", jobid, i, address);
+	}
+}
+
+int main(void)
+{
+	char line[1024], xpub[1024];
+	uint32_t jobid, change, from, to;
+	int r;
+	for (;;) {
+		fgets(line, sizeof(line), stdin);
+		r = sscanf(line, "%u %s %u %u %u\n", &jobid, xpub, &change, &from, &to);
+		if (r < 1) {
+			printf("error\n");
+		} else if (r != 5) {
+			printf("%d error\n", jobid);
+		} else {
+			process_job(jobid, xpub, change, from, to);
+		}
+	}
+	return 0;
+}
