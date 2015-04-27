186a4dfdc62bd1b36e4c3d37ad8fd916c8460237
Karel Bilek <kb@karelbilek.com>
2015-04-27 19:13:07 +0200

    Adding udev packaging

---
+++ udev/dist/release.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+cd $(dirname $0)
+
+VERSION="1"
+
+
+install -D -m 0644 ./trezor.rules    ./lib/udev/rules.d/52-trezor-extension.rules
+
+NAME=trezor-udev
+
+tar cfj $NAME-$VERSION.tar.bz2 ./lib
+
+for TYPE in "deb" "rpm"; do
+	fpm \
+		-s tar \
+		-t $TYPE \
+		-a all \
+		-n $NAME \
+		-v $VERSION \
+		--license "LGPL-3.0" \
+		--vendor "SatoshiLabs" \
+        --description "Udev rules for using Bitcoin Trezor on Linux" \
+		--maintainer "SatoshiLabs <stick@satoshilabs.com>" \
+		--url "http://bitcointrezor.com/" \
+		--category "Productivity/Security" \
+		$NAME-$VERSION.tar.bz2
+done
+
+rm $NAME-$VERSION.tar.bz2
+rm -rf  ./lib
