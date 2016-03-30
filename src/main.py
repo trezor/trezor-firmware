c41922a654cbf20df61b9179b8c040fde8496b5f
Pavol Rusnak <stick@gk2.sk>
2016-03-30 18:42:55 +0200

    fix wording

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ d.bar(0, 40, 240, 200, WHITE)
 d.text(10, 28, 'Sending', 2, WHITE, BLUEGRAY)
 
 d.text(10, 80, '110.126967 BTC', 2, BLACK, WHITE)
-d.text(10, 110, 'coins to:', 1, BLACK, WHITE)
+d.text(10, 110, 'to this address:', 1, BLACK, WHITE)
 
 d.text(10, 140, '1BitkeyP2nDd5oa64x', 0, BLACK, WHITE)
 d.text(10, 160, '7AjvBbbwST54W5Zmx2', 0, BLACK, WHITE)
