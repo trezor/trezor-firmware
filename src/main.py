ab9141dd959f9ff02d561a3ada04e90592a71766
slush0 <slush@satoshilabs.com>
2016-04-28 23:48:27 +0200

    Configuration & boot of apps

--- src/main.py
+++ src/main.py
@@ -1,2 +1,17 @@
-import playground
-playground.run()
+import trezor.main
+
+# Load all applications
+from apps import homescreen
+from apps import playground
+# from apps import wallet
+
+# Initialize all applications
+homescreen.boot()
+playground.boot()
+# wallet.boot()
+
+# Load default homescreen
+from apps.homescreen.layout_homescreen import layout_homescreen
+
+# Run main even loop and specify, which screen is default
+trezor.main.run(main_layout=layout_homescreen)
