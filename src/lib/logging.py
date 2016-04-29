f9fa429f84b8d8e277b66ea5d5b61c76f5c995ab
slush0 <slush@satoshilabs.com>
2016-04-29 13:51:47 +0200

    Use constants

--- src/lib/logging.py
+++ src/lib/logging.py
@@ -1,11 +1,13 @@
 import sys
 
-CRITICAL = 50
-ERROR    = 40
-WARNING  = 30
-INFO     = 20
-DEBUG    = 10
-NOTSET   = 0
+# raise Exception("Disabled")
+
+CRITICAL = const(50)
+ERROR = const(40)
+WARNING = const(30)
+INFO = const(20)
+DEBUG = const(10)
+NOTSET = const(0)
 
 _level_dict = {
     CRITICAL: "CRIT",
