7097d1aed6347273b755531ce233d0e8ceede13b
Pavol Rusnak <stick@gk2.sk>
2016-05-05 20:33:49 +0200

    update setup.py to also accept protobuf 3.x

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     ],
     scripts = ['trezorctl'],
     test_suite='tests',
-    install_requires=['ecdsa>=0.9', 'protobuf==2.6.1', 'mnemonic>=0.8', 'hidapi>=0.7.99'],
+    install_requires=['ecdsa>=0.9', 'protobuf>=2.6.1', 'mnemonic>=0.8', 'hidapi>=0.7.99'],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
