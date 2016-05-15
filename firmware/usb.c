55fe98ccd1eb155575ee965ba142353976a5996b
Jochen Hoenicke <hoenicke@gmail.com>
2016-05-15 10:38:02 +0200

    Fix USB HID descriptor

--- firmware/usb.c
+++ firmware/usb.c
@@ -54,20 +54,17 @@ static const struct usb_device_descriptor dev_descr = {
 
 static const uint8_t hid_report_descriptor[] = {
 	0x06, 0x00, 0xff,  // USAGE_PAGE (Reserved)
-	0x09, 0x01,        // USAGE 
+	0x09, 0x01,        // USAGE (1)
 	0xa1, 0x01,        // COLLECTION (Application)
-	0x09, 0x20,        // USAGE (Input Report Data)
 	0x15, 0x00,        // LOGICAL_MINIMUM (0)
-	0x26,0xff, 0x00,   // LOGICAL_MAXIMUM (255)
-	0x75,0x08,         // REPORT_SIZE (8)
-	0x95,0x40,         // REPORT_COUNT (64)
-	0x81,0x02,         // INPUT (Data,Var,Abs)
-	0x09,0x21,         // USAGE (Output Report Data)
-	0x15,0x00,         // LOGICAL_MINIMUM (0)
-	0x26,0xff, 0x00,   // LOGICAL_MAXIMUM (255)
-	0x75,0x08,         // REPORT_SIZE (8)
-	0x95,0x40,         // REPORT_COUNT (64)
-	0x91,0x02,         // OUTPUT (Data,Var,Abs)
+	0x26, 0xff, 0x00,  // LOGICAL_MAXIMUM (255)
+	0x85, 0x3f,        // REPORT_ID (63)
+	0x75, 0x08,        // REPORT_SIZE (8)
+	0x95, 0x3f,        // REPORT_COUNT (63)
+	0x09, 0x01,        // USAGE (1)
+	0x81, 0x02,        // INPUT (Data,Var,Abs)
+	0x09, 0x01,        // USAGE (1)
+	0x91, 0x02,        // OUTPUT (Data,Var,Abs)
 	0xc0               // END_COLLECTION
 };
 
