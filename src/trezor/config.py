af9d2cf21fabc84ba0bd63357e717812951f1101
Pavol Rusnak <stick@gk2.sk>
2016-05-23 16:05:52 +0200

    add mock for trezor.config

--- src/trezor/config.py
+++ src/trezor/config.py
@@ -1,3 +1,17 @@
+# mock in-memory implementation
+
+_mock = {}
+
+def get(app, key, default=None):
+    return _mock.get((app << 8) | key, default)
+
+def set(app, key, value):
+    _mock[(app << 8) | key] = value
+    return True
+
+# real implementation commented below
+
+'''
 from TrezorConfig import Config
 
 _config = Config()
@@ -8,3 +22,4 @@ def get(app, key, default=None):
 
 def set(app, key, value):
     return _config.set(app, key, value)
+'''
