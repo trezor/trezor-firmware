ea3bd0b6f8a1592a8cb190246e9e14c85d6148e9
Pavol Rusnak <stick@gk2.sk>
2016-05-31 14:31:28 +0200

    rename resource collector script and move it to scripts

--- tools/res_collect.py
+++ tools/res_collect.py
@@ -3,7 +3,11 @@ import os
 
 resources = {}
 
+os.chdir('..')
+
 def process_file(name):
+    if name.endswith('.gitignore'):
+        return
     if name.endswith('.py'):
         return
     with open(name, 'rb') as f:
@@ -16,10 +20,13 @@ for res in os.scandir('src/trezor/res/'):
 
 # scan apps
 for app in os.scandir('src/apps/'):
-    if app.is_dir():
+    if app.is_dir() and os.path.isdir('src/apps/%s/res/' % app.name):
         for res in os.scandir('src/apps/%s/res/' % app.name):
             if res.is_file():
                 process_file('src/apps/%s/res/%s' % (app.name, res.name))
 
 with open('src/trezor/res/resources.py', 'wt') as f:
-    f.write('resdata = ' + str(resources))
+    f.write('resdata = {\n')
+    for k in sorted(resources.keys()):
+        f.write("    '%s': %s,\n" % (k, resources[k]))
+    f.write('}\n')
