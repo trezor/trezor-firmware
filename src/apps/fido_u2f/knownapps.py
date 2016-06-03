7e8e0e343974f52c07c388d8fdd83020105dde63
Pavol Rusnak <stick@gk2.sk>
2016-06-03 15:09:12 +0200

    add fido_u2f app skeleton

---
+++ src/apps/fido_u2f/knownapps.py
@@ -0,0 +1,5 @@
+knownapps = {
+    b'c50f8a7b708e92f82e7a50e2bdc55d8fd91a22fe6b29c0cdf7805530842af581': 'Dropbox', # https://www.dropbox.com/u2f-app-id.json
+    b'70617dfed065863af47c15556c91798880828cc407fdf70ae85011569465a075': 'Github',  # https://github.com/u2f/trusted_facets
+    b'a54672b222c4cf95e151ed8d4d3c767a6cc349435943794e884f3d023a8229fd': 'Google',  # https://www.gstatic.com/securitykey/origins.json
+}
