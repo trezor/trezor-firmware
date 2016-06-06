c0dfe2d702863236a9caccd9afef5caeb4a95e95
Pavol Rusnak <stick@gk2.sk>
2016-06-06 09:51:28 +0200

    fix include in ssss

--- extmod/modtrezorcrypto/ssss.h
+++ extmod/modtrezorcrypto/ssss.h
@@ -2,7 +2,7 @@
 #define __SSSS_H__
 
 #include <stdbool.h>
-#include "bignum.h"
+#include "trezor-crypto/bignum.h"
 
 bool ssss_split(const bignum256 *secret, int m, int n, bignum256 *shares);
 bool ssss_combine(const bignum256 *shares, int n, bignum256 *secret);
