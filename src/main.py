788eb675a521625cc5e363d4d575f44facaf2ec3
Jan Pochyla <jpochyla@gmail.com>
2016-06-09 16:32:46 +0200

    remove seed app initialization

--- src/main.py
+++ src/main.py
@@ -6,19 +6,17 @@ from apps import playground
 from apps import homescreen
 from apps import management
 from apps import wallet
-from apps import seed
 
 # Initialize all applications
 playground.boot()
 homescreen.boot()
 management.boot()
 wallet.boot()
-seed.boot()
 
-#change backlight to white for better visibility
+# Change backlight to white for better visibility
 trezor.ui.display.backlight(255)
 
-# just a demo to show how to register USB ifaces
+# Just a demo to show how to register USB ifaces
 msg.setup([(1, 0xF53C), (2, 0xF1D0)])
 
 # Load default homescreen
