d3d21bdbbd22fcc6d7f760f8922a6b6ce0b891ac
Pavol Rusnak <stick@gk2.sk>
2016-06-13 01:23:53 +0200

    remove main_stick and company

--- emu_stick.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd `dirname $0`/src
-if [ "$1" == -d ]; then
-    shift
-    gdb --args ../vendor/micropython/unix/micropython $* -O0 -X heapsize=100000 main_stick.py
-else
-    ../vendor/micropython/unix/micropython $* -O0 -X heapsize=100000 main_stick.py
-fi
--- src/main_stick.py
+++
@@ -1,8 +0,0 @@
-import trezor.main
-
-from apps import playground_stick
-playground_stick.boot()
-
-from apps.playground_stick import layout_homescreen
-# Run main even loop and specify, which screen is default
-trezor.main.run(main_layout=layout_homescreen.layout_homescreen)
