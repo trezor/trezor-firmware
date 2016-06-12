e6b8dc3cc580fabee26c84a4187c519f21bc0ae5
Pavol Rusnak <stick@gk2.sk>
2016-06-13 01:24:47 +0200

    add perf option to emu.sh (-p)

--- emu.sh
+++ emu.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+
+ARGS="${ARGS:--O0 -X heapsize=100000}"
+MAIN="${MAIN:-main.py}"
+BROWSER="${BROWSER:-chromium}"
+
 cd `dirname $0`/src
-if [ "$1" == -d ]; then
-    shift
-    gdb --args ../vendor/micropython/unix/micropython $* -O0 -X heapsize=100000 main.py
-else
-    ../vendor/micropython/unix/micropython $* -O0 -X heapsize=100000 main.py
-fi
+
+case "$1" in
+    "-d")
+        shift
+        gdb --args ../vendor/micropython/unix/micropython $ARGS $* $MAIN
+        ;;
+    "-p")
+        shift
+        ../vendor/micropython/unix/micropython $ARGS $* $MAIN &
+        perf record -F 100 -p $! -g -- sleep 600
+        perf script > perf.trace
+        ../vendor/flamegraph/stackcollapse-perf.pl perf.trace | ../vendor/flamegraph/flamegraph.pl > perf.svg
+        $BROWSER perf.svg
+        ;;
+    *)
+        ../vendor/micropython/unix/micropython $ARGS $* $MAIN
+esac
