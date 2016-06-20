ce734df1b0ebc47c36423fd87380f4d49a80d5dc
Pavol Rusnak <stick@gk2.sk>
2016-06-20 16:08:57 +0200

    add u2f icons, remove toig/toif from assets

--- src/apps/fido_u2f/knownapps.py
+++ src/apps/fido_u2f/knownapps.py
@@ -1,6 +1,6 @@
 knownapps = {
-    b'c50f8a7b708e92f82e7a50e2bdc55d8fd91a22fe6b29c0cdf7805530842af581': 'Dropbox', # https://www.dropbox.com/u2f-app-id.json
-    b'70617dfed065863af47c15556c91798880828cc407fdf70ae85011569465a075': 'Github',  # https://github.com/u2f/trusted_facets
-    b'a54672b222c4cf95e151ed8d4d3c767a6cc349435943794e884f3d023a8229fd': 'Google',  # https://www.gstatic.com/securitykey/origins.json
+    b'c50f8a7b708e92f82e7a50e2bdc55d8fd91a22fe6b29c0cdf7805530842af581': 'Dropbox',    # https://www.dropbox.com/u2f-app-id.json
+    b'70617dfed065863af47c15556c91798880828cc407fdf70ae85011569465a075': 'Github',     # https://github.com/u2f/trusted_facets
+    b'a54672b222c4cf95e151ed8d4d3c767a6cc349435943794e884f3d023a8229fd': 'Google',     # https://www.gstatic.com/securitykey/origins.json
     b'08b2a3d41939aa31668493cb36cdcc4f16c4d9b4c8238b73c2f672c033007197': 'Slush Pool', # https://slushpool.com/static/security/u2f.json
 }
