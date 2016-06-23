f9d82511f2987c4ef74b6439276a43016257798a
Pavol Rusnak <stick@gk2.sk>
2016-06-23 18:39:02 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='trezor',
-    version='0.6.12',
+    version='0.6.13',
     author='Bitcoin TREZOR',
     author_email='info@bitcointrezor.com',
     description='Python library for communicating with TREZOR Bitcoin Hardware Wallet',
