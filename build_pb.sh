6b17bf179b17a0b51e582f62208d72198cfa4d8d
Jochen Hoenicke <hoenicke@gmail.com>
2016-07-03 15:12:00 +0200

    monkey-patch build_pb.sh to support python3

--- build_pb.sh
+++ build_pb.sh
@@ -6,3 +6,6 @@ cd $CURDIR/../trezor-common/protob
 for i in messages types ; do
     protoc --python_out=$CURDIR/trezorlib/ -I/usr/include -I. $i.proto
 done
+
+# hack to make output python 3 compatible
+sed -i 's/^import types_pb2/from . import types_pb2/g' $CURDIR/trezorlib/messages_pb2.py
