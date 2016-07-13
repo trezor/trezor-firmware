5dae02bbbf083e8b1fb0d2ff3f01dcaaa0c63fbd
Jochen Hoenicke <hoenicke@gmail.com>
2016-07-13 12:51:47 +0200

    Faster SPI communication
    According to specs, the display can handle up to 10 MHz SPI traffic
    (minimum cycle time is 100 ns). This patch sets the SPI port to
    7.5 MHz (60 MHz clock with divisor 8).  In my tests the display works
    even fine with SPI frequency at 60 MHz.  I also minimized the delays
    (the spec doesn't require a long setup time) and use the registers to
    check when transfer is done instead of using a fixed delay.
    
    This patch also changes the swipe function to swipe pixel by pixel.
    Otherwise, the swipe would happen too fast to be seen.
    
    This changes the display update time from 11 ms to 1.5 ms.

--- setup.c
+++ setup.c
@@ -55,7 +55,7 @@ void setup(void)
 	gpio_set_af(GPIOA, GPIO_AF5, GPIO5 | GPIO7);
 
 //	spi_disable_crc(SPI1);
-	spi_init_master(SPI1, SPI_CR1_BAUDRATE_FPCLK_DIV_64, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE, SPI_CR1_CPHA_CLK_TRANSITION_1, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
+	spi_init_master(SPI1, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE, SPI_CR1_CPHA_CLK_TRANSITION_1, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
 	spi_enable_ss_output(SPI1);
 //	spi_enable_software_slave_management(SPI1);
 //	spi_set_nss_high(SPI1);
@@ -71,4 +71,5 @@ void setupApp(void)
 {
 	// hotfix for old bootloader
 	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO9);
+	spi_init_master(SPI1, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE, SPI_CR1_CPHA_CLK_TRANSITION_1, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
 }
