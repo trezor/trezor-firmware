f42b62fa400a42242f54093b0f39c5714f2177e1
Pavol Rusnak <stick@gk2.sk>
2016-09-20 16:58:47 +0200

    use mock config for stmhal, setup hid in boot

--- src/trezor/config.py
+++ src/trezor/config.py
@@ -1,50 +1,40 @@
 import sys
+import ustruct
 
-if sys.platform == 'trezor': # stmhal - use binary module (not working atm)
+# mock implementation using binary file
 
-    from TrezorConfig import Config
+_mock = {}
 
-    _config = Config()
-
-    def get(app, key, default=None):
-        v = _config.get(app, key)
-        return v if v else default
-
-    def set(app, key, value):
-        return _config.set(app, key, value)
-
-else: # emulator (mock implementation using binary file)
-
-    import ustruct
-
-    _mock = {}
+if sys.platform in ['trezor', 'pyboard']: # stmhal
+    _file = '/flash/trezor.config'
+else:
     _file = '/var/tmp/trezor.config'
 
-    def _load():
-        try:
-            with open(_file, 'rb') as f:
-                while True:
-                    d = f.read(4)
-                    if len(d) != 4:
-                        break
-                    k, l = ustruct.unpack('<HH', d)
-                    v = f.read(l)
-                    _mock[k] = v
-        except OSError:
-            pass
+def _load():
+    try:
+        with open(_file, 'rb') as f:
+            while True:
+                d = f.read(4)
+                if len(d) != 4:
+                    break
+                k, l = ustruct.unpack('<HH', d)
+                v = f.read(l)
+                _mock[k] = v
+    except OSError:
+        pass
 
-    def _save():
-        with open(_file, 'wb') as f:
-            for k, v in _mock.items():
-                f.write(ustruct.pack('<HH', k, len(v)))
-                f.write(v)
+def _save():
+    with open(_file, 'wb') as f:
+        for k, v in _mock.items():
+            f.write(ustruct.pack('<HH', k, len(v)))
+            f.write(v)
 
-    _load()
+_load()
 
-    def get(app, key, default=None):
-        return _mock.get((app << 8) | key, default)
+def get(app, key, default=None):
+    return _mock.get((app << 8) | key, default)
 
-    def set(app, key, value):
-        _mock[(app << 8) | key] = value
-        _save()
-        return True
+def set(app, key, value):
+    _mock[(app << 8) | key] = value
+    _save()
+    return True
