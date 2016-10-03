a1b75b879d81e53fe5ab5db5ee2d36612270f9a3
Pavol Rusnak <stick@gk2.sk>
2016-10-03 18:16:15 +0200

    remove the need for pb2 subdir for pb2py generator

--- tools/build_pb.sh
+++ tools/build_pb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 CURDIR=$(pwd)
+mkdir -p $CURDIR/pb2/
+echo > $CURDIR/pb2/__init__.py
 
 INDEX=../src/trezor/messages/wire_types.py
 rm -f $INDEX
@@ -8,8 +10,12 @@ for i in types messages storage ; do
     # Compile .proto files to python2 modules using google protobuf library
     cd $CURDIR/../../trezor-common/protob
     protoc --python_out=$CURDIR/pb2/ -I/usr/include -I. $i.proto
+done
 
+for i in types messages storage ; do
     # Convert google protobuf library to trezor's internal format
     cd $CURDIR
     ./pb2py $i ../src/trezor/messages/ $INDEX
 done
+
+rm -rf $CURDIR/pb2/
