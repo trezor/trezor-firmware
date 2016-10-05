bd1774e764e14dc98b5431a1499a60492d3c36b8
Pavol Rusnak <stick@gk2.sk>
2016-10-05 21:36:11 +0200

    add experiment with logo/fingerprint switching

--- bootloader/ui-mockup/boot_info.py
+++ bootloader/ui-mockup/boot_info.py
@@ -23,7 +23,7 @@ ui.display.backlight(255)
 image = res.load('./res/sl_logo.toif')
 ui.display.image(60, 0, image)
 
-ui.display.text_center(120, 160, "SatoshiLabs firmware", 1, ui.WHITE, ui.BLACK)
+ui.display.text_center(120, 160, "SatoshiLabs", 1, ui.WHITE, ui.BLACK)
 ui.display.text_center(120, 183, "v0.1.1", 1, ui.GREY, ui.BLACK)
 
 reboot = button.Button((0, 240 - 48, 240, 48), 'Reboot', normal_style=DEFAULT_BUTTON, active_style=DEFAULT_BUTTON_ACTIVE)
