73d0bba0f01229f3912f2783e19233c14a9d0b3d
chren <peter.privalinec@gmail.com>
2016-10-05 20:27:35 +0200

    vendor neutral mockup

--- bootloader/ui-mockup/boot_init.py
+++ bootloader/ui-mockup/boot_init.py
@@ -7,10 +7,10 @@ from trezor import ui, res
 ui.display.clear()
 ui.display.backlight(255)
 
-image = res.load('./res/monero_logo.toif')
+image = res.load('./res/sl_logo.toif')
 ui.display.image(60, 32, image)
 
-ui.display.text_center(120, 192, "MONERO", ui.BOLD, ui.WHITE, ui.BLACK)
+ui.display.text_center(120, 192, "SatoshiLabs", ui.BOLD, ui.WHITE, ui.BLACK)
 ui.display.text_center(120, 215, "v0.1.1", 1, ui.GREY, ui.BLACK)
 
 while True:
