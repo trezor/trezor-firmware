1420ad591be981d546f5f277b2348a7431948ad3
Pavol Rusnak <stick@gk2.sk>
2016-10-20 16:40:46 +0200

    apps: add apps.common.coins (+generator)

---
+++ src/apps/common/coins-gen.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import json
+from collections import OrderedDict
+
+coins = json.load(open('../../../../trezor-common/coins.json', 'r'))
+
+print('_coins = [')
+for c in coins:
+    d = OrderedDict()
+    for n in ['coin_name', 'coin_shortcut', 'maxfee_kb', 'address_type', 'address_type_p2sh', 'address_type_p2wpkh', 'address_type_p2wsh', 'signed_message_header', 'bip44']:
+        d[n] = c[n]
+    d['xpub_magic'] = int(c['xpub_magic'], 16)
+    d['xprv_magic'] = int(c['xprv_magic'], 16)
+    print('    {', end='')
+    for k in d:
+        print('%s: %s, ' % (repr(k), repr(d[k])), end='')
+    print('},')
+print(']\n')
