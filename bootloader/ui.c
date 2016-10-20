815f0057dde241fb33b0c99af176127404fd8c08
Pavol Rusnak <stick@gk2.sk>
2016-10-20 15:25:26 +0200

    bootloader: fix build

--- bootloader/ui.c
+++ bootloader/ui.c
@@ -29,7 +29,7 @@ void screen_upload_progress(int permil)
     char *plabel = label;
     // TODO: convert permil -> plabel
     display_text_center(120, 192 + 32, "Uploading firmware", -1, FONT_NORMAL, ui_WHITE, ui_BLACK);
-    display_loader(permil, ui_BLUE, ui_BLACK, 0, 0, 0);
+    display_loader(permil, 0, ui_BLUE, ui_BLACK, 0, 0, 0);
     display_text_center(120, 192 / 2 + 14 / 2, plabel, -1, FONT_BOLD, ui_WHITE, ui_BLACK);
     display_refresh();
 }
