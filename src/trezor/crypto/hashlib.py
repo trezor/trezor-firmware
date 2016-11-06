c95ed063db7b3c67249211303dc7250ef11eb5d3
Pavol Rusnak <stick@gk2.sk>
2016-11-06 16:19:25 +0100

    trezor.crypto.hashlib: remove HashIO (lives as HashWriter somewhere else)

--- src/trezor/crypto/hashlib.py
+++ src/trezor/crypto/hashlib.py
@@ -5,15 +5,3 @@ from TrezorCrypto import Sha256 as sha256
 from TrezorCrypto import Sha512 as sha512
 from TrezorCrypto import Sha3_256 as sha3_256
 from TrezorCrypto import Sha3_512 as sha3_512
-
-class HashIO:
-
-    def __init__(self, hashfunc=sha256):
-        self.hashfunc = hashfunc
-        self.ctx = hashfunc()
-
-    def write(self, data):
-        self.ctx.update(data)
-
-    def getvalue(self):
-        return self.ctx.digest()
