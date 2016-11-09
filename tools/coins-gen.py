3015045df6d47aea57f59a0ae66c5411ae5ecf2a
Pavol Rusnak <stick@gk2.sk>
2016-11-09 14:46:59 +0100

    apps.common.coins: update to return CoinType, not dict (also modify generator)

--- tools/coins-gen.py
+++ tools/coins-gen.py
@@ -7,12 +7,10 @@ coins = json.load(open('../../trezor-common/coins.json', 'r'))
 print('_coins = [')
 for c in coins:
     d = OrderedDict()
-    for n in ['coin_name', 'coin_shortcut', 'maxfee_kb', 'address_type', 'address_type_p2sh', 'address_type_p2wpkh', 'address_type_p2wsh', 'signed_message_header', 'bip44']:
+    for n in ['coin_name', 'coin_shortcut', 'address_type', 'maxfee_kb', 'address_type_p2sh', 'address_type_p2wpkh', 'address_type_p2wsh', 'signed_message_header']:
         d[n] = c[n]
-    d['xpub_magic'] = int(c['xpub_magic'], 16)
-    d['xprv_magic'] = int(c['xprv_magic'], 16)
-    print('    {')
+    print('    CoinType(')
     for k in d:
-        print('        %s: %s,' % (repr(k), repr(d[k])))
-    print('    },')
+        print('        %s=%s,' % (k, repr(d[k])))
+    print('    ),')
 print(']\n')
