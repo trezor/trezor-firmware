105b9c816750031ff89924c443527f3b962395d9
Pavol Rusnak <stick@gk2.sk>
2016-11-09 14:52:43 +0100

    tools: simplify coins-gen.py

--- tools/coins-gen.py
+++ tools/coins-gen.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 import json
-from collections import OrderedDict
+
+fields = [
+    'coin_name',
+    'coin_shortcut',
+    'address_type',
+    'maxfee_kb',
+    'address_type_p2sh',
+    'address_type_p2wpkh',
+    'address_type_p2wsh',
+    'signed_message_header',
+]
 
 coins = json.load(open('../../trezor-common/coins.json', 'r'))
 
 print('_coins = [')
 for c in coins:
-    d = OrderedDict()
-    for n in ['coin_name', 'coin_shortcut', 'address_type', 'maxfee_kb', 'address_type_p2sh', 'address_type_p2wpkh', 'address_type_p2wsh', 'signed_message_header']:
-        d[n] = c[n]
     print('    CoinType(')
-    for k in d:
-        print('        %s=%s,' % (k, repr(d[k])))
+    for n in fields:
+        print('        %s=%s,' % (n, repr(c[n])))
     print('    ),')
 print(']\n')
