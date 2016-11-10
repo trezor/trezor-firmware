05e035d56a10e6d91f18761cdf4e90aa340674f1
Pavol Rusnak <stick@gk2.sk>
2016-11-10 13:27:42 +0100

    apps.fido_u2f: add fastmail to known providers

--- src/apps/fido_u2f/knownapps.py
+++ src/apps/fido_u2f/knownapps.py
@@ -3,6 +3,7 @@ from trezor.crypto import hashlib
 knownapps = {
     hashlib.sha256(b'https://bitbucket.org').digest()                            : 'Bitbucket',
     hashlib.sha256(b'https://www.dropbox.com/u2f-app-id.json').digest()          : 'Dropbox',
+    hashlib.sha256(b'https://www.fastmail.com').digest()                         : 'FastMail',
     hashlib.sha256(b'https://github.com/u2f/trusted_facets').digest()            : 'GitHub',
     hashlib.sha256(b'https://gitlab.com').digest()                               : 'GitLab',
     hashlib.sha256(b'https://www.gstatic.com/securitykey/origins.json').digest() : 'Google',
