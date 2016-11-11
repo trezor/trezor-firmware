4c056d8aaa2f06332b7e5ef0ede352aea47847b9
Jan Pochyla <jpochyla@gmail.com>
2016-11-11 11:22:17 +0100

    wallet: fix layout_get_public_key

--- src/apps/wallet/layout_get_public_key.py
+++ src/apps/wallet/layout_get_public_key.py
@@ -1,15 +1,15 @@
 from trezor.utils import unimport
+from ..common import seed
 
 
 @unimport
 async def layout_get_public_key(message, session_id):
     from trezor.messages.HDNodeType import HDNodeType
     from trezor.messages.PublicKey import PublicKey
-    from ..common.seed import get_node
 
-    node = await get_node(session_id, message.address_n)
+    node = await seed.get_node(session_id, message.address_n)
 
-    node_xpub = node.serialize_public(0x0488B21E)
+    node_xpub = node.serialize_public()
     node_type = HDNodeType(
         depth=node.depth(),
         child_num=node.child_num(),
