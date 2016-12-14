e111f6338c97f3c23549b804edf916039e299898
Jan Pochyla <jpochyla@gmail.com>
2016-12-14 11:10:01 +0100

    apps.homescreen: display device label

--- src/apps/homescreen/homescreen.py
+++ src/apps/homescreen/homescreen.py
@@ -1,6 +1,7 @@
 from trezor import ui, loop, res
 from trezor.utils import unimport
 
+
 async def swipe_to_rotate():
     from trezor.ui.swipe import Swipe
 
@@ -9,32 +10,34 @@ async def swipe_to_rotate():
         ui.display.orientation(degrees)
 
 
-'''
-async def animate_logo():
-    image = res.load('apps/homescreen/res/trezor.toig')
-
-    def render(fg):
-        ui.display.icon(0, 0, image, fg, ui.BLACK)
-    await ui.animate_pulse(render, ui.WHITE, ui.DARK_GREY, speed=800000)
-'''
-
 async def dim_screen():
     current = ui.display.backlight()
 
-    await loop.Sleep(5*1000000)
+    await loop.Sleep(5 * 1000000)
     await ui.backlight_slide(ui.BACKLIGHT_DIM)
 
     try:
         while True:
             await loop.Sleep(1000000)
-    except:
+    finally:
         # Return back to original brightness
         ui.display.backlight(current)
 
-@unimport
-async def layout_homescreen():
+
+def display_homescreen():
+    from apps.common import storage
+
     image = res.load('apps/homescreen/res/trezor.toig')
     ui.display.icon(0, 0, image, ui.WHITE, ui.BLACK)
 
+    ui.display.bar(0, 180, 240, 240, ui.BLACK)
+
+    label = storage.get_label() or 'My TREZOR'
+    ui.display.text_center(120, 210, label, ui.BOLD, ui.WHITE, ui.BLACK)
+
+
+@unimport
+async def layout_homescreen():
+    display_homescreen()
     ui.display.backlight(ui.BACKLIGHT_NORMAL)
     await loop.Wait([swipe_to_rotate(), dim_screen()])
