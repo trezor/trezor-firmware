d30e565cb3b5edbee4441209e5153c1fc5e41e0f
Jan Pochyla <jpochyla@gmail.com>
2017-01-06 14:06:03 +0100

    trezor.main: fix for new loop

--- src/trezor/main.py
+++ src/trezor/main.py
@@ -7,20 +7,21 @@ from trezor import loop
 from trezor import workflow
 from trezor import log
 
-log.level = log.INFO
+log.level = log.DEBUG
+# log.level = log.INFO
 
 
 def perf_info_debug():
     while True:
-        queue = [str(x[2]).split("'")[1] for x in loop.scheduled_tasks]
+        queue_len = len(loop._scheduled_tasks)
 
         delay_avg = sum(loop.log_delay_rb) / loop.log_delay_rb_len
         delay_last = loop.log_delay_rb[loop.log_delay_pos]
 
         mem_alloc = gc.mem_alloc()
         gc.collect()
-        log.debug(__name__, "mem_alloc: %s/%s, delay_avg: %d, delay_last: %d, queue: %s",
-                  mem_alloc, gc.mem_alloc(), delay_avg, delay_last, ', '.join(queue))
+        log.debug(__name__, "mem_alloc: %s/%s, delay_avg: %d, delay_last: %d, queue_len: %d",
+                  mem_alloc, gc.mem_alloc(), delay_avg, delay_last, queue_len)
 
         yield loop.Sleep(1000000)
 
