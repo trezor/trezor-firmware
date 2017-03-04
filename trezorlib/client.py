4758eb7b075a5e5e3b049870d94e36379579e297
Pavol Rusnak <stick@gk2.sk>
2017-03-04 16:20:38 +0100

    add more coins to coin expansion

--- trezorlib/client.py
+++ trezorlib/client.py
@@ -460,7 +460,17 @@ class ProtocolMixin(object):
             n = n[1:]
 
         # coin_name/a/b/c => 44'/SLIP44_constant'/a/b/c
-        coins = { "Bitcoin": 0, "Testnet": 1, "Namecoin": 7, "Litecoin": 2, "Dogecoin": 3, "Dash": 5, "Zcash": 133, }
+        coins = {
+            "Bitcoin": 0,
+            "Testnet": 1,
+            "Namecoin": 7,
+            "Litecoin": 2,
+            "Dogecoin": 3,
+            "Dash": 5,
+            "Ether": 60,
+            "EtherClassic": 61,
+            "Zcash": 133,
+        }
         if n[0] in coins:
             n = ["44'", "%d'" % coins[n[0]] ] + n[1:]
 
