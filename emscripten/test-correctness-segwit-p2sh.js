0094706e0e6594bf991a9bcaec33bccc4189ca18
Karel Bilek <kb@karelbilek.com>
2017-03-31 22:19:37 +0200

    Adding segwit test vector

--- emscripten/test-correctness-segwit-p2sh.js
+++ emscripten/test-correctness-segwit-p2sh.js
@@ -2,7 +2,7 @@ var crypto = require('./trezor-crypto');
 var bitcoin = require('bitcoinjs-lib');
 
 var XPUB =
-    'xpub6BiVtCpG9fQPxnPmHXG8PhtzQdWC2Su4qWu6XW9tpWFYhxydCLJGrWBJZ5H6qTAHdPQ7pQhtpjiYZVZARo14qHiay2fvrX996oEP42u8wZy';
+    'xpub6CVKsQYXc9awxgV1tWbG4foDvdcnieK2JkbpPEBKB5WwAPKBZ1mstLbKVB4ov7QzxzjaxNK6EfmNY5Jsk2cG26EVcEkycGW4tchT2dyUhrx';
 var node = bitcoin.HDNode.fromBase58(XPUB).derive(0);
 
 var nodeStruct = {
@@ -13,10 +13,10 @@ var nodeStruct = {
     public_key: node.keyPair.getPublicKeyBuffer()
 };
 
-var addresses = crypto.deriveAddressRange(nodeStruct, 0, 999, 0, false);
+var addresses = crypto.deriveAddressRange(nodeStruct, 0, 999, 5, true);
 
 var fs = require('fs');
-var loaded = fs.readFileSync('test-addresses.txt').toString().split("\n");
+var loaded = fs.readFileSync('test-addresses-segwit-p2sh.txt').toString().split("\n");
 
 for (var i = 0; i < 1000; i++) {
   if (loaded[i] !== addresses[i]) {
