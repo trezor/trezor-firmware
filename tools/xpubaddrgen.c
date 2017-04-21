fa82ba6d3f4ca9bbfee0c3dd7f8da22a8bfc6f11
Pavol Rusnak <stick@gk2.sk>
2017-04-21 13:53:10 +0200

    fix xpubaddrgen

--- tools/xpubaddrgen.c
+++ tools/xpubaddrgen.c
@@ -5,10 +5,13 @@
 #include "bip32.h"
 #include "ecdsa.h"
 
+#define VERSION_PUBLIC  0x0488b21e
+#define VERSION_PRIVATE 0x0488ade4
+
 void process_job(uint32_t jobid, const char *xpub, uint32_t change, uint32_t from, uint32_t to)
 {
 	HDNode node, child;
-	if (change > 1 || to <= from || hdnode_deserialize(xpub, &node, NULL) != 0) {
+	if (change > 1 || to <= from || hdnode_deserialize(xpub, VERSION_PUBLIC, VERSION_PRIVATE, &node, NULL) != 0) {
 		printf("%d error\n", jobid);
 		return;
 	}
