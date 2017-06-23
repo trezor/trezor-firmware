886c3a0a11f7125e3e6355b35bcaeb2887b30009
Pavol Rusnak <stick@gk2.sk>
2017-06-23 17:50:45 +0200

    modtrezorcrypto: use lowercase QSTRs so we don't have to 'import as'

--- src/trezor/crypto/__init__.py
+++ src/trezor/crypto/__init__.py
@@ -1,6 +1,6 @@
-from trezorcrypto import Pbkdf2 as pbkdf2
-from trezorcrypto import Rfc6979 as rfc6979
 from trezorcrypto import bip32
 from trezorcrypto import bip39
+from trezorcrypto import pbkdf2
 from trezorcrypto import random
+from trezorcrypto import rfc6979
 from trezorcrypto import ssss
--- src/trezor/crypto/hashlib.py
+++ src/trezor/crypto/hashlib.py
@@ -1,8 +1,8 @@
-from trezorcrypto import Blake2b as blake2b
-from trezorcrypto import Blake2s as blake2s
-from trezorcrypto import Ripemd160 as ripemd160
-from trezorcrypto import Sha1 as sha1
-from trezorcrypto import Sha256 as sha256
-from trezorcrypto import Sha512 as sha512
-from trezorcrypto import Sha3_256 as sha3_256
-from trezorcrypto import Sha3_512 as sha3_512
+from trezorcrypto import blake2b
+from trezorcrypto import blake2s
+from trezorcrypto import ripemd160
+from trezorcrypto import sha1
+from trezorcrypto import sha256
+from trezorcrypto import sha512
+from trezorcrypto import sha3_256
+from trezorcrypto import sha3_512
