f18ec19aeb8f3747202741bcdd07879baf53a81e
Pavol Rusnak <stick@gk2.sk>
2017-07-05 12:52:56 +0200

    bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ else:
 
 setup(
     name='trezor',
-    version='0.7.15',
+    version='0.7.16',
     author='TREZOR',
     author_email='info@trezor.io',
     description='Python library for communicating with TREZOR Hardware Wallet',
