86bcede12d7c174412e560e3be998849668a2759
Pavol Rusnak <stick@gk2.sk>
2017-07-11 23:45:08 +0200

    bootloader: add changelog, bump version to 1.3.2

--- bootloader/bootloader.h
+++ bootloader/bootloader.h
@@ -22,14 +22,14 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 3
-#define VERSION_PATCH 1
+#define VERSION_PATCH 2
 
 #define STR(X) #X
 #define VERSTR(X) STR(X)
 
 #define VERSION_MAJOR_CHAR "\x01"
 #define VERSION_MINOR_CHAR "\x03"
-#define VERSION_PATCH_CHAR "\x01"
+#define VERSION_PATCH_CHAR "\x02"
 
 #include <stdbool.h>
 #include "memory.h"
