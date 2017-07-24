8d0852403c012879bd4f7c616adea26ac5924b24
Pavol Rusnak <stick@gk2.sk>
2017-07-24 23:43:30 +0200

    move emscripten build to separate repository (hd-wallet/fastxpub)

--- emscripten/pre.js
+++
@@ -1,9 +0,0 @@
-// stub importScripts for the faulty detection of web worker env
-if (typeof importScripts === 'undefined'
-    && typeof WorkerGlobalScope !== 'undefined'
-    && this instanceof WorkerGlobalScope
-   ) {
-       this.importScripts = function () {
-           throw new Error('importScripts is a stub');
-       };
-   }
