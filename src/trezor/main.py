552444c85696c572e63ee970e322083b2ddf0bca
Jan Pochyla <jpochyla@gmail.com>
2017-08-14 11:09:47 +0200

    remove src/lib

--- src/trezor/main.py
+++ src/trezor/main.py
@@ -1,31 +1,13 @@
-import gc
-import micropython
-import sys
-
-sys.path.append('lib')
-
+from trezor import config
 from trezor import loop
 from trezor import workflow
 from trezor import log
 
+config.init()
+
 log.level = log.DEBUG
 
 
-def perf_info():
-    prev = 0
-    peak = 0
-    sleep = loop.sleep(100000)
-    while True:
-        gc.collect()
-        used = gc.mem_alloc()
-        if used != prev:
-            prev = used
-            peak = max(peak, used)
-            print('peak %d, used %d' % (peak, used))
-        yield sleep
-
-
 def run(default_workflow):
-    # loop.schedule_task(perf_info())
     workflow.start_default(default_workflow)
     loop.run_forever()
--- tests/common.py
+++ tests/common.py
@@ -1,8 +1,6 @@
 import sys
-
 sys.path.append('../src')
-sys.path.append('../src/lib')
-
-import unittest
 
 from ubinascii import hexlify, unhexlify
+
+import unittest
