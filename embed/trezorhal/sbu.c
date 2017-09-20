1da18c955bab03bf15ef4bcb5287575706b6c89a
Jan Pochyla <jpochyla@gmail.com>
2017-09-20 14:07:13 +0200

    trezorhal/sbu: use PA2/PA3

--- embed/trezorhal/sbu.c
+++ embed/trezorhal/sbu.c
@@ -17,7 +17,7 @@ int sbu_init(void) {
     GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStructure.Pull = GPIO_NOPULL;
     GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
 
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
@@ -26,6 +26,6 @@ int sbu_init(void) {
 }
 
 void sbu_set(bool sbu1, bool sbu2) {
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, sbu1 ? GPIO_PIN_SET : GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, sbu2 ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, sbu1 ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, sbu2 ? GPIO_PIN_SET : GPIO_PIN_RESET);
 }
