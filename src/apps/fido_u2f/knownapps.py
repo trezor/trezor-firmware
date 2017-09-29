2b5bfb19904e48f6a808d614649ef9434ae43338
Arthur Gautier <superbaloo+registrations.github@superbaloo.net>
2017-09-29 16:05:35 +0000

    u2f: Adds Gandi.net as verified app

--- src/apps/fido_u2f/knownapps.py
+++ src/apps/fido_u2f/knownapps.py
@@ -1,12 +1,13 @@
 from trezor.crypto import hashlib
 
 knownapps = {
-    hashlib.sha256(b'https://bitbucket.org').digest():                            'Bitbucket',
-    hashlib.sha256(b'https://www.dropbox.com/u2f-app-id.json').digest():          'Dropbox',
-    hashlib.sha256(b'https://www.fastmail.com').digest():                         'FastMail',
-    hashlib.sha256(b'https://github.com/u2f/trusted_facets').digest():            'GitHub',
-    hashlib.sha256(b'https://gitlab.com').digest():                               'GitLab',
-    hashlib.sha256(b'https://www.gstatic.com/securitykey/origins.json').digest(): 'Google',
-    hashlib.sha256(b'https://slushpool.com/static/security/u2f.json').digest():   'Slush Pool',
-    hashlib.sha256(b'https://demo.yubico.com').digest():                          'Yubico U2F Demo',
+    hashlib.sha256(b'https://bitbucket.org').digest():                                 'Bitbucket',
+    hashlib.sha256(b'https://www.dropbox.com/u2f-app-id.json').digest():               'Dropbox',
+    hashlib.sha256(b'https://www.fastmail.com').digest():                              'FastMail',
+    hashlib.sha256(b'https://github.com/u2f/trusted_facets').digest():                 'GitHub',
+    hashlib.sha256(b'https://gitlab.com').digest():                                    'GitLab',
+    hashlib.sha256(b'https://www.gstatic.com/securitykey/origins.json').digest():      'Google',
+    hashlib.sha256(b'https://slushpool.com/static/security/u2f.json').digest():        'Slush Pool',
+    hashlib.sha256(b'https://demo.yubico.com').digest():                               'Yubico U2F Demo',
+    hashlib.sha256(b'https://account.gandi.net/api/u2f/trusted_facets.json').digest(): 'Gandi.net',
 }
