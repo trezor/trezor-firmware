936487a523a1d9ca15c0a8b36f524e223f2671b2
Jan Pochyla <jpochyla@gmail.com>
2017-10-31 13:55:51 +0100

    tests: run v1 protocol tests

--- tests/run_tests_device_emu.sh
+++ tests/run_tests_device_emu.sh
@@ -8,8 +8,12 @@ sleep 1
 
 # run tests
 cd ../tests
-TREZOR_TRANSPORT_V1=0 ./run_tests_device.sh
-error=$?
-
+error=0
+if ! TREZOR_TRANSPORT_V1=0 ./run_tests_device.sh; then
+    error=1
+fi
+if ! TREZOR_TRANSPORT_V1=1 ./run_tests_device.sh; then
+    error=1
+fi
 kill $upy_pid
 exit $error
