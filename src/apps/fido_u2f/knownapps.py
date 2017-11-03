af86f57da3214626b0bd439e72fd3e779cd6a3f2
Pavol Rusnak <stick@gk2.sk>
2017-11-03 19:03:28 +0100

    u2f: add bitfinex

--- src/apps/fido_u2f/knownapps.py
+++ src/apps/fido_u2f/knownapps.py
@@ -11,6 +11,7 @@ knownapps = {
     hashlib.sha256(b'https://keepersecurity.com').digest():                            'Keeper',
     hashlib.sha256(b'https://slushpool.com/static/security/u2f.json').digest():        'Slush Pool',
     hashlib.sha256(b'https://vault.bitwarden.com/app-id.json').digest():               'bitwarden',
+    hashlib.sha256(b'https://www.bitfinex.com').digest():                              'Bitfinex',
     hashlib.sha256(b'https://www.dropbox.com/u2f-app-id.json').digest():               'Dropbox',
     hashlib.sha256(b'https://www.fastmail.com').digest():                              'FastMail',
     hashlib.sha256(b'https://www.gstatic.com/securitykey/origins.json').digest():      'Google',
