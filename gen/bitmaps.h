5e98b0ffd6fd679c07b04f9eaa1b04db63fd7216
Jochen Hoenicke <hoenicke@gmail.com>
2017-11-03 18:57:50 +0100

    Added U2F entry for Bitfinex (#237)

--- gen/bitmaps.h
+++ gen/bitmaps.h
@@ -32,6 +32,7 @@ extern const BITMAP bmp_logo48_empty;
 extern const BITMAP bmp_logo64;
 extern const BITMAP bmp_logo64_empty;
 extern const BITMAP bmp_u2f_bitbucket;
+extern const BITMAP bmp_u2f_bitfinex;
 extern const BITMAP bmp_u2f_dropbox;
 extern const BITMAP bmp_u2f_fastmail;
 extern const BITMAP bmp_u2f_gandi;
