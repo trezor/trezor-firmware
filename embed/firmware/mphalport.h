77cc62644de045a234bd175fcc80c5cb647c3b64
Pavol Rusnak <stick@gk2.sk>
2017-11-05 18:02:47 +0100

    firmware: don't return CYCCNT contents as it is always zero anyway (cycle counter is disabled)

--- embed/firmware/mphalport.h
+++ embed/firmware/mphalport.h
@@ -1,9 +1,8 @@
-#include STM32_HAL_H
 
 #include "lib/utils/interrupt_char.h"
 
 static inline mp_uint_t mp_hal_ticks_cpu(void) {
-    return DWT->CYCCNT;
+    return 0;
 }
 
 void mp_hal_set_vcp_iface(int iface_num);
