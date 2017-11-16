4770df8912189e34859c406ce5bc08c3dff144c4
Pavol Rusnak <stick@gk2.sk>
2017-11-16 19:50:42 +0100

    update version to 1.6.0

--- firmware/trezor.h
+++ firmware/trezor.h
@@ -21,8 +21,8 @@
 #define __TREZOR_H__
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 5
-#define VERSION_PATCH 2
+#define VERSION_MINOR 6
+#define VERSION_PATCH 0
 
 #define STR(X) #X
 #define VERSTR(X) STR(X)
