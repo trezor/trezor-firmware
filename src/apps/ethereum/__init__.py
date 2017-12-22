688b568bd9dd0acd707236f30b96e82c0b1401fc
Tomas Susanka <tsusanka@gmail.com>
2017-12-22 16:17:32 +0100

    ethereum: files rename

--- src/apps/ethereum/__init__.py
+++ src/apps/ethereum/__init__.py
@@ -6,13 +6,13 @@ from trezor.messages.wire_types import \
 
 @unimport
 def dispatch_EthereumGetAddress(*args, **kwargs):
-    from .ethereum_get_address import layout_ethereum_get_address
+    from .get_address import layout_ethereum_get_address
     return layout_ethereum_get_address(*args, **kwargs)
 
 
 @unimport
 def dispatch_EthereumSignTx(*args, **kwargs):
-    from .ethereum_sign_tx import ethereum_sign_tx
+    from .sign_tx import ethereum_sign_tx
     return ethereum_sign_tx(*args, **kwargs)
 
 
