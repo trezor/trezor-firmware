1530afe4adb9367358cb3d4363ac3b11bf67a2f8
Pavol Rusnak <stick@gk2.sk>
2018-01-13 15:28:35 +0100

    vendor: update trezor-crypto, adapt codebase to changes

--- embed/extmod/modtrezorcrypto/rand.c
+++ embed/extmod/modtrezorcrypto/rand.c
@@ -12,21 +12,3 @@ uint32_t random32(void)
 {
 	return rng_get();
 }
-
-uint32_t random_uniform(uint32_t n)
-{
-	uint32_t x, max = 0xFFFFFFFF - (0xFFFFFFFF % n);
-	while ((x = random32()) >= max);
-	return x / (max / n);
-}
-
-void random_buffer(uint8_t *buf, size_t len)
-{
-	uint32_t r = 0;
-	for (size_t i = 0; i < len; i++) {
-		if (i % 4 == 0) {
-			r = random32();
-		}
-		buf[i] = (r >> ((i % 4) * 8)) & 0xFF;
-	}
-}
