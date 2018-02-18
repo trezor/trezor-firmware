4b8a963938ed4648cb70d09fae8663818a9feee8
Karel Bilek <kb@karelbilek.com>
2018-02-19 00:41:34 +0100

    emulator: add docker build

---
+++ build-emulator.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+IMAGE=trezor-mcu-build-emulator
+TAG=${1:-master}
+ELFFILE=build/trezor-emulator-$TAG
+
+docker build -f Dockerfile.emulator -t $IMAGE .
+docker run -t -v $(pwd)/build:/build:z $IMAGE /bin/sh -c "\
+	git clone https://github.com/trezor/trezor-mcu && \
+	cd trezor-mcu && \
+	git checkout $TAG && \
+	git submodule update --init && \
+	make -C vendor/libopencm3 && \
+	make -C vendor/nanopb/generator/proto && \
+	make -C firmware/protob && \
+	EMULATOR=1 make && \
+	EMULATOR=1 make -C emulator && \
+	EMULATOR=1 make -C firmware && \
+	cp firmware/trezor.elf /$ELFFILE"
