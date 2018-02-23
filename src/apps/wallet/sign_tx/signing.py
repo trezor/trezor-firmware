1b64088957cbe08c2696597819db4c30217b28bd
Jan Pochyla <jpochyla@gmail.com>
2018-02-23 19:55:07 +0100

    signing/multisig: fix pubkey index lookup

--- src/apps/wallet/sign_tx/signing.py
+++ src/apps/wallet/sign_tx/signing.py
@@ -345,7 +345,7 @@ async def sign_tx(tx: SignTx, root):
             signature = ecdsa_sign(key_sign, bip143_hash)
             if txi.multisig:
                 # find out place of our signature based on the pubkey
-                signature_index = multisig_pubkey_index(txi_sign.multisig, key_sign_pub)
+                signature_index = multisig_pubkey_index(txi.multisig, key_sign_pub)
                 witness = get_p2wsh_witness(txi.multisig, signature, signature_index, get_hash_type(coin))
             else:
                 witness = get_p2wpkh_witness(signature, key_sign_pub, get_hash_type(coin))
