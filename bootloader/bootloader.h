5ad53e99c37c7b98a18002bffc0295ef83a96281
Pavol Rusnak <stick@gk2.sk>
2018-03-13 15:42:31 +0100

    bootloader: bump version to 1.4.0

--- bootloader/bootloader.h
+++ bootloader/bootloader.h
@@ -21,15 +21,15 @@
 #define __BOOTLOADER_H__
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 3
-#define VERSION_PATCH 3
+#define VERSION_MINOR 4
+#define VERSION_PATCH 0
 
 #define STR(X) #X
 #define VERSTR(X) STR(X)
 
 #define VERSION_MAJOR_CHAR "\x01"
-#define VERSION_MINOR_CHAR "\x03"
-#define VERSION_PATCH_CHAR "\x03"
+#define VERSION_MINOR_CHAR "\x04"
+#define VERSION_PATCH_CHAR "\x00"
 
 #include <stdbool.h>
 #include "memory.h"
