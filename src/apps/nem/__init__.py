73415049df081cdf16bc1d28bef6f2422f2f8e83
Tomas Susanka <tsusanka@gmail.com>
2018-04-04 12:01:44 +0200

    nem: fix unused imports
    as in 9b9183c4f77370390ec39b8ac4eb200dffcd5b9b

--- src/apps/nem/__init__.py
+++ src/apps/nem/__init__.py
@@ -1,15 +1,12 @@
 from trezor.wire import register, protobuf_workflow
-from trezor.utils import unimport
 from trezor.messages.wire_types import NEMGetAddress, NEMSignTx
 
 
-@unimport
 def dispatch_NemGetAddress(*args, **kwargs):
     from .get_address import nem_get_address
     return nem_get_address(*args, **kwargs)
 
 
-@unimport
 def dispatch_NemSignTx(*args, **kwargs):
     from .signing import nem_sign_tx
     return nem_sign_tx(*args, **kwargs)
