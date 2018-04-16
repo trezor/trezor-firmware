dc5115055f84ef7e83cf7bf0e57619fc6b1213c4
Aleksey Popov <alepooop@gmail.com>
2018-04-16 23:09:32 +0300

    app.lisk: Add lisk_sign_message

--- src/apps/lisk/__init__.py
+++ src/apps/lisk/__init__.py
@@ -1,6 +1,6 @@
 from trezor.wire import register, protobuf_workflow
 from trezor.messages.wire_types import \
-    LiskGetAddress, LiskGetPublicKey
+    LiskGetAddress, LiskSignMessage, LiskGetPublicKey
 
 
 def dispatch_LiskGetAddress(*args, **kwargs):
@@ -13,6 +13,12 @@ def dispatch_LiskGetPublicKey(*args, **kwargs):
     return lisk_get_public_key(*args, **kwargs)
 
 
+def dispatch_LiskSignMessage(*args, **kwargs):
+    from .sign_message import lisk_sign_message
+    return lisk_sign_message(*args, **kwargs)
+
+
 def boot():
     register(LiskGetPublicKey, protobuf_workflow, dispatch_LiskGetPublicKey)
     register(LiskGetAddress, protobuf_workflow, dispatch_LiskGetAddress)
+    register(LiskSignMessage, protobuf_workflow, dispatch_LiskSignMessage)
