45cca15e5f4e7768d59362ebd0f7525731bd2602
Aleksey Popov <alepooop@gmail.com>
2018-04-16 20:56:07 +0300

    lisk: Add lisk_sign_tx

--- trezorlib/client.py
+++ trezorlib/client.py
@@ -654,7 +654,50 @@ class ProtocolMixin(object):
             resp = self.call(proto.LiskVerifyMessage(signature=signature, public_key=pubkey, message=message))
         except CallException as e:
             resp = e
-        return  isinstance(resp, proto.Success)
+        return isinstance(resp, proto.Success)
+
+    @expect(proto.LiskSignedTx)
+    def lisk_sign_tx(self, n, transaction):
+        n = self._convert_prime(n)
+
+        def asset_to_proto(asset):
+            msg = proto.LiskTransactionAsset()
+
+            if "votes" in asset:
+                msg.votes = asset["votes"]
+            if "data" in asset:
+                msg.data = asset["data"]
+            if "signature" in asset:
+                msg.signature = proto.LiskSignatureType()
+                msg.signature.public_key = binascii.unhexlify(asset["signature"]["publicKey"])
+            if "delegate" in asset:
+                msg.delegate = proto.LiskDelegateType()
+                msg.delegate.username = asset["delegate"]["username"]
+            if "multisignature" in asset:
+                msg.multisignature = proto.LiskMultisignatureType()
+                msg.multisignature.min = asset["multisignature"]["min"]
+                msg.multisignature.life_time = asset["multisignature"]["lifetime"]
+                msg.multisignature.keys_group = asset["multisignature"]["keysgroup"]
+            return msg
+
+        msg = proto.LiskTransactionCommon()
+
+        msg.type = transaction["type"]
+        msg.fee = int(transaction["fee"])  # Lisk use strings for big numbers (javascript issue)
+        msg.amount = int(transaction["amount"])  # And we convert it back to number
+        msg.timestamp = transaction["timestamp"]
+
+        if "recipientId" in transaction:
+            msg.recipient_id = transaction["recipientId"]
+        if "senderPublicKey" in transaction:
+            msg.sender_public_key = binascii.unhexlify(transaction["senderPublicKey"])
+        if "requesterPublicKey" in transaction:
+            msg.requester_public_key = binascii.unhexlify(transaction["requesterPublicKey"])
+        if "signature" in transaction:
+            msg.signature = binascii.unhexlify(transaction["signature"])
+
+        msg.asset = asset_to_proto(transaction["asset"])
+        return self.call(proto.LiskSignTx(address_n=n, transaction=msg))
 
     @field('entropy')
     @expect(proto.Entropy)
