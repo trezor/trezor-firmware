1adcaea9cdee8ded8658d64a405ea725dbba0fc0
Pavol Rusnak <stick@gk2.sk>
2018-04-23 15:20:01 +0100

    remove debug printfs from test_check_cashaddr

--- tests/test_check_cashaddr.h
+++ tests/test_check_cashaddr.h
@@ -57,8 +57,6 @@ START_TEST(test_cashaddr)
 		ck_assert_int_eq(res, 1);
 		res = cash_encode(rebuild, hrp, data, data_len);
 		ck_assert_int_eq(res, 1);
-		printf("%s\n", rebuild);
-		printf("%s\n", valid_cashchecksum[i]);
 		ck_assert_int_eq(my_strncasecmp(rebuild, valid_cashchecksum[i], 92), 0);
 	}
 	for (i = 0; i < sizeof(valid_cashaddr) / sizeof(valid_cashaddr[0]); ++i) {
@@ -77,8 +75,6 @@ START_TEST(test_cashaddr)
 		ck_assert_int_eq(memcmp(rawdata + 1, prog + 1, 20), 0);
 		ret = cash_addr_encode(rebuild, hrp, prog, 21);
 		ck_assert_int_eq(ret, 1);
-		printf("%s\n", rebuild);
-		printf("%s\n", valid_cashaddr[i].cashaddress);
 		ck_assert_int_eq(my_strncasecmp(rebuild, valid_cashaddr[i].cashaddress, 92), 0);
 	}
 }
