ca8ebacab9a707182f5160453202c35b42aa32be
matejcik <ja@matejcik.cz>
2018-05-09 18:09:31 +0200

    logging: convenience function to set up protobuf logging

--- trezorlib/log.py
+++ trezorlib/log.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Set, Type
+from typing import Set, Type, Optional
 
 from . import protobuf
 
@@ -8,12 +8,27 @@ OMITTED_MESSAGES = set()  # type: Set[Type[protobuf.MessageType]]
 
 class PrettyProtobufFormatter(logging.Formatter):
 
-    def format(self, record):
+    def format(self, record: logging.LogRecord) -> str:
         time = self.formatTime(record)
-        message = '[{time}] {level}: {msg}'.format(time=time, level=record.levelname, msg=super().format(record))
+        message = '[{time}] {source} {level}: {msg}'.format(
+            time=time, level=record.levelname.upper(),
+            source=record.name,
+            msg=super().format(record))
         if hasattr(record, 'protobuf'):
             if type(record.protobuf) in OMITTED_MESSAGES:
                 message += " ({} bytes)".format(record.protobuf.ByteSize())
             else:
-                message += "\n" + protobuf.format_message(record.protobuf)
+                message += "\n" + protobuf.format_message(record.protobuf, collapse_cointypes=True)
         return message
+
+
+def enable_debug_output(handler: Optional[logging.Handler] = None):
+    if handler is None:
+        handler = logging.StreamHandler()
+
+    formatter = PrettyProtobufFormatter()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger('trezorlib')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
