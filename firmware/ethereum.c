1f470cf1f18de0235675cf71091bba7bf4fb762f
Pavol Rusnak <stick@gk2.sk>
2018-05-25 14:22:54 +0200

    ethereum: use original ethereum message digest

--- firmware/ethereum.c
+++ firmware/ethereum.c
@@ -628,9 +628,17 @@ static void ethereum_message_hash(const uint8_t *message, size_t message_len, ui
 	struct SHA3_CTX ctx;
 	sha3_256_Init(&ctx);
 	sha3_Update(&ctx, (const uint8_t *)"\x19" "Ethereum Signed Message:\n", 26);
-	uint8_t varint[5];
-	uint32_t l = ser_length(message_len, varint);
-	sha3_Update(&ctx, varint, l);
+	uint8_t c;
+	if (message_len > 1000000000) { c = '0' + message_len / 1000000000 % 10; sha3_Update(&ctx, &c, 1); }
+	if (message_len > 100000000)  { c = '0' + message_len / 100000000  % 10; sha3_Update(&ctx, &c, 1); }
+	if (message_len > 10000000)   { c = '0' + message_len / 10000000   % 10; sha3_Update(&ctx, &c, 1); }
+	if (message_len > 1000000)    { c = '0' + message_len / 1000000    % 10; sha3_Update(&ctx, &c, 1); }
+	if (message_len > 100000)     { c = '0' + message_len / 100000     % 10; sha3_Update(&ctx, &c, 1); }
+	if (message_len > 10000)      { c = '0' + message_len / 10000      % 10; sha3_Update(&ctx, &c, 1); }
+	if (message_len > 1000)       { c = '0' + message_len / 1000       % 10; sha3_Update(&ctx, &c, 1); }
+	if (message_len > 100)        { c = '0' + message_len / 100        % 10; sha3_Update(&ctx, &c, 1); }
+	if (message_len > 10)         { c = '0' + message_len / 10         % 10; sha3_Update(&ctx, &c, 1); }
+	                                c = '0' + message_len              % 10; sha3_Update(&ctx, &c, 1);
 	sha3_Update(&ctx, message, message_len);
 	keccak_Final(&ctx, hash);
 }
