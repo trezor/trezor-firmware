cc3882abf134d7c7725e943fe47876bf2cb2f140
Pavol Rusnak <stick@gk2.sk>
2018-08-03 19:13:28 +0200

    vendor: update trezor-common; regenerate messages

--- src/trezor/messages/__init__.py
+++ src/trezor/messages/__init__.py
@@ -8,21 +8,21 @@ registered = {}  # int -> class, dynamically registered message types
 
 
 def register(msg_type):
-    '''Register custom message type in runtime.'''
+    """Register custom message type in runtime."""
     if __debug__:
-        log.debug(__name__, 'register %s', msg_type)
+        log.debug(__name__, "register %s", msg_type)
     registered[msg_type.MESSAGE_WIRE_TYPE] = msg_type
 
 
 def get_type(wire_type):
-    '''Get message class for handling given wire_type.'''
+    """Get message class for handling given wire_type."""
     if wire_type in registered:
         # message class is explicitly registered
         msg_type = registered[wire_type]
     else:
         # import message class from trezor.messages dynamically
         name = type_to_name[wire_type]
-        module = __import__('trezor.messages.%s' % name, None, None, (name, ), 0)
+        module = __import__("trezor.messages.%s" % name, None, None, (name,), 0)
         msg_type = getattr(module, name)
     return msg_type
 
