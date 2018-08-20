bdd7e932a01d2698ba60e1cd84b7fa3ad80d59f4
Pavol Rusnak <stick@gk2.sk>
2018-08-20 18:25:00 +0200

    embed: bump version to 2.0.8

--- embed/firmware/version.h
+++ embed/firmware/version.h
@@ -1,6 +1,6 @@
 #define VERSION_MAJOR       2
 #define VERSION_MINOR       0
-#define VERSION_PATCH       7
+#define VERSION_PATCH       8
 #define VERSION_BUILD       0
 
 #define FIX_VERSION_MAJOR   2
