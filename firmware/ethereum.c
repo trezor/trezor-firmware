b7a82060a92479fd305d694c21900484d8eb2b6a
Pavol Rusnak <stick@gk2.sk>
2018-08-20 15:51:17 +0200

    ethereum: fix max_chain_id

--- firmware/ethereum.c
+++ firmware/ethereum.c
@@ -37,7 +37,7 @@
 #include "messages.pb.h"
 
 /* maximum supported chain id.  v must fit in an uint32_t. */
-#define MAX_CHAIN_ID 2147483630
+#define MAX_CHAIN_ID 2147483629
 
 static bool ethereum_signing = false;
 static uint32_t data_total, data_left;
