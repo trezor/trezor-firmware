c0770ad13151a97235c9395775b8ddbabd16252a
Tomas Susanka <tsusanka@gmail.com>
2018-09-06 14:49:11 +0200

    cardano: remove sign/verify functions (#347)

--- src/apps/cardano/__init__.py
+++ src/apps/cardano/__init__.py
@@ -5,6 +5,4 @@ from trezor.messages import MessageType
 def boot():
     wire.add(MessageType.CardanoGetAddress, __name__, "get_address")
     wire.add(MessageType.CardanoGetPublicKey, __name__, "get_public_key")
-    wire.add(MessageType.CardanoSignMessage, __name__, "sign_message")
-    wire.add(MessageType.CardanoVerifyMessage, __name__, "sign_transaction")
-    wire.add(MessageType.CardanoSignTx, __name__, "verify_message")
+    wire.add(MessageType.CardanoSignTx, __name__, "sign_transaction")
