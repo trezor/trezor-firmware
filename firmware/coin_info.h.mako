matheusd/trezor-mcu

firmware: generate coin defs with Mako

firmware/coin_info.h.mako
@@ -1,0 +1,14 @@
+// This file is automatically generated from coin_info.h.mako
+// DO NOT EDIT
+
+#ifndef __COIN_INFO_H__
+#define __COIN_INFO_H__
+
+#include "coins.h"
+
+<% coins_list = list(supported_on("trezor1", coins)) %>\
+#define COINS_COUNT (${len(coins_list)})
+
+extern const CoinInfo coins[COINS_COUNT];
+
+#endif

firmware/ethereum_tokens.c.mako
@@ -1,0 +1,24 @@
+// This file is automatically generated from ethereum_tokens.c.mako
+// DO NOT EDIT
+
+#include <string.h>
+#include "ethereum_tokens.h"
+
+const TokenType tokens[TOKENS_COUNT] = {
+% for t in erc20:
+	{${"{:>2}".format(t.chain_id)}, ${c_str(t.address_bytes)}, " ${ascii(t.symbol)}", ${t.decimals}}, // ${t.chain} / ${t.name}
+% endfor
+};
+
+const TokenType *UnknownToken = (const TokenType *)1;
+
+const TokenType *tokenByChainAddress(uint8_t chain_id, const uint8_t *address)
+{
+	if (!address) return 0;
+	for (int i = 0; i < TOKENS_COUNT; i++) {
+		if (chain_id == tokens[i].chain_id && memcmp(address, tokens[i].address, 20) == 0) {
+			return &(tokens[i]);
+		}
+	}
+	return UnknownToken;
+}

firmware/ethereum_tokens.h.mako
@@ -1,0 +1,24 @@
+// This file is automatically generated from ethereum_tokens.h.mako
+// DO NOT EDIT
+
+#ifndef __ETHEREUM_TOKENS_H__
+#define __ETHEREUM_TOKENS_H__
+
+#include <stdint.h>
+
+#define TOKENS_COUNT ${len(erc20)}
+
+typedef struct {
+    uint8_t chain_id;
+    const char * const address;
+    const char * const ticker;
+    int decimals;
+} TokenType;
+
+extern const TokenType tokens[TOKENS_COUNT];
+
+extern const TokenType *UnknownToken;
+
+const TokenType *tokenByChainAddress(uint8_t chain_id, const uint8_t *address);
+
+#endif

firmware/nem_mosaics.h.mako
@@ -1,0 +1,14 @@
+// This file is automatically generated from nem_mosaics.h.mako
+// DO NOT EDIT
+
+#ifndef __NEM_MOSAICS_H__
+#define __NEM_MOSAICS_H__
+
+#include "messages-nem.pb.h"
+
+#define NEM_MOSAIC_DEFINITIONS_COUNT (${len(nem)})
+
+extern const NEMSignTx_NEMMosaicCreation_NEMMosaicDefinition NEM_MOSAIC_DEFINITIONS[NEM_MOSAIC_DEFINITIONS_COUNT];
+extern const NEMSignTx_NEMMosaicCreation_NEMMosaicDefinition *NEM_MOSAIC_DEFINITION_XEM;
+
+#endif

trezor/trezor-firmware

firmware/coin_info.h.mako
@@ -1,0 +1,14 @@
+// This file is automatically generated from coin_info.h.mako
+// DO NOT EDIT
+
+#ifndef __COIN_INFO_H__
+#define __COIN_INFO_H__
+
+#include "coins.h"
+
+<% coins_list = list(supported_on("trezor1", bitcoin)) %>\
+#define COINS_COUNT (${len(coins_list)})
+
+extern const CoinInfo coins[COINS_COUNT];
+
+#endif

firmware/ethereum_tokens.c.mako
@@ -1,0 +1,24 @@
+// This file is automatically generated from ethereum_tokens.c.mako
+// DO NOT EDIT
+
+#include <string.h>
+#include "ethereum_tokens.h"
+
+const TokenType tokens[TOKENS_COUNT] = {
+% for t in supported_on("trezor1", erc20):
+	{${"{:>2}".format(t.chain_id)}, ${c_str(t.address_bytes)}, " ${ascii(t.symbol)}", ${t.decimals}}, // ${t.chain} / ${t.name}
+% endfor
+};
+
+const TokenType *UnknownToken = (const TokenType *)1;
+
+const TokenType *tokenByChainAddress(uint32_t chain_id, const uint8_t *address)
+{
+	if (!address) return 0;
+	for (int i = 0; i < TOKENS_COUNT; i++) {
+		if (chain_id == tokens[i].chain_id && memcmp(address, tokens[i].address, 20) == 0) {
+			return &(tokens[i]);
+		}
+	}
+	return UnknownToken;
+}

firmware/ethereum_tokens.h.mako
@@ -1,0 +1,25 @@
+// This file is automatically generated from ethereum_tokens.h.mako
+// DO NOT EDIT
+
+#ifndef __ETHEREUM_TOKENS_H__
+#define __ETHEREUM_TOKENS_H__
+
+#include <stdint.h>
+
+<% erc20_list = list(supported_on("trezor1", erc20)) %>\
+#define TOKENS_COUNT ${len(erc20_list)}
+
+typedef struct {
+    uint32_t chain_id;
+    const char * const address;
+    const char * const ticker;
+    int decimals;
+} TokenType;
+
+extern const TokenType tokens[TOKENS_COUNT];
+
+extern const TokenType *UnknownToken;
+
+const TokenType *tokenByChainAddress(uint32_t chain_id, const uint8_t *address);
+
+#endif

firmware/nem_mosaics.h.mako
@@ -1,0 +1,15 @@
+// This file is automatically generated from nem_mosaics.h.mako
+// DO NOT EDIT
+
+#ifndef __NEM_MOSAICS_H__
+#define __NEM_MOSAICS_H__
+
+#include "messages-nem.pb.h"
+
+<% nem_list = list(supported_on("trezor1", nem)) %>\
+#define NEM_MOSAIC_DEFINITIONS_COUNT (${len(nem_list)})
+
+extern const NEMSignTx_NEMMosaicCreation_NEMMosaicDefinition NEM_MOSAIC_DEFINITIONS[NEM_MOSAIC_DEFINITIONS_COUNT];
+extern const NEMSignTx_NEMMosaicCreation_NEMMosaicDefinition *NEM_MOSAIC_DEFINITION_XEM;
+
+#endif
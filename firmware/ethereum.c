d36038aa1d07a451ca4f0378d6803964efefd3ec
matejcik <ja@matejcik.cz>
2018-07-30 17:40:07 +0200

    firmware: generate coin defs with Mako

--- firmware/ethereum.c
+++ firmware/ethereum.c
@@ -33,6 +33,7 @@
 #include "util.h"
 #include "gettext.h"
 #include "ethereum_tokens.h"
+#include "ethereum_networks.h"
 #include "memzero.h"
 #include "messages.pb.h"
 
@@ -247,32 +248,7 @@ static void ethereumFormatAmount(const bignum256 *amnt, const TokenType *token, 
 		if (tx_type == 1 || tx_type == 6) {
 			suffix = " WAN";
 		} else {
-			// constants from trezor-common/defs/ethereum/networks.json
-			switch (chain_id) {
-				case    1: suffix = " ETH";  break;  // Ethereum
-				case    2: suffix = " EXP";  break;  // Expanse
-				case    3: suffix = " tROP"; break;  // Ethereum Testnet Ropsten
-				case    4: suffix = " tRIN"; break;  // Ethereum Testnet Rinkeby
-				case    8: suffix = " UBQ";  break;  // UBIQ
-				case   20: suffix = " EOSC"; break;  // EOS Classic
-				case   28: suffix = " ETSC"; break;  // Ethereum Social
-				case   30: suffix = " RSK";  break;  // RSK
-				case   31: suffix = " tRSK"; break;  // RSK Testnet
-				case   42: suffix = " tKOV"; break;  // Ethereum Testnet Kovan
-				case   60: suffix = " GO";   break;  // GoChain
-				case   61: suffix = " ETC";  break;  // Ethereum Classic
-				case   62: suffix = " tETC"; break;  // Ethereum Classic Testnet
-				case   64: suffix = " ELLA"; break;  // Ellaism
-				case  820: suffix = " CLO";  break;  // Callisto
-				case 1620: suffix = " ATH";  break;  // Atheios
-				case 1987: suffix = " EGEM"; break;  // EtherGem
-				case 31102: suffix = " ESN"; break;  // Ethersocial Network
-				case 200625: suffix = " AKA"; break; // Akroma
-				case 1313114: suffix = " ETHO"; break; // Ether-1
-				case 7762959: suffix = " MUSI"; break; // Musicoin
-				case 3125659152: suffix = " PIRL"; break; // Pirl
-				default  : suffix = " UNKN"; break;  // unknown chain
-			}
+			ASSIGN_ETHEREUM_SUFFIX(suffix, chain_id);
 		}
 	}
 	bn_format(amnt, NULL, suffix, decimals, 0, false, buf, buflen);
