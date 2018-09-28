98dedc7baa7916dc158cfa47a949014c214e3f59
Pavol Rusnak <stick@gk2.sk>
2018-09-28 20:48:30 +0200

    nix: add shell.nix

---
+++ shell.nix
@@ -0,0 +1,9 @@
+with import <nixpkgs> {};
+
+let
+  myPython = python36.withPackages(p: [p.trezor p.Mako p.munch]);
+in
+  stdenv.mkDerivation {
+    name = "trezor-mcu-dev";
+    buildInputs = [ myPython protobuf gnumake gcc gcc-arm-embedded pkgconfig SDL2 SDL2_image ];
+  }
