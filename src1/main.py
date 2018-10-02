96ddcf39df88201c9a81b14392183b533f70debe
Pavol Rusnak <stick@gk2.sk>
2018-10-02 17:32:38 +0200

    firmware: use separate source tree (src1) for T1 app code for now

---
+++ src1/main.py
@@ -0,0 +1,18 @@
+import trezorio as io
+from trezorui import Display
+
+d = Display()
+
+d.clear()
+d.backlight(255)
+
+i = 0
+
+while True:
+    d.print('Loop %d\n' % i)
+    i += 1
+    r = [0, 0]
+    if io.poll([io.TOUCH], r, 1000000):
+        print('TOUCH', r)
+    else:
+        print('NOTOUCH')
