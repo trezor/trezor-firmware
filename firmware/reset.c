9730471a734ff800e455340a4b735edec79e8b9c
Pavol Rusnak <stick@gk2.sk>
2018-10-11 15:27:03 +0200

    reset: wording

--- firmware/reset.c
+++ firmware/reset.c
@@ -45,7 +45,7 @@ void reset_init(bool display_random, uint32_t _strength, bool passphrase_protect
 	no_backup = _no_backup;
 
 	if (display_random && (skip_backup || no_backup)) {
-		fsm_sendFailure(FailureType_Failure_ProcessError, "Can't show internal entropy when no_backup is used");
+		fsm_sendFailure(FailureType_Failure_ProcessError, "Can't show internal entropy when backup is skipped");
 		layoutHome();
 		return;
 	}
