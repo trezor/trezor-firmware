ef464e1596bce3b467b3688ffc3bb40d22a32398
Pavol Rusnak <stick@gk2.sk>
2018-10-12 16:50:09 +0200

    nix: add autoflake

--- shell.nix
+++ shell.nix
@@ -5,5 +5,5 @@ let
 in
   stdenv.mkDerivation {
     name = "trezor-core-dev";
-    buildInputs = [ myPython protobuf scons gnumake gcc gcc-arm-embedded-7 pkgconfig SDL2 SDL2_image ];
+    buildInputs = [ myPython protobuf scons gnumake gcc gcc-arm-embedded-7 pkgconfig SDL2 SDL2_image autoflake ];
   }
