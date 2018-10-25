7bd3cd6b136e756ba30a27c68f81974d6adb2cf2
Vladyslav Burzakovskyy <vladislav.burzakovskij@satoshilabs.com>
2018-10-25 02:24:13 +0200

    fix bash autocompletion

--- bash_completion.d/trezorctl.sh
+++ bash_completion.d/trezorctl.sh
@@ -5,7 +5,7 @@ _trezorctl()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    cmds=$(trezorctl --help | grep -A 1 'Available commands:' | tail -n 1 | tr '{},' '   ')
+    cmds=$(trezorctl --help | grep '^  [a-z]' | awk '{ print $1 }')
 
     COMPREPLY=($(compgen -W "${cmds}" -- ${cur}))
     return 0
