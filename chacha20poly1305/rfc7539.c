7079277fb0aa5db9c7cbbdec9117b8239aeccff7
Andrew Kozlik <42678794+andrewkozlik@users.noreply.github.com>
2018-11-30 15:17:52 +0100

    Fix counter initialization bug in rfc7539_init(). Fix const correctness in rfc7539.h and chacha20poly1305.h. (#188)

--- chacha20poly1305/rfc7539.c
+++ chacha20poly1305/rfc7539.c
@@ -7,10 +7,11 @@
 
 // Initialize the ChaCha20 + Poly1305 context for encryption or decryption
 // using a 32 byte key and 12 byte nonce as in the RFC 7539 style.
-void rfc7539_init(chacha20poly1305_ctx *ctx, uint8_t key[32], uint8_t nonce[12]) {
+void rfc7539_init(chacha20poly1305_ctx *ctx, const uint8_t key[32], const uint8_t nonce[12]) {
     unsigned char block0[64] = {0};
 
     ECRYPT_keysetup(&ctx->chacha20, key, 256, 16);
+    ctx->chacha20.input[12] = 0;
     ctx->chacha20.input[13] = U8TO32_LITTLE(nonce + 0);
     ctx->chacha20.input[14] = U8TO32_LITTLE(nonce + 4);
     ctx->chacha20.input[15] = U8TO32_LITTLE(nonce + 8);
@@ -24,7 +25,7 @@ void rfc7539_init(chacha20poly1305_ctx *ctx, uint8_t key[32], uint8_t nonce[12])
 // Include authenticated data in the Poly1305 MAC using the RFC 7539
 // style with 16 byte padding. This must only be called once and prior
 // to encryption or decryption.
-void rfc7539_auth(chacha20poly1305_ctx *ctx, uint8_t *in, size_t n) {
+void rfc7539_auth(chacha20poly1305_ctx *ctx, const uint8_t *in, size_t n) {
     uint8_t padding[16] = {0};
     poly1305_update(&ctx->poly1305, in, n);
     if (n % 16 != 0)
