8120ee91f602776f6933343af334a79a32e5aed1
Pavol Rusnak <stick@gk2.sk>
2018-12-04 14:19:58 +0100

    boardloader+bootloader: bump versions

--- embed/bootloader/version.h
+++ embed/bootloader/version.h
@@ -1,6 +1,6 @@
 #define VERSION_MAJOR       2
 #define VERSION_MINOR       0
-#define VERSION_PATCH       1
+#define VERSION_PATCH       2
 #define VERSION_BUILD       0
 #define VERSION_UINT32      (VERSION_MAJOR | (VERSION_MINOR << 8) | (VERSION_PATCH << 16) | (VERSION_BUILD << 24))
 
