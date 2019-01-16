89dc1966dff859976c8d79fc86664708ced79c36
Tomas Susanka <tsusanka@gmail.com>
2019-01-16 15:53:36 +0100

    protob: build; templates: build

--- src/trezor/messages/MoneroLiveRefreshStartRequest.py
+++ src/trezor/messages/MoneroLiveRefreshStartRequest.py
@@ -9,20 +9,20 @@ if __debug__:
         List = None  # type: ignore
 
 
-class EthereumGetPublicKey(p.MessageType):
-    MESSAGE_WIRE_TYPE = 450
+class MoneroLiveRefreshStartRequest(p.MessageType):
+    MESSAGE_WIRE_TYPE = 552
 
     def __init__(
         self,
         address_n: List[int] = None,
-        show_display: bool = None,
+        network_type: int = None,
     ) -> None:
         self.address_n = address_n if address_n is not None else []
-        self.show_display = show_display
+        self.network_type = network_type
 
     @classmethod
     def get_fields(cls):
         return {
             1: ('address_n', p.UVarintType, p.FLAG_REPEATED),
-            2: ('show_display', p.BoolType, 0),
+            2: ('network_type', p.UVarintType, 0),
         }
