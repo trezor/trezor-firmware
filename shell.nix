75e65bbb2d200e7b2f88405500410246636faa82
Pavol Rusnak <pavol@rusnak.io>
2019-01-29 02:14:34 +0100

    nix: use python3, not python36

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 
 let
-  myPython = python36.withPackages(ps: [ps.trezor ps.pytest ps.flake8 ps.isort ps.black ps.Mako ps.munch ps.Pyro4]);
+  myPython = python3.withPackages(ps: [ps.trezor ps.pytest ps.flake8 ps.isort ps.black ps.Mako ps.munch ps.Pyro4]);
 in
   stdenv.mkDerivation {
     name = "trezor-core-dev";
