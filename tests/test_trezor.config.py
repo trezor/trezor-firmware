69efeac015e3c05be05ca98c734cd66a1d5e43be
Andrew Kozlik <andrew.kozlik@gmail.com>
2019-02-12 18:21:23 +0100

    tests: Limit the APP values in test_trezor.config to 1, ..., 63
    to account for the fact that the top two bits are being used
    as permissions flags.

--- tests/test_trezor.config.py
+++ tests/test_trezor.config.py
@@ -11,7 +11,7 @@ PINKEY = 0x00
 
 def random_entry():
     while True:
-        appid, key = 1 + random.uniform(127), random.uniform(256)
+        appid, key = 1 + random.uniform(63), random.uniform(256)
         if appid != PINAPP or key != PINKEY:
             break
     return appid, key
