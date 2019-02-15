5137f4ec00fa9c0ad506078859ce7e854a50de90
Pavol Rusnak <pavol@rusnak.io>
2019-02-15 14:25:49 +0100

    firmware: bump version to 1.8.0

--- firmware/trezor.h
+++ firmware/trezor.h
@@ -23,8 +23,8 @@
 #include <stdint.h>
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 7
-#define VERSION_PATCH 3
+#define VERSION_MINOR 8
+#define VERSION_PATCH 0
 
 #define STR(X) #X
 #define VERSTR(X) STR(X)
