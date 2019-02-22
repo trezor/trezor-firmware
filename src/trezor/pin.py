456a2c68d6fe794e37027c50526e403a78ba2874
Andrew Kozlik <andrew.kozlik@gmail.com>
2019-02-22 21:53:37 +0100

    pin: change show_pin_timeout() to display arbitrary message
    Don't pre-check old PIN when removing PIN protection.

--- src/trezor/pin.py
+++ src/trezor/pin.py
@@ -5,11 +5,11 @@ def pin_to_int(pin: str) -> int:
     return int("1" + pin)
 
 
-def show_pin_timeout(seconds: int, progress: int) -> bool:
+def show_pin_timeout(seconds: int, progress: int, message: str) -> bool:
     if progress == 0:
         ui.display.bar(0, 0, ui.WIDTH, ui.HEIGHT, ui.BG)
         ui.display.text_center(
-            ui.WIDTH // 2, 37, "Verifying PIN", ui.BOLD, ui.FG, ui.BG, ui.WIDTH
+            ui.WIDTH // 2, 37, message, ui.BOLD, ui.FG, ui.BG, ui.WIDTH
         )
     ui.display.loader(progress, 0, ui.FG, ui.BG)
     if seconds == 0:
