61f403c421bdbcf56f0ff867f80a43da0d7921d3
Pavol Rusnak <pavol@rusnak.io>
2019-02-28 16:28:48 +0100

    tests: update test_msg_ethereum_getpublickey to test node field

--- trezorlib/tests/device_tests/test_msg_ethereum_getpublickey.py
+++ trezorlib/tests/device_tests/test_msg_ethereum_getpublickey.py
@@ -26,7 +26,19 @@ from .common import TrezorTest
 class TestMsgEthereumGetPublicKey(TrezorTest):
     def test_ethereum_getpublickey(self):
         self.setup_mnemonic_nopin_nopassphrase()
+        res = ethereum.get_public_node(self.client, [H_(44), H_(60), H_(0)])
+        assert res.node.depth == 3
+        assert res.node.fingerprint == 0xC10CFFDA
+        assert res.node.child_num == 0x80000000
         assert (
-            ethereum.get_public_node(self.client, [H_(44), H_(60), H_(0)]).xpub
+            res.node.chain_code.hex()
+            == "813d9feda6421f97a6472ff36679aa9e211ff88f6bdee51093af313ce628087e"
+        )
+        assert (
+            res.node.public_key.hex()
+            == "0318c22dedce01caca32354f98428e3af06a452f3fa84e6af8f1b6aa362affa641"
+        )
+        assert (
+            res.xpub
             == "xpub6D54vV8eUYHMVBZCnz4SLjuiQngXURVCGKKGoJrWUDRegdMByLTJKfRs64q3UKiQCsSHJPtCQehTvERczdghS7gb8oedWSyNDtBU1zYDJtb"
         )
