97171a85f219ef62beef606f431220cc53ffd3d3
matejcik <ja@matejcik.cz>
2019-03-04 15:43:10 +0100

    tools: simplify helloworld.py

--- tools/helloworld.py
+++ tools/helloworld.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-from trezorlib.client import TrezorClient
-from trezorlib.transport import get_transport
+from trezorlib.client import get_default_client
 from trezorlib.tools import parse_path
 from trezorlib import btc
-from trezorlib.ui import ClickUI
 
 
 def main():
     # Use first connected device
-    transport = get_transport()
-
-    # Creates object for manipulating TREZOR
-
-    ui = ClickUI()
-    client = TrezorClient(transport, ui)
+    client = get_default_client()
 
     # Print out TREZOR's features and settings
     print(client.features)
@@ -21,11 +14,9 @@ def main():
     # Get the first address of first BIP44 account
     # (should be the same address as shown in wallet.trezor.io)
     bip32_path = parse_path("44'/0'/0'/0/0")
-    address = btc.get_address(client, 'Bitcoin', bip32_path, True)
-    print('Bitcoin address:', address)
-
-    client.close()
+    address = btc.get_address(client, "Bitcoin", bip32_path, True)
+    print("Bitcoin address:", address)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
