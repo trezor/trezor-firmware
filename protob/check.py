04bac52951764d5516206f5359cfc5f5c24eb38f
Pavol Rusnak <pavol@rusnak.io>
2019-03-13 16:59:26 +0100

    protob: add check for message prefixes; fix binance message names

---
+++ protob/check.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from glob import glob
+import sys
+
+error = False
+
+for fn in sorted(glob("messages-*.proto")):
+    with open(fn, "rt") as f:
+        prefix = fn.split(".")[0][9:].capitalize()
+        if prefix in ["Bitcoin", "Bootloader", "Common", "Crypto", "Management"]:
+            continue
+        if prefix == "Nem":
+            prefix = "NEM"
+        for line in f:
+            line = line.strip().split(" ")
+            if line[0] not in ["enum", "message"]:
+                continue
+            if not line[1].startswith(prefix) and not line[1].startswith("Debug" + prefix):
+                print("ERROR:", fn, line[1])
+                error = True
+
+if error:
+    sys.exit(1)
