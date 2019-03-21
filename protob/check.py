86e124e3b214ac7470590a1f31b9fcff6559d9c0
matejcik <ja@matejcik.cz>
2019-03-21 12:30:37 +0100

    protob: fix check.py to match proper prefix

--- protob/check.py
+++ protob/check.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 from glob import glob
 import os
+import re
 import sys
 
 error = False
 
 MYDIR = os.path.dirname(__file__)
 
+EXPECTED_PREFIX_RE = re.compile(r"messages-(\w+)\.proto")
+
 for fn in sorted(glob(os.path.join(MYDIR, "messages-*.proto"))):
     with open(fn, "rt") as f:
-        prefix = fn.split(".")[0][9:].capitalize()
+        prefix = EXPECTED_PREFIX_RE.search(fn).group(1).capitalize()
         if prefix in ["Bitcoin", "Bootloader", "Common", "Crypto", "Management"]:
             continue
         if prefix == "Nem":
