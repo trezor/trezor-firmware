2813522b05cef4e0e545a101f8b3559a3183b45b
matejcik <ja@matejcik.cz>
2019-04-15 16:15:28 +0200

    tools: refresh mem_* debug tools, blackify

--- tools/mem_read.py
+++ tools/mem_read.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from trezorlib.debuglink import DebugLink
-from trezorlib.client import TrezorClient
 from trezorlib.transport import enumerate_devices
 import sys
 
@@ -13,28 +12,28 @@ import sys
 # be running a firmware that was built with debug link enabled
 
 
+def find_debug():
+    for device in enumerate_devices():
+        try:
+            debug_transport = device.find_debug()
+            debug = DebugLink(debug_transport, auto_interact=False)
+            debug.open()
+            return debug
+        except Exception:
+            continue
+    else:
+        print("No suitable Trezor device found")
+        sys.exit(1)
+
+
 def main():
-    # List all debuggable TREZORs
-    devices = [device for device in enumerate_devices() if hasattr(device, 'find_debug')]
-
-    # Check whether we found any
-    if len(devices) == 0:
-        print('No TREZOR found')
-        return
-
-    # Use first connected device
-    transport = devices[0]
-    debug_transport = devices[0].find_debug()
-
-    # Creates object for manipulating TREZOR
-    client = TrezorClient(transport)
-    debug = DebugLink(debug_transport)
+    debug = find_debug()
 
     arg1 = int(sys.argv[1], 16)
     arg2 = int(sys.argv[2], 16)
     step = 0x400 if arg2 >= 0x400 else arg2
 
-    f = open('memory.dat', 'wb')
+    f = open("memory.dat", "wb")
 
     for addr in range(arg1, arg1 + arg2, step):
         mem = debug.memory_read(addr, step)
@@ -42,8 +41,6 @@ def main():
 
     f.close()
 
-    client.close()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tools/mem_write.py
+++ tools/mem_write.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
 from trezorlib.debuglink import DebugLink
-from trezorlib.client import TrezorClient
 from trezorlib.transport import enumerate_devices
 import sys
 
 
+def find_debug():
+    for device in enumerate_devices():
+        try:
+            debug_transport = device.find_debug()
+            debug = DebugLink(debug_transport, auto_interact=False)
+            debug.open()
+            return debug
+        except Exception:
+            continue
+    else:
+        print("No suitable Trezor device found")
+        sys.exit(1)
+
+
 def main():
-    # List all debuggable TREZORs
-    devices = [device for device in enumerate_devices() if hasattr(device, 'find_debug')]
-
-    # Check whether we found any
-    if len(devices) == 0:
-        print('No TREZOR found')
-        return
-
-    # Use first connected device
-    transport = devices[0]
-    debug_transport = devices[0].find_debug()
-
-    # Creates object for manipulating TREZOR
-    client = TrezorClient(transport)
-    debug = DebugLink(debug_transport)
-
+    debug = find_debug()
     debug.memory_write(int(sys.argv[1], 16), bytes.fromhex(sys.argv[2]), flash=True)
-    client.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
