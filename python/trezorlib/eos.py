e0b258de9d2dc59e62984ac104a9091d1636d1b2
Andriy Tkachyshyn <atkachyshyn@gmail.com>
2019-04-18 00:14:30 +0300

    changes according to code review

--- python/trezorlib/eos.py
+++ python/trezorlib/eos.py
@@ -1,11 +1,7 @@
-import binascii
 from datetime import datetime
-from . import messages as proto
+from . import messages
 from .tools import CallException, expect, session, b58decode
 
-
-def int_to_big_endian(value):
-    return value.to_bytes((value.bit_length() + 7) // 8, "big")
 
 def name_to_number(name):
     length = len(name)
@@ -26,52 +22,33 @@ def name_to_number(name):
 
     return value
 
-def asset_to_number(asset):
-    amount_str, symbol_str = asset.split(' ')
-    dot_pos = amount_str.find('.')
-
-    # parse symbol
-    if dot_pos != -1:
-        precision_digit = len(amount_str) - dot_pos - 1
-    else:
-        precision_digit = 0
-
-    sym = symbol_from_string(precision_digit, symbol_str)
-
-    # parse amount
-    if dot_pos != -1:
-        int_part = int(amount_str[:dot_pos])
-        fract_part = int(amount_str[dot_pos + 1:])
-        if int_part < 0:
-            fract_part *= -1
-    else:
-        int_part = int(amount_str)
-        fract_part = 0
-
-    amount = int_part
-    amount *= symbol_precision(sym)
-    amount += fract_part
-
-    return amount, sym
 
 def char_to_symbol(c):
     if c >= 'a' and c <= 'z':
         return ord(c) - ord('a') + 6
-    if c >= '1' and c <= '5':
+    elif c >= '1' and c <= '5':
         return ord(c) - ord('1') + 1
-    return 0
+    else:
+        return 0
 
-def symbol_from_string(p, name):
-    length = len(name)
-    result = 0
-    for i in range(0, length):
-        result |= ord(name[i]) << (8 * (i + 1))
 
-    result |= p
-    return result
+def parse_asset(asset):
+    amount_str, symbol_str = asset.split(' ')
 
-def symbol_precision(sym):
-    return pow(10, (sym & 0xff))
+    # "-1.0000" => ["-1", "0000"] => -10000
+    amount_parts = amount_str.split(".", maxsplit=1)
+    amount = int("".join(amount_parts))
+
+    precision = 0
+    if len(amount_parts) > 1:
+        precision = len(amount_parts[1])
+
+        # 4, "EOS" => b"\x04EOS" => little-endian uint32
+    symbol_bytes = bytes([precision]) + symbol_str.encode()
+    symbol = int.from_bytes(symbol_bytes, "little")
+
+    return messages.EosAsset(amount=amount, symbol=symbol)
+
 
 def public_key_to_buffer(pub_key):
     _t = 0
@@ -86,143 +63,128 @@ def public_key_to_buffer(pub_key):
         _t = 1
 
     return _t, b58decode(pub_key, None)[:-4]
-        
+
 
 def parse_common(action):
     authorization = []
     for auth in action['authorization']:
         authorization.append(
-            proto.EosPermissionLevel(
+            messages.EosPermissionLevel(
                 actor=name_to_number(auth['actor']),
                 permission=name_to_number(auth['permission'])
             )
         )
 
-    return proto.EosActionCommon(
+    return messages.EosActionCommon(
         account=name_to_number(action['account']),
         name=name_to_number(action['name']),
         authorization=authorization
     )
 
-def parse_transfer(data):
-    amount, symbol = asset_to_number(data['quantity'])
 
-    return proto.EosActionTransfer(
+def parse_transfer(data):
+    return messages.EosActionTransfer(
         sender=name_to_number(data['from']),
         receiver=name_to_number(data['to']),
         memo=data['memo'],
-        quantity=proto.EosAsset(
-            amount=amount,
-            symbol=symbol
-        )
+        quantity=parse_asset(data['quantity'])
     )
+
 
 def parse_vote_producer(data):
     producers = []
     for producer in data['producers']:
         producers.append(name_to_number(producer))
 
-    return proto.EosActionVoteProducer(
+    return messages.EosActionVoteProducer(
         voter=name_to_number(data['account']),
         proxy=name_to_number(data['proxy']),
         producers=producers
     )
 
-def parse_buy_ram(data):
-    amount, symbol = asset_to_number(data['quant'])
 
-    return proto.EosActionBuyRam(
+def parse_buy_ram(data):
+    return messages.EosActionBuyRam(
         payer=name_to_number(data['payer']),
         receiver=name_to_number(data['receiver']),
-        quantity=proto.EosAsset(
-            amount=amount,
-            symbol=symbol
-        )
+        quantity=parse_asset(data['quant'])
     )
 
+
 def parse_buy_rambytes(data):
-    return proto.EosActionBuyRamBytes(
+    return messages.EosActionBuyRamBytes(
         payer=name_to_number(data['payer']),
         receiver=name_to_number(data['receiver']),
         bytes=int(data['bytes'])
     )
 
+
 def parse_sell_ram(data):
-    return proto.EosActionSellRam(
+    return messages.EosActionSellRam(
         account=name_to_number(data['account']),
         bytes=int(data['bytes'])
     )
 
-def parse_delegate(data):
-    amount_net, symbol_net = asset_to_number(data['stake_net_quantity'])
-    amount_cpu, symbol_cpu = asset_to_number(data['stake_cpu_quantity'])
 
-    return proto.EosActionDelegate(
+def parse_delegate(data):
+    return messages.EosActionDelegate(
         sender=name_to_number(data['sender']),
         receiver=name_to_number(data['receiver']),
-        net_quantity=proto.EosAsset(
-            amount=amount_net,
-            symbol=symbol_net
-        ),
-        cpu_quantity=proto.EosAsset(
-            amount=amount_cpu,
-            symbol=symbol_cpu
-        ),
+        net_quantity=parse_asset(data['stake_net_quantity']),
+        cpu_quantity=parse_asset(data['stake_cpu_quantity']),
         transfer=bool(data['transfer'])
     )
 
-def parse_undelegate(data):
-    amount_net, symbol_net = asset_to_number(data['unstake_net_quantity'])
-    amount_cpu, symbol_cpu = asset_to_number(data['unstake_cpu_quantity'])
 
-    return proto.EosActionUndelegate(
+def parse_undelegate(data):
+    return messages.EosActionUndelegate(
         sender=name_to_number(data['sender']),
         receiver=name_to_number(data['receiver']),
-        net_quantity=proto.EosAsset(
-            amount=amount_net,
-            symbol=symbol_net
-        ),
-        cpu_quantity=proto.EosAsset(
-            amount=amount_cpu,
-            symbol=symbol_cpu
-        )
+        net_quantity=parse_asset(data['unstake_net_quantity']),
+        cpu_quantity=parse_asset(data['unstake_cpu_quantity'])
     )
 
+
 def parse_refund(data):
-    return proto.EosActionRefund(
+    return messages.EosActionRefund(
         owner=name_to_number(data['owner'])
     )
+
 
 def parse_updateauth(data):
     auth = parse_authorization(data['auth'])
 
-    return proto.EosActionUpdateAuth(
+    return messages.EosActionUpdateAuth(
         account=name_to_number(data['account']),
         permission=name_to_number(data['permission']),
         parent=name_to_number(data['parent']),
         auth=auth
     )
 
+
 def parse_deleteauth(data):
-    return proto.EosActionDeleteAuth(
+    return messages.EosActionDeleteAuth(
         account=name_to_number(data['account']),
         permission=name_to_number(data['permission'])
     )
 
+
 def parse_linkauth(data):
-    return proto.EosActionLinkAuth(
+    return messages.EosActionLinkAuth(
         account=name_to_number(data['account']),
         code=name_to_number(data['code']),
         type=name_to_number(data['type']),
         requirement=name_to_number(data['requirement'])
     )
 
+
 def parse_unlinkauth(data):
-    return proto.EosActionUnlinkAuth(
+    return messages.EosActionUnlinkAuth(
         account=name_to_number(data['account']),
         code=name_to_number(data['code']),
         type=name_to_number(data['type'])
     )
+
 
 def parse_authorization(data):
     keys = []
@@ -230,7 +192,7 @@ def parse_authorization(data):
         _t, _k = public_key_to_buffer(key['key'])
 
         keys.append(
-            proto.EosAuthorizationKey(
+            messages.EosAuthorizationKey(
                 type=_t,
                 key=_k,
                 address_n=[],
@@ -241,8 +203,8 @@ def parse_authorization(data):
     accounts = []
     for account in data['accounts']:
         accounts.append(
-            proto.EosAuthorizationAccount(
-                account=proto.EosPermissionLevel(
+            messages.EosAuthorizationAccount(
+                account=messages.EosPermissionLevel(
                     actor=name_to_number(account['permission']['actor']),
                     permission=name_to_number(account['permission']['permission'])
                 ),
@@ -253,13 +215,13 @@ def parse_authorization(data):
     waits = []
     for wait in data['waits']:
         waits.append(
-            proto.EosAuthorizationWait(
+            messages.EosAuthorizationWait(
                 wait_sec=int(wait['wait_sec']),
                 weight=int(wait['weight'])
             )
         )
 
-    return proto.EosAuthorization(
+    return messages.EosAuthorization(
         threshold=int(data['threshold']),
         keys=keys,
         accounts=accounts,
@@ -271,7 +233,7 @@ def parse_new_account(data):
     owner = parse_authorization(data['owner'])
     active = parse_authorization(data['active'])
 
-    return proto.EosActionNewAccount(
+    return messages.EosActionNewAccount(
         creator=name_to_number(data['creator']),
         name=name_to_number(data['name']),
         owner=owner,
@@ -280,13 +242,15 @@ def parse_new_account(data):
 
 
 def parse_unknown(data):
-    return proto.EosActionUnknown(
-        data_size=len(binascii.unhexlify(data)),
-        data_chunk=binascii.unhexlify(data)
+    data_bytes = bytes.fromhex(data)
+    return messages.EosActionUnknown(
+        data_size=len(data_bytes),
+        data_chunk=data_bytes
     )
 
+
 def parse_action(action):
-    txAction = proto.EosTxActionAck()
+    txAction = messages.EosTxActionAck()
     data = action['data']
 
     txAction.common = parse_common(action)
@@ -324,10 +288,9 @@ def parse_action(action):
     return txAction
 
 
-
 def parse_transaction_json(json):
     tx = type('Transaction', (object,), {})()
-    tx.chain_id = binascii.unhexlify(json['chain_id'])
+    tx.chain_id = bytes.fromhex(json['chain_id'])
 
     body = json['transaction']
 
@@ -347,9 +310,10 @@ def parse_transaction_json(json):
 
 # ====== Client functions ====== #
 
-@expect(proto.EosPublicKey)
+
+@expect(messages.EosPublicKey)
 def get_public_key(client, n, show_display=False, multisig=None):
-    response = client.call(proto.EosGetPublicKey(address_n=n, show_display=show_display))
+    response = client.call(messages.EosGetPublicKey(address_n=n, show_display=show_display))
     return response
 
 
@@ -357,7 +321,7 @@ def get_public_key(client, n, show_display=False, multisig=None):
 def sign_tx(client, address, transaction):
     tx = parse_transaction_json(transaction)
 
-    header = proto.EosTxHeader()
+    header = messages.EosTxHeader()
     header.expiration = tx.expiration
     header.ref_block_num = tx.ref_block_num
     header.ref_block_prefix = tx.ref_block_prefix
@@ -365,7 +329,7 @@ def sign_tx(client, address, transaction):
     header.max_cpu_usage_ms = tx.max_cpu_usage_ms
     header.delay_sec = tx.delay_sec
 
-    msg = proto.EosSignTx()
+    msg = messages.EosSignTx()
     msg.address_n = address
     msg.chain_id = tx.chain_id
     msg.header = header
@@ -374,7 +338,7 @@ def sign_tx(client, address, transaction):
     response = client.call(msg)
 
     try:
-        while isinstance(response, proto.EosTxActionRequest):
+        while isinstance(response, messages.EosTxActionRequest):
             response = client.call(parse_action(tx.actions.pop(0)))
     except IndexError:
         # pop from empty list
@@ -383,8 +347,7 @@ def sign_tx(client, address, transaction):
             "Reached end of operations without a signature.",
         ) from None
 
-    if not isinstance(response, proto.EosSignedTx):
-        raise CallException(proto.FailureType.UnexpectedMessage, response)
+    if not isinstance(response, messages.EosSignedTx):
+        raise CallException(messages.FailureType.UnexpectedMessage, response)
 
     return response
-
