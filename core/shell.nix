e7592a4c04c59cbadb32037c87b74350d08b27e5
Pavol Rusnak <pavol@rusnak.io>
2019-04-18 00:26:04 +0200

    nix: consolidate various shell.nix files into one

--- core/shell.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {};
-
-let
-  myPython = python3.withPackages(ps: [ps.trezor ps.pytest ps.flake8 ps.isort ps.black ps.Mako ps.munch ps.Pyro4 ps.pillow]);
-in
-  stdenv.mkDerivation {
-    name = "trezor-core-dev";
-    buildInputs = [ myPython protobuf scons gnumake gcc gcc-arm-embedded pkgconfig SDL2 SDL2_image autoflake clang-tools ];
-  }
--- python/shell.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {};
-
-let
-  myPython = python3.withPackages(p: [p.pytest p.black p.isort p.flake8 p.requests p.mnemonic p.construct p.pyblake2 p.mock p.ecdsa p.click p.libusb1 p.protobuf p.typing-extensions]);
-in
-  stdenv.mkDerivation {
-    name = "python-trezor-dev";
-    buildInputs = [ myPython autoflake protobuf ];
-  }
