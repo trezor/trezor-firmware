9f8ebcf1833e2f56c86df854c915385b0e9f43af
matejcik <ja@matejcik.cz>
2019-04-18 14:52:24 +0200

    build: make build scripts work from monorepo root
    incidentally fixes #9 and the dreaded non-bug #8

--- legacy/build.sh
+++ legacy/build.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 set -e
 
+cd "$(dirname $0)/.."
+
 BOOTLOADER_COMMIT=${1:-HEAD}
 FIRMWARE_COMMIT=${2:-HEAD}
 
 if [ "$BOOTLOADER_COMMIT" = "EMU" ]; then
-  export EMULATOR=1
+    export EMULATOR=1
 fi
 
 if [ "$EMULATOR" = 1 ]; then
-  IMAGE=trezor-mcu-emulator
+    IMAGE=trezor-mcu-emulator
 else
-  IMAGE=trezor-mcu-build
+    IMAGE=trezor-mcu-build
 fi
 
-docker build -t "$IMAGE" --build-arg EMULATOR=$EMULATOR .
-docker run -it -v $(pwd):/src:z --user="$(stat -c "%u:%g" .)" "$IMAGE" \
-  /src/script/fullbuild "$BOOTLOADER_COMMIT" "$FIRMWARE_COMMIT"
+USER=$(ls -lnd . | awk '{ print $3 }')
+GROUP=$(ls -lnd . | awk '{ print $4 }')
+
+docker build -t "$IMAGE" --build-arg EMULATOR=$EMULATOR legacy
+docker run -it -v $(pwd):/src:z --user="$USER:$GROUP" "$IMAGE" \
+    /src/legacy/script/fullbuild "$BOOTLOADER_COMMIT" "$FIRMWARE_COMMIT"
