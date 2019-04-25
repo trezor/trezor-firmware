5507688ce6226845fa75de88afeb7686e025df05
Pavol Rusnak <pavol@rusnak.io>
2019-04-25 14:11:37 +0200

    legacy: more build script fixing

--- legacy/build.sh
+++ legacy/build.sh
@@ -5,20 +5,11 @@ cd "$(dirname $0)/.."
 
 BOOTLOADER_COMMIT=${1:-HEAD}
 FIRMWARE_COMMIT=${2:-HEAD}
-
-if [ "$BOOTLOADER_COMMIT" = "EMU" ]; then
-    export EMULATOR=1
-fi
-
-if [ "$EMULATOR" = 1 ]; then
-    IMAGE=trezor-mcu-emulator
-else
-    IMAGE=trezor-mcu-build
-fi
+IMAGE=trezor-mcu-build
 
 USER=$(ls -lnd . | awk '{ print $3 }')
 GROUP=$(ls -lnd . | awk '{ print $4 }')
 
-docker build -t "$IMAGE" --build-arg EMULATOR=$EMULATOR legacy
+docker build -t "$IMAGE" .
 docker run -it -v $(pwd):/src:z --user="$USER:$GROUP" "$IMAGE" \
     /src/legacy/script/fullbuild "$BOOTLOADER_COMMIT" "$FIRMWARE_COMMIT"
