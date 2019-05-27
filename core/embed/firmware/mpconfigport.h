9bc3af08b2596d2e1b06cc2b5d6bc880bb1fcef9
Pavol Rusnak <pavol@rusnak.io>
2019-05-27 14:27:12 +0200

    core: update mpconfigport.h from upstream (for both stm32 and unix)

--- core/embed/firmware/mpconfigport.h
+++ core/embed/firmware/mpconfigport.h
@@ -57,6 +57,7 @@
 #define MICROPY_OPT_COMPUTED_GOTO   (1)
 #define MICROPY_OPT_CACHE_MAP_LOOKUP_IN_BYTECODE (0)
 #define MICROPY_OPT_MPZ_BITWISE     (1)
+#define MICROPY_OPT_MATH_FACTORIAL  (0)
 
 // Python internal features
 #define MICROPY_READER_VFS          (0)
@@ -85,6 +86,8 @@
 
 // control over Python builtins
 #define MICROPY_PY_FUNCTION_ATTRS   (1)
+#define MICROPY_PY_DESCRIPTORS      (0)
+#define MICROPY_PY_DELATTR_SETATTR  (0)
 #define MICROPY_PY_BUILTINS_STR_UNICODE (1)
 #define MICROPY_PY_BUILTINS_STR_CENTER (1)
 #define MICROPY_PY_BUILTINS_STR_PARTITION (0)
@@ -92,9 +95,11 @@
 #define MICROPY_PY_BUILTINS_MEMORYVIEW (1)
 #define MICROPY_PY_BUILTINS_FROZENSET (0)
 #define MICROPY_PY_BUILTINS_SLICE_ATTRS (1)
+#define MICROPY_PY_BUILTINS_ROUND_INT (0)
 #define MICROPY_PY_ALL_SPECIAL_METHODS (0)
-#define MICROPY_PY_BUILTINS_COMPILE (0)
-#define MICROPY_PY_BUILTINS_EXECFILE (0)
+#define MICROPY_PY_BUILTINS_COMPILE (MICROPY_ENABLE_COMPILER)
+#define MICROPY_PY_BUILTINS_EXECFILE (MICROPY_ENABLE_COMPILER)
+#define MICROPY_PY_BUILTINS_NOTIMPLEMENTED (1)
 #define MICROPY_PY_BUILTINS_INPUT   (0)
 #define MICROPY_PY_BUILTINS_POW3    (0)
 #define MICROPY_PY_BUILTINS_HELP    (0)
@@ -102,29 +107,37 @@
 #define MICROPY_PY_BUILTINS_HELP_MODULES (0)
 #define MICROPY_PY_MICROPYTHON_MEM_INFO (1)
 #define MICROPY_PY_ARRAY_SLICE_ASSIGN (1)
-#define MICROPY_PY_COLLECTIONS        (0)
+#define MICROPY_PY_COLLECTIONS       (0)
+#define MICROPY_PY_COLLECTIONS_DEQUE (0)
 #define MICROPY_PY_COLLECTIONS_ORDEREDDICT (0)
 #define MICROPY_PY_MATH_SPECIAL_FUNCTIONS (0)
+#define MICROPY_PY_MATH_ISCLOSE     (0)
+#define MICROPY_PY_MATH_FACTORIAL   (0)
 #define MICROPY_PY_CMATH            (0)
 #define MICROPY_PY_IO               (0)
-#define MICROPY_PY_IO_FILEIO        (0)
+#define MICROPY_PY_IO_IOBASE        (0)
+#define MICROPY_PY_IO_FILEIO        (MICROPY_VFS_FAT) // because mp_type_fileio/textio point to fatfs impl
 #define MICROPY_PY_SYS_MAXSIZE      (0)
 #define MICROPY_PY_SYS_EXIT         (0)
 #define MICROPY_PY_SYS_STDFILES     (0)
 #define MICROPY_PY_SYS_STDIO_BUFFER (0)
+#define MICROPY_PY_SYS_PLATFORM     "trezor"
 #define MICROPY_PY_UERRNO           (0)
 #ifndef MICROPY_PY_THREAD
 #define MICROPY_PY_THREAD           (0)
 #endif
-#define MICROPY_PY_THREAD_GIL       (0)
 
 // extended modules
 #define MICROPY_PY_UCTYPES          (1)
 #define MICROPY_PY_UZLIB            (0)
 #define MICROPY_PY_UJSON            (0)
 #define MICROPY_PY_URE              (0)
+#define MICROPY_PY_URE_SUB          (0)
 #define MICROPY_PY_UHEAPQ           (0)
 #define MICROPY_PY_UHASHLIB         (0)
+#define MICROPY_PY_UHASHLIB_MD5     (0)
+#define MICROPY_PY_UHASHLIB_SHA1    (0)
+#define MICROPY_PY_UCRYPTOLIB       (0)
 #define MICROPY_PY_UBINASCII        (1)
 #define MICROPY_PY_UBINASCII_CRC32  (0)
 #define MICROPY_PY_URANDOM          (0)
@@ -132,7 +145,11 @@
 #define MICROPY_PY_USELECT          (0)
 #define MICROPY_PY_UTIMEQ           (1)
 #define MICROPY_PY_UTIME_MP_HAL     (1)
+#define MICROPY_PY_OS_DUPTERM       (0)
+#define MICROPY_PY_LWIP_SOCK_RAW    (0)
 #define MICROPY_PY_MACHINE          (0)
+#define MICROPY_PY_UWEBSOCKET       (0)
+#define MICROPY_PY_WEBREPL          (0)
 #define MICROPY_PY_FRAMEBUF         (0)
 #define MICROPY_PY_USOCKET          (0)
 #define MICROPY_PY_NETWORK          (0)
@@ -192,7 +209,6 @@ typedef long mp_off_t;
 
 #define MICROPY_HW_BOARD_NAME "TREZORv2"
 #define MICROPY_HW_MCU_NAME "STM32F427xx"
-#define MICROPY_PY_SYS_PLATFORM "trezor"
 #define MICROPY_HW_HAS_SDCARD 1
 
 // There is no classical C heap in bare-metal ports, only Python
