50f163661d7d31216d1f721e8dc251927d767888
Tomas Susanka <tsusanka@gmail.com>
2019-06-28 12:26:34 +0200

    core/slip39: modify wording on success screen
    (cherry picked from commit 5aa592a69af8bdf14cb98f01941174b82c753b34)

--- core/src/apps/management/backup_device.py
+++ core/src/apps/management/backup_device.py
@@ -12,18 +12,19 @@ async def backup_device(ctx, msg):
     if not storage.needs_backup():
         raise wire.ProcessError("Seed already backed up")
 
+    mnemonic_secret, mnemonic_type = mnemonic.get()
+    slip39 = mnemonic_type == mnemonic.TYPE_SLIP39
+
     # warn user about mnemonic safety
-    await layout.show_backup_warning(ctx, "Back up your seed", "I understand")
+    await layout.show_backup_warning(ctx, "Back up your seed", "I understand", slip39)
 
     storage.set_unfinished_backup(True)
     storage.set_backed_up()
 
-    mnemonic_secret, mnemonic_type = mnemonic.get()
-    if mnemonic_type == mnemonic.TYPE_BIP39:
-        await layout.bip39_show_and_confirm_mnemonic(ctx, mnemonic_secret.decode())
-
-    elif mnemonic_type == mnemonic.TYPE_SLIP39:
+    if slip39:
         await backup_slip39_wallet(ctx, mnemonic_secret)
+    else:
+        await layout.bip39_show_and_confirm_mnemonic(ctx, mnemonic_secret.decode())
 
     storage.set_unfinished_backup(False)
 
