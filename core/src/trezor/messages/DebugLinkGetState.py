0890f68c0c61a5839bea665905f571ad124dba41
matejcik <ja@matejcik.cz>
2019-07-30 17:57:59 +0200

    core: use channels to give feedback over debuglink
    all debug input signals are now channels, and DebugLinkDecision handler
    waits until the input was consumed. This means that the input events are
    queued; originally, if an input event arrived before the previous was
    consumed, the previous input would be lost.
    
    reset words and their positions are now also channels, and
    DebugLinkGetState can wait for their updates, if required

--- core/src/trezor/messages/DebugLinkGetState.py
+++ core/src/trezor/messages/DebugLinkGetState.py
@@ -12,3 +12,18 @@ if __debug__:
 
 class DebugLinkGetState(p.MessageType):
     MESSAGE_WIRE_TYPE = 101
+
+    def __init__(
+        self,
+        wait_word_list: bool = None,
+        wait_word_pos: bool = None,
+    ) -> None:
+        self.wait_word_list = wait_word_list
+        self.wait_word_pos = wait_word_pos
+
+    @classmethod
+    def get_fields(cls) -> Dict:
+        return {
+            1: ('wait_word_list', p.BoolType, 0),
+            2: ('wait_word_pos', p.BoolType, 0),
+        }
