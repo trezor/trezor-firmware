8190bf381923e418c9fd6bfeb62583141a3ce1f1
Pavol Rusnak <pavol@rusnak.io>
2019-07-31 12:04:14 +0200

    legacy: fix oledSCA in simple recovery
    (cherry picked from commit ddd3e9c577eb2df31c2b2174db34bd85587e1e74)

--- legacy/firmware/recovery.c
+++ legacy/firmware/recovery.c
@@ -448,16 +448,29 @@ void next_word(void) {
   if (word_pos == 0) {
     const char *const *wl = mnemonic_wordlist();
     strlcpy(fake_word, wl[random_uniform(2048)], sizeof(fake_word));
-    layoutDialogSwipe(&bmp_icon_info, NULL, NULL, NULL,
-                      _("Please enter the word"), NULL, fake_word, NULL,
-                      _("on your computer"), NULL);
+
+    layoutLast = layoutDialogSwipe;
+    layoutSwipe();
+    oledDrawStringCenter(OLED_WIDTH / 2, 8, _("Please enter the word"), FONT_STANDARD);
+    oledDrawStringCenter(OLED_WIDTH / 2, 24, fake_word, FONT_FIXED | FONT_DOUBLE);
+    oledDrawStringCenter(OLED_WIDTH / 2, 48, _("on your computer"), FONT_STANDARD);
+    // 30 is the maximum pixels used for a pixel row in the BIP39 word "abstract"
+    oledSCA(24 - 2, 24 + 15 + 2, 30);
+    oledInvert(0, 24 - 2, OLED_WIDTH - 1, 24 + 15 + 2);
+    oledRefresh();
   } else {
     fake_word[0] = 0;
     char desc[] = "##th word";
     format_number(desc, word_pos);
-    layoutDialogSwipe(&bmp_icon_info, NULL, NULL, NULL, _("Please enter the"),
-                      NULL, (word_pos < 10 ? desc + 1 : desc), NULL,
-                      _("of your mnemonic"), NULL);
+    layoutLast = layoutDialogSwipe;
+    layoutSwipe();
+    oledDrawStringCenter(OLED_WIDTH / 2, 8, _("Please enter the"), FONT_STANDARD);
+    oledDrawStringCenter(OLED_WIDTH / 2, 24, (word_pos < 10 ? desc + 1 : desc), FONT_FIXED | FONT_DOUBLE);
+    oledDrawStringCenter(OLED_WIDTH / 2, 48, _("of your mnemonic"), FONT_STANDARD);
+    // 30 is the maximum pixels used for a pixel row in the BIP39 word "abstract"
+    oledSCA(24 - 2, 24 + 15 + 2, 30);
+    oledInvert(0, 24 - 2, OLED_WIDTH - 1, 24 + 15 + 2);
+    oledRefresh();
   }
   recovery_request();
 }
