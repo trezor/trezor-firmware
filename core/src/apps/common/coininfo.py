dcf7411979ade9d893bd9cdcffbe325c3a10a4a2
Ryxor <team@umbru.io>
2019-08-22 13:46:13 +1200

    Add Umbru

--- core/src/apps/common/coininfo.py
+++ core/src/apps/common/coininfo.py
@@ -1526,6 +1526,29 @@ def by_name(name: str) -> CoinInfo:
             curve_name='secp256k1',
             confidential_assets=None,
         )
+    elif name == "Umbru":
+        return CoinInfo(
+            coin_name=name,
+            coin_shortcut="UMBRU",
+            address_type=68,
+            address_type_p2sh=5,
+            maxfee_kb=100000,
+            signed_message_header="DarkCoin Signed Message:\n",
+            xpub_magic=0x0488b21e,
+            xpub_magic_segwit_p2sh=None,
+            xpub_magic_segwit_native=None,
+            bech32_prefix=None,
+            cashaddr_prefix=None,
+            slip44=395,
+            segwit=False,
+            fork_id=None,
+            force_bip143=False,
+            bip115=False,
+            decred=False,
+            negative_fee=False,
+            curve_name='secp256k1',
+            confidential_assets=None,
+        )
     elif name == "VIPSTARCOIN":
         return CoinInfo(
             coin_name=name,
