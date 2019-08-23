9ce2fceabe5c1a4da68de6a61336a8a6e986f5cc
matejcik <ja@matejcik.cz>
2019-08-23 12:48:23 +0200

    python: make gen_check fail if coins.json are outdated

--- python/helper-scripts/build-coins-json.sh
+++ python/helper-scripts/build-coins-json.sh
@@ -14,7 +14,8 @@ if [ "$1" == "--check" ]; then
     TMP=$(mktemp)
     $BUILD_COINS_AT $TMP
     diff -q $DEST $TMP
-    if [ "$?" -ne 0 ]; then
+    ERR=$?
+    if [ "$ERR" -ne 0 ]; then
         echo "Please run $0"
     fi
     rm $TMP
