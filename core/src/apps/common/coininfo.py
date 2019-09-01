64034c6d7dcaf81fad3298b08c95181c26f07c28
TheCreator <31379149+Flurbos@users.noreply.github.com>
2019-09-01 17:47:58 +1000

    common: enable UNO (#472)

--- core/src/apps/common/coininfo.py
+++ core/src/apps/common/coininfo.py
@@ -1553,6 +1553,29 @@ def by_name(name: str) -> CoinInfo:
                 curve_name='secp256k1',
                 confidential_assets=None,
             )
+        elif name == "Unobtanium":
+            return CoinInfo(
+                coin_name=name,
+                coin_shortcut="UNO",
+                address_type=130,
+                address_type_p2sh=30,
+                maxfee_kb=2000000,
+                signed_message_header="Unobtanium Signed Message:\n",
+                xpub_magic=0x0488b21e,
+                xpub_magic_segwit_p2sh=None,
+                xpub_magic_segwit_native=None,
+                bech32_prefix=None,
+                cashaddr_prefix=None,
+                slip44=92,
+                segwit=False,
+                fork_id=None,
+                force_bip143=False,
+                bip115=False,
+                decred=False,
+                negative_fee=False,
+                curve_name='secp256k1',
+                confidential_assets=None,
+            )
         elif name == "VIPSTARCOIN":
             return CoinInfo(
                 coin_name=name,
