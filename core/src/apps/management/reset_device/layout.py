6163e64aa9534ee36bc8f141a6b882b7c0c1a8d3
ciny <stanislav.marcinko@satoshilabs.com>
2019-09-18 11:44:55 +0200

    core: rename trezor.ui.slip39 to more general name num_input

--- core/src/apps/management/reset_device/layout.py
+++ core/src/apps/management/reset_device/layout.py
@@ -7,7 +7,7 @@ from trezor.ui.button import Button, ButtonDefault
 from trezor.ui.checklist import Checklist
 from trezor.ui.info import InfoConfirm
 from trezor.ui.scroll import Paginated
-from trezor.ui.slip39 import NumInput
+from trezor.ui.num_input import NumInput
 from trezor.ui.text import Text
 
 from apps.common.confirm import confirm, hold_to_confirm, require_confirm
