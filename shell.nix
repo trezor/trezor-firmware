552e6c5d62072bf0687f4edef1b3e008d15724b9
Tomas Susanka <tsusanka@gmail.com>
2019-09-24 10:18:42 +0200

    nix: modify shell.nix to work with pipenv

--- shell.nix
+++ shell.nix
@@ -1,44 +1,29 @@
 with import <nixpkgs> {};
 
-let
-  MyPython = python3.withPackages(ps: [
-    ps.Mako
-    ps.Pyro4
-    ps.black
-    ps.coverage
-    ps.ed25519
-    ps.flake8
-    ps.flaky
-    ps.graphviz
-    ps.isort
-    ps.mock
-    ps.munch
-    ps.pillow
-    ps.pytest
-    ps.pytest-random-order
-    ps.trezor
-  ]);
-in
-  stdenv.mkDerivation {
-    name = "trezor-firmware-dev";
-    buildInputs = [
-      MyPython
-      SDL2
-      SDL2_image
-      autoflake
-      check
-      clang-tools
-      gcc
-      gcc-arm-embedded
-      gnumake
-      graphviz
-      openssl
-      pipenv
-      pkgconfig
-      protobuf
-      python2Packages.demjson
-      scons
-      valgrind
-      zlib
-    ];
-  }
+stdenv.mkDerivation {
+  name = "trezor-firmware-dev";
+  buildInputs = [
+    SDL2
+    SDL2_image
+    autoflake
+    check
+    clang-tools
+    gcc
+    gcc-arm-embedded
+    gnumake
+    graphviz
+    libusb1
+    openssl
+    pipenv
+    pkgconfig
+    protobuf
+    scons
+    valgrind
+    zlib
+  ];
+  LD_LIBRARY_PATH="${libusb1}/lib";
+  shellHook = ''
+    pipenv shell
+    exit
+  '';
+}
