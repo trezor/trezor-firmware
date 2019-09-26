e4ef2a3b78815c49547bf637a849baddb4948694
Andrew Kozlik <andrew.kozlik@gmail.com>
2019-09-26 17:42:35 +0200

    u2f: Store hashes of U2F application parameters instead of pre-images.
    (cherry picked from commit e4c13b6357dd89ef1f0ef8f741a61d14a1b9cdaa)

--- common/defs/webauthn/gen.py
+++ common/defs/webauthn/gen.py
@@ -16,35 +16,39 @@ def c_bytes(h):
 
 
 def gen_core(data):
-    print("_knownapps = {")
+    print("# contents generated via script in")
+    print("# trezor-common/defs/webauthn/gen.py")
+    print("# do not edit manually")
+    print()
+    print("knownapps = {")
     print("    # U2F")
     for d in data:
-        for url in d.get("u2f", []):
+        for appid in d.get("u2f", []):
             label = d["label"]
-            print('    "%s": {"label": "%s", "use_sign_count": True},' % (url, label))
+            h = bytes.fromhex(appid)
+            print("    %s: {" % h)
+            print('        "label": "%s",' % label)
+            print('        "use_sign_count": True,')
+            print("    },")
     print("    # WebAuthn")
     for d in data:
         for origin in d.get("webauthn", []):
+            h = sha256(origin.encode()).digest()
             label, use_sign_count = (d["label"], d.get("use_sign_count", None))
-            if use_sign_count is None:
-                print('    "%s": {"label": "%s"},' % (origin, label))
-            else:
-                print(
-                    '    "%s": {"label": "%s", "use_sign_count": %s},'
-                    % (origin, label, use_sign_count)
-                )
+            print("    %s: {" % h)
+            print('        "label": "%s",' % label)
+            if use_sign_count is not None:
+                print('        "use_sign_count": %s,' % use_sign_count)
+            print("    },")
     print("}")
 
 
 def gen_mcu(data):
     for d in data:
-        for url in d.get("u2f", []):
+        for appid in d.get("u2f", []):
             label = d["label"]
-            h = sha256(url.encode()).digest()
-            print(
-                '\t{\n\t\t// U2F: %s\n\t\t%s,\n\t\t"%s"\n\t},'
-                % (url, c_bytes(h), label)
-            )
+            h = bytes.fromhex(appid)
+            print('\t{\n\t\t// U2F\n\t\t%s,\n\t\t"%s"\n\t},' % (c_bytes(h), label))
         for origin in d.get("webauthn", []):
             label = d["label"]
             h = sha256(origin.encode()).digest()
