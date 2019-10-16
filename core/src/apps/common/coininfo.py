d28bc3c3a679af3bc50b3c45ba649a0549fca2f5
Pavol Rusnak <pavol@rusnak.io>
2019-10-16 10:18:33 +0000

    common/defs: remove BSD - website does not exist, maintainer not reachable

--- core/src/apps/common/coininfo.py
+++ core/src/apps/common/coininfo.py
@@ -472,29 +472,6 @@ def by_name(name: str) -> CoinInfo:
                 curve_name='secp256k1',
                 confidential_assets=None,
             )
-        elif name == "Bitsend":
-            return CoinInfo(
-                coin_name=name,
-                coin_shortcut="BSD",
-                address_type=102,
-                address_type_p2sh=5,
-                maxfee_kb=1000000,
-                signed_message_header="Bitsend Signed Message:\n",
-                xpub_magic=0x0488b21e,
-                xpub_magic_segwit_p2sh=0x049d7cb2,
-                xpub_magic_segwit_native=0x04b24746,
-                bech32_prefix="bsd",
-                cashaddr_prefix=None,
-                slip44=91,
-                segwit=True,
-                fork_id=None,
-                force_bip143=False,
-                bip115=False,
-                decred=False,
-                negative_fee=False,
-                curve_name='secp256k1',
-                confidential_assets=None,
-            )
         elif name == "BlockStamp":
             return CoinInfo(
                 coin_name=name,
