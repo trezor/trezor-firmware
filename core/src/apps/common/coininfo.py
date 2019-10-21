178d58c76353a61315372830ea54ba331a3eb119
Pavol Rusnak <pavol@rusnak.io>
2019-10-21 09:48:19 +0000

    common/defs: remove BTDX as it has no Trezor compatible wallet

--- core/src/apps/common/coininfo.py
+++ core/src/apps/common/coininfo.py
@@ -265,29 +265,6 @@ def by_name(name: str) -> CoinInfo:
                 curve_name='secp256k1',
                 confidential_assets=None,
             )
-        elif name == "Bitcloud":
-            return CoinInfo(
-                coin_name=name,
-                coin_shortcut="BTDX",
-                address_type=25,
-                address_type_p2sh=5,
-                maxfee_kb=1000000,
-                signed_message_header="Diamond Signed Message:\n",
-                xpub_magic=0x0488b21e,
-                xpub_magic_segwit_p2sh=None,
-                xpub_magic_segwit_native=None,
-                bech32_prefix=None,
-                cashaddr_prefix=None,
-                slip44=218,
-                segwit=False,
-                fork_id=None,
-                force_bip143=False,
-                bip115=False,
-                decred=False,
-                negative_fee=False,
-                curve_name='secp256k1',
-                confidential_assets=None,
-            )
         elif name == "Bcash":
             return CoinInfo(
                 coin_name=name,
