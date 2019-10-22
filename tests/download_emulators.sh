bfa50470122bb9c31ed1f73eb6f80b6c6bc44118
Tomas Susanka <tsusanka@gmail.com>
2019-10-22 10:54:18 +0200

    tests/upgrade tests: patch emulators on NixOS

--- tests/download_emulators.sh
+++ tests/download_emulators.sh
@@ -3,6 +3,10 @@ SITE="https://firmware.corp.sldev.cz/upgrade_tests/"
 cd "$(dirname "$0")"
 
 # download all emulators without index files, without directories and only if not present
-wget --no-verbose --no-clobber --no-parent --cut-dirs=2 --no-host-directories --recursive --reject "index.html*" -P emulators/ $SITE
+wget -e robots=off --no-verbose --no-clobber --no-parent --cut-dirs=2 --no-host-directories --recursive --reject "index.html*" -P emulators/ $SITE
 
 chmod u+x emulators/trezor-emu-*
+
+if [ -f /etc/NIXOS ]; then
+  cd emulators && nix-shell --run "autoPatchelf trezor-emu*"
+fi
