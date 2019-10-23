a81b46fb8ed38d24332eacd06339a12090127b22
Fernando Sobreira <fbsobreira@gmail.com>
2019-10-22 22:02:30 -0300

    Fix get address test

--- tests/device_tests/test_msg_tron_get_address.py
+++ tests/device_tests/test_msg_tron_get_address.py
@@ -16,61 +16,57 @@
 
 import pytest
 
-from trezorlib import debuglink, messages as proto, tron
+from trezorlib import messages as proto, tron
 from trezorlib.tools import CallException, parse_path
 
+TRON_DEFAULT_PATH = "m/44'/195'/0'/0/0"
+MNEMONIC12 = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+MNEMONICALLALLALL = "all all all all all all all all all all all all"
 
-@pytest.mark.tron
-@pytest.mark.skip_t2
+pytestmark = [
+    pytest.mark.altcoin,
+    pytest.mark.tron,
+]
+
+
 class TestMsgTronGetAddress:
-    def test_tron_getaddressAllAllAll(self):
-        self.setup_mnemonic_allallall()
-
+    @pytest.mark.setup_client(mnemonic=MNEMONICALLALLALL)
+    def test_tron_getaddressAllAllAll(self, client):
         assert (
-            tron.get_address(self.client, parse_path("m/44'/195'/0'/0/0"))
+            tron.get_address(client, parse_path("m/44'/195'/0'/0/0"))
             == "TY72iA3SBtrds3QLYsS7LwYfkzXwAXCRWT"
         )
         assert (
-            tron.get_address(self.client, parse_path("m/44'/195'/1'/0/0"))
+            tron.get_address(client, parse_path("m/44'/195'/1'/0/0"))
             == "TNPgkSKfz2xS39fcyfrouz7QPbkkJaDLYv"
         )
         assert (
-            tron.get_address(self.client, parse_path("m/44'/195'/1'/0/1"))
+            tron.get_address(client, parse_path("m/44'/195'/1'/0/1"))
             == "TFgADKfQF6nUZjrumi2qW3XugZ2sJ7Yf2i"
         )
 
-    def test_tron_getaddress(self):
-        debuglink.load_device_by_mnemonic(
-            self.client,
-            mnemonic="abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about",
-            pin="",
-            passphrase_protection=False,
-            label="test",
-            language="english",
-        )
-
+    @pytest.mark.setup_client(mnemonic=MNEMONIC12)
+    def test_tron_getaddress(self, client):
         assert (
-            tron.get_address(self.client, parse_path("m/44'/195'/0'/0/0"))
+            tron.get_address(client, parse_path("m/44'/195'/0'/0/0"))
             == "TUEZSdKsoDHQMeZwihtdoBiN46zxhGWYdH"
         )
         assert (
-            tron.get_address(self.client, parse_path("m/44'/195'/1'/0/0"))
+            tron.get_address(client, parse_path("m/44'/195'/1'/0/0"))
             == "TLrpNTBuCpGMrB9TyVwgEhNVRhtWEQPHh4"
         )
         assert (
-            tron.get_address(self.client, parse_path("m/44'/195'/2'/0/0"))
+            tron.get_address(client, parse_path("m/44'/195'/2'/0/0"))
             == "TZJ9qkoxUB1SGdbtChgAjUphBmkJwAeBaW"
         )
         assert (
-            tron.get_address(self.client, parse_path("m/44'/195'/1'/0/1"))
+            tron.get_address(client, parse_path("m/44'/195'/1'/0/1"))
             == "TUT9qMmtJtnjJhpazPaLraWSTaThhBpWyR"
         )
 
-    def test_tron_get_address_fail(self):
-        self.setup_mnemonic_nopin_nopassphrase()
-
+    def test_tron_get_address_fail(self, client):
         with pytest.raises(CallException) as exc:
-            tron.get_address(self.client, parse_path("m/0/1"))
+            tron.get_address(client, parse_path("m/0/1"))
 
         assert exc.value.args[0] == proto.FailureType.DataError
         assert exc.value.args[1].endswith("Forbidden key path")
