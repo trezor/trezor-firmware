7476e9463f65165da081c8f582de0ab6a632c7ba
Pavol Rusnak <pavol@rusnak.io>
2019-11-09 18:54:13 +0000

    legacy/bootloader: don't include FONT_FIXED

--- legacy/gen/fonts.c
+++ legacy/gen/fonts.c
@@ -1,14 +1,14 @@
 #include "fonts.h"
 
-#define FONTS 2
-
 const uint8_t *const font_data[FONTS][128 - 32] = {
     {
 #include "font.inc"
     },
+#ifndef FONT_SKIP_FIXED
     {
 #include "fontfixed.inc"
     },
+#endif
 };
 
 int fontCharWidth(uint8_t font, uint8_t c) {
--- legacy/gen/fonts.h
+++ legacy/gen/fonts.h
@@ -5,10 +5,17 @@
 
 #define FONT_HEIGHT 8
 #define FONT_STANDARD 0
+
+#ifndef FONT_SKIP_FIXED
 #define FONT_FIXED 1
+#define FONTS 2
+#else
+#define FONTS 1
+#endif
+
 #define FONT_DOUBLE 0x80
 
-extern const uint8_t *const font_data[2][128 - 32];
+extern const uint8_t *const font_data[FONTS][128 - 32];
 
 int fontCharWidth(uint8_t font, uint8_t c);
 const uint8_t *fontCharData(uint8_t font, uint8_t c);
