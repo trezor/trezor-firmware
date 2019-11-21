e4495e84db1ce214c06d324bfd4a11409988e3e9
MIPPL <mip.pl@hotmail.com>
2019-11-21 09:18:47 +0100

    add Sinovate data to definitions

--- core/src/apps/common/coininfo.py
+++ core/src/apps/common/coininfo.py
@@ -1392,6 +1392,29 @@ def by_name(name: str) -> CoinInfo:
                 curve_name='secp256k1',
                 confidential_assets=None,
             )
+        elif name == "Sinovate":
+            return CoinInfo(
+                coin_name=name,
+                coin_shortcut="SIN",
+                address_type=63,
+                address_type_p2sh=5,
+                maxfee_kb=1000000,
+                signed_message_header="Sinovate Signed Message:\n",
+                xpub_magic=0x0‭488B21E‬,
+                xpub_magic_segwit_p2sh=None,
+                xpub_magic_segwit_native=None,
+                bech32_prefix=None,
+                cashaddr_prefix=None,
+                slip44=715,
+                segwit=False,
+                fork_id=None,
+                force_bip143=False,
+                bip115=False,
+                decred=False,
+                negative_fee=False,
+                curve_name='secp256k1',
+                confidential_assets=None,
+            )
         elif name == "SmartCash":
             return CoinInfo(
                 coin_name=name,
