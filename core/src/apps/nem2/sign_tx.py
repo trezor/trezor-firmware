5f60371b0cc76aa6fe5523a80e19a8343980ed0e
Darren Langley <darren@downrightdigital.io>
2019-11-27 22:04:12 +1000

    fixed returned hash

--- core/src/apps/nem2/sign_tx.py
+++ core/src/apps/nem2/sign_tx.py
@@ -1,4 +1,5 @@
 from trezor.crypto.curve import ed25519
+from trezor.crypto.hashlib import sha3_256
 from trezor.messages.NEM2SignedTx import NEM2SignedTx
 from trezor.messages.NEM2SignTx import NEM2SignTx
 from ubinascii import unhexlify, hexlify
@@ -6,7 +7,7 @@ from ubinascii import unhexlify, hexlify
 from apps.common import seed
 from apps.common.paths import validate_path
 from apps.nem2 import CURVE, transfer, mosaic
-from apps.nem2.helpers import NEM2_HASH_ALG, check_path
+from apps.nem2.helpers import NEM2_HASH_ALG, check_path, NEM2_TRANSACTION_TYPE_AGGREGATE_BONDED, NEM2_TRANSACTION_TYPE_AGGREGATE_COMPLETE
 from apps.nem2.validators import validate
 
 
@@ -72,24 +73,28 @@ async def sign_tx(ctx, msg: NEM2SignTx, keychain):
             )
 
     # https://nemtech.github.io/concepts/transaction.html#signing-a-transaction    
-    print("PUBLIC ", hexlify(public_key))
-    print("PRIVATE ", hexlify(node.private_key()))
-
     # signing bytes (all tx data expect size, signature and signer)
-    # everything after the first 100 bytes of serialised transaction
+    # everything after the first 108 bytes of serialised transaction
     signing_bytes = tx[108:]    
     
+    # sign tx
     generation_hash_bytes = unhexlify(msg.generation_hash)
-
     signature = ed25519.sign(node.private_key(), generation_hash_bytes + signing_bytes, NEM2_HASH_ALG)    
-    print("signing_bytes ", hexlify(generation_hash_bytes + signing_bytes))
 
+    # prepare payload
     payload = tx[:8] + signature + public_key + tx[104:]
-    print("payload ", hexlify(payload))
 
-    hash_content = signature[:32] + node.public_key() + generation_hash_bytes + tx[104:]
+    # prepare hash content
+    payload_without_header = payload[108:]
+    data_bytes = generation_hash_bytes + payload_without_header
+    if msg.transaction.type == NEM2_TRANSACTION_TYPE_AGGREGATE_BONDED or msg.transaction.type == NEM2_TRANSACTION_TYPE_AGGREGATE_COMPLETE:
+        data_bytes = generation_hash_bytes + payload_without_header[0:52]
+
+    first_half_of_sig = payload[8:40]
+    signer = payload[72:104]
+    hash_bytes = first_half_of_sig + signer + data_bytes    
 
     resp = NEM2SignedTx()
     resp.payload = payload
-    resp.hash = ed25519.sign(node.private_key(), hash_content, NEM2_HASH_ALG)
+    resp.hash = sha3_256(hash_bytes, keccak=True).digest()
     return resp
--- tests/device_tests/test_msg_nem2_signtx_mosaic_definition.py
+++ tests/device_tests/test_msg_nem2_signtx_mosaic_definition.py
@@ -28,12 +28,12 @@ class TestMsgNEM2SignTxMosaics:
     def test_nem2_signtx_mosaic_definition(self, client):
         tx = nem2.sign_tx(
             client,
-            parse_path("m/44'/43'/0'"),
+            parse_path("m/44'/43'/0'/0'/0'"),
             {
                 "type": nem2.TYPE_MOSAIC_DEFINITION,
                 "network_type": nem2.NETWORK_TYPE_TEST_NET,
                 "generation_hash": "9F1979BEBA29C47E59B40393ABB516801A353CFC0C18BC241FEDE41939C907E7",
-                "version": 1,
+                "version": 38913,
                 "max_fee": "100",
                 "deadline": "113728610090",
                 "nonce": 3095715558,
@@ -51,6 +51,6 @@ class TestMsgNEM2SignTxMosaics:
             == "9600000000000000FD1497976AFA3155798512F9EF0E8B534DEFCFEF6A00700CC5CF08363B77DA737ADC5E9BC4999A864A6830E9C54BD1FC0A4330206EDCFD153C999199341D9C0CA8F70E4D5C357273968B12417AE8B742E35E530623C2488D0A73306B412715000000000001984D4164000000000000002ADFC07A1A00000019C6809AB2C4650B7B00000000000000E6DE84B80764"
         )
         assert (
-            tx.hash.hex()
-            == "C271DFA2397D879ACE264A8CECE03E2D8E1E01302D2A8B86E41A8ED440A12955"
+            tx.hash.hex().upper()
+            == "3F5E26802ABA3D0598A1930BD80BD711D6797887135D318B15EB1EC223733959"
         )
