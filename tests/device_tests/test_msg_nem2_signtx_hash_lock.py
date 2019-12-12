96612ff9cfd46bfff095fa814e5997a58fee282e
Chris Martin <chris@crunchycloud.io>
2019-12-12 22:59:47 +1000

    update hash lock device test to use proper HD wallet path

--- tests/device_tests/test_msg_nem2_signtx_hash_lock.py
+++ tests/device_tests/test_msg_nem2_signtx_hash_lock.py
@@ -37,19 +37,19 @@ class TestMsgNEM2SignTxHashLock:
         #     "9F1979BEBA29C47E59B40393ABB516801A353CFC0C18BC241FEDE41939C907E7",
         #     {
         #         "type": nem2.TYPE_AGGREGATE_COMPLETE,
-        #         "network": nem2.NETWORK_TYPE_MIJIN_TEST,
+        #         "network": nem2.NETWORK_TYPE_TEST_NET,
         #         "version": 36865,
         #         "maxFee": "100",
         #         "deadline": "113728610090",
         #         "innerTransactions": [
         #             {
-        #                 "network": nem2.NETWORK_TYPE_MIJIN_TEST,
+        #                 "network": nem2.NETWORK_TYPE_TEST_NET,
         #                 "type": nem2.TYPE_TRANSACTION_TRANSFER,
         #                 "version": 36865,
         #                 "publicKey": "BBCE621C6DAB03ECB620356EC029BF74B417E734A699970ADFE504C0CE9EE8AD",
         #                 "recipientAddress": {
         #                     "address": "SAAXKRVD6HLAN6IHYM7YWTS3W2BGYXCRBNSA4Q6Y",
-        #                     "networkType": nem2.NETWORK_TYPE_MIJIN_TEST
+        #                     "networkType": nem2.NETWORK_TYPE_TEST_NET
         #                 },
         #                 "message": {
         #                     "payload": "send 100 cat.currency to distributor",
@@ -63,7 +63,7 @@ class TestMsgNEM2SignTxHashLock:
         #                 ]
         #             },
         #             {
-        #                 "network": nem2.NETWORK_TYPE_MIJIN_TEST,
+        #                 "network": nem2.NETWORK_TYPE_TEST_NET,
         #                 "type": nem2.TYPE_MOSAIC_DEFINITION,
         #                 "version": 36865,
         #                 "publicKey": "596FEAB15D98BFD75F1743E9DC8A36474A3D0C06AE78ED134C231336C38A6297",
@@ -79,12 +79,12 @@ class TestMsgNEM2SignTxHashLock:
 
         signed_hash_lock_tx = nem2.sign_tx(
             client,
-            parse_path("m/44'/43'/0'/0'/0'"),
+            parse_path("m/44'/43'/0'"),
             "9F1979BEBA29C47E59B40393ABB516801A353CFC0C18BC241FEDE41939C907E7",
             {
                 "type": nem2.TYPE_HASH_LOCK,
-                "network": nem2.NETWORK_TYPE_MIJIN_TEST,
-                "version": 36865,
+                "network": nem2.NETWORK_TYPE_TEST_NET,
+                "version": 38913,
                 "maxFee": "0",
                 "deadline": "113728610090",
                 "mosaic": {
@@ -92,15 +92,15 @@ class TestMsgNEM2SignTxHashLock:
                     "id": "85BBEA6CC462B244"
                 },
                 "duration": "480",
-                "hash": "4FF8C7E00B02D7380900B7795B4CDDC5B9D03536B5BD896BF54F8E5C53F4A32B"
+                "hash": "552ED44797EC2B5C195F11D2C5CABFBA2257E1C345AE894C51634BF3ADEA3505"
             },
         )
 
         assert (
             signed_hash_lock_tx.payload.hex().upper()
-            == "B800000000000000B8A4B2F9CC46839DBCBF85577CD99B4F1B1A5AD1DBCCF405A2D6005F72AB702758CF09045C5B83AB2E3DC38084171DDCD57D6A4BF27A797FC425D3071B36F60EA8F70E4D5C357273968B12417AE8B742E35E530623C2488D0A73306B41271500000000000190484100000000000000002ADFC07A1A00000044B262C46CEABB858096980000000000E0010000000000004FF8C7E00B02D7380900B7795B4CDDC5B9D03536B5BD896BF54F8E5C53F4A32B"
+            == "B800000000000000D6D8509973A7F34D161B3F3D14AA08A30BA2B314A8A2B96A960D9A8FC5B3CF2610CEF2544E060C037EFDC8615F87B349B4530D0AFFE46827956553963F923401252D2E9F95C4671EEB0C67C6666890567E35976B32666263CD390FC188CCF317000000000198484100000000000000002ADFC07A1A00000044B262C46CEABB858096980000000000E001000000000000552ED44797EC2B5C195F11D2C5CABFBA2257E1C345AE894C51634BF3ADEA3505"
         )
         assert (
             signed_hash_lock_tx.hash.hex().upper()
-            == "36398BE11F57CF56A3AB65BD70AE19480FB0CBB093B2A3CD62646DD0123B5320"
+            == "F0E3B83E0FA207829EBCC31648263ACB01327DFCD22FBEA3BE271FE39ACA8B7F"
         )
