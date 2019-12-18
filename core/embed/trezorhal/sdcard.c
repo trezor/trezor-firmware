38859784e1b67a0db05001c545a92c3603530460
Ondrej Mikle <ondrej.mikle@satoshilabs.com>
2019-12-18 17:11:11 +0100

    core: check whether USB structures are correct to avoid faults in shutdown
    (cherry picked from commit 0064fda203d8e2252e257643085769634fb80473)

--- core/embed/trezorhal/sdcard.c
+++ core/embed/trezorhal/sdcard.c
@@ -214,7 +214,9 @@ uint64_t sdcard_get_capacity_in_bytes(void) {
 
 void SDIO_IRQHandler(void) {
   IRQ_ENTER(SDIO_IRQn);
-  HAL_SD_IRQHandler(&sd_handle);
+  if (sd_handle.Instance) {
+    HAL_SD_IRQHandler(&sd_handle);
+  }
   IRQ_EXIT(SDIO_IRQn);
 }
 
