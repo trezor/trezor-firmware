cd2c46cedd1eb7a5c936e0d8f643de4f690b63e9
Pavol Rusnak <pavol@rusnak.io>
2020-01-02 16:32:31 +0100

    fixup! core: add trezor.sdappdata

--- core/src/trezor/sdappdata.py
+++ core/src/trezor/sdappdata.py
@@ -40,7 +40,7 @@ class SdAppData:
         self.fs.unmount()
         self.sd.power(False)
 
-    def _key_to_fname_enckey(self, key: bytes) -> (str, bytes):
+    def _key_to_filename_enckey(self, key: bytes) -> (str, str, bytes):
         n0 = self.node.clone()
         n0.derive_path([key])
         n = n0.clone()
@@ -50,17 +50,18 @@ class SdAppData:
         n.derive_path(["enc"])
         enckey = n.key()
         fn = hexlify(fnkey[:20]).decode()
-        fn = "%s/%s/%s" % (self.dir, fn[:2], fn[2:])
-        return fn, enckey
+        prefix = "%s/%s" % (self.dir, fn[:2])
+        fullpath = "%s/%s" % (prefix, fn[2:])
+        return prefix, fullpath, enckey
 
     def get(self, key: bytes) -> bytes:
-        fn, enckey = self._key_to_fname_enckey(key)
+        _, fullpath, enckey = self._key_to_filename_enckey(key)
         try:
-            s = self.fs.stat(fn)
+            s = self.fs.stat(fullpath)
         except OSError:
             raise KeyError
         buf = bytearray(s[0])
-        with self.fs.open(fn, "r") as f:
+        with self.fs.open(fullpath, "r") as f:
             f.read(buf)
         if buf[:4] != MAGIC:
             raise ValueError("Invalid magic")
@@ -68,7 +69,7 @@ class SdAppData:
             raise ValueError("Invalid version")
         nonce = buf[8:20]
         ctx = chacha20poly1305(enckey, nonce)
-        ctx.auth(fn)
+        ctx.auth(fullpath)
         value = ctx.decrypt(buf[20:-16])
         tag = ctx.finish()
         if tag != buf[-16:]:
@@ -76,14 +77,14 @@ class SdAppData:
         return value
 
     def set(self, key: bytes, value: bytes) -> None:
-        fn, enckey = self._key_to_fname_enckey(key)
-        self.fs.mkdir(fn[:-39], True)  # create prefix directory
+        prefix, fullpath, enckey = self._key_to_filename_enckey(key)
+        self.fs.mkdir(prefix, True)
         nonce = random.bytes(12)
         ctx = chacha20poly1305(enckey, nonce)
-        ctx.auth(fn)
+        ctx.auth(fullpath)
         enc = ctx.encrypt(value)
         tag = ctx.finish()
-        with self.fs.open(fn, "w") as f:
+        with self.fs.open(fullpath, "w") as f:
             f.write(MAGIC)
             f.write(VERSION.to_bytes(4, "big"))
             f.write(nonce)
@@ -91,5 +92,5 @@ class SdAppData:
             f.write(tag)
 
     def delete(self, key: bytes) -> None:
-        fn, _ = self._key_to_fname_enckey(key)
-        self.fs.unlink(fn)
+        _, fullpath, _ = self._key_to_filename_enckey(key)
+        self.fs.unlink(fullpath)
