85f2e18828c104864a18a673a0be3470b034f757
matejcik <ja@matejcik.cz>
2020-01-16 14:35:27 +0100

    core: convert emu.sh to a simple dependency-less compat wrapper

--- core/emu.sh
+++ core/emu.sh
@@ -1,35 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/sh
+PYOPT="${PYOPT:-1}"
 
-MICROPYTHON="${MICROPYTHON:-${PWD}/build/unix/micropython}"
-TREZOR_SRC=$(cd "${PWD}/src/"; pwd)
-BROWSER="${BROWSER:-chromium}"
+if [ -n "$1" ]; then
+    echo "This is just a compatibility wrapper. Use emu.py if you want features."
+    exit 1
+fi
 
-source ./trezor_cmd.sh
-
-cd "${TREZOR_SRC}"
-
-case "$1" in
-    "-d")
-        shift
-        OPERATING_SYSTEM=$(uname)
-        if [ "$OPERATING_SYSTEM" = "Darwin" ]; then
-            PATH=/usr/bin /usr/bin/lldb -f $MICROPYTHON -- $ARGS $* $MAIN
-        else
-            gdb --args $MICROPYTHON $ARGS $* $MAIN
-        fi
-        ;;
-    "-r")
-        shift
-        while true; do
-            $MICROPYTHON $ARGS $* $MAIN &
-            UPY_PID=$!
-            find -name '*.py' | inotifywait -q -e close_write --fromfile -
-            echo Restarting ...
-            kill $UPY_PID
-        done
-        ;;
-    *)
-        echo "Starting emulator: $MICROPYTHON $ARGS $* $MAIN"
-        $MICROPYTHON $ARGS $* $MAIN 2>&1 | tee "${TREZOR_LOGFILE}"
-        exit ${PIPESTATUS[0]}
-esac
+cd src
+../build/unix/micropython -O$PYOPT -X heapsize=20M -m main
