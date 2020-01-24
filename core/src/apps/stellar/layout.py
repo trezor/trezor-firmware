zcore-dev/trezor-firmware-1

utils: introduce format_plural and move format functions to strings.py

core/src/apps/stellar/layout.py
@@ -1,4 +1,4 @@
-from trezor import ui, utils
+from trezor import strings, ui, utils
 from trezor.messages import ButtonRequestType
 from trezor.ui.text import Text
 
@@ -60,7 +60,7 @@ def format_amount(amount: int, ticker=True) -> str:
     t = ""
     if ticker:
         t = " XLM"
-    return utils.format_amount(amount, consts.AMOUNT_DIVISIBILITY) + t
+    return strings.format_amount(amount, consts.AMOUNT_DIVISIBILITY) + t
 
 
 def split(text):

trezor/trezor-firmware

core/src/apps/stellar/layout.py
@@ -1,4 +1,4 @@
-from trezor import ui, utils
+from trezor import strings, ui, utils
 from trezor.messages import ButtonRequestType
 from trezor.ui.text import Text
 
@@ -77,7 +77,7 @@ def format_amount(amount: int, ticker=True) -> str:
     t = ""
     if ticker:
         t = " XLM"
-    return utils.format_amount(amount, consts.AMOUNT_DECIMALS) + t
+    return strings.format_amount(amount, consts.AMOUNT_DECIMALS) + t
 
 
 def split(text):