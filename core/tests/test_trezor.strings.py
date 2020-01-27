bf1514d1ac53096523328b68cc083eb1f8b8eef4
Tomas Susanka <tsusanka@gmail.com>
2020-01-27 09:11:11 +0000

    core/test/strings: add test for words ending with 'hsxz'

--- core/tests/test_trezor.strings.py
+++ core/tests/test_trezor.strings.py
@@ -15,13 +15,16 @@ class TestStrings(unittest.TestCase):
         for v in VECTORS:
             self.assertEqual(strings.format_amount(v[0], v[1]), v[2])
 
-
     def test_format_plural(self):
         VECTORS = [
             ("We need {count} more {plural}", 3, "share", "We need 3 more shares"),
             ("We need {count} more {plural}", 1, "share", "We need 1 more share"),
             ("We need {count} more {plural}", 1, "candy", "We need 1 more candy"),
             ("We need {count} more {plural}", 7, "candy", "We need 7 more candies"),
+            ("We need {count} more {plural}", 1, "hash", "We need 1 more hash"),
+            ("We need {count} more {plural}", 2, "hash", "We need 2 more hashes"),
+            ("We need {count} more {plural}", 1, "fuzz", "We need 1 more fuzz"),
+            ("We need {count} more {plural}", 2, "fuzz", "We need 2 more fuzzes"),
         ]
         for v in VECTORS:
             self.assertEqual(strings.format_plural(v[0], v[1], v[2]), v[3])
