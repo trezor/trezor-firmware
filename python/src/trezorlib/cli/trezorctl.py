4b0d8d5f6d951122ae753dd3e34b8e9e8621bf24
matejcik <ja@matejcik.cz>
2020-02-10 16:22:48 +0100

    python/trezorctl: tweak error message in get-session

--- python/src/trezorlib/cli/trezorctl.py
+++ python/src/trezorlib/cli/trezorctl.py
@@ -255,7 +255,7 @@ def get_session(connect):
     client = connect()
     get_address(client, "Testnet", PASSPHRASE_TEST_PATH)
     if client.session_id is None:
-        raise click.ClickException("Passphrase not enabled, session ID not available.")
+        raise click.ClickException("Passphrase not enabled or firmware too old.")
     else:
         return client.session_id.hex()
 
