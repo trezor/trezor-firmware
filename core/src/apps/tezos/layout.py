21676a06623272a6ee33788a0f1dcd45fc6c319b
Tomas Susanka <tsusanka@gmail.com>
2020-03-02 08:47:04 +0000

    ore/tezos: better wording in delegation cancel

--- core/src/apps/tezos/layout.py
+++ core/src/apps/tezos/layout.py
@@ -103,14 +103,14 @@ async def require_confirm_proposals(ctx, proposals):
 
 
 async def require_confirm_delegation_manager_withdraw(ctx, address):
-    text = Text("Cancel delegation", ui.ICON_RECEIVE, icon_color=ui.RED)
+    text = Text("Remove delegation", ui.ICON_RECEIVE, icon_color=ui.RED)
     text.bold("Delegator:")
     text.mono(*split_address(address))
     await require_confirm(ctx, text, ButtonRequestType.SignTx)
 
 
 async def require_confirm_manager_remove_delegate(ctx, fee):
-    text = Text("Cancel delegation", ui.ICON_RECEIVE, ui.RED)
+    text = Text("Remove delegation", ui.ICON_RECEIVE, ui.RED)
     text.normal("Fee:")
     text.bold(format_tezos_amount(fee))
     await require_hold_to_confirm(ctx, text, ButtonRequestType.SignTx)
