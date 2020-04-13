d3b0ca3da8af3c5589d10f3b470e857b79e220ba
Dusan Klinec <dusan.klinec@gmail.com>
2020-04-13 20:43:50 +0200

    xmr: proto - add fields for protocol optimization

--- python/src/trezorlib/messages/MoneroTransferDetails.py
+++ python/src/trezorlib/messages/MoneroTransferDetails.py
@@ -18,11 +18,15 @@ class MoneroTransferDetails(p.MessageType):
         tx_pub_key: bytes = None,
         additional_tx_pub_keys: List[bytes] = None,
         internal_output_index: int = None,
+        sub_addr_major: int = None,
+        sub_addr_minor: int = None,
     ) -> None:
         self.out_key = out_key
         self.tx_pub_key = tx_pub_key
         self.additional_tx_pub_keys = additional_tx_pub_keys if additional_tx_pub_keys is not None else []
         self.internal_output_index = internal_output_index
+        self.sub_addr_major = sub_addr_major
+        self.sub_addr_minor = sub_addr_minor
 
     @classmethod
     def get_fields(cls) -> Dict:
@@ -31,4 +35,6 @@ class MoneroTransferDetails(p.MessageType):
             2: ('tx_pub_key', p.BytesType, 0),
             3: ('additional_tx_pub_keys', p.BytesType, p.FLAG_REPEATED),
             4: ('internal_output_index', p.UVarintType, 0),
+            5: ('sub_addr_major', p.UVarintType, 0),
+            6: ('sub_addr_minor', p.UVarintType, 0),
         }
