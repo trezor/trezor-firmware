4e25c396d251803aed36241c39b9533aad983b96
Serge <serge-name@users.noreply.github.com>
2020-05-03 18:47:40 +0300

    python/trezorctl: display embedded v2 image fingerprint (fixes #977)

--- python/src/trezorlib/cli/firmware.py
+++ python/src/trezorlib/cli/firmware.py
@@ -70,6 +70,13 @@ def validate_firmware(version, fw, expected_fingerprint=None):
 
     fingerprint = firmware.digest(version, fw).hex()
     click.echo("Firmware fingerprint: {}".format(fingerprint))
+    if version == firmware.FirmwareFormat.TREZOR_ONE and fw.embedded_onev2:
+        fingerprint_onev2 = firmware.digest(
+            firmware.FirmwareFormat.TREZOR_ONE_V2, fw.embedded_onev2
+        ).hex()
+        click.echo(
+            "Embedded v2 image fingerprint: {}".format(fingerprint_onev2)
+        )
     if expected_fingerprint and fingerprint != expected_fingerprint:
         click.echo("Expected fingerprint: {}".format(expected_fingerprint))
         click.echo("Fingerprints do not match, aborting.")
