01832d5ae9c5914ce49276ea31bdf90c8279ff43
matejcik <ja@matejcik.cz>
2020-05-25 12:49:16 +0200

    core: call close_others() in place of ButtonRequest
    this makes sense, really: close_others() requests UI exclusivity, and
    that is something that generally happens at the same places we emit a
    ButtonRequest

--- core/src/apps/common/__init__.py
+++ core/src/apps/common/__init__.py
@@ -1,3 +1,19 @@
 from micropython import const
 
+from trezor import workflow
+from trezor.messages import ButtonRequestType
+from trezor.messages.ButtonAck import ButtonAck
+from trezor.messages.ButtonRequest import ButtonRequest
+
+if False:
+    from trezor import wire
+    from trezor.messages.ButtonRequest import EnumTypeButtonRequestType
+
 HARDENED = const(0x80000000)
+
+
+async def button_request(
+    ctx: wire.GenericContext, code: EnumTypeButtonRequestType = ButtonRequestType.Other
+) -> None:
+    workflow.close_others()
+    await ctx.call(ButtonRequest(code=code), ButtonAck)
