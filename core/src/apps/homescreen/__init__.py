cfb4acc35278134bc5a27987b88989174e80cdc0
matejcik <ja@matejcik.cz>
2020-06-01 12:07:57 +0200

    core: simplify homescreen and lockscreen implementations

--- core/src/apps/homescreen/__init__.py
+++ core/src/apps/homescreen/__init__.py
@@ -1,11 +1,10 @@
 import storage.device
-from trezor import io, res, ui
-
-if False:
-    from typing import Any, Coroutine
+from trezor import loop, res, ui
 
 
 class HomescreenBase(ui.Layout):
+    RENDER_SLEEP = loop.SLEEP_FOREVER
+
     def __init__(self) -> None:
         self.repaint = True
 
@@ -17,16 +16,3 @@ class HomescreenBase(ui.Layout):
     def render_homescreen(self) -> None:
         ui.display.avatar(48, 48 - 10, self.image, ui.WHITE, ui.BLACK)
         ui.display.text_center(ui.WIDTH // 2, 220, self.label, ui.BOLD, ui.FG, ui.BG)
-
-    def dispatch(self, event: int, x: int, y: int) -> None:
-        if event is ui.RENDER and self.repaint:
-            self.repaint = False
-            self.on_render()
-        elif event is io.TOUCH_END:
-            self.on_touch_end(x, y)
-
-    def __iter__(self) -> Coroutine[Any, Any, ui.ResultValue]:
-        # called whenever `await homescreen` is invoked.
-        # we want to repaint once after that and then never again
-        self.repaint = True
-        return super().__iter__()
