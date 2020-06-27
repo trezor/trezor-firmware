24f803e9c022a7a7b13c857f8096312f61b7729a
Tomas Susanka <tsusanka@gmail.com>
2020-06-27 14:37:35 +0000

    Revert "tests: do not check internal firmware.corp certificate"
    This reverts commit 7364883c04fd5cafb4b40407c2f2d78521624112.
    
    Fixed by adminkos.

--- tests/download_emulators.sh
+++ tests/download_emulators.sh
@@ -5,7 +5,7 @@ SITE="https://firmware.corp.sldev.cz/upgrade_tests/"
 cd "$(dirname "$0")"
 
 # download all emulators without index files, without directories and only if not present
-wget --no-check-certificate -e robots=off --no-verbose --no-clobber --no-parent --cut-dirs=2 --no-host-directories --recursive --reject "index.html*" -P emulators/ $SITE
+wget -e robots=off --no-verbose --no-clobber --no-parent --cut-dirs=2 --no-host-directories --recursive --reject "index.html*" -P emulators/ $SITE
 
 chmod u+x emulators/trezor-emu-*
 
