d368f1abc1e66e4942f949456b30702b66618e8b
Ondrej Mikle <ondrej.mikle@gmail.com>
2020-06-29 15:47:34 +0200

    legacy/intermediate_fw: make version match latest bootloader included

--- legacy/intermediate_fw/version.h
+++ legacy/intermediate_fw/version.h
@@ -1,7 +1,10 @@
+// Matches the bootloader version included in this firmware
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 9
-#define VERSION_PATCH 2
+#define VERSION_MINOR 8
+#define VERSION_PATCH 0
+
 
 #define FIX_VERSION_MAJOR 1
-#define FIX_VERSION_MINOR 9
+#define FIX_VERSION_MINOR 8
 #define FIX_VERSION_PATCH 0
+
