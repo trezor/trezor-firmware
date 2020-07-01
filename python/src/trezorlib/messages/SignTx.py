14a8da9c2830802df1474f463377669513da64fd
Tomas Susanka <tsusanka@gmail.com>
2020-07-01 13:56:31 +0000

    common: remove underscore from 'overwintered' field
    Due to some Suite/Connect compatibility issues.

--- python/src/trezorlib/messages/SignTx.py
+++ python/src/trezorlib/messages/SignTx.py
@@ -21,7 +21,7 @@ class SignTx(p.MessageType):
         version: int = None,
         lock_time: int = None,
         expiry: int = None,
-        _overwintered: bool = None,
+        overwintered: bool = None,
         version_group_id: int = None,
         timestamp: int = None,
         branch_id: int = None,
@@ -32,7 +32,7 @@ class SignTx(p.MessageType):
         self.version = version
         self.lock_time = lock_time
         self.expiry = expiry
-        self._overwintered = _overwintered
+        self.overwintered = overwintered
         self.version_group_id = version_group_id
         self.timestamp = timestamp
         self.branch_id = branch_id
@@ -46,7 +46,7 @@ class SignTx(p.MessageType):
             4: ('version', p.UVarintType, 0),  # default=1
             5: ('lock_time', p.UVarintType, 0),  # default=0
             6: ('expiry', p.UVarintType, 0),
-            7: ('_overwintered', p.BoolType, 0),
+            7: ('overwintered', p.BoolType, 0),
             8: ('version_group_id', p.UVarintType, 0),
             9: ('timestamp', p.UVarintType, 0),
             10: ('branch_id', p.UVarintType, 0),
