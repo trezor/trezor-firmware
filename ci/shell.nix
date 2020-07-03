6c09c61978a97560f84de3f9c8aae6fc5ecc7fd3
Pavol Rusnak <pavol@rusnak.io>
2020-07-03 12:32:55 +0200

    nix: unify ci/shell.nix and shell.nix

--- ci/shell.nix
+++ ci/shell.nix
@@ -2,7 +2,7 @@
 with import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/467ce5a9f45aaf96110b41eb863a56866e1c2c3c.tar.gz) {};
 
 stdenv.mkDerivation {
-  name = "trezor-firmware-docker";
+  name = "trezor-firmware-env";
   buildInputs = [
     SDL2
     SDL2_image
@@ -12,6 +12,7 @@ stdenv.mkDerivation {
     gcc
     gcc-arm-embedded
     git
+    gitAndTools.git-subrepo
     gnumake
     graphviz
     libffi
@@ -23,6 +24,18 @@ stdenv.mkDerivation {
     protobuf3_6
     valgrind
     zlib
+  ] ++ stdenv.lib.optionals (stdenv.isDarwin) [
+    darwin.apple_sdk.frameworks.CoreAudio
+    darwin.apple_sdk.frameworks.AudioToolbox
+    darwin.apple_sdk.frameworks.ForceFeedback
+    darwin.apple_sdk.frameworks.CoreVideo
+    darwin.apple_sdk.frameworks.Cocoa
+    darwin.apple_sdk.frameworks.Carbon
+    darwin.apple_sdk.frameworks.IOKit
+    darwin.apple_sdk.frameworks.QuartzCore
+    darwin.apple_sdk.frameworks.Metal
+    darwin.libobjc
+    libiconv
   ];
   LD_LIBRARY_PATH = "${libffi}/lib:${libjpeg.out}/lib:${libusb1}/lib:${libressl.out}/lib";
   NIX_ENFORCE_PURITY = 0;
