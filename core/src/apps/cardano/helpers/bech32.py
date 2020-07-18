1a8375e1bcf6367e41739260aecaadb4a0a412ec
gabrielkerekes <gabriel.kerekes.ml@hotmail.com>
2020-07-18 09:27:56 +0200

    Update bech32 to support decoding of larger bechs
    Also use better names for functions

--- core/src/apps/cardano/helpers/bech32.py
+++ core/src/apps/cardano/helpers/bech32.py
@@ -1,14 +1,18 @@
 from trezor.crypto import bech32
 
 
-"""
-Helper function to encode data longer than segwit addresses. We don't
-need the decode function. If it ever is needed trezor.crypto.bech32
-implementation needs to be updated for decoding longer strings than
-90 characters.
-"""
-
-
-def bech32_encode(human_readable_part: str, data: bytes) -> str:
+def encode(hrp: str, data: bytes) -> str:
     converted_bits = bech32.convertbits(data, 8, 5)
-    return bech32.bech32_encode(human_readable_part, converted_bits)
+    return bech32.bech32_encode(hrp, converted_bits)
+
+
+def decode(hrp: str, bech: str) -> bytes:
+    decoded_hrp, data = bech32.bech32_decode(bech, 130)
+    if decoded_hrp != hrp:
+        raise ValueError("Bech 32 decode failed")
+
+    decoded = bech32.convertbits(data, 5, 8, False)
+    if decoded is None:
+        raise ValueError("Bech 32 decode failed")
+
+    return bytes(decoded)
