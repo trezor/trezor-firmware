98f20b1f6039273678fd43001564aa4fc07dfe18
matejcik <ja@matejcik.cz>
2020-08-05 12:43:23 +0200

    tests: fix test_msg_webauthn on-device
    by not asking for a full-capacity list of credentials, which will
    usually not fit in an unfragmented chunk of memory

--- tests/device_tests/test_msg_webauthn.py
+++ tests/device_tests/test_msg_webauthn.py
@@ -27,85 +27,75 @@ RK_CAPACITY = 100
 
 @pytest.mark.skip_t1
 @pytest.mark.altcoin
-class TestMsgWebAuthn:
-    @pytest.mark.setup_client(mnemonic=MNEMONIC12)
-    def test_add_remove(self, client):
-        # Remove index 0 should fail.
-        with pytest.raises(TrezorFailure):
-            fido.remove_credential(client, 0)
+@pytest.mark.setup_client(mnemonic=MNEMONIC12)
+def test_add_remove(client):
+    # Remove index 0 should fail.
+    with pytest.raises(TrezorFailure):
+        fido.remove_credential(client, 0)
 
-        # List should be empty.
-        assert fido.list_credentials(client) == []
+    # List should be empty.
+    assert fido.list_credentials(client) == []
 
-        # Add valid credential #1.
-        fido.add_credential(client, CRED1)
+    # Add valid credential #1.
+    fido.add_credential(client, CRED1)
 
-        # Check that the credential was added and parameters are correct.
-        creds = fido.list_credentials(client)
-        assert len(creds) == 1
-        assert creds[0].rp_id == "example.com"
-        assert creds[0].rp_name == "Example"
-        assert creds[0].user_id == bytes.fromhex(
-            "3082019330820138A0030201023082019330820138A003020102308201933082"
-        )
-        assert creds[0].user_name == "johnpsmith@example.com"
-        assert creds[0].user_display_name == "John P. Smith"
-        assert creds[0].creation_time == 3
-        assert creds[0].hmac_secret is True
+    # Check that the credential was added and parameters are correct.
+    creds = fido.list_credentials(client)
+    assert len(creds) == 1
+    assert creds[0].rp_id == "example.com"
+    assert creds[0].rp_name == "Example"
+    assert creds[0].user_id == bytes.fromhex(
+        "3082019330820138A0030201023082019330820138A003020102308201933082"
+    )
+    assert creds[0].user_name == "johnpsmith@example.com"
+    assert creds[0].user_display_name == "John P. Smith"
+    assert creds[0].creation_time == 3
+    assert creds[0].hmac_secret is True
 
-        # Add valid credential #2, which has same rpId and userId as credential #1.
-        fido.add_credential(client, CRED2)
+    # Add valid credential #2, which has same rpId and userId as credential #1.
+    fido.add_credential(client, CRED2)
 
-        # Check that the credential #2 replaced credential #1 and parameters are correct.
-        creds = fido.list_credentials(client)
-        assert len(creds) == 1
-        assert creds[0].rp_id == "example.com"
-        assert creds[0].rp_name is None
-        assert creds[0].user_id == bytes.fromhex(
-            "3082019330820138A0030201023082019330820138A003020102308201933082"
-        )
-        assert creds[0].user_name == "johnpsmith@example.com"
-        assert creds[0].user_display_name is None
-        assert creds[0].creation_time == 2
-        assert creds[0].hmac_secret is True
+    # Check that the credential #2 replaced credential #1 and parameters are correct.
+    creds = fido.list_credentials(client)
+    assert len(creds) == 1
+    assert creds[0].rp_id == "example.com"
+    assert creds[0].rp_name is None
+    assert creds[0].user_id == bytes.fromhex(
+        "3082019330820138A0030201023082019330820138A003020102308201933082"
+    )
+    assert creds[0].user_name == "johnpsmith@example.com"
+    assert creds[0].user_display_name is None
+    assert creds[0].creation_time == 2
+    assert creds[0].hmac_secret is True
 
-        # Adding an invalid credential should appear as if user cancelled.
-        with pytest.raises(Cancelled):
-            fido.add_credential(client, CRED1[:-2])
+    # Adding an invalid credential should appear as if user cancelled.
+    with pytest.raises(Cancelled):
+        fido.add_credential(client, CRED1[:-2])
 
-        # Check that the invalid credential was not added.
-        creds = fido.list_credentials(client)
-        assert len(creds) == 1
+    # Check that the invalid credential was not added.
+    creds = fido.list_credentials(client)
+    assert len(creds) == 1
 
-        # Add valid credential, which has same userId as #2, but different rpId.
-        fido.add_credential(client, CRED3)
+    # Add valid credential, which has same userId as #2, but different rpId.
+    fido.add_credential(client, CRED3)
 
-        # Check that the credential was added.
-        creds = fido.list_credentials(client)
-        assert len(creds) == 2
+    # Check that the credential was added.
+    creds = fido.list_credentials(client)
+    assert len(creds) == 2
 
-        # Fill up the credential storage to maximum capacity.
-        for cred in CREDS[: RK_CAPACITY - 2]:
-            fido.add_credential(client, cred)
+    # Fill up the credential storage to maximum capacity.
+    for cred in CREDS[: RK_CAPACITY - 2]:
+        fido.add_credential(client, cred)
 
-        # Adding one more valid credential to full storage should fail.
-        with pytest.raises(TrezorFailure):
-            fido.add_credential(client, CREDS[-1])
-
-        # Removing the index, which is one past the end, should fail.
-        with pytest.raises(TrezorFailure):
-            fido.remove_credential(client, RK_CAPACITY)
-
-        # Remove index 2.
-        fido.remove_credential(client, 2)
-
-        # Check that the credential was removed.
-        creds = fido.list_credentials(client)
-        assert len(creds) == RK_CAPACITY - 1
-
-        # Adding another valid credential should succeed now.
+    # Adding one more valid credential to full storage should fail.
+    with pytest.raises(TrezorFailure):
         fido.add_credential(client, CREDS[-1])
 
-        # Check that the credential was added.
-        creds = fido.list_credentials(client)
-        assert len(creds) == RK_CAPACITY
+    # Removing the index, which is one past the end, should fail.
+    with pytest.raises(TrezorFailure):
+        fido.remove_credential(client, RK_CAPACITY)
+
+    # Remove index 2.
+    fido.remove_credential(client, 2)
+    # Adding another valid credential should succeed now.
+    fido.add_credential(client, CREDS[-1])
