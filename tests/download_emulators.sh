af3a41964edc9ac52a575ec6a23ea276518b784b
Martin Milata <martin@martinmilata.cz>
2020-08-26 21:19:22 +0200

    tests: patch ELF interpreter in download_emulators.sh

--- tests/download_emulators.sh
+++ tests/download_emulators.sh
@@ -10,4 +10,11 @@ wget -e robots=off --no-verbose --no-clobber --no-parent --cut-dirs=2 --no-host-
 chmod u+x emulators/trezor-emu-*
 
 # are we in Nix(OS)?
-command -v nix-shell >/dev/null && nix-shell -p autoPatchelfHook SDL2 SDL2_image --run "autoPatchelf emulators/trezor-emu-*"
+if command -v nix-shell >/dev/null; then
+  nix-shell -p autoPatchelfHook SDL2 SDL2_image --run "autoPatchelf emulators/trezor-emu-*"
+# remove NixOS specific interpreter
+elif command -v patchelf >/dev/null; then
+  patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 emulators/trezor-emu-*
+else
+  echo "Not on NixOS and patchelf not found: emulators probably have wrong ELF interpreter."
+fi
