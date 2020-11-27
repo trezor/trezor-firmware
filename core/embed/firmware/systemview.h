641dcd419647a6bcdfdcd59057866ea7dc8a9e97
Ondrej Mikle <ondrej.mikle@gmail.com>
2020-11-27 14:25:24 +0100

    feat(core): measure systick with systemview

--- core/embed/firmware/systemview.h
+++ core/embed/firmware/systemview.h
@@ -4,8 +4,13 @@
 
 #ifdef SYSTEM_VIEW
 
+#include "SEGGER_SYSVIEW.h"
+
 void enable_systemview();
 
+#else
+#define SEGGER_SYSVIEW_RecordEnterISR() do {} while(0)
+#define SEGGER_SYSVIEW_RecordExitISR() do {} while(0)
 #endif
 
 #endif //CORE_SYSTEMVIEW_H
