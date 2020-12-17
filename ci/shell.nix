b9c0b597bcf5c462f16d85e054caf9cdbf5047e7
Pavol Rusnak <pavol@rusnak.io>
2020-12-17 14:32:02 +0100

    nix: bump stable nixos-20.09 to latest

--- ci/shell.nix
+++ ci/shell.nix
@@ -1,10 +1,10 @@
 { fullDeps ? false }:
 
-# the last successful build of nixos-20.09 (stable) as of 2020-10-11
+# the last successful build of nixos-20.09 (stable) as of 2020-12-15
 with import
   (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/0b8799ecaaf0dc6b4c11583a3c96ca5b40fcfdfb.tar.gz";
-    sha256 = "11m4aig6cv0zi3gbq2xn9by29cfvnsxgzf9qsvz67qr0yq29ryyz";
+    url = "https://github.com/NixOS/nixpkgs/archive/647cc06986c1ae4a2bb05298e0cf598723e42970.tar.gz";
+    sha256 = "1n1sd5lbds08vxy8x9l94w0z8bbq39fh2rrr6mnq0rmhf4xb2mj1";
   })
 { };
 
