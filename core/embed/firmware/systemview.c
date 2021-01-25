d99127771c0e061a029de88eaefb255ff9b62e2d
Ondrej Mikle <ondrej.mikle@gmail.com>
2021-01-25 13:19:57 +0100

    chore(core): reorder includes

--- core/embed/firmware/systemview.c
+++ core/embed/firmware/systemview.c
@@ -4,7 +4,9 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
+#include "irq.h"
 #include "mpconfigport.h"
+#include "supervise.h"
 
 #include "SEGGER_SYSVIEW.h"
 #include "SEGGER_SYSVIEW_Conf.h"
@@ -49,12 +51,6 @@ typedef struct {
 } SCS_REGS;
 
 extern uint32_t SystemCoreClock;
-
-static inline uint32_t is_mode_unprivileged(void) {
-  uint32_t r0;
-  __asm__ volatile("mrs %0, control" : "=r"(r0));
-  return r0 & 1;
-}
 
 uint32_t svc_get_dwt_cyccnt() {
   if (is_mode_unprivileged()) {
