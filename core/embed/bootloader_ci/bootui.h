a1e63924295bd01aa9b34e2c1a54c212e91627d3
Ondrej Mikle <ondrej.mikle@gmail.com>
2021-01-27 17:16:34 +0100

    chore(core): bootloader_ci - decouple file links from original bootloader

--- core/embed/bootloader_ci/bootui.h
+++ core/embed/bootloader_ci/bootui.h
@@ -1,1 +1,68 @@
-../bootloader/bootui.h
+/*
+ * This file is part of the Trezor project, https://trezor.io/
+ *
+ * Copyright (c) SatoshiLabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __BOOTUI_H__
+#define __BOOTUI_H__
+
+#include "image.h"
+#include "secbool.h"
+
+void ui_screen_boot(const vendor_header* const vhdr,
+                    const image_header* const hdr);
+void ui_screen_boot_wait(int wait_seconds);
+void ui_screen_boot_click(void);
+
+void ui_screen_first(void);
+void ui_screen_second(void);
+void ui_screen_third(void);
+
+void ui_screen_info(secbool buttons, const vendor_header* const vhdr,
+                    const image_header* const hdr);
+void ui_screen_info_fingerprint(const image_header* const hdr);
+
+void ui_screen_install_confirm_upgrade(const vendor_header* const vhdr,
+                                       const image_header* const hdr);
+void ui_screen_install_confirm_newvendor_or_downgrade_wipe(
+    const vendor_header* const vhdr, const image_header* const hdr,
+    secbool downgrade_wipe);
+void ui_screen_install(void);
+void ui_screen_install_progress_erase(int pos, int len);
+void ui_screen_install_progress_upload(int pos);
+
+void ui_screen_wipe_confirm(void);
+void ui_screen_wipe(void);
+void ui_screen_wipe_progress(int pos, int len);
+
+void ui_screen_done(int restart_seconds, secbool full_redraw);
+
+void ui_screen_fail(void);
+
+void ui_fadein(void);
+void ui_fadeout(void);
+
+// clang-format off
+#define INPUT_CANCEL 0x01        // Cancel button
+#define INPUT_CONFIRM 0x02       // Confirm button
+#define INPUT_LONG_CONFIRM 0x04  // Long Confirm button
+#define INPUT_INFO 0x08          // Info icon
+// clang-format on
+
+int ui_user_input(int zones);
+
+#endif
--- core/embed/bootloader_ci/version.h
+++ core/embed/bootloader_ci/version.h
@@ -1,1 +1,14 @@
-../bootloader/version.h
+#define VERSION_MAJOR 2
+#define VERSION_MINOR 0
+#define VERSION_PATCH 4
+#define VERSION_BUILD 0
+#define VERSION_UINT32                                            \
+  (VERSION_MAJOR | (VERSION_MINOR << 8) | (VERSION_PATCH << 16) | \
+   (VERSION_BUILD << 24))
+
+#define FIX_VERSION_MAJOR 2
+#define FIX_VERSION_MINOR 0
+#define FIX_VERSION_PATCH 0
+#define FIX_VERSION_BUILD 0
+
+#define VERSION_MONOTONIC 1
