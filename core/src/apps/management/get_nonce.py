585892a254d8b1f47b6742fc5a68bea469514f23
Andrew Kozlik <andrew.kozlik@gmail.com>
2021-01-28 20:12:56 +0100

    fixup! feat(core): Implement GetNonce command.

--- core/src/apps/management/get_nonce.py
+++ core/src/apps/management/get_nonce.py
@@ -1,3 +1,5 @@
+from micropython import const
+
 from storage import cache
 from trezor import wire
 from trezor.crypto import random
@@ -5,15 +7,19 @@ from trezor.messages.GetNonce import GetNonce
 from trezor.messages.Nonce import Nonce
 
 if False:
-    from typing import Set
+    from typing import List
+
+_MAX_NONCES_COUNT = const(10)
 
 
 async def get_nonce(ctx: wire.Context, msg: GetNonce) -> Nonce:
     nonce = random.bytes(32)
-    nonces: Set[bytes] = cache.get(cache.APP_COMMON_NONCES)
+    nonces: List[bytes] = cache.get(cache.APP_COMMON_NONCES)
     if not nonces:
-        nonces = set()
+        nonces = []
         cache.set(cache.APP_COMMON_NONCES, nonces)
+    elif len(nonces) >= _MAX_NONCES_COUNT:
+        nonces.pop()
 
-    nonces.add(nonce)
+    nonces.insert(0, nonce)
     return Nonce(nonce=nonce)
