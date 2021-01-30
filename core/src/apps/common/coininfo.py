39e0a2ab280b4804b82348bfa690803783c4c094
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2021-01-30 12:57:12 +0000

    NewYorkCoin Integration

--- core/src/apps/common/coininfo.py
+++ core/src/apps/common/coininfo.py
@@ -1130,6 +1130,34 @@ def by_name(name: str) -> CoinInfo:
                 overwintered=False,
                 confidential_assets=None,
             )
+        elif name == "Newyorkcoin":
+            return CoinInfo(
+                coin_name=name,
+                coin_shortcut="NYC",
+                decimals=8,
+                address_type=60,
+                address_type_p2sh=22,
+                maxfee_kb=1200000000000,
+                signed_message_header="Newyorkcoin Signed Message:\n",
+                xpub_magic=0x0488b21e,
+                xpub_magic_segwit_p2sh=None,
+                xpub_magic_segwit_native=None,
+                xpub_magic_multisig_segwit_p2sh=None,
+                xpub_magic_multisig_segwit_native=None,
+                bech32_prefix=None,
+                cashaddr_prefix=None,
+                slip44=3,
+                segwit=False,
+                fork_id=None,
+                force_bip143=False,
+                decred=False,
+                negative_fee=False,
+                curve_name='secp256k1',
+                extra_data=False,
+                timestamp=False,
+                overwintered=False,
+                confidential_assets=None,
+            )    
         elif name == "NIX":
             return CoinInfo(
                 coin_name=name,
