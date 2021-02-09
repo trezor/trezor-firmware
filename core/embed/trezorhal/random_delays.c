9a237a4f219b6babb1a668aa49c110401d860aed
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2021-02-09 17:43:17 +0100

    feat(core): make random delays use default drbg

--- core/embed/trezorhal/random_delays.c
+++ core/embed/trezorhal/random_delays.c
@@ -48,39 +48,60 @@ https://link.springer.com/content/pdf/10.1007%2F978-3-540-72354-7_3.pdf
 extern void shutdown(void);
 
 #define BUFFER_LENGTH 64
-#define RESEED_INTERVAL 65536
 
-static CHACHA_DRBG_CTX drbg_ctx;
 static uint8_t buffer[BUFFER_LENGTH];
-static size_t buffer_index;
+static size_t buffer_index = 0;
 static uint8_t session_delay;
 static bool refresh_session_delay;
 static secbool rdi_disabled = sectrue;
 
-static void rdi_reseed(void) {
-  uint8_t entropy[CHACHA_DRBG_SEED_LENGTH];
-  random_buffer(entropy, CHACHA_DRBG_SEED_LENGTH);
-  chacha_drbg_reseed(&drbg_ctx, entropy, sizeof(entropy), NULL, 0);
-}
-
-static void buffer_refill(void) {
-  chacha_drbg_generate(&drbg_ctx, buffer, BUFFER_LENGTH);
-}
+static void buffer_refill(void) { drbg_generate(buffer, BUFFER_LENGTH); }
 
 static uint32_t random8(void) {
-  buffer_index += 1;
-  if (buffer_index >= BUFFER_LENGTH) {
+  if (buffer_index == 0) {
     buffer_refill();
-    if (RESEED_INTERVAL != 0 && drbg_ctx.reseed_counter > RESEED_INTERVAL)
-      rdi_reseed();
-    buffer_index = 0;
   }
-  return buffer[buffer_index];
+
+  uint8_t value = buffer[buffer_index];
+  memzero(&buffer[buffer_index], 1);
+  buffer_index = (buffer_index + 1) % sizeof(buffer);
+  return value;
 }
 
 void rdi_refresh_session_delay(void) {
   if (rdi_disabled == secfalse)  // if rdi enabled
     refresh_session_delay = true;
+}
+
+void wait(uint32_t delay) {
+  // wait (30 + delay) ticks
+  asm volatile(
+      "ldr r0, %0;"  // r0 = delay
+      "loop:"
+      "subs r0, $3;"  // r0 -= 3
+      "bhs loop;"     // if (r0 >= 3): goto loop
+      // loop (delay // 3) times
+      // every loop takes 3 ticks
+      // r0 == (delay % 3) - 3
+      "add r0, $3;"  // r0 += 3
+      // r0 == delay % 3
+      "and r0, r0, $3;"  // r0 %= 4, make sure that 0 <= r0 < 4
+      "ldr r1, =table;"  // r1 = &table
+      "tbb [r1, r0];"    // jump 2*r1[r0] bytes forward, that is goto wait_r0
+      "base:"
+      "table:"  // table of branch lengths
+      ".byte (wait_0 - base)/2;"
+      ".byte (wait_1 - base)/2;"
+      ".byte (wait_2 - base)/2;"
+      ".byte (wait_2 - base)/2;"  // next instruction must be 2-byte aligned
+      "wait_2:"
+      "add r0, $1;"  // wait one tick
+      "wait_1:"
+      "add r0, $1;"  // wait one tick
+      "wait_0:"
+      :
+      : "m"(delay)
+      : "r0", "r1");
 }
 
 void rdi_handler(uint32_t uw_tick) {
@@ -90,36 +111,8 @@ void rdi_handler(uint32_t uw_tick) {
       refresh_session_delay = false;
     }
 
-    uint32_t delay = random8() + session_delay;
+    wait(random8() + session_delay);
 
-    // wait (30 + delay) ticks
-    asm volatile(
-        "ldr r0, %0;"  // r0 = delay
-        "loop:"
-        "subs r0, $3;"  // r0 -= 3
-        "bhs loop;"     // if (r0 >= 3): goto loop
-        // loop (delay // 3) times
-        // every loop takes 3 ticks
-        // r0 == (delay % 3) - 3
-        "add r0, $3;"  // r0 += 3
-        // r0 == delay % 3
-        "and r0, r0, $3;"  // r0 %= 4, make sure that 0 <= r0 < 4
-        "ldr r1, =table;"  // r1 = &table
-        "tbb [r1, r0];"    // jump 2*r1[r0] bytes forward, that is goto wait_r0
-        "base:"
-        "table:"  // table of branch lengths
-        ".byte (wait_0 - base)/2;"
-        ".byte (wait_1 - base)/2;"
-        ".byte (wait_2 - base)/2;"
-        ".byte (wait_2 - base)/2;"  // next instruction must be 2-byte aligned
-        "wait_2:"
-        "add r0, $1;"  // wait one tick
-        "wait_1:"
-        "add r0, $1;"  // wait one tick
-        "wait_0:"
-        :
-        : "m"(delay)
-        : "r0", "r1");
   } else {  // if rdi disabled or rdi_disabled corrupted
     ensure(rdi_disabled, "Fault detected");
   }
@@ -127,11 +120,6 @@ void rdi_handler(uint32_t uw_tick) {
 
 void rdi_start(void) {
   if (rdi_disabled == sectrue) {  // if rdi disabled
-    uint8_t entropy[CHACHA_DRBG_SEED_LENGTH];
-    random_buffer(entropy, CHACHA_DRBG_SEED_LENGTH);
-    chacha_drbg_init(&drbg_ctx, entropy, sizeof(entropy), NULL, 0);
-    buffer_refill();
-    buffer_index = 0;
     refresh_session_delay = true;
     rdi_disabled = secfalse;
   }
@@ -141,7 +129,6 @@ void rdi_stop(void) {
   if (rdi_disabled == secfalse) {  // if rdi enabled
     rdi_disabled = sectrue;
     session_delay = 0;
-    memzero(&drbg_ctx, sizeof(drbg_ctx));
   }
 }
 
@@ -150,7 +137,7 @@ void rdi_stop(void) {
  * against fault injection.
  */
 void wait_random(void) {
-  int wait = drbg_random32() & 0xff;
+  int wait = random8();
   volatile int i = 0;
   volatile int j = wait;
   while (i < wait) {
