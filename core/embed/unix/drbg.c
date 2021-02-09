211ac4017589a722f5f5adc9915039f3d9d758a5
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2021-02-09 10:51:07 +0100

    feat(core): use chacha-drbg instead of hmac-drbg

--- core/embed/unix/drbg.c
+++ core/embed/unix/drbg.c
@@ -19,23 +19,23 @@
 
 #include "drbg.h"
 
-#include "hmac_drbg.h"
+#include "chacha_drbg.h"
 #include "rand.h"
 
-static HMAC_DRBG_CTX drbg_ctx;
+static CHACHA_DRBG_CTX drbg_ctx;
 
 void drbg_init(void) {
   uint8_t entropy[48];
   random_buffer(entropy, sizeof(entropy));
-  hmac_drbg_init(&drbg_ctx, entropy, sizeof(entropy), NULL, 0);
+  chacha_drbg_init(&drbg_ctx, entropy, sizeof(entropy), NULL, 0);
 }
 
 void drbg_reseed(const uint8_t *entropy, size_t len) {
-  hmac_drbg_reseed(&drbg_ctx, entropy, len, NULL, 0);
+  chacha_drbg_reseed(&drbg_ctx, entropy, len, NULL, 0);
 }
 
 void drbg_generate(uint8_t *buf, size_t len) {
-  hmac_drbg_generate(&drbg_ctx, buf, len);
+  chacha_drbg_generate(&drbg_ctx, buf, len);
 }
 
 uint32_t drbg_random32(void) {
