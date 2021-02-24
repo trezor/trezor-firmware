10be0230a5aba731a061b74f2e30d4661fe15da1
Ondrej Mikle <ondrej.mikle@gmail.com>
2021-02-24 10:58:24 +0100

    chore(legacy): bump bootloader version

--- legacy/bootloader/version.h
+++ legacy/bootloader/version.h
@@ -1,7 +1,7 @@
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 8
-#define VERSION_PATCH 1
+#define VERSION_MINOR 9
+#define VERSION_PATCH 0
 
 #define VERSION_MAJOR_CHAR "\x01"
-#define VERSION_MINOR_CHAR "\x08"
-#define VERSION_PATCH_CHAR "\x01"
+#define VERSION_MINOR_CHAR "\x09"
+#define VERSION_PATCH_CHAR "\x00"
