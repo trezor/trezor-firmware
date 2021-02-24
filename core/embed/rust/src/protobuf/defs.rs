e062e8cdea4711e039f9514a604625c37a31460b
Jan Pochyla <jpochyla@gmail.com>
2021-02-24 19:23:33 +0100

    rust: Optimize defs storage, use binary search

--- core/embed/rust/src/protobuf/defs.rs
+++ core/embed/rust/src/protobuf/defs.rs
@@ -7,10 +7,10 @@ pub struct MsgDef {
 
 impl MsgDef {
     pub fn for_wire_id(wire_id: u16) -> Option<Self> {
-        find_msg_index(wire_id).map(|msg_index| unsafe {
-            // SAFETY: We are taking the index right out of `find_msg_index` so we can be
-            // sure it's trusted.
-            find_msg(msg_index)
+        find_msg_offset(wire_id).map(|msg_offset| unsafe {
+            // SAFETY: We are taking the offset right out of `find_msg_offset` so we can be
+            // sure it's to be trusted.
+            get_msg(msg_offset)
         })
     }
 
@@ -23,7 +23,7 @@ impl MsgDef {
 pub struct FieldDef {
     pub tag: u8,
     flags_and_type: u8,
-    enum_or_msg_index: u16,
+    enum_or_msg_offset: u16,
     pub name: u16,
 }
 
@@ -35,8 +35,8 @@ impl FieldDef {
             2 => FieldType::Bool,
             3 => FieldType::Bytes,
             4 => FieldType::String,
-            5 => FieldType::Enum(unsafe { find_enum(self.enum_or_msg_index) }),
-            6 => FieldType::Msg(unsafe { find_msg(self.enum_or_msg_index) }),
+            5 => FieldType::Enum(unsafe { get_enum(self.enum_or_msg_offset) }),
+            6 => FieldType::Msg(unsafe { get_msg(self.enum_or_msg_offset) }),
             _ => unreachable!(),
         }
     }
@@ -94,11 +94,11 @@ static ENUM_DEFS: &[u8] = include_bytes!("../../../../src/trezor/messages/proto_
 static MSG_DEFS: &[u8] = include_bytes!("../../../../src/trezor/messages/proto_msgs.data");
 static WIRE_DEFS: &[u8] = include_bytes!("../../../../src/trezor/messages/proto_wire.data");
 
-fn find_msg_index(wire_id: u16) -> Option<u16> {
+fn find_msg_offset(wire_id: u16) -> Option<u16> {
     #[repr(C, packed)]
     struct WireDef {
         wire_id: u16,
-        msg_index: u16,
+        msg_offset: u16,
     }
 
     let wire_defs: &[WireDef] = unsafe {
@@ -107,15 +107,13 @@ fn find_msg_index(wire_id: u16) -> Option<u16> {
             WIRE_DEFS.len() / mem::size_of::<WireDef>(),
         )
     };
-    for def in wire_defs {
-        if def.wire_id == wire_id {
-            return Some(def.msg_index);
-        }
-    }
-    None
+    wire_defs
+        .binary_search_by_key(&wire_id, |def| def.wire_id)
+        .map(|i| wire_defs[i].msg_offset)
+        .ok()
 }
 
-unsafe fn find_msg(msg_index: u16) -> MsgDef {
+unsafe fn get_msg(msg_offset: u16) -> MsgDef {
     // #[repr(C, packed)]
     // struct MsgDef {
     //     fields_count: u8,
@@ -124,47 +122,32 @@ unsafe fn find_msg(msg_index: u16) -> MsgDef {
     //     defaults: [u8],
     // }
 
-    let mut ptr = MSG_DEFS.as_ptr();
-    let mut i = 0;
-    loop {
-        let fields_count = ptr.offset(0).read() as usize;
-        let defaults_size = ptr.offset(1).read() as usize;
+    let ptr = MSG_DEFS.as_ptr().add(msg_offset as usize);
+    let fields_count = ptr.offset(0).read() as usize;
+    let defaults_size = ptr.offset(1).read() as usize;
 
-        let fields_size = fields_count * mem::size_of::<FieldDef>();
-        let fields_ptr = ptr.offset(2);
-        let defaults_ptr = ptr.offset(2).add(fields_size);
+    let fields_size = fields_count * mem::size_of::<FieldDef>();
+    let fields_ptr = ptr.offset(2);
+    let defaults_ptr = ptr.offset(2).add(fields_size);
 
-        if i == msg_index {
-            break MsgDef {
-                fields: slice::from_raw_parts(fields_ptr.cast(), fields_count),
-                defaults: slice::from_raw_parts(defaults_ptr.cast(), defaults_size),
-            };
-        } else {
-            ptr = defaults_ptr.add(defaults_size);
-            i += 1;
-        }
+    MsgDef {
+        fields: slice::from_raw_parts(fields_ptr.cast(), fields_count),
+        defaults: slice::from_raw_parts(defaults_ptr.cast(), defaults_size),
     }
 }
 
-unsafe fn find_enum(enum_index: u16) -> EnumDef {
+unsafe fn get_enum(enum_offset: u16) -> EnumDef {
     // #[repr(C, packed)]
     // struct EnumDef {
     //     count: u8,
     //     vals: [u16],
     // }
 
-    let mut ptr = ENUM_DEFS.as_ptr();
-    let mut i = 0;
-    loop {
-        let count = ptr.offset(0).read() as usize;
-        let vals = ptr.offset(1);
-        if i == enum_index {
-            break EnumDef {
-                values: slice::from_raw_parts(vals.cast(), count),
-            };
-        } else {
-            ptr = vals.add(count * 2);
-            i += 1;
-        }
+    let ptr = ENUM_DEFS.as_ptr().add(enum_offset as usize);
+    let count = ptr.offset(0).read() as usize;
+    let vals = ptr.offset(1);
+
+    EnumDef {
+        values: slice::from_raw_parts(vals.cast(), count),
     }
 }
