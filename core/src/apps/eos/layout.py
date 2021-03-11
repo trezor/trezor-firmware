dd3b689dede7db8cd070a92147a54d074eeebde6
Martin Milata <martin@martinmilata.cz>
2021-03-11 16:38:51 +0100

    refactor(core): convert apps.eos to layouts

--- core/src/apps/eos/layout.py
+++ core/src/apps/eos/layout.py
@@ -1,19 +1,21 @@
 from trezor import ui, wire
 from trezor.enums import ButtonRequestType
-from trezor.ui.components.tt.text import Text
-
-from apps.common.confirm import require_confirm
+from trezor.strings import format_plural
+from trezor.ui.layouts import confirm_action, show_pubkey
 
 
 async def require_get_public_key(ctx: wire.Context, public_key: str) -> None:
-    text = Text("Confirm public key", ui.ICON_RECEIVE, ui.GREEN)
-    text.normal(public_key)
-    await require_confirm(ctx, text, ButtonRequestType.PublicKey)
+    await show_pubkey(ctx, public_key)
 
 
 async def require_sign_tx(ctx: wire.Context, num_actions: int) -> None:
-    text = Text("Sign transaction", ui.ICON_SEND, ui.GREEN)
-    text.normal("You are about")
-    text.normal("to sign {}".format(num_actions))
-    text.normal("action(s).")
-    await require_confirm(ctx, text, ButtonRequestType.SignTx)
+    await confirm_action(
+        ctx,
+        "confirm_tx",
+        title="Sign transaction",
+        description="You are about to sign {}.",
+        description_param=format_plural("{count} {plural}", num_actions, "action"),
+        icon=ui.ICON_SEND,
+        icon_color=ui.GREEN,
+        br_code=ButtonRequestType.SignTx,
+    )
