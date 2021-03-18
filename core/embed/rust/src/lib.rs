77759351d95dcf84e152eb315e1f5f80510c6de7
Jan Pochyla <jpochyla@gmail.com>
2021-03-18 11:06:16 +0100

    rust: Minor panic message change

--- core/embed/rust/src/lib.rs
+++ core/embed/rust/src/lib.rs
@@ -20,9 +20,15 @@ use cstr_core::CStr;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    // Although it would be ideal to use the original error message, ignoring it
+    // lets us avoid the `fmt` machinery and its code size and is also important for
+    // security reasons, as we do not always controls the message contents. We
+    // should also avoid printing "panic" or "rust" on the user screen to avoid any
+    // confusion.
+
     // SAFETY: Safe because we are passing in \0-terminated literals.
     let empty = unsafe { CStr::from_bytes_with_nul_unchecked("\0".as_bytes()) };
-    let msg = unsafe { CStr::from_bytes_with_nul_unchecked("rust\0".as_bytes()) };
+    let msg = unsafe { CStr::from_bytes_with_nul_unchecked("rs\0".as_bytes()) };
 
     // TODO: Ideally we would take the file and line info out of
     // `PanicInfo::location()`.
