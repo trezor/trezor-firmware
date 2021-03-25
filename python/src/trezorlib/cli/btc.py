23abf7aff07c3299862d5e6356c144dc32258c01
mcudev <29890609+mcudev@users.noreply.github.com>
2021-03-25 10:33:15 -0400

    python/trezorctl: make bitcoin regtest a supported coin for trezorctl btc get-descriptor

--- python/src/trezorlib/cli/btc.py
+++ python/src/trezorlib/cli/btc.py
@@ -211,7 +211,7 @@ def _get_descriptor(client, coin, account, script_type, show_display):
 
     if coin is None or coin == "Bitcoin":
         coin_type = 0
-    elif coin == "Testnet":
+    elif coin == "Testnet" or coin == "Regtest":
         coin_type = 1
     else:
         raise ValueError("Unsupported coin")
