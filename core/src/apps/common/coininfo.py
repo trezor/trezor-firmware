2852b947ec8ada68d71cc94b74c179126b90b561
Pavol Rusnak <pavol@rusnak.io>
2021-04-08 14:17:43 +0200

    chore(core): regenerate coins

--- core/src/apps/common/coininfo.py
+++ core/src/apps/common/coininfo.py
@@ -1494,6 +1494,34 @@ def by_name(name: str) -> CoinInfo:
                 overwintered=False,
                 confidential_assets=None,
             )
+        elif name == "Ravencoin Testnet":
+            return CoinInfo(
+                coin_name=name,
+                coin_shortcut="tRVN",
+                decimals=8,
+                address_type=111,
+                address_type_p2sh=196,
+                maxfee_kb=170000000000,
+                signed_message_header="Raven Signed Message:\n",
+                xpub_magic=0x043587cf,
+                xpub_magic_segwit_p2sh=None,
+                xpub_magic_segwit_native=None,
+                xpub_magic_multisig_segwit_p2sh=None,
+                xpub_magic_multisig_segwit_native=None,
+                bech32_prefix=None,
+                cashaddr_prefix=None,
+                slip44=1,
+                segwit=False,
+                fork_id=None,
+                force_bip143=False,
+                decred=False,
+                negative_fee=False,
+                curve_name='secp256k1',
+                extra_data=False,
+                timestamp=False,
+                overwintered=False,
+                confidential_assets=None,
+            )
         elif name == "Ritocoin":
             return CoinInfo(
                 coin_name=name,
