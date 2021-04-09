b5c1b37a56ca5f79695bf0625b6b33e82209b629
matejcik <ja@matejcik.cz>
2021-04-09 11:42:45 +0200

    chore(core/tests): bump heap size for core unit tests

--- core/tests/run_tests.sh
+++ core/tests/run_tests.sh
@@ -3,7 +3,7 @@
 declare -a results
 declare -i passed=0 failed=0 exit_code=0
 declare COLOR_GREEN='\e[32m' COLOR_RED='\e[91m' COLOR_RESET='\e[39m'
-MICROPYTHON="${MICROPYTHON:-../build/unix/trezor-emu-core -X heapsize=1M}"
+MICROPYTHON="${MICROPYTHON:-../build/unix/trezor-emu-core -X heapsize=2M}"
 
 print_summary() {
     echo
