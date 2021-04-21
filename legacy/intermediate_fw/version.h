89ad7cc3661bc952901540007a44c71d2dc586d7
Pavol Rusnak <pavol@rusnak.io>
2021-04-21 18:49:09 +0200

    fix(legacy): update intermediate_fw version to 1.10.0
    (cherry picked from commit f606b718818f6cbd0233628412f3597471cb6a82)

--- legacy/intermediate_fw/version.h
+++ legacy/intermediate_fw/version.h
@@ -1,8 +1,8 @@
 // Matches the bootloader version included in this firmware
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 9
+#define VERSION_MINOR 10
 #define VERSION_PATCH 0
 
 #define FIX_VERSION_MAJOR 1
-#define FIX_VERSION_MINOR 9
+#define FIX_VERSION_MINOR 10
 #define FIX_VERSION_PATCH 0
