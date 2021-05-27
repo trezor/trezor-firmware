92b6ecf7a79905689e720f47ecf0ee0be7e218ae
matejcik <ja@matejcik.cz>
2021-05-27 13:48:16 +0200

    refactor(core): create and use protobuf uvarint writer

--- core/src/apps/common/writers.py
+++ core/src/apps/common/writers.py
@@ -99,3 +99,13 @@ def write_bitcoin_varint(w: Writer, n: int) -> None:
         w.append((n >> 8) & 0xFF)
         w.append((n >> 16) & 0xFF)
         w.append((n >> 24) & 0xFF)
+
+
+def write_uvarint(w: Writer, n: int) -> None:
+    ensure(n >= 0 and n <= 0xFFFF_FFFF_FFFF_FFFF)
+    shifted = 1
+    while shifted:
+        shifted = n >> 7
+        byte = (n & 0x7F) | (0x80 if shifted else 0x00)
+        w.append(byte)
+        n = shifted
--- core/src/apps/eos/sign_tx.py
+++ core/src/apps/eos/sign_tx.py
@@ -28,7 +28,7 @@ async def sign_tx(ctx: wire.Context, msg: EosSignTx, keychain: Keychain) -> EosS
     sha = HashWriter(sha256())
     await _init(ctx, sha, msg)
     await _actions(ctx, sha, msg.num_actions)
-    writers.write_variant32(sha, 0)
+    writers.write_uvarint(sha, 0)
     writers.write_bytes_fixed(sha, bytearray(32), 32)
 
     digest = sha.get_digest()
@@ -42,8 +42,8 @@ async def sign_tx(ctx: wire.Context, msg: EosSignTx, keychain: Keychain) -> EosS
 async def _init(ctx: wire.Context, sha: HashWriter, msg: EosSignTx) -> None:
     writers.write_bytes_fixed(sha, msg.chain_id, 32)
     writers.write_header(sha, msg.header)
-    writers.write_variant32(sha, 0)
-    writers.write_variant32(sha, msg.num_actions)
+    writers.write_uvarint(sha, 0)
+    writers.write_uvarint(sha, msg.num_actions)
 
     await require_sign_tx(ctx, msg.num_actions)
 
