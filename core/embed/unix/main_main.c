4059f5675782bfa99fb179c4eb59c74b377b699f
Jan Pochyla <jpochyla@gmail.com>
2021-06-12 12:19:31 +0200

    feat(core): Split unix main, link core obj into Rust test target

---
+++ core/embed/unix/main_main.c
@@ -0,0 +1,22 @@
+#include "py/mpthread.h"
+#include "py/runtime.h"
+#include "py/stackctrl.h"
+
+#include "common.h"
+
+MP_NOINLINE int main_(int argc, char **argv);
+
+int main(int argc, char **argv) {
+  collect_hw_entropy();
+
+#if MICROPY_PY_THREAD
+  mp_thread_init();
+#endif
+  // We should capture stack top ASAP after start, and it should be
+  // captured guaranteedly before any other stack variables are allocated.
+  // For this, actual main (renamed main_) should not be inlined into
+  // this function. main_() itself may have other functions inlined (with
+  // their own stack variables), that's why we need this main/main_ split.
+  mp_stack_ctrl_init();
+  return main_(argc, argv);
+}
