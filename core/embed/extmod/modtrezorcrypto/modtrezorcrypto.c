df21c692cbe6e2ad9eef810d6fe8ba05751617ac
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2021-06-21 12:49:27 +0200

    style(core): fix style

--- core/embed/extmod/modtrezorcrypto/modtrezorcrypto.c
+++ core/embed/extmod/modtrezorcrypto/modtrezorcrypto.c
@@ -111,7 +111,7 @@ STATIC MP_DEFINE_CONST_DICT(mp_module_trezorcrypto_globals,
 
 const mp_obj_module_t mp_module_trezorcrypto = {
     .base = {&mp_type_module},
-    .globals = (mp_obj_dict_t*)&mp_module_trezorcrypto_globals,
+    .globals = (mp_obj_dict_t *)&mp_module_trezorcrypto_globals,
 };
 
 MP_REGISTER_MODULE(MP_QSTR_trezorcrypto, mp_module_trezorcrypto,
