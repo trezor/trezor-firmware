3f1dee4dfd1f1cfa3cfe078bbee0eadc711f635f
Pavol Rusnak <pavol@rusnak.io>
2021-07-21 15:15:55 +0200

    feat(core): add BIP-350 test cases to test_trezor.crypto.bech32

--- core/tests/test_trezor.crypto.bech32.py
+++ core/tests/test_trezor.crypto.bech32.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Pieter Wuille
+# Copyright (c) 2017, 2020 Pieter Wuille
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -32,13 +32,21 @@ def segwit_scriptpubkey(witver, witprog):
 
 VALID_CHECKSUM = [
     # BIP-173
-    "A12UEL5L",
-    "a12uel5l",
-    "an83characterlonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1tt5tgs",
-    "abcdef1qpzry9x8gf2tvdw0s3jn54khce6mua7lmqqqxw",
-    "11qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqc8247j",
-    "split1checkupstagehandshakeupstreamerranterredcaperred2y9e3w",
-    "?1ezyfcl",
+    ("A12UEL5L", bech32.Encoding.BECH32),
+    ("a12uel5l", bech32.Encoding.BECH32),
+    ("an83characterlonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1tt5tgs", bech32.Encoding.BECH32),
+    ("abcdef1qpzry9x8gf2tvdw0s3jn54khce6mua7lmqqqxw", bech32.Encoding.BECH32),
+    ("11qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqc8247j", bech32.Encoding.BECH32),
+    ("split1checkupstagehandshakeupstreamerranterredcaperred2y9e3w", bech32.Encoding.BECH32),
+    ("?1ezyfcl", bech32.Encoding.BECH32),
+    # BIP-350
+    ("A1LQFN3A", bech32.Encoding.BECH32M),
+    ("a1lqfn3a", bech32.Encoding.BECH32M),
+    ("an83characterlonghumanreadablepartthatcontainsthetheexcludedcharactersbioandnumber11sg7hg6", bech32.Encoding.BECH32M),
+    ("abcdef1l7aum6echk45nj3s0wdvt2fg8x9yrzpqzd3ryx", bech32.Encoding.BECH32M),
+    ("11llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllludsr8", bech32.Encoding.BECH32M),
+    ("split1checkupstagehandshakeupstreamerranterredcaperredlc445v", bech32.Encoding.BECH32M),
+    ("?1v759aa", bech32.Encoding.BECH32M),
 ]
 
 INVALID_CHECKSUM = [
@@ -55,15 +63,39 @@ INVALID_CHECKSUM = [
     "A1G7SGD8",
     "10a06t8",
     "1qzzfhee",
+    # BIP-350
+    " 1xj0phk",
+    "\x7F" + "1g6xzxy",
+    "\x80" + "1vctc34",
+    "an84characterslonghumanreadablepartthatcontainsthetheexcludedcharactersbioandnumber11d6pts4",
+    "qyrz8wqd2c9m",
+    "1qyrz8wqd2c9m",
+    "y1b0jsk6g",
+    "lt1igcx5c0",
+    "in1muywd",
+    "mm1crxm3i",
+    "au1s5cgom",
+    "M1VUXWEZ",
+    "16plkw9",
+    "1p2gdwpf",
 ]
 
 VALID_ADDRESS = [
     # BIP-173
-    ["BC1QW508D6QEJXTDG4Y5R3ZARVARY0C5XW7KV8F3T4", "0014751e76e8199196d454941c45d1b3a323f1433bd6"],
-    ["tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7",
-     "00201863143c14c5166804bd19203356da136c985678cd4d27a1b8c6329604903262"],
-    ["tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy",
-     "0020000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433"],
+    ("BC1QW508D6QEJXTDG4Y5R3ZARVARY0C5XW7KV8F3T4", "0014751e76e8199196d454941c45d1b3a323f1433bd6"),
+    ("tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7",
+     "00201863143c14c5166804bd19203356da136c985678cd4d27a1b8c6329604903262"),
+    ("tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy",
+     "0020000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433"),
+    # BIP-350
+    ("BC1QW508D6QEJXTDG4Y5R3ZARVARY0C5XW7KV8F3T4", "0014751e76e8199196d454941c45d1b3a323f1433bd6"),
+    ("tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7", "00201863143c14c5166804bd19203356da136c985678cd4d27a1b8c6329604903262"),
+    ("bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kt5nd6y", "5128751e76e8199196d454941c45d1b3a323f1433bd6751e76e8199196d454941c45d1b3a323f1433bd6"),
+    ("BC1SW50QGDZ25J", "6002751e"),
+    ("bc1zw508d6qejxtdg4y5r3zarvaryvaxxpcs", "5210751e76e8199196d454941c45d1b3a323"),
+    ("tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy", "0020000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433"),
+    ("tb1pqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesf3hn0c", "5120000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433"),
+    ("bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqzk5jj0", "512079be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798"),
 ]
 
 INVALID_ADDRESS = [
@@ -78,7 +110,22 @@ INVALID_ADDRESS = [
     "bc1zw508d6qejxtdg4y5r3zarvaryvqyzf3du",
     "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3pjxtptv",
     "bc1gmk9yu",
-
+    # BIP-350
+    "tc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vq5zuyut",
+    "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqh2y7hd",
+    "tb1z0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqglt7rf",
+    "BC1S0XLXVLHEMJA6C4DQV22UAPCTQUPFHLXM9H8Z3K2E72Q4K9HCZ7VQ54WELL",
+    "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kemeawh",
+    "tb1q0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vq24jc47",
+    "bc1p38j9r5y49hruaue7wxjce0updqjuyyx0kh56v8s25huc6995vvpql3jow4",
+    "BC130XLXVLHEMJA6C4DQV22UAPCTQUPFHLXM9H8Z3K2E72Q4K9HCZ7VQ7ZWS8R",
+    "bc1pw5dgrnzv",
+    "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7v8n0nx0muaewav253zgeav",
+    "BC1QR508D6QEJXTDG4Y5R3ZARVARYV98GJ9P",
+    "tb1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vq47Zagq",
+    "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7v07qwwzcrf",
+    "tb1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vpggkg4j",
+    "bc1gmk9yu",
 ]
 
 INVALID_ADDRESS_ENC = [
@@ -95,19 +142,21 @@ class TestCryptoBech32(unittest.TestCase):
 
     def test_valid_checksum(self):
         """Test checksum creation and validation."""
-        for test in VALID_CHECKSUM:
-            hrp, _, _ = bech32.bech32_decode(test)
+        for test_enc in VALID_CHECKSUM:
+            test, enc = test_enc
+            sep = test.rfind("1")
+            hrp, _, spec = bech32.bech32_decode(test)
             self.assertIsNotNone(hrp)
-            pos = test.rfind('1')
-            test = test[:pos + 1] + chr(ord(test[pos + 1]) ^ 1) + test[pos + 2:]
-            hrp, _, _ = bech32.bech32_decode(test)
-            self.assertIsNone(hrp)
+            self.assertEqual(enc, spec)
+            self.assertEqual(hrp, test[:sep].lower())
 
     def test_invalid_checksum(self):
         """Test validation of invalid checksums."""
         for test in INVALID_CHECKSUM:
-            hrp, _, _ = bech32.bech32_decode(test)
+            hrp, data, spec = bech32.bech32_decode(test)
             self.assertIsNone(hrp)
+            self.assertIsNone(data)
+            self.assertIsNone(spec)
 
     def test_valid_address(self):
         """Test whether valid addresses decode to the correct output."""
