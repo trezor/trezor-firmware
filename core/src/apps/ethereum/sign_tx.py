9c09dcd6d26d0107da05cf7c56c5ed86be6da29c
yowl <yowl@arbitraryexecution.com>
2021-08-05 17:04:43 -0700

    removed python changelog, removed trailing newlines in changelog, added newline in sign_tx.py (linting)

--- core/src/apps/ethereum/sign_tx.py
+++ core/src/apps/ethereum/sign_tx.py
@@ -19,6 +19,7 @@ from .layout import (
 # maximum supported chain id
 MAX_CHAIN_ID = (0xFFFF_FFFF_FFFF_FFFF - 35) / 2
 
+
 @with_keychain_from_chain_id
 async def sign_tx(ctx, msg, keychain):
     msg = sanitize(msg)
