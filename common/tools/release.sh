2f94575c6b45af27f81716ac53c1ad48de07084d
matejcik <ja@matejcik.cz>
2021-08-23 12:55:57 +0200

    feat(common): create release script

---
+++ common/tools/release.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+HERE=$(dirname $0)
+
+CHECK_OUTPUT=$(mktemp -d)
+trap "rm -r $CHECK_OUTPUT" EXIT
+
+$HERE/cointool.py check > $CHECK_OUTPUT/pre.txt
+
+ETH_DIR=$HERE/../defs/ethereum
+ETH_REPOS="chains tokens"
+
+for dir in $ETH_REPOS; do
+    (
+        cd $ETH_DIR/$dir; \
+        git checkout master; \
+        git pull origin master
+    )
+done
+
+$HERE/support.py release
+
+$HERE/cointool.py check > $CHECK_OUTPUT/post.txt
+
+make -C $HERE/../.. gen
+
+diff $CHECK_OUTPUT/pre.txt $CHECK_OUTPUT/post.txt
