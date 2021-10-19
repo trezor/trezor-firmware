2e31826dd111a0335aa5c43fb5581ae410224992
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2021-10-19 21:09:59 +0200

    refactor(core,crypto): rename schnorr to BIP340

--- core/embed/extmod/modtrezorcrypto/modtrezorcrypto-bip340.h
+++ core/embed/extmod/modtrezorcrypto/modtrezorcrypto-bip340.h
@@ -19,15 +19,15 @@
 
 #include "py/objstr.h"
 
-#include "zkp_schnorr.h"
+#include "zkp_bip340.h"
 
-/// package: trezorcrypto.schnorr
+/// package: trezorcrypto.bip340
 
 /// def generate_secret() -> bytes:
 ///     """
 ///     Generate secret key.
 ///     """
-STATIC mp_obj_t mod_trezorcrypto_schnorr_generate_secret() {
+STATIC mp_obj_t mod_trezorcrypt_bip340_generate_secret() {
   vstr_t sk = {0};
   vstr_init_len(&sk, 32);
   for (;;) {
@@ -51,14 +51,14 @@ STATIC mp_obj_t mod_trezorcrypto_schnorr_generate_secret() {
   }
   return mp_obj_new_str_from_vstr(&mp_type_bytes, &sk);
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(mod_trezorcrypto_schnorr_generate_secret_obj,
-                                 mod_trezorcrypto_schnorr_generate_secret);
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(mod_trezorcrypt_bip340_generate_secret_obj,
+                                 mod_trezorcrypt_bip340_generate_secret);
 
 /// def publickey(secret_key: bytes) -> bytes:
 ///     """
 ///     Computes public key from secret key.
 ///     """
-STATIC mp_obj_t mod_trezorcrypto_schnorr_publickey(mp_obj_t secret_key) {
+STATIC mp_obj_t mod_trezorcrypt_bip340_publickey(mp_obj_t secret_key) {
   mp_buffer_info_t sk = {0};
   mp_get_buffer_raise(secret_key, &sk, MP_BUFFER_READ);
   if (sk.len != 32) {
@@ -67,7 +67,7 @@ STATIC mp_obj_t mod_trezorcrypto_schnorr_publickey(mp_obj_t secret_key) {
   vstr_t pk = {0};
   vstr_init_len(&pk, 32);
   int ret =
-      zkp_schnorr_get_public_key((const uint8_t *)sk.buf, (uint8_t *)pk.buf);
+      zkp_bip340_get_public_key((const uint8_t *)sk.buf, (uint8_t *)pk.buf);
   if (0 != ret) {
     vstr_clear(&pk);
     mp_raise_ValueError("Invalid secret key");
@@ -75,8 +75,8 @@ STATIC mp_obj_t mod_trezorcrypto_schnorr_publickey(mp_obj_t secret_key) {
   return mp_obj_new_str_from_vstr(&mp_type_bytes, &pk);
 }
 
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_trezorcrypto_schnorr_publickey_obj,
-                                 mod_trezorcrypto_schnorr_publickey);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_trezorcrypt_bip340_publickey_obj,
+                                 mod_trezorcrypt_bip340_publickey);
 
 /// def sign(
 ///     secret_key: bytes,
@@ -85,8 +85,8 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_trezorcrypto_schnorr_publickey_obj,
 ///     """
 ///     Uses secret key to produce the signature of the digest.
 ///     """
-STATIC mp_obj_t mod_trezorcrypto_schnorr_sign(mp_obj_t secret_key,
-                                              mp_obj_t digest) {
+STATIC mp_obj_t mod_trezorcrypt_bip340_sign(mp_obj_t secret_key,
+                                            mp_obj_t digest) {
   mp_buffer_info_t sk = {0}, dig = {0};
   mp_get_buffer_raise(secret_key, &sk, MP_BUFFER_READ);
   mp_get_buffer_raise(digest, &dig, MP_BUFFER_READ);
@@ -100,8 +100,8 @@ STATIC mp_obj_t mod_trezorcrypto_schnorr_sign(mp_obj_t secret_key,
   vstr_t sig = {0};
   vstr_init_len(&sig, 64);
   int ret =
-      zkp_schnorr_sign_digest((const uint8_t *)sk.buf, (const uint8_t *)dig.buf,
-                              (uint8_t *)sig.buf, NULL);
+      zkp_bip340_sign_digest((const uint8_t *)sk.buf, (const uint8_t *)dig.buf,
+                             (uint8_t *)sig.buf, NULL);
   if (0 != ret) {
     vstr_clear(&sig);
     mp_raise_ValueError("Signing failed");
@@ -109,17 +109,17 @@ STATIC mp_obj_t mod_trezorcrypto_schnorr_sign(mp_obj_t secret_key,
   return mp_obj_new_str_from_vstr(&mp_type_bytes, &sig);
 }
 
-STATIC MP_DEFINE_CONST_FUN_OBJ_2(mod_trezorcrypto_schnorr_sign_obj,
-                                 mod_trezorcrypto_schnorr_sign);
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(mod_trezorcrypt_bip340_sign_obj,
+                                 mod_trezorcrypt_bip340_sign);
 
 /// def verify(public_key: bytes, signature: bytes, digest: bytes) -> bool:
 ///     """
 ///     Uses public key to verify the signature of the digest.
 ///     Returns True on success.
 ///     """
-STATIC mp_obj_t mod_trezorcrypto_schnorr_verify(mp_obj_t public_key,
-                                                mp_obj_t signature,
-                                                mp_obj_t digest) {
+STATIC mp_obj_t mod_trezorcrypt_bip340_verify(mp_obj_t public_key,
+                                              mp_obj_t signature,
+                                              mp_obj_t digest) {
   mp_buffer_info_t pk = {0}, sig = {0}, dig = {0};
   mp_get_buffer_raise(public_key, &pk, MP_BUFFER_READ);
   mp_get_buffer_raise(signature, &sig, MP_BUFFER_READ);
@@ -133,27 +133,27 @@ STATIC mp_obj_t mod_trezorcrypto_schnorr_verify(mp_obj_t public_key,
   if (dig.len != 32) {
     return mp_const_false;
   }
-  int ret = zkp_schnorr_verify_digest((const uint8_t *)pk.buf,
-                                      (const uint8_t *)sig.buf,
-                                      (const uint8_t *)dig.buf);
+  int ret = zkp_bip340_verify_digest((const uint8_t *)pk.buf,
+                                     (const uint8_t *)sig.buf,
+                                     (const uint8_t *)dig.buf);
   return mp_obj_new_bool(ret == 0);
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_3(mod_trezorcrypto_schnorr_verify_obj,
-                                 mod_trezorcrypto_schnorr_verify);
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(mod_trezorcrypt_bip340_verify_obj,
+                                 mod_trezorcrypt_bip340_verify);
 
-STATIC const mp_rom_map_elem_t mod_trezorcrypto_schnorr_globals_table[] = {
-    {MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_schnorr)},
+STATIC const mp_rom_map_elem_t mod_trezorcrypt_bip340_globals_table[] = {
+    {MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_bip340)},
     {MP_ROM_QSTR(MP_QSTR_generate_secret),
-     MP_ROM_PTR(&mod_trezorcrypto_schnorr_generate_secret_obj)},
+     MP_ROM_PTR(&mod_trezorcrypt_bip340_generate_secret_obj)},
     {MP_ROM_QSTR(MP_QSTR_publickey),
-     MP_ROM_PTR(&mod_trezorcrypto_schnorr_publickey_obj)},
-    {MP_ROM_QSTR(MP_QSTR_sign), MP_ROM_PTR(&mod_trezorcrypto_schnorr_sign_obj)},
+     MP_ROM_PTR(&mod_trezorcrypt_bip340_publickey_obj)},
+    {MP_ROM_QSTR(MP_QSTR_sign), MP_ROM_PTR(&mod_trezorcrypt_bip340_sign_obj)},
     {MP_ROM_QSTR(MP_QSTR_verify),
-     MP_ROM_PTR(&mod_trezorcrypto_schnorr_verify_obj)}};
-STATIC MP_DEFINE_CONST_DICT(mod_trezorcrypto_schnorr_globals,
-                            mod_trezorcrypto_schnorr_globals_table);
+     MP_ROM_PTR(&mod_trezorcrypt_bip340_verify_obj)}};
+STATIC MP_DEFINE_CONST_DICT(mod_trezorcrypt_bip340_globals,
+                            mod_trezorcrypt_bip340_globals_table);
 
-STATIC const mp_obj_module_t mod_trezorcrypto_schnorr_module = {
+STATIC const mp_obj_module_t mod_trezorcrypt_bip340_module = {
     .base = {&mp_type_module},
-    .globals = (mp_obj_dict_t *)&mod_trezorcrypto_schnorr_globals,
+    .globals = (mp_obj_dict_t *)&mod_trezorcrypt_bip340_globals,
 };
--- core/mocks/generated/trezorcrypto/bip340.pyi
+++ core/mocks/generated/trezorcrypto/bip340.pyi
@@ -1,21 +1,21 @@
 from typing import *
 
 
-# extmod/modtrezorcrypto/modtrezorcrypto-schnorr.h
+# extmod/modtrezorcrypto/modtrezorcrypto-bip340.h
 def generate_secret() -> bytes:
     """
     Generate secret key.
     """
 
 
-# extmod/modtrezorcrypto/modtrezorcrypto-schnorr.h
+# extmod/modtrezorcrypto/modtrezorcrypto-bip340.h
 def publickey(secret_key: bytes) -> bytes:
     """
     Computes public key from secret key.
     """
 
 
-# extmod/modtrezorcrypto/modtrezorcrypto-schnorr.h
+# extmod/modtrezorcrypto/modtrezorcrypto-bip340.h
 def sign(
     secret_key: bytes,
     digest: bytes,
@@ -25,7 +25,7 @@ def sign(
     """
 
 
-# extmod/modtrezorcrypto/modtrezorcrypto-schnorr.h
+# extmod/modtrezorcrypto/modtrezorcrypto-bip340.h
 def verify(public_key: bytes, signature: bytes, digest: bytes) -> bool:
     """
     Uses public key to verify the signature of the digest.
