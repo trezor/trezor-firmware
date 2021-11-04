9e4dd1266dcd98e0ac6edde7f6e28ec975d23ada
Andrew Kozlik <andrew.kozlik@gmail.com>
2021-11-04 11:14:35 +0100

    chore(core): Remove USE_SECP256K1_ZKP_BIP340 build option.

--- core/embed/extmod/modtrezorcrypto/modtrezorcrypto.c
+++ core/embed/extmod/modtrezorcrypto/modtrezorcrypto.c
@@ -38,6 +38,7 @@ static void wrapped_ui_wait_callback(uint32_t current, uint32_t total) {
 
 #include "modtrezorcrypto-aes.h"
 #include "modtrezorcrypto-bip32.h"
+#include "modtrezorcrypto-bip340.h"
 #include "modtrezorcrypto-bip39.h"
 #include "modtrezorcrypto-blake256.h"
 #include "modtrezorcrypto-blake2b.h"
@@ -52,9 +53,6 @@ static void wrapped_ui_wait_callback(uint32_t current, uint32_t total) {
 #include "modtrezorcrypto-pbkdf2.h"
 #include "modtrezorcrypto-random.h"
 #include "modtrezorcrypto-ripemd160.h"
-#ifdef USE_SECP256K1_ZKP_BIP340
-#include "modtrezorcrypto-bip340.h"
-#endif
 #include "modtrezorcrypto-secp256k1.h"
 #include "modtrezorcrypto-sha1.h"
 #include "modtrezorcrypto-sha256.h"
@@ -106,9 +104,7 @@ STATIC const mp_rom_map_elem_t mp_module_trezorcrypto_globals_table[] = {
      MP_ROM_PTR(&mod_trezorcrypto_Ripemd160_type)},
     {MP_ROM_QSTR(MP_QSTR_secp256k1),
      MP_ROM_PTR(&mod_trezorcrypto_secp256k1_module)},
-#ifdef USE_SECP256K1_ZKP_BIP340
     {MP_ROM_QSTR(MP_QSTR_bip340), MP_ROM_PTR(&mod_trezorcrypto_bip340_module)},
-#endif
     {MP_ROM_QSTR(MP_QSTR_sha1), MP_ROM_PTR(&mod_trezorcrypto_Sha1_type)},
     {MP_ROM_QSTR(MP_QSTR_sha256), MP_ROM_PTR(&mod_trezorcrypto_Sha256_type)},
     {MP_ROM_QSTR(MP_QSTR_sha512), MP_ROM_PTR(&mod_trezorcrypto_Sha512_type)},
