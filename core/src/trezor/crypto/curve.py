aaceb5bcc6867d5b44c2945ad77d6ed8b4394502
Andrew Kozlik <andrew.kozlik@gmail.com>
2021-11-04 11:14:35 +0100

    chore(core): Remove USE_SECP256K1_ZKP_BIP340 build option.

--- core/src/trezor/crypto/curve.py
+++ core/src/trezor/crypto/curve.py
@@ -1,6 +1,1 @@
-from trezorcrypto import curve25519, ed25519, nist256p1, secp256k1  # noqa: F401
-
-try:
-    from trezorcrypto import bip340  # noqa: F401
-except ImportError:
-    pass
+from trezorcrypto import bip340, curve25519, ed25519, nist256p1, secp256k1  # noqa: F401
