d765de409fcfd17f1a611a503fa7b859e22129a0
Jan Pochyla <jpochyla@gmail.com>
2021-11-26 19:46:55 +0100

    chore(core/rust): Add Paginated component

---
+++ core/embed/rust/src/ui/component/pad.rs
@@ -0,0 +1,32 @@
+use crate::ui::{
+    display::{self, Color},
+    geometry::Rect,
+};
+
+pub struct Pad {
+    area: Rect,
+    color: Color,
+    clear: bool,
+}
+
+impl Pad {
+    pub fn with_background(area: Rect, color: Color) -> Self {
+        Self {
+            area,
+            color,
+            clear: false,
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.clear = true;
+    }
+
+    pub fn paint(&mut self) {
+        if self.clear {
+            self.clear = false;
+
+            display::rect(self.area, self.color);
+        }
+    }
+}
--- core/embed/rust/src/ui/model_tt/component/confirm.rs
+++ core/embed/rust/src/ui/model_tt/component/confirm.rs
@@ -1,8 +1,7 @@
 use crate::{
     time::Instant,
     ui::{
-        component::{Child, Component, ComponentExt, Event, EventCtx},
-        display::{self, Color},
+        component::{Child, Component, ComponentExt, Event, EventCtx, Pad},
         geometry::Rect,
     },
 };
@@ -108,33 +107,5 @@ where
         d.open("HoldToConfirm");
         self.content.trace(d);
         d.close();
-    }
-}
-
-struct Pad {
-    area: Rect,
-    color: Color,
-    clear: bool,
-}
-
-impl Pad {
-    fn with_background(area: Rect, color: Color) -> Self {
-        Self {
-            area,
-            color,
-            clear: false,
-        }
-    }
-
-    fn clear(&mut self) {
-        self.clear = true;
-    }
-
-    fn paint(&mut self) {
-        if self.clear {
-            self.clear = false;
-
-            display::rect(self.area, self.color);
-        }
     }
 }
