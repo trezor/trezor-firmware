7450befd9ec4dd0f6c382d4a8c8a99053b8fc762
matejcik <ja@matejcik.cz>
2021-11-26 15:50:43 +0100

    chore(python): regenerate GNU copyright headers

--- python/src/trezorlib/misc.py
+++ python/src/trezorlib/misc.py
@@ -1,6 +1,6 @@
 # This file is part of the Trezor project.
 #
-# Copyright (C) 2012-2019 SatoshiLabs and contributors
+# Copyright (C) 2012-2022 SatoshiLabs and contributors
 #
 # This library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License version 3
