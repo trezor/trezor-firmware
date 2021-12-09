e6b062e80bb20c7b0071aad72b5f115cca3f3a9c
matejcik <ja@matejcik.cz>
2021-12-09 13:30:48 +0100

    chore(python): bump typing_extensions requirement

--- python/setup.py
+++ python/setup.py
@@ -29,7 +29,7 @@ install_requires = [
     "click>=7,<9",
     "libusb1>=1.6.4",
     "construct>=2.9",
-    "typing_extensions>=3.7.4",
+    "typing_extensions>=3.10",
     "dataclasses ; python_version<'3.7'",
 ]
 
