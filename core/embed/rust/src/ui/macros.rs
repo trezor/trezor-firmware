0e299cc9d026272f3280bb3fd8b81052632af15c
Martin Milata <martin@martinmilata.cz>
2022-01-18 12:46:33 +0100

    feat(core/rust/ui): add new icons
    [no changelog]

--- core/embed/rust/src/ui/macros.rs
+++ core/embed/rust/src/ui/macros.rs
@@ -1,10 +1,6 @@
 #[allow(unused_macros)] // T1 doesn't use icons (yet)
 macro_rules! include_res {
     ($filename:expr) => {
-        include_bytes!(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/../../src/trezor/res/",
-            $filename,
-        ))
+        include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/ui/", $filename))
     };
 }
