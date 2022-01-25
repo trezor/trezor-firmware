9cf5fd351a355a04b8a376f75e869f084351da50
grdddj <jiri.musil06@seznam.cz>
2022-01-25 18:20:05 +0100

    chore(tests): adding type hints to helper functions used in sign_tx tests

--- tests/device_tests/signtx.py
+++ tests/device_tests/signtx.py
@@ -3,46 +3,46 @@ from trezorlib import messages
 T = messages.RequestType
 
 
-def request_input(n, tx_hash=None):
+def request_input(n: int, tx_hash: bytes = None) -> messages.TxRequest:
     return messages.TxRequest(
         request_type=T.TXINPUT,
         details=messages.TxRequestDetailsType(request_index=n, tx_hash=tx_hash),
     )
 
 
-def request_output(n, tx_hash=None):
+def request_output(n: int, tx_hash: bytes = None) -> messages.TxRequest:
     return messages.TxRequest(
         request_type=T.TXOUTPUT,
         details=messages.TxRequestDetailsType(request_index=n, tx_hash=tx_hash),
     )
 
 
-def request_orig_input(n, tx_hash):
+def request_orig_input(n: int, tx_hash: bytes) -> messages.TxRequest:
     return messages.TxRequest(
         request_type=T.TXORIGINPUT,
         details=messages.TxRequestDetailsType(request_index=n, tx_hash=tx_hash),
     )
 
 
-def request_orig_output(n, tx_hash):
+def request_orig_output(n: int, tx_hash: bytes) -> messages.TxRequest:
     return messages.TxRequest(
         request_type=T.TXORIGOUTPUT,
         details=messages.TxRequestDetailsType(request_index=n, tx_hash=tx_hash),
     )
 
 
-def request_meta(tx_hash):
+def request_meta(tx_hash: bytes) -> messages.TxRequest:
     return messages.TxRequest(
         request_type=T.TXMETA,
         details=messages.TxRequestDetailsType(tx_hash=tx_hash),
     )
 
 
-def request_finished():
+def request_finished() -> messages.TxRequest:
     return messages.TxRequest(request_type=T.TXFINISHED)
 
 
-def request_extra_data(ofs, len, tx_hash):
+def request_extra_data(ofs: int, len: int, tx_hash: bytes) -> messages.TxRequest:
     return messages.TxRequest(
         request_type=T.TXEXTRADATA,
         details=messages.TxRequestDetailsType(
