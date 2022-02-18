561169a557401f701c22fc5e34f5929b75bd7fe9
Pavol Rusnak <pavol@rusnak.io>
2022-02-18 15:58:40 +0100

    feat(python): add bech32 implementation
    Use the HWI version from [1] because it contains type hints and it is
    formatted using black. The reference implementation [2] does not contain
    these improvements.
    
    [1] https://github.com/bitcoin-core/HWI/blob/master/hwilib/_bech32.py
    [2] https://github.com/sipa/bech32/blob/master/ref/python/segwit_addr.py

---
+++ python/src/trezorlib/bech32.py
@@ -0,0 +1,160 @@
+# Copyright (c) 2017 Pieter Wuille
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""
+Bech32 Conversion Utilities
+***************************
+
+Reference implementation for Bech32 and segwit addresses.
+"""
+
+from enum import Enum
+from typing import List, Optional, Tuple, Union
+
+CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+BECH32_CONST = 1
+BECH32M_CONST = 0x2BC830A3
+
+
+class Encoding(Enum):
+    """Enumeration type to list the various supported encodings."""
+
+    BECH32 = 1
+    BECH32M = 2
+
+
+def bech32_polymod(values: List[int]) -> int:
+    """Internal function that computes the Bech32 checksum."""
+    generator = [0x3B6A57B2, 0x26508E6D, 0x1EA119FA, 0x3D4233DD, 0x2A1462B3]
+    chk = 1
+    for value in values:
+        top = chk >> 25
+        chk = (chk & 0x1FFFFFF) << 5 ^ value
+        for i in range(5):
+            chk ^= generator[i] if ((top >> i) & 1) else 0
+    return chk
+
+
+def bech32_hrp_expand(hrp: str) -> List[int]:
+    """Expand the HRP into values for checksum computation."""
+    return [ord(x) >> 5 for x in hrp] + [0] + [ord(x) & 31 for x in hrp]
+
+
+def bech32_verify_checksum(hrp: str, data: List[int]) -> Optional[Encoding]:
+    """Verify a checksum given HRP and converted data characters."""
+    check = bech32_polymod(bech32_hrp_expand(hrp) + data)
+    if check == BECH32_CONST:
+        return Encoding.BECH32
+    elif check == BECH32M_CONST:
+        return Encoding.BECH32M
+    else:
+        return None
+
+
+def bech32_create_checksum(encoding: Encoding, hrp: str, data: List[int]) -> List[int]:
+    """Compute the checksum values given HRP and data."""
+    values = bech32_hrp_expand(hrp) + data
+    const = BECH32M_CONST if encoding == Encoding.BECH32M else BECH32_CONST
+    polymod = bech32_polymod(values + [0, 0, 0, 0, 0, 0]) ^ const
+    return [(polymod >> 5 * (5 - i)) & 31 for i in range(6)]
+
+
+def bech32_encode(encoding: Encoding, hrp: str, data: List[int]) -> str:
+    """Compute a Bech32 string given HRP and data values."""
+    combined = data + bech32_create_checksum(encoding, hrp, data)
+    return hrp + "1" + "".join([CHARSET[d] for d in combined])
+
+
+def bech32_decode(
+    bech: str,
+) -> Tuple[Optional[Encoding], Optional[str], Optional[List[int]]]:
+    """Validate a Bech32 string, and determine HRP and data."""
+    if (any(ord(x) < 33 or ord(x) > 126 for x in bech)) or (
+        bech.lower() != bech and bech.upper() != bech
+    ):
+        return (None, None, None)
+    bech = bech.lower()
+    pos = bech.rfind("1")
+    if pos < 1 or pos + 7 > len(bech) or len(bech) > 90:
+        return (None, None, None)
+    if not all(x in CHARSET for x in bech[pos + 1 :]):
+        return (None, None, None)
+    hrp = bech[:pos]
+    data = [CHARSET.find(x) for x in bech[pos + 1 :]]
+    encoding = bech32_verify_checksum(hrp, data)
+    if encoding is None:
+        return (None, None, None)
+    return (encoding, hrp, data[:-6])
+
+
+def convertbits(
+    data: Union[bytes, List[int]], frombits: int, tobits: int, pad: bool = True
+) -> Optional[List[int]]:
+    """General power-of-2 base conversion."""
+    acc = 0
+    bits = 0
+    ret = []
+    maxv = (1 << tobits) - 1
+    max_acc = (1 << (frombits + tobits - 1)) - 1
+    for value in data:
+        if value < 0 or (value >> frombits):
+            return None
+        acc = ((acc << frombits) | value) & max_acc
+        bits += frombits
+        while bits >= tobits:
+            bits -= tobits
+            ret.append((acc >> bits) & maxv)
+    if pad:
+        if bits:
+            ret.append((acc << (tobits - bits)) & maxv)
+    elif bits >= frombits or ((acc << (tobits - bits)) & maxv):
+        return None
+    return ret
+
+
+def decode(hrp: str, addr: str) -> Tuple[Optional[int], Optional[List[int]]]:
+    """Decode a segwit address."""
+    encoding, hrpgot, data = bech32_decode(addr)
+    if hrpgot != hrp or hrpgot is None or data is None:
+        return (None, None)
+    decoded = convertbits(data[1:], 5, 8, False)
+    if decoded is None or len(decoded) < 2 or len(decoded) > 40:
+        return (None, None)
+    if data[0] > 16:
+        return (None, None)
+    if data[0] == 0 and len(decoded) != 20 and len(decoded) != 32:
+        return (None, None)
+    if (data[0] == 0 and encoding != Encoding.BECH32) or (
+        data[0] != 0 and encoding != Encoding.BECH32M
+    ):
+        return (None, None)
+    return (data[0], decoded)
+
+
+def encode(hrp: str, witver: int, witprog: bytes) -> Optional[str]:
+    """Encode a segwit address."""
+    encoding = Encoding.BECH32 if witver == 0 else Encoding.BECH32M
+    conv_bits = convertbits(witprog, 8, 5)
+    if conv_bits is None:
+        return None
+    ret = bech32_encode(encoding, hrp, [witver] + conv_bits)
+    if decode(hrp, ret) == (None, None):
+        return None
+    return ret
