9a4b6b912bcfdd0352a3d14e9c2757d0af16f4bc
Tomas Krnak <tomas@krnak.cz>
2022-03-01 09:43:48 +0100

    test(zcash): disable SECP256K1_ZKP

--- core/src/trezor/crypto/curve.py
+++ core/src/trezor/crypto/curve.py
@@ -1,1 +1,5 @@
-from trezorcrypto import bip340, curve25519, ed25519, nist256p1, secp256k1  # noqa: F401
+from trezorcrypto import curve25519, ed25519, nist256p1, secp256k1  # noqa: F401
+if False:
+	from trezorcrypto import bip340
+else:
+	bip340 = object()
