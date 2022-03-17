dcc4f6aaf931345d0dc39a2a67aa1938d220092b
grdddj <jiri.musil06@seznam.cz>
2022-03-17 13:56:35 +0100

    WIP - fix storage unit test

--- core/tests/test_storage.py
+++ core/tests/test_storage.py
@@ -1,21 +1,19 @@
 from common import *
-from trezor import config
-from storage import device
+from trezor import config, storagedevice
 
 
 class TestConfig(unittest.TestCase):
-
     def test_counter(self):
         config.init()
         config.wipe()
         for i in range(150):
-            self.assertEqual(device.next_u2f_counter(), i)
-        device.set_u2f_counter(350)
+            self.assertEqual(storagedevice.get_next_u2f_counter(), i)
+        storagedevice.set_u2f_counter(350)
         for i in range(351, 500):
-            self.assertEqual(device.next_u2f_counter(), i)
-        device.set_u2f_counter(0)
-        self.assertEqual(device.next_u2f_counter(), 1)
+            self.assertEqual(storagedevice.get_next_u2f_counter(), i)
+        storagedevice.set_u2f_counter(0)
+        self.assertEqual(storagedevice.get_next_u2f_counter(), 1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
