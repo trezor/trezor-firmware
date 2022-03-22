82182ae078fdd576601631529c4f9c56048fe22f
matejcik <ja@matejcik.cz>
2022-03-22 12:12:11 +0100

    ci: use external servers in CI and scripts

--- tests/ui_tests/reporting/download.py
+++ tests/ui_tests/reporting/download.py
@@ -7,19 +7,12 @@ from typing import Dict
 
 import requests
 
-RECORDS_WEBSITE = "https://firmware.corp.sldev.cz/ui_tests/"
+RECORDS_WEBSITE = "https://data.trezor.io/dev/firmware/ui_tests/"
 FIXTURES_MASTER = "https://raw.githubusercontent.com/trezor/trezor-firmware/master/tests/ui_tests/fixtures.json"
 FIXTURES_CURRENT = Path(__file__).resolve().parent.parent / "fixtures.json"
 
-_dns_failed = False
-
 
 def fetch_recorded(hash: str, path: Path) -> None:
-    global _dns_failed
-
-    if _dns_failed:
-        raise RuntimeError("Not trying firmware.corp.sldev.cz again after DNS failure.")
-
     zip_src = RECORDS_WEBSITE + hash + ".zip"
     zip_dest = path / "recorded.zip"
 
@@ -27,11 +20,6 @@ def fetch_recorded(hash: str, path: Path) -> None:
         urllib.request.urlretrieve(zip_src, zip_dest)
     except urllib.error.HTTPError:
         raise RuntimeError(f"No such recorded collection was found on '{zip_src}'.")
-    except urllib.error.URLError:
-        _dns_failed = True
-        raise RuntimeError(
-            "Server firmware.corp.sldev.cz could not be found. Are you on VPN?"
-        )
 
     with zipfile.ZipFile(zip_dest, "r") as z:
         z.extractall(path)
