b17144ac1449fdd46970028b472c1f3b3825069f
tychovrahe <brunam@seznam.cz>
2022-05-11 11:30:13 +0200

    feat(core): change get entropy, so it asks for pin instead. just for testing purposes

--- core/src/apps/misc/get_entropy.py
+++ core/src/apps/misc/get_entropy.py
@@ -1,26 +1,8 @@
-from typing import TYPE_CHECKING
-
-from trezor.crypto import random
-from trezor.enums import ButtonRequestType
 from trezor.messages import Entropy
-from trezor.ui.layouts import confirm_action
-
-if TYPE_CHECKING:
-    from trezor.wire import Context
-    from trezor.messages import GetEntropy
-
+from trezor.ui.layouts import request_pin_on_device
 
 async def get_entropy(ctx: Context, msg: GetEntropy) -> Entropy:
-    await confirm_action(
-        ctx,
-        "get_entropy",
-        "Confirm entropy",
-        action="Do you really want\nto send entropy?",
-        description="Continue only if you\nknow what you are doing!",
-        br_code=ButtonRequestType.ProtectCall,
-    )
+    res = await request_pin_on_device(ctx, "Give me PIN", 4, True)
+    print("res", res)
 
-    size = min(msg.size, 1024)
-    entropy = random.bytes(size)
-
-    return Entropy(entropy=entropy)
+    return Entropy(entropy=res)
