15c215f22474a638a4705578835203c37d8d242c
matejcik <ja@matejcik.cz>
2022-05-11 14:39:47 +0200

    wip: LRU cache modification

--- core/embed/rust/src/lru.rs
+++ core/embed/rust/src/lru.rs
@@ -5,7 +5,6 @@
 //! `u8` index type).
 
 use core::mem;
-
 use heapless::Vec;
 
 pub struct LruCache<T, const N: usize> {
@@ -18,24 +17,16 @@ pub struct LruCache<T, const N: usize> {
 }
 
 struct Entry<T> {
-    val: T,
+    val: Option<T>,
     /// Index of previous (more recently used) entry in the linked-list.
     prev: u8,
     /// Index of next (less recently used) entry in the linked-list.
     next: u8,
 }
 
-impl<T, const N: usize> Default for LruCache<T, N> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
 impl<T, const N: usize> LruCache<T, N> {
-    /// Create a new, empty, LRU cache.
     pub const fn new() -> Self {
         debug_assert!(N < u8::MAX as usize, "Capacity overflow");
-
         Self {
             entries: Vec::new(),
             head: 0,
@@ -43,27 +34,38 @@ impl<T, const N: usize> LruCache<T, N> {
         }
     }
 
-    pub fn insert(&mut self, val: T) -> Option<T> {
-        let entry = Entry {
-            val,
-            prev: 0,
-            next: 0,
-        };
-        if self.is_full() {
-            let tail = self.pop_tail();
-            let prev = mem::replace(&mut self.entries[tail as usize], entry);
-            self.push_head(tail);
-            Some(prev.val)
-        } else {
-            let _ = self.entries.push(entry);
-            self.push_head((self.len() - 1) as u8);
-            None
-        }
+    /// Reset the cache to an empty state.
+    pub fn reset(&mut self) {
+        self.entries = (0..N)
+            .map(|i| Entry {
+                val: None,
+                prev: (i - 1).max(0) as u8,
+                next: (i + 1).min(N - 1) as u8,
+            })
+            .collect();
+        self.head = 0;
+        self.tail = (N - 1) as u8;
     }
 
-    pub fn find(&mut self, mut pred: impl FnMut(&T) -> bool) -> Option<&mut T> {
+    fn initialized(&self) -> bool {
+        self.entries.len() == N
+    }
+
+    pub fn insert(&mut self, val: T) -> Option<T> {
+        if !self.initialized() {
+            self.reset();
+        }
+
+        let tail = self.tail;
+        let entry = &mut self.entries[tail as usize];
+        let prev_val = mem::replace(&mut entry.val, Some(val));
+        self.push_head(tail);
+        prev_val
+    }
+
+    pub fn find_first(&mut self, pred: &impl Fn(&T) -> bool) -> Option<&mut T> {
         for (i, entry) in self.entries.iter().enumerate() {
-            if pred(&entry.val) {
+            if entry.val.as_ref().map_or(false, pred) {
                 self.touch_index(i as u8);
                 return self.front_mut();
             }
@@ -71,34 +73,55 @@ impl<T, const N: usize> LruCache<T, N> {
         None
     }
 
+    pub fn drop_first(&mut self, pred: &impl Fn(&T) -> bool) -> bool {
+        for (i, entry) in self.entries.iter().enumerate() {
+            if entry.val.as_ref().map_or(false, pred) {
+                self.push_tail(i as u8);
+                return true;
+            }
+        }
+        false
+    }
+
     pub fn front(&self) -> Option<&T> {
-        self.entries.get(self.head as usize).map(|e| &e.val)
+        if !self.initialized() {
+            return None;
+        }
+        self.entries
+            .get(self.head as usize)
+            .and_then(|e| e.val.as_ref())
     }
 
     pub fn front_mut(&mut self) -> Option<&mut T> {
-        self.entries.get_mut(self.head as usize).map(|e| &mut e.val)
+        if !self.initialized() {
+            return None;
+        }
+        self.entries
+            .get_mut(self.head as usize)
+            .and_then(|e| e.val.as_mut())
     }
 
-    pub fn len(&self) -> usize {
-        self.entries.len()
-    }
+    // pub fn len(&self) -> usize {
+    //     self.entries.iter().filter(|v| v.val.is_some()).count()
+    // }
 
-    pub fn is_empty(&self) -> bool {
-        self.entries.is_empty()
-    }
+    // pub fn is_empty(&self) -> bool {
+    //     self.len() == 0
+    // }
 
-    pub fn is_full(&self) -> bool {
-        self.entries.is_full()
-    }
+    // pub fn is_full(&self) -> bool {
+    //     self.len() >= N
+    // }
 
     fn touch_index(&mut self, i: u8) {
+        debug_assert!(self.initialized());
         if i != self.head {
-            self.remove(i);
             self.push_head(i);
         }
     }
 
     fn remove(&mut self, i: u8) {
+        debug_assert!(self.initialized());
         let prev = self.entries[i as usize].prev;
         let next = self.entries[i as usize].next;
         if i == self.head {
@@ -114,19 +137,18 @@ impl<T, const N: usize> LruCache<T, N> {
     }
 
     fn push_head(&mut self, i: u8) {
-        if self.entries.len() == 1 {
-            self.tail = i;
-        } else {
-            self.entries[i as usize].next = self.head;
-            self.entries[self.head as usize].prev = i;
-        }
+        debug_assert!(self.initialized());
+        self.remove(i);
+        self.entries[i as usize].next = self.head;
+        self.entries[self.head as usize].prev = i;
         self.head = i;
     }
 
-    fn pop_tail(&mut self) -> u8 {
-        let old_tail = self.tail;
-        let new_tail = self.entries[old_tail as usize].prev;
-        self.tail = new_tail;
-        old_tail
+    fn push_tail(&mut self, i: u8) {
+        debug_assert!(self.initialized());
+        self.remove(i);
+        self.entries[i as usize].prev = self.tail;
+        self.entries[self.tail as usize].next = i;
+        self.tail = i;
     }
 }
