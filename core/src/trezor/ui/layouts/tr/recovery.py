1f1e12842f5644f4da56bf229d4aea9d724f74e6
grdddj <jiri.musil06@seznam.cz>
2022-05-16 11:47:13 +0200

    feat(core/rust): create seed recovery dialogue for model R

--- core/src/trezor/ui/layouts/tr/recovery.py
+++ core/src/trezor/ui/layouts/tr/recovery.py
@@ -1,18 +1,61 @@
 from typing import TYPE_CHECKING
 
+from trezor.enums import ButtonRequestType
+
+import trezorui2
+
+from ..common import button_request, interact
+from . import RustLayout, get_bool
+
 if TYPE_CHECKING:
     from trezor import wire
     from typing import Iterable, Callable
 
 
 async def request_word_count(ctx: wire.GenericContext, dry_run: bool) -> int:
-    raise NotImplementedError
+    await button_request(ctx, "word_count", code=ButtonRequestType.MnemonicWordCount)
+
+    if dry_run:
+        title = "Seed check"
+    else:
+        title = "Recovery mode"
+    text = "Number of words?"
+
+    count = await interact(
+        ctx,
+        RustLayout(
+            trezorui2.request_word_count(
+                title=title,
+                text=text,
+            )
+        ),
+        br_type="request_word_count",
+        br_code=ButtonRequestType.MnemonicWordCount,
+    )
+    # It can be returning a string
+    return int(count)
 
 
 async def request_word(
     ctx: wire.GenericContext, word_index: int, word_count: int, is_slip39: bool
 ) -> str:
-    raise NotImplementedError
+    prompt = f"Choose word {word_index + 1} of {word_count}:"
+
+    if is_slip39:
+        raise NotImplementedError
+    else:
+        word = await interact(
+            ctx,
+            RustLayout(
+                trezorui2.request_word_bip39(
+                    prompt=prompt,
+                )
+            ),
+            br_type="request_word",
+            br_code=ButtonRequestType.MnemonicInput,
+        )
+
+    return word
 
 
 async def show_remaining_shares(
@@ -37,4 +80,10 @@ async def continue_recovery(
     subtext: str | None,
     info_func: Callable | None,
 ) -> bool:
-    raise NotImplementedError
+    return await get_bool(
+        ctx=ctx,
+        title="Recovery",
+        data=f"{text}\n\n{subtext or ''}",
+        br_type="recovery",
+        br_code=ButtonRequestType.RecoveryHomepage,
+    )
