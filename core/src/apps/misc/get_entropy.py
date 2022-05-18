3b2e95f5dd9f12d454c2893189089fcd483eedb0
Simon Males <sime@sime.net.au>
2022-05-18 15:48:59 +0200

    feat(core): change get entropy, so it asks for pin confirmation. just for testing purposes

--- core/src/apps/misc/get_entropy.py
+++ core/src/apps/misc/get_entropy.py
@@ -1,8 +1,8 @@
 from trezor.messages import Entropy
-from trezor.ui.layouts import request_pin_on_device
+from ..common.request_pin import request_pin_confirm
 
 async def get_entropy(ctx: Context, msg: GetEntropy) -> Entropy:
-    res = await request_pin_on_device(ctx, "Give me PIN", 4, True)
+    res = await request_pin_confirm(ctx)
     print("res", res)
 
     return Entropy(entropy=res)
