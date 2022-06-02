1785b9486a018e13f81a944358a4a7b8399d9d29
tychovrahe <brunam@seznam.cz>
2022-06-02 21:57:01 +0200

    exposing button event function to rust

--- core/embed/rust/src/trezorhal/io.rs
+++ core/embed/rust/src/trezorhal/io.rs
@@ -1,15 +1,14 @@
-extern "C" {
-    fn touch_read() -> cty::c_uint;
-    fn touch_unpack_x(event: cty::c_uint) -> cty::c_ushort;
-    fn touch_unpack_y(event: cty::c_uint) -> cty::c_ushort;
-}
+use super::ffi;
 
 pub fn io_touch_read() -> u32 {
-    unsafe { touch_read() }
+    unsafe { ffi::touch_read() }
 }
 pub fn io_touch_unpack_x(event: u32) -> u16 {
-    unsafe { touch_unpack_x(event) }
+    unsafe { ffi::touch_unpack_x(event) }
 }
 pub fn io_touch_unpack_y(event: u32) -> u16 {
-    unsafe { touch_unpack_y(event) }
+    unsafe { ffi::touch_unpack_y(event) }
+}
+pub fn io_button_read() -> u32 {
+    unsafe { ffi::button_read() }
 }
--- core/embed/rust/src/trezorhal/mod.rs
+++ core/embed/rust/src/trezorhal/mod.rs
@@ -2,7 +2,7 @@ pub mod bip39;
 pub mod common;
 #[cfg(feature = "ui")]
 pub mod display;
-pub mod io;
 mod ffi;
+pub mod io;
 pub mod random;
 pub mod slip39;
