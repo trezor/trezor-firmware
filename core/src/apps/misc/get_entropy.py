3ef66f8bb111a8672374191796c192f7c430def4
grdddj <jiri.musil06@seznam.cz>
2022-06-13 15:22:54 +0200

    Revert "TEMP: `trezorctl crypto get-entropy 1111` for calling passphrase dialogue"
    This reverts commit f655aa63d5ff1935cda50da5096ee88e6fd2eddd.

--- core/src/apps/misc/get_entropy.py
+++ core/src/apps/misc/get_entropy.py
@@ -20,23 +20,6 @@ async def get_entropy(ctx: Context, msg: GetEntropy) -> Entropy:
         br_code=ButtonRequestType.ProtectCall,
     )
 
-    # WARNING: Only for testing purposes to spawn the passphrase dialogue
-    # Usage: `trezorctl crypto get-entropy 1111`
-    # Optionally: `trezorctl crypto get-entropy 111120` - setting max_len to 20 (must be less than 50)
-    msg_size_str = str(msg.size)
-    if msg_size_str.startswith("1111"):
-        from trezor.ui.layouts import request_passphrase_on_device
-
-        max_len = 50
-        if len(msg_size_str) > 5 and int(msg_size_str[4:]) < max_len:
-            max_len = int(msg_size_str[4:])
-        print("max_len", max_len)
-
-        res = await request_passphrase_on_device(ctx, max_len=max_len)
-        print("res", res)
-
-        return Entropy(entropy=res)
-
     size = min(msg.size, 1024)
     entropy = random.bytes(size)
 
