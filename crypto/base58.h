1f6eb43196fe2a960363dce5668cd589ee288939
Pavol Rusnak <pavol@rusnak.io>
2022-07-05 14:38:08 +0200

    chore(crypto): remove graphene base58 functions as they are not used anywhere
    [no changelog]

--- crypto/base58.h
+++ crypto/base58.h
@@ -43,11 +43,4 @@ int b58check(const void *bin, size_t binsz, HasherType hasher_type,
              const char *base58str);
 bool b58enc(char *b58, size_t *b58sz, const void *data, size_t binsz);
 
-#if USE_GRAPHENE
-int base58gph_encode_check(const uint8_t *data, int datalen, char *str,
-                           int strsize);
-int base58gph_decode_check(const char *str, uint8_t *data, int datalen);
-int b58gphcheck(const void *bin, size_t binsz, const char *base58str);
-#endif
-
 #endif
