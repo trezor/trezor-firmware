6bc4a81fb988e043e3c51f59af39adf806637004
tychovrahe <brunam@seznam.cz>
2022-08-09 19:16:54 +0200

    feat(core): Introduce stack overflow detection by moving stack to the start of RAM

--- core/embed/trezorhal/supervise.h
+++ core/embed/trezorhal/supervise.h
@@ -16,8 +16,14 @@ static inline uint32_t is_mode_unprivileged(void) {
   return r0 & 1;
 }
 
+static inline uint32_t is_mode_handler(void) {
+  uint32_t r0;
+  __asm__ volatile("mrs %0, ipsr" : "=r"(r0));
+  return (r0 & 0x1FF) != 0;
+}
+
 static inline void svc_enableIRQ(uint32_t IRQn) {
-  if (is_mode_unprivileged()) {
+  if (is_mode_unprivileged() && !is_mode_handler()) {
     register uint32_t r0 __asm__("r0") = IRQn;
     __asm__ __volatile__("svc %0" ::"i"(SVC_ENABLE_IRQ), "r"(r0) : "memory");
   } else {
@@ -26,7 +32,7 @@ static inline void svc_enableIRQ(uint32_t IRQn) {
 }
 
 static inline void svc_disableIRQ(uint32_t IRQn) {
-  if (is_mode_unprivileged()) {
+  if (is_mode_unprivileged() && !is_mode_handler()) {
     register uint32_t r0 __asm__("r0") = IRQn;
     __asm__ __volatile__("svc %0" ::"i"(SVC_DISABLE_IRQ), "r"(r0) : "memory");
   } else {
@@ -35,7 +41,7 @@ static inline void svc_disableIRQ(uint32_t IRQn) {
 }
 
 static inline void svc_setpriority(uint32_t IRQn, uint32_t priority) {
-  if (is_mode_unprivileged()) {
+  if (is_mode_unprivileged() && !is_mode_handler()) {
     register uint32_t r0 __asm__("r0") = IRQn;
     register uint32_t r1 __asm__("r1") = priority;
     __asm__ __volatile__("svc %0" ::"i"(SVC_SET_PRIORITY), "r"(r0), "r"(r1)
@@ -46,14 +52,14 @@ static inline void svc_setpriority(uint32_t IRQn, uint32_t priority) {
 }
 
 static inline void svc_shutdown(void) {
-  if (is_mode_unprivileged()) {
+  if (is_mode_unprivileged() && !is_mode_handler()) {
     __asm__ __volatile__("svc %0" ::"i"(SVC_SHUTDOWN) : "memory");
   } else {
     shutdown_privileged();
   }
 }
 static inline void svc_reboot_to_bootloader(void) {
-  if (is_mode_unprivileged()) {
+  if (is_mode_unprivileged() && !is_mode_handler()) {
     __asm__ __volatile__("svc %0" ::"i"(SVC_REBOOT_TO_BOOTLOADER) : "memory");
   } else {
     reboot_to_bootloader();
