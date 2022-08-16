98f3aff11dbd9784cf28247f90d58ac2eeb28c64
tychovrahe <brunam@seznam.cz>
2022-08-16 17:32:20 +0200

    clippy1

--- core/embed/rust/build.rs
+++ core/embed/rust/build.rs
@@ -321,11 +321,7 @@ fn is_firmware() -> bool {
 }
 
 fn is_bootloader() -> bool {
-    if cfg!(feature = "bootloader") {
-        true
-    } else {
-        false
-    }
+    cfg!(feature = "bootloader")
 }
 
 #[cfg(feature = "test")]
