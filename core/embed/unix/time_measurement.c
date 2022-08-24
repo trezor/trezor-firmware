1d471d957333e91e4cddad89c68eedcc54be0b2c
tychovrahe <brunam@seznam.cz>
2022-08-24 11:53:00 +0200

    time measurement unix

---
+++ core/embed/unix/time_measurement.c
@@ -0,0 +1,11 @@
+
+#include "common.h"
+
+void init_ticks(void) {
+}
+
+void get_ticks(void) {
+}
+
+void clear_acc(void) {
+}
---
+++ core/embed/unix/time_measurement.h
@@ -0,0 +1,7 @@
+//
+// Created by mbruna on 14.7.22.
+//
+
+void init_ticks(void);
+void get_ticks(void);
+void clear_acc(void);
