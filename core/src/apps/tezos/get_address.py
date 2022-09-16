164be3ac8fb210324e53678dbe1acbc2ac5b3343
grdddj <jiri.musil06@seznam.cz>
2022-09-16 18:48:52 +0200

    chore(core): decrease tezos size by 860 bytes

--- core/src/apps/tezos/get_address.py
+++ core/src/apps/tezos/get_address.py
@@ -1,16 +1,11 @@
 from typing import TYPE_CHECKING
 
-from trezor.crypto import hashlib
-from trezor.messages import TezosAddress
-from trezor.ui.layouts import show_address
-
-from apps.common import paths, seed
 from apps.common.keychain import with_slip44_keychain
 
-from . import CURVE, PATTERNS, SLIP44_ID, helpers
+from . import CURVE, PATTERNS, SLIP44_ID
 
 if TYPE_CHECKING:
-    from trezor.messages import TezosGetAddress
+    from trezor.messages import TezosGetAddress, TezosAddress
     from apps.common.keychain import Keychain
     from trezor.wire import Context
 
@@ -19,18 +14,22 @@ if TYPE_CHECKING:
 async def get_address(
     ctx: Context, msg: TezosGetAddress, keychain: Keychain
 ) -> TezosAddress:
+    from trezor.crypto import hashlib
+    from trezor.messages import TezosAddress
+    from trezor.ui.layouts import show_address
+    from apps.common import paths, seed
+    from . import helpers
+
     await paths.validate_path(ctx, keychain, msg.address_n)
 
     node = keychain.derive(msg.address_n)
 
     pk = seed.remove_ed25519_prefix(node.public_key())
     pkh = hashlib.blake2b(pk, outlen=helpers.PUBLIC_KEY_HASH_SIZE).digest()
-    address = helpers.base58_encode_check(
-        pkh, prefix=helpers.TEZOS_ED25519_ADDRESS_PREFIX
-    )
+    address = helpers.base58_encode_check(pkh, helpers.TEZOS_ED25519_ADDRESS_PREFIX)
 
     if msg.show_display:
         title = paths.address_n_to_str(msg.address_n)
-        await show_address(ctx, address=address, title=title)
+        await show_address(ctx, address, title=title)
 
     return TezosAddress(address=address)
--- core/tests/test_apps.tezos.encode.py
+++ core/tests/test_apps.tezos.encode.py
@@ -3,13 +3,20 @@ from common import *
 if not utils.BITCOIN_ONLY:
     from trezor.enums import TezosContractType
     from trezor.messages import TezosContractID
-    from apps.tezos.helpers import base58_encode_check, write_bool
+    from apps.tezos.helpers import base58_encode_check, write_bool, CONTRACT_ID_SIZE
     from apps.tezos.sign_tx import (
-        _encode_contract_id,
+        write_uint8,
+        write_bytes_fixed,
         _encode_data_with_bool_prefix,
         _encode_zarith,
         _encode_natural,
     )
+
+
+# NOTE: copy-pasted from apps.tezos.sign_tx
+def _encode_contract_id(w: "Writer", contract_id: TezosContractID) -> None:
+    write_uint8(w, contract_id.tag)
+    write_bytes_fixed(w, contract_id.hash, CONTRACT_ID_SIZE - 1)
 
 
 @unittest.skipUnless(not utils.BITCOIN_ONLY, "altcoin")
