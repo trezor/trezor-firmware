5b609d1f018aa25f314fddbaeaa02535597518ee
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2022-09-20 15:19:01 +0100

    added blackcoin support

--- core/src/apps/common/coininfo.py
+++ core/src/apps/common/coininfo.py
@@ -411,6 +411,35 @@ def by_name(name: str) -> CoinInfo:
                 overwintered=False,
                 confidential_assets=None,
             )
+        if name == "BlackCoin":
+            return CoinInfo(
+                coin_name=name,
+                coin_shortcut="BLK",
+                decimals=8,
+                address_type=25,
+                address_type_p2sh=85,
+                maxfee_kb=14000000,
+                signed_message_header="BlackCoin Signed Message:\n",
+                xpub_magic=0x0488b21e,
+                xpub_magic_segwit_p2sh=None,
+                xpub_magic_segwit_native=None,
+                xpub_magic_multisig_segwit_p2sh=None,
+                xpub_magic_multisig_segwit_native=None,
+                bech32_prefix=None,
+                cashaddr_prefix=None,
+                slip44=10,
+                segwit=False,
+                taproot=False,
+                fork_id=None,
+                force_bip143=False,
+                decred=False,
+                negative_fee=False,
+                curve_name='secp256k1',
+                extra_data=False,
+                timestamp=True,
+                overwintered=False,
+                confidential_assets=None,
+            )
         if name == "CPUchain":
             return CoinInfo(
                 coin_name=name,
