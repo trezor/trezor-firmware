48055ba6aa47754a2270c5460c294f5c6b62c751
grdddj <jiri.musil06@seznam.cz>
2022-10-04 19:42:49 +0200

    feat(tools): add scripts for firmware.elf file analysis using binsize
    [no changelog]

---
+++ core/tools/size/get.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CORE_DIR=$(realpath "${CURR_DIR}/../..")
+
+FIRMWARE_ELF="${CORE_DIR}/build/firmware/firmware.elf"
+MAP_FILE="${CORE_DIR}/build/firmware/firmware.map"
+
+export BINSIZE_ROOT_DIR="${CORE_DIR}"
+binsize get "${FIRMWARE_ELF}" -m "${MAP_FILE}" -s ".flash" -s ".flash2"  $@
