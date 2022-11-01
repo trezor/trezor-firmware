jia58628/trezor-trezor-firmware

refactor(python): convert firmware parsing to classes

python/src/trezorlib/firmware/vendor.py
@@ -1,0 +1,129 @@
+import hashlib
+import typing as t
+from copy import copy
+
+import construct as c
+
+from construct_classes import Struct, subcon
+
+from .. import cosi
+from ..toif import ToifStruct
+from ..tools import TupleAdapter
+from . import consts, util
+
+__all__ = [
+    "VendorTrust",
+    "VendorHeader",
+]
+
+
+class VendorTrust(Struct):
+    show_vendor_string: bool
+    require_user_click: bool
+    red_background: bool
+    delay: int
+
+    _reserved: int = 0
+
+    @staticmethod
+    def _transform(data: bytes) -> bytes:
+        """Byte-swap and bit-invert the VendorTrust field.
+
+        Vendor trust is interpreted as a bitmask in a 16-bit little-endian integer,
+        with the added twist that 0 means set and 1 means unset.
+        We feed it to a `BitStruct` that expects a big-endian sequence where bits have
+        the traditional meaning. We must therefore do a bitwise negation of each byte,
+        and return them in reverse order. This is the same transformation both ways,
+        fortunately, so we don't need two separate functions.
+        """
+        return bytes(~b & 0xFF for b in data)[::-1]
+
+    SUBCON = c.Transformed(
+        c.BitStruct(
+            "_reserved" / c.Default(c.BitsInteger(9), 0),
+            "show_vendor_string" / c.Flag,
+            "require_user_click" / c.Flag,
+            "red_background" / c.Flag,
+            "delay" / c.BitsInteger(4),
+        ),
+        _transform,
+        2,
+        _transform,
+        2,
+    )
+
+
+class VendorHeader(Struct):
+    header_len: int
+    expiry: int
+    version: t.Tuple[int, int]
+    sig_m: int
+    # sig_n: int
+    pubkeys: t.List[bytes]
+    text: str
+    image: t.Dict[str, t.Any]
+    sigmask: int
+    signature: bytes
+
+    trust: VendorTrust = subcon(VendorTrust)
+
+    # fmt: off
+    SUBCON = c.Struct(
+        "_start_offset" / c.Tell,
+        "magic" / c.Const(b"TRZV"),
+        "header_len" / c.Int32ul,
+        "expiry" / c.Int32ul,
+        "version" / TupleAdapter(c.Int8ul, c.Int8ul),
+        "sig_m" / c.Int8ul,
+        "sig_n" / c.Rebuild(c.Int8ul, c.len_(c.this.pubkeys)),
+        "trust" / VendorTrust.SUBCON,
+        "_reserved" / c.Padding(14),
+        "pubkeys" / c.Bytes(32)[c.this.sig_n],
+        "text" / c.Aligned(4, c.PascalString(c.Int8ul, "utf-8")),
+        "image" / ToifStruct,
+        "_end_offset" / c.Tell,
+
+        "_min_header_len" / c.Check(c.this.header_len > (c.this._end_offset - c.this._start_offset) + 65),
+        "_header_len_aligned" / c.Check(c.this.header_len % 512 == 0),
+
+        c.Padding(c.this.header_len - c.this._end_offset + c.this._start_offset - 65),
+        "sigmask" / c.Byte,
+        "signature" / c.Bytes(64),
+    )
+    # fmt: on
+
+    def digest(self) -> bytes:
+        cpy = copy(self)
+        cpy.sigmask = 0
+        cpy.signature = b"\x00" * 64
+        return hashlib.blake2s(cpy.build()).digest()
+
+    def vhash(self) -> bytes:
+        h = hashlib.blake2s()
+        sig_n = len(self.pubkeys)
+        h.update(self.sig_m.to_bytes(1, "little"))
+        h.update(sig_n.to_bytes(1, "little"))
+        for i in range(8):
+            if i < sig_n:
+                h.update(self.pubkeys[i])
+            else:
+                h.update(b"\x00" * 32)
+        return h.digest()
+
+    def verify(self, pubkeys: t.Sequence[bytes] = consts.V2_BOOTLOADER_KEYS) -> None:
+        digest = self.digest()
+        try:
+            cosi.verify(
+                self.signature,
+                digest,
+                consts.V2_SIGS_REQUIRED,
+                pubkeys,
+                self.sigmask,
+            )
+        except Exception:
+            raise util.InvalidSignatureError("Invalid vendor header signature.")
+
+        # XXX expiry is not used now
+        # now = time.gmtime()
+        # if time.gmtime(fw.vendor_header.expiry) < now:
+        #     raise ValueError("Vendor header expired.")

python/tests/test_firmware.py
@@ -1,0 +1,156 @@
+from pathlib import Path
+
+import construct
+import pytest
+import requests
+
+from trezorlib import firmware
+from trezorlib.firmware import (
+    VendorFirmware,
+    LegacyFirmware,
+    LegacyV2Firmware,
+    VendorHeader,
+)
+
+CORE_FW_VERSION = "2.4.2"
+CORE_FW_FINGERPRINT = "54ccf155510b5292bd17ed748409d0d135112e24e62eb74184639460beecb213"
+LEGACY_FW_VERSION = "1.10.3"
+LEGACY_FW_FINGERPRINT = (
+    "bf0cc936a9afbf0a4ae7b727a2817fb69fba432d7230a0ff7b79b4a73b845197"
+)
+
+CORE_FW = f"https://data.trezor.io/firmware/2/trezor-{CORE_FW_VERSION}.bin"
+LEGACY_FW = f"https://data.trezor.io/firmware/1/trezor-{LEGACY_FW_VERSION}.bin"
+
+HERE = Path(__file__).parent
+
+VENDOR_HEADER = (
+    HERE.parent.parent
+    / "core"
+    / "embed"
+    / "vendorheader"
+    / "vendorheader_satoshilabs_signed_prod.bin"
+)
+
+
+@pytest.fixture()
+def legacy_fw() -> bytes:
+    filename = "trezor-" + LEGACY_FW_VERSION + ".bin"
+    path = HERE / filename
+    if not path.exists():
+        r = requests.get(LEGACY_FW)
+        r.raise_for_status()
+        path.write_bytes(r.content)
+    return path.read_bytes()
+
+
+@pytest.fixture()
+def core_fw() -> bytes:
+    filename = "trezor-" + CORE_FW_VERSION + ".bin"
+    path = HERE / filename
+    if not path.exists():
+        r = requests.get(CORE_FW)
+        r.raise_for_status()
+        path.write_bytes(r.content)
+    return path.read_bytes()
+
+
+def test_core_basic(core_fw: bytes) -> None:
+    fw = VendorFirmware.parse(core_fw)
+    fw.verify()
+    assert fw.digest().hex() == CORE_FW_FINGERPRINT
+    version_str = ".".join(str(x) for x in fw.firmware.header.version)
+    assert version_str.startswith(CORE_FW_VERSION)
+    assert fw.vendor_header.text == "SatoshiLabs"
+    assert fw.build() == core_fw
+
+
+def test_vendor_header(core_fw: bytes) -> None:
+    fw = VendorFirmware.parse(core_fw)
+
+    vh_data = fw.vendor_header.build()
+    assert vh_data in core_fw
+    assert vh_data == VENDOR_HEADER.read_bytes()
+
+    vh = VendorHeader.parse(vh_data)
+    assert vh == fw.vendor_header
+    vh.verify()
+
+    with pytest.raises(construct.ConstructError):
+        VendorFirmware.parse(vh_data)
+
+
+def test_core_code_hashes(core_fw: bytes) -> None:
+    fw = VendorFirmware.parse(core_fw)
+    fw.firmware.header.hashes = []
+    assert fw.digest().hex() == CORE_FW_FINGERPRINT
+
+
+def test_legacy_basic(legacy_fw: bytes) -> None:
+    fw = LegacyFirmware.parse(legacy_fw)
+    fw.verify()
+    assert fw.digest().hex() == LEGACY_FW_FINGERPRINT
+    assert fw.build() == legacy_fw
+
+
+def test_unsigned(legacy_fw: bytes) -> None:
+    legacy = LegacyFirmware.parse(legacy_fw)
+
+    legacy.verify()
+    legacy.key_indexes = [0, 0, 0]
+    legacy.signatures = [b"", b"", b""]
+
+    with pytest.raises(firmware.Unsigned):
+        legacy.verify()
+
+    assert legacy.embedded_v2 is not None
+    legacy.embedded_v2.verify()
+
+    legacy.embedded_v2.header.v1_key_indexes = [0, 0, 0]
+    legacy.embedded_v2.header.v1_signatures = [b"", b"", b""]
+    with pytest.raises(firmware.Unsigned):
+        legacy.embedded_v2.verify()
+
+
+def test_disallow_unsigned(core_fw: bytes) -> None:
+    core = VendorFirmware.parse(core_fw)
+    core.firmware.header.sigmask = 0
+    core.firmware.header.signature = b""
+    with pytest.raises(firmware.InvalidSignatureError):
+        core.verify()
+
+
+def test_embedded_v2(legacy_fw: bytes) -> None:
+    legacy = LegacyFirmware.parse(legacy_fw)
+    assert legacy.embedded_v2 is not None
+    legacy.embedded_v2.verify()
+
+    embedded_data = legacy.embedded_v2.build()
+    cutoff_data = legacy_fw[256:]
+    assert cutoff_data == embedded_data
+    embedded = LegacyV2Firmware.parse(cutoff_data)
+    assert embedded == legacy.embedded_v2
+
+
+def test_integrity_legacy(legacy_fw: bytes) -> None:
+    legacy = LegacyFirmware.parse(legacy_fw)
+    legacy.verify()
+
+    modified_data = bytearray(legacy_fw)
+    modified_data[-1] ^= 0x01
+    modified = LegacyFirmware.parse(modified_data)
+    assert modified.digest() != legacy.digest()
+    with pytest.raises(firmware.InvalidSignatureError):
+        modified.verify()
+
+
+def test_integrity_core(core_fw: bytes) -> None:
+    core = VendorFirmware.parse(core_fw)
+    core.verify()
+
+    modified_data = bytearray(core_fw)
+    modified_data[-1] ^= 0x01
+    modified = VendorFirmware.parse(modified_data)
+    assert modified.digest() != core.digest()
+    with pytest.raises(firmware.FirmwareIntegrityError):
+        modified.verify()

trezor/trezor-firmware

python/src/trezorlib/firmware/vendor.py
@@ -1,0 +1,128 @@
+import hashlib
+import typing as t
+from copy import copy
+
+import construct as c
+from construct_classes import Struct, subcon
+
+from .. import cosi
+from ..toif import ToifStruct
+from ..tools import TupleAdapter
+from . import consts, util
+
+__all__ = [
+    "VendorTrust",
+    "VendorHeader",
+]
+
+
+def _transform_vendor_trust(data: bytes) -> bytes:
+    """Byte-swap and bit-invert the VendorTrust field.
+
+    Vendor trust is interpreted as a bitmask in a 16-bit little-endian integer,
+    with the added twist that 0 means set and 1 means unset.
+    We feed it to a `BitStruct` that expects a big-endian sequence where bits have
+    the traditional meaning. We must therefore do a bitwise negation of each byte,
+    and return them in reverse order. This is the same transformation both ways,
+    fortunately, so we don't need two separate functions.
+    """
+    return bytes(~b & 0xFF for b in data)[::-1]
+
+
+class VendorTrust(Struct):
+    show_vendor_string: bool
+    require_user_click: bool
+    red_background: bool
+    delay: int
+
+    _reserved: int = 0
+
+    SUBCON = c.Transformed(
+        c.BitStruct(
+            "_reserved" / c.Default(c.BitsInteger(9), 0),
+            "show_vendor_string" / c.Flag,
+            "require_user_click" / c.Flag,
+            "red_background" / c.Flag,
+            "delay" / c.BitsInteger(4),
+        ),
+        _transform_vendor_trust,
+        2,
+        _transform_vendor_trust,
+        2,
+    )
+
+
+class VendorHeader(Struct):
+    header_len: int
+    expiry: int
+    version: t.Tuple[int, int]
+    sig_m: int
+    # sig_n: int
+    pubkeys: t.List[bytes]
+    text: str
+    image: t.Dict[str, t.Any]
+    sigmask: int
+    signature: bytes
+
+    trust: VendorTrust = subcon(VendorTrust)
+
+    # fmt: off
+    SUBCON = c.Struct(
+        "_start_offset" / c.Tell,
+        "magic" / c.Const(b"TRZV"),
+        "header_len" / c.Int32ul,
+        "expiry" / c.Int32ul,
+        "version" / TupleAdapter(c.Int8ul, c.Int8ul),
+        "sig_m" / c.Int8ul,
+        "sig_n" / c.Rebuild(c.Int8ul, c.len_(c.this.pubkeys)),
+        "trust" / VendorTrust.SUBCON,
+        "_reserved" / c.Padding(14),
+        "pubkeys" / c.Bytes(32)[c.this.sig_n],
+        "text" / c.Aligned(4, c.PascalString(c.Int8ul, "utf-8")),
+        "image" / ToifStruct,
+        "_end_offset" / c.Tell,
+
+        "_min_header_len" / c.Check(c.this.header_len > (c.this._end_offset - c.this._start_offset) + 65),
+        "_header_len_aligned" / c.Check(c.this.header_len % 512 == 0),
+
+        c.Padding(c.this.header_len - c.this._end_offset + c.this._start_offset - 65),
+        "sigmask" / c.Byte,
+        "signature" / c.Bytes(64),
+    )
+    # fmt: on
+
+    def digest(self) -> bytes:
+        cpy = copy(self)
+        cpy.sigmask = 0
+        cpy.signature = b"\x00" * 64
+        return hashlib.blake2s(cpy.build()).digest()
+
+    def vhash(self) -> bytes:
+        h = hashlib.blake2s()
+        sig_n = len(self.pubkeys)
+        h.update(self.sig_m.to_bytes(1, "little"))
+        h.update(sig_n.to_bytes(1, "little"))
+        for i in range(8):
+            if i < sig_n:
+                h.update(self.pubkeys[i])
+            else:
+                h.update(b"\x00" * 32)
+        return h.digest()
+
+    def verify(self, pubkeys: t.Sequence[bytes] = consts.V2_BOOTLOADER_KEYS) -> None:
+        digest = self.digest()
+        try:
+            cosi.verify(
+                self.signature,
+                digest,
+                consts.V2_SIGS_REQUIRED,
+                pubkeys,
+                self.sigmask,
+            )
+        except Exception:
+            raise util.InvalidSignatureError("Invalid vendor header signature.")
+
+        # XXX expiry is not used now
+        # now = time.gmtime()
+        # if time.gmtime(fw.vendor_header.expiry) < now:
+        #     raise ValueError("Vendor header expired.")

python/tests/test_firmware.py
@@ -1,0 +1,153 @@
+from pathlib import Path
+
+import construct
+import pytest
+import requests
+
+from trezorlib import firmware
+from trezorlib.firmware import (
+    VendorFirmware,
+    LegacyFirmware,
+    LegacyV2Firmware,
+    VendorHeader,
+)
+
+CORE_FW_VERSION = "2.4.2"
+CORE_FW_FINGERPRINT = "54ccf155510b5292bd17ed748409d0d135112e24e62eb74184639460beecb213"
+LEGACY_FW_VERSION = "1.10.3"
+LEGACY_FW_FINGERPRINT = (
+    "bf0cc936a9afbf0a4ae7b727a2817fb69fba432d7230a0ff7b79b4a73b845197"
+)
+
+CORE_FW = f"https://data.trezor.io/firmware/2/trezor-{CORE_FW_VERSION}.bin"
+LEGACY_FW = f"https://data.trezor.io/firmware/1/trezor-{LEGACY_FW_VERSION}.bin"
+
+HERE = Path(__file__).parent
+
+VENDOR_HEADER = (
+    HERE.parent.parent
+    / "core"
+    / "embed"
+    / "vendorheader"
+    / "vendorheader_satoshilabs_signed_prod.bin"
+)
+
+
+def _fetch(url: str, version: str) -> bytes:
+    path = HERE / f"trezor-{version}.bin"
+    if not path.exists():
+        r = requests.get(url)
+        r.raise_for_status()
+        path.write_bytes(r.content)
+    return path.read_bytes()
+
+
+@pytest.fixture()
+def legacy_fw() -> bytes:
+    return _fetch(LEGACY_FW, LEGACY_FW_VERSION)
+
+
+@pytest.fixture()
+def core_fw() -> bytes:
+    return _fetch(CORE_FW, CORE_FW_VERSION)
+
+
+def test_core_basic(core_fw: bytes) -> None:
+    fw = VendorFirmware.parse(core_fw)
+    fw.verify()
+    assert fw.digest().hex() == CORE_FW_FINGERPRINT
+    version_str = ".".join(str(x) for x in fw.firmware.header.version)
+    assert version_str.startswith(CORE_FW_VERSION)
+    assert fw.vendor_header.text == "SatoshiLabs"
+    assert fw.build() == core_fw
+
+
+def test_vendor_header(core_fw: bytes) -> None:
+    fw = VendorFirmware.parse(core_fw)
+
+    vh_data = fw.vendor_header.build()
+    assert vh_data in core_fw
+    assert vh_data == VENDOR_HEADER.read_bytes()
+
+    vh = VendorHeader.parse(vh_data)
+    assert vh == fw.vendor_header
+    vh.verify()
+
+    with pytest.raises(construct.ConstructError):
+        VendorFirmware.parse(vh_data)
+
+
+def test_core_code_hashes(core_fw: bytes) -> None:
+    fw = VendorFirmware.parse(core_fw)
+    fw.firmware.header.hashes = []
+    assert fw.digest().hex() == CORE_FW_FINGERPRINT
+
+
+def test_legacy_basic(legacy_fw: bytes) -> None:
+    fw = LegacyFirmware.parse(legacy_fw)
+    fw.verify()
+    assert fw.digest().hex() == LEGACY_FW_FINGERPRINT
+    assert fw.build() == legacy_fw
+
+
+def test_unsigned(legacy_fw: bytes) -> None:
+    legacy = LegacyFirmware.parse(legacy_fw)
+
+    legacy.verify()
+    legacy.key_indexes = [0, 0, 0]
+    legacy.signatures = [b"", b"", b""]
+
+    with pytest.raises(firmware.Unsigned):
+        legacy.verify()
+
+    assert legacy.embedded_v2 is not None
+    legacy.embedded_v2.verify()
+
+    legacy.embedded_v2.header.v1_key_indexes = [0, 0, 0]
+    legacy.embedded_v2.header.v1_signatures = [b"", b"", b""]
+    with pytest.raises(firmware.Unsigned):
+        legacy.embedded_v2.verify()
+
+
+def test_disallow_unsigned(core_fw: bytes) -> None:
+    core = VendorFirmware.parse(core_fw)
+    core.firmware.header.sigmask = 0
+    core.firmware.header.signature = b""
+    with pytest.raises(firmware.InvalidSignatureError):
+        core.verify()
+
+
+def test_embedded_v2(legacy_fw: bytes) -> None:
+    legacy = LegacyFirmware.parse(legacy_fw)
+    assert legacy.embedded_v2 is not None
+    legacy.embedded_v2.verify()
+
+    embedded_data = legacy.embedded_v2.build()
+    cutoff_data = legacy_fw[256:]
+    assert cutoff_data == embedded_data
+    embedded = LegacyV2Firmware.parse(cutoff_data)
+    assert embedded == legacy.embedded_v2
+
+
+def test_integrity_legacy(legacy_fw: bytes) -> None:
+    legacy = LegacyFirmware.parse(legacy_fw)
+    legacy.verify()
+
+    modified_data = bytearray(legacy_fw)
+    modified_data[-1] ^= 0x01
+    modified = LegacyFirmware.parse(modified_data)
+    assert modified.digest() != legacy.digest()
+    with pytest.raises(firmware.InvalidSignatureError):
+        modified.verify()
+
+
+def test_integrity_core(core_fw: bytes) -> None:
+    core = VendorFirmware.parse(core_fw)
+    core.verify()
+
+    modified_data = bytearray(core_fw)
+    modified_data[-1] ^= 0x01
+    modified = VendorFirmware.parse(modified_data)
+    assert modified.digest() != core.digest()
+    with pytest.raises(firmware.FirmwareIntegrityError):
+        modified.verify()