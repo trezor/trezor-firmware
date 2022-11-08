jia58628/trezor-trezor-firmware

chore(core): decrease nem size by 1550 bytes

core/src/apps/nem/mosaic/layout.py
@@ -1,15 +1,6 @@
 from typing import TYPE_CHECKING
 
-from trezor import ui
-from trezor.enums import NEMMosaicLevy, NEMSupplyChangeType
-from trezor.ui.layouts import confirm_properties
-
-from ..layout import (
-    require_confirm_content,
-    require_confirm_fee,
-    require_confirm_final,
-    require_confirm_text,
-)
+from ..layout import require_confirm_content, require_confirm_final
 
 if TYPE_CHECKING:
     from trezor.messages import (
@@ -24,8 +15,14 @@ if TYPE_CHECKING:
 async def ask_mosaic_creation(
     ctx: Context, common: NEMTransactionCommon, creation: NEMMosaicCreation
 ) -> None:
-    await require_confirm_content(ctx, "Create mosaic", _creation_message(creation))
-    await require_confirm_properties(ctx, creation.definition)
+    from ..layout import require_confirm_fee
+
+    creation_message = [
+        ("Create mosaic", creation.definition.mosaic),
+        ("under namespace", creation.definition.namespace),
+    ]
+    await require_confirm_content(ctx, "Create mosaic", creation_message)
+    await _require_confirm_properties(ctx, creation.definition)
     await require_confirm_fee(ctx, "Confirm creation fee", creation.fee)
 
     await require_confirm_final(ctx, common.fee)
@@ -34,11 +31,19 @@ async def ask_mosaic_creation(
 async def ask_supply_change(
     ctx: Context, common: NEMTransactionCommon, change: NEMMosaicSupplyChange
 ) -> None:
-    await require_confirm_content(ctx, "Supply change", _supply_message(change))
+    from trezor.enums import NEMSupplyChangeType
+    from ..layout import require_confirm_text
+
+    supply_message = [
+        ("Modify supply for", change.mosaic),
+        ("under namespace", change.namespace),
+    ]
+    await require_confirm_content(ctx, "Supply change", supply_message)
+    common_msg = " supply by " + str(change.delta) + " whole units?"
     if change.type == NEMSupplyChangeType.SupplyChange_Decrease:
-        msg = "Decrease supply by " + str(change.delta) + " whole units?"
+        msg = "Decrease" + common_msg
     elif change.type == NEMSupplyChangeType.SupplyChange_Increase:
-        msg = "Increase supply by " + str(change.delta) + " whole units?"
+        msg = "Increase" + common_msg
     else:
         raise ValueError("Invalid supply change type")
     await require_confirm_text(ctx, msg)
@@ -46,45 +51,30 @@ async def ask_supply_change(
     await require_confirm_final(ctx, common.fee)
 
 
-def _creation_message(mosaic_creation: NEMMosaicCreation) -> list[tuple[str, str]]:
-    return [
-        ("Create mosaic", mosaic_creation.definition.mosaic),
-        ("under namespace", mosaic_creation.definition.namespace),
-    ]
-
-
-def _supply_message(supply_change: NEMMosaicSupplyChange) -> list[tuple[str, str]]:
-    return [
-        ("Modify supply for", supply_change.mosaic),
-        ("under namespace", supply_change.namespace),
-    ]
-
-
-async def require_confirm_properties(
+async def _require_confirm_properties(
     ctx: Context, definition: NEMMosaicDefinition
 ) -> None:
+    from trezor.enums import NEMMosaicLevy
+    from trezor import ui
+    from trezor.ui.layouts import confirm_properties
+
     properties = []
+    properties_append = properties.append  # cache
 
     # description
     if definition.description:
-        properties.append(("Description:", definition.description))
+        properties_append(("Description:", definition.description))
 
     # transferable
-    if definition.transferable:
-        transferable = "Yes"
-    else:
-        transferable = "No"
-    properties.append(("Transferable?", transferable))
+    transferable = "Yes" if definition.transferable else "No"
+    properties_append(("Transferable?", transferable))
 
     # mutable_supply
-    if definition.mutable_supply:
-        imm = "mutable"
-    else:
-        imm = "immutable"
+    imm = "mutable" if definition.mutable_supply else "immutable"
     if definition.supply:
-        properties.append(("Initial supply:", str(definition.supply) + "\n" + imm))
+        properties_append(("Initial supply:", str(definition.supply) + "\n" + imm))
     else:
-        properties.append(("Initial supply:", imm))
+        properties_append(("Initial supply:", imm))
 
     # levy
     if definition.levy:
@@ -93,24 +83,25 @@ async def require_confirm_properties(
         assert definition.levy_namespace is not None
         assert definition.levy_mosaic is not None
 
-        properties.append(("Levy recipient:", definition.levy_address))
+        properties_append(("Levy recipient:", definition.levy_address))
 
-        properties.append(("Levy fee:", str(definition.fee)))
-        properties.append(("Levy divisibility:", str(definition.divisibility)))
+        properties_append(("Levy fee:", str(definition.fee)))
+        properties_append(("Levy divisibility:", str(definition.divisibility)))
 
-        properties.append(("Levy namespace:", definition.levy_namespace))
-        properties.append(("Levy mosaic:", definition.levy_mosaic))
+        properties_append(("Levy namespace:", definition.levy_namespace))
+        properties_append(("Levy mosaic:", definition.levy_mosaic))
 
-        if definition.levy == NEMMosaicLevy.MosaicLevy_Absolute:
-            levy_type = "absolute"
-        else:
-            levy_type = "percentile"
-        properties.append(("Levy type:", levy_type))
+        levy_type = (
+            "absolute"
+            if definition.levy == NEMMosaicLevy.MosaicLevy_Absolute
+            else "percentile"
+        )
+        properties_append(("Levy type:", levy_type))
 
     await confirm_properties(
         ctx,
         "confirm_properties",
-        title="Confirm properties",
-        props=properties,
+        "Confirm properties",
+        properties,
         icon_color=ui.ORANGE_ICON,
     )

core/src/apps/nem/mosaic/serialize.py
@@ -1,9 +1,5 @@
 from typing import TYPE_CHECKING
 
-from ..helpers import (
-    NEM_TRANSACTION_TYPE_MOSAIC_CREATION,
-    NEM_TRANSACTION_TYPE_MOSAIC_SUPPLY_CHANGE,
-)
 from ..writers import (
     serialize_tx_common,
     write_bytes_with_len,
@@ -23,52 +19,58 @@ if TYPE_CHECKING:
 def serialize_mosaic_creation(
     common: NEMTransactionCommon, creation: NEMMosaicCreation, public_key: bytes
 ) -> bytes:
+    from ..helpers import NEM_TRANSACTION_TYPE_MOSAIC_CREATION
+
+    write_bytes_with_len_local = write_bytes_with_len  # cache
+
     w = serialize_tx_common(common, public_key, NEM_TRANSACTION_TYPE_MOSAIC_CREATION)
 
     mosaics_w = bytearray()
-    write_bytes_with_len(mosaics_w, public_key)
+    write_bytes_with_len_local(mosaics_w, public_key)
+
+    creation_definition = creation.definition  # cache
 
     identifier_w = bytearray()
-    write_bytes_with_len(identifier_w, creation.definition.namespace.encode())
-    write_bytes_with_len(identifier_w, creation.definition.mosaic.encode())
+    write_bytes_with_len_local(identifier_w, creation_definition.namespace.encode())
+    write_bytes_with_len_local(identifier_w, creation_definition.mosaic.encode())
 
-    write_bytes_with_len(mosaics_w, identifier_w)
-    write_bytes_with_len(mosaics_w, creation.definition.description.encode())
+    write_bytes_with_len_local(mosaics_w, identifier_w)
+    write_bytes_with_len_local(mosaics_w, creation_definition.description.encode())
     write_uint32_le(mosaics_w, 4)  # number of properties
 
-    _write_property(mosaics_w, "divisibility", creation.definition.divisibility)
-    _write_property(mosaics_w, "initialSupply", creation.definition.supply)
-    _write_property(mosaics_w, "supplyMutable", creation.definition.mutable_supply)
-    _write_property(mosaics_w, "transferable", creation.definition.transferable)
+    _write_property(mosaics_w, "divisibility", creation_definition.divisibility)
+    _write_property(mosaics_w, "initialSupply", creation_definition.supply)
+    _write_property(mosaics_w, "supplyMutable", creation_definition.mutable_supply)
+    _write_property(mosaics_w, "transferable", creation_definition.transferable)
 
-    if creation.definition.levy:
+    if creation_definition.levy:
         # all below asserts checked by nem.validators._validate_mosaic_creation
-        assert creation.definition.levy_namespace is not None
-        assert creation.definition.levy_mosaic is not None
-        assert creation.definition.levy_address is not None
-        assert creation.definition.fee is not None
+        assert creation_definition.levy_namespace is not None
+        assert creation_definition.levy_mosaic is not None
+        assert creation_definition.levy_address is not None
+        assert creation_definition.fee is not None
 
         levy_identifier_w = bytearray()
-        write_bytes_with_len(
-            levy_identifier_w, creation.definition.levy_namespace.encode()
+        write_bytes_with_len_local(
+            levy_identifier_w, creation_definition.levy_namespace.encode()
         )
-        write_bytes_with_len(
-            levy_identifier_w, creation.definition.levy_mosaic.encode()
+        write_bytes_with_len_local(
+            levy_identifier_w, creation_definition.levy_mosaic.encode()
         )
 
         levy_w = bytearray()
-        write_uint32_le(levy_w, creation.definition.levy)
-        write_bytes_with_len(levy_w, creation.definition.levy_address.encode())
-        write_bytes_with_len(levy_w, levy_identifier_w)
-        write_uint64_le(levy_w, creation.definition.fee)
+        write_uint32_le(levy_w, creation_definition.levy)
+        write_bytes_with_len_local(levy_w, creation_definition.levy_address.encode())
+        write_bytes_with_len_local(levy_w, levy_identifier_w)
+        write_uint64_le(levy_w, creation_definition.fee)
 
-        write_bytes_with_len(mosaics_w, levy_w)
+        write_bytes_with_len_local(mosaics_w, levy_w)
     else:
         write_uint32_le(mosaics_w, 0)  # no levy
 
-    write_bytes_with_len(w, mosaics_w)
+    write_bytes_with_len_local(w, mosaics_w)
 
-    write_bytes_with_len(w, creation.sink.encode())
+    write_bytes_with_len_local(w, creation.sink.encode())
     write_uint64_le(w, creation.fee)
 
     return w
@@ -77,6 +79,8 @@ def serialize_mosaic_creation(
 def serialize_mosaic_supply_change(
     common: NEMTransactionCommon, change: NEMMosaicSupplyChange, public_key: bytes
 ) -> bytes:
+    from ..helpers import NEM_TRANSACTION_TYPE_MOSAIC_SUPPLY_CHANGE
+
     w = serialize_tx_common(
         common, public_key, NEM_TRANSACTION_TYPE_MOSAIC_SUPPLY_CHANGE
     )

core/src/apps/nem/sign_tx.py
@@ -1,44 +1,51 @@
 from typing import TYPE_CHECKING
 
-from trezor import wire
-from trezor.crypto.curve import ed25519
-from trezor.messages import NEMSignedTx
-
-from apps.common import seed
 from apps.common.keychain import with_slip44_keychain
-from apps.common.paths import validate_path
 
-from . import CURVE, PATTERNS, SLIP44_ID, mosaic, multisig, namespace, transfer
-from .helpers import NEM_HASH_ALG, check_path
-from .validators import validate
+from . import CURVE, PATTERNS, SLIP44_ID
 
 if TYPE_CHECKING:
-    from trezor.messages import NEMSignTx
+    from trezor.messages import NEMSignTx, NEMSignedTx
     from apps.common.keychain import Keychain
+    from trezor.wire import Context
 
 
 @with_slip44_keychain(*PATTERNS, slip44_id=SLIP44_ID, curve=CURVE)
-async def sign_tx(ctx: wire.Context, msg: NEMSignTx, keychain: Keychain) -> NEMSignedTx:
+async def sign_tx(ctx: Context, msg: NEMSignTx, keychain: Keychain) -> NEMSignedTx:
+    from trezor.wire import DataError
+    from trezor.crypto.curve import ed25519
+    from trezor.messages import NEMSignedTx
+
+    from apps.common import seed
+    from apps.common.paths import validate_path
+
+    from . import mosaic, multisig, namespace, transfer
+    from .helpers import NEM_HASH_ALG, check_path
+    from .validators import validate
+
     validate(msg)
+
+    msg_multisig = msg.multisig  # cache
+    msg_transaction = msg.transaction  # cache
 
     await validate_path(
         ctx,
         keychain,
-        msg.transaction.address_n,
-        check_path(msg.transaction.address_n, msg.transaction.network),
+        msg_transaction.address_n,
+        check_path(msg_transaction.address_n, msg_transaction.network),
     )
 
-    node = keychain.derive(msg.transaction.address_n)
+    node = keychain.derive(msg_transaction.address_n)
 
-    if msg.multisig:
-        if msg.multisig.signer is None:
-            raise wire.DataError("No signer provided")
-        public_key = msg.multisig.signer
-        common = msg.multisig
+    if msg_multisig:
+        if msg_multisig.signer is None:
+            raise DataError("No signer provided")
+        public_key = msg_multisig.signer
+        common = msg_multisig
         await multisig.ask(ctx, msg)
     else:
         public_key = seed.remove_ed25519_prefix(node.public_key())
-        common = msg.transaction
+        common = msg_transaction
 
     if msg.transfer:
         tx = await transfer.transfer(ctx, public_key, common, msg.transfer, node)
@@ -54,28 +61,28 @@ async def sign_tx(ctx: wire.Context, msg: NEMSignTx, keychain: Keychain) -> NEMS
             public_key,
             common,
             msg.aggregate_modification,
-            msg.multisig is not None,
+            msg_multisig is not None,
         )
     elif msg.importance_transfer:
         tx = await transfer.importance_transfer(
             ctx, public_key, common, msg.importance_transfer
         )
     else:
-        raise wire.DataError("No transaction provided")
+        raise DataError("No transaction provided")
 
-    if msg.multisig:
+    if msg_multisig:
         # wrap transaction in multisig wrapper
         if msg.cosigning:
-            assert msg.multisig.signer is not None
+            assert msg_multisig.signer is not None
             tx = multisig.cosign(
                 seed.remove_ed25519_prefix(node.public_key()),
-                msg.transaction,
+                msg_transaction,
                 tx,
-                msg.multisig.signer,
+                msg_multisig.signer,
             )
         else:
             tx = multisig.initiate(
-                seed.remove_ed25519_prefix(node.public_key()), msg.transaction, tx
+                seed.remove_ed25519_prefix(node.public_key()), msg_transaction, tx
             )
 
     signature = ed25519.sign(node.private_key(), tx, NEM_HASH_ALG)

trezor/trezor-firmware

core/src/apps/nem/mosaic/layout.py
@@ -1,15 +1,6 @@
 from typing import TYPE_CHECKING
 
-from trezor import ui
-from trezor.enums import NEMMosaicLevy, NEMSupplyChangeType
-from trezor.ui.layouts import confirm_properties
-
-from ..layout import (
-    require_confirm_content,
-    require_confirm_fee,
-    require_confirm_final,
-    require_confirm_text,
-)
+from ..layout import require_confirm_content, require_confirm_final
 
 if TYPE_CHECKING:
     from trezor.messages import (
@@ -24,8 +15,14 @@ if TYPE_CHECKING:
 async def ask_mosaic_creation(
     ctx: Context, common: NEMTransactionCommon, creation: NEMMosaicCreation
 ) -> None:
-    await require_confirm_content(ctx, "Create mosaic", _creation_message(creation))
-    await require_confirm_properties(ctx, creation.definition)
+    from ..layout import require_confirm_fee
+
+    creation_message = [
+        ("Create mosaic", creation.definition.mosaic),
+        ("under namespace", creation.definition.namespace),
+    ]
+    await require_confirm_content(ctx, "Create mosaic", creation_message)
+    await _require_confirm_properties(ctx, creation.definition)
     await require_confirm_fee(ctx, "Confirm creation fee", creation.fee)
 
     await require_confirm_final(ctx, common.fee)
@@ -34,57 +31,49 @@ async def ask_mosaic_creation(
 async def ask_supply_change(
     ctx: Context, common: NEMTransactionCommon, change: NEMMosaicSupplyChange
 ) -> None:
-    await require_confirm_content(ctx, "Supply change", _supply_message(change))
+    from trezor.enums import NEMSupplyChangeType
+    from ..layout import require_confirm_text
+
+    supply_message = [
+        ("Modify supply for", change.mosaic),
+        ("under namespace", change.namespace),
+    ]
+    await require_confirm_content(ctx, "Supply change", supply_message)
     if change.type == NEMSupplyChangeType.SupplyChange_Decrease:
-        msg = "Decrease supply by " + str(change.delta) + " whole units?"
+        action = "Decrease"
     elif change.type == NEMSupplyChangeType.SupplyChange_Increase:
-        msg = "Increase supply by " + str(change.delta) + " whole units?"
+        action = "Increase"
     else:
         raise ValueError("Invalid supply change type")
-    await require_confirm_text(ctx, msg)
+    await require_confirm_text(ctx, f"{action} supply by {change.delta} whole units?")
 
     await require_confirm_final(ctx, common.fee)
 
 
-def _creation_message(mosaic_creation: NEMMosaicCreation) -> list[tuple[str, str]]:
-    return [
-        ("Create mosaic", mosaic_creation.definition.mosaic),
-        ("under namespace", mosaic_creation.definition.namespace),
-    ]
-
-
-def _supply_message(supply_change: NEMMosaicSupplyChange) -> list[tuple[str, str]]:
-    return [
-        ("Modify supply for", supply_change.mosaic),
-        ("under namespace", supply_change.namespace),
-    ]
-
-
-async def require_confirm_properties(
+async def _require_confirm_properties(
     ctx: Context, definition: NEMMosaicDefinition
 ) -> None:
+    from trezor.enums import NEMMosaicLevy
+    from trezor import ui
+    from trezor.ui.layouts import confirm_properties
+
     properties = []
+    append = properties.append  # local_cache_attribute
 
     # description
     if definition.description:
-        properties.append(("Description:", definition.description))
+        append(("Description:", definition.description))
 
     # transferable
-    if definition.transferable:
-        transferable = "Yes"
-    else:
-        transferable = "No"
-    properties.append(("Transferable?", transferable))
+    transferable = "Yes" if definition.transferable else "No"
+    append(("Transferable?", transferable))
 
     # mutable_supply
-    if definition.mutable_supply:
-        imm = "mutable"
-    else:
-        imm = "immutable"
+    imm = "mutable" if definition.mutable_supply else "immutable"
     if definition.supply:
-        properties.append(("Initial supply:", str(definition.supply) + "\n" + imm))
+        append(("Initial supply:", str(definition.supply) + "\n" + imm))
     else:
-        properties.append(("Initial supply:", imm))
+        append(("Initial supply:", imm))
 
     # levy
     if definition.levy:
@@ -93,24 +82,25 @@ async def require_confirm_properties(
         assert definition.levy_namespace is not None
         assert definition.levy_mosaic is not None
 
-        properties.append(("Levy recipient:", definition.levy_address))
+        append(("Levy recipient:", definition.levy_address))
 
-        properties.append(("Levy fee:", str(definition.fee)))
-        properties.append(("Levy divisibility:", str(definition.divisibility)))
+        append(("Levy fee:", str(definition.fee)))
+        append(("Levy divisibility:", str(definition.divisibility)))
 
-        properties.append(("Levy namespace:", definition.levy_namespace))
-        properties.append(("Levy mosaic:", definition.levy_mosaic))
+        append(("Levy namespace:", definition.levy_namespace))
+        append(("Levy mosaic:", definition.levy_mosaic))
 
-        if definition.levy == NEMMosaicLevy.MosaicLevy_Absolute:
-            levy_type = "absolute"
-        else:
-            levy_type = "percentile"
-        properties.append(("Levy type:", levy_type))
+        levy_type = (
+            "absolute"
+            if definition.levy == NEMMosaicLevy.MosaicLevy_Absolute
+            else "percentile"
+        )
+        append(("Levy type:", levy_type))
 
     await confirm_properties(
         ctx,
         "confirm_properties",
-        title="Confirm properties",
-        props=properties,
+        "Confirm properties",
+        properties,
         icon_color=ui.ORANGE_ICON,
     )

core/src/apps/nem/mosaic/serialize.py
@@ -1,9 +1,5 @@
 from typing import TYPE_CHECKING
 
-from ..helpers import (
-    NEM_TRANSACTION_TYPE_MOSAIC_CREATION,
-    NEM_TRANSACTION_TYPE_MOSAIC_SUPPLY_CHANGE,
-)
 from ..writers import (
     serialize_tx_common,
     write_bytes_with_len,
@@ -23,44 +19,43 @@ if TYPE_CHECKING:
 def serialize_mosaic_creation(
     common: NEMTransactionCommon, creation: NEMMosaicCreation, public_key: bytes
 ) -> bytes:
+    from ..helpers import NEM_TRANSACTION_TYPE_MOSAIC_CREATION
+
     w = serialize_tx_common(common, public_key, NEM_TRANSACTION_TYPE_MOSAIC_CREATION)
 
     mosaics_w = bytearray()
     write_bytes_with_len(mosaics_w, public_key)
+    definition = creation.definition  # local_cache_attribute
 
     identifier_w = bytearray()
-    write_bytes_with_len(identifier_w, creation.definition.namespace.encode())
-    write_bytes_with_len(identifier_w, creation.definition.mosaic.encode())
+    write_bytes_with_len(identifier_w, definition.namespace.encode())
+    write_bytes_with_len(identifier_w, definition.mosaic.encode())
 
     write_bytes_with_len(mosaics_w, identifier_w)
-    write_bytes_with_len(mosaics_w, creation.definition.description.encode())
+    write_bytes_with_len(mosaics_w, definition.description.encode())
     write_uint32_le(mosaics_w, 4)  # number of properties
 
-    _write_property(mosaics_w, "divisibility", creation.definition.divisibility)
-    _write_property(mosaics_w, "initialSupply", creation.definition.supply)
-    _write_property(mosaics_w, "supplyMutable", creation.definition.mutable_supply)
-    _write_property(mosaics_w, "transferable", creation.definition.transferable)
+    _write_property(mosaics_w, "divisibility", definition.divisibility)
+    _write_property(mosaics_w, "initialSupply", definition.supply)
+    _write_property(mosaics_w, "supplyMutable", definition.mutable_supply)
+    _write_property(mosaics_w, "transferable", definition.transferable)
 
-    if creation.definition.levy:
+    if definition.levy:
         # all below asserts checked by nem.validators._validate_mosaic_creation
-        assert creation.definition.levy_namespace is not None
-        assert creation.definition.levy_mosaic is not None
-        assert creation.definition.levy_address is not None
-        assert creation.definition.fee is not None
+        assert definition.levy_namespace is not None
+        assert definition.levy_mosaic is not None
+        assert definition.levy_address is not None
+        assert definition.fee is not None
 
         levy_identifier_w = bytearray()
-        write_bytes_with_len(
-            levy_identifier_w, creation.definition.levy_namespace.encode()
-        )
-        write_bytes_with_len(
-            levy_identifier_w, creation.definition.levy_mosaic.encode()
-        )
+        write_bytes_with_len(levy_identifier_w, definition.levy_namespace.encode())
+        write_bytes_with_len(levy_identifier_w, definition.levy_mosaic.encode())
 
         levy_w = bytearray()
-        write_uint32_le(levy_w, creation.definition.levy)
-        write_bytes_with_len(levy_w, creation.definition.levy_address.encode())
+        write_uint32_le(levy_w, definition.levy)
+        write_bytes_with_len(levy_w, definition.levy_address.encode())
         write_bytes_with_len(levy_w, levy_identifier_w)
-        write_uint64_le(levy_w, creation.definition.fee)
+        write_uint64_le(levy_w, definition.fee)
 
         write_bytes_with_len(mosaics_w, levy_w)
     else:
@@ -77,6 +72,8 @@ def serialize_mosaic_creation(
 def serialize_mosaic_supply_change(
     common: NEMTransactionCommon, change: NEMMosaicSupplyChange, public_key: bytes
 ) -> bytes:
+    from ..helpers import NEM_TRANSACTION_TYPE_MOSAIC_SUPPLY_CHANGE
+
     w = serialize_tx_common(
         common, public_key, NEM_TRANSACTION_TYPE_MOSAIC_SUPPLY_CHANGE
     )

core/src/apps/nem/sign_tx.py
@@ -1,44 +1,50 @@
 from typing import TYPE_CHECKING
 
-from trezor import wire
-from trezor.crypto.curve import ed25519
-from trezor.messages import NEMSignedTx
-
-from apps.common import seed
 from apps.common.keychain import with_slip44_keychain
-from apps.common.paths import validate_path
 
-from . import CURVE, PATTERNS, SLIP44_ID, mosaic, multisig, namespace, transfer
-from .helpers import NEM_HASH_ALG, check_path
-from .validators import validate
+from . import CURVE, PATTERNS, SLIP44_ID
 
 if TYPE_CHECKING:
-    from trezor.messages import NEMSignTx
+    from trezor.messages import NEMSignTx, NEMSignedTx
     from apps.common.keychain import Keychain
+    from trezor.wire import Context
 
 
 @with_slip44_keychain(*PATTERNS, slip44_id=SLIP44_ID, curve=CURVE)
-async def sign_tx(ctx: wire.Context, msg: NEMSignTx, keychain: Keychain) -> NEMSignedTx:
+async def sign_tx(ctx: Context, msg: NEMSignTx, keychain: Keychain) -> NEMSignedTx:
+    from trezor.wire import DataError
+    from trezor.crypto.curve import ed25519
+    from trezor.messages import NEMSignedTx
+
+    from apps.common import seed
+    from apps.common.paths import validate_path
+
+    from . import mosaic, multisig, namespace, transfer
+    from .helpers import NEM_HASH_ALG, check_path
+    from .validators import validate
+
     validate(msg)
+    msg_multisig = msg.multisig  # local_cache_attribute
+    transaction = msg.transaction  # local_cache_attribute
 
     await validate_path(
         ctx,
         keychain,
-        msg.transaction.address_n,
-        check_path(msg.transaction.address_n, msg.transaction.network),
+        transaction.address_n,
+        check_path(transaction.address_n, transaction.network),
     )
 
-    node = keychain.derive(msg.transaction.address_n)
+    node = keychain.derive(transaction.address_n)
 
-    if msg.multisig:
-        if msg.multisig.signer is None:
-            raise wire.DataError("No signer provided")
-        public_key = msg.multisig.signer
-        common = msg.multisig
+    if msg_multisig:
+        if msg_multisig.signer is None:
+            raise DataError("No signer provided")
+        public_key = msg_multisig.signer
+        common = msg_multisig
         await multisig.ask(ctx, msg)
     else:
         public_key = seed.remove_ed25519_prefix(node.public_key())
-        common = msg.transaction
+        common = transaction
 
     if msg.transfer:
         tx = await transfer.transfer(ctx, public_key, common, msg.transfer, node)
@@ -54,28 +60,28 @@ async def sign_tx(ctx: wire.Context, msg: NEMSignTx, keychain: Keychain) -> NEMS
             public_key,
             common,
             msg.aggregate_modification,
-            msg.multisig is not None,
+            msg_multisig is not None,
         )
     elif msg.importance_transfer:
         tx = await transfer.importance_transfer(
             ctx, public_key, common, msg.importance_transfer
         )
     else:
-        raise wire.DataError("No transaction provided")
+        raise DataError("No transaction provided")
 
-    if msg.multisig:
+    if msg_multisig:
         # wrap transaction in multisig wrapper
         if msg.cosigning:
-            assert msg.multisig.signer is not None
+            assert msg_multisig.signer is not None
             tx = multisig.cosign(
                 seed.remove_ed25519_prefix(node.public_key()),
-                msg.transaction,
+                transaction,
                 tx,
-                msg.multisig.signer,
+                msg_multisig.signer,
             )
         else:
             tx = multisig.initiate(
-                seed.remove_ed25519_prefix(node.public_key()), msg.transaction, tx
+                seed.remove_ed25519_prefix(node.public_key()), transaction, tx
             )
 
     signature = ed25519.sign(node.private_key(), tx, NEM_HASH_ALG)