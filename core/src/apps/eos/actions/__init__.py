jia58628/trezor-trezor-firmware

chore(core): decrease eos size by 1kb

core/src/apps/eos/actions/__init__.py
@@ -1,119 +1,125 @@
 from typing import TYPE_CHECKING
 
-from trezor.crypto.hashlib import sha256
-from trezor.messages import EosTxActionAck, EosTxActionRequest
-from trezor.utils import HashWriter
-
-from .. import helpers, writers
-from . import layout
-
 if TYPE_CHECKING:
     from trezor import wire
-    from trezor.utils import Writer
+    from trezor.utils import Writer, HashWriter
+    from trezor.messages import EosTxActionAck
 
 
 async def process_action(
     ctx: wire.Context, sha: HashWriter, action: EosTxActionAck
 ) -> None:
+    from .. import helpers, writers
+    from . import layout
+
     name = helpers.eos_name_to_string(action.common.name)
     account = helpers.eos_name_to_string(action.common.account)
 
-    if not check_action(action, name, account):
+    if not _check_action(action, name, account):
         raise ValueError("Invalid action")
 
     w = bytearray()
     if account == "eosio":
         if name == "buyram":
-            assert action.buy_ram is not None  # check_action
+            assert action.buy_ram is not None  # _check_action
             await layout.confirm_action_buyram(ctx, action.buy_ram)
             writers.write_action_buyram(w, action.buy_ram)
         elif name == "buyrambytes":
-            assert action.buy_ram_bytes is not None  # check_action
+            assert action.buy_ram_bytes is not None  # _check_action
             await layout.confirm_action_buyrambytes(ctx, action.buy_ram_bytes)
             writers.write_action_buyrambytes(w, action.buy_ram_bytes)
         elif name == "sellram":
-            assert action.sell_ram is not None  # check_action
+            assert action.sell_ram is not None  # _check_action
             await layout.confirm_action_sellram(ctx, action.sell_ram)
             writers.write_action_sellram(w, action.sell_ram)
         elif name == "delegatebw":
-            assert action.delegate is not None  # check_action
+            assert action.delegate is not None  # _check_action
             await layout.confirm_action_delegate(ctx, action.delegate)
             writers.write_action_delegate(w, action.delegate)
         elif name == "undelegatebw":
-            assert action.undelegate is not None  # check_action
+            assert action.undelegate is not None  # _check_action
             await layout.confirm_action_undelegate(ctx, action.undelegate)
             writers.write_action_undelegate(w, action.undelegate)
         elif name == "refund":
-            assert action.refund is not None  # check_action
+            assert action.refund is not None  # _check_action
             await layout.confirm_action_refund(ctx, action.refund)
             writers.write_action_refund(w, action.refund)
         elif name == "voteproducer":
-            assert action.vote_producer is not None  # check_action
+            assert action.vote_producer is not None  # _check_action
             await layout.confirm_action_voteproducer(ctx, action.vote_producer)
             writers.write_action_voteproducer(w, action.vote_producer)
         elif name == "updateauth":
-            assert action.update_auth is not None  # check_action
+            assert action.update_auth is not None  # _check_action
             await layout.confirm_action_updateauth(ctx, action.update_auth)
             writers.write_action_updateauth(w, action.update_auth)
         elif name == "deleteauth":
-            assert action.delete_auth is not None  # check_action
+            assert action.delete_auth is not None  # _check_action
             await layout.confirm_action_deleteauth(ctx, action.delete_auth)
             writers.write_action_deleteauth(w, action.delete_auth)
         elif name == "linkauth":
-            assert action.link_auth is not None  # check_action
+            assert action.link_auth is not None  # _check_action
             await layout.confirm_action_linkauth(ctx, action.link_auth)
             writers.write_action_linkauth(w, action.link_auth)
         elif name == "unlinkauth":
-            assert action.unlink_auth is not None  # check_action
+            assert action.unlink_auth is not None  # _check_action
             await layout.confirm_action_unlinkauth(ctx, action.unlink_auth)
             writers.write_action_unlinkauth(w, action.unlink_auth)
         elif name == "newaccount":
-            assert action.new_account is not None  # check_action
+            assert action.new_account is not None  # _check_action
             await layout.confirm_action_newaccount(ctx, action.new_account)
             writers.write_action_newaccount(w, action.new_account)
         else:
             raise ValueError("Unrecognized action type for eosio")
     elif name == "transfer":
-        assert action.transfer is not None  # check_action
+        assert action.transfer is not None  # _check_action
         await layout.confirm_action_transfer(ctx, action.transfer, account)
         writers.write_action_transfer(w, action.transfer)
     else:
-        await process_unknown_action(ctx, w, action)
+        await _process_unknown_action(ctx, w, action)
 
     writers.write_action_common(sha, action.common)
     writers.write_bytes_prefixed(sha, w)
 
 
-async def process_unknown_action(
+async def _process_unknown_action(
     ctx: wire.Context, w: Writer, action: EosTxActionAck
 ) -> None:
-    assert action.unknown is not None
-    checksum = HashWriter(sha256())
-    writers.write_uvarint(checksum, action.unknown.data_size)
-    checksum.extend(action.unknown.data_chunk)
+    from trezor.crypto.hashlib import sha256
+    from trezor.utils import HashWriter
+    from trezor.messages import EosTxActionAck, EosTxActionRequest
+    from .. import writers
+    from . import layout
 
-    writers.write_bytes_unchecked(w, action.unknown.data_chunk)
-    bytes_left = action.unknown.data_size - len(action.unknown.data_chunk)
+    action_unknown = action.unknown  # cache
+    assert action_unknown is not None
+    data_chunk = action_unknown.data_chunk  # cache
+
+    checksum = HashWriter(sha256())
+    writers.write_uvarint(checksum, action_unknown.data_size)
+    checksum.extend(data_chunk)
+
+    writers.write_bytes_unchecked(w, data_chunk)
+    bytes_left = action_unknown.data_size - len(data_chunk)
 
     while bytes_left != 0:
         action = await ctx.call(
             EosTxActionRequest(data_size=bytes_left), EosTxActionAck
         )
 
-        if action.unknown is None:
+        if action_unknown is None:
             raise ValueError("Bad response. Unknown struct expected.")
 
-        checksum.extend(action.unknown.data_chunk)
-        writers.write_bytes_unchecked(w, action.unknown.data_chunk)
+        checksum.extend(data_chunk)
+        writers.write_bytes_unchecked(w, data_chunk)
 
-        bytes_left -= len(action.unknown.data_chunk)
+        bytes_left -= len(data_chunk)
         if bytes_left < 0:
             raise ValueError("Bad response. Buffer overflow.")
 
     await layout.confirm_action_unknown(ctx, action.common, checksum.get_digest())
 
 
-def check_action(action: EosTxActionAck, name: str, account: str) -> bool:
+def _check_action(action: EosTxActionAck, name: str, account: str) -> bool:
     if account == "eosio":
         return (
             (name == "buyram" and action.buy_ram is not None)

core/src/apps/eos/sign_tx.py
@@ -1,36 +1,50 @@
 from typing import TYPE_CHECKING
 
-from trezor import wire
-from trezor.crypto.curve import secp256k1
-from trezor.crypto.hashlib import sha256
-from trezor.messages import EosSignedTx, EosTxActionAck, EosTxActionRequest
-from trezor.utils import HashWriter
-
-from apps.common import paths
-from apps.common.keychain import Keychain, auto_keychain
-
-from . import writers
-from .actions import process_action
-from .helpers import base58_encode
-from .layout import require_sign_tx
+from apps.common.keychain import auto_keychain
 
 if TYPE_CHECKING:
-    from trezor.messages import EosSignTx
+    from trezor.wire import Context
+    from trezor.messages import EosSignTx, EosSignedTx
+    from apps.common.keychain import Keychain
 
 
 @auto_keychain(__name__)
-async def sign_tx(ctx: wire.Context, msg: EosSignTx, keychain: Keychain) -> EosSignedTx:
-    if not msg.num_actions:
-        raise wire.DataError("No actions")
+async def sign_tx(ctx: Context, msg: EosSignTx, keychain: Keychain) -> EosSignedTx:
+    from trezor.wire import DataError
+    from trezor.crypto.curve import secp256k1
+    from trezor.crypto.hashlib import sha256
+    from trezor.messages import EosSignedTx, EosTxActionAck, EosTxActionRequest
+    from trezor.utils import HashWriter
+    from apps.common import paths
+    from .writers import write_uvarint, write_header, write_bytes_fixed
+    from .actions import process_action
+    from .helpers import base58_encode
+    from .layout import require_sign_tx
+
+    num_actions = msg.num_actions  # cache
+
+    if not num_actions:
+        raise DataError("No actions")
 
     await paths.validate_path(ctx, keychain, msg.address_n)
 
     node = keychain.derive(msg.address_n)
     sha = HashWriter(sha256())
-    await _init(ctx, sha, msg)
-    await _actions(ctx, sha, msg.num_actions)
-    writers.write_uvarint(sha, 0)
-    writers.write_bytes_fixed(sha, bytearray(32), 32)
+
+    # init
+    write_bytes_fixed(sha, msg.chain_id, 32)
+    write_header(sha, msg.header)
+    write_uvarint(sha, 0)
+    write_uvarint(sha, num_actions)
+    await require_sign_tx(ctx, num_actions)
+
+    # actions
+    for _ in range(num_actions):
+        action = await ctx.call(EosTxActionRequest(), EosTxActionAck)
+        await process_action(ctx, sha, action)
+
+    write_uvarint(sha, 0)
+    write_bytes_fixed(sha, bytearray(32), 32)
 
     digest = sha.get_digest()
     signature = secp256k1.sign(
@@ -38,18 +52,3 @@ async def sign_tx(ctx: wire.Context, msg: EosSignTx, keychain: Keychain) -> EosS
     )
 
     return EosSignedTx(signature=base58_encode("SIG_", "K1", signature))
-
-
-async def _init(ctx: wire.Context, sha: HashWriter, msg: EosSignTx) -> None:
-    writers.write_bytes_fixed(sha, msg.chain_id, 32)
-    writers.write_header(sha, msg.header)
-    writers.write_uvarint(sha, 0)
-    writers.write_uvarint(sha, msg.num_actions)
-
-    await require_sign_tx(ctx, msg.num_actions)
-
-
-async def _actions(ctx: wire.Context, sha: HashWriter, num_actions: int) -> None:
-    for _ in range(num_actions):
-        action = await ctx.call(EosTxActionRequest(), EosTxActionAck)
-        await process_action(ctx, sha, action)

trezor/trezor-firmware

core/src/apps/eos/actions/__init__.py
@@ -1,119 +1,125 @@
 from typing import TYPE_CHECKING
 
-from trezor.crypto.hashlib import sha256
-from trezor.messages import EosTxActionAck, EosTxActionRequest
-from trezor.utils import HashWriter
-
-from .. import helpers, writers
-from . import layout
-
 if TYPE_CHECKING:
     from trezor import wire
-    from trezor.utils import Writer
+    from trezor.utils import Writer, HashWriter
+    from trezor.messages import EosTxActionAck
 
 
 async def process_action(
     ctx: wire.Context, sha: HashWriter, action: EosTxActionAck
 ) -> None:
+    from .. import helpers, writers
+    from . import layout
+
     name = helpers.eos_name_to_string(action.common.name)
     account = helpers.eos_name_to_string(action.common.account)
 
-    if not check_action(action, name, account):
+    if not _check_action(action, name, account):
         raise ValueError("Invalid action")
 
     w = bytearray()
     if account == "eosio":
         if name == "buyram":
-            assert action.buy_ram is not None  # check_action
+            assert action.buy_ram is not None  # _check_action
             await layout.confirm_action_buyram(ctx, action.buy_ram)
             writers.write_action_buyram(w, action.buy_ram)
         elif name == "buyrambytes":
-            assert action.buy_ram_bytes is not None  # check_action
+            assert action.buy_ram_bytes is not None  # _check_action
             await layout.confirm_action_buyrambytes(ctx, action.buy_ram_bytes)
             writers.write_action_buyrambytes(w, action.buy_ram_bytes)
         elif name == "sellram":
-            assert action.sell_ram is not None  # check_action
+            assert action.sell_ram is not None  # _check_action
             await layout.confirm_action_sellram(ctx, action.sell_ram)
             writers.write_action_sellram(w, action.sell_ram)
         elif name == "delegatebw":
-            assert action.delegate is not None  # check_action
+            assert action.delegate is not None  # _check_action
             await layout.confirm_action_delegate(ctx, action.delegate)
             writers.write_action_delegate(w, action.delegate)
         elif name == "undelegatebw":
-            assert action.undelegate is not None  # check_action
+            assert action.undelegate is not None  # _check_action
             await layout.confirm_action_undelegate(ctx, action.undelegate)
             writers.write_action_undelegate(w, action.undelegate)
         elif name == "refund":
-            assert action.refund is not None  # check_action
+            assert action.refund is not None  # _check_action
             await layout.confirm_action_refund(ctx, action.refund)
             writers.write_action_refund(w, action.refund)
         elif name == "voteproducer":
-            assert action.vote_producer is not None  # check_action
+            assert action.vote_producer is not None  # _check_action
             await layout.confirm_action_voteproducer(ctx, action.vote_producer)
             writers.write_action_voteproducer(w, action.vote_producer)
         elif name == "updateauth":
-            assert action.update_auth is not None  # check_action
+            assert action.update_auth is not None  # _check_action
             await layout.confirm_action_updateauth(ctx, action.update_auth)
             writers.write_action_updateauth(w, action.update_auth)
         elif name == "deleteauth":
-            assert action.delete_auth is not None  # check_action
+            assert action.delete_auth is not None  # _check_action
             await layout.confirm_action_deleteauth(ctx, action.delete_auth)
             writers.write_action_deleteauth(w, action.delete_auth)
         elif name == "linkauth":
-            assert action.link_auth is not None  # check_action
+            assert action.link_auth is not None  # _check_action
             await layout.confirm_action_linkauth(ctx, action.link_auth)
             writers.write_action_linkauth(w, action.link_auth)
         elif name == "unlinkauth":
-            assert action.unlink_auth is not None  # check_action
+            assert action.unlink_auth is not None  # _check_action
             await layout.confirm_action_unlinkauth(ctx, action.unlink_auth)
             writers.write_action_unlinkauth(w, action.unlink_auth)
         elif name == "newaccount":
-            assert action.new_account is not None  # check_action
+            assert action.new_account is not None  # _check_action
             await layout.confirm_action_newaccount(ctx, action.new_account)
             writers.write_action_newaccount(w, action.new_account)
         else:
             raise ValueError("Unrecognized action type for eosio")
     elif name == "transfer":
-        assert action.transfer is not None  # check_action
+        assert action.transfer is not None  # _check_action
         await layout.confirm_action_transfer(ctx, action.transfer, account)
         writers.write_action_transfer(w, action.transfer)
     else:
-        await process_unknown_action(ctx, w, action)
+        await _process_unknown_action(ctx, w, action)
 
     writers.write_action_common(sha, action.common)
     writers.write_bytes_prefixed(sha, w)
 
 
-async def process_unknown_action(
+async def _process_unknown_action(
     ctx: wire.Context, w: Writer, action: EosTxActionAck
 ) -> None:
-    assert action.unknown is not None
-    checksum = HashWriter(sha256())
-    writers.write_uvarint(checksum, action.unknown.data_size)
-    checksum.extend(action.unknown.data_chunk)
+    from trezor.crypto.hashlib import sha256
+    from trezor.utils import HashWriter
+    from trezor.messages import EosTxActionAck, EosTxActionRequest
+    from .. import writers
+    from . import layout
 
-    writers.write_bytes_unchecked(w, action.unknown.data_chunk)
-    bytes_left = action.unknown.data_size - len(action.unknown.data_chunk)
+    unknown = action.unknown  # local_cache_attribute
+    assert unknown is not None
+    data_chunk = unknown.data_chunk  # local_cache_attribute
+
+    checksum = HashWriter(sha256())
+    writers.write_uvarint(checksum, unknown.data_size)
+    checksum.extend(data_chunk)
+
+    writers.write_bytes_unchecked(w, data_chunk)
+    bytes_left = unknown.data_size - len(data_chunk)
 
     while bytes_left != 0:
         action = await ctx.call(
             EosTxActionRequest(data_size=bytes_left), EosTxActionAck
         )
 
-        if action.unknown is None:
+        if unknown is None:
             raise ValueError("Bad response. Unknown struct expected.")
 
-        checksum.extend(action.unknown.data_chunk)
-        writers.write_bytes_unchecked(w, action.unknown.data_chunk)
+        checksum.extend(data_chunk)
+        writers.write_bytes_unchecked(w, data_chunk)
 
-        bytes_left -= len(action.unknown.data_chunk)
+        bytes_left -= len(data_chunk)
         if bytes_left < 0:
             raise ValueError("Bad response. Buffer overflow.")
 
     await layout.confirm_action_unknown(ctx, action.common, checksum.get_digest())
 
 
-def check_action(action: EosTxActionAck, name: str, account: str) -> bool:
+def _check_action(action: EosTxActionAck, name: str, account: str) -> bool:
     if account == "eosio":
         return (
             (name == "buyram" and action.buy_ram is not None)

core/src/apps/eos/sign_tx.py
@@ -1,36 +1,50 @@
 from typing import TYPE_CHECKING
 
-from trezor import wire
-from trezor.crypto.curve import secp256k1
-from trezor.crypto.hashlib import sha256
-from trezor.messages import EosSignedTx, EosTxActionAck, EosTxActionRequest
-from trezor.utils import HashWriter
-
-from apps.common import paths
-from apps.common.keychain import Keychain, auto_keychain
-
-from . import writers
-from .actions import process_action
-from .helpers import base58_encode
-from .layout import require_sign_tx
+from apps.common.keychain import auto_keychain
 
 if TYPE_CHECKING:
-    from trezor.messages import EosSignTx
+    from trezor.wire import Context
+    from trezor.messages import EosSignTx, EosSignedTx
+    from apps.common.keychain import Keychain
 
 
 @auto_keychain(__name__)
-async def sign_tx(ctx: wire.Context, msg: EosSignTx, keychain: Keychain) -> EosSignedTx:
-    if not msg.num_actions:
-        raise wire.DataError("No actions")
+async def sign_tx(ctx: Context, msg: EosSignTx, keychain: Keychain) -> EosSignedTx:
+    from trezor.wire import DataError
+    from trezor.crypto.curve import secp256k1
+    from trezor.crypto.hashlib import sha256
+    from trezor.messages import EosSignedTx, EosTxActionAck, EosTxActionRequest
+    from trezor.utils import HashWriter
+    from apps.common import paths
+    from .writers import write_uvarint, write_header, write_bytes_fixed
+    from .actions import process_action
+    from .helpers import base58_encode
+    from .layout import require_sign_tx
+
+    num_actions = msg.num_actions  # local_cache_attribute
+
+    if not num_actions:
+        raise DataError("No actions")
 
     await paths.validate_path(ctx, keychain, msg.address_n)
 
     node = keychain.derive(msg.address_n)
     sha = HashWriter(sha256())
-    await _init(ctx, sha, msg)
-    await _actions(ctx, sha, msg.num_actions)
-    writers.write_uvarint(sha, 0)
-    writers.write_bytes_fixed(sha, bytearray(32), 32)
+
+    # init
+    write_bytes_fixed(sha, msg.chain_id, 32)
+    write_header(sha, msg.header)
+    write_uvarint(sha, 0)
+    write_uvarint(sha, num_actions)
+    await require_sign_tx(ctx, num_actions)
+
+    # actions
+    for _ in range(num_actions):
+        action = await ctx.call(EosTxActionRequest(), EosTxActionAck)
+        await process_action(ctx, sha, action)
+
+    write_uvarint(sha, 0)
+    write_bytes_fixed(sha, bytearray(32), 32)
 
     digest = sha.get_digest()
     signature = secp256k1.sign(
@@ -38,18 +52,3 @@ async def sign_tx(ctx: wire.Context, msg: EosSignTx, keychain: Keychain) -> EosS
     )
 
     return EosSignedTx(signature=base58_encode("SIG_", "K1", signature))
-
-
-async def _init(ctx: wire.Context, sha: HashWriter, msg: EosSignTx) -> None:
-    writers.write_bytes_fixed(sha, msg.chain_id, 32)
-    writers.write_header(sha, msg.header)
-    writers.write_uvarint(sha, 0)
-    writers.write_uvarint(sha, msg.num_actions)
-
-    await require_sign_tx(ctx, msg.num_actions)
-
-
-async def _actions(ctx: wire.Context, sha: HashWriter, num_actions: int) -> None:
-    for _ in range(num_actions):
-        action = await ctx.call(EosTxActionRequest(), EosTxActionAck)
-        await process_action(ctx, sha, action)