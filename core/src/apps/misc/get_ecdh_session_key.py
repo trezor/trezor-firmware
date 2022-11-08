jia58628/trezor-trezor-firmware

chore(core): decrease misc size by 430 bytes

core/src/apps/misc/get_ecdh_session_key.py
@@ -1,85 +1,70 @@
 from typing import TYPE_CHECKING
-from ustruct import pack, unpack
-
-from trezor import ui, wire
-from trezor.crypto.hashlib import sha256
-from trezor.messages import ECDHSessionKey
-from trezor.ui.layouts import confirm_address
-
-from apps.common.keychain import get_keychain
-from apps.common.paths import HARDENED, AlwaysMatchingSchema
-
-from .sign_identity import serialize_identity, serialize_identity_without_proto
 
 if TYPE_CHECKING:
-    from trezor.messages import GetECDHSessionKey, IdentityType
-
-    from apps.common.paths import Bip32Path
+    from trezor.messages import GetECDHSessionKey, ECDHSessionKey
+    from trezor.wire import Context
 
 # This module implements the SLIP-0017 Elliptic Curve Diffie-Hellman algorithm, using a
 # deterministic hierarchy, see https://github.com/satoshilabs/slips/blob/master/slip-0017.md.
 
 
-async def get_ecdh_session_key(
-    ctx: wire.Context, msg: GetECDHSessionKey
-) -> ECDHSessionKey:
-    if msg.ecdsa_curve_name is None:
-        msg.ecdsa_curve_name = "secp256k1"
+async def get_ecdh_session_key(ctx: Context, msg: GetECDHSessionKey) -> ECDHSessionKey:
+    from ustruct import pack, unpack
+    from trezor.crypto.hashlib import sha256
+    from trezor.ui.layouts import confirm_address
+    from .sign_identity import serialize_identity_without_proto, serialize_identity
+    from trezor import ui
+    from trezor.wire import DataError
+    from trezor.messages import ECDHSessionKey
+    from apps.common.keychain import get_keychain
+    from apps.common.paths import HARDENED, AlwaysMatchingSchema
 
-    keychain = await get_keychain(ctx, msg.ecdsa_curve_name, [AlwaysMatchingSchema])
-    identity = serialize_identity(msg.identity)
+    msg_ecdsa_curve_name = msg.ecdsa_curve_name or "secp256k1"  # cache
+    msg_identity = msg.identity  # cache
+    msg_peer_public_key = msg.peer_public_key  # cache
 
-    await require_confirm_ecdh_session_key(ctx, msg.identity)
+    keychain = await get_keychain(ctx, msg_ecdsa_curve_name, [AlwaysMatchingSchema])
+    identity = serialize_identity(msg_identity)
 
-    address_n = get_ecdh_path(identity, msg.identity.index or 0)
-    node = keychain.derive(address_n)
-
-    session_key = ecdh(
-        seckey=node.private_key(),
-        peer_public_key=msg.peer_public_key,
-        curve=msg.ecdsa_curve_name,
-    )
-    return ECDHSessionKey(session_key=session_key, public_key=node.public_key())
-
-
-async def require_confirm_ecdh_session_key(
-    ctx: wire.Context, identity: IdentityType
-) -> None:
-    proto = identity.proto.upper() if identity.proto else "identity"
+    # require_confirm_ecdh_session_key
+    proto = msg_identity.proto.upper() if msg_identity.proto else "identity"
     await confirm_address(
         ctx,
         f"Decrypt {proto}",
-        serialize_identity_without_proto(identity),
-        description=None,
+        serialize_identity_without_proto(msg_identity),
+        None,
         icon=ui.ICON_DEFAULT,
         icon_color=ui.ORANGE_ICON,
     )
+    # END require_confirm_ecdh_session_key
 
-
-def get_ecdh_path(identity: str, index: int) -> Bip32Path:
+    # get_ecdh_path
+    index = msg.identity.index or 0
     identity_hash = sha256(pack("<I", index) + identity.encode()).digest()
-
     address_n = [HARDENED | x for x in (17,) + unpack("<IIII", identity_hash[:16])]
+    # END get_ecdh_path
 
-    return address_n
+    node = keychain.derive(address_n)
 
-
-def ecdh(seckey: bytes, peer_public_key: bytes, curve: str) -> bytes:
-    if curve == "secp256k1":
+    # ecdh
+    if msg_ecdsa_curve_name == "secp256k1":
         from trezor.crypto.curve import secp256k1
 
-        session_key = secp256k1.multiply(seckey, peer_public_key)
-    elif curve == "nist256p1":
+        session_key = secp256k1.multiply(node.private_key(), msg_peer_public_key)
+    elif msg_ecdsa_curve_name == "nist256p1":
         from trezor.crypto.curve import nist256p1
 
-        session_key = nist256p1.multiply(seckey, peer_public_key)
-    elif curve == "curve25519":
+        session_key = nist256p1.multiply(node.private_key(), msg_peer_public_key)
+    elif msg_ecdsa_curve_name == "curve25519":
         from trezor.crypto.curve import curve25519
 
-        if peer_public_key[0] != 0x40:
-            raise wire.DataError("Curve25519 public key should start with 0x40")
-        session_key = b"\x04" + curve25519.multiply(seckey, peer_public_key[1:])
+        if msg_peer_public_key[0] != 0x40:
+            raise DataError("Curve25519 public key should start with 0x40")
+        session_key = b"\x04" + curve25519.multiply(
+            node.private_key(), msg_peer_public_key[1:]
+        )
     else:
-        raise wire.DataError("Unsupported curve for ECDH: " + curve)
+        raise DataError("Unsupported curve for ECDH: " + msg_ecdsa_curve_name)
+    # END ecdh
 
-    return session_key
+    return ECDHSessionKey(session_key=session_key, public_key=node.public_key())

core/src/apps/misc/sign_identity.py
@@ -1,38 +1,50 @@
 from typing import TYPE_CHECKING
-from ustruct import pack, unpack
 
-from trezor import wire
 from trezor.crypto.hashlib import sha256
-from trezor.messages import SignedIdentity
-from trezor.ui.layouts import confirm_sign_identity
 
 from apps.common import coininfo
-from apps.common.keychain import get_keychain
-from apps.common.paths import HARDENED, AlwaysMatchingSchema
 
 if TYPE_CHECKING:
-    from trezor.messages import IdentityType, SignIdentity
-
-    from apps.common.paths import Bip32Path
+    from trezor.messages import IdentityType, SignIdentity, SignedIdentity
+    from trezor.wire import Context
 
 # This module implements the SLIP-0013 authentication using a deterministic hierarchy, see
 # https://github.com/satoshilabs/slips/blob/master/slip-0013.md.
 
 
-async def sign_identity(ctx: wire.Context, msg: SignIdentity) -> SignedIdentity:
-    if msg.ecdsa_curve_name is None:
-        msg.ecdsa_curve_name = "secp256k1"
+async def sign_identity(ctx: Context, msg: SignIdentity) -> SignedIdentity:
+    from ustruct import pack, unpack
+    from trezor.messages import SignedIdentity
+    from trezor.ui.layouts import confirm_sign_identity
+    from apps.common.keychain import get_keychain
+    from apps.common.paths import HARDENED, AlwaysMatchingSchema
 
-    keychain = await get_keychain(ctx, msg.ecdsa_curve_name, [AlwaysMatchingSchema])
-    identity = serialize_identity(msg.identity)
+    msg_ecdsa_curve_name = msg.ecdsa_curve_name or "secp256k1"  # cache
+    msg_identity = msg.identity  # cache
+    msg_identity_proto = msg_identity.proto  # cache
+    msg_challenge_visual = msg.challenge_visual  # cache
+    msg_challenge_hidden = msg.challenge_hidden  # cache
 
-    await require_confirm_sign_identity(ctx, msg.identity, msg.challenge_visual)
+    keychain = await get_keychain(ctx, msg_ecdsa_curve_name, [AlwaysMatchingSchema])
+    identity = serialize_identity(msg_identity)
 
-    address_n = get_identity_path(identity, msg.identity.index or 0)
+    # require_confirm_sign_identity
+    proto = msg_identity_proto.upper() if msg_identity_proto else "identity"
+    await confirm_sign_identity(
+        ctx, proto, serialize_identity_without_proto(msg_identity), msg_challenge_visual
+    )
+    # END require_confirm_sign_identity
+
+    # get_identity_path
+    index = msg_identity.index or 0
+    identity_hash = sha256(pack("<I", index) + identity.encode()).digest()
+    address_n = [HARDENED | x for x in (13,) + unpack("<IIII", identity_hash[:16])]
+    # END get_identity_path
+
     node = keychain.derive(address_n)
 
     coin = coininfo.by_name("Bitcoin")
-    if msg.ecdsa_curve_name == "secp256k1":
+    if msg_ecdsa_curve_name == "secp256k1":
         # hardcoded bitcoin address type
         address: str | None = node.address(coin.address_type)
     else:
@@ -42,49 +54,40 @@ async def sign_identity(ctx: wire.Context, msg: SignIdentity) -> SignedIdentity:
         pubkey = b"\x00" + pubkey[1:]
     seckey = node.private_key()
 
-    if msg.identity.proto == "gpg":
+    if msg_identity_proto == "gpg":
         signature = sign_challenge(
             seckey,
-            msg.challenge_hidden,
-            msg.challenge_visual,
+            msg_challenge_hidden,
+            msg_challenge_visual,
             "gpg",
-            msg.ecdsa_curve_name,
+            msg_ecdsa_curve_name,
         )
-    elif msg.identity.proto == "signify":
+    elif msg_identity_proto == "signify":
         signature = sign_challenge(
             seckey,
-            msg.challenge_hidden,
-            msg.challenge_visual,
+            msg_challenge_hidden,
+            msg_challenge_visual,
             "signify",
-            msg.ecdsa_curve_name,
+            msg_ecdsa_curve_name,
         )
-    elif msg.identity.proto == "ssh":
+    elif msg_identity_proto == "ssh":
         signature = sign_challenge(
             seckey,
-            msg.challenge_hidden,
-            msg.challenge_visual,
+            msg_challenge_hidden,
+            msg_challenge_visual,
             "ssh",
-            msg.ecdsa_curve_name,
+            msg_ecdsa_curve_name,
         )
     else:
         signature = sign_challenge(
             seckey,
-            msg.challenge_hidden,
-            msg.challenge_visual,
+            msg_challenge_hidden,
+            msg_challenge_visual,
             coin,
-            msg.ecdsa_curve_name,
+            msg_ecdsa_curve_name,
         )
 
     return SignedIdentity(address=address, public_key=pubkey, signature=signature)
-
-
-async def require_confirm_sign_identity(
-    ctx: wire.Context, identity: IdentityType, challenge_visual: str | None
-) -> None:
-    proto = identity.proto.upper() if identity.proto else "identity"
-    await confirm_sign_identity(
-        ctx, proto, serialize_identity_without_proto(identity), challenge_visual
-    )
 
 
 def serialize_identity(identity: IdentityType) -> str:
@@ -110,14 +113,6 @@ def serialize_identity_without_proto(identity: IdentityType) -> str:
     return s
 
 
-def get_identity_path(identity: str, index: int) -> Bip32Path:
-    identity_hash = sha256(pack("<I", index) + identity.encode()).digest()
-
-    address_n = [HARDENED | x for x in (13,) + unpack("<IIII", identity_hash[:16])]
-
-    return address_n
-
-
 def sign_challenge(
     seckey: bytes,
     challenge_hidden: bytes,
@@ -126,12 +121,13 @@ def sign_challenge(
     curve: str,
 ) -> bytes:
     from apps.common.signverify import message_digest
+    from trezor.wire import DataError
 
     if sigtype == "gpg":
         data = challenge_hidden
     elif sigtype == "signify":
         if curve != "ed25519":
-            raise wire.DataError("Unsupported curve")
+            raise DataError("Unsupported curve")
         data = challenge_hidden
     elif sigtype == "ssh":
         if curve != "ed25519":
@@ -146,7 +142,7 @@ def sign_challenge(
         )
         data = message_digest(sigtype, challenge)
     else:
-        raise wire.DataError("Unsupported sigtype")
+        raise DataError("Unsupported sigtype")
 
     if curve == "secp256k1":
         from trezor.crypto.curve import secp256k1
@@ -161,7 +157,7 @@ def sign_challenge(
 
         signature = ed25519.sign(seckey, data)
     else:
-        raise wire.DataError("Unknown curve")
+        raise DataError("Unknown curve")
 
     if curve == "ed25519":
         signature = b"\x00" + signature

trezor/trezor-firmware

core/src/apps/misc/get_ecdh_session_key.py
@@ -1,85 +1,67 @@
 from typing import TYPE_CHECKING
-from ustruct import pack, unpack
-
-from trezor import ui, wire
-from trezor.crypto.hashlib import sha256
-from trezor.messages import ECDHSessionKey
-from trezor.ui.layouts import confirm_address
-
-from apps.common.keychain import get_keychain
-from apps.common.paths import HARDENED, AlwaysMatchingSchema
-
-from .sign_identity import serialize_identity, serialize_identity_without_proto
 
 if TYPE_CHECKING:
-    from trezor.messages import GetECDHSessionKey, IdentityType
-
-    from apps.common.paths import Bip32Path
+    from trezor.messages import GetECDHSessionKey, ECDHSessionKey
+    from trezor.wire import Context
 
 # This module implements the SLIP-0017 Elliptic Curve Diffie-Hellman algorithm, using a
 # deterministic hierarchy, see https://github.com/satoshilabs/slips/blob/master/slip-0017.md.
 
 
-async def get_ecdh_session_key(
-    ctx: wire.Context, msg: GetECDHSessionKey
-) -> ECDHSessionKey:
-    if msg.ecdsa_curve_name is None:
-        msg.ecdsa_curve_name = "secp256k1"
-
-    keychain = await get_keychain(ctx, msg.ecdsa_curve_name, [AlwaysMatchingSchema])
-    identity = serialize_identity(msg.identity)
-
-    await require_confirm_ecdh_session_key(ctx, msg.identity)
-
-    address_n = get_ecdh_path(identity, msg.identity.index or 0)
-    node = keychain.derive(address_n)
-
-    session_key = ecdh(
-        seckey=node.private_key(),
-        peer_public_key=msg.peer_public_key,
-        curve=msg.ecdsa_curve_name,
+async def get_ecdh_session_key(ctx: Context, msg: GetECDHSessionKey) -> ECDHSessionKey:
+    from trezor.ui.layouts import confirm_address
+    from .sign_identity import (
+        get_identity_path,
+        serialize_identity_without_proto,
+        serialize_identity,
     )
-    return ECDHSessionKey(session_key=session_key, public_key=node.public_key())
+    from trezor import ui
+    from trezor.wire import DataError
+    from trezor.messages import ECDHSessionKey
+    from apps.common.keychain import get_keychain
+    from apps.common.paths import AlwaysMatchingSchema
 
+    msg_identity = msg.identity  # local_cache_attribute
+    peer_public_key = msg.peer_public_key  # local_cache_attribute
+    curve_name = msg.ecdsa_curve_name or "secp256k1"
 
-async def require_confirm_ecdh_session_key(
-    ctx: wire.Context, identity: IdentityType
-) -> None:
-    proto = identity.proto.upper() if identity.proto else "identity"
+    keychain = await get_keychain(ctx, curve_name, [AlwaysMatchingSchema])
+    identity = serialize_identity(msg_identity)
+
+    # require_confirm_ecdh_session_key
+    proto = msg_identity.proto.upper() if msg_identity.proto else "identity"
     await confirm_address(
         ctx,
         f"Decrypt {proto}",
-        serialize_identity_without_proto(identity),
-        description=None,
+        serialize_identity_without_proto(msg_identity),
+        None,
         icon=ui.ICON_DEFAULT,
         icon_color=ui.ORANGE_ICON,
     )
+    # END require_confirm_ecdh_session_key
 
+    address_n = get_identity_path(identity, msg_identity.index or 0, 17)
+    node = keychain.derive(address_n)
 
-def get_ecdh_path(identity: str, index: int) -> Bip32Path:
-    identity_hash = sha256(pack("<I", index) + identity.encode()).digest()
-
-    address_n = [HARDENED | x for x in (17,) + unpack("<IIII", identity_hash[:16])]
-
-    return address_n
-
-
-def ecdh(seckey: bytes, peer_public_key: bytes, curve: str) -> bytes:
-    if curve == "secp256k1":
+    # ecdh
+    if curve_name == "secp256k1":
         from trezor.crypto.curve import secp256k1
 
-        session_key = secp256k1.multiply(seckey, peer_public_key)
-    elif curve == "nist256p1":
+        session_key = secp256k1.multiply(node.private_key(), peer_public_key)
+    elif curve_name == "nist256p1":
         from trezor.crypto.curve import nist256p1
 
-        session_key = nist256p1.multiply(seckey, peer_public_key)
-    elif curve == "curve25519":
+        session_key = nist256p1.multiply(node.private_key(), peer_public_key)
+    elif curve_name == "curve25519":
         from trezor.crypto.curve import curve25519
 
         if peer_public_key[0] != 0x40:
-            raise wire.DataError("Curve25519 public key should start with 0x40")
-        session_key = b"\x04" + curve25519.multiply(seckey, peer_public_key[1:])
+            raise DataError("Curve25519 public key should start with 0x40")
+        session_key = b"\x04" + curve25519.multiply(
+            node.private_key(), peer_public_key[1:]
+        )
     else:
-        raise wire.DataError("Unsupported curve for ECDH: " + curve)
+        raise DataError("Unsupported curve for ECDH: " + curve_name)
+    # END ecdh
 
-    return session_key
+    return ECDHSessionKey(session_key=session_key, public_key=node.public_key())

core/src/apps/misc/sign_identity.py
@@ -1,38 +1,45 @@
 from typing import TYPE_CHECKING
-from ustruct import pack, unpack
 
-from trezor import wire
 from trezor.crypto.hashlib import sha256
-from trezor.messages import SignedIdentity
-from trezor.ui.layouts import confirm_sign_identity
 
 from apps.common import coininfo
-from apps.common.keychain import get_keychain
-from apps.common.paths import HARDENED, AlwaysMatchingSchema
 
 if TYPE_CHECKING:
-    from trezor.messages import IdentityType, SignIdentity
-
+    from trezor.messages import IdentityType, SignIdentity, SignedIdentity
+    from trezor.wire import Context
     from apps.common.paths import Bip32Path
 
 # This module implements the SLIP-0013 authentication using a deterministic hierarchy, see
 # https://github.com/satoshilabs/slips/blob/master/slip-0013.md.
 
 
-async def sign_identity(ctx: wire.Context, msg: SignIdentity) -> SignedIdentity:
-    if msg.ecdsa_curve_name is None:
-        msg.ecdsa_curve_name = "secp256k1"
+async def sign_identity(ctx: Context, msg: SignIdentity) -> SignedIdentity:
+    from trezor.messages import SignedIdentity
+    from trezor.ui.layouts import confirm_sign_identity
+    from apps.common.keychain import get_keychain
+    from apps.common.paths import AlwaysMatchingSchema
 
-    keychain = await get_keychain(ctx, msg.ecdsa_curve_name, [AlwaysMatchingSchema])
-    identity = serialize_identity(msg.identity)
+    msg_identity = msg.identity  # local_cache_attribute
+    msg_identity_proto = msg_identity.proto  # local_cache_attribute
+    challenge_visual = msg.challenge_visual  # local_cache_attribute
+    challenge_hidden = msg.challenge_hidden  # local_cache_attribute
+    curve_name = msg.ecdsa_curve_name or "secp256k1"
 
-    await require_confirm_sign_identity(ctx, msg.identity, msg.challenge_visual)
+    keychain = await get_keychain(ctx, curve_name, [AlwaysMatchingSchema])
+    identity = serialize_identity(msg_identity)
 
-    address_n = get_identity_path(identity, msg.identity.index or 0)
+    # require_confirm_sign_identity
+    proto = msg_identity_proto.upper() if msg_identity_proto else "identity"
+    await confirm_sign_identity(
+        ctx, proto, serialize_identity_without_proto(msg_identity), challenge_visual
+    )
+    # END require_confirm_sign_identity
+
+    address_n = get_identity_path(identity, msg_identity.index or 0, 13)
     node = keychain.derive(address_n)
 
     coin = coininfo.by_name("Bitcoin")
-    if msg.ecdsa_curve_name == "secp256k1":
+    if curve_name == "secp256k1":
         # hardcoded bitcoin address type
         address: str | None = node.address(coin.address_type)
     else:
@@ -42,49 +49,20 @@ async def sign_identity(ctx: wire.Context, msg: SignIdentity) -> SignedIdentity:
         pubkey = b"\x00" + pubkey[1:]
     seckey = node.private_key()
 
-    if msg.identity.proto == "gpg":
-        signature = sign_challenge(
-            seckey,
-            msg.challenge_hidden,
-            msg.challenge_visual,
-            "gpg",
-            msg.ecdsa_curve_name,
-        )
-    elif msg.identity.proto == "signify":
-        signature = sign_challenge(
-            seckey,
-            msg.challenge_hidden,
-            msg.challenge_visual,
-            "signify",
-            msg.ecdsa_curve_name,
-        )
-    elif msg.identity.proto == "ssh":
-        signature = sign_challenge(
-            seckey,
-            msg.challenge_hidden,
-            msg.challenge_visual,
-            "ssh",
-            msg.ecdsa_curve_name,
-        )
+    if msg_identity_proto in ("gpg", "signify", "ssh"):
+        sigtype = msg_identity_proto
     else:
-        signature = sign_challenge(
-            seckey,
-            msg.challenge_hidden,
-            msg.challenge_visual,
-            coin,
-            msg.ecdsa_curve_name,
-        )
+        sigtype = coin
+
+    signature = sign_challenge(
+        seckey,
+        challenge_hidden,
+        challenge_visual,
+        sigtype,
+        curve_name,
+    )
 
     return SignedIdentity(address=address, public_key=pubkey, signature=signature)
-
-
-async def require_confirm_sign_identity(
-    ctx: wire.Context, identity: IdentityType, challenge_visual: str | None
-) -> None:
-    proto = identity.proto.upper() if identity.proto else "identity"
-    await confirm_sign_identity(
-        ctx, proto, serialize_identity_without_proto(identity), challenge_visual
-    )
 
 
 def serialize_identity(identity: IdentityType) -> str:
@@ -110,12 +88,13 @@ def serialize_identity_without_proto(identity: IdentityType) -> str:
     return s
 
 
-def get_identity_path(identity: str, index: int) -> Bip32Path:
+def get_identity_path(identity: str, index: int, num: int) -> Bip32Path:
+    from ustruct import pack, unpack
+    from apps.common.paths import HARDENED
+
     identity_hash = sha256(pack("<I", index) + identity.encode()).digest()
 
-    address_n = [HARDENED | x for x in (13,) + unpack("<IIII", identity_hash[:16])]
-
-    return address_n
+    return [HARDENED | x for x in (num,) + unpack("<IIII", identity_hash[:16])]
 
 
 def sign_challenge(
@@ -126,12 +105,13 @@ def sign_challenge(
     curve: str,
 ) -> bytes:
     from apps.common.signverify import message_digest
+    from trezor.wire import DataError
 
     if sigtype == "gpg":
         data = challenge_hidden
     elif sigtype == "signify":
         if curve != "ed25519":
-            raise wire.DataError("Unsupported curve")
+            raise DataError("Unsupported curve")
         data = challenge_hidden
     elif sigtype == "ssh":
         if curve != "ed25519":
@@ -146,7 +126,7 @@ def sign_challenge(
         )
         data = message_digest(sigtype, challenge)
     else:
-        raise wire.DataError("Unsupported sigtype")
+        raise DataError("Unsupported sigtype")
 
     if curve == "secp256k1":
         from trezor.crypto.curve import secp256k1
@@ -161,7 +141,7 @@ def sign_challenge(
 
         signature = ed25519.sign(seckey, data)
     else:
-        raise wire.DataError("Unknown curve")
+        raise DataError("Unknown curve")
 
     if curve == "ed25519":
         signature = b"\x00" + signature