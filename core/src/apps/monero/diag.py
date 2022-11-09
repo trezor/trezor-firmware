jia58628/trezor-trezor-firmware

chore(core): decrease monero size by 2630 bytes

core/src/apps/monero/diag.py
@@ -45,28 +45,32 @@ if __debug__:
         return Failure(**kwargs)
 
     async def diag(ctx, msg, **kwargs) -> Failure:
-        log.debug(__name__, "----diagnostics")
+        msg_ins = msg.ins  # cache
+        mem_check = check_mem  # cache
+        log_debug = log.debug  # cache
+
+        log_debug(__name__, "----diagnostics")
         gc.collect()
 
-        if msg.ins == 0:
-            check_mem(0)
+        if msg_ins == 0:
+            mem_check(0)
             return retit()
 
-        elif msg.ins == 1:
-            check_mem(1)
+        elif msg_ins == 1:
+            mem_check(1)
             micropython.mem_info(1)
             return retit()
 
-        elif msg.ins == 2:
-            log.debug(__name__, "_____________________________________________")
-            log.debug(__name__, "_____________________________________________")
-            log.debug(__name__, "_____________________________________________")
+        elif msg_ins == 2:
+            log_debug(__name__, "_____________________________________________")
+            log_debug(__name__, "_____________________________________________")
+            log_debug(__name__, "_____________________________________________")
             return retit()
 
-        elif msg.ins == 3:
+        elif msg_ins == 3:
             pass
 
-        elif msg.ins == 4:
+        elif msg_ins == 4:
             total = 0
             monero = 0
 
@@ -78,14 +82,14 @@ if __debug__:
             log.info(__name__, "Total modules: %s, Monero modules: %s", total, monero)
             return retit()
 
-        elif msg.ins in [5, 6, 7]:
-            check_mem()
+        elif msg_ins in [5, 6, 7]:
+            mem_check()
             from apps.monero.xmr import bulletproof as bp
 
-            check_mem("BP Imported")
+            mem_check("BP Imported")
             from apps.monero.xmr import crypto
 
-            check_mem("Crypto Imported")
+            mem_check("Crypto Imported")
 
             bpi = bp.BulletProofBuilder()
             bpi.gc_fnc = gc.collect
@@ -93,25 +97,25 @@ if __debug__:
 
             vals = [crypto.Scalar((1 << 30) - 1 + 16), crypto.Scalar(22222)]
             masks = [crypto.random_scalar(), crypto.random_scalar()]
-            check_mem("BP pre input")
+            mem_check("BP pre input")
 
-            if msg.ins == 5:
+            if msg_ins == 5:
                 bp_res = bpi.prove_testnet(vals[0], masks[0])
-                check_mem("BP post prove")
+                mem_check("BP post prove")
                 bpi.verify_testnet(bp_res)
-                check_mem("BP post verify")
+                mem_check("BP post verify")
 
-            elif msg.ins == 6:
+            elif msg_ins == 6:
                 bp_res = bpi.prove(vals[0], masks[0])
-                check_mem("BP post prove")
+                mem_check("BP post prove")
                 bpi.verify(bp_res)
-                check_mem("BP post verify")
+                mem_check("BP post verify")
 
-            elif msg.ins == 7:
+            elif msg_ins == 7:
                 bp_res = bpi.prove_batch(vals, masks)
-                check_mem("BP post prove")
+                mem_check("BP post prove")
                 bpi.verify(bp_res)
-                check_mem("BP post verify")
+                mem_check("BP post verify")
 
             return retit()
 

core/src/apps/monero/sign_tx.py
@@ -1,8 +1,4 @@
-import gc
 from typing import TYPE_CHECKING
-
-from trezor import log, utils, wire
-from trezor.enums import MessageType
 
 from apps.common.keychain import auto_keychain
 from apps.monero.signing.state import State
@@ -10,12 +6,16 @@ from apps.monero.signing.state import State
 if TYPE_CHECKING:
     from trezor.messages import MoneroTransactionFinalAck
     from apps.common.keychain import Keychain
+    from trezor.wire import Context
 
 
 @auto_keychain(__name__)
 async def sign_tx(
-    ctx: wire.Context, received_msg, keychain: Keychain
+    ctx: Context, received_msg, keychain: Keychain
 ) -> MoneroTransactionFinalAck:
+    import gc
+    from trezor import log, utils
+
     state = State(ctx)
     mods = utils.unimport_begin()
 
@@ -27,7 +27,7 @@ async def sign_tx(
         gc.collect()
         gc.threshold(gc.mem_free() // 4 + gc.mem_alloc())
 
-        result_msg, accept_msgs = await sign_tx_dispatch(state, received_msg, keychain)
+        result_msg, accept_msgs = await _sign_tx_dispatch(state, received_msg, keychain)
         if accept_msgs is None:
             break
 
@@ -41,8 +41,13 @@ async def sign_tx(
     return result_msg
 
 
-async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
-    if msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionInitRequest:
+async def _sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
+    from trezor.enums import MessageType
+    from trezor import wire
+
+    MESSAGE_WIRE_TYPE = msg.MESSAGE_WIRE_TYPE  # cache
+
+    if MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionInitRequest:
         from apps.monero.signing import step_01_init_transaction
 
         return (
@@ -52,7 +57,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             (MessageType.MoneroTransactionSetInputRequest,),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionSetInputRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionSetInputRequest:
         from apps.monero.signing import step_02_set_input
 
         return (
@@ -63,7 +68,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             ),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionInputViniRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionInputViniRequest:
         from apps.monero.signing import step_04_input_vini
 
         return (
@@ -76,7 +81,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             ),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionAllInputsSetRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionAllInputsSetRequest:
         from apps.monero.signing import step_05_all_inputs_set
 
         return (
@@ -84,7 +89,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             (MessageType.MoneroTransactionSetOutputRequest,),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionSetOutputRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionSetOutputRequest:
         from apps.monero.signing import step_06_set_output
 
         is_offloaded_bp = bool(msg.is_offloaded_bp)
@@ -101,7 +106,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             ),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionAllOutSetRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionAllOutSetRequest:
         from apps.monero.signing import step_07_all_outputs_set
 
         return (
@@ -109,7 +114,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             (MessageType.MoneroTransactionSignInputRequest,),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionSignInputRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionSignInputRequest:
         from apps.monero.signing import step_09_sign_input
 
         return (
@@ -130,7 +135,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             ),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionFinalRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionFinalRequest:
         from apps.monero.signing import step_10_sign_final
 
         return step_10_sign_final.final_msg(state), None

core/src/apps/monero/xmr/mlsag_hasher.py
@@ -1,8 +1,5 @@
 from typing import TYPE_CHECKING
 
-from apps.monero.xmr import crypto_helpers
-from apps.monero.xmr.keccak_hasher import KeccakXmrArchive
-
 if TYPE_CHECKING:
     from trezor.utils import HashContext
     from .serialize_messages.tx_rsig_bulletproof import Bulletproof, BulletproofPlus
@@ -14,6 +11,9 @@ class PreMlsagHasher:
     """
 
     def __init__(self) -> None:
+        from apps.monero.xmr import crypto_helpers
+        from apps.monero.xmr.keccak_hasher import KeccakXmrArchive
+
         self.state = 0
         self.kc_master: HashContext = crypto_helpers.get_keccak()
         self.rsig_hasher: HashContext = crypto_helpers.get_keccak()
@@ -62,6 +62,8 @@ class PreMlsagHasher:
         if self.state == 8:
             raise ValueError("State error")
 
+        self_rsig_hasher_update = self.rsig_hasher.update  # cache
+
         if raw:
             # Avoiding problem with the memory fragmentation.
             # If the range proof is passed as a list, hash each element
@@ -69,10 +71,10 @@ class PreMlsagHasher:
             # preserving the byte ordering
             if isinstance(p, list):
                 for x in p:
-                    self.rsig_hasher.update(x)
+                    self_rsig_hasher_update(x)
             else:
                 assert isinstance(p, bytes)
-                self.rsig_hasher.update(p)
+                self_rsig_hasher_update(p)
             return
 
         from .serialize_messages.tx_rsig_bulletproof import Bulletproof, BulletproofPlus
@@ -87,18 +89,18 @@ class PreMlsagHasher:
             else (p.A, p.S, p.T1, p.T2, p.taux, p.mu)
         )
         for fld in fields:
-            self.rsig_hasher.update(fld)
+            self_rsig_hasher_update(fld)
 
         del (fields,)
         for i in range(len(p.L)):
-            self.rsig_hasher.update(p.L[i])
+            self_rsig_hasher_update(p.L[i])
         for i in range(len(p.R)):
-            self.rsig_hasher.update(p.R[i])
+            self_rsig_hasher_update(p.R[i])
 
         if not is_plus:
-            self.rsig_hasher.update(p.a)
-            self.rsig_hasher.update(p.b)
-            self.rsig_hasher.update(p.t)
+            self_rsig_hasher_update(p.a)
+            self_rsig_hasher_update(p.b)
+            self_rsig_hasher_update(p.t)
 
     def get_digest(self) -> bytes:
         if self.state != 6:

trezor/trezor-firmware

core/src/apps/monero/diag.py
@@ -45,28 +45,31 @@ if __debug__:
         return Failure(**kwargs)
 
     async def diag(ctx, msg, **kwargs) -> Failure:
-        log.debug(__name__, "----diagnostics")
+        ins = msg.ins  # local_cache_attribute
+        debug = log.debug  # local_cache_attribute
+
+        debug(__name__, "----diagnostics")
         gc.collect()
 
-        if msg.ins == 0:
+        if ins == 0:
             check_mem(0)
             return retit()
 
-        elif msg.ins == 1:
+        elif ins == 1:
             check_mem(1)
             micropython.mem_info(1)
             return retit()
 
-        elif msg.ins == 2:
-            log.debug(__name__, "_____________________________________________")
-            log.debug(__name__, "_____________________________________________")
-            log.debug(__name__, "_____________________________________________")
+        elif ins == 2:
+            debug(__name__, "_____________________________________________")
+            debug(__name__, "_____________________________________________")
+            debug(__name__, "_____________________________________________")
             return retit()
 
-        elif msg.ins == 3:
+        elif ins == 3:
             pass
 
-        elif msg.ins == 4:
+        elif ins == 4:
             total = 0
             monero = 0
 
@@ -78,7 +81,7 @@ if __debug__:
             log.info(__name__, "Total modules: %s, Monero modules: %s", total, monero)
             return retit()
 
-        elif msg.ins in [5, 6, 7]:
+        elif ins in [5, 6, 7]:
             check_mem()
             from apps.monero.xmr import bulletproof as bp
 
@@ -95,19 +98,19 @@ if __debug__:
             masks = [crypto.random_scalar(), crypto.random_scalar()]
             check_mem("BP pre input")
 
-            if msg.ins == 5:
+            if ins == 5:
                 bp_res = bpi.prove_testnet(vals[0], masks[0])
                 check_mem("BP post prove")
                 bpi.verify_testnet(bp_res)
                 check_mem("BP post verify")
 
-            elif msg.ins == 6:
+            elif ins == 6:
                 bp_res = bpi.prove(vals[0], masks[0])
                 check_mem("BP post prove")
                 bpi.verify(bp_res)
                 check_mem("BP post verify")
 
-            elif msg.ins == 7:
+            elif ins == 7:
                 bp_res = bpi.prove_batch(vals, masks)
                 check_mem("BP post prove")
                 bpi.verify(bp_res)

core/src/apps/monero/sign_tx.py
@@ -1,21 +1,22 @@
-import gc
 from typing import TYPE_CHECKING
 
-from trezor import log, utils, wire
-from trezor.enums import MessageType
-
 from apps.common.keychain import auto_keychain
-from apps.monero.signing.state import State
 
 if TYPE_CHECKING:
     from trezor.messages import MoneroTransactionFinalAck
     from apps.common.keychain import Keychain
+    from apps.monero.signing.state import State
+    from trezor.wire import Context
 
 
 @auto_keychain(__name__)
 async def sign_tx(
-    ctx: wire.Context, received_msg, keychain: Keychain
+    ctx: Context, received_msg, keychain: Keychain
 ) -> MoneroTransactionFinalAck:
+    import gc
+    from trezor import log, utils
+    from apps.monero.signing.state import State
+
     state = State(ctx)
     mods = utils.unimport_begin()
 
@@ -27,7 +28,7 @@ async def sign_tx(
         gc.collect()
         gc.threshold(gc.mem_free() // 4 + gc.mem_alloc())
 
-        result_msg, accept_msgs = await sign_tx_dispatch(state, received_msg, keychain)
+        result_msg, accept_msgs = await _sign_tx_dispatch(state, received_msg, keychain)
         if accept_msgs is None:
             break
 
@@ -41,8 +42,13 @@ async def sign_tx(
     return result_msg
 
 
-async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
-    if msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionInitRequest:
+async def _sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
+    from trezor.enums import MessageType
+    from trezor import wire
+
+    MESSAGE_WIRE_TYPE = msg.MESSAGE_WIRE_TYPE  # local_cache_attribute
+
+    if MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionInitRequest:
         from apps.monero.signing import step_01_init_transaction
 
         return (
@@ -52,7 +58,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             (MessageType.MoneroTransactionSetInputRequest,),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionSetInputRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionSetInputRequest:
         from apps.monero.signing import step_02_set_input
 
         return (
@@ -63,7 +69,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             ),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionInputViniRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionInputViniRequest:
         from apps.monero.signing import step_04_input_vini
 
         return (
@@ -76,7 +82,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             ),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionAllInputsSetRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionAllInputsSetRequest:
         from apps.monero.signing import step_05_all_inputs_set
 
         return (
@@ -84,7 +90,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             (MessageType.MoneroTransactionSetOutputRequest,),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionSetOutputRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionSetOutputRequest:
         from apps.monero.signing import step_06_set_output
 
         is_offloaded_bp = bool(msg.is_offloaded_bp)
@@ -101,7 +107,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             ),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionAllOutSetRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionAllOutSetRequest:
         from apps.monero.signing import step_07_all_outputs_set
 
         return (
@@ -109,7 +115,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             (MessageType.MoneroTransactionSignInputRequest,),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionSignInputRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionSignInputRequest:
         from apps.monero.signing import step_09_sign_input
 
         return (
@@ -130,7 +136,7 @@ async def sign_tx_dispatch(state: State, msg, keychain: Keychain) -> tuple:
             ),
         )
 
-    elif msg.MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionFinalRequest:
+    elif MESSAGE_WIRE_TYPE == MessageType.MoneroTransactionFinalRequest:
         from apps.monero.signing import step_10_sign_final
 
         return step_10_sign_final.final_msg(state), None

core/src/apps/monero/xmr/mlsag_hasher.py
@@ -1,8 +1,5 @@
 from typing import TYPE_CHECKING
 
-from apps.monero.xmr import crypto_helpers
-from apps.monero.xmr.keccak_hasher import KeccakXmrArchive
-
 if TYPE_CHECKING:
     from trezor.utils import HashContext
     from .serialize_messages.tx_rsig_bulletproof import BulletproofPlus
@@ -14,6 +11,9 @@ class PreMlsagHasher:
     """
 
     def __init__(self) -> None:
+        from apps.monero.xmr import crypto_helpers
+        from apps.monero.xmr.keccak_hasher import KeccakXmrArchive
+
         self.state = 0
         self.kc_master: HashContext = crypto_helpers.get_keccak()
         self.rsig_hasher: HashContext = crypto_helpers.get_keccak()
@@ -61,6 +61,7 @@ class PreMlsagHasher:
     ) -> None:
         if self.state == 8:
             raise ValueError("State error")
+        update = self.rsig_hasher.update  # local_cache_attribute
 
         if raw:
             # Avoiding problem with the memory fragmentation.
@@ -69,22 +70,22 @@ class PreMlsagHasher:
             # preserving the byte ordering
             if isinstance(p, list):
                 for x in p:
-                    self.rsig_hasher.update(x)
+                    update(x)
             else:
                 assert isinstance(p, bytes)
-                self.rsig_hasher.update(p)
+                update(p)
             return
 
         # Hash Bulletproof
         fields = (p.A, p.A1, p.B, p.r1, p.s1, p.d1)
         for fld in fields:
-            self.rsig_hasher.update(fld)
+            update(fld)
 
         del (fields,)
         for i in range(len(p.L)):
-            self.rsig_hasher.update(p.L[i])
+            update(p.L[i])
         for i in range(len(p.R)):
-            self.rsig_hasher.update(p.R[i])
+            update(p.R[i])
 
     def get_digest(self) -> bytes:
         if self.state != 6: