jia58628/trezor-trezor-firmware

chore(core): decrease common size by 5200 bytes

core/src/apps/common/passphrase.py
@@ -1,66 +1,73 @@
 from micropython import const
+from typing import TYPE_CHECKING
 
-import storage.device
-from trezor import wire, workflow
+import storage.device as storage_device
+from trezor.wire import DataError
+
+if TYPE_CHECKING:
+    from trezor.wire import Context
 
 _MAX_PASSPHRASE_LEN = const(50)
 
 
 def is_enabled() -> bool:
-    return storage.device.is_passphrase_enabled()
+    return storage_device.is_passphrase_enabled()
 
 
-async def get(ctx: wire.Context) -> str:
-    if is_enabled():
-        return await _request_from_user(ctx)
-    else:
+async def get(ctx: Context) -> str:
+    from trezor import workflow
+
+    if not is_enabled():
         return ""
-
-
-async def _request_from_user(ctx: wire.Context) -> str:
-    workflow.close_others()  # request exclusive UI access
-    if storage.device.get_passphrase_always_on_device():
-        from trezor.ui.layouts import request_passphrase_on_device
-
-        passphrase = await request_passphrase_on_device(ctx, _MAX_PASSPHRASE_LEN)
     else:
-        passphrase = await _request_on_host(ctx)
-    if len(passphrase.encode()) > _MAX_PASSPHRASE_LEN:
-        raise wire.DataError(
-            f"Maximum passphrase length is {_MAX_PASSPHRASE_LEN} bytes"
-        )
+        workflow.close_others()  # request exclusive UI access
+        if storage_device.get_passphrase_always_on_device():
+            from trezor.ui.layouts import request_passphrase_on_device
 
-    return passphrase
+            passphrase = await request_passphrase_on_device(ctx, _MAX_PASSPHRASE_LEN)
+        else:
+            passphrase = await _request_on_host(ctx)
+        if len(passphrase.encode()) > _MAX_PASSPHRASE_LEN:
+            raise DataError(f"Maximum passphrase length is {_MAX_PASSPHRASE_LEN} bytes")
+
+        return passphrase
 
 
-async def _request_on_host(ctx: wire.Context) -> str:
+async def _request_on_host(ctx: Context) -> str:
     from trezor.messages import PassphraseAck, PassphraseRequest
+    from trezor.ui.layouts import draw_simple_text
 
-    _entry_dialog()
+    # _entry_dialog
+    draw_simple_text(
+        "Passphrase entry", "Please type your\npassphrase on the\nconnected host."
+    )
 
     request = PassphraseRequest()
     ack = await ctx.call(request, PassphraseAck)
+
+    ack_passphrase = ack.passphrase  # cache
+
     if ack.on_device:
         from trezor.ui.layouts import request_passphrase_on_device
 
-        if ack.passphrase is not None:
-            raise wire.DataError("Passphrase provided when it should not be")
+        if ack_passphrase is not None:
+            raise DataError("Passphrase provided when it should not be")
         return await request_passphrase_on_device(ctx, _MAX_PASSPHRASE_LEN)
 
-    if ack.passphrase is None:
-        raise wire.DataError(
+    if ack_passphrase is None:
+        raise DataError(
             "Passphrase not provided and on_device is False. Use empty string to set an empty passphrase."
         )
 
     # non-empty passphrase
-    if ack.passphrase:
+    if ack_passphrase:
         from trezor import ui
         from trezor.ui.layouts import confirm_action, confirm_blob
 
         await confirm_action(
             ctx,
             "passphrase_host1",
-            title="Hidden wallet",
+            "Hidden wallet",
             description="Access hidden wallet?\n\nNext screen will show\nthe passphrase!",
             icon=ui.ICON_CONFIG,
         )
@@ -68,19 +75,11 @@ async def _request_on_host(ctx: wire.Context) -> str:
         await confirm_blob(
             ctx,
             "passphrase_host2",
-            title="Hidden wallet",
-            description="Use this passphrase?\n",
-            data=ack.passphrase,
+            "Hidden wallet",
+            ack_passphrase,
+            "Use this passphrase?\n",
             icon=ui.ICON_CONFIG,
             icon_color=ui.ORANGE_ICON,
         )
 
-    return ack.passphrase
-
-
-def _entry_dialog() -> None:
-    from trezor.ui.layouts import draw_simple_text
-
-    draw_simple_text(
-        "Passphrase entry", "Please type your\npassphrase on the\nconnected host."
-    )
+    return ack_passphrase

core/src/apps/common/readers.py
@@ -1,27 +1,32 @@
-from trezor.utils import BufferReader
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from trezor.utils import BufferReader
 
 
 def read_compact_size(r: BufferReader) -> int:
-    prefix = r.get()
+    r_get = r.get  # cache
+
+    prefix = r_get()
     if prefix < 253:
         n = prefix
     elif prefix == 253:
-        n = r.get()
-        n += r.get() << 8
+        n = r_get()
+        n += r_get() << 8
     elif prefix == 254:
-        n = r.get()
-        n += r.get() << 8
-        n += r.get() << 16
-        n += r.get() << 24
+        n = r_get()
+        n += r_get() << 8
+        n += r_get() << 16
+        n += r_get() << 24
     elif prefix == 255:
-        n = r.get()
-        n += r.get() << 8
-        n += r.get() << 16
-        n += r.get() << 24
-        n += r.get() << 32
-        n += r.get() << 40
-        n += r.get() << 48
-        n += r.get() << 56
+        n = r_get()
+        n += r_get() << 8
+        n += r_get() << 16
+        n += r_get() << 24
+        n += r_get() << 32
+        n += r_get() << 40
+        n += r_get() << 48
+        n += r_get() << 56
     else:
         raise ValueError
     return n

trezor/trezor-firmware

core/src/apps/common/passphrase.py
@@ -1,66 +1,72 @@
 from micropython import const
+from typing import TYPE_CHECKING
 
-import storage.device
-from trezor import wire, workflow
+import storage.device as storage_device
+from trezor.wire import DataError
+
+if TYPE_CHECKING:
+    from trezor.wire import Context
 
 _MAX_PASSPHRASE_LEN = const(50)
 
 
 def is_enabled() -> bool:
-    return storage.device.is_passphrase_enabled()
+    return storage_device.is_passphrase_enabled()
 
 
-async def get(ctx: wire.Context) -> str:
-    if is_enabled():
-        return await _request_from_user(ctx)
-    else:
+async def get(ctx: Context) -> str:
+    from trezor import workflow
+
+    if not is_enabled():
         return ""
-
-
-async def _request_from_user(ctx: wire.Context) -> str:
-    workflow.close_others()  # request exclusive UI access
-    if storage.device.get_passphrase_always_on_device():
-        from trezor.ui.layouts import request_passphrase_on_device
-
-        passphrase = await request_passphrase_on_device(ctx, _MAX_PASSPHRASE_LEN)
     else:
-        passphrase = await _request_on_host(ctx)
-    if len(passphrase.encode()) > _MAX_PASSPHRASE_LEN:
-        raise wire.DataError(
-            f"Maximum passphrase length is {_MAX_PASSPHRASE_LEN} bytes"
-        )
+        workflow.close_others()  # request exclusive UI access
+        if storage_device.get_passphrase_always_on_device():
+            from trezor.ui.layouts import request_passphrase_on_device
 
-    return passphrase
+            passphrase = await request_passphrase_on_device(ctx, _MAX_PASSPHRASE_LEN)
+        else:
+            passphrase = await _request_on_host(ctx)
+        if len(passphrase.encode()) > _MAX_PASSPHRASE_LEN:
+            raise DataError(f"Maximum passphrase length is {_MAX_PASSPHRASE_LEN} bytes")
+
+        return passphrase
 
 
-async def _request_on_host(ctx: wire.Context) -> str:
+async def _request_on_host(ctx: Context) -> str:
     from trezor.messages import PassphraseAck, PassphraseRequest
+    from trezor.ui.layouts import draw_simple_text
 
-    _entry_dialog()
+    # _entry_dialog
+    draw_simple_text(
+        "Passphrase entry", "Please type your\npassphrase on the\nconnected host."
+    )
 
     request = PassphraseRequest()
     ack = await ctx.call(request, PassphraseAck)
+    passphrase = ack.passphrase  # local_cache_attribute
+
     if ack.on_device:
         from trezor.ui.layouts import request_passphrase_on_device
 
-        if ack.passphrase is not None:
-            raise wire.DataError("Passphrase provided when it should not be")
+        if passphrase is not None:
+            raise DataError("Passphrase provided when it should not be")
         return await request_passphrase_on_device(ctx, _MAX_PASSPHRASE_LEN)
 
-    if ack.passphrase is None:
-        raise wire.DataError(
+    if passphrase is None:
+        raise DataError(
             "Passphrase not provided and on_device is False. Use empty string to set an empty passphrase."
         )
 
     # non-empty passphrase
-    if ack.passphrase:
+    if passphrase:
         from trezor import ui
         from trezor.ui.layouts import confirm_action, confirm_blob
 
         await confirm_action(
             ctx,
             "passphrase_host1",
-            title="Hidden wallet",
+            "Hidden wallet",
             description="Access hidden wallet?\n\nNext screen will show\nthe passphrase!",
             icon=ui.ICON_CONFIG,
         )
@@ -68,19 +74,11 @@ async def _request_on_host(ctx: wire.Context) -> str:
         await confirm_blob(
             ctx,
             "passphrase_host2",
-            title="Hidden wallet",
-            description="Use this passphrase?\n",
-            data=ack.passphrase,
+            "Hidden wallet",
+            passphrase,
+            "Use this passphrase?\n",
             icon=ui.ICON_CONFIG,
             icon_color=ui.ORANGE_ICON,
         )
 
-    return ack.passphrase
-
-
-def _entry_dialog() -> None:
-    from trezor.ui.layouts import draw_simple_text
-
-    draw_simple_text(
-        "Passphrase entry", "Please type your\npassphrase on the\nconnected host."
-    )
+    return passphrase

core/src/apps/common/readers.py
@@ -1,27 +1,32 @@
-from trezor.utils import BufferReader
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from trezor.utils import BufferReader
 
 
 def read_compact_size(r: BufferReader) -> int:
-    prefix = r.get()
+    get = r.get  # local_cache_attribute
+
+    prefix = get()
     if prefix < 253:
         n = prefix
     elif prefix == 253:
-        n = r.get()
-        n += r.get() << 8
+        n = get()
+        n += get() << 8
     elif prefix == 254:
-        n = r.get()
-        n += r.get() << 8
-        n += r.get() << 16
-        n += r.get() << 24
+        n = get()
+        n += get() << 8
+        n += get() << 16
+        n += get() << 24
     elif prefix == 255:
-        n = r.get()
-        n += r.get() << 8
-        n += r.get() << 16
-        n += r.get() << 24
-        n += r.get() << 32
-        n += r.get() << 40
-        n += r.get() << 48
-        n += r.get() << 56
+        n = get()
+        n += get() << 8
+        n += get() << 16
+        n += get() << 24
+        n += get() << 32
+        n += get() << 40
+        n += get() << 48
+        n += get() << 56
     else:
         raise ValueError
     return n