jia58628/trezor-trezor-firmware

chore(core): decrease cardano size by 2290 bytes

core/src/apps/cardano/get_public_key.py
@@ -1,29 +1,34 @@
+from typing import TYPE_CHECKING
 from ubinascii import hexlify
 
-from trezor import log, messages, wire
-from trezor.ui.layouts import show_pubkey
-
-from apps.common import paths
-
 from . import seed
-from .helpers.paths import SCHEMA_MINT, SCHEMA_PUBKEY
-from .helpers.utils import derive_public_key
+
+if TYPE_CHECKING:
+    from trezor.wire import Context
+    from trezor.messages import CardanoGetPublicKey, CardanoPublicKey
 
 
 @seed.with_keychain
 async def get_public_key(
-    ctx: wire.Context, msg: messages.CardanoGetPublicKey, keychain: seed.Keychain
-) -> messages.CardanoPublicKey:
+    ctx: Context, msg: CardanoGetPublicKey, keychain: seed.Keychain
+) -> CardanoPublicKey:
+    from trezor import log, wire
+    from trezor.ui.layouts import show_pubkey
+    from apps.common import paths
+    from .helpers.paths import SCHEMA_MINT, SCHEMA_PUBKEY
+
+    address_n = msg.address_n  # cache
+
     await paths.validate_path(
         ctx,
         keychain,
-        msg.address_n,
+        address_n,
         # path must match the PUBKEY schema
-        SCHEMA_PUBKEY.match(msg.address_n) or SCHEMA_MINT.match(msg.address_n),
+        SCHEMA_PUBKEY.match(address_n) or SCHEMA_MINT.match(address_n),
     )
 
     try:
-        key = _get_public_key(keychain, msg.address_n)
+        key = _get_public_key(keychain, address_n)
     except ValueError as e:
         if __debug__:
             log.exception(__name__, e)
@@ -36,14 +41,17 @@ async def get_public_key(
 
 def _get_public_key(
     keychain: seed.Keychain, derivation_path: list[int]
-) -> messages.CardanoPublicKey:
+) -> CardanoPublicKey:
+    from .helpers.utils import derive_public_key
+    from trezor.messages import HDNodeType, CardanoPublicKey
+
     node = keychain.derive(derivation_path)
 
     public_key = hexlify(derive_public_key(keychain, derivation_path)).decode()
     chain_code = hexlify(node.chain_code()).decode()
     xpub_key = public_key + chain_code
 
-    node_type = messages.HDNodeType(
+    node_type = HDNodeType(
         depth=node.depth(),
         child_num=node.child_num(),
         fingerprint=node.fingerprint(),
@@ -51,4 +59,4 @@ def _get_public_key(
         public_key=derive_public_key(keychain, derivation_path),
     )
 
-    return messages.CardanoPublicKey(node=node_type, xpub=xpub_key)
+    return CardanoPublicKey(node=node_type, xpub=xpub_key)

core/src/apps/cardano/sign_tx/ordinary_signer.py
@@ -1,14 +1,13 @@
-from trezor import messages, wire
-from trezor.enums import CardanoCertificateType
+from typing import TYPE_CHECKING
 
-from .. import layout, seed
-from ..helpers.paths import (
-    SCHEMA_MINT,
-    SCHEMA_PAYMENT,
-    SCHEMA_STAKING,
-    WITNESS_PATH_NAME,
-)
+from trezor.wire import ProcessError
+
+from .. import layout
+from ..helpers.paths import SCHEMA_MINT
 from .signer import Signer
+
+if TYPE_CHECKING:
+    from trezor import messages
 
 
 class OrdinarySigner(Signer):
@@ -20,42 +19,51 @@ class OrdinarySigner(Signer):
     SIGNING_MODE_TITLE = "Confirming a transaction."
 
     def _validate_tx_init(self) -> None:
+        self_msg = self.msg  # cache
+        self_assert = self._assert_tx_init_cond  # cache
+
         super()._validate_tx_init()
-        self._assert_tx_init_cond(self.msg.collateral_inputs_count == 0)
-        self._assert_tx_init_cond(not self.msg.has_collateral_return)
-        self._assert_tx_init_cond(self.msg.total_collateral is None)
-        self._assert_tx_init_cond(self.msg.reference_inputs_count == 0)
+        self_assert(self_msg.collateral_inputs_count == 0)
+        self_assert(not self_msg.has_collateral_return)
+        self_assert(self_msg.total_collateral is None)
+        self_assert(self_msg.reference_inputs_count == 0)
 
     async def _confirm_tx(self, tx_hash: bytes) -> None:
+        self_msg = self.msg  # cache
+
         # super() omitted intentionally
         is_network_id_verifiable = self._is_network_id_verifiable()
         await layout.confirm_tx(
             self.ctx,
-            self.msg.fee,
-            self.msg.network_id,
-            self.msg.protocol_magic,
-            self.msg.ttl,
-            self.msg.validity_interval_start,
-            self.msg.total_collateral,
+            self_msg.fee,
+            self_msg.network_id,
+            self_msg.protocol_magic,
+            self_msg.ttl,
+            self_msg.validity_interval_start,
+            self_msg.total_collateral,
             is_network_id_verifiable,
             tx_hash=None,
         )
 
     def _validate_certificate(self, certificate: messages.CardanoTxCertificate) -> None:
+        from trezor.enums import CardanoCertificateType
+
         super()._validate_certificate(certificate)
         if certificate.type == CardanoCertificateType.STAKE_POOL_REGISTRATION:
-            raise wire.ProcessError("Invalid certificate")
+            raise ProcessError("Invalid certificate")
         if certificate.script_hash or certificate.key_hash:
-            raise wire.ProcessError("Invalid certificate")
+            raise ProcessError("Invalid certificate")
 
     def _validate_withdrawal(self, withdrawal: messages.CardanoTxWithdrawal) -> None:
         super()._validate_withdrawal(withdrawal)
         if withdrawal.script_hash or withdrawal.key_hash:
-            raise wire.ProcessError("Invalid withdrawal")
+            raise ProcessError("Invalid withdrawal")
 
     def _validate_witness_request(
         self, witness_request: messages.CardanoTxWitnessRequest
     ) -> None:
+        from .. import seed
+
         super()._validate_witness_request(witness_request)
         is_minting = SCHEMA_MINT.match(witness_request.path)
         tx_has_token_minting = self.msg.minting_asset_groups_count > 0
@@ -65,9 +73,15 @@ class OrdinarySigner(Signer):
             or seed.is_shelley_path(witness_request.path)
             or (is_minting and tx_has_token_minting)
         ):
-            raise wire.ProcessError("Invalid witness request")
+            raise ProcessError("Invalid witness request")
 
     async def _show_witness_request(self, witness_path: list[int]) -> None:
+        from ..helpers.paths import (
+            SCHEMA_PAYMENT,
+            SCHEMA_STAKING,
+            WITNESS_PATH_NAME,
+        )
+
         # super() omitted intentionally
         # We only allow payment, staking or minting paths.
         # If the path is an unusual payment or staking path, we either fail or show the

core/src/apps/cardano/sign_tx/pool_owner_signer.py
@@ -1,9 +1,11 @@
-from trezor import messages, wire
-from trezor.enums import CardanoCertificateType
+from typing import TYPE_CHECKING
 
-from .. import layout
-from ..helpers.paths import SCHEMA_STAKING_ANY_ACCOUNT
+from trezor.wire import ProcessError
+
 from .signer import Signer
+
+if TYPE_CHECKING:
+    from trezor import messages
 
 
 class PoolOwnerSigner(Signer):
@@ -22,18 +24,25 @@ class PoolOwnerSigner(Signer):
     SIGNING_MODE_TITLE = "Confirming pool registration as owner."
 
     def _validate_tx_init(self) -> None:
+        self_msg = self.msg  # cache
+
         super()._validate_tx_init()
-        self._assert_tx_init_cond(self.msg.certificates_count == 1)
-        self._assert_tx_init_cond(self.msg.withdrawals_count == 0)
-        self._assert_tx_init_cond(self.msg.minting_asset_groups_count == 0)
-        self._assert_tx_init_cond(self.msg.script_data_hash is None)
-        self._assert_tx_init_cond(self.msg.collateral_inputs_count == 0)
-        self._assert_tx_init_cond(self.msg.required_signers_count == 0)
-        self._assert_tx_init_cond(not self.msg.has_collateral_return)
-        self._assert_tx_init_cond(self.msg.total_collateral is None)
-        self._assert_tx_init_cond(self.msg.reference_inputs_count == 0)
+        for condition in (
+            self_msg.certificates_count == 1,
+            self_msg.withdrawals_count == 0,
+            self_msg.minting_asset_groups_count == 0,
+            self_msg.script_data_hash is None,
+            self_msg.collateral_inputs_count == 0,
+            self_msg.required_signers_count == 0,
+            not self_msg.has_collateral_return,
+            self_msg.total_collateral is None,
+            self_msg.reference_inputs_count == 0,
+        ):
+            self._assert_tx_init_cond(condition)
 
     async def _confirm_tx(self, tx_hash: bytes) -> None:
+        from .. import layout
+
         # super() omitted intentionally
         await layout.confirm_stake_pool_registration_final(
             self.ctx,
@@ -50,7 +59,7 @@ class PoolOwnerSigner(Signer):
             or output.inline_datum_size > 0
             or output.reference_script_size > 0
         ):
-            raise wire.ProcessError("Invalid output")
+            raise ProcessError("Invalid output")
 
     def _should_show_output(self, output: messages.CardanoTxOutput) -> bool:
         # super() omitted intentionally
@@ -58,16 +67,20 @@ class PoolOwnerSigner(Signer):
         return False
 
     def _validate_certificate(self, certificate: messages.CardanoTxCertificate) -> None:
+        from trezor.enums import CardanoCertificateType
+
         super()._validate_certificate(certificate)
         if certificate.type != CardanoCertificateType.STAKE_POOL_REGISTRATION:
-            raise wire.ProcessError("Invalid certificate")
+            raise ProcessError("Invalid certificate")
 
     def _validate_witness_request(
         self, witness_request: messages.CardanoTxWitnessRequest
     ) -> None:
+        from ..helpers.paths import SCHEMA_STAKING_ANY_ACCOUNT
+
         super()._validate_witness_request(witness_request)
         if not SCHEMA_STAKING_ANY_ACCOUNT.match(witness_request.path):
-            raise wire.ProcessError(
+            raise ProcessError(
                 "Stakepool registration transaction can only contain staking witnesses"
             )
 

trezor/trezor-firmware

core/src/apps/cardano/get_public_key.py
@@ -1,29 +1,34 @@
+from typing import TYPE_CHECKING
 from ubinascii import hexlify
 
-from trezor import log, messages, wire
-from trezor.ui.layouts import show_pubkey
-
-from apps.common import paths
-
 from . import seed
-from .helpers.paths import SCHEMA_MINT, SCHEMA_PUBKEY
-from .helpers.utils import derive_public_key
+
+if TYPE_CHECKING:
+    from trezor.wire import Context
+    from trezor.messages import CardanoGetPublicKey, CardanoPublicKey
 
 
 @seed.with_keychain
 async def get_public_key(
-    ctx: wire.Context, msg: messages.CardanoGetPublicKey, keychain: seed.Keychain
-) -> messages.CardanoPublicKey:
+    ctx: Context, msg: CardanoGetPublicKey, keychain: seed.Keychain
+) -> CardanoPublicKey:
+    from trezor import log, wire
+    from trezor.ui.layouts import show_pubkey
+    from apps.common import paths
+    from .helpers.paths import SCHEMA_MINT, SCHEMA_PUBKEY
+
+    address_n = msg.address_n  # local_cache_attribute
+
     await paths.validate_path(
         ctx,
         keychain,
-        msg.address_n,
+        address_n,
         # path must match the PUBKEY schema
-        SCHEMA_PUBKEY.match(msg.address_n) or SCHEMA_MINT.match(msg.address_n),
+        SCHEMA_PUBKEY.match(address_n) or SCHEMA_MINT.match(address_n),
     )
 
     try:
-        key = _get_public_key(keychain, msg.address_n)
+        key = _get_public_key(keychain, address_n)
     except ValueError as e:
         if __debug__:
             log.exception(__name__, e)
@@ -36,14 +41,17 @@ async def get_public_key(
 
 def _get_public_key(
     keychain: seed.Keychain, derivation_path: list[int]
-) -> messages.CardanoPublicKey:
+) -> CardanoPublicKey:
+    from .helpers.utils import derive_public_key
+    from trezor.messages import HDNodeType, CardanoPublicKey
+
     node = keychain.derive(derivation_path)
 
     public_key = hexlify(derive_public_key(keychain, derivation_path)).decode()
     chain_code = hexlify(node.chain_code()).decode()
     xpub_key = public_key + chain_code
 
-    node_type = messages.HDNodeType(
+    node_type = HDNodeType(
         depth=node.depth(),
         child_num=node.child_num(),
         fingerprint=node.fingerprint(),
@@ -51,4 +59,4 @@ def _get_public_key(
         public_key=derive_public_key(keychain, derivation_path),
     )
 
-    return messages.CardanoPublicKey(node=node_type, xpub=xpub_key)
+    return CardanoPublicKey(node=node_type, xpub=xpub_key)

core/src/apps/cardano/sign_tx/ordinary_signer.py
@@ -1,14 +1,13 @@
-from trezor import messages, wire
-from trezor.enums import CardanoCertificateType
+from typing import TYPE_CHECKING
 
-from .. import layout, seed
-from ..helpers.paths import (
-    SCHEMA_MINT,
-    SCHEMA_PAYMENT,
-    SCHEMA_STAKING,
-    WITNESS_PATH_NAME,
-)
+from trezor.wire import ProcessError
+
+from .. import layout
+from ..helpers.paths import SCHEMA_MINT
 from .signer import Signer
+
+if TYPE_CHECKING:
+    from trezor import messages
 
 
 class OrdinarySigner(Signer):
@@ -20,42 +19,51 @@ class OrdinarySigner(Signer):
     SIGNING_MODE_TITLE = "Confirming a transaction."
 
     def _validate_tx_init(self) -> None:
+        msg = self.msg  # local_cache_attribute
+        _assert_tx_init_cond = self._assert_tx_init_cond  # local_cache_attribute
+
         super()._validate_tx_init()
-        self._assert_tx_init_cond(self.msg.collateral_inputs_count == 0)
-        self._assert_tx_init_cond(not self.msg.has_collateral_return)
-        self._assert_tx_init_cond(self.msg.total_collateral is None)
-        self._assert_tx_init_cond(self.msg.reference_inputs_count == 0)
+        _assert_tx_init_cond(msg.collateral_inputs_count == 0)
+        _assert_tx_init_cond(not msg.has_collateral_return)
+        _assert_tx_init_cond(msg.total_collateral is None)
+        _assert_tx_init_cond(msg.reference_inputs_count == 0)
 
     async def _confirm_tx(self, tx_hash: bytes) -> None:
+        msg = self.msg  # local_cache_attribute
+
         # super() omitted intentionally
         is_network_id_verifiable = self._is_network_id_verifiable()
         await layout.confirm_tx(
             self.ctx,
-            self.msg.fee,
-            self.msg.network_id,
-            self.msg.protocol_magic,
-            self.msg.ttl,
-            self.msg.validity_interval_start,
-            self.msg.total_collateral,
+            msg.fee,
+            msg.network_id,
+            msg.protocol_magic,
+            msg.ttl,
+            msg.validity_interval_start,
+            msg.total_collateral,
             is_network_id_verifiable,
             tx_hash=None,
         )
 
     def _validate_certificate(self, certificate: messages.CardanoTxCertificate) -> None:
+        from trezor.enums import CardanoCertificateType
+
         super()._validate_certificate(certificate)
         if certificate.type == CardanoCertificateType.STAKE_POOL_REGISTRATION:
-            raise wire.ProcessError("Invalid certificate")
+            raise ProcessError("Invalid certificate")
         if certificate.script_hash or certificate.key_hash:
-            raise wire.ProcessError("Invalid certificate")
+            raise ProcessError("Invalid certificate")
 
     def _validate_withdrawal(self, withdrawal: messages.CardanoTxWithdrawal) -> None:
         super()._validate_withdrawal(withdrawal)
         if withdrawal.script_hash or withdrawal.key_hash:
-            raise wire.ProcessError("Invalid withdrawal")
+            raise ProcessError("Invalid withdrawal")
 
     def _validate_witness_request(
         self, witness_request: messages.CardanoTxWitnessRequest
     ) -> None:
+        from .. import seed
+
         super()._validate_witness_request(witness_request)
         is_minting = SCHEMA_MINT.match(witness_request.path)
         tx_has_token_minting = self.msg.minting_asset_groups_count > 0
@@ -65,9 +73,15 @@ class OrdinarySigner(Signer):
             or seed.is_shelley_path(witness_request.path)
             or (is_minting and tx_has_token_minting)
         ):
-            raise wire.ProcessError("Invalid witness request")
+            raise ProcessError("Invalid witness request")
 
     async def _show_witness_request(self, witness_path: list[int]) -> None:
+        from ..helpers.paths import (
+            SCHEMA_PAYMENT,
+            SCHEMA_STAKING,
+            WITNESS_PATH_NAME,
+        )
+
         # super() omitted intentionally
         # We only allow payment, staking or minting paths.
         # If the path is an unusual payment or staking path, we either fail or show the

core/src/apps/cardano/sign_tx/pool_owner_signer.py
@@ -1,9 +1,11 @@
-from trezor import messages, wire
-from trezor.enums import CardanoCertificateType
+from typing import TYPE_CHECKING
 
-from .. import layout
-from ..helpers.paths import SCHEMA_STAKING_ANY_ACCOUNT
+from trezor.wire import ProcessError
+
 from .signer import Signer
+
+if TYPE_CHECKING:
+    from trezor import messages
 
 
 class PoolOwnerSigner(Signer):
@@ -22,18 +24,25 @@ class PoolOwnerSigner(Signer):
     SIGNING_MODE_TITLE = "Confirming pool registration as owner."
 
     def _validate_tx_init(self) -> None:
+        msg = self.msg  # local_cache_attribute
+
         super()._validate_tx_init()
-        self._assert_tx_init_cond(self.msg.certificates_count == 1)
-        self._assert_tx_init_cond(self.msg.withdrawals_count == 0)
-        self._assert_tx_init_cond(self.msg.minting_asset_groups_count == 0)
-        self._assert_tx_init_cond(self.msg.script_data_hash is None)
-        self._assert_tx_init_cond(self.msg.collateral_inputs_count == 0)
-        self._assert_tx_init_cond(self.msg.required_signers_count == 0)
-        self._assert_tx_init_cond(not self.msg.has_collateral_return)
-        self._assert_tx_init_cond(self.msg.total_collateral is None)
-        self._assert_tx_init_cond(self.msg.reference_inputs_count == 0)
+        for condition in (
+            msg.certificates_count == 1,
+            msg.withdrawals_count == 0,
+            msg.minting_asset_groups_count == 0,
+            msg.script_data_hash is None,
+            msg.collateral_inputs_count == 0,
+            msg.required_signers_count == 0,
+            not msg.has_collateral_return,
+            msg.total_collateral is None,
+            msg.reference_inputs_count == 0,
+        ):
+            self._assert_tx_init_cond(condition)
 
     async def _confirm_tx(self, tx_hash: bytes) -> None:
+        from .. import layout
+
         # super() omitted intentionally
         await layout.confirm_stake_pool_registration_final(
             self.ctx,
@@ -50,7 +59,7 @@ class PoolOwnerSigner(Signer):
             or output.inline_datum_size > 0
             or output.reference_script_size > 0
         ):
-            raise wire.ProcessError("Invalid output")
+            raise ProcessError("Invalid output")
 
     def _should_show_output(self, output: messages.CardanoTxOutput) -> bool:
         # super() omitted intentionally
@@ -58,16 +67,20 @@ class PoolOwnerSigner(Signer):
         return False
 
     def _validate_certificate(self, certificate: messages.CardanoTxCertificate) -> None:
+        from trezor.enums import CardanoCertificateType
+
         super()._validate_certificate(certificate)
         if certificate.type != CardanoCertificateType.STAKE_POOL_REGISTRATION:
-            raise wire.ProcessError("Invalid certificate")
+            raise ProcessError("Invalid certificate")
 
     def _validate_witness_request(
         self, witness_request: messages.CardanoTxWitnessRequest
     ) -> None:
+        from ..helpers.paths import SCHEMA_STAKING_ANY_ACCOUNT
+
         super()._validate_witness_request(witness_request)
         if not SCHEMA_STAKING_ANY_ACCOUNT.match(witness_request.path):
-            raise wire.ProcessError(
+            raise ProcessError(
                 "Stakepool registration transaction can only contain staking witnesses"
             )
 