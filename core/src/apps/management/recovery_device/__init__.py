jia58628/trezor-trezor-firmware

chore(core): decrease management size by 1440 bytes

core/src/apps/management/recovery_device/__init__.py
@@ -1,24 +1,9 @@
 from typing import TYPE_CHECKING
 
-import storage
-import storage.device
-import storage.recovery
-from trezor import config, ui, wire, workflow
-from trezor.enums import ButtonRequestType
-from trezor.messages import Success
-from trezor.ui.layouts import confirm_action, confirm_reset_device
-
-from apps.common.request_pin import (
-    error_pin_invalid,
-    request_pin_and_sd_salt,
-    request_pin_confirm,
-)
-
-from .homescreen import recovery_homescreen, recovery_process
-
 if TYPE_CHECKING:
     from trezor.messages import RecoveryDevice
-
+    from trezor.wire import Context
+    from trezor.messages import Success
 
 # List of RecoveryDevice fields that can be set when doing dry-run recovery.
 # All except `dry_run` are allowed for T1 compatibility, but their values are ignored.
@@ -26,69 +11,52 @@ if TYPE_CHECKING:
 DRY_RUN_ALLOWED_FIELDS = ("dry_run", "word_count", "enforce_wordlist", "type")
 
 
-async def recovery_device(ctx: wire.Context, msg: RecoveryDevice) -> Success:
+async def recovery_device(ctx: Context, msg: RecoveryDevice) -> Success:
     """
     Recover BIP39/SLIP39 seed into empty device.
     Recovery is also possible with replugged Trezor. We call this process Persistence.
     User starts the process here using the RecoveryDevice msg and then they can unplug
     the device anytime and continue without a computer.
     """
-    _validate(msg)
+    import storage
+    import storage.device as storage_device
+    import storage.recovery as storage_recovery
+    from trezor import config, ui, wire, workflow
+    from trezor.enums import ButtonRequestType
+    from trezor.ui.layouts import confirm_action, confirm_reset_device
+    from apps.common.request_pin import (
+        error_pin_invalid,
+        request_pin_and_sd_salt,
+        request_pin_confirm,
+    )
+    from .homescreen import recovery_homescreen, recovery_process
 
-    if storage.recovery.is_in_progress():
-        return await recovery_process(ctx)
+    msg_dry_run = msg.dry_run  # cache
 
-    await _continue_dialog(ctx, msg)
-
-    if not msg.dry_run:
-        # wipe storage to make sure the device is in a clear state
-        storage.reset()
-
-    # for dry run pin needs to be entered
-    if msg.dry_run:
-        curpin, salt = await request_pin_and_sd_salt(ctx, "Enter PIN")
-        if not config.check_pin(curpin, salt):
-            await error_pin_invalid(ctx)
-
-    if not msg.dry_run:
-        # set up pin if requested
-        if msg.pin_protection:
-            newpin = await request_pin_confirm(ctx, allow_cancel=False)
-            config.change_pin("", newpin, None, None)
-
-        storage.device.set_passphrase_enabled(bool(msg.passphrase_protection))
-        if msg.u2f_counter is not None:
-            storage.device.set_u2f_counter(msg.u2f_counter)
-        if msg.label is not None:
-            storage.device.set_label(msg.label)
-
-    storage.recovery.set_in_progress(True)
-    storage.recovery.set_dry_run(bool(msg.dry_run))
-
-    workflow.set_default(recovery_homescreen)
-    return await recovery_process(ctx)
-
-
-def _validate(msg: RecoveryDevice) -> None:
-    if not msg.dry_run and storage.device.is_initialized():
+    # --------------------------------------------------------
+    # validate
+    if not msg_dry_run and storage_device.is_initialized():
         raise wire.UnexpectedMessage("Already initialized")
-    if msg.dry_run and not storage.device.is_initialized():
+    if msg_dry_run and not storage_device.is_initialized():
         raise wire.NotInitialized("Device is not initialized")
-
     if msg.enforce_wordlist is False:
         raise wire.ProcessError(
             "Value enforce_wordlist must be True, Trezor Core enforces words automatically."
         )
-
-    if msg.dry_run:
+    if msg_dry_run:
         # check that only allowed fields are set
         for key, value in msg.__dict__.items():
             if key not in DRY_RUN_ALLOWED_FIELDS and value is not None:
                 raise wire.ProcessError(f"Forbidden field set in dry-run: {key}")
+    # END validate
+    # --------------------------------------------------------
 
+    if storage_recovery.is_in_progress():
+        return await recovery_process(ctx)
 
-async def _continue_dialog(ctx: wire.Context, msg: RecoveryDevice) -> None:
-    if not msg.dry_run:
+    # --------------------------------------------------------
+    # _continue_dialog
+    if not msg_dry_run:
         await confirm_reset_device(
             ctx, "Do you really want to\nrecover a wallet?", recovery=True
         )
@@ -96,8 +64,38 @@ async def _continue_dialog(ctx: wire.Context, msg: RecoveryDevice) -> None:
         await confirm_action(
             ctx,
             "confirm_seedcheck",
-            title="Seed check",
+            "Seed check",
             description="Do you really want to check the recovery seed?",
             icon=ui.ICON_RECOVERY,
             br_code=ButtonRequestType.ProtectCall,
         )
+    # END _continue_dialog
+    # --------------------------------------------------------
+
+    if not msg_dry_run:
+        # wipe storage to make sure the device is in a clear state
+        storage.reset()
+
+    # for dry run pin needs to be entered
+    if msg_dry_run:
+        curpin, salt = await request_pin_and_sd_salt(ctx, "Enter PIN")
+        if not config.check_pin(curpin, salt):
+            await error_pin_invalid(ctx)
+
+    if not msg_dry_run:
+        # set up pin if requested
+        if msg.pin_protection:
+            newpin = await request_pin_confirm(ctx, allow_cancel=False)
+            config.change_pin("", newpin, None, None)
+
+        storage_device.set_passphrase_enabled(bool(msg.passphrase_protection))
+        if msg.u2f_counter is not None:
+            storage_device.set_u2f_counter(msg.u2f_counter)
+        if msg.label is not None:
+            storage_device.set_label(msg.label)
+
+    storage_recovery.set_in_progress(True)
+    storage_recovery.set_dry_run(bool(msg_dry_run))
+
+    workflow.set_default(recovery_homescreen)
+    return await recovery_process(ctx)

core/src/apps/management/recovery_device/recover.py
@@ -1,11 +1,8 @@
 from typing import TYPE_CHECKING
 
-import storage.recovery
-import storage.recovery_shares
-from trezor.crypto import bip39, slip39
-from trezor.errors import MnemonicError
-
-from .. import backup_types
+import storage.recovery as storage_recovery
+import storage.recovery_shares as storage_recovery_shares
+from trezor.crypto import slip39
 
 if TYPE_CHECKING:
     from trezor.enums import BackupType
@@ -20,6 +17,9 @@ def process_bip39(words: str) -> bytes:
     Receives single mnemonic and processes it. Returns what is then stored
     in the storage, which is the mnemonic itself for BIP-39.
     """
+    from trezor.crypto import bip39
+    from trezor.errors import MnemonicError
+
     if not bip39.check(words):
         raise MnemonicError
     return words.encode()
@@ -32,17 +32,19 @@ def process_slip39(words: str) -> tuple[bytes | None, slip39.Share]:
     """
     share = slip39.decode_mnemonic(words)
 
-    remaining = storage.recovery.fetch_slip39_remaining_shares()
+    recovery_shares = storage_recovery_shares  # cache
+    recovery = storage_recovery  # cache
+    share_group_index = share.group_index  # cache
+
+    remaining = recovery.fetch_slip39_remaining_shares()
 
     # if this is the first share, parse and store metadata
     if not remaining:
-        storage.recovery.set_slip39_group_count(share.group_count)
-        storage.recovery.set_slip39_iteration_exponent(share.iteration_exponent)
-        storage.recovery.set_slip39_identifier(share.identifier)
-        storage.recovery.set_slip39_remaining_shares(
-            share.threshold - 1, share.group_index
-        )
-        storage.recovery_shares.set(share.index, share.group_index, words)
+        recovery.set_slip39_group_count(share.group_count)
+        recovery.set_slip39_iteration_exponent(share.iteration_exponent)
+        recovery.set_slip39_identifier(share.identifier)
+        recovery.set_slip39_remaining_shares(share.threshold - 1, share_group_index)
+        recovery_shares.set(share.index, share_group_index, words)
 
         # if share threshold and group threshold are 1
         # we can calculate the secret right away
@@ -54,24 +56,21 @@ def process_slip39(words: str) -> tuple[bytes | None, slip39.Share]:
             return None, share
 
     # These should be checked by UI before so it's a Runtime exception otherwise
-    if share.identifier != storage.recovery.get_slip39_identifier():
+    if share.identifier != recovery.get_slip39_identifier():
         raise RuntimeError("Slip39: Share identifiers do not match")
-    if share.iteration_exponent != storage.recovery.get_slip39_iteration_exponent():
+    if share.iteration_exponent != recovery.get_slip39_iteration_exponent():
         raise RuntimeError("Slip39: Share exponents do not match")
-    if storage.recovery_shares.get(share.index, share.group_index):
+    if recovery_shares.get(share.index, share_group_index):
         raise RuntimeError("Slip39: This mnemonic was already entered")
-    if share.group_count != storage.recovery.get_slip39_group_count():
+    if share.group_count != recovery.get_slip39_group_count():
         raise RuntimeError("Slip39: Group count does not match")
 
     remaining_for_share = (
-        storage.recovery.get_slip39_remaining_shares(share.group_index)
-        or share.threshold
+        recovery.get_slip39_remaining_shares(share_group_index) or share.threshold
     )
-    storage.recovery.set_slip39_remaining_shares(
-        remaining_for_share - 1, share.group_index
-    )
-    remaining[share.group_index] = remaining_for_share - 1
-    storage.recovery_shares.set(share.index, share.group_index, words)
+    recovery.set_slip39_remaining_shares(remaining_for_share - 1, share_group_index)
+    remaining[share_group_index] = remaining_for_share - 1
+    recovery_shares.set(share.index, share_group_index, words)
 
     if remaining.count(0) < share.group_threshold:
         # we need more shares
@@ -82,11 +81,11 @@ def process_slip39(words: str) -> tuple[bytes | None, slip39.Share]:
         for i, r in enumerate(remaining):
             # if we have multiple groups pass only the ones with threshold reached
             if r == 0:
-                group = storage.recovery_shares.fetch_group(i)
+                group = recovery_shares.fetch_group(i)
                 mnemonics.extend(group)
     else:
         # in case of slip39 basic we only need the first and only group
-        mnemonics = storage.recovery_shares.fetch_group(0)
+        mnemonics = recovery_shares.fetch_group(0)
 
     _, _, secret = slip39.recover_ems(mnemonics)
     return secret, share
@@ -97,6 +96,8 @@ if TYPE_CHECKING:
 
 
 def load_slip39_state() -> Slip39State:
+    from .. import backup_types
+
     previous_mnemonics = fetch_previous_mnemonics()
     if not previous_mnemonics:
         return None, None
@@ -109,10 +110,10 @@ def load_slip39_state() -> Slip39State:
 
 def fetch_previous_mnemonics() -> list[list[str]] | None:
     mnemonics = []
-    if not storage.recovery.get_slip39_group_count():
+    if not storage_recovery.get_slip39_group_count():
         return None
-    for i in range(storage.recovery.get_slip39_group_count()):
-        mnemonics.append(storage.recovery_shares.fetch_group(i))
+    for i in range(storage_recovery.get_slip39_group_count()):
+        mnemonics.append(storage_recovery_shares.fetch_group(i))
     if not any(p for p in mnemonics):
         return None
     return mnemonics

trezor/trezor-firmware

core/src/apps/management/recovery_device/__init__.py
@@ -1,24 +1,9 @@
 from typing import TYPE_CHECKING
 
-import storage
-import storage.device
-import storage.recovery
-from trezor import config, ui, wire, workflow
-from trezor.enums import ButtonRequestType
-from trezor.messages import Success
-from trezor.ui.layouts import confirm_action, confirm_reset_device
-
-from apps.common.request_pin import (
-    error_pin_invalid,
-    request_pin_and_sd_salt,
-    request_pin_confirm,
-)
-
-from .homescreen import recovery_homescreen, recovery_process
-
 if TYPE_CHECKING:
     from trezor.messages import RecoveryDevice
-
+    from trezor.wire import Context
+    from trezor.messages import Success
 
 # List of RecoveryDevice fields that can be set when doing dry-run recovery.
 # All except `dry_run` are allowed for T1 compatibility, but their values are ignored.
@@ -26,69 +11,52 @@ if TYPE_CHECKING:
 DRY_RUN_ALLOWED_FIELDS = ("dry_run", "word_count", "enforce_wordlist", "type")
 
 
-async def recovery_device(ctx: wire.Context, msg: RecoveryDevice) -> Success:
+async def recovery_device(ctx: Context, msg: RecoveryDevice) -> Success:
     """
     Recover BIP39/SLIP39 seed into empty device.
     Recovery is also possible with replugged Trezor. We call this process Persistence.
     User starts the process here using the RecoveryDevice msg and then they can unplug
     the device anytime and continue without a computer.
     """
-    _validate(msg)
+    import storage
+    import storage.device as storage_device
+    import storage.recovery as storage_recovery
+    from trezor import config, ui, wire, workflow
+    from trezor.enums import ButtonRequestType
+    from trezor.ui.layouts import confirm_action, confirm_reset_device
+    from apps.common.request_pin import (
+        error_pin_invalid,
+        request_pin_and_sd_salt,
+        request_pin_confirm,
+    )
+    from .homescreen import recovery_homescreen, recovery_process
 
-    if storage.recovery.is_in_progress():
-        return await recovery_process(ctx)
+    dry_run = msg.dry_run  # local_cache_attribute
 
-    await _continue_dialog(ctx, msg)
-
-    if not msg.dry_run:
-        # wipe storage to make sure the device is in a clear state
-        storage.reset()
-
-    # for dry run pin needs to be entered
-    if msg.dry_run:
-        curpin, salt = await request_pin_and_sd_salt(ctx, "Enter PIN")
-        if not config.check_pin(curpin, salt):
-            await error_pin_invalid(ctx)
-
-    if not msg.dry_run:
-        # set up pin if requested
-        if msg.pin_protection:
-            newpin = await request_pin_confirm(ctx, allow_cancel=False)
-            config.change_pin("", newpin, None, None)
-
-        storage.device.set_passphrase_enabled(bool(msg.passphrase_protection))
-        if msg.u2f_counter is not None:
-            storage.device.set_u2f_counter(msg.u2f_counter)
-        if msg.label is not None:
-            storage.device.set_label(msg.label)
-
-    storage.recovery.set_in_progress(True)
-    storage.recovery.set_dry_run(bool(msg.dry_run))
-
-    workflow.set_default(recovery_homescreen)
-    return await recovery_process(ctx)
-
-
-def _validate(msg: RecoveryDevice) -> None:
-    if not msg.dry_run and storage.device.is_initialized():
+    # --------------------------------------------------------
+    # validate
+    if not dry_run and storage_device.is_initialized():
         raise wire.UnexpectedMessage("Already initialized")
-    if msg.dry_run and not storage.device.is_initialized():
+    if dry_run and not storage_device.is_initialized():
         raise wire.NotInitialized("Device is not initialized")
-
     if msg.enforce_wordlist is False:
         raise wire.ProcessError(
             "Value enforce_wordlist must be True, Trezor Core enforces words automatically."
         )
-
-    if msg.dry_run:
+    if dry_run:
         # check that only allowed fields are set
         for key, value in msg.__dict__.items():
             if key not in DRY_RUN_ALLOWED_FIELDS and value is not None:
                 raise wire.ProcessError(f"Forbidden field set in dry-run: {key}")
+    # END validate
+    # --------------------------------------------------------
 
+    if storage_recovery.is_in_progress():
+        return await recovery_process(ctx)
 
-async def _continue_dialog(ctx: wire.Context, msg: RecoveryDevice) -> None:
-    if not msg.dry_run:
+    # --------------------------------------------------------
+    # _continue_dialog
+    if not dry_run:
         await confirm_reset_device(
             ctx, "Do you really want to\nrecover a wallet?", recovery=True
         )
@@ -96,8 +64,38 @@ async def _continue_dialog(ctx: wire.Context, msg: RecoveryDevice) -> None:
         await confirm_action(
             ctx,
             "confirm_seedcheck",
-            title="Seed check",
+            "Seed check",
             description="Do you really want to check the recovery seed?",
             icon=ui.ICON_RECOVERY,
             br_code=ButtonRequestType.ProtectCall,
         )
+    # END _continue_dialog
+    # --------------------------------------------------------
+
+    if not dry_run:
+        # wipe storage to make sure the device is in a clear state
+        storage.reset()
+
+    # for dry run pin needs to be entered
+    if dry_run:
+        curpin, salt = await request_pin_and_sd_salt(ctx, "Enter PIN")
+        if not config.check_pin(curpin, salt):
+            await error_pin_invalid(ctx)
+
+    if not dry_run:
+        # set up pin if requested
+        if msg.pin_protection:
+            newpin = await request_pin_confirm(ctx, allow_cancel=False)
+            config.change_pin("", newpin, None, None)
+
+        storage_device.set_passphrase_enabled(bool(msg.passphrase_protection))
+        if msg.u2f_counter is not None:
+            storage_device.set_u2f_counter(msg.u2f_counter)
+        if msg.label is not None:
+            storage_device.set_label(msg.label)
+
+    storage_recovery.set_in_progress(True)
+    storage_recovery.set_dry_run(bool(dry_run))
+
+    workflow.set_default(recovery_homescreen)
+    return await recovery_process(ctx)

core/src/apps/management/recovery_device/recover.py
@@ -1,11 +1,8 @@
 from typing import TYPE_CHECKING
 
-import storage.recovery
+import storage.recovery as storage_recovery
 import storage.recovery_shares
-from trezor.crypto import bip39, slip39
-from trezor.errors import MnemonicError
-
-from .. import backup_types
+from trezor.crypto import slip39
 
 if TYPE_CHECKING:
     from trezor.enums import BackupType
@@ -20,6 +17,9 @@ def process_bip39(words: str) -> bytes:
     Receives single mnemonic and processes it. Returns what is then stored
     in the storage, which is the mnemonic itself for BIP-39.
     """
+    from trezor.crypto import bip39
+    from trezor.errors import MnemonicError
+
     if not bip39.check(words):
         raise MnemonicError
     return words.encode()
@@ -32,17 +32,17 @@ def process_slip39(words: str) -> tuple[bytes | None, slip39.Share]:
     """
     share = slip39.decode_mnemonic(words)
 
-    remaining = storage.recovery.fetch_slip39_remaining_shares()
+    group_index = share.group_index  # local_cache_attribute
+
+    remaining = storage_recovery.fetch_slip39_remaining_shares()
 
     # if this is the first share, parse and store metadata
     if not remaining:
-        storage.recovery.set_slip39_group_count(share.group_count)
-        storage.recovery.set_slip39_iteration_exponent(share.iteration_exponent)
-        storage.recovery.set_slip39_identifier(share.identifier)
-        storage.recovery.set_slip39_remaining_shares(
-            share.threshold - 1, share.group_index
-        )
-        storage.recovery_shares.set(share.index, share.group_index, words)
+        storage_recovery.set_slip39_group_count(share.group_count)
+        storage_recovery.set_slip39_iteration_exponent(share.iteration_exponent)
+        storage_recovery.set_slip39_identifier(share.identifier)
+        storage_recovery.set_slip39_remaining_shares(share.threshold - 1, group_index)
+        storage.recovery_shares.set(share.index, group_index, words)
 
         # if share threshold and group threshold are 1
         # we can calculate the secret right away
@@ -54,24 +54,21 @@ def process_slip39(words: str) -> tuple[bytes | None, slip39.Share]:
             return None, share
 
     # These should be checked by UI before so it's a Runtime exception otherwise
-    if share.identifier != storage.recovery.get_slip39_identifier():
+    if share.identifier != storage_recovery.get_slip39_identifier():
         raise RuntimeError("Slip39: Share identifiers do not match")
-    if share.iteration_exponent != storage.recovery.get_slip39_iteration_exponent():
+    if share.iteration_exponent != storage_recovery.get_slip39_iteration_exponent():
         raise RuntimeError("Slip39: Share exponents do not match")
-    if storage.recovery_shares.get(share.index, share.group_index):
+    if storage.recovery_shares.get(share.index, group_index):
         raise RuntimeError("Slip39: This mnemonic was already entered")
-    if share.group_count != storage.recovery.get_slip39_group_count():
+    if share.group_count != storage_recovery.get_slip39_group_count():
         raise RuntimeError("Slip39: Group count does not match")
 
     remaining_for_share = (
-        storage.recovery.get_slip39_remaining_shares(share.group_index)
-        or share.threshold
+        storage_recovery.get_slip39_remaining_shares(group_index) or share.threshold
     )
-    storage.recovery.set_slip39_remaining_shares(
-        remaining_for_share - 1, share.group_index
-    )
-    remaining[share.group_index] = remaining_for_share - 1
-    storage.recovery_shares.set(share.index, share.group_index, words)
+    storage_recovery.set_slip39_remaining_shares(remaining_for_share - 1, group_index)
+    remaining[group_index] = remaining_for_share - 1
+    storage.recovery_shares.set(share.index, group_index, words)
 
     if remaining.count(0) < share.group_threshold:
         # we need more shares
@@ -97,6 +94,8 @@ if TYPE_CHECKING:
 
 
 def load_slip39_state() -> Slip39State:
+    from .. import backup_types
+
     previous_mnemonics = fetch_previous_mnemonics()
     if not previous_mnemonics:
         return None, None
@@ -109,9 +108,9 @@ def load_slip39_state() -> Slip39State:
 
 def fetch_previous_mnemonics() -> list[list[str]] | None:
     mnemonics = []
-    if not storage.recovery.get_slip39_group_count():
+    if not storage_recovery.get_slip39_group_count():
         return None
-    for i in range(storage.recovery.get_slip39_group_count()):
+    for i in range(storage_recovery.get_slip39_group_count()):
         mnemonics.append(storage.recovery_shares.fetch_group(i))
     if not any(p for p in mnemonics):
         return None