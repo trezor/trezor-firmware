jia58628/trezor-trezor-firmware

chore(core): decrease bitcoin size by 1740 bytes

core/src/apps/bitcoin/authorization.py
@@ -1,16 +1,11 @@
 from micropython import const
 from typing import TYPE_CHECKING
 
-from trezor import utils, wire
-from trezor.messages import AuthorizeCoinJoin
-
-from apps.common import authorization
-
 from .common import BIP32_WALLET_DEPTH
-from .writers import write_bytes_prefixed
 
 if TYPE_CHECKING:
     from trezor.messages import (
+        AuthorizeCoinJoin,
         GetOwnershipProof,
         SignTx,
         TxInput,
@@ -27,14 +22,19 @@ class CoinJoinAuthorization:
         self.params = params
 
     def check_get_ownership_proof(self, msg: GetOwnershipProof) -> bool:
+        from trezor import utils
+        from .writers import write_bytes_prefixed
+
+        self_params = self.params  # cache
+
         # Check whether the current params matches the parameters of the request.
-        coordinator = utils.empty_bytearray(1 + len(self.params.coordinator.encode()))
-        write_bytes_prefixed(coordinator, self.params.coordinator.encode())
+        coordinator = utils.empty_bytearray(1 + len(self_params.coordinator.encode()))
+        write_bytes_prefixed(coordinator, self_params.coordinator.encode())
         return (
             len(msg.address_n) >= BIP32_WALLET_DEPTH
-            and msg.address_n[:-BIP32_WALLET_DEPTH] == self.params.address_n
-            and msg.coin_name == self.params.coin_name
-            and msg.script_type == self.params.script_type
+            and msg.address_n[:-BIP32_WALLET_DEPTH] == self_params.address_n
+            and msg.coin_name == self_params.coin_name
+            and msg.script_type == self_params.script_type
             and msg.commitment_data.startswith(bytes(coordinator))
         )
 
@@ -48,15 +48,22 @@ class CoinJoinAuthorization:
         )
 
     def approve_sign_tx(self, msg: SignTx) -> bool:
-        if self.params.max_rounds < 1 or msg.coin_name != self.params.coin_name:
+        from apps.common import authorization
+
+        self_params = self.params  # cache
+
+        if self_params.max_rounds < 1 or msg.coin_name != self_params.coin_name:
             return False
 
-        self.params.max_rounds -= 1
-        authorization.set(self.params)
+        self_params.max_rounds -= 1
+        authorization.set(self_params)
         return True
 
 
 def from_cached_message(auth_msg: MessageType) -> CoinJoinAuthorization:
+    from trezor import wire
+    from trezor.messages import AuthorizeCoinJoin
+
     if not AuthorizeCoinJoin.is_type_of(auth_msg):
         raise wire.ProcessError("Appropriate params was not found")
 

core/src/apps/bitcoin/get_ownership_id.py
@@ -1,25 +1,30 @@
 from typing import TYPE_CHECKING
 
-from trezor import wire
-from trezor.enums import InputScriptType
-from trezor.messages import OwnershipId
-
-from apps.common.paths import validate_path
-
-from . import addresses, common, scripts
-from .keychain import validate_path_against_script_type, with_keychain
-from .ownership import get_identifier
+from .keychain import with_keychain
 
 if TYPE_CHECKING:
-    from trezor.messages import GetOwnershipId
+    from trezor.messages import GetOwnershipId, OwnershipId
+    from trezor.wire import Context
     from apps.common.coininfo import CoinInfo
     from apps.common.keychain import Keychain
 
 
 @with_keychain
 async def get_ownership_id(
-    ctx: wire.Context, msg: GetOwnershipId, keychain: Keychain, coin: CoinInfo
+    ctx: Context, msg: GetOwnershipId, keychain: Keychain, coin: CoinInfo
 ) -> OwnershipId:
+    from trezor.wire import DataError
+    from trezor.enums import InputScriptType
+    from trezor.messages import OwnershipId
+
+    from apps.common.paths import validate_path
+
+    from . import addresses, common, scripts
+    from .keychain import validate_path_against_script_type
+    from .ownership import get_identifier
+
+    msg_script_type = msg.script_type  # cache
+
     await validate_path(
         ctx,
         keychain,
@@ -27,17 +32,17 @@ async def get_ownership_id(
         validate_path_against_script_type(coin, msg),
     )
 
-    if msg.script_type not in common.INTERNAL_INPUT_SCRIPT_TYPES:
-        raise wire.DataError("Invalid script type")
+    if msg_script_type not in common.INTERNAL_INPUT_SCRIPT_TYPES:
+        raise DataError("Invalid script type")
 
-    if msg.script_type in common.SEGWIT_INPUT_SCRIPT_TYPES and not coin.segwit:
-        raise wire.DataError("Segwit not enabled on this coin")
+    if msg_script_type in common.SEGWIT_INPUT_SCRIPT_TYPES and not coin.segwit:
+        raise DataError("Segwit not enabled on this coin")
 
-    if msg.script_type == InputScriptType.SPENDTAPROOT and not coin.taproot:
-        raise wire.DataError("Taproot not enabled on this coin")
+    if msg_script_type == InputScriptType.SPENDTAPROOT and not coin.taproot:
+        raise DataError("Taproot not enabled on this coin")
 
     node = keychain.derive(msg.address_n)
-    address = addresses.get_address(msg.script_type, coin, node, msg.multisig)
+    address = addresses.get_address(msg_script_type, coin, node, msg.multisig)
     script_pubkey = scripts.output_derive_script(address, coin)
     ownership_id = get_identifier(script_pubkey, keychain)
 

core/src/apps/bitcoin/get_ownership_proof.py
@@ -1,18 +1,10 @@
 from typing import TYPE_CHECKING
 
-from trezor import ui, wire
-from trezor.enums import InputScriptType
-from trezor.messages import OwnershipProof
-from trezor.ui.layouts import confirm_action, confirm_blob
-
-from apps.common.paths import validate_path
-
-from . import addresses, common, scripts
-from .keychain import validate_path_against_script_type, with_keychain
-from .ownership import generate_proof, get_identifier
+from .keychain import with_keychain
 
 if TYPE_CHECKING:
-    from trezor.messages import GetOwnershipProof
+    from trezor.messages import GetOwnershipProof, OwnershipProof
+    from trezor.wire import Context
     from apps.common.coininfo import CoinInfo
     from apps.common.keychain import Keychain
     from .authorization import CoinJoinAuthorization
@@ -20,15 +12,30 @@ if TYPE_CHECKING:
 
 @with_keychain
 async def get_ownership_proof(
-    ctx: wire.Context,
+    ctx: Context,
     msg: GetOwnershipProof,
     keychain: Keychain,
     coin: CoinInfo,
     authorization: CoinJoinAuthorization | None = None,
 ) -> OwnershipProof:
+    from trezor import ui
+    from trezor.wire import DataError, ProcessError
+    from trezor.enums import InputScriptType
+    from trezor.messages import OwnershipProof
+    from trezor.ui.layouts import confirm_action, confirm_blob
+
+    from apps.common.paths import validate_path
+
+    from . import addresses, common, scripts
+    from .keychain import validate_path_against_script_type
+    from .ownership import generate_proof, get_identifier
+
+    msg_script_type = msg.script_type  # cache
+    msg_ownership_ids = msg.ownership_ids  # cache
+
     if authorization:
         if not authorization.check_get_ownership_proof(msg):
-            raise wire.ProcessError("Unauthorized operation")
+            raise ProcessError("Unauthorized operation")
     else:
         await validate_path(
             ctx,
@@ -37,57 +44,57 @@ async def get_ownership_proof(
             validate_path_against_script_type(coin, msg),
         )
 
-    if msg.script_type not in common.INTERNAL_INPUT_SCRIPT_TYPES:
-        raise wire.DataError("Invalid script type")
+    if msg_script_type not in common.INTERNAL_INPUT_SCRIPT_TYPES:
+        raise DataError("Invalid script type")
 
-    if msg.script_type in common.SEGWIT_INPUT_SCRIPT_TYPES and not coin.segwit:
-        raise wire.DataError("Segwit not enabled on this coin")
+    if msg_script_type in common.SEGWIT_INPUT_SCRIPT_TYPES and not coin.segwit:
+        raise DataError("Segwit not enabled on this coin")
 
-    if msg.script_type == InputScriptType.SPENDTAPROOT and not coin.taproot:
-        raise wire.DataError("Taproot not enabled on this coin")
+    if msg_script_type == InputScriptType.SPENDTAPROOT and not coin.taproot:
+        raise DataError("Taproot not enabled on this coin")
 
     node = keychain.derive(msg.address_n)
-    address = addresses.get_address(msg.script_type, coin, node, msg.multisig)
+    address = addresses.get_address(msg_script_type, coin, node, msg.multisig)
     script_pubkey = scripts.output_derive_script(address, coin)
     ownership_id = get_identifier(script_pubkey, keychain)
 
     # If the scriptPubKey is multisig, then the caller has to provide
     # ownership IDs, otherwise providing an ID is optional.
     if msg.multisig:
-        if ownership_id not in msg.ownership_ids:
-            raise wire.DataError("Missing ownership identifier")
-    elif msg.ownership_ids:
-        if msg.ownership_ids != [ownership_id]:
-            raise wire.DataError("Invalid ownership identifier")
+        if ownership_id not in msg_ownership_ids:
+            raise DataError("Missing ownership identifier")
+    elif msg_ownership_ids:
+        if msg_ownership_ids != [ownership_id]:
+            raise DataError("Invalid ownership identifier")
     else:
-        msg.ownership_ids = [ownership_id]
+        msg_ownership_ids = [ownership_id]
 
     # In order to set the "user confirmation" bit in the proof, the user must actually confirm.
     if msg.user_confirmation and not authorization:
         await confirm_action(
             ctx,
             "confirm_ownership_proof",
-            title="Proof of ownership",
+            "Proof of ownership",
             description="Do you want to create a proof of ownership?",
         )
         if msg.commitment_data:
             await confirm_blob(
                 ctx,
                 "confirm_ownership_proof",
-                title="Proof of ownership",
-                description="Commitment data:",
-                data=msg.commitment_data,
+                "Proof of ownership",
+                msg.commitment_data,
+                "Commitment data:",
                 icon=ui.ICON_CONFIG,
                 icon_color=ui.ORANGE_ICON,
             )
 
     ownership_proof, signature = generate_proof(
         node,
-        msg.script_type,
+        msg_script_type,
         msg.multisig,
         coin,
         msg.user_confirmation,
-        msg.ownership_ids,
+        msg_ownership_ids,
         script_pubkey,
         msg.commitment_data,
     )

core/src/apps/bitcoin/sign_tx/zcash_v4.py
@@ -1,32 +1,19 @@
-import ustruct as struct
 from micropython import const
 from typing import TYPE_CHECKING
 
-from trezor import wire
 from trezor.crypto.hashlib import blake2b
-from trezor.utils import HashWriter, ensure
+from trezor.utils import HashWriter
+from trezor.wire import DataError
 
-from apps.common.coininfo import CoinInfo
-from apps.common.keychain import Keychain
-from apps.common.writers import write_compact_size
-
-from ..scripts import write_bip143_script_code_prefixed
-from ..writers import (
-    TX_HASH_SIZE,
-    get_tx_hash,
-    write_bytes_fixed,
-    write_bytes_reversed,
-    write_tx_output,
-    write_uint32,
-    write_uint64,
-)
-from . import approvers, helpers
+from ..writers import TX_HASH_SIZE, write_bytes_reversed, write_uint32, write_uint64
 from .bitcoinlike import Bitcoinlike
 
 if TYPE_CHECKING:
     from trezor.messages import PrevTx, SignTx, TxInput, TxOutput
+    from apps.common.coininfo import CoinInfo
+    from apps.common.keychain import Keychain
+    from . import approvers
     from typing import Sequence
-    from apps.common import coininfo
     from .sig_hasher import SigHasher
     from .tx_info import OriginalTxInfo, TxInfo
     from ..common import SigHashType
@@ -47,6 +34,8 @@ class Zip243SigHasher:
         write_uint32(self.h_sequence, txi.sequence)
 
     def add_output(self, txo: TxOutput, script_pubkey: bytes) -> None:
+        from ..writers import write_tx_output
+
         write_tx_output(self.h_outputs, txo, script_pubkey)
 
     def hash143(
@@ -55,9 +44,13 @@ class Zip243SigHasher:
         public_keys: Sequence[bytes | memoryview],
         threshold: int,
         tx: SignTx | PrevTx,
-        coin: coininfo.CoinInfo,
+        coin: CoinInfo,
         hash_type: int,
     ) -> bytes:
+        import ustruct as struct
+        from ..scripts import write_bip143_script_code_prefixed
+        from ..writers import get_tx_hash, write_bytes_fixed
+
         h_preimage = HashWriter(
             blake2b(
                 outlen=32,
@@ -129,22 +122,29 @@ class ZcashV4(Bitcoinlike):
         coin: CoinInfo,
         approver: approvers.Approver | None,
     ) -> None:
+        from trezor.utils import ensure
+
         ensure(coin.overwintered)
         super().__init__(tx, keychain, coin, approver)
 
         if tx.version != 4:
-            raise wire.DataError("Unsupported transaction version.")
+            raise DataError("Unsupported transaction version.")
 
     def create_sig_hasher(self, tx: SignTx | PrevTx) -> SigHasher:
         return Zip243SigHasher()
 
     async def step7_finish(self) -> None:
-        self.write_tx_footer(self.serialized_tx, self.tx_info.tx)
+        from apps.common.writers import write_compact_size
+        from . import helpers
 
-        write_uint64(self.serialized_tx, 0)  # valueBalance
-        write_compact_size(self.serialized_tx, 0)  # nShieldedSpend
-        write_compact_size(self.serialized_tx, 0)  # nShieldedOutput
-        write_compact_size(self.serialized_tx, 0)  # nJoinSplit
+        self_serialized_tx = self.serialized_tx  # cache
+
+        self.write_tx_footer(self_serialized_tx, self.tx_info.tx)
+
+        write_uint64(self_serialized_tx, 0)  # valueBalance
+        write_compact_size(self_serialized_tx, 0)  # nShieldedSpend
+        write_compact_size(self_serialized_tx, 0)  # nShieldedOutput
+        write_compact_size(self_serialized_tx, 0)  # nJoinSplit
 
         await helpers.request_tx_finish(self.tx_req)
 
@@ -178,7 +178,7 @@ class ZcashV4(Bitcoinlike):
             write_uint32(w, tx.version)
         else:
             if tx.version_group_id is None:
-                raise wire.DataError("Version group ID is missing")
+                raise DataError("Version group ID is missing")
             # nVersion | fOverwintered
             write_uint32(w, tx.version | _OVERWINTERED)
             write_uint32(w, tx.version_group_id)  # nVersionGroupId

trezor/trezor-firmware

core/src/apps/bitcoin/authorization.py
@@ -1,16 +1,11 @@
 from micropython import const
 from typing import TYPE_CHECKING
 
-from trezor import utils, wire
-from trezor.messages import AuthorizeCoinJoin
-
-from apps.common import authorization
-
 from .common import BIP32_WALLET_DEPTH
-from .writers import write_bytes_prefixed
 
 if TYPE_CHECKING:
     from trezor.messages import (
+        AuthorizeCoinJoin,
         GetOwnershipProof,
         SignTx,
         TxInput,
@@ -27,14 +22,19 @@ class CoinJoinAuthorization:
         self.params = params
 
     def check_get_ownership_proof(self, msg: GetOwnershipProof) -> bool:
+        from trezor import utils
+        from .writers import write_bytes_prefixed
+
+        params = self.params  # local_cache_attribute
+
         # Check whether the current params matches the parameters of the request.
-        coordinator = utils.empty_bytearray(1 + len(self.params.coordinator.encode()))
-        write_bytes_prefixed(coordinator, self.params.coordinator.encode())
+        coordinator = utils.empty_bytearray(1 + len(params.coordinator.encode()))
+        write_bytes_prefixed(coordinator, params.coordinator.encode())
         return (
             len(msg.address_n) >= BIP32_WALLET_DEPTH
-            and msg.address_n[:-BIP32_WALLET_DEPTH] == self.params.address_n
-            and msg.coin_name == self.params.coin_name
-            and msg.script_type == self.params.script_type
+            and msg.address_n[:-BIP32_WALLET_DEPTH] == params.address_n
+            and msg.coin_name == params.coin_name
+            and msg.script_type == params.script_type
             and msg.commitment_data.startswith(bytes(coordinator))
         )
 
@@ -48,15 +48,22 @@ class CoinJoinAuthorization:
         )
 
     def approve_sign_tx(self, msg: SignTx) -> bool:
-        if self.params.max_rounds < 1 or msg.coin_name != self.params.coin_name:
+        from apps.common import authorization
+
+        params = self.params  # local_cache_attribute
+
+        if params.max_rounds < 1 or msg.coin_name != params.coin_name:
             return False
 
-        self.params.max_rounds -= 1
-        authorization.set(self.params)
+        params.max_rounds -= 1
+        authorization.set(params)
         return True
 
 
 def from_cached_message(auth_msg: MessageType) -> CoinJoinAuthorization:
+    from trezor import wire
+    from trezor.messages import AuthorizeCoinJoin
+
     if not AuthorizeCoinJoin.is_type_of(auth_msg):
         raise wire.ProcessError("Appropriate params was not found")
 

core/src/apps/bitcoin/get_ownership_id.py
@@ -1,25 +1,30 @@
 from typing import TYPE_CHECKING
 
-from trezor import wire
-from trezor.enums import InputScriptType
-from trezor.messages import OwnershipId
-
-from apps.common.paths import validate_path
-
-from . import addresses, common, scripts
-from .keychain import validate_path_against_script_type, with_keychain
-from .ownership import get_identifier
+from .keychain import with_keychain
 
 if TYPE_CHECKING:
-    from trezor.messages import GetOwnershipId
+    from trezor.messages import GetOwnershipId, OwnershipId
+    from trezor.wire import Context
     from apps.common.coininfo import CoinInfo
     from apps.common.keychain import Keychain
 
 
 @with_keychain
 async def get_ownership_id(
-    ctx: wire.Context, msg: GetOwnershipId, keychain: Keychain, coin: CoinInfo
+    ctx: Context, msg: GetOwnershipId, keychain: Keychain, coin: CoinInfo
 ) -> OwnershipId:
+    from trezor.wire import DataError
+    from trezor.enums import InputScriptType
+    from trezor.messages import OwnershipId
+
+    from apps.common.paths import validate_path
+
+    from . import addresses, common, scripts
+    from .keychain import validate_path_against_script_type
+    from .ownership import get_identifier
+
+    script_type = msg.script_type  # local_cache_attribute
+
     await validate_path(
         ctx,
         keychain,
@@ -27,17 +32,17 @@ async def get_ownership_id(
         validate_path_against_script_type(coin, msg),
     )
 
-    if msg.script_type not in common.INTERNAL_INPUT_SCRIPT_TYPES:
-        raise wire.DataError("Invalid script type")
+    if script_type not in common.INTERNAL_INPUT_SCRIPT_TYPES:
+        raise DataError("Invalid script type")
 
-    if msg.script_type in common.SEGWIT_INPUT_SCRIPT_TYPES and not coin.segwit:
-        raise wire.DataError("Segwit not enabled on this coin")
+    if script_type in common.SEGWIT_INPUT_SCRIPT_TYPES and not coin.segwit:
+        raise DataError("Segwit not enabled on this coin")
 
-    if msg.script_type == InputScriptType.SPENDTAPROOT and not coin.taproot:
-        raise wire.DataError("Taproot not enabled on this coin")
+    if script_type == InputScriptType.SPENDTAPROOT and not coin.taproot:
+        raise DataError("Taproot not enabled on this coin")
 
     node = keychain.derive(msg.address_n)
-    address = addresses.get_address(msg.script_type, coin, node, msg.multisig)
+    address = addresses.get_address(script_type, coin, node, msg.multisig)
     script_pubkey = scripts.output_derive_script(address, coin)
     ownership_id = get_identifier(script_pubkey, keychain)
 

core/src/apps/bitcoin/get_ownership_proof.py
@@ -1,18 +1,10 @@
 from typing import TYPE_CHECKING
 
-from trezor import ui, wire
-from trezor.enums import InputScriptType
-from trezor.messages import OwnershipProof
-from trezor.ui.layouts import confirm_action, confirm_blob
-
-from apps.common.paths import validate_path
-
-from . import addresses, common, scripts
-from .keychain import validate_path_against_script_type, with_keychain
-from .ownership import generate_proof, get_identifier
+from .keychain import with_keychain
 
 if TYPE_CHECKING:
-    from trezor.messages import GetOwnershipProof
+    from trezor.messages import GetOwnershipProof, OwnershipProof
+    from trezor.wire import Context
     from apps.common.coininfo import CoinInfo
     from apps.common.keychain import Keychain
     from .authorization import CoinJoinAuthorization
@@ -20,15 +12,30 @@ if TYPE_CHECKING:
 
 @with_keychain
 async def get_ownership_proof(
-    ctx: wire.Context,
+    ctx: Context,
     msg: GetOwnershipProof,
     keychain: Keychain,
     coin: CoinInfo,
     authorization: CoinJoinAuthorization | None = None,
 ) -> OwnershipProof:
+    from trezor import ui
+    from trezor.wire import DataError, ProcessError
+    from trezor.enums import InputScriptType
+    from trezor.messages import OwnershipProof
+    from trezor.ui.layouts import confirm_action, confirm_blob
+
+    from apps.common.paths import validate_path
+
+    from . import addresses, common, scripts
+    from .keychain import validate_path_against_script_type
+    from .ownership import generate_proof, get_identifier
+
+    script_type = msg.script_type  # local_cache_attribute
+    ownership_ids = msg.ownership_ids  # local_cache_attribute
+
     if authorization:
         if not authorization.check_get_ownership_proof(msg):
-            raise wire.ProcessError("Unauthorized operation")
+            raise ProcessError("Unauthorized operation")
     else:
         await validate_path(
             ctx,
@@ -37,57 +44,57 @@ async def get_ownership_proof(
             validate_path_against_script_type(coin, msg),
         )
 
-    if msg.script_type not in common.INTERNAL_INPUT_SCRIPT_TYPES:
-        raise wire.DataError("Invalid script type")
+    if script_type not in common.INTERNAL_INPUT_SCRIPT_TYPES:
+        raise DataError("Invalid script type")
 
-    if msg.script_type in common.SEGWIT_INPUT_SCRIPT_TYPES and not coin.segwit:
-        raise wire.DataError("Segwit not enabled on this coin")
+    if script_type in common.SEGWIT_INPUT_SCRIPT_TYPES and not coin.segwit:
+        raise DataError("Segwit not enabled on this coin")
 
-    if msg.script_type == InputScriptType.SPENDTAPROOT and not coin.taproot:
-        raise wire.DataError("Taproot not enabled on this coin")
+    if script_type == InputScriptType.SPENDTAPROOT and not coin.taproot:
+        raise DataError("Taproot not enabled on this coin")
 
     node = keychain.derive(msg.address_n)
-    address = addresses.get_address(msg.script_type, coin, node, msg.multisig)
+    address = addresses.get_address(script_type, coin, node, msg.multisig)
     script_pubkey = scripts.output_derive_script(address, coin)
     ownership_id = get_identifier(script_pubkey, keychain)
 
     # If the scriptPubKey is multisig, then the caller has to provide
     # ownership IDs, otherwise providing an ID is optional.
     if msg.multisig:
-        if ownership_id not in msg.ownership_ids:
-            raise wire.DataError("Missing ownership identifier")
-    elif msg.ownership_ids:
-        if msg.ownership_ids != [ownership_id]:
-            raise wire.DataError("Invalid ownership identifier")
+        if ownership_id not in ownership_ids:
+            raise DataError("Missing ownership identifier")
+    elif ownership_ids:
+        if ownership_ids != [ownership_id]:
+            raise DataError("Invalid ownership identifier")
     else:
-        msg.ownership_ids = [ownership_id]
+        ownership_ids = [ownership_id]
 
     # In order to set the "user confirmation" bit in the proof, the user must actually confirm.
     if msg.user_confirmation and not authorization:
         await confirm_action(
             ctx,
             "confirm_ownership_proof",
-            title="Proof of ownership",
+            "Proof of ownership",
             description="Do you want to create a proof of ownership?",
         )
         if msg.commitment_data:
             await confirm_blob(
                 ctx,
                 "confirm_ownership_proof",
-                title="Proof of ownership",
-                description="Commitment data:",
-                data=msg.commitment_data,
+                "Proof of ownership",
+                msg.commitment_data,
+                "Commitment data:",
                 icon=ui.ICON_CONFIG,
                 icon_color=ui.ORANGE_ICON,
             )
 
     ownership_proof, signature = generate_proof(
         node,
-        msg.script_type,
+        script_type,
         msg.multisig,
         coin,
         msg.user_confirmation,
-        msg.ownership_ids,
+        ownership_ids,
         script_pubkey,
         msg.commitment_data,
     )

core/src/apps/bitcoin/sign_tx/zcash_v4.py
@@ -1,32 +1,19 @@
-import ustruct as struct
 from micropython import const
 from typing import TYPE_CHECKING
 
-from trezor import wire
 from trezor.crypto.hashlib import blake2b
-from trezor.utils import HashWriter, ensure
+from trezor.utils import HashWriter
+from trezor.wire import DataError
 
-from apps.common.coininfo import CoinInfo
-from apps.common.keychain import Keychain
-from apps.common.writers import write_compact_size
-
-from ..scripts import write_bip143_script_code_prefixed
-from ..writers import (
-    TX_HASH_SIZE,
-    get_tx_hash,
-    write_bytes_fixed,
-    write_bytes_reversed,
-    write_tx_output,
-    write_uint32,
-    write_uint64,
-)
-from . import approvers, helpers
+from ..writers import TX_HASH_SIZE, write_bytes_reversed, write_uint32, write_uint64
 from .bitcoinlike import Bitcoinlike
 
 if TYPE_CHECKING:
     from trezor.messages import PrevTx, SignTx, TxInput, TxOutput
+    from apps.common.coininfo import CoinInfo
+    from apps.common.keychain import Keychain
+    from . import approvers
     from typing import Sequence
-    from apps.common import coininfo
     from .sig_hasher import SigHasher
     from .tx_info import OriginalTxInfo, TxInfo
     from ..common import SigHashType
@@ -47,6 +34,8 @@ class Zip243SigHasher:
         write_uint32(self.h_sequence, txi.sequence)
 
     def add_output(self, txo: TxOutput, script_pubkey: bytes) -> None:
+        from ..writers import write_tx_output
+
         write_tx_output(self.h_outputs, txo, script_pubkey)
 
     def hash143(
@@ -55,9 +44,13 @@ class Zip243SigHasher:
         public_keys: Sequence[bytes | memoryview],
         threshold: int,
         tx: SignTx | PrevTx,
-        coin: coininfo.CoinInfo,
+        coin: CoinInfo,
         hash_type: int,
     ) -> bytes:
+        import ustruct as struct
+        from ..scripts import write_bip143_script_code_prefixed
+        from ..writers import get_tx_hash, write_bytes_fixed
+
         h_preimage = HashWriter(
             blake2b(
                 outlen=32,
@@ -129,23 +122,30 @@ class ZcashV4(Bitcoinlike):
         coin: CoinInfo,
         approver: approvers.Approver | None,
     ) -> None:
+        from trezor.utils import ensure
+
         ensure(coin.overwintered)
         super().__init__(tx, keychain, coin, approver)
 
         if tx.version != 4:
-            raise wire.DataError("Unsupported transaction version.")
+            raise DataError("Unsupported transaction version.")
 
     def create_sig_hasher(self, tx: SignTx | PrevTx) -> SigHasher:
         return Zip243SigHasher()
 
     async def step7_finish(self) -> None:
-        if self.serialize:
-            self.write_tx_footer(self.serialized_tx, self.tx_info.tx)
+        from apps.common.writers import write_compact_size
+        from . import helpers
 
-            write_uint64(self.serialized_tx, 0)  # valueBalance
-            write_compact_size(self.serialized_tx, 0)  # nShieldedSpend
-            write_compact_size(self.serialized_tx, 0)  # nShieldedOutput
-            write_compact_size(self.serialized_tx, 0)  # nJoinSplit
+        serialized_tx = self.serialized_tx  # local_cache_attribute
+
+        if self.serialize:
+            self.write_tx_footer(serialized_tx, self.tx_info.tx)
+
+            write_uint64(serialized_tx, 0)  # valueBalance
+            write_compact_size(serialized_tx, 0)  # nShieldedSpend
+            write_compact_size(serialized_tx, 0)  # nShieldedOutput
+            write_compact_size(serialized_tx, 0)  # nJoinSplit
 
         await helpers.request_tx_finish(self.tx_req)
 
@@ -179,7 +179,7 @@ class ZcashV4(Bitcoinlike):
             write_uint32(w, tx.version)
         else:
             if tx.version_group_id is None:
-                raise wire.DataError("Version group ID is missing")
+                raise DataError("Version group ID is missing")
             # nVersion | fOverwintered
             write_uint32(w, tx.version | _OVERWINTERED)
             write_uint32(w, tx.version_group_id)  # nVersionGroupId