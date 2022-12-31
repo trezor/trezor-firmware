3d89465d2333609d7faa182785cd99170e86f89a
grdddj <jiri.musil06@seznam.cz>
2022-12-31 13:36:29 +0100

    WIP - delete model_1 from core

--- core/mocks/generated/trezorui2.pyi
+++ core/mocks/generated/trezorui2.pyi
@@ -1,37 +1,6 @@
 from typing import *
 CONFIRMED: object
 CANCELLED: object
-
-
-# rust/src/ui/model_t1/layout.rs
-def disable_animation(disable: bool) -> None:
-    """Disable animations, debug builds only."""
-
-
-# rust/src/ui/model_t1/layout.rs
-def confirm_action(
-    *,
-    title: str,
-    action: str | None = None,
-    description: str | None = None,
-    verb: str | None = None,
-    verb_cancel: str | None = None,
-    hold: bool | None = None,
-    reverse: bool = False,
-) -> object:
-    """Confirm action."""
-
-
-# rust/src/ui/model_t1/layout.rs
-def confirm_text(
-    *,
-    title: str,
-    data: str,
-    description: str | None,
-) -> object:
-    """Confirm text."""
-CONFIRMED: object
-CANCELLED: object
 INFO: object
 
 
