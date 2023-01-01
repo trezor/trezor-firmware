68b5790f4f8a8b6ce02e0b369c1956742de586dd
grdddj <jiri.musil06@seznam.cz>
2023-01-01 17:35:39 +0100

    WIP - delete unused icons

--- core/embed/rust/src/ui/model_tr/theme.rs
+++ core/embed/rust/src/ui/model_tr/theme.rs
@@ -37,13 +37,7 @@ pub const ICON_ARROW_UP: IconAndName =
     IconAndName::new(include_res!("model_tr/res/arrow_up.toif"), "arrow_up"); // 10*6
 pub const ICON_ARROW_DOWN: IconAndName =
     IconAndName::new(include_res!("model_tr/res/arrow_down.toif"), "arrow_down"); // 10*6
-pub const ICON_AMOUNT: IconAndName =
-    IconAndName::new(include_res!("model_tr/res/amount_smaller.toif"), "amount"); // 8*8
 pub const ICON_BIN: IconAndName = IconAndName::new(include_res!("model_tr/res/bin.toif"), "bin"); // 10*10
-pub const ICON_CANCEL_OUTLINE: IconAndName = IconAndName::new(
-    include_res!("model_tr/res/cancel_for_outline.toif"),
-    "cancel_outline",
-); // 8*8
 pub const ICON_CANCEL: IconAndName = IconAndName::new(
     include_res!("model_tr/res/cancel_no_outline.toif"),
     "cancel",
@@ -52,21 +46,14 @@ pub const ICON_DELETE: IconAndName =
     IconAndName::new(include_res!("model_tr/res/delete.toif"), "delete"); // 12*8
 pub const ICON_EYE: IconAndName =
     IconAndName::new(include_res!("model_tr/res/eye_round.toif"), "eye"); // 12*7
-pub const ICON_FAIL: IconAndName = IconAndName::new(include_res!("model_tr/res/fail.toif"), "fail");
 pub const ICON_LOCK: IconAndName = IconAndName::new(include_res!("model_tr/res/lock.toif"), "lock"); // 10*10
 pub const ICON_NEXT_PAGE: IconAndName =
     IconAndName::new(include_res!("model_tr/res/next_page.toif"), "next_page"); // 10*8
-pub const ICON_PARAM: IconAndName =
-    IconAndName::new(include_res!("model_tr/res/param_smaller.toif"), "param"); // 7*8
 pub const ICON_PREV_PAGE: IconAndName =
     IconAndName::new(include_res!("model_tr/res/prev_page.toif"), "prev_page"); // 8*10
 pub const ICON_SUCCESS: IconAndName =
     IconAndName::new(include_res!("model_tr/res/success.toif"), "success");
 pub const ICON_TICK: IconAndName = IconAndName::new(include_res!("model_tr/res/tick.toif"), "tick"); // 10*10
-pub const ICON_USER: IconAndName =
-    IconAndName::new(include_res!("model_tr/res/user_smaller.toif"), "user"); // 8*8
-pub const ICON_WALLET: IconAndName =
-    IconAndName::new(include_res!("model_tr/res/wallet.toif"), "wallet"); // 10*10
 pub const ICON_WARNING: IconAndName =
     IconAndName::new(include_res!("model_tr/res/warning.toif"), "warning"); // 12*12
 
