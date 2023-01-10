6fcbed4c6389a9a20f5818d9128d07af235242fa
tychovrahe <brunam@seznam.cz>
2023-01-10 16:47:23 +0100

    build(core): embed bootloader according to trezor model
    [no changelog]

--- core/site_scons/tools.py
+++ core/site_scons/tools.py
@@ -44,3 +44,14 @@ def configure_board(model, env, defines, sources):
     defines += [f'HW_REVISION={hw_revision}', ]
     sources += [f'embed/trezorhal/displays/{display}', ]
     env.get('ENV')['TREZOR_BOARD'] = board
+
+
+def get_model_identifier(model):
+    if model == '1':
+        return "T1B1"
+    elif model == 'T':
+        return "T2T1"
+    elif model == 'R':
+        return "T2B1"
+    else:
+        raise Exception("Unknown model")
