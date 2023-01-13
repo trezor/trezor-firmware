d08d59b1d8dd5ef8f8632fd9728ffe1556b0e15c
grdddj <jiri.musil06@seznam.cz>
2023-01-13 16:21:27 +0100

    feat(core): implement loader based on time in milliseconds
    [no changelog]

--- core/src/trezor/pin.py
+++ core/src/trezor/pin.py
@@ -7,7 +7,21 @@ if TYPE_CHECKING:
 _previous_seconds: int | None = None
 _previous_remaining: str | None = None
 _progress_layout: ProgressLayout | None = None
+_started_with_empty_loader = False
 keepalive_callback: Any = None
+
+
+def render_empty_loader(message: str, description: str) -> None:
+    """Render empty loader to prevent the screen appear to be frozen."""
+    from trezor.ui.layouts import pin_progress
+
+    global _progress_layout
+    global _started_with_empty_loader
+
+    _progress_layout = pin_progress(message, description)
+    _progress_layout.report(0, None)
+
+    _started_with_empty_loader = True
 
 
 def show_pin_timeout(seconds: int, progress: int, message: str) -> bool:
@@ -16,6 +30,7 @@ def show_pin_timeout(seconds: int, progress: int, message: str) -> bool:
     global _previous_seconds
     global _previous_remaining
     global _progress_layout
+    global _started_with_empty_loader
 
     if callable(keepalive_callback):
         keepalive_callback()
@@ -35,9 +50,18 @@ def show_pin_timeout(seconds: int, progress: int, message: str) -> bool:
     else:
         remaining = _previous_remaining
 
-    if progress == 0 or _progress_layout is None:
+    # create the layout if it doesn't exist yet or should be started again
+    if _progress_layout is None or (progress == 0 and not _started_with_empty_loader):
         _progress_layout = pin_progress(message, description=remaining or "")
+
+    # reset the flag - the render_empty_loader() has the effect only in the first call
+    # of this function, where we do not want to re-initialize the layout
+    # to avoid a screen flicker
+    _started_with_empty_loader = False
+
+    # update the progress layout
     _progress_layout.report(progress, remaining)
+
     # drop the layout when done so trezor.ui doesn't have to remain in memory
     if seconds == 0:
         _progress_layout = None
