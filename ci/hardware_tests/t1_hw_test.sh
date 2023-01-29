39b76967e85fa1fdda401f602415d046c3efd415
Martin Milata <martin@martinmilata.cz>
2023-01-29 01:30:51 +0100

    ci: use new firmware filenames

--- ci/hardware_tests/t1_hw_test.sh
+++ ci/hardware_tests/t1_hw_test.sh
@@ -17,5 +17,5 @@ set +a
 ./record_video.sh ${T1_CAMERA} ${CI_COMMIT_SHORT_SHA} start
 (cd ../.. && poetry install)
 poetry run python bootstrap.py t1
-poetry run python bootstrap.py t1 ../../trezor-*.bin
+poetry run python bootstrap.py t1 ../../firmware-T1*.bin
 poetry run pytest ../../tests/device_tests
