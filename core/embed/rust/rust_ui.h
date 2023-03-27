112c67cb305f242ca2483e899b506b9ea124a962
tychovrahe <brunam@seznam.cz>
2023-03-27 15:23:09 +0200

    refactor(core): remove unused functions
    [no changelog]

--- core/embed/rust/rust_ui.h
+++ core/embed/rust/rust_ui.h
@@ -2,11 +2,6 @@
 
 #include "common.h"
 
-void loader_uncompress_r(int32_t y_offset, uint16_t fg_color, uint16_t bg_color,
-                         uint16_t icon_color, int32_t progress,
-                         int32_t indeterminate, const uint8_t* icon_data,
-                         uint32_t icon_data_size);
-
 uint32_t screen_install_confirm(const char* vendor_str, uint8_t vendor_str_len,
                                 const char* version_str,
                                 const uint8_t* fingerprint,
--- core/embed/rust/src/ui/display/loader/mod.rs
+++ core/embed/rust/src/ui/display/loader/mod.rs
@@ -3,7 +3,6 @@ mod rectangular;
 mod starry;
 
 use crate::ui::display::{Color, Icon};
-use core::slice::from_raw_parts;
 
 #[cfg(feature = "model_tt")]
 use crate::ui::display::loader::circular::{
@@ -38,34 +37,4 @@ pub fn loader_indeterminate(
     icon: Option<(Icon, Color)>,
 ) {
     indeterminate(progress, y_offset, fg_color, bg_color, icon);
-}
-
-//TODO: remove when loader is no longer called from C
-#[no_mangle]
-pub extern "C" fn loader_uncompress_r(
-    y_offset: cty::int32_t,
-    fg_color: cty::uint16_t,
-    bg_color: cty::uint16_t,
-    icon_color: cty::uint16_t,
-    progress: cty::int32_t,
-    indeterminate: cty::int32_t,
-    icon_data: cty::uintptr_t,
-    icon_data_size: cty::uint32_t,
-) {
-    let fg = Color::from_u16(fg_color);
-    let bg = Color::from_u16(bg_color);
-    let ic_color = Color::from_u16(icon_color);
-
-    let i = if icon_data != 0 {
-        let data_slice = unsafe { from_raw_parts(icon_data as _, icon_data_size as _) };
-        Some((Icon::new(data_slice), ic_color))
-    } else {
-        None
-    };
-
-    if indeterminate == 0 {
-        loader(progress as _, y_offset as _, fg, bg, i);
-    } else {
-        loader_indeterminate(progress as _, y_offset as _, fg, bg, i);
-    }
 }
