eb72fb42415bc2c6c197012914c8f09c7e8f3adb
tychovrahe <brunam@seznam.cz>
2023-03-27 15:23:09 +0200

    refactor(core): remove unused functions
    [no changelog]

--- core/mocks/generated/trezorui.pyi
+++ core/mocks/generated/trezorui.pyi
@@ -12,10 +12,6 @@ class Display:
     FONT_NORMAL: int  # id of normal-width font
     FONT_BOLD: int  # id of bold-width font
     FONT_DEMIBOLD: int  # id of demibold font
-    TOIF_FULL_COLOR_BE: int  # full color big endian TOI image
-    TOIF_FULL_COLOR_LE: int  # full color little endian TOI image
-    TOIF_GRAYSCALE_EH: int  # grayscale even high TOI image
-    TOIF_FULL_COLOR_BE: int  # grayscale odd high TOI image
 
     def __init__(self) -> None:
         """
@@ -38,147 +34,6 @@ class Display:
         height h of color color.
         """
 
-    def bar_radius(
-        self,
-        x: int,
-        y: int,
-        w: int,
-        h: int,
-        fgcolor: int,
-        bgcolor: int | None = None,
-        radius: int | None = None,
-    ) -> None:
-        """
-        Renders a rounded bar at position (x,y = upper left corner) with width w
-        and height h of color fgcolor. Background is set to bgcolor and corners
-        are drawn with radius radius.
-        """
-
-    def toif_info(self, image: bytes) -> tuple[int, int, int]:
-        """
-        Returns tuple containing TOIF image dimensions: width, height, and
-        format Raises an exception for corrupted images.
-        """
-
-    def image(self, x: int, y: int, image: bytes) -> None:
-        """
-        Renders an image at position (x,y).
-        The image needs to be in Trezor Optimized Image Format (TOIF) -
-        full-color mode.
-        """
-
-    def avatar(
-        self, x: int, y: int, image: bytes, fgcolor: int, bgcolor: int
-    ) -> None:
-        """
-        Renders an avatar at position (x,y).
-        The image needs to be in Trezor Optimized Image Format (TOIF) -
-        full-color mode. Image needs to be of exactly AVATAR_IMAGE_SIZE x
-        AVATAR_IMAGE_SIZE pixels size.
-        """
-
-    def icon(
-        self, x: int, y: int, icon: bytes, fgcolor: int, bgcolor: int
-    ) -> None:
-        """
-        Renders an icon at position (x,y), fgcolor is used as foreground color,
-        bgcolor as background. The icon needs to be in Trezor Optimized Image
-        Format (TOIF) - gray-scale mode.
-        """
-
-    def loader(
-        self,
-        progress: int,
-        indeterminate: bool,
-        yoffset: int,
-        fgcolor: int,
-        bgcolor: int,
-        icon: bytes | None = None,
-        iconfgcolor: int | None = None,
-    ) -> None:
-        """
-        Renders a rotating loader graphic.
-        Progress determines its position (0-1000), fgcolor is used as foreground
-        color, bgcolor as background. When icon and iconfgcolor are provided, an
-        icon is drawn in the middle using the color specified in iconfgcolor.
-        Icon needs to be of exactly LOADER_ICON_SIZE x LOADER_ICON_SIZE pixels
-        size.
-        """
-
-    def print(self, text: str) -> None:
-        """
-        Renders text using 5x8 bitmap font (using special text mode).
-        """
-
-    def text(
-        self,
-        x: int,
-        y: int,
-        text: str,
-        font: int,
-        fgcolor: int,
-        bgcolor: int,
-        text_offset: int | None = None,
-        text_len: int | None = None,
-    ) -> None:
-        """
-        Renders left-aligned text at position (x,y) where x is left position and
-        y is baseline. Font font is used for rendering, fgcolor is used as
-        foreground color, bgcolor as background.
-        Arguments text_offset and text_len can be used to render a substring of
-        the text.
-        """
-
-    def text_center(
-        self,
-        x: int,
-        y: int,
-        text: str,
-        font: int,
-        fgcolor: int,
-        bgcolor: int,
-    ) -> None:
-        """
-        Renders text centered at position (x,y) where x is text center and y is
-        baseline. Font font is used for rendering, fgcolor is used as foreground
-        color, bgcolor as background.
-        """
-
-    def text_right(
-        self,
-        x: int,
-        y: int,
-        text: str,
-        font: int,
-        fgcolor: int,
-        bgcolor: int,
-    ) -> None:
-        """
-        Renders right-aligned text at position (x,y) where x is right position
-        and y is baseline. Font font is used for rendering, fgcolor is used as
-        foreground color, bgcolor as background.
-        """
-
-    def text_width(
-        self,
-        text: str,
-        font: int,
-        text_offset: int | None = None,
-        text_len: int | None = None,
-    ) -> int:
-        """
-        Returns a width of text in pixels. Font font is used for rendering.
-        Arguments text_offset and text_len can be used to render a substring of
-        the text.
-        """
-
-    def text_split(self, text: str, font: int, requested_width: int) -> int:
-        """
-        Returns how many characters of the string can be used before exceeding
-        the requested width. Tries to avoid breaking words if possible. Font
-        font is used for rendering.
-        """
-
     def orientation(self, degrees: int | None = None) -> int:
         """
         Sets display orientation to 0, 90, 180 or 270 degrees.
@@ -191,12 +46,6 @@ class Display:
         """
         Sets backlight intensity to the value specified in val.
         Call without the val parameter to just perform the read of the value.
-        """
-
-    def offset(self, xy: tuple[int, int] | None = None) -> tuple[int, int]:
-        """
-        Sets offset (x, y) for all subsequent drawing calls.
-        Call without the xy parameter to just perform the read of the value.
         """
 
     def save(self, prefix: str) -> None:
--- core/tests/test_trezor.ui.display.py
+++ core/tests/test_trezor.ui.display.py
@@ -14,59 +14,6 @@ class TestDisplay(unittest.TestCase):
     def test_bar(self):
         display.bar(0, 0, 10, 10, 0xFFFF)
 
-    def test_bar_radius(self):
-        display.bar_radius(0, 0, 10, 10, 0xFFFF, 0x0000, 16)
-
-    def test_image(self):
-        pass
-
-    def test_icon(self):
-        pass
-
-    def test_text(self):
-        display.text(120, 120, 'Test', 0, 0xFFFF, 0x0000)
-        display.text(120, 120, 'Test', 0, 0xFFFF, 0x0000, 2)
-        display.text(120, 120, 'Test', 0, 0xFFFF, 0x0000, 2, 1)
-        display.text(120, 120, 'Těst', 0, 0xFFFF, 0x0000, 2, 2)
-
-        display.text(120, 120, "ǑǑǑǑǑǑǑǑ", 0, 0xFFFF, 0x0000)
-        for off in (0, 2, 3, 8, 16):
-            display.text(120, 120, "ǑǑǑǑǑǑǑǑ", 0, 0xFFFF, 0x0000, off)
-            display.text(120, 120, "ǑǑǑǑǑǑǑǑ", 0, 0xFFFF, 0x0000, off, 0)
-
-        for off, tlen in ((2, 5), (2, 14), (3, 5), (3, 13), (8, 1), (8, 8)):
-            display.text(120, 120, "ǑǑǑǑǑǑǑǑ", 0, 0xFFFF, 0x0000, off, tlen)
-
-    def test_text_center(self):
-        display.text_center(120, 120, 'Test', 0, 0xFFFF, 0x0000)
-
-    def test_text_right(self):
-        display.text_right(120, 120, 'Test', 0, 0xFFFF, 0x0000)
-
-    def test_text_width(self):
-        display.text_width('Test', 0)
-        display.text_width('Test', 0, 2)
-        display.text_width('Test', 0, 2, 1)
-        display.text_width('Těst', 0, 2, 2)
-
-        display.text_width("ǑǑǑǑǑǑǑǑ", 0)
-        for off in (0, 2, 3, 8, 16):
-            display.text_width("ǑǑǑǑǑǑǑǑ", 0, off)
-            self.assertEqual(display.text_width("ǑǑǑǑǑǑǑǑ", 0, off, 0), 0)
-
-        for off, tlen in ((0, 8), (0, 16), (2, 5), (2, 14), (3, 5), (3, 13)):
-            display.text_width("ǑǑǑǑǑǑǑǑ", 0, off, tlen)
-
-        self.assertEqual(display.text_width("ǑǑǑǑǑǑǑǑ", 8), 0)
-        self.assertEqual(display.text_width("ǑǑǑǑǑǑǑǑ", 8, 1), 0)
-        self.assertEqual(display.text_width("ǑǑǑǑǑǑǑǑ", 8, 8), 0)
-        self.assertEqual(display.text_width("ǑǑǑǑǑǑǑǑ", 9), 0)
-        self.assertEqual(display.text_width("ǑǑǑǑǑǑǑǑ", 9, 1), 0)
-        self.assertEqual(display.text_width("ǑǑǑǑǑǑǑǑ", 15, 1), 0)
-
-    def test_loader(self):
-        display.loader(333, False, 0, 0xFFFF, 0x0000)
-
     def test_orientation(self):
         for o in [0, 90, 180, 270]:
             display.orientation(o)
@@ -74,14 +21,6 @@ class TestDisplay(unittest.TestCase):
     def test_backlight(self):
         for b in range(256):
             display.backlight(b)
-
-    def test_offset(self):
-        for x in range(-4, 5):
-            for y in range(-4, 5):
-                o = (x * 57, y * 57)
-                display.offset(o)
-                o2 = display.offset()
-                self.assertEqual(o, o2)
 
     def test_raw(self):
         pass
