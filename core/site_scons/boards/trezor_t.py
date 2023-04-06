d29870b1bb3eeac898b70398cb29a53693a2141e
tychovrahe <brunam@seznam.cz>
2023-04-06 16:04:44 +0200

    refactor(core): make emulated dma2d usable outside of emulator

--- core/site_scons/boards/trezor_t.py
+++ core/site_scons/boards/trezor_t.py
@@ -30,6 +30,12 @@ def configure(env, features_wanted, defines, sources):
         sources += ['embed/trezorhal/sbu.c', ]
         features_available.append("sbu")
 
+    if "dma2d" in features_wanted:
+        defines += ["USE_DMA2D", ]
+        sources += ['embed/trezorhal/dma2d.c', ]
+        sources += ['vendor/micropython/lib/stm32lib/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma2d.c', ]
+        features_available.append("dma2d")
+
     env.get('ENV')['TREZOR_BOARD'] = board
 
     return features_available
