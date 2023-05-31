319a665d0d1eec843ee744f599e932e48701ed58
matejcik <ja@matejcik.cz>
2023-05-31 14:47:22 +0200

    chore(core): add T2B1 vendor header data

--- core/embed/vendorheader/generate.sh
+++ core/embed/vendorheader/generate.sh
@@ -1,16 +1,24 @@
-BUILDVH=../../tools/build_vendorheader
-BINCTL=../../tools/headertool.py
+#!/usr/bin/env bash
 
 cd $(dirname $0)
 
-# construct all vendor headers
-for fn in *.json; do
-    name=$(echo $fn | sed 's/vendor_\(.*\)\.json/\1/')
-    $BUILDVH vendor_${name}.json vendor_${name}.toif vendorheader_${name}_unsigned.bin
-done
+BUILDVH=$(realpath ../../tools/build_vendorheader)
+BINCTL=$(realpath ../../tools/headertool.py)
 
-# sign dev and QA vendor header
-for name in unsafe qa_DO_NOT_SIGN; do
-    cp -a vendorheader_${name}_unsigned.bin vendorheader_${name}_signed_dev.bin
-    $BINCTL -D vendorheader_${name}_signed_dev.bin
+MODELS=(T2T1 T2B1)
+
+for MODEL in ${MODELS[@]}; do
+    cd $MODEL
+    # construct all vendor headers
+    for fn in *.json; do
+        name=$(echo $fn | sed 's/vendor_\(.*\)\.json/\1/')
+        $BUILDVH vendor_${name}.json vendor_${name}.toif vendorheader_${name}_unsigned.bin
+    done
+
+    # sign dev and QA vendor header
+    for name in unsafe qa_DO_NOT_SIGN; do
+        cp -a vendorheader_${name}_unsigned.bin vendorheader_${name}_signed_dev.bin
+        $BINCTL -D vendorheader_${name}_signed_dev.bin
+    done
+    cd ..
 done
