166959f21020381cc32cb0e16ab6cfe3854513ce
grdddj <jiri.musil06@seznam.cz>
2023-06-05 10:56:06 +0200

    feat(core/rust): use icon for displaying device name in welcome screen
    [no changelog]

--- core/embed/rust/src/ui/model_tr/component/welcome_screen.rs
+++ core/embed/rust/src/ui/model_tr/component/welcome_screen.rs
@@ -1,15 +1,11 @@
 use crate::ui::{
     component::{Component, Event, EventCtx, Never},
-    constant::MODEL_NAME,
-    display,
     geometry::{self, Offset, Rect},
 };
 
 use super::super::theme;
 
-const TEXT_BOTTOM_MARGIN: i16 = 2; // matching the homescreen label margin
 const ICON_TOP_MARGIN: i16 = 11;
-const MODEL_NAME_FONT: display::Font = display::Font::NORMAL;
 
 pub struct WelcomeScreen {
     area: Rect,
@@ -34,10 +30,9 @@ impl Component for WelcomeScreen {
     }
 
     fn paint(&mut self) {
-        display::text_center(
-            self.area.bottom_center() - Offset::y(TEXT_BOTTOM_MARGIN),
-            MODEL_NAME,
-            MODEL_NAME_FONT,
+        theme::ICON_DEVICE_NAME.draw(
+            self.area.bottom_center(),
+            geometry::BOTTOM_CENTER,
             theme::FG,
             theme::BG,
         );
@@ -54,6 +49,6 @@ impl Component for WelcomeScreen {
 impl crate::trace::Trace for WelcomeScreen {
     fn trace(&self, t: &mut dyn crate::trace::Tracer) {
         t.component("WelcomeScreen");
-        t.string("model_name", MODEL_NAME);
+        t.string("model_name", "Trezor Model R");
     }
 }
