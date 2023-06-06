cd722f512652ad652a50eae5b9865da6f6f0fc2e
tychovrahe <brunam@seznam.cz>
2023-06-06 11:35:25 +0200

    fix(python): fix T2B1 keys in trezorlib
    [no changelog]

--- python/src/trezorlib/firmware/models.py
+++ python/src/trezorlib/firmware/models.py
@@ -190,9 +190,9 @@ TREZOR_R = ModelKeys(
     bootloader_keys=[
         bytes.fromhex(key)
         for key in (
-            "549a45557008d5518a9a151dc6a3568cf73830a7fe419f2626d9f30d024b2bec",
-            "c16c7027f8a3962607bf24cdec2e3cd2344e1f6071e8260b3dda52b1a5107cb7",
-            "87180f933178b2832bee2d7046c7f4b98300ca7d7fb2e4567169c8730a1c4020",
+            "bf4e6f004fcb32cec683f22c88c1a86c1518c6de8ac97002d84a63bea3e375dd",
+            "d2def691c1e9d809d8190cf7af935c10688f68983479b4ee9abac19104878ec1",
+            "07c85134946bf89fa19bdc2c5e5ff9ce01296508ee0863d0ff6d63331d1a2516",
         )
     ],
     bootloader_sigs_needed=2,
