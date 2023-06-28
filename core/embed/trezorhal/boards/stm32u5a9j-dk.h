22946a362dc9b98bc8a65237a47e81d4e39e31e3
tychovrahe <brunam@seznam.cz>
2023-06-28 10:51:37 +0200

    feat(core): add support for STM32U5A9J-DK board
    [no changelog]

---
+++ core/embed/trezorhal/boards/stm32u5a9j-dk.h
@@ -0,0 +1,15 @@
+#ifndef STM32U5A9J_DK_H_
+#define STM32U5A9J_DK_H_
+
+#define HSE_16MHZ
+#define VDD_1V8 1
+
+//#define USE_I2C 1
+#define USE_RGB_COLORS 1
+#define USE_TOUCH 1
+//#define USE_SBU 1
+//#define USE_DISP_I8080_8BIT_DW 1
+
+#include "displays/dsi.h"
+
+#endif  // STM32U5A9J_DK_H_
