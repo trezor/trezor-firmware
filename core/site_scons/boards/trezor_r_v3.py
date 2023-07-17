6ae3f94e8fcd5ae88a9b44d8031f8171ecac4a18
grdddj <jiri.musil06@seznam.cz>
2023-07-17 16:07:31 +0200

    chore(style): apply black and isort style lints for site_scons python files
    [no changelog]

--- core/site_scons/boards/trezor_r_v3.py
+++ core/site_scons/boards/trezor_r_v3.py
@@ -5,11 +5,11 @@ from .stm32f4_common import stm32f4_common_files
 
 
 def configure(
-        env: dict,
-        features_wanted: list[str],
-        defines: list[str | tuple[str, str]],
-        sources: list[str],
-        paths: list[str],
+    env: dict,
+    features_wanted: list[str],
+    defines: list[str | tuple[str, str]],
+    sources: list[str],
+    paths: list[str],
 ) -> list[str]:
     features_available: list[str] = []
     hw_model = get_hw_model_as_number("T2B1")
@@ -21,9 +21,13 @@ def configure(
 
     stm32f4_common_files(env, defines, sources, paths)
 
-    env.get("ENV")['CPU_ASFLAGS'] = '-mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16'
-    env.get("ENV")['CPU_CCFLAGS'] = '-mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mtune=cortex-m4 '
-    env.get("ENV")['RUST_TARGET'] = 'thumbv7em-none-eabihf'
+    env.get("ENV")[
+        "CPU_ASFLAGS"
+    ] = "-mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16"
+    env.get("ENV")[
+        "CPU_CCFLAGS"
+    ] = "-mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mtune=cortex-m4 "
+    env.get("ENV")["RUST_TARGET"] = "thumbv7em-none-eabihf"
 
     defines += [mcu]
     defines += [f'TREZOR_BOARD=\\"boards/{board}\\"']
@@ -45,12 +49,12 @@ def configure(
 
     if "usb" in features_wanted:
         sources += [
-            'embed/trezorhal/stm32f4/usb.c',
-            'embed/trezorhal/stm32f4/usbd_conf.c',
-            'embed/trezorhal/stm32f4/usbd_core.c',
-            'embed/trezorhal/stm32f4/usbd_ctlreq.c',
-            'embed/trezorhal/stm32f4/usbd_ioreq.c',
-            'vendor/micropython/lib/stm32lib/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c',
+            "embed/trezorhal/stm32f4/usb.c",
+            "embed/trezorhal/stm32f4/usbd_conf.c",
+            "embed/trezorhal/stm32f4/usbd_core.c",
+            "embed/trezorhal/stm32f4/usbd_ctlreq.c",
+            "embed/trezorhal/stm32f4/usbd_ioreq.c",
+            "vendor/micropython/lib/stm32lib/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c",
         ]
         features_available.append("usb")
 
