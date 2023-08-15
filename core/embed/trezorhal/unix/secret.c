c0de11fbeb0ca753ed4def72bbc6143d32e0bd83
matejcik <ja@matejcik.cz>
2023-08-15 17:11:36 +0200

    fix(core): use stm32f4 implementation of secret.c for unix too
    (fixes bootloader_emu build because there was an error in the unix dummy
    impl)

--- core/embed/trezorhal/unix/secret.c
+++ core/embed/trezorhal/unix/secret.c
@@ -1,9 +1,1 @@
-#include "secret.h"
-
-void secret_write(uint8_t* data, uint32_t offset, uint32_t len) {}
-
-void secret_read(uint8_t* data, uint32_t offset, uint32_t len) {}
-
-secbool secret_wiped(void) { return secfalse; }
-
-void secret_erase(void) {}
+../stm32f4/secret.c
