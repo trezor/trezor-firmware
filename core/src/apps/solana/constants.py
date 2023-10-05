b58005877296f3d985375672fc2476330528ae32
Zsolt Albert <contact@zsoltalbert.eu>
2023-10-05 17:11:30 +0200

    Updated with solana defaults

--- core/src/apps/solana/constants.py
+++ core/src/apps/solana/constants.py
@@ -6,3 +6,6 @@ ADDRESS_SIG = const(0)
 ADDRESS_SIG_READ_ONLY = const(1)
 ADDRESS_READ_ONLY = const(2)
 ADDRESS_RW = const(3)
+
+SOLANA_BASE_FEE_LAMPORTS    = const(5000)
+SOLANA_CU_LIMIT             = const(200000)
