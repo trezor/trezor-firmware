adb6c8d94a9c2a24a90ae7cb8ad364bcea16aa32
Zsolt Albert <contact@zsoltalbert.eu>
2023-10-12 14:35:39 +0200

    BufferReader->read changed to read_memoryview

--- core/src/apps/solana/transaction/parse.py
+++ core/src/apps/solana/transaction/parse.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 
 from trezor.crypto import base58
+from apps.common.readers import read_uint32_le, read_uint64_le, read_compact_size
 
 from ..constants import (
     ADDRESS_READ_ONLY,
@@ -42,28 +43,13 @@ def parseHeader(serialized_tx: BufferReader) -> tuple[bool, int, int, int, int]:
         num_read_only_addresses,
     )
 
-
-# TODO SOL: Shall be considerd to use CompactU16 instead of VarInt
-def parseVarInt(serialized_tx: BufferReader) -> int:
-    value = 0
-    shift = 0
-    while serialized_tx.remaining_count():
-        B = serialized_tx.get()
-        value += (B & 0b01111111) << shift
-        shift += 7
-        if B & 0b10000000 == 0:
-            return value
-
-    raise BufferError
-
-
 def parseAddresses(
     serialized_tx: BufferReader,
     num_required_signatures: int,
     num_signature_read_only_addresses: int,
     num_read_only_addresses: int,
 ) -> list[Address]:
-    num_of_addresses = parseVarInt(serialized_tx)
+    num_of_addresses = read_compact_size(serialized_tx)
 
     assert (
         num_of_addresses
@@ -97,9 +83,12 @@ def parseAddresses(
     return addresses
 
 
+# data = r.read_memoryview(4)
+# return int.from_bytes(data, "little")
+
 def parseBlockHash(serialized_tx: BufferReader) -> bytes:
     assert serialized_tx.remaining_count() >= 32
-    return serialized_tx.read(32)
+    return bytes(serialized_tx.read_memoryview(32))
 
 
 def parseInstructions(
@@ -108,21 +97,21 @@ def parseInstructions(
     serialized_tx: BufferReader,
     # [program_index, instruction_id, accounts, instruction_data]
 ) -> list[tuple[int, int, list[int], bytes]]:
-    num_of_instructions = parseVarInt(serialized_tx)
+    num_of_instructions = read_compact_size(serialized_tx)
 
     instructions: list[tuple[int, int, list[int], bytes]] = []
 
     for _ in range(num_of_instructions):
         program_index = serialized_tx.get()
         program_id = base58.encode(addresses[program_index][0])
-        num_of_accounts = parseVarInt(serialized_tx)
+        num_of_accounts = read_compact_size(serialized_tx)
         accounts: list[int] = []
         for _ in range(num_of_accounts):
             assert serialized_tx.remaining_count() > 0
             account_index = serialized_tx.get()
             accounts.append(account_index)
 
-        data_length = parseVarInt(serialized_tx)
+        data_length = read_compact_size(serialized_tx)
 
         instruction_id_format = get_instruction_id_format(program_id)
         instruction_id_length = instruction_id_format["length"]
@@ -135,41 +124,19 @@ def parseInstructions(
             instruction_id = 0
         else:
             instruction_id = int.from_bytes(
-                serialized_tx.read(instruction_id_length), "little"
+                serialized_tx.read_memoryview(instruction_id_length), "little"
             )
 
-        instruction_data = serialized_tx.read(
-            max(0, data_length - instruction_id_length)
-        )
+        instruction_data = serialized_tx.read_memoryview(data_length - instruction_id_length)
 
         instructions.append((program_index, instruction_id, accounts, instruction_data))
 
     return instructions
 
 
-def parseU32(serialized_tx: BufferReader) -> int:
-    assert serialized_tx.remaining_count() >= 4
-    return int.from_bytes(serialized_tx.read(4), "little")
-
-
-def parseU64(serialized_tx: BufferReader) -> int:
-    assert serialized_tx.remaining_count() >= 8
-    return int.from_bytes(serialized_tx.read(8), "little")
-
-
-def parseI32(serialized_tx: BufferReader) -> int:
-    assert serialized_tx.remaining_count() >= 4
-    return int.from_bytes(serialized_tx.read(4), "little")
-
-
-def parseI64(serialized_tx: BufferReader) -> int:
-    assert serialized_tx.remaining_count() >= 8
-    return int.from_bytes(serialized_tx.read(8), "little")
-
-
 def parsePubkey(serialized_tx: BufferReader) -> bytes:
     assert serialized_tx.remaining_count() >= 32
-    return serialized_tx.read(32)
+    return bytes(serialized_tx.read_memoryview(32))
 
 
 def parseEnum(serialized_tx: BufferReader) -> int:
@@ -179,26 +146,26 @@ def parseEnum(serialized_tx: BufferReader) -> int:
 
 def parseString(serialized_tx: BufferReader) -> str:
     # TODO SOL: validation shall be checked (length is less than 2^32 or even less)
-    length = parseU64(serialized_tx)
+    length = read_uint64_le(serialized_tx)
     assert serialized_tx.remaining_count() >= length
-    return serialized_tx.read(length).decode("utf-8")
+    return bytes(serialized_tx.read_memoryview(length)).decode("utf-8")
 
 
 def parseMemo(serialized_tx: BufferReader) -> str:
-    return serialized_tx.read(serialized_tx.remaining_count()).decode("utf-8")
+    return bytes(serialized_tx.read_memoryview(serialized_tx.remaining_count())).decode("utf-8")
 
 
 def parseProperty(reader: BufferReader, type: str) -> str | int | bytes:
     if type == "u8":
         return reader.get()
     elif type == "u32":
-        return parseU32(reader)
+        return read_uint32_le(reader)
     elif type == "u64":
-        return parseU64(reader)
+        return read_uint64_le(reader)
     elif type == "i32":
-        return parseI32(reader)
+        return read_uint32_le(reader)
     elif type == "i64":
-        return parseI64(reader)
+        return read_uint64_le(reader)
     elif type in ("pubkey", "authority"):
         return parsePubkey(reader)
     elif type == "enum":
@@ -218,17 +185,17 @@ def parseLut(
     address_table_lookup_rw_addresses = []
     address_table_lookup_ro_addresses = []
 
-    address_table_lookups_count = parseVarInt(serialized_tx)
+    address_table_lookups_count = read_compact_size(serialized_tx)
     for _ in range(address_table_lookups_count):
         account = parsePubkey(serialized_tx)
 
-        table_rw_indexes_count = parseVarInt(serialized_tx)
+        table_rw_indexes_count = read_compact_size(serialized_tx)
         for _ in range(table_rw_indexes_count):
             address_table_lookup_rw_addresses.append(
                 (account, serialized_tx.get(), ADDRESS_RW)
             )
 
-        table_ro_indexes_count = parseVarInt(serialized_tx)
+        table_ro_indexes_count = read_compact_size(serialized_tx)
         for _ in range(table_ro_indexes_count):
             address_table_lookup_ro_addresses.append(
                 (account, serialized_tx.get(), ADDRESS_READ_ONLY)
