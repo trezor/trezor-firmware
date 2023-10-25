fd93c86278676cf67b186afe7bae15548652a462
cepetr <petr@cerninovi.cz>
2023-10-25 13:19:18 +0200

    fixup! feat(core/bootloader): make firmware flashing more safe

--- core/embed/trezorhal/flash.h
+++ core/embed/trezorhal/flash.h
@@ -58,7 +58,7 @@ secbool flash_write_word(uint16_t sector, uint32_t offset, uint32_t data);
 uint32_t flash_wait_and_clear_status_flags(void);
 
 // Erases the single sector in the designated flash area
-// The 'offset' parameter indicates the sector's location within the flash area
+// The 'offset' parameter must indicate the relative sector offset within the flash area
 // If 'offset' is outside the bounds of the flash area, '_bytes_erase' is set to
 // 0 otherwise, '_bytes_erased' is set to the size of the erased sector
 secbool flash_area_erase_partial(const flash_area_t *area, uint32_t offset,
