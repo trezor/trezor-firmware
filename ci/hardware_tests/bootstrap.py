2dcf953cc12b631a59a54673225c8a9c1d1c3eb2
Martin Milata <martin@martinmilata.cz>
2023-11-24 15:51:47 +0100

    test

--- ci/hardware_tests/bootstrap.py
+++ ci/hardware_tests/bootstrap.py
@@ -6,18 +6,18 @@ from device.tt import TrezorT
 
 
 def main(model: str, file: str = None):
-    t1 = TrezorOne(
-        os.environ["T1_UHUB_LOCATION"],
-        os.environ["T1_ARDUINO_SERIAL"],
-        os.environ["T1_UHUB_PORT"],
-    )
+#    t1 = TrezorOne(
+#        os.environ["T1_UHUB_LOCATION"],
+#        os.environ["T1_ARDUINO_SERIAL"],
+#        os.environ["T1_UHUB_PORT"],
+#    )
     tt = TrezorT(os.environ["TT_UHUB_LOCATION"], os.environ["TT_UHUB_PORT"])
 
     if model == "t1":
         tt.power_off()
-        path = t1.update_firmware(file)
+        #path = t1.update_firmware(file)
     elif model == "tt":
-        t1.power_off()
+        #t1.power_off()
         path = tt.update_firmware(file)
     else:
         raise ValueError("Unknown Trezor model.")
