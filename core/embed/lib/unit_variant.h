8313c15f343fddcc6d62d5fb976fe34cdec7452c
tychovrahe <brunam@seznam.cz>
2024-01-12 15:42:01 +0100

    feat(core): allow SD card hotswap based on production date for Model T

--- core/embed/lib/unit_variant.h
+++ core/embed/lib/unit_variant.h
@@ -9,4 +9,6 @@ bool unit_variant_present(void);
 uint8_t unit_variant_get_color(void);
 bool unit_variant_get_btconly(void);
 
+bool unit_variant_is_sd_hotswap_enabled(void);
+
 #endif  //_UNIT_VARIANT_H
