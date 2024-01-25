931fb4be0b5502fb010489ace79462ec7861d24b
matejcik <ja@matejcik.cz>
2024-01-25 11:08:46 +0100

    chore(python): drop source support for Python 3.6 and 3.7

--- python/tools/trezorctl_script_client.py
+++ python/tools/trezorctl_script_client.py
@@ -86,7 +86,7 @@ def get_address() -> str:
     args = """
         trezorctl --script get-address -n "m/49h/0h/0h/0/0"
     """.strip()
-    p = subprocess.Popen(  # type: ignore [No overloads for "__new__" match the provided arguments]
+    p = subprocess.Popen(
         args,
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
