6ba65802b622b12da3b04a09290f32bb1af753cf
Martin Milata <martin@martinmilata.cz>
2024-01-30 01:54:02 +0100

    fix(core/bitcoin): display descriptors for taproot XPUBs

--- core/src/apps/bitcoin/common.py
+++ core/src/apps/bitcoin/common.py
@@ -212,3 +212,49 @@ def format_fee_rate(
         shortcut = ""
 
     return f"{fee_rate_formatted} sat{shortcut}/{'v' if coin.segwit else ''}B"
+
+
+# function copied from python/src/trezorlib/tools.py
+def descriptor_checksum(desc: str) -> str:
+    def _polymod(c: int, val: int) -> int:
+        c0 = c >> 35
+        c = ((c & 0x7FFFFFFFF) << 5) ^ val
+        if c0 & 1:
+            c ^= 0xF5DEE51989
+        if c0 & 2:
+            c ^= 0xA9FDCA3312
+        if c0 & 4:
+            c ^= 0x1BAB10E32D
+        if c0 & 8:
+            c ^= 0x3706B1677A
+        if c0 & 16:
+            c ^= 0x644D626FFD
+        return c
+
+    INPUT_CHARSET = "0123456789()[],'/*abcdefgh@:$%{}IJKLMNOPQRSTUVWXYZ&+-.;<=>?!^_|~ijklmnopqrstuvwxyzABCDEFGH`#\"\\ "
+    CHECKSUM_CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+
+    c = 1
+    cls = 0
+    clscount = 0
+    for ch in desc:
+        pos = INPUT_CHARSET.find(ch)
+        if pos == -1:
+            return ""
+        c = _polymod(c, pos & 31)
+        cls = cls * 3 + (pos >> 5)
+        clscount += 1
+        if clscount == 3:
+            c = _polymod(c, cls)
+            cls = 0
+            clscount = 0
+    if clscount > 0:
+        c = _polymod(c, cls)
+    for j in range(0, 8):
+        c = _polymod(c, 0)
+    c ^= 1
+
+    ret = [""] * 8
+    for j in range(0, 8):
+        ret[j] = CHECKSUM_CHARSET[(c >> (5 * (7 - j))) & 31]
+    return "".join(ret)
