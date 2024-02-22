458a7bc538fa0e289e083ea16ce0c358c576ac67
tychovrahe <brunam@seznam.cz>
2024-02-22 22:24:56 +0100

    feat(core): improve flexibility of combine script, add combine fw make targets
    [no changelog]

--- core/embed/models/model_T2B1_layout.c
+++ core/embed/models/model_T2B1_layout.c
@@ -6,16 +6,18 @@ const flash_area_t STORAGE_AREAS[STORAGE_AREAS_COUNT] = {
         .num_subareas = 1,
         .subarea[0] =
             {
-                .first_sector = 4,
-                .num_sectors = 1,
+                .first_sector = STORAGE_1_SECTOR_START,
+                .num_sectors =
+                    STORAGE_1_SECTOR_END - STORAGE_1_SECTOR_START + 1,
             },
     },
     {
         .num_subareas = 1,
         .subarea[0] =
             {
-                .first_sector = 16,
-                .num_sectors = 1,
+                .first_sector = STORAGE_2_SECTOR_START,
+                .num_sectors =
+                    STORAGE_2_SECTOR_END - STORAGE_2_SECTOR_START + 1,
             },
     },
 };
@@ -24,8 +26,9 @@ const flash_area_t BOARDLOADER_AREA = {
     .num_subareas = 1,
     .subarea[0] =
         {
-            .first_sector = 0,
-            .num_sectors = 3,
+            .first_sector = BOARDLOADER_SECTOR_START,
+            .num_sectors =
+                BOARDLOADER_SECTOR_END - BOARDLOADER_SECTOR_START + 1,
         },
 };
 
@@ -51,8 +54,8 @@ const flash_area_t BOOTLOADER_AREA = {
     .num_subareas = 1,
     .subarea[0] =
         {
-            .first_sector = 5,
-            .num_sectors = 1,
+            .first_sector = BOOTLOADER_SECTOR_START,
+            .num_sectors = BOOTLOADER_SECTOR_END - BOOTLOADER_SECTOR_START + 1,
         },
 };
 
@@ -60,13 +63,14 @@ const flash_area_t FIRMWARE_AREA = {
     .num_subareas = 2,
     .subarea[0] =
         {
-            .first_sector = 6,
-            .num_sectors = 6,
+            .first_sector = FIRMWARE_SECTOR_START,
+            .num_sectors = FIRMWARE_SECTOR_END - FIRMWARE_SECTOR_START + 1,
         },
     .subarea[1] =
         {
-            .first_sector = 17,
-            .num_sectors = 7,
+            .first_sector = FIRMWARE_P2_SECTOR_START,
+            .num_sectors =
+                FIRMWARE_P2_SECTOR_END - FIRMWARE_P2_SECTOR_START + 1,
         },
 };
 
