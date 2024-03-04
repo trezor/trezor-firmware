f6adb53f732e32d71501df7f597ff5a32e6d4967
Martin Milata <martin@martinmilata.cz>
2024-03-04 01:02:05 +0100

    fix(core): T3T1 emulator
    [no changelog]

--- core/embed/trezorhal/boards/board-unix.h
+++ core/embed/trezorhal/boards/board-unix.h
@@ -14,6 +14,8 @@
 #define USE_SD_CARD 1
 #define USE_SBU 1
 #define USE_RGB_COLORS 1
+#define USE_BACKLIGHT 1
+#define USE_OPTIGA 1
 #endif
 
 #ifdef TREZOR_MODEL_1
@@ -26,12 +28,14 @@
 #define USE_OPTIGA 1
 #endif
 
-#include "display-unix.h"
-
-#ifdef TREZOR_MODEL_R
-#define USE_BUTTON 1
-#elif TREZOR_MODEL_T
+#ifdef TREZOR_MODEL_T3T1
 #define USE_TOUCH 1
+#define USE_SD_CARD 1
+#define USE_SBU 1
+#define USE_RGB_COLORS 1
+#define USE_BACKLIGHT 1
 #endif
+
+#include "display-unix.h"
 
 #endif  //_BOARD_UNIX_H
--- core/src/apps/debug/__init__.py
+++ core/src/apps/debug/__init__.py
@@ -171,7 +171,11 @@ if __debug__:
         debug_events.last_event += 1
 
         # TT click on specific coordinates, with possible hold
-        if x is not None and y is not None and utils.INTERNAL_MODEL in ("T2T1", "D001"):
+        if (
+            x is not None
+            and y is not None
+            and utils.INTERNAL_MODEL in ("T2T1", "T3T1", "D001")
+        ):
             click_chan.publish((debug_events.last_event, x, y, msg.hold_ms))
         # TR press specific button
         elif msg.physical_button is not None and utils.INTERNAL_MODEL in ("T2B1",):
