2ba42d716e872dfb1bf8cbe40f723998c362aa68
Martin Milata <martin@martinmilata.cz>
2024-03-04 12:05:19 +0100

    feat(core): init T3T1 UI layouts: meta

--- core/embed/rust/src/ui/constant.rs
+++ core/embed/rust/src/ui/constant.rs
@@ -9,5 +9,5 @@
 pub use super::model_mercury::constant::*;
 #[cfg(all(feature = "model_tr", not(feature = "model_tt")))]
 pub use super::model_tr::constant::*;
-#[cfg(feature = "model_tt")]
+#[cfg(all(feature = "model_tt", not(feature = "model_mercury")))]
 pub use super::model_tt::constant::*;
