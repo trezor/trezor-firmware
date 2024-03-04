9b1f431c90c560cad292552b68ce4fc2f61ac276
Martin Milata <martin@martinmilata.cz>
2024-03-04 12:05:19 +0100

    feat(core): init T3T1 UI layouts: meta

--- core/site_scons/site_tools/micropython/__init__.py
+++ core/site_scons/site_tools/micropython/__init__.py
@@ -46,6 +46,7 @@ def generate(env):
         optiga = env["optiga"]
         layout_tt = env["ui_layout"] == "UI_LAYOUT_TT"
         layout_tr = env["ui_layout"] == "UI_LAYOUT_TR"
+        layout_mercury = env["ui_layout"] == "UI_LAYOUT_MERCURY"
         thp = env["thp"]
         interim = f"{target[:-4]}.i"  # replace .mpy with .i
         sed_scripts = [
@@ -54,6 +55,7 @@ def generate(env):
             rf"-e 's/utils\.USE_OPTIGA/{optiga}/g'",
             rf"-e 's/utils\.UI_LAYOUT == \"TT\"/{layout_tt}/g'",
             rf"-e 's/utils\.UI_LAYOUT == \"TR\"/{layout_tr}/g'",
+            rf"-e 's/utils\.UI_LAYOUT == \"MERCURY\"/{layout_mercury}/g'",
             rf"-e 's/utils\.USE_THP/{thp}/g'",
             r"-e 's/if TYPE_CHECKING/if False/'",
             r"-e 's/import typing/# \0/'",
