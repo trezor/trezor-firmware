0565eba3a78e23bb2c1cb5cfd20bfdae59c4aa3b
matejcik <ja@matejcik.cz>
2024-04-02 15:49:33 +0200

    refactor(core): improve safety of translation blobs
    * the public interface to Translations is now completely safe
    * it is more obvious that `map_translated` needs to work the way it does
    * documentation is improved

--- core/embed/rust/src/translations/translated_string.rs
+++ core/embed/rust/src/translations/translated_string.rs
@@ -10,16 +10,27 @@ impl TranslatedString {
             .unwrap_or(self.untranslated())
     }
 
+    /// Maps the translated string to a value using a closure.
+    ///
+    /// # Safety
+    ///
+    /// This is the only safe way to access a reference to the flash data. This
+    /// function guarantees that the reference is valid throughout the
+    /// closure, but as soon as the closure returns, the lock held on flash
+    /// data is released. This means that translations could get deinited
+    /// and rewritten, invalidating the reference.
+    ///
+    /// To guarantee that the reference does not escape the closure, we use a
+    /// HRTB of the closure's argument, which ensures that the lifetime of
+    /// the reference is too short to store it outside in any form.
     pub fn map_translated<F, T>(self, fun: F) -> T
     where
         F: for<'a> FnOnce(&'a str) -> T,
-        T: 'static,
     {
         // SAFETY: The bound on F _somehow_ ensures that the reference cannot escape
         // the closure. (I don't understand how, but it does), see soundness test below.
-        // For good measure, we limit the return value to 'static.
-        let translations = unsafe { super::flash::get() };
-        fun(self.translate(translations))
+        let translations = unwrap!(super::flash::get());
+        fun(self.translate(translations.as_ref()))
     }
 
     pub const fn as_tstring(self) -> TString<'static> {
