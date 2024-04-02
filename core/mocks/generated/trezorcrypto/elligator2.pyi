ad07985a5b5465df7e11ee5140541495af4dd77c
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2024-04-02 15:31:24 +0200

    feat(core): add elligator2 python bindings
    [no changelog]

---
+++ core/mocks/generated/trezorcrypto/elligator2.pyi
@@ -0,0 +1,8 @@
+from typing import *
+
+
+# extmod/modtrezorcrypto/modtrezorcrypto-elligator2.h
+def map_to_curve25519(input: bytes) -> bytes:
+    """
+    Maps a 32-byte input to a curve25519 point.
+    """
