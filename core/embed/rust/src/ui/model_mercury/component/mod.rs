26da0e1a2dc1b94eb24e46b912e67ecdd9d7545a
obrusvit <vit.obrusnik@gmail.com>
2024-04-16 23:22:15 +0200

    fix(core): rebase on drawlib
    mercury theme.rs needs some cleanup after this action

--- core/embed/rust/src/ui/model_mercury/component/mod.rs
+++ core/embed/rust/src/ui/model_mercury/component/mod.rs
@@ -1,3 +1,5 @@
+#[cfg(feature = "translations")]
+mod address_details;
 pub mod bl_confirm;
 mod button;
 #[cfg(feature = "translations")]
@@ -10,7 +12,15 @@ mod vertical_menu;
 mod fido_icons;
 mod error;
 mod frame;
+#[cfg(feature = "micropython")]
+mod homescreen;
+mod keyboard;
 mod loader;
+#[cfg(feature = "translations")]
+mod number_input;
+#[cfg(feature = "translations")]
+mod page;
+mod progress;
 mod result;
 mod scroll;
 mod share_words;
@@ -18,14 +28,21 @@ mod simple_page;
 mod swipe;
 mod welcome_screen;
 
+#[cfg(feature = "translations")]
+pub use address_details::AddressDetails;
 pub use button::{
-    Button, ButtonContent, ButtonMsg, ButtonStyle, ButtonStyleSheet, CancelInfoConfirmMsg, IconText,
+    Button, ButtonContent, ButtonMsg, ButtonStyle, ButtonStyleSheet, CancelConfirmMsg,
+    CancelInfoConfirmMsg, IconText,
 };
+#[cfg(feature = "translations")]
+pub use coinjoin_progress::CoinJoinProgress;
+pub use dialog::{Dialog, DialogMsg, IconDialog};
 pub use error::ErrorScreen;
+pub use fido::{FidoConfirm, FidoMsg};
+pub use footer::Footer;
 pub use frame::{Frame, FrameMsg};
 #[cfg(feature = "micropython")]
 pub use homescreen::{check_homescreen_format, Homescreen, HomescreenMsg, Lockscreen};
-pub use footer::Footer;
 pub use keyboard::{
     bip39::Bip39Input,
     mnemonic::{MnemonicInput, MnemonicKeyboard, MnemonicKeyboardMsg},
@@ -35,6 +52,11 @@ pub use keyboard::{
     word_count::{SelectWordCount, SelectWordCountMsg},
 };
 pub use loader::{Loader, LoaderMsg, LoaderStyle, LoaderStyleSheet};
+#[cfg(feature = "translations")]
+pub use number_input::{NumberInputDialog, NumberInputDialogMsg};
+#[cfg(feature = "translations")]
+pub use page::ButtonPage;
+pub use progress::Progress;
 pub use result::{ResultFooter, ResultScreen, ResultStyle};
 pub use scroll::ScrollBar;
 pub use share_words::ShareWords;
