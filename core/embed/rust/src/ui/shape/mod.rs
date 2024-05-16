e23f0d210671fc12ec82a7278a20a7f1d3df0180
cepetr <petr@cerninovi.cz>
2024-05-16 15:59:21 +0200

    feat(core/rust): introduce ImageBuffer for advanced drawing
    [no changelog]

--- core/embed/rust/src/ui/shape/mod.rs
+++ core/embed/rust/src/ui/shape/mod.rs
@@ -21,7 +21,9 @@ pub use bitmap::{Bitmap, BitmapFormat, BitmapView};
 #[cfg(feature = "ui_blurring")]
 pub use blur::Blurring;
 pub use cache::drawing_cache::DrawingCache;
-pub use canvas::{BasicCanvas, Canvas, Mono8Canvas, Rgb565Canvas, Rgba8888Canvas, Viewport};
+pub use canvas::{
+    BasicCanvas, Canvas, CanvasBuilder, Mono8Canvas, Rgb565Canvas, Rgba8888Canvas, Viewport,
+};
 pub use circle::Circle;
 pub use display::render_on_display;
 #[cfg(feature = "ui_jpeg_decoder")]
@@ -30,4 +32,4 @@ pub use qrcode::QrImage;
 pub use render::{DirectRenderer, ProgressiveRenderer, Renderer};
 pub use text::Text;
 pub use toif::ToifImage;
-pub use utils::PI4;
+pub use utils::ImageBuffer;
