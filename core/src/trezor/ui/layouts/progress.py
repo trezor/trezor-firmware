5f30194a348315169bc09500cc052c9d3242db65
tychovrahe <brunam@seznam.cz>
2024-05-23 13:21:24 +0200

    fix(core): prevent unnecessary display refreshing
    [no changelog]

--- core/src/trezor/ui/layouts/progress.py
+++ core/src/trezor/ui/layouts/progress.py
@@ -36,8 +36,8 @@ class RustProgress:
         self.layout = layout
         ui.backlight_fade(ui.BacklightLevels.DIM)
         self.layout.attach_timer_fn(self.set_timer)
-        self.layout.paint()
-        ui.refresh()
+        if self.layout.paint():
+            ui.refresh()
         ui.backlight_fade(ui.BacklightLevels.NORMAL)
 
     def set_timer(self, token: int, deadline: int) -> None:
@@ -46,8 +46,8 @@ class RustProgress:
     def report(self, value: int, description: str | None = None):
         msg = self.layout.progress_event(value, description or "")
         assert msg is None
-        self.layout.paint()
-        ui.refresh()
+        if self.layout.paint():
+            ui.refresh()
 
 
 def progress(
