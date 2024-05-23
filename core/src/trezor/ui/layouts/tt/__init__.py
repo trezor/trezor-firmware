9195c1ee31360d7827747616c24d5b215511b7d6
tychovrahe <brunam@seznam.cz>
2024-05-23 13:21:24 +0200

    fix(core): prevent unnecessary display refreshing
    [no changelog]

--- core/src/trezor/ui/layouts/tt/__init__.py
+++ core/src/trezor/ui/layouts/tt/__init__.py
@@ -58,7 +58,8 @@ class RustLayout(LayoutParentType[T]):
 
         painted = self.layout.paint()
 
-        ui.refresh()
+        if painted:
+            ui.refresh()
         if storage_cache.homescreen_shown is not None and painted:
             storage_cache.homescreen_shown = None
 
