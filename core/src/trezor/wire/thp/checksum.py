ed580cdef8a97a71830f1e6d81a9c18e43e5be2f
M1nd3r <petrsedlacek.km@seznam.cz>
2024-05-28 12:27:43 +0200

    fixup! feat(core): add THP checksum computation and validation [no changelog]

--- core/src/trezor/wire/thp/checksum.py
+++ core/src/trezor/wire/thp/checksum.py
@@ -1,19 +1,18 @@
 from micropython import const
 
-from trezor import utils
 from trezor.crypto import crc
 
 CHECKSUM_LENGTH = const(4)
 
 
-def compute(data: bytes | utils.BufferType) -> bytes:
+def compute(data: bytes) -> bytes:
     """
     Returns a CRC-32 checksum of the provided `data`.
     """
     return crc.crc32(data).to_bytes(CHECKSUM_LENGTH, "big")
 
 
-def is_valid(checksum: bytes | utils.BufferType, data: bytes) -> bool:
+def is_valid(checksum: bytes, data: bytes) -> bool:
     """
     Checks whether the CRC-32 checksum of the `data` is the same
     as the checksum provided in `checksum`.
