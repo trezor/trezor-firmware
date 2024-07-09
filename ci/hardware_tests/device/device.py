e2d5c854304a7ab57dc132d5f5cdf2478ac6b227
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-07-09 15:43:55 +0300

    Refactor subprocess calls to avoid security risk

--- ci/hardware_tests/device/device.py
+++ ci/hardware_tests/device/device.py
@@ -15,10 +15,9 @@ class Device:
         print(msg, flush=True, file=sys.stderr)
 
     def run_trezorctl(self, cmd: str, **kwargs):
-        full_cmd = "trezorctl "
-        full_cmd += cmd
-        self.log(f"[software/trezorctl] Running '{full_cmd}'")
-        return run(full_cmd, shell=True, check=True, **kwargs)
+        full_cmd = ["trezorctl"] + cmd.split()
+        self.log(f"[software/trezorctl] Running '{' '.join(full_cmd)}'")
+        return subprocess.run(full_cmd, check=True, **kwargs)
 
     def check_model(self, model=None):
         res = self.run_trezorctl("list", capture_output=True, text=True)
@@ -45,21 +44,15 @@ class Device:
     def power_on(self):
         self.now()
         self.log("[hardware/usb] Turning power on...")
-        run(
-            f"uhubctl {self._hub()} -p {self.device_port} -a on",
-            shell=True,
-            check=True,
-        )
+        hub_command = ["uhubctl", self._hub(), "-p", str(self.device_port), "-a", "on"]
+        subprocess.run(hub_command, check=True)
         self.wait(3)
 
     def power_off(self):
         self.now()
         self.log("[hardware/usb] Turning power off...")
-        run(
-            f"uhubctl {self._hub()} -p {self.device_port} -r 5 -a off",
-            shell=True,
-            check=True,
-        )
+        hub_command = ["uhubctl", self._hub(), "-p", str(self.device_port), "-r", "5", "-a", "off"]
+        subprocess.run(hub_command, check=True)
         self.wait(3)
 
     def touch(self, location, action):
