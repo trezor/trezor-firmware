ff226836fb64762cda4d7e0bf1d009b912528fd0
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2024-09-01 18:01:54 +0200

    refactor(crypto): introduce symbolic constants for size optimization

--- crypto/options.h
+++ crypto/options.h
@@ -23,6 +23,18 @@
 #ifndef __OPTIONS_H__
 #define __OPTIONS_H__
 
+#ifndef OPTIMIZE_SIZE
+#define OPTIMIZE_SIZE 1
+#endif
+
+#ifndef OPTIMIZE_SIZE_BLAKE2S
+#define OPTIMIZE_SIZE_BLAKE2S 0
+#endif
+
+#ifndef OPTIMIZE_SIZE_BLAKE2B
+#define OPTIMIZE_SIZE_BLAKE2B OPTIMIZE_SIZE
+#endif
+
 // use precomputed Curve Points (some scalar multiples of curve base point G)
 #ifndef USE_PRECOMPUTED_CP
 #define USE_PRECOMPUTED_CP 1
