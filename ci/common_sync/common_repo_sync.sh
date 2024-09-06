950061157fcb83e71b1ff1e58a30d325977b578f
vdovhanych <dovhanych@me.com>
2024-09-06 09:24:04 +0200

    ci: fix trezor-common sync gh worfklow

--- ci/common_sync/common_repo_sync.sh
+++ ci/common_sync/common_repo_sync.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-GH_USER="$GITHUB_BOT_USERNAME"
-GH_TOKEN="$GITHUB_BOT_TOKEN"
+COMMON_REPO_TOKEN="$BOT_TOKEN"
+COMMON_REPO_USER="$BOT_USERNAME"
 
 # checkout to temporary branch
 git checkout -B tmp
 
 # setup trezor-common remote
-git remote add sync-common https://$GH_USER:$GH_TOKEN@github.com/trezor/trezor-common.git 2>/dev/null
+git remote add sync-common https://$COMMON_REPO_USER:$COMMON_REPO_TOKEN@github.com/trezor/trezor-common.git 2>/dev/null
 
 # top commit in HEAD before monorepo was introduced
 TOP_COMMIT_IN_COMMON=893fd219d4a01bcffa0cd9cfa631856371ec5aa9
