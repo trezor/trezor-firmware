0b8d42358ed54a8fb6a0003021772123975616d0
tychovrahe <brunam@seznam.cz>
2024-09-12 14:33:25 +0200

    chore(core): remove flash otp interface from python
    [no changelog]

--- core/mocks/generated/trezorio/__init__.pyi
+++ core/mocks/generated/trezorio/__init__.pyi
@@ -1,36 +1,6 @@
 from typing import *
 
 
-# extmod/modtrezorio/modtrezorio-flash.h
-class FlashOTP:
-    """
-    """
-
-    def __init__(self) -> None:
-        """
-        """
-
-    def write(self, block: int, offset: int, data: bytes) -> None:
-        """
-        Writes data to OTP flash
-        """
-
-    def read(self, block: int, offset: int, data: bytearray) -> None:
-        """
-        Reads data from OTP flash
-        """
-
-    def lock(self, block: int) -> None:
-        """
-        Lock OTP flash block
-        """
-
-    def is_locked(self, block: int) -> bool:
-        """
-        Is OTP flash block locked?
-        """
-
-
 # extmod/modtrezorio/modtrezorio-hid.h
 class HID:
     """
