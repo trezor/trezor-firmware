58764de73fe2e855b556d923c66e7a2353bf6a77
cepetr <petr@cerninovi.cz>
2024-10-03 17:23:20 +0200

    refactor(core): remove common.c on all platforms
    [no changelog]

--- core/embed/trezorhal/stm32f4/platform.c
+++ core/embed/trezorhal/stm32f4/platform.c
@@ -207,4 +207,23 @@ void set_core_clock(clock_settings_t settings) {
 }
 #endif
 
+// reference RM0090 section 35.12.1 Figure 413
+#define USB_OTG_HS_DATA_FIFO_RAM (USB_OTG_HS_PERIPH_BASE + 0x20000U)
+#define USB_OTG_HS_DATA_FIFO_SIZE (4096U)
+
+// Clears USB FIFO memory to prevent data leakage of sensitive information
+__attribute((used)) void clear_otg_hs_memory(void) {
+  // use the HAL version due to section 2.1.6 of STM32F42xx Errata sheet
+  __HAL_RCC_USB_OTG_HS_CLK_ENABLE();  // enable USB_OTG_HS peripheral clock so
+                                      // that the peripheral memory is
+                                      // accessible
+  memset_reg(
+      (volatile void *)USB_OTG_HS_DATA_FIFO_RAM,
+      (volatile void *)(USB_OTG_HS_DATA_FIFO_RAM + USB_OTG_HS_DATA_FIFO_SIZE),
+      0);
+
+  __HAL_RCC_USB_OTG_HS_CLK_DISABLE();  // disable USB OTG_HS peripheral clock as
+                                       // the peripheral is not needed right now
+}
+
 #endif  // KERNEL_MODE
