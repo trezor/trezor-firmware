d38c2e179013e8b908d7fb32e87f77aacf2793c4
tychovrahe <brunam@seznam.cz>
2024-10-29 11:15:05 +0100

    chore(core): drop obsolete boards
    [no changelog]

--- core/site_scons/models/T2B1/__init__.py
+++ core/site_scons/models/T2B1/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Optional
 
-from . import emulator, trezor_r_v3, trezor_r_v4, trezor_r_v6, trezor_r_v10
+from . import emulator, trezor_r_v10
 
 
 def configure_board(
@@ -19,9 +19,6 @@ def configure_board(
     # Mapping of revisions to their respective configurations
     revision_map = {
         "emulator": emulator,
-        3: trezor_r_v3,
-        4: trezor_r_v4,
-        6: trezor_r_v6,
         10: trezor_r_v10,
     }
 
