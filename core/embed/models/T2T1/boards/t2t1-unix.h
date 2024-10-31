03c35deec72d065a42a911ac6bde5deedb5f89e5
cepetr <petr@cerninovi.cz>
2024-10-31 21:15:17 +0100

    refactor(core): remove unused MAX_DISPLAY_RESx
    [no changelog]

--- core/embed/models/T2T1/boards/t2t1-unix.h
+++ core/embed/models/T2T1/boards/t2t1-unix.h
@@ -2,8 +2,6 @@
 #define BOARDS_T2T1_UNIX_H
 
 // ILI9341V, GC9307 and ST7789V drivers support 240px x 320px display resolution
-#define MAX_DISPLAY_RESX 240
-#define MAX_DISPLAY_RESY 320
 #define DISPLAY_RESX 240
 #define DISPLAY_RESY 240
 #define TREZOR_FONT_BPP 4
