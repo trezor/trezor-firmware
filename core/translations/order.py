b40ce852eaa1dd03d9cd23d9cef694163bb6537b
Martin Milata <martin@martinmilata.cz>
2024-11-07 14:30:15 +0100

    build(core/translations): check order.json for holes
    [no changelog]

--- core/translations/order.py
+++ core/translations/order.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 import json
 import os
-
+import sys
 from pathlib import Path
+
+import click
 
 HERE = Path(__file__).parent
 
@@ -36,5 +38,22 @@ def generate_new_order() -> None:
         print("No new items found.")
 
 
-if __name__ == "__main__":
+def check_order() -> None:
+    old_order: dict[str, str] = json.loads(output_file.read_text())
+    keys = { int(k) for k in old_order.keys() }
+    for i in range(len(old_order)):
+        if i not in keys:
+            raise ValueError(f"order.json is missing index {i}")
+
+
+@click.command()
+@click.option("--check", is_flag=True, help="Do not modify order.json, only check if it's valid.")
+def main(check: bool | None) -> None:
+    check_order()
+    if check:
+        sys.exit(0)
     generate_new_order()
+
+
+if __name__ == "__main__":
+    main()
