f9ccced691111130062606409608d42467c67f80
M1nd3r <petrsedlacek.km@seznam.cz>
2024-11-14 15:12:55 +0100

    refactor(core/python): update and unify type ignore statements [no changelog]

--- core/src/trezor/ui/layouts/mercury/__init__.py
+++ core/src/trezor/ui/layouts/mercury/__init__.py
@@ -1114,7 +1114,7 @@ def request_passphrase_on_device(max_len: int) -> Awaitable[str]:
         ButtonRequestType.PassphraseEntry,
         raise_on_cancel=ActionCancelled("Passphrase entry cancelled"),
     )
-    return result  # type: ignore ["UiResult" is incompatible with "str"]
+    return result  # type: ignore [Expression of type "Coroutine[Any, Any, str | UiResult]" is incompatible with return type "Awaitable[str]"]
 
 
 def request_pin_on_device(
@@ -1143,7 +1143,7 @@ def request_pin_on_device(
         ButtonRequestType.PinEntry,
         raise_on_cancel=PinCancelled,
     )
-    return result  # type: ignore ["UiResult" is incompatible with "str"]
+    return result  # type: ignore [Expression of type "Coroutine[Any, Any, str | UiResult]" is incompatible with return type "Awaitable[str]"]
 
 
 async def confirm_reenter_pin(is_wipe_code: bool = False) -> None:
