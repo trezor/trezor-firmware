d3d80fc636a774fde797fa59fc22b2037bfdbbe1
tychovrahe <brunam@seznam.cz>
2024-11-15 13:24:03 +0100

    feat(core): support optiga on T3W1
    [no changelog]

--- core/embed/sec/optiga/stm32f4/optiga_hal.c
+++ core/embed/sec/optiga/stm32f4/optiga_hal.c
@@ -7,15 +7,28 @@
 #ifdef KERNEL_MODE
 
 void optiga_hal_init(void) {
+  GPIO_InitTypeDef GPIO_InitStructure = {0};
+
   OPTIGA_RST_CLK_EN();
   // init reset pin
-  GPIO_InitTypeDef GPIO_InitStructure = {0};
   GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStructure.Pull = GPIO_NOPULL;
   GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_LOW;
   GPIO_InitStructure.Alternate = 0;
   GPIO_InitStructure.Pin = OPTIGA_RST_PIN;
   HAL_GPIO_Init(OPTIGA_RST_PORT, &GPIO_InitStructure);
+
+#ifdef OPTIGA_PWR_PIN
+  GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStructure.Pull = GPIO_NOPULL;
+  GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStructure.Alternate = 0;
+  GPIO_InitStructure.Pin = OPTIGA_PWR_PIN;
+  HAL_GPIO_Init(OPTIGA_PWR_PORT, &GPIO_InitStructure);
+  HAL_GPIO_WritePin(OPTIGA_PWR_PORT, OPTIGA_PWR_PIN, GPIO_PIN_RESET);
+  hal_delay(10);
+#endif
+
   // perform reset on every initialization
   HAL_GPIO_WritePin(OPTIGA_RST_PORT, OPTIGA_RST_PIN, GPIO_PIN_RESET);
   hal_delay(10);
