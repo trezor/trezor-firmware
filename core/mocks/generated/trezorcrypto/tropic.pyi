bd6b93dd2a953f8ea24a9ca622ea32422d0318f6
Ioan Bizău <i@ibz.me>
2024-11-27 16:19:12 +0100

    fixup! fixup! feat(core): add libtropic to unix build

--- core/mocks/generated/trezorcrypto/tropic.pyi
+++ core/mocks/generated/trezorcrypto/tropic.pyi
@@ -1,7 +1,7 @@
 from typing import *
 
 
-# extmod/modtrezorcrypto/modtrezorcrypto-tropic.h
+# upymod/modtrezorcrypto/modtrezorcrypto-tropic.h
 class TropicError(Exception):
     """Error returned by the Tropic Square chip."""
 
@@ -20,13 +20,6 @@ class TropicError(Exception):
     ) -> None:
         """
         Generate ECC key in the device's ECC key slot.
-        """
-
-    def random_get(
-        len: int,
-    ) -> bytes:
-        """
-        Get number of random bytes.
         """
 
     def sign(
