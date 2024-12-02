23afb00d4faf1179036fd8d549f9137486d66ad1
M1nd3r <petrsedlacek.km@seznam.cz>
2024-12-02 15:38:30 +0100

    chore(core): adapt emu.py to new trezorlib [no changelog]

--- core/emu.py
+++ core/emu.py
@@ -282,9 +282,11 @@ def cli(
             label = "Emulator"
 
         assert emulator.client is not None
-        trezorlib.device.wipe(emulator.client)
+        trezorlib.device.wipe(emulator.client.get_management_session())
+        emulator.client = emulator.client.get_new_client()
+
         trezorlib.debuglink.load_device(
-            emulator.client,
+            emulator.client.get_management_session(),
             mnemonics,
             pin=None,
             passphrase_protection=False,
