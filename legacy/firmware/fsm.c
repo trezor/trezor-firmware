0d6bfde21408c68052f75a482e078696fab3c227
M1nd3r <petrsedlacek.km@seznam.cz>
2024-12-02 15:42:35 +0100

    feat(legacy): adapt legacy to THP changes [no changelog]

--- legacy/firmware/fsm.c
+++ legacy/firmware/fsm.c
@@ -191,6 +191,12 @@ void fsm_sendFailure(FailureType code, const char *text)
       case FailureType_Failure_InvalidSession:
         text = _("Invalid session");
         break;
+      case FailureType_Failure_ThpUnallocatedSession:
+        text = _("Unallocated session");
+        break;
+      case FailureType_Failure_InvalidProtocol:
+        text = _("Invalid protocol");
+        break;
       case FailureType_Failure_FirmwareError:
         text = _("Firmware error");
         break;
