8aef86725985381b23c31d5c3c88bb08bbe73128
M1nd3r <petrsedlacek.km@seznam.cz>
2024-12-04 16:21:06 +0100

    refactor(core): move wire buffer handling completely to memory_manager [no changelog]

--- core/src/trezor/wire/thp/channel_manager.py
+++ core/src/trezor/wire/thp/channel_manager.py
@@ -1,7 +1,6 @@
 from typing import TYPE_CHECKING
 
 from storage import cache_thp
-from trezor import utils
 
 from . import ChannelState, interface_manager
 from .channel import Channel
@@ -10,18 +9,17 @@ if TYPE_CHECKING:
     from trezorio import WireInterface
 
 
-def create_new_channel(iface: WireInterface, buffer: utils.BufferType) -> Channel:
+def create_new_channel(iface: WireInterface) -> Channel:
     """
     Creates a new channel for the interface `iface` with the buffer `buffer`.
     """
     channel_cache = cache_thp.get_new_channel(interface_manager.encode_iface(iface))
     channel = Channel(channel_cache)
-    channel.set_buffer(buffer)
     channel.set_channel_state(ChannelState.TH1)
     return channel
 
 
-def load_cached_channels(buffer: utils.BufferType) -> dict[int, Channel]:
+def load_cached_channels() -> dict[int, Channel]:
     """
     Returns all allocated channels from cache.
     """
@@ -29,6 +27,4 @@ def load_cached_channels(buffer: utils.BufferType) -> dict[int, Channel]:
     cached_channels = cache_thp.get_all_allocated_channels()
     for channel in cached_channels:
         channels[int.from_bytes(channel.channel_id, "big")] = Channel(channel)
-    for channel in channels.values():
-        channel.set_buffer(buffer)
     return channels
