a384dc0a1c8e1999b5c7716ba7ba34339096bc81
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2024-12-09 13:46:32 +0100

    chore: update nix-shell

--- ci/shell.nix
+++ ci/shell.nix
@@ -70,23 +70,33 @@ let
     # to use official binary, remove rustfmt from buildInputs and add it to extensions:
     extensions = [ "rust-src" "clippy" "rustfmt" ];
   };
-  openocd-stm = (nixpkgs.openocd.overrideAttrs (oldAttrs: {
-    src = nixpkgs.fetchFromGitHub {
+  openocd-stm = (newNixpkgs.openocd.overrideAttrs (oldAttrs: {
+    src = newNixpkgs.fetchFromGitHub {
       owner = "STMicroelectronics";
       repo = "OpenOCD";
-      rev = "openocd-cubeide-v1.12.0";
-      sha256 = "7REQi9pcT6pn8yiAMpQpRQ+0ouMQelcciMAHyUonkVA=";
+      rev = "openocd-cubeide-v1.13.0";
+      sha256 = "a811402e19f0bfe496f6eecdc05ecea57f79a323879a810efaaff101cb0f420f";
     };
-    version = "stm-cubeide-v1.12.0";
-    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ nixpkgs.autoreconfHook ];
-
-    # following two lines can be removed after bumping nixpkgs to newer than c58e6fbf258df1572b535ac1868ec42faf7675dd
-    buildInputs = oldAttrs.buildInputs ++ [ nixpkgs.jimtcl nixpkgs.libjaylink ];
-    configureFlags = oldAttrs.configureFlags ++ [ "--disable-internal-jimtcl" "--disable-internal-libjaylink" ];
+    version = "stm-cubeide-v1.13.0";
+    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ newNixpkgs.autoreconfHook ];
+  }));
+  # backport https://github.com/NixOS/nixpkgs/pull/229537
+  # remove after nixpkgs bump
+  gcc-arm-embedded-gdbfix = (nixpkgs.gcc-arm-embedded.overrideAttrs (oldAttrs: {
+    postFixup = ''
+      mv $out/bin/arm-none-eabi-gdb $out/bin/arm-none-eabi-gdb-unwrapped
+      cat <<EOF > $out/bin/arm-none-eabi-gdb
+      #!${nixpkgs.runtimeShell}
+      export PYTHONPATH=${nixpkgs.python38}/lib/python3.8
+      export PYTHONHOME=${nixpkgs.python38}/bin/python3.8
+      exec $out/bin/arm-none-eabi-gdb-unwrapped "\$@"
+      EOF
+      chmod +x $out/bin/arm-none-eabi-gdb
+    '';
   }));
   llvmPackages = nixpkgs.llvmPackages_14;
   # see pyright/README.md for update procedure
-  pyright = nixpkgs.callPackage ./pyright {};
+  pyright = nixpkgs.callPackage ./ci/pyright {};
 in
 with nixpkgs;
 stdenvNoCC.mkDerivation ({
@@ -111,7 +121,7 @@ stdenvNoCC.mkDerivation ({
     crowdin-cli  # for translations
     curl  # for connect tests
     editorconfig-checker
-    gcc-arm-embedded
+    (if devTools then gcc-arm-embedded-gdbfix else gcc-arm-embedded)
     git
     gitAndTools.git-subrepo
     gnumake
@@ -153,8 +163,10 @@ stdenvNoCC.mkDerivation ({
     dejavu_fonts
   ] ++ lib.optionals devTools [
     shellcheck
-    gdb
     openocd-stm
+    kcachegrind
+  ] ++ lib.optionals (devTools && !stdenv.isDarwin) [
+    gdb
   ] ++ lib.optionals (devTools && acceptJlink) [
     nrfutil
     nrfconnect
