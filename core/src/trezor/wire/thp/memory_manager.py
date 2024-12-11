c5d3e3f74c7fdeec01d6aab8a56b9ad93d1928e5
M1nd3r <petrsedlacek.km@seznam.cz>
2024-12-11 11:46:55 +0100

    wip buffer locking- [no changelog]

--- core/src/trezor/wire/thp/memory_manager.py
+++ core/src/trezor/wire/thp/memory_manager.py
@@ -1,70 +1,150 @@
-from storage.cache_thp import SESSION_ID_LENGTH, TAG_LENGTH
-from trezor import log, protobuf, utils
+import utime
+from micropython import const
+
+from storage.cache_thp import SESSION_ID_LENGTH
+from trezor import protobuf, utils
 from trezor.wire.message_handler import get_msg_type
 
-from . import ChannelState, ThpError
-from .checksum import CHECKSUM_LENGTH
-from .writer import (
-    INIT_HEADER_LENGTH,
-    MAX_PAYLOAD_LEN,
-    MESSAGE_TYPE_LENGTH,
-    PACKET_LENGTH,
-)
+from . import ThpError
+from .writer import MAX_PAYLOAD_LEN, MESSAGE_TYPE_LENGTH
 
 _PROTOBUF_BUFFER_SIZE = 8192
 READ_BUFFER = bytearray(_PROTOBUF_BUFFER_SIZE)
 WRITE_BUFFER = bytearray(_PROTOBUF_BUFFER_SIZE)
 
 
-def select_buffer(
-    channel_state: int,
-    channel_buffer: utils.BufferType,
-    packet_payload: utils.BufferType,
-    payload_length: int,
-) -> utils.BufferType:
+lock_owner_cid: int | None = None
+lock_time: int = 0
 
-    if channel_state is ChannelState.ENCRYPTED_TRANSPORT:
-        session_id = packet_payload[0]
-        if session_id == 0:
-            pass
-            # TODO use small buffer
-        else:
-            pass
-            # TODO use big buffer but only if the channel owns the buffer lock.
-            # Otherwise send BUSY message and return
+READ_BUFFER_SLICE: memoryview | None = None
+WRITE_BUFFER_SLICE: memoryview | None = None
+
+# Buffer types
+_READ: int = const(0)
+_WRITE: int = const(1)
+
+
+#
+# Access to buffer slices
+
+
+def get_new_read_buffer(channel_id: int, length: int) -> memoryview:
+    return _get_new_buffer(_READ, channel_id, length)
+
+
+def get_new_write_buffer(channel_id: int, length: int) -> memoryview:
+    return _get_new_buffer(_WRITE, channel_id, length)
+
+
+def get_existing_read_buffer(channel_id: int) -> memoryview:
+    return _get_existing_buffer(_READ, channel_id)
+
+
+def get_existing_write_buffer(channel_id: int) -> memoryview:
+    return _get_existing_buffer(_WRITE, channel_id)
+
+
+def _get_new_buffer(buffer_type: int, channel_id: int, length: int) -> memoryview:
+    if is_locked():
+        if not is_owner(channel_id):
+            raise BufferError
+        update_lock_time()
     else:
-        pass
-        # TODO use small buffer
-    try:
-        # TODO for now, we create a new big buffer every time. It should be changed
-        buffer: utils.BufferType = _get_buffer_for_read(payload_length, channel_buffer)
-        return buffer
-    except Exception as e:
-        if __debug__ and utils.ALLOW_DEBUG_MESSAGES:
-            log.exception(__name__, e)
-    raise Exception("Failed to create a buffer for channel")  # TODO handle better
+        update_lock(channel_id)
+
+    if buffer_type == _READ:
+        global READ_BUFFER
+        buffer = READ_BUFFER
+    elif buffer_type == _WRITE:
+        global WRITE_BUFFER
+        buffer = WRITE_BUFFER
+    else:
+        raise Exception("Invalid buffer_type")
+
+    if length > MAX_PAYLOAD_LEN or length > len(buffer):
+        raise ThpError("Message is too large")  # TODO reword
+
+    if buffer_type == _READ:
+        global READ_BUFFER_SLICE
+        READ_BUFFER_SLICE = memoryview(READ_BUFFER)[:length]
+        return READ_BUFFER_SLICE
+
+    if buffer_type == _WRITE:
+        global WRITE_BUFFER_SLICE
+        WRITE_BUFFER_SLICE = memoryview(WRITE_BUFFER)[:length]
+        return WRITE_BUFFER_SLICE
+
+    raise Exception("Invalid buffer_type")
 
 
-def get_write_buffer(
-    buffer: utils.BufferType, msg: protobuf.MessageType
-) -> utils.BufferType:
-    msg_size = protobuf.encoded_length(msg)
-    payload_size = SESSION_ID_LENGTH + MESSAGE_TYPE_LENGTH + msg_size
-    required_min_size = payload_size + CHECKSUM_LENGTH + TAG_LENGTH
+def _get_existing_buffer(buffer_type: int, channel_id: int) -> memoryview:
+    if not is_owner(channel_id):
+        raise BufferError
+    update_lock_time()
 
-    if required_min_size > len(buffer):
-        return _get_buffer_for_write(required_min_size, buffer)
-    return buffer
+    if buffer_type == _READ:
+        global READ_BUFFER_SLICE
+        if READ_BUFFER_SLICE is None:
+            raise BufferError
+        return READ_BUFFER_SLICE
+
+    if buffer_type == _WRITE:
+        global WRITE_BUFFER_SLICE
+        if WRITE_BUFFER_SLICE is None:
+            raise BufferError
+        return WRITE_BUFFER_SLICE
+
+    raise Exception("Invalid buffer_type")
+
+
+#
+# Buffer locking
+
+
+def is_locked() -> bool:
+    global lock_owner_cid
+    global lock_time
+
+    time_diff = utime.ticks_diff(utime.ticks_ms(), lock_time)
+    return lock_owner_cid is not None and time_diff < 200
+
+
+def is_owner(channel_id: int) -> bool:
+    global lock_owner_cid
+    return lock_owner_cid is not None and lock_owner_cid == channel_id
+
+
+def update_lock(channel_id: int) -> None:
+    set_owner(channel_id)
+    update_lock_time()
+
+
+def set_owner(channel_id: int) -> None:
+    global lock_owner_cid
+    lock_owner_cid = channel_id
+
+
+def update_lock_time() -> None:
+    global lock_time
+    lock_time = utime.ticks_ms()
+
+
+#
+# Helper for encoding messages into buffer
 
 
 def encode_into_buffer(
     buffer: utils.BufferType, msg: protobuf.MessageType, session_id: int
 ) -> int:
+    """Encode protobuf message `msg` into the `buffer`, including session id
+    an messages's wire type. Will fail if provided message has no wire type."""
+
     # cannot write message without wire type
     msg_type = msg.MESSAGE_WIRE_TYPE
     if msg_type is None:
         msg_type = get_msg_type(msg.MESSAGE_NAME)
-    assert msg_type is not None
+    if msg_type is None:
+        raise Exception("Message has no wire type.")
 
     msg_size = protobuf.encoded_length(msg)
     payload_size = SESSION_ID_LENGTH + MESSAGE_TYPE_LENGTH + msg_size
@@ -96,84 +176,3 @@ def _encode_message_into_buffer(
     buffer: memoryview, message: protobuf.MessageType, buffer_offset: int = 0
 ) -> None:
     protobuf.encode(memoryview(buffer[buffer_offset:]), message)
-
-
-def _get_buffer_for_read(
-    payload_length: int,
-    existing_buffer: utils.BufferType,
-    max_length: int = MAX_PAYLOAD_LEN,
-) -> utils.BufferType:
-    length = payload_length + INIT_HEADER_LENGTH
-    if __debug__ and utils.ALLOW_DEBUG_MESSAGES:
-        log.debug(
-            __name__,
-            "get_buffer_for_read - length: %d, %s %s",
-            length,
-            "existing buffer type:",
-            type(existing_buffer),
-        )
-    if length > max_length:
-        raise ThpError("Message too large")
-
-    if length > len(existing_buffer):
-        if __debug__ and utils.ALLOW_DEBUG_MESSAGES:
-            log.debug(__name__, "Allocating a new buffer")
-
-        if length > len(READ_BUFFER):
-            if __debug__ and utils.ALLOW_DEBUG_MESSAGES:
-                log.debug(
-                    __name__,
-                    "Required length is %d, where raw buffer has capacity only %d",
-                    length,
-                    len(READ_BUFFER),
-                )
-            raise ThpError("Message is too large")
-
-        try:
-            payload: utils.BufferType = memoryview(READ_BUFFER)[:length]
-        except MemoryError:
-            payload = memoryview(READ_BUFFER)[:PACKET_LENGTH]
-            raise ThpError("Message is too large")
-        return payload
-
-    # reuse a part of the supplied buffer
-    if __debug__ and utils.ALLOW_DEBUG_MESSAGES:
-        log.debug(__name__, "Reusing already allocated buffer")
-    return memoryview(existing_buffer)[:length]
-
-
-def _get_buffer_for_write(
-    payload_length: int,
-    existing_buffer: utils.BufferType,
-    max_length: int = MAX_PAYLOAD_LEN,
-) -> utils.BufferType:
-    length = payload_length + INIT_HEADER_LENGTH
-    if __debug__ and utils.ALLOW_DEBUG_MESSAGES:
-        log.debug(
-            __name__,
-            "get_buffer_for_write - length: %d, %s %s",
-            length,
-            "existing buffer type:",
-            type(existing_buffer),
-        )
-    if length > max_length:
-        raise ThpError("Message too large")
-
-    if length > len(existing_buffer):
-        if __debug__ and utils.ALLOW_DEBUG_MESSAGES:
-            log.debug(__name__, "Creating a new write buffer from raw write buffer")
-
-        if length > len(WRITE_BUFFER):
-            raise ThpError("Message is too large")
-
-        try:
-            payload: utils.BufferType = memoryview(WRITE_BUFFER)[:length]
-        except MemoryError:
-            payload = memoryview(WRITE_BUFFER)[:PACKET_LENGTH]
-            raise ThpError("Message is too large")
-        return payload
-
-    # reuse a part of the supplied buffer
-    if __debug__ and utils.ALLOW_DEBUG_MESSAGES:
-        log.debug(__name__, "Reusing already allocated buffer")
-    return memoryview(existing_buffer)[:length]
