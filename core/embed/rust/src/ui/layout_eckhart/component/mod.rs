d35c4535fa079df2dd4ffce96e881bb428b4e35b
obrusvit <vit.obrusnik@gmail.com>
2025-01-06 18:39:46 +0100

    feat(eckhart): hint component

--- core/embed/rust/src/ui/layout_eckhart/component/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/component/mod.rs
@@ -2,12 +2,14 @@ pub mod bl_confirm;
 mod button;
 mod error;
 mod header;
+mod hint;
 mod result;
 mod welcome_screen;
 
 pub use button::{Button, ButtonMsg, ButtonStyle, ButtonStyleSheet, IconText};
 pub use error::ErrorScreen;
 pub use header::{Header, HeaderMsg};
+pub use hint::Hint;
 pub use result::{ResultFooter, ResultScreen, ResultStyle};
 pub use welcome_screen::WelcomeScreen;
 
