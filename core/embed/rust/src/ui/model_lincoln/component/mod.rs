04dfd31aa0f33a28ec30e2f161d31e145ece3cfe
obrusvit <vit.obrusnik@gmail.com>
2025-01-06 18:39:46 +0100

    feat(lincoln): hint component

--- core/embed/rust/src/ui/model_lincoln/component/mod.rs
+++ core/embed/rust/src/ui/model_lincoln/component/mod.rs
@@ -1,12 +1,14 @@
 mod button;
 mod error;
 mod header;
+mod hint;
 mod result;
 mod welcome_screen;
 
 pub use button::{ButtonStyle, ButtonStyleSheet};
 pub use error::ErrorScreen;
 pub use header::Header;
+pub use hint::Hint;
 pub use result::{ResultFooter, ResultScreen, ResultStyle};
 pub use welcome_screen::WelcomeScreen;
 
