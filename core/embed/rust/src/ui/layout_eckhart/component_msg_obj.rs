8c06e64e8810b540032024e945fd896f06668eef
obrusvit <vit.obrusnik@gmail.com>
2025-01-11 17:22:08 +0100

    feat(eckhart): full-screen textual component
    - TextScreen is a full-screen component for (paginated) texts
    - it's supposed to wrap FormattedText or Paragraphs

--- core/embed/rust/src/ui/layout_eckhart/component_msg_obj.rs
+++ core/embed/rust/src/ui/layout_eckhart/component_msg_obj.rs
@@ -6,9 +6,14 @@ use crate::{
             text::paragraphs::{ParagraphSource, Paragraphs},
             Component, Timeout,
         },
-        layout::{obj::ComponentMsgObj, result::CANCELLED},
+        layout::{
+            obj::ComponentMsgObj,
+            result::{CANCELLED, CONFIRMED, INFO},
+        },
     },
 };
+
+use super::component::{AllowedTextContent, TextScreen, TextScreenMsg};
 
 // Clippy/compiler complains about conflicting implementations
 // TODO move the common impls to a common module
@@ -30,5 +35,18 @@ where
 {
     fn msg_try_into_obj(&self, _msg: Self::Msg) -> Result<Obj, Error> {
         Ok(CANCELLED.as_obj())
+    }
+}
+
+impl<T> ComponentMsgObj for TextScreen<T>
+where
+    T: AllowedTextContent,
+{
+    fn msg_try_into_obj(&self, msg: Self::Msg) -> Result<Obj, Error> {
+        match msg {
+            TextScreenMsg::Cancelled => Ok(CANCELLED.as_obj()),
+            TextScreenMsg::Confirmed => Ok(CONFIRMED.as_obj()),
+            TextScreenMsg::Menu => Ok(INFO.as_obj()),
+        }
     }
 }
