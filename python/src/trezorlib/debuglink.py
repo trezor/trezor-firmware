18c022f652b1b5ea6bf11116796a528f8f6aa3c6
Roman Zeyde <roman.zeyde@satoshilabs.com>
2025-01-14 18:30:38 +0200

    refactor(core): use layout name for translation font keys
    It would simplify `TranslationsDir.load_lang` method.
    
    [no changelog]

--- python/src/trezorlib/debuglink.py
+++ python/src/trezorlib/debuglink.py
@@ -80,6 +80,7 @@ class LayoutType(Enum):
     Bolt = auto()
     Caesar = auto()
     Delizia = auto()
+    Eckhart = auto()
 
     @classmethod
     def from_model(cls, model: models.TrezorModel) -> "LayoutType":
@@ -89,6 +90,8 @@ class LayoutType(Enum):
             return cls.Caesar
         if model in (models.T3T1,):
             return cls.Delizia
+        if model in (models.T3W1,):
+            return cls.Eckhart
         if model in (models.T1B1,):
             return cls.T1
         raise ValueError(f"Unknown model: {model}")
