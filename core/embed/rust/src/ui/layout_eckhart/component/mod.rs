17ba44d8c155aaaf78657c46b293167228d49963
obrusvit <vit.obrusnik@gmail.com>
2025-01-15 15:44:08 +0100

    feat(eckhart): componets
    - Header
    - Hint
    - ActionBar

--- core/embed/rust/src/ui/layout_eckhart/component/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/component/mod.rs
@@ -1,11 +1,17 @@
+mod action_bar;
 pub mod bl_confirm;
 mod button;
 mod error;
+mod header;
+mod hint;
 mod result;
 mod welcome_screen;
 
+pub use action_bar::ActionBar;
 pub use button::{Button, ButtonMsg, ButtonStyle, ButtonStyleSheet, IconText};
 pub use error::ErrorScreen;
+pub use header::{Header, HeaderMsg};
+pub use hint::Hint;
 pub use result::{ResultFooter, ResultScreen, ResultStyle};
 pub use welcome_screen::WelcomeScreen;
 
