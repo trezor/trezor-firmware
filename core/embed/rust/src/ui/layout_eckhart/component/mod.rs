7c856fc4b081e124abf1718dfce56b889cba0ebd
obrusvit <vit.obrusnik@gmail.com>
2025-01-15 15:44:08 +0100

    feat(eckhart): header component

--- core/embed/rust/src/ui/layout_eckhart/component/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/component/mod.rs
@@ -1,11 +1,13 @@
 pub mod bl_confirm;
 mod button;
 mod error;
+mod header;
 mod result;
 mod welcome_screen;
 
 pub use button::{Button, ButtonMsg, ButtonStyle, ButtonStyleSheet, IconText};
 pub use error::ErrorScreen;
+pub use header::{Header, HeaderMsg};
 pub use result::{ResultFooter, ResultScreen, ResultStyle};
 pub use welcome_screen::WelcomeScreen;
 
