bcdec3b902b15c6aac3df2ef5a384414b3a970a0
tychovrahe <brunam@seznam.cz>
2025-01-15 08:56:07 +0100

    refactor(core): define model-specific include files in site-scons
    [no changelog]

--- core/embed/models/trezor_model.h
+++ core/embed/models/trezor_model.h
@@ -1,25 +1,5 @@
-#ifndef MODELS_MODEL_H_
-#define MODELS_MODEL_H_
+#pragma once
 
 #include <util/flash_layout.h>
 #include "otp_layout.h"
-
-#if defined TREZOR_MODEL_T2T1
-#include "T2T1/model_T2T1.h"
-#elif defined TREZOR_MODEL_T2B1
-#include "T2B1/model_T2B1.h"
-#elif defined TREZOR_MODEL_T3T1
-#include "T3T1/model_T3T1.h"
-#elif defined TREZOR_MODEL_T3B1
-#include "T3B1/model_T3B1.h"
-#elif defined TREZOR_MODEL_T3W1
-#include "T3W1/model_T3W1.h"
-#elif defined TREZOR_MODEL_D001
-#include "D001/model_D001.h"
-#elif defined TREZOR_MODEL_D002
-#include "D002/model_D002.h"
-#else
-#error Unknown Trezor model
-#endif
-
-#endif
+#include MODEL_HEADER
