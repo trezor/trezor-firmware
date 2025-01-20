64202649b2f8383699c5c09e0b31ae1f56dae3bc
obrusvit <vit.obrusnik@gmail.com>
2025-01-20 11:21:47 +0100

    feat(eckhart): full-screen vertical menu component

--- core/embed/rust/src/ui/layout_eckhart/component/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/component/mod.rs
@@ -6,6 +6,7 @@ mod header;
 mod hint;
 mod result;
 mod text_screen;
+mod vertical_menu_page;
 mod welcome_screen;
 
 pub use action_bar::ActionBar;
@@ -15,6 +16,7 @@ pub use header::{Header, HeaderMsg};
 pub use hint::Hint;
 pub use result::{ResultFooter, ResultScreen, ResultStyle};
 pub use text_screen::{AllowedTextContent, TextScreen, TextScreenMsg};
+pub use vertical_menu_page::VerticalMenuPage;
 pub use welcome_screen::WelcomeScreen;
 
 use super::{constant, theme};
