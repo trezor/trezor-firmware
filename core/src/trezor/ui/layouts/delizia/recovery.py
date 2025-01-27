3ff621e613ef1488a75b5b3292252fcbc4e3332d
obrusvit <vit.obrusnik@gmail.com>
2025-01-27 15:33:38 +0100

    fix(core): explicitly delete keyboard layout
    - seems that keyboard LayoutObj was not properly deallocated resulting
    in MemoryError. Fix by calling the dunder `del` method explicitly fixes
    the issue
    - the problem was on Model T (Bolt), but applying it for all layouts to
    prevent similar problem

--- core/src/trezor/ui/layouts/delizia/recovery.py
+++ core/src/trezor/ui/layouts/delizia/recovery.py
@@ -33,6 +33,7 @@ async def request_word(
 ) -> str:
     prompt = TR.recovery__word_x_of_y_template.format(word_index + 1, word_count)
     can_go_back = word_index > 0
+
     if is_slip39:
         keyboard = trezorui_api.request_slip39(
             prompt=prompt, prefill_word=prefill_word, can_go_back=can_go_back
@@ -42,11 +43,14 @@ async def request_word(
             prompt=prompt, prefill_word=prefill_word, can_go_back=can_go_back
         )
 
-    word: str = await interact(
-        keyboard,
-        "mnemonic" if send_button_request else None,
-        ButtonRequestType.MnemonicInput,
-    )
+    try:
+        word: str = await interact(
+            keyboard,
+            "mnemonic" if send_button_request else None,
+            ButtonRequestType.MnemonicInput,
+        )
+    finally:
+        keyboard.__del__()
     return word
 
 
