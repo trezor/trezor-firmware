ff0baa7e3833d5f875dba8f9b1d6c17a9603fff7
M1nd3r <petrsedlacek.km@seznam.cz>
2025-02-04 15:19:56 +0100

    feat(python): implement session based trezorlib

--- python/src/trezorlib/exceptions.py
+++ python/src/trezorlib/exceptions.py
@@ -85,3 +85,7 @@ class UnexpectedMessageError(TrezorException):
         self.expected = expected
         self.actual = actual
         super().__init__(f"Expected {expected.__name__} but Trezor sent {actual}")
+
+
+class DeviceLockedException(TrezorException):
+    pass
