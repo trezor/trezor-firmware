d04cf76c3b5d6f491dde1b445272d1f146590f4a
obrusvit <vit.obrusnik@gmail.com>
2025-02-04 11:41:45 +0100

    refactor(core): feature-gate non-bootloader fonts
    - a crutch to preserve code which depends on specific fonts
    - see https://github.com/trezor/trezor-firmware/issues/4574
    
    [no changelog]

--- core/embed/rust/src/ui/layout_bolt/fonts/mod.rs
+++ core/embed/rust/src/ui/layout_bolt/fonts/mod.rs
@@ -1,14 +1,24 @@
+#[cfg(not(feature = "bootloader"))]
 mod font_robotomono_medium_20;
 mod font_tthoves_bold_17;
+#[cfg(not(feature = "bootloader"))]
 mod font_tthoves_demibold_21;
 mod font_tthoves_regular_21;
 
+#[cfg(not(feature = "bootloader"))]
 use font_robotomono_medium_20::Font_RobotoMono_Medium_20_info;
 use font_tthoves_bold_17::Font_TTHoves_Bold_17_upper_info;
+#[cfg(not(feature = "bootloader"))]
 use font_tthoves_demibold_21::Font_TTHoves_DemiBold_21_info;
 use font_tthoves_regular_21::Font_TTHoves_Regular_21_info;
 
 pub const FONT_NORMAL: crate::ui::display::Font = &Font_TTHoves_Regular_21_info;
 pub const FONT_BOLD_UPPER: crate::ui::display::Font = &Font_TTHoves_Bold_17_upper_info;
+#[cfg(feature = "bootloader")]
+pub const FONT_DEMIBOLD: crate::ui::display::Font = FONT_NORMAL;
+#[cfg(not(feature = "bootloader"))]
 pub const FONT_DEMIBOLD: crate::ui::display::Font = &Font_TTHoves_DemiBold_21_info;
+#[cfg(feature = "bootloader")]
+pub const FONT_MONO: crate::ui::display::Font = FONT_NORMAL;
+#[cfg(not(feature = "bootloader"))]
 pub const FONT_MONO: crate::ui::display::Font = &Font_RobotoMono_Medium_20_info;
--- core/embed/rust/src/ui/layout_delizia/fonts/mod.rs
+++ core/embed/rust/src/ui/layout_delizia/fonts/mod.rs
@@ -1,14 +1,29 @@
+#[cfg(not(feature = "bootloader"))]
 mod font_robotomono_medium_21;
+#[cfg(not(feature = "bootloader"))]
 mod font_ttsatoshi_demibold_18;
 mod font_ttsatoshi_demibold_21;
+#[cfg(not(feature = "bootloader"))]
 mod font_ttsatoshi_demibold_42;
 
+#[cfg(not(feature = "bootloader"))]
 use font_robotomono_medium_21::Font_RobotoMono_Medium_21_info;
+#[cfg(not(feature = "bootloader"))]
 use font_ttsatoshi_demibold_18::Font_TTSatoshi_DemiBold_18_info;
 use font_ttsatoshi_demibold_21::Font_TTSatoshi_DemiBold_21_info;
+#[cfg(not(feature = "bootloader"))]
 use font_ttsatoshi_demibold_42::Font_TTSatoshi_DemiBold_42_info;
 
 pub const FONT_DEMIBOLD: crate::ui::display::Font = &Font_TTSatoshi_DemiBold_21_info;
+#[cfg(feature = "bootloader")]
+pub const FONT_MONO: crate::ui::display::Font = FONT_DEMIBOLD;
+#[cfg(not(feature = "bootloader"))]
 pub const FONT_MONO: crate::ui::display::Font = &Font_RobotoMono_Medium_21_info;
+#[cfg(feature = "bootloader")]
+pub const FONT_BIG: crate::ui::display::Font = FONT_DEMIBOLD;
+#[cfg(not(feature = "bootloader"))]
 pub const FONT_BIG: crate::ui::display::Font = &Font_TTSatoshi_DemiBold_42_info;
+#[cfg(feature = "bootloader")]
+pub const FONT_SUB: crate::ui::display::Font = FONT_DEMIBOLD;
+#[cfg(not(feature = "bootloader"))]
 pub const FONT_SUB: crate::ui::display::Font = &Font_TTSatoshi_DemiBold_18_info;
