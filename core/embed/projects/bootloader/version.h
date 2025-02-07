45394f2b26c82b8231093425abe79d2dda978f96
obrusvit <vit.obrusnik@gmail.com>
2025-02-07 17:25:02 +0100

    chore(core): bump bootloader version to 2.1.11
    [no changelog]

--- core/embed/projects/bootloader/version.h
+++ core/embed/projects/bootloader/version.h
@@ -4,7 +4,7 @@
 
 #define VERSION_MAJOR 2
 #define VERSION_MINOR 1
-#define VERSION_PATCH 10
+#define VERSION_PATCH 11
 #define VERSION_BUILD 0
 #define VERSION_UINT32                                            \
   (VERSION_MAJOR | (VERSION_MINOR << 8) | (VERSION_PATCH << 16) | \
