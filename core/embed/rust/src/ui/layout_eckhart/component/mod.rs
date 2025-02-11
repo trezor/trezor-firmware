046e653b6100dadcd255e9e4aa374c96447ac597
Lukas Bielesch <bielesch.lukas@gmail.com>
2025-02-11 12:00:38 +0100

    feat(eckhart): Update vertical menu components

--- core/embed/rust/src/ui/layout_eckhart/component/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/component/mod.rs
@@ -6,17 +6,19 @@ mod header;
 mod hint;
 mod result;
 mod text_screen;
-mod vertical_menu_page;
+mod vertical_menu;
+mod vertical_menu_screen;
 mod welcome_screen;
 
 pub use action_bar::ActionBar;
-pub use button::{Button, ButtonMsg, ButtonStyle, ButtonStyleSheet, IconText};
+pub use button::{Button, ButtonContent, ButtonMsg, ButtonStyle, ButtonStyleSheet, IconText};
 pub use error::ErrorScreen;
 pub use header::{Header, HeaderMsg};
 pub use hint::Hint;
 pub use result::{ResultFooter, ResultScreen, ResultStyle};
 pub use text_screen::{AllowedTextContent, TextScreen, TextScreenMsg};
-pub use vertical_menu_page::VerticalMenuPage;
+pub use vertical_menu::{VerticalMenu, VerticalMenuMsg, MENU_MAX_ITEMS};
+pub use vertical_menu_screen::{VerticalMenuScreen, VerticalMenuScreenMsg};
 pub use welcome_screen::WelcomeScreen;
 
 use super::{constant, theme};
