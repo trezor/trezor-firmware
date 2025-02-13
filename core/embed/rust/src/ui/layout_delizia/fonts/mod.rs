bfebcd98aaaf02a01d698b74d62d1658ae2c4fe1
tychovrahe <brunam@seznam.cz>
2025-02-13 16:17:32 +0100

    fix(core): fix TS5 bootloader font
    [no changelog]

--- core/embed/rust/src/ui/layout_delizia/fonts/mod.rs
+++ core/embed/rust/src/ui/layout_delizia/fonts/mod.rs
@@ -6,8 +6,13 @@ mod font_ttsatoshi_demibold_21;
 #[cfg(not(feature = "bootloader"))]
 mod font_ttsatoshi_demibold_42;
 
+#[cfg(feature = "bootloader")]
+mod font_tthoves_bold_17;
+
 #[cfg(not(feature = "bootloader"))]
 use font_robotomono_medium_21::Font_RobotoMono_Medium_21_info;
+#[cfg(feature = "bootloader")]
+use font_tthoves_bold_17::Font_TTHoves_Bold_17_upper_info;
 #[cfg(not(feature = "bootloader"))]
 use font_ttsatoshi_demibold_18::Font_TTSatoshi_DemiBold_18_info;
 use font_ttsatoshi_demibold_21::Font_TTSatoshi_DemiBold_21_info;
@@ -15,6 +20,7 @@ use font_ttsatoshi_demibold_21::Font_TTSatoshi_DemiBold_21_info;
 use font_ttsatoshi_demibold_42::Font_TTSatoshi_DemiBold_42_info;
 
 pub const FONT_DEMIBOLD: crate::ui::display::Font = &Font_TTSatoshi_DemiBold_21_info;
+
 #[cfg(feature = "bootloader")]
 pub const FONT_MONO: crate::ui::display::Font = FONT_DEMIBOLD;
 #[cfg(not(feature = "bootloader"))]
@@ -24,6 +30,6 @@ pub const FONT_BIG: crate::ui::display::Font = FONT_DEMIBOLD;
 #[cfg(not(feature = "bootloader"))]
 pub const FONT_BIG: crate::ui::display::Font = &Font_TTSatoshi_DemiBold_42_info;
 #[cfg(feature = "bootloader")]
-pub const FONT_SUB: crate::ui::display::Font = FONT_DEMIBOLD;
+pub const FONT_SUB: crate::ui::display::Font = &Font_TTHoves_Bold_17_upper_info;
 #[cfg(not(feature = "bootloader"))]
 pub const FONT_SUB: crate::ui::display::Font = &Font_TTSatoshi_DemiBold_18_info;
