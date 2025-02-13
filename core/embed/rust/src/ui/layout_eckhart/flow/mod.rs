2f77a18e160f4512c7d600ea4c5c5f2c72e23fa9
Lukas Bielesch <bielesch.lukas@gmail.com>
2025-02-13 09:39:50 +0100

    feat(eckhart): passphrase flow

--- core/embed/rust/src/ui/layout_eckhart/flow/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/flow/mod.rs
@@ -1,5 +1,7 @@
 pub mod eckhart_swipe_flow_test;
 pub mod show_share_words;
+pub mod request_passphrase;
 
 pub use eckhart_swipe_flow_test::new_eckhart_swipe_flow;
+pub use request_passphrase::RequestPassphrase;
 pub use show_share_words::new_show_share_words_flow;
