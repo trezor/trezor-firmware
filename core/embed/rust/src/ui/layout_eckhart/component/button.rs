baefa27e3210fbf95bc0fdd623d622e96018ba0b
Lukas Bielesch <bielesch.lukas@gmail.com>
2025-02-16 11:42:04 +0100

    chore(eckhart): fix button text and subtext rendering
    - add new Button 'is_pressed' function

--- core/embed/rust/src/ui/layout_eckhart/component/button.rs
+++ core/embed/rust/src/ui/layout_eckhart/component/button.rs
@@ -36,7 +36,7 @@ pub struct Button {
 }
 
 impl Button {
-    pub const BASELINE_OFFSET: Offset = Offset::new(2, 6);
+    pub const BASELINE_OFFSET: Offset = Offset::new(12, 6);
     const LINE_SPACING: i16 = 7;
     const SUBTEXT_STYLE: TextStyle = theme::label_menu_item_subtitle();
 
@@ -133,6 +133,10 @@ impl Button {
             self.state,
             State::Initial | State::Pressed | State::Released
         )
+    }
+
+    pub fn is_pressed(&self) -> bool {
+        matches!(self.state, State::Pressed)
     }
 
     pub fn long_press(&self) -> Option<Duration> {
@@ -249,9 +253,8 @@ impl Button {
                 });
             }
             ButtonContent::TextAndSubtext(text, subtext) => {
-                let text_y_offset = Offset::y(
-                    self.content_height() / 2 - self.style().font.allcase_text_height() / 2,
-                );
+                let text_y_offset =
+                    Offset::y(self.content_height() / 2 - self.style().font.allcase_text_height());
                 let subtext_y_offset = Offset::y(self.content_height() / 2);
                 let start_of_baseline = match self.text_align {
                     Alignment::Start => {
@@ -271,7 +274,7 @@ impl Button {
                         .render(target);
                 });
 
-                text.map(|subtext| {
+                subtext.map(|subtext| {
                     shape::Text::new(subtext_baseline, subtext, Self::SUBTEXT_STYLE.text_font)
                         .with_fg(Self::SUBTEXT_STYLE.text_color)
                         .with_align(self.text_align)
