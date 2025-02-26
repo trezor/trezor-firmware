031ac4dbd4d82d1f3f93f984001f4ff4f244650e
Lukas Bielesch <bielesch.lukas@gmail.com>
2025-02-26 12:14:22 +0100

    feat(eckhart): confirm reset flow

--- core/embed/rust/src/ui/layout_eckhart/flow/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/flow/mod.rs
@@ -1,5 +1,7 @@
+pub mod confirm_reset;
 pub mod request_passphrase;
 pub mod show_share_words;
 
+pub use confirm_reset::new_confirm_reset;
 pub use request_passphrase::RequestPassphrase;
 pub use show_share_words::new_show_share_words_flow;
