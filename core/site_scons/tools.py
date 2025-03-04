90d77135929183bb1da54a58794eab9f97c629c1
Roman Zeyde <roman.zeyde@satoshilabs.com>
2025-03-04 13:23:12 +0200

    build(core): fix `RUST_PRINT_TYPES_SIZES` passing to `SConscript.firmware`
    Use a list to collect RUSTFLAGS, to prevent overwriting them.
    
    Also:
    - fix a typo in `SConscript.unix` (it was using `firmware` build directory)
    - use keyword arguments for better readability
    - reformat Rust library build command string
    
    [no changelog]

--- core/site_scons/tools.py
+++ core/site_scons/tools.py
@@ -128,9 +128,7 @@ def embed_raw_binary(obj_program, env, section, target_, file):
     )
 
 
-def add_rust_lib(
-    env, build, profile, features, all_paths, build_dir, print_types_sizes=False
-):
+def add_rust_lib(*, env, build, profile, features, all_paths, build_dir):
     RUST_LIB = "trezor_lib"
     RUST_TARGET = env.get("ENV")["RUST_TARGET"]
 
@@ -156,22 +154,26 @@ def add_rust_lib(
             "-Z build-std=core",
             "-Z build-std-features=panic_immediate_abort",
         ]
+        build_cmd = f"cargo build {profile} " + " ".join(cargo_opts)
 
-        if print_types_sizes:
+        unstable_rustc_flags = [
             # see https://nnethercote.github.io/perf-book/type-sizes.html#measuring-type-sizes for more details
-            env.Append(ENV={"RUSTFLAGS": "-Z print-type-sizes"})
+            "print-type-sizes",
+            # Adds an ELF section with Rust functions' stack sizes. See the following links for more details:
+            # - https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/emit-stack-sizes.html
+            # - https://blog.japaric.io/stack-analysis/
+            # - https://github.com/japaric/stack-sizes/
+            "emit-stack-sizes",
+        ]
 
-        # Adds an ELF section with Rust functions' stack sizes. See the following links for more details:
-        # - https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/emit-stack-sizes.html
-        # - https://blog.japaric.io/stack-analysis/
-        # - https://github.com/japaric/stack-sizes/
-        env.Append(ENV={"RUSTFLAGS": "-Z emit-stack-sizes"})
+        env.Append(ENV={"RUSTFLAGS": " ".join(f"-Z {f}" for f in unstable_rustc_flags)})
 
         bindgen_macros = get_bindgen_defines(env.get("CPPDEFINES"), all_paths)
 
         return (
-            f"export BINDGEN_MACROS={shlex.quote(bindgen_macros)}; export BUILD_DIR='{build_dir}'; cd embed/rust; cargo build {profile} "
-            + " ".join(cargo_opts)
+            f"export BINDGEN_MACROS={shlex.quote(bindgen_macros)}; "
+            f"export BUILD_DIR='{build_dir}'; "
+            f"cd embed/rust; {build_cmd} > {build_dir}/rust-type-sizes.log"
         )
 
     rust = env.Command(
