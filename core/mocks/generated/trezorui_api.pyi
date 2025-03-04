f267f05c16ce06464fe797ea914a86bd509784b8
Lukas Bielesch <bielesch.lukas@gmail.com>
2025-03-04 14:24:54 +0100

    feat(eckhart): show danger flow

--- core/mocks/generated/trezorui_api.pyi
+++ core/mocks/generated/trezorui_api.pyi
@@ -494,6 +494,7 @@ def show_danger(
     title: str,
     description: str,
     value: str = "",
+    menu_title: str | None = None,
     verb_cancel: str | None = None,
 ) -> LayoutObj[UiResult]:
     """Warning modal that makes it easier to cancel than to continue."""
