8a395cd4c1c45904365511cec1d070e01fd3433a
obrusvit <vit.obrusnik@gmail.com>
2025-03-05 00:13:26 +0100

    feat(eckhart): introduce BldTextScreen
    - basic bootloader screen parameterizable in a similar fashion as
    TextScreen used in Firmware

--- core/embed/rust/src/ui/layout_eckhart/bootloader/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/bootloader/mod.rs
@@ -1,5 +1,6 @@
 mod bld_actionbar;
 mod bld_header;
+mod bld_text_screen;
 mod confirm;
 mod intro;
 mod menu;
@@ -8,6 +9,7 @@ mod welcome;
 pub use bld_actionbar::BldActionBar;
 pub use bld_header::BldHeader;
 pub use confirm::{ConfirmScreen, ConfirmTitle};
+pub use bld_text_screen::BldTextScreen;
 pub use intro::IntroScreen;
 pub use menu::BldMenuScreen;
 pub use welcome::BldWelcomeScreen;
