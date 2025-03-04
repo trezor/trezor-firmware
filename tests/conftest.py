2c77ca6adfe223a22dae5402426cb5dba28ed216
M1nd3r <petrsedlacek.km@seznam.cz>
2025-03-04 16:40:32 +0100

    fixup! fixup! chore(tests): adapt testing framework to session based [no changelog]

--- tests/conftest.py
+++ tests/conftest.py
@@ -304,11 +304,11 @@ def _client_unlocked(
         should_format = sd_marker.kwargs.get("formatted", True)
         _raw_client.debug.erase_sd_card(format=should_format)
 
-        if _raw_client.is_invalidated:
-            _raw_client = _raw_client.get_new_client()
-        session = _raw_client.get_seedless_session()
-        wipe_device(session)
-        sleep(1.5)  # Makes tests more stable (wait for wipe to finish)
+    if _raw_client.is_invalidated:
+        _raw_client = _raw_client.get_new_client()
+    session = _raw_client.get_seedless_session()
+    wipe_device(session)
+    sleep(1.5)  # Makes tests more stable (wait for wipe to finish)
 
     if not _raw_client.features.bootloader_mode:
         _raw_client.refresh_features()
