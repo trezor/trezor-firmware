dfc03b4d235f4d3d5211cb523c2f0cd7557aabb7
Lukas Bielesch <bielesch.lukas@gmail.com>
2025-03-06 13:14:25 +0100

    feat(eckhart): receive address flow

--- core/embed/rust/src/ui/layout_eckhart/flow/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/flow/mod.rs
@@ -1,9 +1,11 @@
 pub mod confirm_reset;
+pub mod get_address;
 pub mod prompt_backup;
 pub mod request_passphrase;
 pub mod show_share_words;
 
 pub use confirm_reset::new_confirm_reset;
+pub use get_address::GetAddress;
 pub use prompt_backup::PromptBackup;
 pub use request_passphrase::RequestPassphrase;
 pub use show_share_words::new_show_share_words_flow;
