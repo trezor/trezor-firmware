fe84100dbf35a519a146d2e03f3d8e60122d1fa2
obrusvit <vit.obrusnik@gmail.com>
2025-03-10 17:58:21 +0100

    chore(eckhart): delete old bootloader components

--- core/embed/rust/src/ui/layout_eckhart/component/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/component/mod.rs
@@ -1,9 +1,7 @@
 mod button;
 mod error;
-mod result;
 mod welcome_screen;
 
 pub use button::{Button, ButtonContent, ButtonMsg, ButtonStyle, ButtonStyleSheet, IconText};
 pub use error::ErrorScreen;
-pub use result::{ResultFooter, ResultScreen, ResultStyle};
 pub use welcome_screen::WelcomeScreen;
