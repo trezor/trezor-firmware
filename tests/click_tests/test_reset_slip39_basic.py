44490a51e7d63d8e4f28f8b0757cf9b03ca1bbaf
obrusvit <vit.obrusnik@gmail.com>
2025-03-10 15:08:13 +0100

    fix(core): fix Delizia request_number dialog
    - NumberInputDialog now reacts to tap to continue
    - change the types there to u16 to be consistent with NumberInputSlider

--- tests/click_tests/test_reset_slip39_basic.py
+++ tests/click_tests/test_reset_slip39_basic.py
@@ -97,7 +97,8 @@ def test_reset_slip39_basic(
     if num_of_shares == 1 and threshold == 1:
         reset.set_selection(debug, buttons.reset_plus(model_name), 0)
     elif num_of_shares == 16 and threshold == 16:
-        reset.set_selection(debug, buttons.reset_plus(model_name), 11)
+        # set threshold - dialog starts at 9
+        reset.set_selection(debug, buttons.reset_plus(model_name), 7)
     else:
         raise RuntimeError("not a supported combination")
 
