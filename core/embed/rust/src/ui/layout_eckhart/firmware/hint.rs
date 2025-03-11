f77ff4dbcc59a1a5a6f1f904f49fd41d5fa228a8
obrusvit <vit.obrusnik@gmail.com>
2025-03-11 15:35:01 +0100

    feat(eckhart): implement several FirmwareUI fns
    - confirm_emphasize
    - show_error
    - homescreen ButtonContent
    - homescreen and lockscreen
    - HomeScreen, ConfirmHomescreen full-screen components
    - confirm_set_new_pin
    - fix clippy and style

--- core/embed/rust/src/ui/layout_eckhart/firmware/hint.rs
+++ core/embed/rust/src/ui/layout_eckhart/firmware/hint.rs
@@ -60,12 +60,33 @@ impl<'a> Hint<'a> {
             Instruction::new(text.into(), theme::GREY, icon, Some(theme::GREY_LIGHT));
         Self::from_content(HintContent::Instruction(instruction_component))
     }
+
     pub fn new_instruction_green<T: Into<TString<'static>>>(text: T, icon: Option<Icon>) -> Self {
         let instruction_component = Instruction::new(
             text.into(),
             theme::GREEN_LIME,
             icon,
             Some(theme::GREEN_LIME),
+        );
+        Self::from_content(HintContent::Instruction(instruction_component))
+    }
+
+    pub fn new_warning<T: Into<TString<'static>>>(text: T) -> Self {
+        let instruction_component = Instruction::new(
+            text.into(),
+            theme::GREY,
+            Some(theme::ICON_INFO),
+            Some(theme::ORANGE),
+        );
+        Self::from_content(HintContent::Instruction(instruction_component))
+    }
+
+    pub fn new_warning_danger<T: Into<TString<'static>>>(text: T) -> Self {
+        let instruction_component = Instruction::new(
+            text.into(),
+            theme::RED,
+            Some(theme::ICON_WARNING),
+            Some(theme::RED),
         );
         Self::from_content(HintContent::Instruction(instruction_component))
     }
@@ -89,13 +110,10 @@ impl<'a> Hint<'a> {
     }
 
     pub fn update(&mut self, pager: Pager) {
-        match &mut self.content {
-            HintContent::PageCounter(counter) => {
-                counter.update(pager);
-                self.swipe_allow_down = counter.pager.is_first();
-                self.swipe_allow_up = counter.pager.is_last();
-            }
-            _ => {}
+        if let HintContent::PageCounter(counter) = &mut self.content {
+            counter.update(pager);
+            self.swipe_allow_down = counter.pager.is_first();
+            self.swipe_allow_up = counter.pager.is_last();
         }
     }
 
@@ -113,15 +131,12 @@ impl<'a> Component for Hint<'a> {
         debug_assert!(bounds.width() == screen().width());
         debug_assert!(bounds.height() == self.content.height());
         let bounds = bounds.inset(Self::HINT_INSETS);
-        match &mut self.content {
-            HintContent::Instruction(instruction) => {
-                let text_area = match instruction.icon {
-                    Some(_) => bounds.split_left(instruction.icon_width()).1,
-                    None => bounds,
-                };
-                instruction.label.place(text_area);
-            }
-            _ => {}
+        if let HintContent::Instruction(instruction) = &mut self.content {
+            let text_area = match instruction.icon {
+                Some(_) => bounds.split_left(instruction.icon_width()).1,
+                None => bounds,
+            };
+            instruction.label.place(text_area);
         }
         self.area = bounds;
         self.area
@@ -222,7 +237,7 @@ impl<'a> Instruction<'a> {
         icon: Option<Icon>,
         icon_color: Option<Color>,
     ) -> Self {
-        let mut text_style = Self::STYLE_INSTRUCTION.clone();
+        let mut text_style = *Self::STYLE_INSTRUCTION;
         text_style.text_color = text_color;
         Self {
             label: Label::left_aligned(text, text_style).vertically_centered(),
