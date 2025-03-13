f86d9af364ef51780e66aa6ebae4a89b87f188f5
obrusvit <vit.obrusnik@gmail.com>
2025-03-13 23:46:13 +0100

    feat(eckhart): implement homescreen and lockscreen
    - HomeScreen, ConfirmHomescreen full-screen components
    - respective FirmwareUI implementation

--- core/embed/rust/src/ui/layout_eckhart/constant.rs
+++ core/embed/rust/src/ui/layout_eckhart/constant.rs
@@ -5,13 +5,8 @@ use crate::trezorhal::display::{DISPLAY_RESX, DISPLAY_RESY};
 pub const WIDTH: i16 = DISPLAY_RESX as _;
 pub const HEIGHT: i16 = DISPLAY_RESY as _;
 
-// TODO: below constants copied from mercury
 pub const LINE_SPACE: i16 = 4;
 pub const FONT_BPP: i16 = 4;
-
-pub const LOADER_OUTER: i16 = 60;
-pub const LOADER_INNER: i16 = 52;
-pub const LOADER_ICON_MAX_SIZE: i16 = 64;
 
 pub const fn size() -> Offset {
     Offset::new(WIDTH, HEIGHT)
