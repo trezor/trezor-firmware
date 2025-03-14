b41c19d42af1ab47df46f859516b4c884d6675d6
Martin Milata <martin@martinmilata.cz>
2025-03-14 21:44:45 +0100

    fixup! chore(tests): adapt testing framework to session based [no changelog]

--- python/src/trezorlib/debuglink.py
+++ python/src/trezorlib/debuglink.py
@@ -1471,6 +1471,10 @@ class TrezorClientDebugLink(TrezorClient):
 
         raise RuntimeError("Unexpected call")
 
+    def reset_protocol(self):
+        super().reset_protocol()
+        self._seedless_session = self.get_seedless_session(new_session=True)
+
 
 def load_device(
     session: "Session",
