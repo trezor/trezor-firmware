e49c56328e60523e7eb995545ed334ec7fe6dba7
Lukas Bielesch <bielesch.lukas@gmail.com>
2025-03-17 09:07:56 +0100

    chore(eckhart): make recovery notifications layout-specific

--- core/src/apps/management/recovery_device/layout.py
+++ core/src/apps/management/recovery_device/layout.py
@@ -3,7 +3,11 @@ from typing import TYPE_CHECKING
 from trezor import TR
 from trezor.ui.layouts.recovery import (  # noqa: F401
     request_word_count,
+    show_already_added,
+    show_dry_run_result,
     show_group_share_success,
+    show_group_thresholod,
+    show_identifier_mismatch,
     show_recovery_warning,
 )
 
@@ -66,64 +70,18 @@ async def request_mnemonic(
             word_validity.check(backup_type, non_empty_words)
         except word_validity.AlreadyAdded:
             # show_share_already_added
-            await show_recovery_warning(
-                "warning_known_share",
-                TR.recovery__share_already_entered,
-                TR.recovery__enter_different_share,
-            )
+            await show_already_added()
             return None
         except word_validity.IdentifierMismatch:
             # show_identifier_mismatch
-            await show_recovery_warning(
-                "warning_mismatched_share",
-                "",
-                TR.recovery__share_from_another_multi_share_backup,
-            )
+            await show_identifier_mismatch()
             return None
         except word_validity.ThresholdReached:
             # show_group_threshold_reached
-            await show_recovery_warning(
-                "warning_group_threshold",
-                TR.recovery__group_threshold_reached,
-                TR.recovery__enter_share_from_diff_group,
-            )
+            await show_group_thresholod()
             return None
 
     return " ".join(words)
-
-
-async def show_dry_run_result(result: bool, is_slip39: bool) -> None:
-    from trezor.ui.layouts import show_success
-
-    if result:
-        if is_slip39:
-            text = TR.recovery__dry_run_slip39_valid_match
-        else:
-            text = TR.recovery__dry_run_bip39_valid_match
-        await show_success("success_dry_recovery", text, button=TR.buttons__continue)
-    else:
-        if is_slip39:
-            text = TR.recovery__dry_run_slip39_valid_mismatch
-        else:
-            text = TR.recovery__dry_run_bip39_valid_mismatch
-        await show_recovery_warning(
-            "warning_dry_recovery", "", text, button=TR.buttons__continue
-        )
-
-
-async def show_invalid_mnemonic(word_count: int) -> None:
-    if backup_types.is_slip39_word_count(word_count):
-        await show_recovery_warning(
-            "warning_invalid_share",
-            TR.words__please_try_again,
-            TR.recovery__invalid_share_entered,
-        )
-    else:
-        await show_recovery_warning(
-            "warning_invalid_seed",
-            TR.words__please_try_again,
-            TR.recovery__invalid_wallet_backup_entered,
-        )
 
 
 def enter_share(
