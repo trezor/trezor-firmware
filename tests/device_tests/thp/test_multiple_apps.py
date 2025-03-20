0b5d2df333f6e80c3c9cc09b624a085558e2399b
M1nd3r <petrsedlacek.km@seznam.cz>
2025-03-20 15:36:42 +0100

    chore(python): test fallback in handshake

--- tests/device_tests/thp/test_multiple_apps.py
+++ tests/device_tests/thp/test_multiple_apps.py
@@ -13,6 +13,7 @@ def test_multiple_hosts(client: Client) -> None:
     assert isinstance(client.protocol, ProtocolV2Channel)
     protocol_1 = client.protocol
     protocol_2 = ProtocolV2Channel(protocol_1.transport, protocol_1.mapping)
+    protocol_2._reset_sync_bits()
 
     nonce_1 = os.urandom(8)
     nonce_2 = os.urandom(8)
@@ -34,7 +35,29 @@ def test_multiple_hosts(client: Client) -> None:
     protocol_1._read_ack()
     protocol_1._read_handshake_init_response()
 
+    protocol_2._send_handshake_init_request()
+
+    with pytest.raises(Exception) as e:
+        protocol_2._read_ack()
+    assert e.value.args[0] == "Received ThpError: TRANSPORT BUSY"
+
     time.sleep(0.2)  # To pass LOCK_TIME
+    protocol_2._init_noise()
     protocol_2._send_handshake_init_request()
     protocol_2._read_ack()
-    # protocol_2._read_handshake_init_response()
+    protocol_2._read_handshake_init_response()
+
+    protocol_2._send_handshake_completion_request()
+    protocol_2._read_ack()
+    protocol_2._read_handshake_completion_response()
+
+    protocol_2._do_pairing(helper_debug=client.debug)
+
+    protocol_1._send_handshake_completion_request()
+    protocol_1._read_ack()
+
+    with pytest.raises(Exception) as e:
+        protocol_1._read_handshake_completion_response()
+    assert e.value.args[0] == "Received ThpError: UNALLOCATED CHANNEL"
+
+    # TODO - test ACK fallback, test standard encrypted message fallback
