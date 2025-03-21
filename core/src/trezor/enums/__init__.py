bd44702f346331afb03a806431d421bf3a8c20e5
M1nd3r <petrsedlacek.km@seznam.cz>
2025-03-21 13:51:56 +0100

    chore: unify FailureType enum across branches

--- core/src/trezor/enums/__init__.py
+++ core/src/trezor/enums/__init__.py
@@ -39,10 +39,10 @@ if TYPE_CHECKING:
         PinMismatch = 12
         WipeCodeMismatch = 13
         InvalidSession = 14
-        ThpUnallocatedSession = 15
-        InvalidProtocol = 16
-        BufferError = 17
-        DeviceIsBusy = 18
+        DeviceIsBusy = 15
+        ThpUnallocatedSession = 16
+        InvalidProtocol = 17
+        BufferError = 18
         FirmwareError = 99
 
     class ButtonRequestType(IntEnum):
