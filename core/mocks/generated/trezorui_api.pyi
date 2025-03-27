2f53fb8d407a35aa8a1619914282eb6f54da5edc
tychovrahe <brunam@seznam.cz>
2025-03-27 19:10:44 +0100

    fix(core): make ble_event upy api function optional
    [no changelog]

--- core/mocks/generated/trezorui_api.pyi
+++ core/mocks/generated/trezorui_api.pyi
@@ -22,12 +22,13 @@ class LayoutObj(Generic[T]):
     if utils.USE_BUTTON:
         def button_event(self, event: int, button: int) -> LayoutState | None:
             """Receive a button event `event` for button `button`."""
+    if utils.USE_BLE:
+        def ble_event(self, event: int, data: bytes) -> LayoutState | None:
+            """Receive a BLE events."""
     def progress_event(self, value: int, description: str) -> LayoutState | None:
         """Receive a progress event."""
     def usb_event(self, connected: bool) -> LayoutState | None:
         """Receive a USB connect/disconnect event."""
-    def ble_event(self, event: tuple[int, int | None]) -> LayoutState | None:
-        """Receive a BLE event."""
     def timer(self, token: int) -> LayoutState | None:
         """Callback for the timer set by `attach_timer_fn`.
         This function should be called by the executor after the corresponding
