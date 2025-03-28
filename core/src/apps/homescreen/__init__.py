243caa7ab46da3e1eb85f47e7621bd1f0f4238a6
Ioan Bizău <i@ibz.me>
2025-03-28 13:52:36 +0100

    feat(eckhart): introduce the device menu

--- core/src/apps/homescreen/__init__.py
+++ core/src/apps/homescreen/__init__.py
@@ -5,7 +5,7 @@ import storage.cache
 import storage.device
 from trezor import config, wire
 from trezor.enums import MessageType
-from trezor.ui.layouts import error_popup, raise_if_not_confirmed
+from trezor.ui.layouts import raise_if_not_confirmed
 from trezor.ui.layouts.homescreen import Busyscreen, Homescreen, Lockscreen
 
 from apps.base import busy_expiry_ms, lock_device
@@ -22,7 +22,7 @@ async def busyscreen() -> None:
 
 async def homescreen() -> None:
     from trezor import TR
-    from trezorui_api import INFO
+    from trezorui_api import INFO, show_device_menu
 
     if storage.device.is_initialized():
         label = storage.device.get_label()
@@ -57,15 +57,37 @@ async def homescreen() -> None:
     try:
         res = await obj.get_result()
         if res is INFO:
-            # trigger device menu
-            await raise_if_not_confirmed(
-                error_popup(
-                    "Not implemented",
-                    "DeviceMenu not yet implemented",
-                    button="OK then..",
+
+            # MOCK DATA
+            failed_backup = True
+            battery_percentage = 22
+            paired_devices = ["Suite on my de-Googled Phone"]
+            #
+
+            menu_result = await raise_if_not_confirmed(
+                show_device_menu(
+                    failed_backup=failed_backup,
+                    battery_percentage=battery_percentage,
+                    paired_devices=paired_devices,
                 ),
                 "device_menu",
             )
+            print(menu_result)
+            if menu_result == "DevicePair":
+                import trezorui_api
+
+                await raise_if_not_confirmed(
+                    trezorui_api.show_pairing_device_name(
+                        device_name="My Trez",
+                    ),
+                    "device_name",
+                )
+                await raise_if_not_confirmed(
+                    trezorui_api.show_pairing_code(
+                        code="123456",
+                    ),
+                    "pairing_code",
+                )
         else:
             lock_device()
     finally:
