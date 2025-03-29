5cddd1ab4e1068ac4c287473d86245ce69d7eb3b
obrusvit <vit.obrusnik@gmail.com>
2025-03-29 13:35:00 +0100

    feat(eckhart): improve pagination of ActionBar
    - Single mode now handles pagination with the right_button shown at the
    last page
    - new PaginateOnly mode to only render navigation buttons without
    cancel/confirm buttons

--- core/embed/rust/src/ui/layout_eckhart/theme/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/theme/mod.rs
@@ -155,9 +155,9 @@ pub const fn button_default() -> ButtonStyleSheet {
         },
         disabled: &ButtonStyle {
             font: fonts::FONT_SATOSHI_MEDIUM_26,
-            text_color: GREY_LIGHT,
+            text_color: GREY,
             button_color: BG,
-            icon_color: GREY_LIGHT,
+            icon_color: GREY,
             background_color: BG,
         },
     }
