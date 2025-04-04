b296eb7da5f039fae8f589c21e1aa5b98e0d3b12
Lukas Bielesch <bielesch.lukas@gmail.com>
2025-04-04 15:24:41 +0200

    feat(eckhart): implement trait functions: confirm_value_intro, confirm_modify_output, confirm_modify_fee, confirm_with_menu, show_info_with_cancel and confirm_props

---
+++ core/embed/rust/src/ui/layout_eckhart/flow/confirm_value_intro.rs
@@ -0,0 +1,133 @@
+use crate::{
+    error,
+    micropython::obj::Obj,
+    strutil::TString,
+    translations::TR,
+    ui::{
+        component::{
+            text::paragraphs::{Paragraph, ParagraphSource},
+            ComponentExt,
+        },
+        flow::{
+            base::{Decision, DecisionBuilder as _},
+            FlowController, FlowMsg, SwipeFlow,
+        },
+        geometry::{Alignment, Direction, LinearPlacement, Offset},
+        layout::util::StrOrBytes,
+    },
+};
+
+use super::super::{
+    component::Button,
+    firmware::{
+        ActionBar, Header, ShortMenuVec, TextScreen, TextScreenMsg, VerticalMenu,
+        VerticalMenuScreen, VerticalMenuScreenMsg,
+    },
+    theme,
+};
+
+const TIMEOUT_MS: u32 = 2000;
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub enum ConfirmValueIntro {
+    Intro,
+    Menu,
+}
+
+impl FlowController for ConfirmValueIntro {
+    #[inline]
+    fn index(&'static self) -> usize {
+        *self as usize
+    }
+
+    fn handle_swipe(&'static self, _direction: Direction) -> Decision {
+        self.do_nothing()
+    }
+
+    fn handle_event(&'static self, msg: FlowMsg) -> Decision {
+        match (self, msg) {
+            (Self::Intro, FlowMsg::Confirmed) => self.return_msg(FlowMsg::Confirmed),
+            (Self::Intro, FlowMsg::Info) => Self::Menu.goto(),
+            (Self::Menu, FlowMsg::Choice(0)) => self.return_msg(FlowMsg::Info),
+            (Self::Menu, FlowMsg::Choice(1)) => self.return_msg(FlowMsg::Cancelled),
+            (Self::Menu, FlowMsg::Cancelled) => Self::Intro.goto(),
+            _ => self.do_nothing(),
+        }
+    }
+}
+
+#[allow(clippy::too_many_arguments)]
+pub fn new_confirm_value_intro(
+    title: TString<'static>,
+    subtitle: Option<TString<'static>>,
+    value: Obj,
+    value_menu_label: TString<'static>,
+    cancel_menu_label: Option<TString<'static>>,
+    confirm_button_label: Option<TString<'static>>,
+    hold: bool,
+    chunkify: bool,
+) -> Result<SwipeFlow, error::Error> {
+    let cancel_menu_label = cancel_menu_label.unwrap_or(TR::buttons__cancel.into());
+
+    // Intro
+    let mut confirm_button = Button::with_text(
+        confirm_button_label.unwrap_or(TR::sign_message__confirm_without_review.into()),
+    )
+    .styled(theme::button_confirm());
+    if hold {
+        confirm_button = confirm_button.with_long_press(theme::LOCK_HOLD_DURATION);
+    }
+
+    let value = if value != Obj::const_none() {
+        unwrap!(value.try_into())
+    } else {
+        StrOrBytes::Str("".into())
+    };
+
+    let intro_style = if chunkify {
+        &theme::TEXT_MONO_ADDRESS_CHUNKS
+    } else {
+        &theme::TEXT_MONO_ADDRESS
+    };
+    let content_intro = TextScreen::new(
+        Paragraph::new(intro_style, value.as_str_offset(0))
+            .into_paragraphs()
+            .with_placement(LinearPlacement::vertical()),
+    )
+    .with_page_limit(1)
+    .with_header(Header::new(title).with_menu_button())
+    .with_subtitle(subtitle.unwrap_or(TString::empty()))
+    .with_action_bar(ActionBar::new_single(confirm_button))
+    .map(|msg| match msg {
+        TextScreenMsg::Confirmed => Some(FlowMsg::Confirmed),
+        TextScreenMsg::Cancelled => Some(FlowMsg::Cancelled),
+        TextScreenMsg::Menu => Some(FlowMsg::Info),
+    });
+
+    let menu_items = VerticalMenu::<ShortMenuVec>::empty()
+        .with_item(
+            Button::with_text(value_menu_label)
+                .styled(theme::menu_item_title())
+                .with_text_align(Alignment::Start)
+                .with_content_offset(Offset::x(12)),
+        )
+        .with_item(
+            Button::with_text(cancel_menu_label)
+                .styled(theme::menu_item_title_orange())
+                .with_text_align(Alignment::Start)
+                .with_content_offset(Offset::x(12)),
+        );
+
+    let content_menu = VerticalMenuScreen::new(menu_items)
+        .with_header(Header::new(TString::empty()).with_close_button())
+        .map(move |msg| match msg {
+            VerticalMenuScreenMsg::Selected(i) => Some(FlowMsg::Choice(i)),
+            VerticalMenuScreenMsg::Close => Some(FlowMsg::Cancelled),
+            _ => None,
+        });
+
+    let mut res = SwipeFlow::new(&ConfirmValueIntro::Intro)?;
+    res.add_page(&ConfirmValueIntro::Intro, content_intro)?
+        .add_page(&ConfirmValueIntro::Menu, content_menu)?;
+    Ok(res)
+}
