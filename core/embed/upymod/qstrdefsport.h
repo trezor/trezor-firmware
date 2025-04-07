879cc69c6308ab9282d2248a21e7700f55abc785
Roman Zeyde <roman.zeyde@satoshilabs.com>
2025-04-07 14:09:21 +0300

    perf(core): intern non-alphanumeric characters
    It was found by running `micropython.mem_info()` with the following patch:
    ```diff
    diff --git a/py/gc.c b/py/gc.c
    index 604334c95..bff62b754 100644
    --- a/py/gc.c
    +++ b/py/gc.c
    @@ -840,6 +840,7 @@ void gc_dump_info(void) {
             (uint)info.total, (uint)info.used, (uint)info.free);
         mp_printf(&mp_plat_print, " No. of 1-blocks: %u, 2-blocks: %u, max blk sz: %u, max free sz: %u\n",
             (uint)info.num_1block, (uint)info.num_2block, (uint)info.max_block, (uint)info.max_free);
    +    qstr_dump_data();
     }
    
     void gc_dump_alloc_table(void) {
    ```
    
    New QSTR definitions:
    ```
    $ tail -n4 ./core/build/unix/genhdr/qstrdefs.generated.h
    QDEF(MP_QSTR__paren_close_, 46476, 1, ")")
    QDEF(MP_QSTR__paren_open_, 46477, 1, "(")
    QDEF(MP_QSTR__lt_, 46489, 1, "<")
    QDEF(MP_QSTR__semicolon_, 46494, 1, ";")
    ```
    
    [no changelog]

---
+++ core/embed/upymod/qstrdefsport.h
@@ -0,0 +1,8 @@
+// Some additional strings that can’t be expressed using the `MP_QSTR_Foo`
+// syntax:
+// https://docs.micropython.org/en/latest/develop/qstr.html#compile-time-qstr-generation
+
+Q())
+Q(()
+Q(<)
+Q(;)
