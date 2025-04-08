6473b3ca487caa76cbf0b5b9ed90bbcf0ffc4cd0
Roman Zeyde <roman.zeyde@satoshilabs.com>
2025-04-08 15:49:28 +0300

    feat(vendor): update MicroPython to support sorted qstr pools
    Requires https://github.com/trezor/micropython/pull/20.
    
    [no changelog]

--- core/embed/rust/qstr.h
+++ core/embed/rust/qstr.h
@@ -1,5 +1,21 @@
 enum Qstr {
-#define QDEF(id, hash, len, str) id,
+
+// Copied from `vendor/micropython/py/qstr.h`:
+
+#define QDEF0(id, hash, len, str) id,
+#define QDEF1(id, hash, len, str)
 #include "genhdr/qstrdefs.generated.h"
-#undef QDEF
+#undef QDEF0
+#undef QDEF1
+  MP_QSTRnumber_of_static,
+  // unused but shifts the enum counter back one
+  MP_QSTRstart_of_main = MP_QSTRnumber_of_static - 1,
+
+#define QDEF0(id, hash, len, str)
+#define QDEF1(id, hash, len, str) id,
+#include "genhdr/qstrdefs.generated.h"
+#undef QDEF0
+#undef QDEF1
+  // no underscore so it can't clash with any of the above
+  MP_QSTRnumber_of,
 };
