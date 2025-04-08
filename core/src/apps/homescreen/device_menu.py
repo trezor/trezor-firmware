9474a4b3f7a1f113732b4edfe0bd0b11394c847d
Martin Milata <martin@martinmilata.cz>
2025-04-08 17:05:30 +0200

    feat(core): BLE pairing flow

--- core/src/apps/homescreen/device_menu.py
+++ core/src/apps/homescreen/device_menu.py
@@ -1,4 +1,5 @@
 import storage.device
+import trezorble as ble
 import trezorui_api
 from trezor import utils
 from trezor.ui.layouts import raise_if_not_confirmed
@@ -8,7 +9,7 @@ async def handle_device_menu() -> None:
     # MOCK DATA
     failed_backup = True
     battery_percentage = 22
-    paired_devices = ["Trezor Suite"]
+    paired_devices = ["Trezor Suite"] if ble.is_connected() else []
     # ###
     firmware_version = ".".join(map(str, utils.VERSION))
     device_name = storage.device.get_label() or "Trezor"
@@ -27,5 +28,11 @@ async def handle_device_menu() -> None:
         from apps.management.ble.pair_new_device import pair_new_device
 
         await pair_new_device()
+    elif menu_result == "DeviceDisconnect":
+        from trezor.messages import BleUnpair
+
+        from apps.management.ble.unpair import unpair
+
+        await unpair(BleUnpair(all=False))
     else:
         raise RuntimeError(f"Unknown menu {menu_result}")
