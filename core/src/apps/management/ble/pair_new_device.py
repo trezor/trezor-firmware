e4ac52c65a62829a7456efc55685cc3587c5e951
Martin Milata <martin@martinmilata.cz>
2025-04-08 17:05:30 +0200

    feat(core): BLE pairing flow

--- core/src/apps/management/ble/pair_new_device.py
+++ core/src/apps/management/ble/pair_new_device.py
@@ -1,17 +1,39 @@
+import trezorble as ble
 import trezorui_api
-from trezor.ui.layouts import interact
+from storage import device as storage_device
+from trezor.ui.layouts import CONFIRMED, raise_if_not_confirmed
+from trezor.wire import ActionCancelled
+
+
+def _end_pairing() -> None:
+    if ble.peer_count() > 0:
+        ble.start_advertising(True, storage_device.get_label())
+    else:
+        ble.stop_advertising()
 
 
 async def pair_new_device() -> None:
-    label = "Trezor T3W1"
-    await interact(
-        trezorui_api.show_pairing_device_name(device_name=label),
-        None,
-        raise_on_cancel=None,  # for UI testing
-    )
+    label = storage_device.get_label() or "Trezor T3W1"
+    ble.start_advertising(False, label)
+    try:
+        code = await raise_if_not_confirmed(
+            trezorui_api.show_pairing_device_name(
+                device_name=label,
+            ),
+            None,
+        )
+        if not isinstance(code, int):
+            raise ActionCancelled
 
-    code = 12345
-    await interact(
-        trezorui_api.show_pairing_code(code=f"{code:0>6}"),
-        None,
-    )
+        result = await raise_if_not_confirmed(
+            trezorui_api.show_pairing_code(
+                code=f"{code:0>6}",
+            ),
+            None,
+        )
+        if result is CONFIRMED:
+            ble.allow_pairing(code)
+        else:
+            ble.reject_pairing()
+    finally:
+        _end_pairing()
