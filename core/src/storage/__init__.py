76fdafa3809501cf9263a768f4917fa926128239
M1nd3r <petrsedlacek.km@seznam.cz>
2025-04-17 17:46:00 +0200

    chore(storage): make device secret and credential key counter persist reset [no changelog]

--- core/src/storage/__init__.py
+++ core/src/storage/__init__.py
@@ -40,12 +40,23 @@ def init_unlocked() -> None:
 
 def reset(excluded: Tuple[bytes, bytes] | None) -> None:
     """
-    Wipes storage but keeps the device id unchanged.
+    Wipes storage but keeps the device id, device secret, and credential counter unchanged.
     """
+    from trezor import utils
+
     device_id = device.get_device_id()
+
+    if utils.USE_THP:
+        device_secret = device.get_device_secret()
+        credential_counter = device.get_cred_auth_key_counter()
     wipe(clear_cache=False)
     wipe_cache(excluded)
     common.set(common.APP_DEVICE, device.DEVICE_ID, device_id.encode(), public=True)
+    if utils.USE_THP:
+        common.set(common.APP_DEVICE, device.DEVICE_SECRET, device_secret, True)
+        common.set(
+            common.APP_DEVICE, device.CRED_AUTH_KEY_COUNTER, credential_counter, True
+        )
 
 
 def _migrate_from_version_01() -> None:
