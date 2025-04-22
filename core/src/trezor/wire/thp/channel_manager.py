2d9008bc70e254a13a1f4fceeee48775fae92e7a
M1nd3r <petrsedlacek.km@seznam.cz>
2025-04-22 15:51:54 +0200

    fix(core): disable unwanted replacing of channels

--- core/src/trezor/wire/thp/channel_manager.py
+++ core/src/trezor/wire/thp/channel_manager.py
@@ -8,6 +8,10 @@ from .channel import Channel
 if TYPE_CHECKING:
     from trezorio import WireInterface
 
+if __debug__:
+    from trezor import log
+
+CHANNELS_LOADED: bool = False
 
 def create_new_channel(iface: WireInterface) -> Channel:
     """
@@ -19,12 +23,21 @@ def create_new_channel(iface: WireInterface) -> Channel:
     return channel
 
 
-def load_cached_channels() -> dict[int, Channel]:
+def load_cached_channels(channels_dict: dict[int, Channel]) -> None:
     """
     Returns all allocated channels from cache.
     """
-    channels: dict[int, Channel] = {}
+    global CHANNELS_LOADED
+
+    if CHANNELS_LOADED:
+        if __debug__:
+            log.debug(__name__, "Channels already loaded, process skipped.")
+        return
+
     cached_channels = cache_thp.get_all_allocated_channels()
     for channel in cached_channels:
-        channels[int.from_bytes(channel.channel_id, "big")] = Channel(channel)
-    return channels
+        channel_id = int.from_bytes(channel.channel_id, "big")
+        channels_dict[channel_id] = Channel(channel)
+    if __debug__:
+        log.debug(__name__, "Channels loaded from cache.")
+    CHANNELS_LOADED = True
