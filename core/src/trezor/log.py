af17735486951db191d17b9080deb5708e4ced27
M1nd3r <petrsedlacek.km@seznam.cz>
2025-04-28 10:41:28 +0200

    chore(core): simplify exception func in log.py [no changelog]

--- core/src/trezor/log.py
+++ core/src/trezor/log.py
@@ -56,15 +56,14 @@ def exception(name: str, exc: BaseException) -> None:
     # we also need to instruct typechecker to ignore the missing argument
     # in ui.Result exception
     if exc.__class__.__name__ == "Result":
-        _log(
+        debug(
             name,
-            _DEBUG,
             "ui.Result: %s",
             exc.value,  # type: ignore [Cannot access attribute "value" for class "BaseException"]
         )
     elif exc.__class__.__name__ == "Cancelled":
-        _log(name, _DEBUG, "ui.Cancelled")
+        debug(name, "ui.Cancelled")
     else:
-        _log(name, _ERROR, "exception:")
+        error(name, "exception:")
         # since mypy 0.770 we cannot override sys, so print_exception is unknown
         sys.print_exception(exc)  # type: ignore ["print_exception" is not a known attribute of module]
