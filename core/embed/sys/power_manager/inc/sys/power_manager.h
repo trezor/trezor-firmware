b49b7439a7a9f5e79ab70bf02e375e21fbe722bf
kopecdav <david.kopecky@satoshilabs.com>
2025-04-30 19:48:34 +0200

    feat(core): integrate data recovery from backup ram [no changelog]

--- core/embed/sys/power_manager/inc/sys/power_manager.h
+++ core/embed/sys/power_manager/inc/sys/power_manager.h
@@ -82,7 +82,7 @@ typedef struct {
 } pm_report_t;
 
 /* Public API functions */
-pm_status_t pm_init(bool turned_on);
+pm_status_t pm_init(bool skip_boot_sequence);
 void pm_deinit(void);
 pm_status_t pm_get_events(pm_event_t* event_flags);
 pm_status_t pm_get_state(pm_state_t* state);
