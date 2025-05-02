1fdd398d40691899c39fc88197abe3747aa22564
cepetr <petr@cerninovi.cz>
2025-05-02 15:24:54 +0200

    refactor(core): prepare for secure/non-secure memory layout
    [no changelog]

--- core/embed/models/T2T1/model_T2T1.h
+++ core/embed/models/T2T1/model_T2T1.h
@@ -1,5 +1,23 @@
-#ifndef MODELS_MODEL_T2T1_H_
-#define MODELS_MODEL_T2T1_H_
+/*
+ * This file is part of the Trezor project, https://trezor.io/
+ *
+ * Copyright (c) SatoshiLabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 
 #include "bootloaders/bootloader_hashes.h"
 
@@ -29,102 +47,7 @@
 #define DISPLAY_JUMP_BEHAVIOR DISPLAY_RETAIN_CONTENT
 #define RSOD_INFINITE_LOOP 1
 
-// SHARED WITH MAKEFILE
-// common
-
-#define FLASH_START 0x08000000
 #define NORCOW_SECTOR_SIZE (1 * 64 * 1024)  // 64 kB
 #define NORCOW_MIN_VERSION 0x00000000
 
-// FLASH layout
-#define BOARDLOADER_START 0x08000000
-#define BOARDLOADER_MAXSIZE (3 * 16 * 1024)  // 48 kB
-#define BOARDLOADER_SECTOR_START 0
-#define BOARDLOADER_SECTOR_END 2
-
-#define BOARDCAPS_START 0x0800BF00
-#define BOARDCAPS_MAXSIZE 0x100
-
-#define UNUSED_1_START 0x0800C000
-#define UNUSED_1_MAXSIZE (1 * 16 * 1024)  // 16 kB
-#define UNUSED_1_SECTOR_START 3
-#define UNUSED_1_SECTOR_END 3
-
-#define STORAGE_1_START 0x08010000
-#define STORAGE_1_MAXSIZE (1 * 64 * 1024)  // 64 kB
-#define STORAGE_1_SECTOR_START 4
-#define STORAGE_1_SECTOR_END 4
-
-#define BOOTLOADER_START 0x08020000
-#define BOOTLOADER_MAXSIZE (1 * 128 * 1024)  // 128 kB
-#define BOOTLOADER_SECTOR_START 5
-#define BOOTLOADER_SECTOR_END 5
-
-#define FIRMWARE_START 0x08040000
-#define FIRMWARE_MAXSIZE (13 * 128 * 1024)  // 1664 kB
-#define FIRMWARE_P1_START 0x08040000
-#define FIRMWARE_P1_MAXSIZE (6 * 128 * 1024)
-#define FIRMWARE_P1_SECTOR_START 6
-#define FIRMWARE_P1_SECTOR_END 11
-// part of firmware P1
-#define KERNEL_START 0x08040000
-#define KERNEL_MAXSIZE (4 * 128 * 1024)
-
-#define ASSETS_START 0x08100000
-#define ASSETS_MAXSIZE (3 * 16 * 1024)  // 48 kB
-#define ASSETS_SECTOR_START 12
-#define ASSETS_SECTOR_END 14
-
-#define UNUSED_2_START 0x0810C000
-#define UNUSED_2_MAXSIZE (1 * 16 * 1024)  // 16 kB
-#define UNUSED_2_SECTOR_START 15
-#define UNUSED_2_SECTOR_END 15
-
-#define STORAGE_2_START 0x08110000
-#define STORAGE_2_MAXSIZE (1 * 64 * 1024)  // 64 kB
-#define STORAGE_2_SECTOR_START 16
-#define STORAGE_2_SECTOR_END 16
-
-#define FIRMWARE_P2_START 0x08120000
-#define FIRMWARE_P2_MAXSIZE (7 * 128 * 1024)
-#define FIRMWARE_P2_SECTOR_START 17
-#define FIRMWARE_P2_SECTOR_END 23
-
-// Ram layout - shared boardloader, bootloader, prodtest
-#define S_MAIN_STACK_START 0x10000000
-#define S_MAIN_STACK_SIZE (16 * 1024)
-
-#define S_FB1_RAM_START 0x10004000
-#define S_FB1_RAM_SIZE (0)
-
-#define S_MAIN_RAM_START 0x10004000
-#define S_MAIN_RAM_SIZE (48 * 1024 - 0x100)
-
-// RAM layout - kernel
-#define K_MAIN_STACK_START 0x10000000
-#define K_MAIN_STACK_SIZE (8 * 1024)
-
-#define K_FB1_RAM_START 0x1000C000
-#define K_FB1_RAM_SIZE (0)
-
-#define K_MAIN_RAM_START 0x1000C000
-#define K_MAIN_RAM_SIZE (16 * 1024 - 0x100)
-
-// RAM layout - common
-#define BOOTARGS_START 0x1000FF00
-#define BOOTARGS_SIZE 0x100
-
-#define DMABUF_RAM_START 0x20000000
-#define DMABUF_RAM_SIZE (1 * 1024)
-
-#define AUX1_RAM_START (0x20000400)
-#define AUX1_RAM_SIZE (191 * 1024)
-
-#define AUX2_RAM_START 0x10002000
-#define AUX2_RAM_SIZE (40 * 1024)
-
-// misc
-#define CODE_ALIGNMENT 0x200
-#define COREAPP_ALIGNMENT 0x200
-
-#endif
+#include "memory.h"
--- core/embed/models/T3W1/model_T3W1.h
+++ core/embed/models/T3W1/model_T3W1.h
@@ -1,7 +1,24 @@
-#ifndef MODELS_MODEL_T3W1_H_
-#define MODELS_MODEL_T3W1_H_
 
-// #include "bootloaders/bootloader_hashes.h"
+/*
+ * This file is part of the Trezor project, https://trezor.io/
+ *
+ * Copyright (c) SatoshiLabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 
 #include <rtl/sizedefs.h>
 
@@ -29,77 +46,7 @@
 
 #define DISPLAY_JUMP_BEHAVIOR DISPLAY_RESET_CONTENT
 
-// SHARED WITH MAKEFILE, LINKER SCRIPT etc.
-// misc
-#define FLASH_START 0x0C004000
 #define NORCOW_SECTOR_SIZE (16 * 8 * 1024)  // 128 kB
 #define NORCOW_MIN_VERSION 0x00000006
 
-// FLASH layout
-#define SECRET_START 0x0C000000
-#define SECRET_MAXSIZE (2 * 8 * 1024)  // 8 kB
-#define SECRET_SECTOR_START 0x0
-#define SECRET_SECTOR_END 0x1
-
-// overlaps with secret
-#define BHK_START 0x0C002000
-#define BHK_MAXSIZE (1 * 8 * 1024)  // 8 kB
-#define BHK_SECTOR_START 0x1
-#define BHK_SECTOR_END 0x1
-
-#define BOARDLOADER_START 0x0C004000
-#define BOARDLOADER_MAXSIZE (8 * 8 * 1024)  // 64 kB
-#define BOARDLOADER_SECTOR_START 0x2
-#define BOARDLOADER_SECTOR_END 0x9
-
-#define BOARDCAPS_START 0x0C013F00
-#define BOARDCAPS_MAXSIZE 0x100
-
-#define BOOTLOADER_START 0x0C014000
-#define BOOTLOADER_MAXSIZE (24 * 8 * 1024)  // 192 kB
-#define BOOTLOADER_SECTOR_START 0x0A
-#define BOOTLOADER_SECTOR_END 0x21
-
-#define FIRMWARE_START 0x0C044000
-#define FIRMWARE_MAXSIZE (430 * 8 * 1024)  // 3440 kB
-#define FIRMWARE_SECTOR_START 0x22
-#define FIRMWARE_SECTOR_END 0x1CF
-#define KERNEL_START 0x0C044000
-#define KERNEL_MAXSIZE (512 * 1024)  // 512 kB
-
-#define STORAGE_1_START 0x0C3A0000
-#define STORAGE_1_MAXSIZE (16 * 8 * 1024)  // 128 kB
-#define STORAGE_1_SECTOR_START 0x1D0
-#define STORAGE_1_SECTOR_END 0x1DF
-
-#define STORAGE_2_START 0x0C3C0000
-#define STORAGE_2_MAXSIZE (16 * 8 * 1024)  // 128 kB
-#define STORAGE_2_SECTOR_START 0x1E0
-#define STORAGE_2_SECTOR_END 0x1EF
-
-#define ASSETS_START 0x0C3E0000
-#define ASSETS_MAXSIZE (16 * 8 * 1024)  // 128 kB
-#define ASSETS_SECTOR_START 0x1F0
-#define ASSETS_SECTOR_END 0x1FF
-
-// RAM layout
-#define BOOTARGS_START 0x30000000
-#define BOOTARGS_SIZE 0x200
-
-#define FB1_RAM_START 0x30000200
-#define FB1_RAM_SIZE (768 * 1024 - 512)
-
-#define MAIN_RAM_START 0x300C0000
-#define MAIN_RAM_SIZE (64 * 1024)
-
-#define FB2_RAM_START 0x300D0000
-#define FB2_RAM_SIZE (768 * 1024)
-
-#define AUX1_RAM_START 0x30190000
-#define AUX1_RAM_SIZE (896 * 1024)
-
-// misc
-#define CODE_ALIGNMENT 0x400
-#define COREAPP_ALIGNMENT 0x2000
-
-#endif
+#include "memory.h"
