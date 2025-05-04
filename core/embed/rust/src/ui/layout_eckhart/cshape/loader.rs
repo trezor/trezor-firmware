634b8cfbdfce78b8d108f741cf2fc20e081ed249
obrusvit <vit.obrusnik@gmail.com>
2025-05-04 18:43:57 +0200

    feat(eckhart): implement render_loader
    - `render_loader` shows classic circular loader going around the
    display, implemented by rendering `ScreenBorder` and hiding it by 5
    black rectangles which progressively diminish
    - `render_loader_indeterminate` shows progress loader without known
    duration, implemented by rendering `ScreenBorder` in clip which travels
    around the SCREEN in an octagonal shape. This achieves an effect of a
    constant length line going around the screen shape

--- core/embed/rust/src/ui/layout_eckhart/cshape/loader.rs
+++ core/embed/rust/src/ui/layout_eckhart/cshape/loader.rs
@@ -1,96 +1,179 @@
 use crate::ui::{
     constant::SCREEN,
-    geometry::{Offset, Rect},
+    geometry::{Alignment2D, Offset, Point, Rect},
     lerp::Lerp,
     shape::{self, Renderer},
 };
 
-use super::{super::theme::BG, ScreenBorder};
+use super::{super::theme, ScreenBorder};
 
-pub fn render_loader<'s>(
-    progress: u16,
-    border: &'static ScreenBorder,
-    target: &mut impl Renderer<'s>,
-) {
-    // convert to ration from 0.0 to 1.0
-    let progress_ratio = (progress as f32 / 1000.0).clamp(0.0, 1.0);
-    let (clip, top_gap) = get_clips(progress_ratio);
-    render_clipped_border(border, clip, top_gap, u8::MAX, target);
-}
-
-fn get_clips(progress_ratio: f32) -> (Rect, Rect) {
-    /// Ratio of total_duration for the bottom part of the border
-    const BOTTOM_DURATION_RATIO: f32 = 0.125;
-    /// Ratio of total_duration for the side parts of the border
-    const SIDES_DURATION_RATIO: f32 = 0.5;
-    /// Ratio of total_duration for the top part of the border
-    const TOP_DURATION_RATIO: f32 = 0.375;
-
-    const TOP_GAP_ZERO: Rect = Rect::from_center_and_size(
-        SCREEN.top_center().ofs(Offset::y(ScreenBorder::WIDTH / 2)),
-        Offset::zero(),
-    );
-    const TOP_GAP_FULL: Rect = Rect::from_center_and_size(
-        SCREEN.top_center().ofs(Offset::y(ScreenBorder::WIDTH / 2)),
-        Offset::new(SCREEN.width(), ScreenBorder::WIDTH),
-    );
-
-    match progress_ratio {
-        // Bottom phase growing linearly
-        p if p < BOTTOM_DURATION_RATIO => {
-            let bottom_progress = (p / BOTTOM_DURATION_RATIO).clamp(0.0, 1.0);
-            let width = i16::lerp(0, SCREEN.width(), bottom_progress);
-            let clip = Rect::from_center_and_size(
-                SCREEN
-                    .bottom_center()
-                    .ofs(Offset::y(-ScreenBorder::WIDTH / 2)),
-                Offset::new(width, ScreenBorder::WIDTH),
-            );
-            (clip, TOP_GAP_FULL)
-        }
-
-        // Sides phase growing up linearly
-        p if p < (BOTTOM_DURATION_RATIO + SIDES_DURATION_RATIO) => {
-            let sides_progress =
-                ((p - BOTTOM_DURATION_RATIO) / SIDES_DURATION_RATIO).clamp(0.0, 1.0);
-            let height = i16::lerp(ScreenBorder::WIDTH, SCREEN.height(), sides_progress);
-            let clip = Rect::from_bottom_left_and_size(
-                SCREEN.bottom_left(),
-                Offset::new(SCREEN.width(), height),
-            );
-            (clip, TOP_GAP_FULL)
-        }
-
-        // Top gap shrinking linearly
-        p if p < 1.0 => {
-            let top_progress = ((p - BOTTOM_DURATION_RATIO - SIDES_DURATION_RATIO)
-                / TOP_DURATION_RATIO)
-                .clamp(0.0, 1.0);
-            let width = i16::lerp(SCREEN.width(), 0, top_progress);
-            let top_gap = Rect::from_center_and_size(
-                SCREEN.top_center().ofs(Offset::y(ScreenBorder::WIDTH / 2)),
-                Offset::new(width, ScreenBorder::WIDTH),
-            );
-            (SCREEN, top_gap)
-        }
-
-        // Animation complete
-        _ => (SCREEN, TOP_GAP_ZERO),
+/// Renders the loader. Higher `progress` reveals the `border` from the top in
+/// clock-wise direction. Used in ProgressScreen and Bootloader. `progress` goes
+/// from 0 to 1000.
+pub fn render_loader<'s>(progress: u16, border: &'s ScreenBorder, target: &mut impl Renderer<'s>) {
+    let progress_ratio = progress_to_ratio(progress);
+    // Draw the border first
+    border.render(u8::MAX, target);
+    // Draw the progressively shrinking covers
+    for cover in get_progress_covers(progress_ratio) {
+        shape::Bar::new(cover)
+            .with_bg(theme::BLACK)
+            .with_alpha(u8::MAX)
+            .render(target);
     }
 }
 
-fn render_clipped_border<'s>(
-    border: &'static ScreenBorder,
-    clip: Rect,
-    top_gap: Rect,
-    alpha: u8,
+/// Render the loader in indeterminate mode. A constant size portion of the
+/// border is rendered at each time moving around in clock-wise direction.
+pub fn render_loader_indeterminate<'s>(
+    progress: u16,
+    border: &'s ScreenBorder,
     target: &mut impl Renderer<'s>,
 ) {
+    let progress_ratio = progress_to_ratio(progress);
+    let clip = get_clip_indeterminate(progress_ratio);
+    // Draw the border in clip
     target.in_clip(clip, &|target| {
-        border.render(alpha, target);
+        border.render(u8::MAX, target);
     });
-    shape::Bar::new(top_gap)
-        .with_bg(BG)
-        .with_fg(BG)
-        .render(target);
+}
+
+fn progress_to_ratio(progress: u16) -> f32 {
+    // convert to ratio from 0.0 to 1.0
+    (progress as f32 / 1000.0).clamp(0.0, 1.0)
+}
+
+fn get_clip_indeterminate(progress_ratio: f32) -> Rect {
+    const CLIP_SIZE: i16 = 190;
+
+    // Define 8 points (+1 duplicate) for an octagonal path around the display
+    // Position them to ensure the clip always shows `CLIP_SIZE`-wide part of the
+    // border "Right end" and "Left start" points are shifted in y-axis a little
+    // bit upwards to account for the irregular corner shape
+    const PATH_POINTS: [Point; 9] = [
+        // Top start
+        Point::new(CLIP_SIZE / 2, -CLIP_SIZE / 2 + ScreenBorder::WIDTH),
+        // Top end
+        Point::new(
+            SCREEN.width() - CLIP_SIZE / 2,
+            -CLIP_SIZE / 2 + ScreenBorder::WIDTH,
+        ),
+        // Right start
+        Point::new(
+            SCREEN.width() + CLIP_SIZE / 2 - ScreenBorder::WIDTH,
+            CLIP_SIZE / 2,
+        ),
+        // Right end
+        Point::new(
+            SCREEN.width() + CLIP_SIZE / 2 - ScreenBorder::WIDTH,
+            SCREEN.height() - CLIP_SIZE / 2 - 60,
+        ),
+        // Bottom start
+        Point::new(
+            SCREEN.width() - CLIP_SIZE / 2 - ScreenBorder::WIDTH,
+            SCREEN.height() + CLIP_SIZE / 2 - ScreenBorder::WIDTH,
+        ),
+        // Bottom end
+        Point::new(
+            CLIP_SIZE / 2,
+            SCREEN.height() + CLIP_SIZE / 2 - ScreenBorder::WIDTH,
+        ),
+        // Left start
+        Point::new(
+            -CLIP_SIZE / 2 + ScreenBorder::WIDTH,
+            SCREEN.height() - CLIP_SIZE / 2 - 60,
+        ),
+        // Left end
+        Point::new(-CLIP_SIZE / 2 + ScreenBorder::WIDTH, CLIP_SIZE / 2),
+        // Top start - duplicate to close the loop
+        Point::new(CLIP_SIZE / 2, -CLIP_SIZE / 2 + ScreenBorder::WIDTH),
+    ];
+
+    // Calculate which segment we're in and how far along that segment
+    let path_length = PATH_POINTS.len() - 1; // -1 because the last point is a duplicate
+    let segment_position = progress_ratio * path_length as f32;
+
+    // Integer part gives us the segment
+    let segment = segment_position as usize % path_length;
+
+    // Fractional part gives us the position within the segment
+    let segment_ratio = segment_position - segment as f32;
+
+    // Get the current point and the next point
+    let current = PATH_POINTS[segment];
+    let next = PATH_POINTS[segment + 1];
+
+    // Linearly interpolate between the current and next points
+    let center = Point::lerp(current, next, segment_ratio);
+
+    Rect::snap(center, Offset::uniform(CLIP_SIZE), Alignment2D::CENTER)
+}
+
+fn get_progress_covers(progress_ratio: f32) -> impl Iterator<Item = Rect> {
+    let cover_1 = {
+        // Top-center to top-right
+        const PROGRESS_PORTION: f32 = 0.11;
+        const PROGRESS_START: f32 = 0.0;
+        const FULL_WIDTH: i16 = 190;
+        let progress = ((progress_ratio - PROGRESS_START) / PROGRESS_PORTION).clamp(0.0, 1.0);
+        let width = ((1.0 - progress) * FULL_WIDTH as f32) as i16;
+        Rect::snap(
+            SCREEN.top_right(),
+            Offset::new(width, theme::ICON_BORDER_TR.toif.height()),
+            Alignment2D::TOP_RIGHT,
+        )
+    };
+    let cover_2 = {
+        // Top-right to bottom-right
+        const PROGRESS_PORTION: f32 = 0.3;
+        const PROGRESS_START: f32 = 0.11;
+        const FULL_HEIGHT: i16 = 502;
+        let progress = ((progress_ratio - PROGRESS_START) / PROGRESS_PORTION).clamp(0.0, 1.0);
+        let height = ((1.0 - progress) * FULL_HEIGHT as f32) as i16;
+        Rect::snap(
+            SCREEN.bottom_right(),
+            Offset::new(theme::ICON_BORDER_BR.toif.width(), height),
+            Alignment2D::BOTTOM_RIGHT,
+        )
+    };
+    let cover_3 = {
+        // Bottom-right to bottom-left
+        const PROGRESS_PORTION: f32 = 0.18;
+        const PROGRESS_START: f32 = 0.41;
+        const FULL_WIDTH: i16 = 298;
+        let progress = ((progress_ratio - PROGRESS_START) / PROGRESS_PORTION).clamp(0.0, 1.0);
+        let width = ((1.0 - progress) * FULL_WIDTH as f32) as i16;
+        Rect::snap(
+            SCREEN.bottom_left() + Offset::x(theme::ICON_BORDER_BL.toif.width()),
+            Offset::new(width, ScreenBorder::WIDTH),
+            Alignment2D::BOTTOM_LEFT,
+        )
+    };
+    let cover_4 = {
+        // Bottom-left to top-left
+        const PROGRESS_PORTION: f32 = 0.3;
+        const PROGRESS_START: f32 = 0.59;
+        const FULL_HEIGHT: i16 = 502;
+        let progress = ((progress_ratio - PROGRESS_START) / PROGRESS_PORTION).clamp(0.0, 1.0);
+        let height = ((1.0 - progress) * FULL_HEIGHT as f32) as i16;
+        Rect::snap(
+            SCREEN.top_left() + Offset::y(theme::ICON_BORDER_TL.toif.height()),
+            Offset::new(theme::ICON_BORDER_BL.toif.width(), height),
+            Alignment2D::TOP_LEFT,
+        )
+    };
+    let cover_5 = {
+        // Top-left to top-center
+        const PROGRESS_PORTION: f32 = 0.11;
+        const PROGRESS_START: f32 = 0.89;
+        const FULL_WIDTH: i16 = 190;
+        let progress = ((progress_ratio - PROGRESS_START) / PROGRESS_PORTION).clamp(0.0, 1.0);
+        let width = ((1.0 - progress) * FULL_WIDTH as f32) as i16;
+        Rect::snap(
+            SCREEN.top_center(),
+            Offset::new(width, theme::ICON_BORDER_TL.toif.height()),
+            Alignment2D::TOP_RIGHT,
+        )
+    };
+    [cover_1, cover_2, cover_3, cover_4, cover_5].into_iter()
 }
