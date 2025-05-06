f5dee957b3eb07a21514665f4034496d133defad
kopecdav <david.kopecky@satoshilabs.com>
2025-05-06 13:38:05 +0200

    feat(core): add pm_wait_until_active function [no changelog]

--- core/embed/sys/power_manager/inc/sys/power_manager.h
+++ core/embed/sys/power_manager/inc/sys/power_manager.h
@@ -102,6 +102,7 @@ pm_status_t pm_get_report(pm_report_t* report);
 pm_status_t pm_charging_enable(void);
 pm_status_t pm_charging_disable(void);
 pm_status_t pm_store_data_to_backup_ram(void);
+pm_status_t pm_wait_until_active(uint32_t timeout_ms);
 pm_status_t pm_wakeup_flags_set(pm_wakeup_flags_t flags);
 pm_status_t pm_wakeup_flags_reset(void);
 pm_status_t pm_wakeup_flags_get(pm_wakeup_flags_t* flags);
