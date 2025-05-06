010c5adf892a17c6df44bd79efb0ad4c97f6e64b
cepetr <petr@cerninovi.cz>
2025-05-06 12:48:04 +0200

    refactor(core): remove firmware_calc_hash callback
    [no changelog]

--- core/embed/util/fwutils/inc/util/fwutils.h
+++ core/embed/util/fwutils/inc/util/fwutils.h
@@ -22,26 +22,23 @@
 
 #include <trezor_types.h>
 
-// Callback function for firmware hash calculation.
-typedef void (*firmware_hash_callback_t)(void* context, uint32_t progress,
-                                         uint32_t total);
-
-// Calculates hash of the firmware area.
+// (Re)starts calculation of the firmware hash
 //
-// `challenge` is a optional pointer to the challenge data.
+// `challenge` is a pointer to the challenge data (optional, can be NULL).
 // `challenge_len` is the length of the challenge data (1..32).
+//
+// Return 0 on success, -1 on error.
+int firmware_hash_start(const uint8_t* challenge, size_t challenge_len);
+
+// Continues with the firmware hash calculation.
+//
 // `hash` is a pointer to a buffer where the hash will be stored.
 // `hash_len` is size of the buffer (must be at least 32).
-// `callback` is an optional callback function that will be called during the
-// hash calculation.
-// `callback_context` is a pointer that will be passed to the callback function.
 //
-// Returns `sectrue` if the hash was calculated successfully, `secfalse`
-// otherwise.
-secbool firmware_calc_hash(const uint8_t* challenge, size_t challenge_len,
-                           uint8_t* hash, size_t hash_len,
-                           firmware_hash_callback_t callback,
-                           void* callback_context);
+// Return value between 0 and 100 indicates the progress of the hash
+// calculation. 100 means the hash was calculated successfully and
+// `hash` contains the hash. -1 means an error occurred during the
+int firmware_hash_continue(uint8_t* hash, size_t hash_len);
 
 // Reads the firmware vendor string from the header in the firmware area.
 //
