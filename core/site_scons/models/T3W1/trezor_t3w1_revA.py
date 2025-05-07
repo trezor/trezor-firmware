2a6828f7ce4cf359dfe33ed55a92896a1e997e05
cepetr <petr@cerninovi.cz>
2025-05-07 16:17:07 +0200

    chore(core): remove unnecessary definitions
    [no changelog]

--- core/site_scons/models/T3W1/trezor_t3w1_revA.py
+++ core/site_scons/models/T3W1/trezor_t3w1_revA.py
@@ -249,7 +249,4 @@ def configure(
 
     env.get("ENV")["LINKER_SCRIPT"] = linker_script
 
-    defs = env.get("CPPDEFINES_IMPLICIT")
-    defs += ["__ARM_FEATURE_CMSE=3"]
-
     return features_available
