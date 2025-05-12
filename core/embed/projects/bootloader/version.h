f1b4c1d3160d46388573467d91bd779e1b38c914
tychovrahe <brunam@seznam.cz>
2025-05-12 15:27:38 +0200

    bump versions

--- core/embed/projects/bootloader/version.h
+++ core/embed/projects/bootloader/version.h
@@ -5,7 +5,7 @@
 #define VERSION_MAJOR 2
 #define VERSION_MINOR 1
 #define VERSION_PATCH 11
-#define VERSION_BUILD 0
+#define VERSION_BUILD 1
 #define VERSION_UINT32                                            \
   (VERSION_MAJOR | (VERSION_MINOR << 8) | (VERSION_PATCH << 16) | \
    (VERSION_BUILD << 24))
