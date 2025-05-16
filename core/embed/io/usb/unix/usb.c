b493229c3828dc428ab7d35b5e41a5ee4d7e697f
tychovrahe <brunam@seznam.cz>
2025-05-16 20:29:26 +0200

    feat(core/unix): implement USB VCP emulation
    [no changelog]

--- core/embed/io/usb/unix/usb.c
+++ core/embed/io/usb/unix/usb.c
@@ -88,7 +88,8 @@ secbool usb_start(void) {
     usb_iface_t *iface = &usb_ifaces[i];
     // skip if not HID or WebUSB interface
     if (iface->type != USB_IFACE_TYPE_HID &&
-        iface->type != USB_IFACE_TYPE_WEBUSB) {
+        iface->type != USB_IFACE_TYPE_WEBUSB &&
+        iface->type != USB_IFACE_TYPE_VCP) {
       continue;
     }
 
@@ -326,6 +327,62 @@ int usb_webusb_write_blocking(uint8_t iface_num, const uint8_t *buf,
     }
   }
   return usb_webusb_write(iface_num, buf, len);
+}
+
+secbool usb_vcp_can_read(uint8_t iface_num) {
+  if (iface_num >= USBD_MAX_NUM_INTERFACES ||
+      usb_ifaces[iface_num].type != USB_IFACE_TYPE_VCP) {
+    return secfalse;
+  }
+  return usb_emulated_poll_read(&usb_ifaces[iface_num]);
+}
+
+secbool usb_vcp_can_write(uint8_t iface_num) {
+  if (iface_num >= USBD_MAX_NUM_INTERFACES ||
+      usb_ifaces[iface_num].type != USB_IFACE_TYPE_VCP) {
+    return secfalse;
+  }
+  return usb_emulated_poll_write(&usb_ifaces[iface_num]);
+}
+
+int usb_vcp_read(uint8_t iface_num, uint8_t *buf, uint32_t len) {
+  if (iface_num >= USBD_MAX_NUM_INTERFACES ||
+      usb_ifaces[iface_num].type != USB_IFACE_TYPE_VCP) {
+    return 0;
+  }
+  return usb_emulated_read(&usb_ifaces[iface_num], buf, len);
+}
+
+int usb_vcp_read_blocking(uint8_t iface_num, uint8_t *buf, uint32_t len,
+                          int timeout) {
+  const uint32_t start = clock();
+  while (sectrue != usb_vcp_can_read(iface_num)) {
+    if (timeout >= 0 &&
+        (1000 * (clock() - start)) / CLOCKS_PER_SEC >= timeout) {
+      return 0;  // Timeout
+    }
+  }
+  return usb_vcp_read(iface_num, buf, len);
+}
+
+int usb_vcp_write(uint8_t iface_num, const uint8_t *buf, uint32_t len) {
+  if (iface_num >= USBD_MAX_NUM_INTERFACES ||
+      usb_ifaces[iface_num].type != USB_IFACE_TYPE_VCP) {
+    return 0;
+  }
+  return usb_emulated_write(&usb_ifaces[iface_num], buf, len);
+}
+
+int usb_vcp_write_blocking(uint8_t iface_num, const uint8_t *buf, uint32_t len,
+                           int timeout) {
+  const uint32_t start = clock();
+  while (sectrue != usb_vcp_can_write(iface_num)) {
+    if (timeout >= 0 &&
+        (1000 * (clock() - start)) / CLOCKS_PER_SEC >= timeout) {
+      return 0;  // Timeout
+    }
+  }
+  return usb_vcp_write(iface_num, buf, len);
 }
 
 void mp_hal_set_vcp_iface(int iface_num) {}
