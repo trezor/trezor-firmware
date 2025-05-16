1bd4d17d0ed391f9742805de6c4c5b539ff39d56
tychovrahe <brunam@seznam.cz>
2025-05-16 17:11:11 +0200

    refactor(core): implement cpuid as separate module
    [no changelog]

--- core/embed/util/cpuid/inc/util/cpuid.h
+++ core/embed/util/cpuid/inc/util/cpuid.h
@@ -17,32 +17,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <trezor_bsp.h>  // required by #ifdef STM32U5 below (see #4306 issue)
-#include <trezor_rtl.h>
+#pragma once
 
-#include <rtl/cli.h>
-#include <util/cpuid.h>
+#include <trezor_types.h>
 
-static void prodtest_get_cpuid(cli_t* cli) {
-  if (cli_arg_count(cli) > 0) {
-    cli_error_arg_count(cli);
-    return;
-  }
+typedef struct {
+  uint32_t id[3];
+} cpuid_t;
 
-  cpuid_t cpuid = {0};
-
-  cpuid_get(&cpuid);
-
-  cli_ok_hexdata(cli, &cpuid.id, sizeof(cpuid.id));
-}
-
-// clang-format off
-
-PRODTEST_CLI_CMD(
-  .name = "get-cpuid",
-  .func = prodtest_get_cpuid,
-  .info = "Retrieve unique CPU ID",
-  .args = ""
-);
-
-
+void cpuid_get(cpuid_t* cpuid);
