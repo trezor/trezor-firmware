b67c8c24bc57146944a3ad751b35f8421bccff51
Roman Zeyde <roman.zeyde@satoshilabs.com>
2025-06-08 22:00:21 +0300

    style(core): prefix unused parameters with `_`
    [no changelog]

--- core/embed/rust/src/ui/layout_caesar/ui_firmware.rs
+++ core/embed/rust/src/ui/layout_caesar/ui_firmware.rs
@@ -1093,10 +1093,10 @@ impl FirmwareUI for UICaesar {
     }
 
     fn show_pairing_code(
-        title: TString<'static>,
-        description: TString<'static>,
-        code: TString<'static>,
-        button: bool,
+        _title: TString<'static>,
+        _description: TString<'static>,
+        _code: TString<'static>,
+        _button: bool,
     ) -> Result<impl LayoutMaybeTrace, Error> {
         Err::<RootComponent<Empty, ModelUI>, Error>(Error::ValueError(
             c"show_pairing_code not supported",
