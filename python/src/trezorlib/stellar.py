cf385229b9e5300430ece876d3bfbd29a7b7490d
M1nd3r <petrsedlacek.km@seznam.cz>
2025-06-16 17:14:18 +0200

    feat(python): change trezorlib to session-based

--- python/src/trezorlib/stellar.py
+++ python/src/trezorlib/stellar.py
@@ -20,8 +20,8 @@ from typing import TYPE_CHECKING, List, Tuple, Union
 from . import exceptions, messages
 
 if TYPE_CHECKING:
-    from .client import TrezorClient
     from .tools import Address
+    from .transport.session import Session
 
     StellarMessageType = Union[
         messages.StellarAccountMergeOp,
@@ -322,12 +322,12 @@ def _read_asset(asset: "Asset") -> messages.StellarAsset:
 
 
 def get_address(
-    client: "TrezorClient",
+    session: "Session",
     address_n: "Address",
     show_display: bool = False,
     chunkify: bool = False,
 ) -> str:
-    return client.call(
+    return session.call(
         messages.StellarGetAddress(
             address_n=address_n, show_display=show_display, chunkify=chunkify
         ),
@@ -336,7 +336,7 @@ def get_address(
 
 
 def sign_tx(
-    client: "TrezorClient",
+    session: "Session",
     tx: messages.StellarSignTx,
     operations: List["StellarMessageType"],
     address_n: "Address",
@@ -352,10 +352,10 @@ def sign_tx(
     # 3. Receive a StellarTxOpRequest message
     # 4. Send operations one by one until all operations have been sent. If there are more operations to sign, the device will send a StellarTxOpRequest message
     # 5. The final message received will be StellarSignedTx which is returned from this method
-    resp = client.call(tx)
+    resp = session.call(tx)
     try:
         while isinstance(resp, messages.StellarTxOpRequest):
-            resp = client.call(operations.pop(0))
+            resp = session.call(operations.pop(0))
     except IndexError:
         # pop from empty list
         raise exceptions.TrezorException(
