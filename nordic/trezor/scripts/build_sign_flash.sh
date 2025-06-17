9add5385cf5007581590d64c725ca0f8f21db4ca
tychovrahe <brunam@seznam.cz>
2025-06-17 10:49:55 +0200

    chore(nordic): modify building script to allow production configuration
    [no changelog]

--- nordic/trezor/scripts/build_sign_flash.sh
+++ nordic/trezor/scripts/build_sign_flash.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Script builds, signs, and/or flashed Nordic board
+# Script builds, signs, and/or flashes Nordic board with optional debug or production overlays
 
 # Run this in `nordic/trezor` to sign and mergehex final image with mcuboot
 # This charade serves to differentiate commands run under poetry shell and ncs shell since their pythons are not compatible
 
-OPTSTRING=":b:pdsf"
+OPTSTRING=":b:pdsfr"
 
 BOARD=
 SIGN=0
 FLASH=0
 PRISTINE=
 DEBUG=
+PRODUCTION=
 
 fatal() {
     echo "$@"
@@ -20,22 +21,22 @@ fatal() {
 
 run_under_ncs_subshell() {
     # In the subshell, toolchain environment is sourced then the command is run
-    # Weird regexp replaces "VARIABLE   : somevalue:bla/bla:he" to "export VARIABLE=somevalue/bla/bla/he" setting up the shell
-    # Just some std::bash_ugliness::hacks
-    (source <(nrfutil toolchain-manager env | perl -pe 's/^(\w+)\s*:\s*(.*)/export \1=\2/');  bash -x -c "$@") || fatal "Error in subshell"
+    (source <(nrfutil toolchain-manager env | perl -pe 's/^(\w+)\s*:\s*(.*)/export \1=\2/');  bash -x -c "$@") \
+        || fatal "Error in subshell"
 }
 
 usage() {
-    echo "$0 [-b board_name] [-p] [-d] [-s] [-f]"
-    cat<<END
-    Parameters: 
+    echo "$0 [-b board_name] [-p] [-d] [-r] [-s] [-f]"
+    cat <<END
+    Parameters:
     -b board_name: build with board name as param
     -p: build with --pristine
     -d: use debug overlay when building
+    -r: use production overlay when building
     -s: sign result
     -f: flash board
-    
-    Each can build/sign/flash be done in one run or separately, but the sequence must follow to make sense"
+
+    Each of build/sign/flash can be done in one run or separately, but the sequence must follow to make sense.
 END
 }
 
@@ -49,6 +50,9 @@ while getopts ${OPTSTRING} opt; do
       ;;
     d)
       DEBUG="-- -DOVERLAY_CONFIG=debug.conf"
+      ;;
+    r)
+      PRODUCTION="-- -DOVERLAY_CONFIG=prod.conf"
       ;;
     s)
       SIGN=1
@@ -65,9 +69,8 @@ done
 
 if [ -n "$BOARD" ]; then
     run_under_ncs_subshell \
-        "west build ./trezor-ble -b $BOARD --sysbuild $PRISTINE $DEBUG"
+        "west build ./trezor-ble -b $BOARD --sysbuild $PRISTINE $DEBUG $PRODUCTION"
 fi
-
 
 if [ "$SIGN" -eq 1 ]; then
     run_under_ncs_subshell \
@@ -80,14 +83,13 @@ if [ "$SIGN" -eq 1 ]; then
     SIGMASK=$(hash_signer -d "$HASH" -m)
     echo "Signed hash $HASH, signature $SIGNATURE, sigmask $SIGMASK"
 
-
     run_under_ncs_subshell \
         "python ./scripts/insert_signatures_hex.py ./build/trezor-ble/zephyr/zephyr.prep.hex $SIGNATURE $SIGMASK -o ./build/trezor-ble/zephyr/zephyr.signed_cosi.hex && \
-        python ./scripts/insert_signatures_bin.py ./build/trezor-ble/zephyr/zephyr.prep.bin $SIGNATURE $SIGMASK -o ./build/trezor-ble/zephyr/zephyr.signed_cosi.bin && \
-        mergehex -m build/mcuboot/zephyr/zephyr.hex build/trezor-ble/zephyr/zephyr.signed_cosi.hex -o build/trezor-ble/zephyr.merged.signed.hex"
+         python ./scripts/insert_signatures_bin.py ./build/trezor-ble/zephyr/zephyr.prep.bin $SIGNATURE $SIGMASK -o ./build/trezor-ble/zephyr/zephyr.signed_cosi.bin && \
+         mergehex -m build/mcuboot/zephyr/zephyr.hex build/trezor-ble/zephyr/zephyr.signed_cosi.hex -o build/zephyr.merged.signed_cosi.hex"
 fi
 
 if [ "$FLASH" -eq 1 ]; then
     run_under_ncs_subshell \
-        'west flash --hex-file ./build/trezor-ble/zephyr.merged.signed.hex'
+        'west flash --hex-file ./build/zephyr.merged.signed_cosi.hex'
 fi
