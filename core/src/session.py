2bc195f2e33898755133caf181e4e234f57ff6b1
Roman Zeyde <roman.zeyde@satoshilabs.com>
2025-06-19 10:34:12 +0300

    WIP

--- core/src/session.py
+++ core/src/session.py
@@ -32,19 +32,59 @@ if utils.USE_BLE:
 import trezorui_api
 from trezor.ui.layouts.common import interact
 
+class Menu:
+    def __init__(self, *items: "Item"):
+        self.items = items
 
-async def test():
-    layout = trezorui_api.select_word(
-        title="",
-        description="",
-        words=(("Account info"), ("Block hash"), ("Fee info")))
-    res = await interact(layout, None)
-    print([res])
-    layout = trezorui_api.multiple_pages_texts(title="Title", verb="verb", items=["a", "B", "c"])
-    res = await interact(layout, None)
-    print([res])
+class Item:
+    def __init__(self, name: str, *items: tuple[str, str]):
+        self.name = name
+        self.items = items
 
-workflow.spawn(test())
+async def test_menu():
+    menu_items = Menu(
+        Item(
+            "Account info",
+            ("Account", "ETH #2"),
+            ("Derivation path", "m/1/2/3/4"),
+        ),
+        Item("Block hash",
+             ("Block hash", "1355557e447b95b89c75a5d4909701d52faf2716eb30c55a6c9fcbb735748f9e"),
+        ),
+        Item("Fee info",
+            ("Gas limit", "21000 units"),
+            ("Max fee per gas", "3.28 Gwei"),
+            ("Max priority fee", "1.7 Gwei"),
+        ),
+    )
+    location = []
+    while True:
+        menu = menu_items
+        for i in location:
+            menu = menu.items[i]
+
+        if isinstance(menu, Item):
+            items = [f'{k}\n{v}' for k, v in menu.items]
+            layout = trezorui_api.multiple_pages_texts(title=menu.name, verb="OK", items=items)
+        else:
+            items = [i.name for i in menu.items]
+            layout = trezorui_api.select_menu(items=items)
+
+        try:
+            res = await interact(layout, "menu")
+            print([res])
+            if isinstance(res, int):
+                assert 0 <= res < len(items)
+                location.append(res)
+                continue
+        except wire.ActionCancelled:
+            pass
+
+        if not location:
+            return
+        location.pop()
+
+workflow.spawn(test_menu())
 
 # start the event loop
 loop.run()
