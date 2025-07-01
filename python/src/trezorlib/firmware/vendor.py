5f80eb371de47e63f121ed90780ed89d104a9cd9
tychovrahe <brunam@seznam.cz>
2025-07-01 22:08:38 +0200

    feat(core): add allow unlimited run vendorheader flag
    [no changelog]

--- python/src/trezorlib/firmware/vendor.py
+++ python/src/trezorlib/firmware/vendor.py
@@ -49,6 +49,7 @@ def _transform_vendor_trust(data: bytes) -> bytes:
 
 
 class VendorTrust(Struct):
+    limit_runtime: bool
     deny_provisioning_access: bool
     _dont_provide_secret: bool
     allow_run_with_secret: bool
@@ -61,7 +62,8 @@ class VendorTrust(Struct):
 
     SUBCON = c.Transformed(
         c.BitStruct(
-            "_reserved" / c.Default(c.BitsInteger(6), 0b111111),
+            "_reserved" / c.Default(c.BitsInteger(5), 0b11111),
+            "limit_runtime" / c.Default(c.Flag, 1),
             "deny_provisioning_access" / c.Default(c.Flag, 1),
             "_dont_provide_secret"
             / c.Default(c.Flag, lambda this: not this.allow_run_with_secret),
