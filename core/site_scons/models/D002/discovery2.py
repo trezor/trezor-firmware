93e0264f2e3ce49823746ee1bc5f382ddbb16a10
cepetr <petr@cerninovi.cz>
2025-07-10 12:13:26 +0200

    fix(core): fix D002 model build
    [no changelog]

--- core/site_scons/models/D002/discovery2.py
+++ core/site_scons/models/D002/discovery2.py
@@ -46,6 +46,7 @@ def configure(
         ("HSE_VALUE", "16000000"),
         ("USE_HSE", "1"),
         ("USE_BOOTARGS_RSOD", "1"),
+        ("LOCKABLE_BOOTLOADER", "1"),
     ]
 
     if "display" in features_wanted:
