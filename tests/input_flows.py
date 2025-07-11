0f480d61d9e64fd2ff117e2f0e09636df0fe5975
Lukas Bielesch <bielesch.lukas@gmail.com>
2025-07-11 17:40:26 +0200

    fixup! fix(core/delizia): inconsistent brightness in set_brightness flow

--- tests/input_flows.py
+++ tests/input_flows.py
@@ -3008,7 +3008,10 @@ class InputFlowSetBrightness(InputFlowBase):
         self.debug.press_yes()
 
     def input_flow_delizia(self):
-        yield from self.input_flow_bolt()
+        yield
+        self.debug.press_yes()
+        yield
+        self.debug.press_yes()
 
     def input_flow_eckhart(self):
         yield
@@ -3023,4 +3026,3 @@ class InputFlowCancelBrightness(InputFlowBase):
     def input_flow_delizia(self):
         yield
         self.debug.click(self.debug.screen_buttons.menu())
-        self.debug.click(self.debug.screen_buttons.vertical_menu_items()[0])
