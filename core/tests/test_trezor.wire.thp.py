91a5e5c4641f339df787ff150f432e7aef6f25fb
M1nd3r <petrsedlacek.km@seznam.cz>
2025-07-12 01:09:05 +0200

    fix: use public_key instead of pubkey in THP

--- core/tests/test_trezor.wire.thp.py
+++ core/tests/test_trezor.wire.thp.py
@@ -361,8 +361,8 @@ class TestTrezorHostProtocol(unittest.TestCase):
         user_message = Message(ThpMessageType.ThpCodeEntryTag, buffer)
         self.interface.mock_read(user_message, gen)
 
-        host_static_pubkey = b"\x00\x11\x22\x33\x44\x55\x66\x77\x00\x11\x22\x33\x44\x55\x66\x77\x00\x11\x22\x33\x44\x55\x66\x77\x00\x11\x22\x33\x44\x55\x66\x77\x00\x11\x22\x33\x44\x55\x66\x77\x00\x11\x22\x33\x44\x55\x66\x77"
-        msg = ThpCredentialRequest(host_static_pubkey=host_static_pubkey)
+        host_static_public_key = b"\x00\x11\x22\x33\x44\x55\x66\x77\x00\x11\x22\x33\x44\x55\x66\x77\x00\x11\x22\x33\x44\x55\x66\x77\x00\x11\x22\x33\x44\x55\x66\x77\x00\x11\x22\x33\x44\x55\x66\x77\x00\x11\x22\x33\x44\x55\x66\x77"
+        msg = ThpCredentialRequest(host_static_public_key=host_static_public_key)
         buffer: bytearray = bytearray(protobuf.encoded_length(msg))
         protobuf.encode(buffer, msg)
         credential_request = Message(ThpMessageType.ThpCredentialRequest, buffer)
