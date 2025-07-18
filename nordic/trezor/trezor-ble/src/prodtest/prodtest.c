0bb667f42f367d4981d2759e2a186d539d2ce8a2
tychovrahe <brunam@seznam.cz>
2025-07-18 17:32:50 +0200

    feat(nordic): enable pairing with trezor
    [no changelog]

--- nordic/trezor/trezor-ble/src/prodtest/prodtest.c
+++ nordic/trezor/trezor-ble/src/prodtest/prodtest.c
@@ -21,6 +21,7 @@
 #include <stdint.h>
 
 #include <zephyr/kernel.h>
+#include <zephyr/settings/settings.h>
 #include <zephyr/types.h>
 
 #include <zephyr/logging/log.h>
@@ -37,14 +38,52 @@ typedef enum {
   PRODTEST_CMD_SPI_DATA = 0x00,
   PRODTEST_CMD_UART_DATA = 0x01,
   PRODTEST_CMD_SET_OUTPUT = 0x02,
+  PRODTEST_CMD_PAIR = 0x03,
 } prodtest_cmd_t;
 
 typedef enum {
   PRODTEST_RESP_SPI = 0x00,
   PRODTEST_RESP_UART = 0x01,
+  PRODTEST_RESP_SUCCESS = 0x02,
+  PRODTEST_RESP_FAILURE = 0x03,
 } prodtest_resp_t;
 
 void prodtest_init(void) { k_sem_give(&prodtest_ok); }
+
+#define PAIRING_SECRET_SIZE 32
+
+static uint8_t pairing_secret[PAIRING_SECRET_SIZE] = {0};
+
+static int prodtest_set(const char *key, size_t len, settings_read_cb read_cb,
+                        void *cb_arg) {
+  if (strcmp(key, "pairing_secret") == 0) {
+    ssize_t rc = read_cb(cb_arg, pairing_secret, sizeof(pairing_secret));
+    return rc < 0 ? (int)rc : 0;
+  }
+  return -ENOENT;
+}
+
+SETTINGS_STATIC_HANDLER_DEFINE(prodtest, "prodtest", NULL, prodtest_set, NULL,
+                               NULL);
+
+bool prodtest_pair(uint8_t *data, uint16_t len) {
+  if (len != PAIRING_SECRET_SIZE) {
+    LOG_ERR("Invalid pairing data length: %d", len);
+    return false;
+  }
+
+  // Save pairing data to settings
+  int rc = settings_save_one("prodtest/pairing_secret", data, len);
+
+  if (rc != 0) {
+    LOG_ERR("Failed to save pairing secret: %d", rc);
+    return false;
+  }
+
+  settings_commit();  // Commit settings to persistent storage
+
+  return true;
+}
 
 static void process_command(uint8_t *data, uint16_t len) {
   uint8_t resp_data[244] = {0};
@@ -60,6 +99,23 @@ static void process_command(uint8_t *data, uint16_t len) {
       break;
     case PRODTEST_CMD_SET_OUTPUT:
       signals_set_reserved(data[1]);
+      break;
+    case PRODTEST_CMD_PAIR:
+      if (len < 1 + PAIRING_SECRET_SIZE) {
+        LOG_ERR("Pairing data too short: %d", len);
+        return;
+      }
+      bool ok = prodtest_pair(&data[1], PAIRING_SECRET_SIZE);
+
+      if (!ok) {
+        resp_data[0] = PRODTEST_RESP_FAILURE;
+        LOG_ERR("Failed to pair");
+        trz_comm_send_msg(NRF_SERVICE_PRODTEST, resp_data, 1);
+        return;
+      }
+
+      resp_data[0] = PRODTEST_RESP_SUCCESS;
+      trz_comm_send_msg(NRF_SERVICE_PRODTEST, resp_data, 1);
       break;
     default:
       break;
