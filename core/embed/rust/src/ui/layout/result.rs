53f62c3cbd72d63c3c12de70e50379ad608458e0
Ioan Bizău <i@ibz.me>
2025-07-21 12:55:54 +0200

    feat(core/ui): support `BACK` as a result
    [no changelog]

--- core/embed/rust/src/ui/layout/result.rs
+++ core/embed/rust/src/ui/layout/result.rs
@@ -2,8 +2,10 @@ use crate::micropython::{macros::obj_type, qstr::Qstr, simple_type::SimpleTypeOb
 
 static CONFIRMED_TYPE: Type = obj_type! { name: Qstr::MP_QSTR_CONFIRMED, };
 static CANCELLED_TYPE: Type = obj_type! { name: Qstr::MP_QSTR_CANCELLED, };
+static BACK_TYPE: Type = obj_type! { name: Qstr::MP_QSTR_BACK, };
 static INFO_TYPE: Type = obj_type! { name: Qstr::MP_QSTR_INFO, };
 
 pub static CONFIRMED: SimpleTypeObj = SimpleTypeObj::new(&CONFIRMED_TYPE);
 pub static CANCELLED: SimpleTypeObj = SimpleTypeObj::new(&CANCELLED_TYPE);
+pub static BACK: SimpleTypeObj = SimpleTypeObj::new(&BACK_TYPE);
 pub static INFO: SimpleTypeObj = SimpleTypeObj::new(&INFO_TYPE);
