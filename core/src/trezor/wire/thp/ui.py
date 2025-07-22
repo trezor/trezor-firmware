72fc05c53961ed2b9e8feb76a84fac85f350058f
M1nd3r <petrsedlacek.km@seznam.cz>
2025-07-22 17:44:17 +0200

    fixup! feat(core): implement THP

--- core/src/trezor/wire/thp/ui.py
+++ core/src/trezor/wire/thp/ui.py
@@ -1,5 +1,3 @@
-import trezorui_api
-
 from ..protocol_common import Context
 
 
@@ -8,20 +6,15 @@ async def show_autoconnect_credential_confirmation_screen(
     host_name: str | None,
     device_name: str | None = None,
 ) -> None:
-    from trezor.enums import ButtonRequestType
-    from trezor.ui.layouts.common import raise_if_cancelled
+    from trezor.ui.layouts import confirm_action
 
     if not device_name:
         action_string = f"Allow {host_name} to connect automatically to this Trezor?"
     else:
         action_string = f"Allow {host_name} on {device_name} to connect automatically to this Trezor?"
 
-    await raise_if_cancelled(
-        trezorui_api.confirm_action(
-            title="Autoconnect credential",
-            action=action_string,
-            description=None,
-        ),
+    await confirm_action(
         br_name="thp_autoconnect_credential_request",
-        br_code=ButtonRequestType.Other,
+        title="Autoconnect credential",
+        action=action_string,
     )
