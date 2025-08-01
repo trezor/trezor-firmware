d4b248df5eba569af647de61ad4acaeda83371b7
Roman Zeyde <roman.zeyde@satoshilabs.com>
2025-08-01 22:25:24 +0300

    test(core): don't ignore exceptions when stopping screen recording
    [no changelog]

--- tests/ui_tests/__init__.py
+++ tests/ui_tests/__init__.py
@@ -8,11 +8,8 @@ from contextlib import contextmanager
 import pytest
 from _pytest.nodes import Node
 from _pytest.outcomes import Failed
-from noise.exceptions import NoiseInvalidMessage
 
 from trezorlib.debuglink import TrezorClientDebugLink as Client
-from trezorlib.exceptions import ThpError
-from trezorlib.transport import Timeout
 
 LOG = logging.getLogger(__name__)
 
@@ -72,12 +69,7 @@ def screen_recording(
 
         # Instead of client.init_device() we create a new management session
         # `Ping` is sent to make sure the device is available.
-        try:
-            client.get_seedless_session().ping(message="", timeout=1)
-        except (ThpError, NoiseInvalidMessage, Timeout):
-            # Do not raise for unsuccessful ping
-            LOG.exception("Ping failed")
-            pass
+        client.get_seedless_session().ping(message="")
         client.debug.stop_recording()
 
     result = testcase.build_result(request)
