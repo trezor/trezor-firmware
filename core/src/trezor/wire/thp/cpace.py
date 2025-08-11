fe242195a93ee3a94aa8c65cdd984803c80d0ee8
M1nd3r <petrsedlacek.km@seznam.cz>
2025-08-11 14:43:08 +0200

    chore(core): make CPace implementation more explanatory [no changelog]

--- core/src/trezor/wire/thp/cpace.py
+++ core/src/trezor/wire/thp/cpace.py
@@ -4,6 +4,10 @@ from trezor.crypto.hashlib import sha512
 
 _PREFIX = b"\x08\x43\x50\x61\x63\x65\x32\x35\x35\x06"
 _PADDING = b"\x6f\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x20"
+_SID_STR = b"\x00"
+
+if __debug__:
+    _LEN_PRS = 6
 
 
 class Cpace:
@@ -21,11 +25,27 @@ class Cpace:
         """
         Generate an ephemeral key pair using Elligator2 with X25519.
         """
-        sha_ctx = sha512(_PREFIX)
-        sha_ctx.update(code_code_entry)
-        sha_ctx.update(_PADDING)
-        sha_ctx.update(self.handshake_hash)
-        sha_ctx.update(b"\x00")
+        assert len(code_code_entry) == _LEN_PRS
+
+        # See https://datatracker.ietf.org/doc/draft-irtf-cfrg-cpace/
+        # len(DSI) = 8 (b"\x08")
+        # DSI = CPace255
+        # len(PRS) = 6 (b"\x06")
+        # PRS = 'code_code_entry'
+        # len_zpad = 111 (b"\x6f")
+        # len(CI) = 32 (b"\x20")
+        # CI = 'self.handshake_hash'
+        # len(sid) = 0 (b"\x00)
+        # sid = b""
+
+        # fmt: off
+        sha_ctx = sha512(_PREFIX)            # len(DSI) | DSI | len(PRS)
+        sha_ctx.update(code_code_entry)      # PRS
+        sha_ctx.update(_PADDING)             # len_zpad | zpad | len(CI)
+        sha_ctx.update(self.handshake_hash)  # CI
+        sha_ctx.update(_SID_STR)             # len(sid) | sid
+        # fmt:on
+
         pregenerator = sha_ctx.digest()[:32]
         generator = elligator2.map_to_curve25519(pregenerator)
         self.trezor_private_key = random.bytes(32)
