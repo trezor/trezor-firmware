c32002082225d3eab6e74670000daba62242729d
Roman Zeyde <roman.zeyde@satoshilabs.com>
2025-08-13 16:50:12 +0300

    feat(core): reimplement THP event loop restarts
    [no changelog]

--- core/src/trezor/wire/thp/channel.py
+++ core/src/trezor/wire/thp/channel.py
@@ -19,14 +19,20 @@ from storage.cache_thp import (
     conditionally_replace_channel,
     is_there_a_channel_to_replace,
 )
-from trezor import loop, protobuf, utils, workflow
-from trezor.wire.errors import WireBufferError
+from trezor import protobuf, utils, workflow
 
-from . import ENCRYPTED, ChannelState, PacketHeader, ThpDecryptionError, ThpError
+from ..protocol_common import Message
+from . import (
+    ACK_MESSAGE,
+    ENCRYPTED,
+    ChannelState,
+    PacketHeader,
+    ThpDecryptionError,
+    ThpError,
+)
 from . import alternating_bit_protocol as ABP
 from . import control_byte, crypto, memory_manager
 from .checksum import CHECKSUM_LENGTH, is_valid
-from .transmission_loop import TransmissionLoop
 from .writer import MESSAGE_TYPE_LENGTH
 
 if __debug__:
@@ -36,19 +42,21 @@ if __debug__:
     from . import state_to_str
 
 if TYPE_CHECKING:
-    from typing import Any, Awaitable
+    from typing import Any, Awaitable, Callable
 
     from trezor.messages import ThpPairingCredential
     from trezor.wire import WireInterface
 
     from .interface_context import ThpContext
+    from .memory_manager import ThpBuffer
     from .pairing_context import PairingContext
     from .session_context import GenericSessionContext
 
 
 class Reassembler:
-    def __init__(self, cid: int) -> None:
+    def __init__(self, cid: int, read_buf: ThpBuffer) -> None:
         self.cid = cid
+        self.thp_read_buf = read_buf
         self.reset()
 
     def reset(self) -> None:
@@ -61,8 +69,6 @@ class Reassembler:
         Process current packet, returning `True` when a valid message is reassembled.
         The parsed message can retrieved via the `message` field (if it's not `None`).
         In case of a checksum error or if the reassembly is not over, return `False`.
-
-        May raise `WireBufferError` if there is a concurrent payload reassembly in progress.
         """
         ctrl_byte = packet[0]
         if control_byte.is_continuation(ctrl_byte):
@@ -70,8 +76,7 @@ class Reassembler:
                 # ignore unexpected continuation packets
                 return False
 
-            # may raise WireBufferError
-            buffer = memory_manager.get_existing_read_buffer(self.cid)
+            buffer = self.thp_read_buf.get(self.buffer_len)
             self._buffer_packet_data(buffer, packet, PacketHeader.CONT_LENGTH)
         else:
             self.reset()
@@ -83,8 +88,7 @@ class Reassembler:
                 buffer = packet[: self.buffer_len]
                 self.bytes_read = len(buffer)
             else:
-                # may raise WireBufferError
-                buffer = memory_manager.get_new_read_buffer(self.cid, self.buffer_len)
+                buffer = self.thp_read_buf.get(self.buffer_len)
                 self._buffer_packet_data(buffer, packet, 0)
 
         assert len(buffer) == self.buffer_len
@@ -124,26 +128,27 @@ class Channel:
     THP protocol encrypted communication channel.
     """
 
-    def __init__(self, channel_cache: ChannelCache, ctx: ThpContext) -> None:
+    def __init__(
+        self,
+        channel_cache: ChannelCache,
+        ctx: ThpContext,
+        buffers: tuple[ThpBuffer, ThpBuffer],
+    ) -> None:
         assert ctx._iface.iface_num() == channel_cache.get_int(CHANNEL_IFACE)
 
         # Channel properties
         self.channel_id: bytes = channel_cache.channel_id
         self.ctx: ThpContext = ctx
+        self.read_buf, self.write_buf = buffers
         if __debug__:
             self._log("channel initialization")
         self.channel_cache: ChannelCache = channel_cache
 
         # Shared variables
         self.sessions: dict[int, GenericSessionContext] = {}
-        self.reassembler = Reassembler(self.get_channel_id_int())
-
-        # Objects for writing a message to a wire
-        self.transmission_loop: TransmissionLoop | None = None
-        self.write_task_spawn: loop.spawn | None = None
+        self.reassembler = Reassembler(self.get_channel_id_int(), self.read_buf)
 
         # Temporary objects
-        self.handshake: crypto.Handshake | None = None
         self.credential: ThpPairingCredential | None = None
         self.connection_context: PairingContext | None = None
 
@@ -156,7 +161,6 @@ class Channel:
 
     def clear(self) -> None:
         clear_sessions_with_channel_id(self.channel_id)
-        memory_manager.release_lock_if_owner(self.get_channel_id_int())
         self.channel_cache.clear()
 
     # ACCESS TO CHANNEL_DATA
@@ -204,36 +208,100 @@ class Channel:
 
     # READ and DECRYPT
 
+    async def recv_payload(
+        self, expected_ctrl_byte: Callable[[int], bool] | None
+    ) -> memoryview:
+        """
+        Receive and return a valid THP payload from this channel & its control byte.
+        Also handle ACKs while waiting for the payload.
+
+        Raise if the received control byte is an unexpected one.
+
+        If `expected_ctrl_byte` is `None`, returns after the first received ACK.
+        """
+        while True:
+            # Handle an existing message (if already reassembled).
+            # Otherwise, receive and reassemble a new one.
+            msg = await self._get_reassembled_message()
+
+            # Synchronization process
+            ctrl_byte = msg[0]
+            payload = msg[PacketHeader.INIT_LENGTH : -CHECKSUM_LENGTH]
+            seq_bit = control_byte.get_seq_bit(ctrl_byte)
+
+            # 1: Handle ACKs
+            if control_byte.is_ack(ctrl_byte):
+                handle_ack(self, control_byte.get_ack_bit(ctrl_byte))
+                if expected_ctrl_byte is None:
+                    return payload
+                continue
+
+            if expected_ctrl_byte is None or not expected_ctrl_byte(ctrl_byte):
+                raise ThpError("Unexpected control byte")
+
+            # 2: Handle message with unexpected sequential bit
+            if seq_bit != ABP.get_expected_receive_seq_bit(self.channel_cache):
+                if __debug__:
+                    self._log(
+                        "Received message with an unexpected sequential bit",
+                    )
+                await send_ack(self, ack_bit=seq_bit)
+                raise ThpError("Received message with an unexpected sequential bit")
+
+            # 3: Send ACK in response
+            await send_ack(self, ack_bit=seq_bit)
+
+            ABP.set_expected_receive_seq_bit(self.channel_cache, 1 - seq_bit)
+
+            return payload
+
+    async def _get_reassembled_message(self) -> memoryview:
+        """Doesn't block if a message has been already reassembled."""
+        while self.reassembler.message is None:
+            # receive and reassemble a new message from this channel
+            channel = await self.ctx.get_next_message()
+            if channel is self:
+                break
+
+            # currently only single-channel sessions are supported during a single event loop run
+            self._log(
+                "Ignoring unexpected channel: ",
+                utils.hexlify_if_bytes(channel.channel_id),
+                logger=log.warning,
+            )
+
+        msg = self.reassembler.message
+        self.reassembler.reset()  # next call will reassemble a new message
+        assert msg is not None
+        return msg
+
     def reassemble(self, packet: utils.BufferType) -> bool:
         """
         Process current packet, returning `True` when a valid message is reassembled.
         The parsed message can retrieved via the `message` field (if it's not `None`).
         In case of a checksum error or if the reassembly is not over, return `False`.
-
-        May raise `WireBufferError` if there is a concurrent payload reassembly in progress.
         """
         if self.get_channel_state() == ChannelState.UNALLOCATED:
             return False
-        try:
-            return self.reassembler.handle_packet(memoryview(packet))
-        except WireBufferError:
-            self.reassembler.reset()
-            raise
+        return self.reassembler.handle_packet(memoryview(packet))
 
-    def decrypt_buffer(
-        self, message_length: int, offset: int = PacketHeader.INIT_LENGTH
-    ) -> None:
-        buffer = memory_manager.get_existing_read_buffer(self.get_channel_id_int())
+    async def decrypt_message(self) -> tuple[int, Message]:
+        """
+        Receive, decrypt and return a `(session_id, message)` tuple.
+        Also handle ACKs while waiting for the message.
+        """
+        payload = await self.recv_payload(control_byte.is_encrypted_transport)
+        self._decrypt_buffer(payload)
+        session_id, message_type = ustruct.unpack(">BH", payload)
+        message = Message(
+            message_type,
+            payload[SESSION_ID_LENGTH + MESSAGE_TYPE_LENGTH : -TAG_LENGTH],
+        )
+        return (session_id, message)
 
-        noise_buffer = memoryview(buffer)[
-            offset : message_length - CHECKSUM_LENGTH - TAG_LENGTH
-        ]
-        tag = buffer[
-            message_length
-            - CHECKSUM_LENGTH
-            - TAG_LENGTH : message_length
-            - CHECKSUM_LENGTH
-        ]
+    def _decrypt_buffer(self, payload: memoryview) -> None:
+        noise_buffer = payload[:-TAG_LENGTH]
+        tag = payload[-TAG_LENGTH:]
 
         key_receive = self.channel_cache.get(CHANNEL_KEY_RECEIVE)
         nonce_receive = self.channel_cache.get_int(CHANNEL_NONCE_RECEIVE)
@@ -264,7 +332,6 @@ class Channel:
         self,
         msg: protobuf.MessageType,
         session_id: int = 0,
-        force: bool = False,
     ) -> None:
         if __debug__:
             self._log(
@@ -279,93 +346,54 @@ class Channel:
                     iface=self.iface,
                 )
 
-        cid = self.get_channel_id_int()
         msg_size = protobuf.encoded_length(msg)
         payload_size = SESSION_ID_LENGTH + MESSAGE_TYPE_LENGTH + msg_size
         length = payload_size + CHECKSUM_LENGTH + TAG_LENGTH + PacketHeader.INIT_LENGTH
 
-        buffer = memory_manager.get_new_write_buffer(cid, length)
+        buffer = self.write_buf.get(length)
         noise_payload_len = memory_manager.encode_into_buffer(buffer, msg, session_id)
-
-        task = self._write_and_encrypt(noise_payload_len=noise_payload_len, force=force)
-        if task is not None:
-            await task
-
-    def write_error(self, err_type: int) -> Awaitable[None]:
-        msg_data = err_type.to_bytes(1, "big")
-        length = len(msg_data) + CHECKSUM_LENGTH
-        header = PacketHeader.get_error_header(self.get_channel_id_int(), length)
-        return self.ctx.write_payload(header, msg_data)
-
-    def write_handshake_message(self, ctrl_byte: int, payload: bytes) -> None:
-        self._prepare_write()
-        self.write_task_spawn = loop.spawn(
-            self._write_encrypted_payload_loop(ctrl_byte, payload)
-        )
-
-    def _write_and_encrypt(
-        self,
-        noise_payload_len: int,
-        force: bool = False,
-    ) -> Awaitable[None] | None:
-        buffer = memory_manager.get_existing_write_buffer(self.get_channel_id_int())
 
         self._encrypt(buffer, noise_payload_len)
         payload_length = noise_payload_len + TAG_LENGTH
 
-        if self.write_task_spawn is not None:
-            self.write_task_spawn.close()  # TODO might break something
-            if __debug__:
-                self._log("Closed write task", logger=log.warning)
-        self._prepare_write()
-        if force:
-            if __debug__:
-                self._log("Writing FORCE message (without async or retransmission).")
+        return await self.write_encrypted_payload(ENCRYPTED, buffer[:payload_length])
 
-            return self._write_encrypted_payload_loop(
-                ENCRYPTED, memoryview(buffer[:payload_length])
-            )
-        self.write_task_spawn = loop.spawn(
-            self._write_encrypted_payload_loop(
-                ENCRYPTED, memoryview(buffer[:payload_length])
-            )
-        )
-        return None
+    def write_handshake_message(
+        self, ctrl_byte: int, payload: bytes
+    ) -> Awaitable[None]:
+        return self.write_encrypted_payload(ctrl_byte, payload)
 
-    def _prepare_write(self) -> None:
-        # TODO add condition that disallows to write when can_send_message is false
-        ABP.set_sending_allowed(self.channel_cache, False)
-
-    async def _write_encrypted_payload_loop(
-        self, ctrl_byte: int, payload: bytes, only_once: bool = False
-    ) -> None:
+    async def write_encrypted_payload(self, ctrl_byte: int, payload: bytes) -> None:
         if __debug__:
             self._log("write_encrypted_payload_loop")
+
+        assert ABP.is_sending_allowed(self.channel_cache)
 
         payload_len = len(payload) + CHECKSUM_LENGTH
         sync_bit = ABP.get_send_seq_bit(self.channel_cache)
         ctrl_byte = control_byte.add_seq_bit_to_ctrl_byte(ctrl_byte, sync_bit)
         header = PacketHeader(ctrl_byte, self.get_channel_id_int(), payload_len)
-        self.transmission_loop = TransmissionLoop(self, header, payload)
-        if only_once:
-            if __debug__:
-                self._log('Starting transmission loop "only once"')
-            await self.transmission_loop.start(max_retransmission_count=1)
-        else:
-            if __debug__:
-                self._log("Starting transmission loop")
-            await self.transmission_loop.start()
+
+        # ACK is needed before sending more data
+        ABP.set_sending_allowed(self.channel_cache, False)
+
+        # TODO implement retransmissions:
+        #   sender = loop.spawn(self._retransmit(header, payload))  # will raise on timeout
+        #   receiver = loop.spawn(self._wait_for_ack())  # will return on success
+        #   await loop.race(sender, receiver)
+        await self.ctx.write_payload(header, payload)
+        await self._wait_for_ack()
+
+        # `ABP.set_sending_allowed()` will be called after a valid ACK
+        assert ABP.is_sending_allowed(self.channel_cache)
 
         ABP.set_send_seq_bit_to_opposite(self.channel_cache)
 
-        # Let the main loop be restarted and clear loop, if there is no other
-        # workflow and the state is ENCRYPTED_TRANSPORT
-        # TODO only once is there to not clear when FALLBACK
-        # TODO missing transmission loop is active -> do not clear
-        if not only_once and self._can_clear_loop():
-            if __debug__:
-                self._log("clearing loop from channel")
-            loop.clear()
+    async def _wait_for_ack(self) -> None:
+        # `ABP.set_sending_allowed()` will be called after a valid ACK
+        while not ABP.is_sending_allowed(self.channel_cache):
+            # wait and return after receiving an ACK, or raise in case of an unexpected message.
+            await self.recv_payload(expected_ctrl_byte=None)
 
     def _encrypt(self, buffer: utils.BufferType, noise_payload_len: int) -> None:
         if __debug__:
@@ -389,11 +417,6 @@ class Channel:
 
         buffer[noise_payload_len : noise_payload_len + TAG_LENGTH] = tag
 
-    def _can_clear_loop(self) -> bool:
-        return (
-            not workflow.tasks
-        ) and self.get_channel_state() is ChannelState.ENCRYPTED_TRANSPORT
-
     if __debug__:
 
         def _log(self, text_1: str, text_2: str = "", logger: Any = log.debug) -> None:
@@ -405,3 +428,30 @@ class Channel:
                 text_2,
                 iface=self.iface,
             )
+
+
+def send_ack(channel: Channel, ack_bit: int) -> Awaitable[None]:
+    ctrl_byte = control_byte.add_ack_bit_to_ctrl_byte(ACK_MESSAGE, ack_bit)
+    header = PacketHeader(ctrl_byte, channel.get_channel_id_int(), CHECKSUM_LENGTH)
+    if __debug__:
+        log.debug(
+            __name__,
+            "Writing ACK message to a channel with cid: %s, ack_bit: %d",
+            hexlify_if_bytes(channel.channel_id),
+            ack_bit,
+            iface=channel.iface,
+        )
+    return channel.ctx.write_payload(header, b"")
+
+
+def handle_ack(ctx: Channel, ack_bit: int) -> None:
+    if not ABP.is_ack_valid(ctx.channel_cache, ack_bit):
+        return
+    # ACK is expected and it has correct sync bit
+    if __debug__:
+        log.debug(
+            __name__,
+            "Received ACK message with correct ack bit",
+            iface=ctx.iface,
+        )
+    ABP.set_sending_allowed(ctx.channel_cache, True)
--- tests/device_tests/thp/test_multiple_hosts.py
+++ tests/device_tests/thp/test_multiple_hosts.py
@@ -1,153 +1,45 @@
-import os
-from time import sleep
-
 import pytest
 
 from trezorlib import exceptions
 from trezorlib.client import ProtocolV2Channel
 from trezorlib.debuglink import TrezorClientDebugLink as Client
 
-from ...conftest import LOCK_TIME
-
 pytestmark = [pytest.mark.protocol("protocol_v2"), pytest.mark.invalidate_client]
 
-# LOCK_TIME = 0.2
 
-
-def _prepare_two_hosts_for_handshake(
-    client: Client, init_noise: bool = True
-) -> tuple[ProtocolV2Channel, ProtocolV2Channel]:
-    # Sleep for LOCK_TIME
-    sleep(LOCK_TIME)
-
-    protocol_1 = client.protocol
-    protocol_1._reset_sync_bits()
-    protocol_2 = ProtocolV2Channel(
-        protocol_1.transport, protocol_1.mapping, prepare_channel_without_pairing=False
+def _new_channel(client) -> ProtocolV2Channel:
+    channel = ProtocolV2Channel(
+        transport=client.transport,
+        mapping=client.mapping,
+        credential=None,
+        prepare_channel_without_pairing=False,
     )
-    protocol_2._reset_sync_bits()
-
-    nonce_1 = os.urandom(8)
-    nonce_2 = os.urandom(8)
-    if nonce_1 == nonce_2:
-        nonce_2 = (int.from_bytes(nonce_1) + 1).to_bytes(8, "big")
-    protocol_1._send_channel_allocation_request(nonce_1)
-    protocol_1.channel_id, protocol_1.device_properties = (
-        protocol_1._read_channel_allocation_response(nonce_1)
-    )
-    protocol_2._send_channel_allocation_request(nonce_2)
-    protocol_2.channel_id, protocol_2.device_properties = (
-        protocol_2._read_channel_allocation_response(nonce_2)
-    )
-    if init_noise:
-        protocol_1._init_noise()
-        protocol_2._init_noise()
-
-    return protocol_1, protocol_2
+    channel._do_channel_allocation()
+    channel._init_noise()
+    return channel
 
 
-def _prepare_two_hosts(client: Client) -> tuple[ProtocolV2Channel, ProtocolV2Channel]:
-    protocol_1, protocol_2 = _prepare_two_hosts_for_handshake(
-        client=client, init_noise=False
-    )
-    protocol_1._do_handshake()
+def test_concurrent_handshakes(client: Client) -> None:
+    protocol_1 = _new_channel(client)
+    protocol_2 = _new_channel(client)
 
-    client.protocol = protocol_1
-    client.do_pairing()
-    sleep(LOCK_TIME)
+    # The first host starts handshake
+    protocol_1._send_handshake_init_request()
+    protocol_1._read_ack()
+    protocol_1._read_handshake_init_response()
+
+    # The second host starts handshake
+    protocol_2._send_handshake_init_request()
+
+    # The second host should not be able to interrupt the first host's handshake
+    with pytest.raises(exceptions.ThpError) as e:
+        protocol_2._read_ack()
+    assert e.value.args[0] == "TRANSPORT BUSY"
+
+    # The first host can complete handshake
+    protocol_1._send_handshake_completion_request()
+    protocol_1._read_ack()
+    protocol_1._read_handshake_completion_response()
+
+    # Now the second handshake can be done
     protocol_2._do_handshake()
-    client.protocol = protocol_2
-    client.do_pairing()
-
-    return protocol_1, protocol_2
-
-
-def test_concurrent_handshakes_1(client: Client) -> None:
-    client = client.get_new_client()
-    protocol_1, protocol_2 = _prepare_two_hosts_for_handshake(client)
-
-    # The first host starts handshake
-    protocol_1._send_handshake_init_request()
-    protocol_1._read_ack()
-    protocol_1._read_handshake_init_response()
-
-    # The second host starts handshake
-    protocol_2._send_handshake_init_request()
-
-    # The second host should not be able to interrupt the first host's handshake
-    # until timeout (LOCK_TIME) has expired
-    with pytest.raises(exceptions.ThpError) as e:
-        protocol_2._read_ack()
-    assert e.value.args[0] == "TRANSPORT BUSY"
-
-    # Wait for LOCK_TIME to expire
-    sleep(LOCK_TIME)
-
-    # The second host retries and finishes handhake successfully
-    protocol_2._init_noise()
-    protocol_2._send_handshake_init_request()
-    protocol_2._read_ack()
-    protocol_2._read_handshake_init_response()
-
-    protocol_2._send_handshake_completion_request()
-    protocol_2._read_ack()
-    protocol_2._read_handshake_completion_response()
-
-    # The second host performs action that results
-    # in the invalidation of the first host's handshake state
-    client.protocol = protocol_2
-    client.do_pairing()
-
-    # Even after LOCK_TIME passes, the first host's channel cannot
-    # be resumed
-    sleep(LOCK_TIME)
-    protocol_1._send_handshake_completion_request()
-    protocol_1._read_ack()
-
-    with pytest.raises(exceptions.ThpError) as e:
-        protocol_1._read_handshake_completion_response()
-    assert e.value.args[0] == "UNALLOCATED CHANNEL"
-
-
-def test_concurrent_handshakes_2(client: Client) -> None:
-    protocol_1, protocol_2 = _prepare_two_hosts_for_handshake(client)
-
-    # The first host starts handshake
-    protocol_1._send_handshake_init_request()
-    protocol_1._read_ack()
-    protocol_1._read_handshake_init_response()
-
-    # The second host starts handshake
-    protocol_2._send_handshake_init_request()
-
-    # The second host should not be able to interrupt the first host's handshake
-    # until timeout (LOCK_TIME) has expired
-    with pytest.raises(exceptions.ThpError) as e:
-        protocol_2._read_ack()
-    assert e.value.args[0] == "TRANSPORT BUSY"
-
-    # Wait for LOCK_TIME to expire
-    sleep(LOCK_TIME)
-
-    # The second host retries and finishes handhake successfully
-    protocol_2._init_noise()
-    protocol_2._send_handshake_init_request()
-    protocol_2._read_ack()
-    protocol_2._read_handshake_init_response()
-
-    protocol_2._send_handshake_completion_request()
-    protocol_2._read_ack()
-    protocol_2._read_handshake_completion_response()
-
-    # The first host tries to continue handshake immediately after
-    # the second host finishes it
-
-    protocol_1._send_handshake_completion_request()
-
-    with pytest.raises(exceptions.ThpError) as e:
-        protocol_1._read_ack()
-
-        # protocol_1._read_handshake_completion_response()
-    assert e.value.args[0] == "TRANSPORT BUSY"
-
-    # TODO - test ACK fallback, test standard encrypted message fallback
