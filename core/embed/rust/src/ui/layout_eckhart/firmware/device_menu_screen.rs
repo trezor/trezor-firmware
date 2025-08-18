c8fb44cc07639b47a99863d3803077ed5271fdfc
Lukas Bielesch <bielesch.lukas@gmail.com>
2025-08-19 00:17:38 +0200

    chore(core/eckhart): add device menu notification entries

--- core/embed/rust/src/ui/layout_eckhart/firmware/device_menu_screen.rs
+++ core/embed/rust/src/ui/layout_eckhart/firmware/device_menu_screen.rs
@@ -100,7 +100,9 @@ struct MenuItem {
     action: Option<Action>,
 }
 const MENU_ITEM_TITLE_STYLE_SHEET: &ButtonStyleSheet = &theme::menu_item_title();
+const MENU_ITEM_LIGHT_WARNING: &ButtonStyleSheet = &theme::menu_item_title_yellow();
 const MENU_ITEM_WARNING: &ButtonStyleSheet = &theme::menu_item_title_orange();
+const MENU_ITEM_ERROR: &ButtonStyleSheet = &theme::menu_item_title_red();
 
 impl MenuItem {
     pub fn new(text: TString<'static>, action: Option<Action>) -> Self {
@@ -250,9 +252,14 @@ impl DeviceMenuScreen {
 
         let devices = screen.add_paired_devices_menu(paired_devices, paired_device_indices);
         let pair_and_connect = screen.add_pair_and_connect_menu(devices, connected_subtext);
-
-        let root =
-            screen.add_root_menu(failed_backup, pair_and_connect, settings, connected_subtext);
+        let pin_unset = pin_code == Some(false);
+        let root = screen.add_root_menu(
+            failed_backup,
+            pin_unset,
+            pair_and_connect,
+            settings,
+            connected_subtext,
+        );
 
         screen.set_active_subscreen(root);
 
@@ -523,6 +530,7 @@ impl DeviceMenuScreen {
     fn add_root_menu(
         &mut self,
         failed_backup: bool,
+        pin_unset: bool,
         pair_and_connect_index: usize,
         settings_index: usize,
         connected_subtext: Option<TString<'static>>,
@@ -534,8 +542,17 @@ impl DeviceMenuScreen {
                 Some(Action::Return(DeviceMenuMsg::BackupFailed)),
             );
             item_backup_failed.with_subtext(Some((TR::words__review.into(), None)));
-            item_backup_failed.with_stylesheet(MENU_ITEM_TITLE_STYLE_SHEET);
+            item_backup_failed.with_stylesheet(MENU_ITEM_ERROR);
             unwrap!(items.push(item_backup_failed));
+        }
+        if pin_unset {
+            let mut item_pin_unset = MenuItem::new(
+                TR::homescreen__title_pin_not_set.into(),
+                Some(Action::Return(DeviceMenuMsg::PinCode)),
+            );
+            item_pin_unset.with_subtext(Some((TR::words__set.into(), None)));
+            item_pin_unset.with_stylesheet(MENU_ITEM_LIGHT_WARNING);
+            unwrap!(items.push(item_pin_unset));
         }
         let mut item_pair_and_connect = MenuItem::new(
             TR::ble__pair_title.into(),
