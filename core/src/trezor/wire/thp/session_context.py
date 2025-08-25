427f9209532a3ae04025b807ea5c5b642a05f0ea
Roman Zeyde <roman.zeyde@satoshilabs.com>
2025-08-26 00:02:53 +0300

    refactor(core): simplify THP session exception handling
    [no changelog]

--- core/src/trezor/wire/thp/session_context.py
+++ core/src/trezor/wire/thp/session_context.py
@@ -35,7 +35,7 @@ class GenericSessionContext(Context):
         self.channel: Channel = channel
         self.session_id: int = session_id
 
-    async def handle(self, message: Message | None = None) -> None:
+    async def handle(self, message: Message) -> None:
         if __debug__:
             log.debug(
                 __name__,
@@ -47,25 +47,22 @@ class GenericSessionContext(Context):
 
         while True:
             try:
-                if message is None:
-                    message = await self._read_next_message()
                 await handle_single_message(self, message)
                 if __debug__:
                     self.channel._log("session loop is over")
-                return
             except protocol_common.WireError as e:
                 if __debug__:
                     log.exception(__name__, e, iface=self.iface)
                 await self.write(failure(e))
-                return
             except UnexpectedMessageException as unexpected:
                 # The workflow was interrupted by an unexpected message. We need to
                 # process it as if it was a new message...
                 message = unexpected.msg
+                continue
             except Exception as exc:
                 if __debug__:
                     log.exception(__name__, exc, iface=self.iface)
-                return
+            return
 
     async def _read_next_message(self) -> Message:
         while True:
