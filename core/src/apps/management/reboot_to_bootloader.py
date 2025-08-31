83599a85acc6a36d3e62be932955be4119c74f99
Roman Zeyde <roman.zeyde@satoshilabs.com>
2025-08-31 13:37:09 +0300

    fix(core): wait 500ms before rebooting to bootloader
    In general, Trezor should not wait for too long for a THP ACK after
    sending the `Success` response (since the last ACK may be lost, similar
    to [Two generals' agreement and TCP handshake](https://stackoverflow.com/q/36352236)).
    
    [no changelog]

--- core/src/apps/management/reboot_to_bootloader.py
+++ core/src/apps/management/reboot_to_bootloader.py
@@ -1,4 +1,5 @@
 import utime
+from micropython import const
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -6,6 +7,9 @@ if TYPE_CHECKING:
 
     from trezor.enums import BootCommand
     from trezor.messages import RebootToBootloader
+
+
+_REBOOT_SUCCESS_TIMEOUT_MS = const(500)
 
 
 async def install_upgrade(
@@ -94,9 +98,14 @@ async def reboot_to_bootloader(msg: RebootToBootloader) -> NoReturn:
         boot_args = None
 
     ctx = get_context()
-    await ctx.write(Success(message="Rebooting"))
-    # make sure the outgoing USB buffer is flushed
-    await loop.wait(ctx.iface.iface_num() | io.POLL_WRITE)
+    # After ACK-ing the `Success` message (over THP), the host may already be waiting for the bootloader to start.
+    # In case this THP ACK packet is lost, the device should stop retransmissions, and reboot anyway.
+    res = await loop.race(
+        ctx.write(Success(message="Rebooting")), loop.sleep(_REBOOT_SUCCESS_TIMEOUT_MS)
+    )
+    if res is None:
+        # make sure the outgoing buffer is flushed
+        await loop.wait(ctx.iface.iface_num() | io.POLL_WRITE)
 
     utime.sleep_ms(10)
     # reboot to the bootloader, pass the firmware header hash if any
