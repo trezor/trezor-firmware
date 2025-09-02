e31abf507c4cb823c9a5d714fc3008318fb45401
kopecdav <david.kopecky@satoshilabs.com>
2025-09-02 10:53:22 +0200

    fixup! feat(core/bootloader): add LED effect to bootloader pairing workflow.

--- core/embed/projects/bootloader/workflow/wf_empty_device.c
+++ core/embed/projects/bootloader/workflow/wf_empty_device.c
@@ -66,7 +66,7 @@ workflow_result_t workflow_empty_device(void) {
 #ifdef USE_BLE
     if (res == WF_OK_UI_ACTION && ui_result == WELCOME_PAIRING_MODE) {
 #ifdef USE_RGB_LED
-      rgb_led_effect_start(RGB_LED_EFFECT_BOOTLOADER_BREATHE, 0);
+      rgb_led_effect_start(RGB_LED_EFFECT_PAIRING, 0);
 #endif
 
       res = workflow_wireless_setup(NULL, NULL, &ios);
