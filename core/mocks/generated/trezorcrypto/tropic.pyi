3722d70720e496c18809c01e9c12eca048e1cb3b
Andrew Kozlik <andrew.kozlik@gmail.com>
2025-09-11 15:09:45 +0200

    feat(core): Read Tropic certificates.

--- core/mocks/generated/trezorcrypto/tropic.pyi
+++ core/mocks/generated/trezorcrypto/tropic.pyi
@@ -5,24 +5,39 @@ from typing import *
 class TropicError(Exception):
     """Error returned by the Tropic Square chip."""
 
-    def ping(message: str) -> str:
-        """
-        Test the session by pinging the chip.
-        """
 
-    def key_generate(
-        key_index: int,
-    ) -> None:
-        """
-        Generate ECC key in the device's ECC key slot.
-        """
+# upymod/modtrezorcrypto/modtrezorcrypto-tropic.h
+def ping(message: str) -> str:
+    """
+    Test the session by pinging the chip.
+    """
 
-    def sign(
-        key_index: int,
-        digest: bytes,
-    ) -> bytes:
-        """
-        Uses the private key at key_index to produce a signature of the digest.
-        """
-    DEVICE_KEY_SLOT: int
-    FIDO_KEY_SLOT: int
+
+# upymod/modtrezorcrypto/modtrezorcrypto-tropic.h
+def key_generate(
+    key_index: int,
+) -> None:
+    """
+    Generate ECC key in the device's ECC key slot.
+    """
+
+
+# upymod/modtrezorcrypto/modtrezorcrypto-tropic.h
+def sign(
+    key_index: int,
+    digest: bytes,
+) -> bytes:
+    """
+    Uses the private key at key_index to produce a signature of the digest.
+    """
+
+
+# upymod/modtrezorcrypto/modtrezorcrypto-tropic.h
+def get_user_data(index: int) -> bytes:
+    """
+    Return the user data stored at the given index.
+    """
+DEVICE_CERT_INDEX: int
+DEVICE_KEY_SLOT: int
+FIDO_CERT_INDEX: int
+FIDO_KEY_SLOT: int
