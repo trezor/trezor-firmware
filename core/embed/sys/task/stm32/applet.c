7dd0dc8f36bbde4a462927685b8908ed0e4987df
cepetr <petr@cerninovi.cz>
2025-09-12 16:08:17 +0200

    refactor(core): remove coreapp specific logic from applets
    [no changelog]

--- core/embed/sys/task/stm32/applet.c
+++ core/embed/sys/task/stm32/applet.c
@@ -40,47 +40,6 @@ void applet_init(applet_t* applet, const applet_layout_t* layout,
   applet->privileges = *privileges;
 }
 
-static void applet_clear_memory(applet_t* applet) {
-  mpu_set_active_applet(&applet->layout);
-
-  if (applet->layout.data1.size > 0) {
-    memset((void*)applet->layout.data1.start, 0, applet->layout.data1.size);
-  }
-  if (applet->layout.data2.size > 0) {
-    memset((void*)applet->layout.data2.start, 0, applet->layout.data2.size);
-  }
-}
-
-bool applet_reset(applet_t* applet, uint32_t cmd, const void* arg,
-                  size_t arg_size) {
-  // Clear all memory the applet is allowed to use
-  applet_clear_memory(applet);
-
-  const applet_header_t* header = (applet_header_t*)applet->layout.code1.start;
-
-  // Reset the applet task (stack pointer, etc.)
-  if (!systask_init(&applet->task, header->stack.start, header->stack.size, 0,
-                    applet)) {
-    return false;
-  }
-
-  // Copy the arguments onto the applet stack
-  void* arg_copy = NULL;
-  if (arg != NULL && arg_size > 0) {
-    arg_copy = systask_push_data(&applet->task, arg, arg_size);
-    if (arg_copy == NULL) {
-      return false;
-    }
-  }
-
-  // Schedule the applet task run
-  uint32_t arg1 = cmd;
-  uint32_t arg2 = (uint32_t)arg_copy;
-  uint32_t arg3 = rng_get();
-
-  return systask_push_call(&applet->task, header->startup, arg1, arg2, arg3);
-}
-
 #ifdef USE_TRUSTZONE
 // Sets unprivileged access to the applet memory regions
 // and allows applet to use some specific peripherals.
