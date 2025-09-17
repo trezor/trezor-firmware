46cb53498dcc7761b028f6d0d11a9be91d435ff9
obrusvit <vit.obrusnik@gmail.com>
2025-09-17 17:37:32 +0200

    feat(core/eckhart): widen border shape
    - instead of drawing the border 1px towards center, make the border 1px
    wider and draw it at the exact edges
    - this is to prevent underlying shapes from peeking at the sides
    - effectively reverts
    https://github.com/trezor/trezor-firmware/pull/5510
    
    [no changelog]

--- core/embed/rust/src/ui/layout_eckhart/cshape/screen_border.rs
+++ core/embed/rust/src/ui/layout_eckhart/cshape/screen_border.rs
@@ -1,6 +1,6 @@
 use crate::ui::{
     display::Color,
-    geometry::{Alignment2D, Insets, Rect},
+    geometry::{Alignment2D, Rect},
     shape::{self, Renderer},
 };
 
@@ -9,47 +9,42 @@ use super::super::{
     theme::{ICON_BORDER_BL, ICON_BORDER_BR, ICON_BORDER_TOP},
 };
 
-/// Custom shape for a full screen border overlay, parameterizable by color. In
-/// order to look same-width along the whole screen, the border is placed 1px
-/// from the actual border to account for minor variations in display
-/// production.
+/// Custom shape for a full screen border overlay, parameterizable by color.
 pub struct ScreenBorder {
     color: Color,
     side_bars: [Rect; 3],
 }
 
 impl ScreenBorder {
-    pub const TOTAL_WIDTH: i16 = 5;
-    pub const LINE_WIDTH: i16 = Self::TOTAL_WIDTH - 1;
+    pub const WIDTH: i16 = 5;
     pub const TOP_ARC_HEIGHT: i16 = ICON_BORDER_TOP.toif.height();
-    pub const SCREEN_SHRUNK: Rect = SCREEN.inset(Insets::uniform(1));
 
     pub const fn new(color: Color) -> Self {
         // Bottom bar: from the right edge of bottom-left icon to the left edge of
         // bottom-right icon.
         let bottom_bar_rect = Rect {
-            x0: Self::SCREEN_SHRUNK.x0 + ICON_BORDER_BL.toif.width(),
-            y0: Self::SCREEN_SHRUNK.y1 - Self::LINE_WIDTH,
-            x1: Self::SCREEN_SHRUNK.x1 - ICON_BORDER_BR.toif.width(),
-            y1: Self::SCREEN_SHRUNK.y1,
+            x0: SCREEN.x0 + ICON_BORDER_BL.toif.width(),
+            y0: SCREEN.y1 - Self::WIDTH,
+            x1: SCREEN.x1 - ICON_BORDER_BR.toif.width(),
+            y1: SCREEN.y1,
         };
 
         // Left bar: from the bottom edge of top-left icon to the top edge of
         // bottom-left icon.
         let left_bar_rect = Rect {
-            x0: Self::SCREEN_SHRUNK.x0,
-            y0: Self::SCREEN_SHRUNK.y0 + Self::TOP_ARC_HEIGHT,
-            x1: Self::SCREEN_SHRUNK.x0 + Self::LINE_WIDTH,
-            y1: Self::SCREEN_SHRUNK.y1 - ICON_BORDER_BL.toif.height(),
+            x0: SCREEN.x0,
+            y0: SCREEN.y0 + Self::TOP_ARC_HEIGHT,
+            x1: SCREEN.x0 + Self::WIDTH,
+            y1: SCREEN.y1 - ICON_BORDER_BL.toif.height(),
         };
 
         // Right bar: from the bottom edge of top-right icon to the top edge of
         // bottom-right icon.
         let right_bar_rect = Rect {
-            x0: Self::SCREEN_SHRUNK.x1 - Self::LINE_WIDTH,
-            y0: Self::SCREEN_SHRUNK.y0 + Self::TOP_ARC_HEIGHT,
-            x1: Self::SCREEN_SHRUNK.x1,
-            y1: Self::SCREEN_SHRUNK.y1 - ICON_BORDER_BR.toif.height(),
+            x0: SCREEN.x1 - Self::WIDTH,
+            y0: SCREEN.y0 + Self::TOP_ARC_HEIGHT,
+            x1: SCREEN.x1,
+            y1: SCREEN.y1 - ICON_BORDER_BR.toif.height(),
         };
         Self {
             color,
@@ -68,17 +63,17 @@ impl ScreenBorder {
         // Draw the irregular corner shapes
         [
             (
-                Self::SCREEN_SHRUNK.top_left(),
+                SCREEN.top_left(),
                 ICON_BORDER_TOP.toif,
                 Alignment2D::TOP_LEFT,
             ),
             (
-                Self::SCREEN_SHRUNK.bottom_left(),
+                SCREEN.bottom_left(),
                 ICON_BORDER_BL.toif,
                 Alignment2D::BOTTOM_LEFT,
             ),
             (
-                Self::SCREEN_SHRUNK.bottom_right(),
+                SCREEN.bottom_right(),
                 ICON_BORDER_BR.toif,
                 Alignment2D::BOTTOM_RIGHT,
             ),
--- core/embed/rust/src/ui/layout_eckhart/res/border/note.md
+++
@@ -1,2 +0,0 @@
-TOP, BL, BR icons are supposed to be used completely at the edge of the display.
-TOP_1, BL_1, BR_1 are adjusted to be rendered with 1px offset from the edge. See screen_border.rs for more details.
