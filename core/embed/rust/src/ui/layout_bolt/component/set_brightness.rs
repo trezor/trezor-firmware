8cc9e538a8b055b73520eece70aab0a996ba5b02
bleska <jiri.blecha@satoshilabs.com>
2025-09-19 14:01:03 +0200

    refactor(core): refactoring the function calls of display backlight driver in RUST so that the u16 data types have been converted to u8 ones. The code has been reviewed whether it's safe - no problems detected.
    [no changelog]

--- core/embed/rust/src/ui/layout_bolt/component/set_brightness.rs
+++ core/embed/rust/src/ui/layout_bolt/component/set_brightness.rs
@@ -36,15 +36,7 @@ impl Component for SetBrightnessDialog {
     fn event(&mut self, ctx: &mut EventCtx, event: Event) -> Option<Self::Msg> {
         match self.0.event(ctx, event) {
             Some(NumberInputSliderDialogMsg::Changed(value)) => {
-                // TODO: needs more analysis; why is "value" u16? Can it be changed to u8?
-                // Original code: display::set_backlight(value.into());
-                // Possible solution: display::set_backlight(value.try_into().unwrap());
-                // It's not save. To rather use unwrap!() macro? Another solution below.
-                if let Ok(val) = value.try_into() {
-                    display::set_backlight(val);
-                } else {
-                    display::set_backlight(255);
-                }
+                display::set_backlight(value.into());
                 None
             }
             Some(NumberInputSliderDialogMsg::Cancelled) => Some(CancelConfirmMsg::Cancelled),
