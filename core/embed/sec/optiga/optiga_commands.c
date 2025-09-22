8b791e4bb20ea9c93b480525cfb2f182ebda1587
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2025-09-22 17:24:50 +0200

    refactor(core/embed): introduce `optiga_set_counter()`
    [no changelog]

--- core/embed/sec/optiga/optiga_commands.c
+++ core/embed/sec/optiga/optiga_commands.c
@@ -1000,4 +1000,9 @@ optiga_result optiga_clear_all_auto_states(void) {
   return OPTIGA_SUCCESS;
 }
 
+optiga_result optiga_set_counter(uint16_t oid, uint8_t value) {
+  uint8_t value_array[8] = {0, 0, 0, 0, 0, 0, 0, value};
+  return optiga_set_data_object(oid, false, value_array, sizeof(value_array));
+}
+
 #endif  // SECURE_MODE
