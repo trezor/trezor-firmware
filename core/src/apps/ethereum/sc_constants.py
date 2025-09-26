58c0e94befeaabe0baef4ccb26c09352353acced
Jiří Čermák <jiri.cermak@satoshilabs.com>
2025-09-26 14:40:26 +0200

    chore(ethereum): upgrading from Holesky to Hoodi
    - Ethereum testnet upgrade.  Updated literal references, and fixtures with the new name and chain ID.
    - SLIP44 base indices updated from 1 to 60.
    - Small change in eth/keychain required to work with older 1 index based
      testnet addresses.

--- core/src/apps/ethereum/sc_constants.py
+++ core/src/apps/ethereum/sc_constants.py
@@ -21,11 +21,11 @@ SC_FUNC_SIG_CLAIM = unhexlify("33986ffa")
 
 # addresses for pool (stake/unstake) and accounting (claim) operations
 ADDRESSES_POOL = (
-    unhexlify("AFA848357154a6a624686b348303EF9a13F63264"),  # holesky testnet
+    unhexlify("AFA848357154a6a624686b348303EF9a13F63264"),  # Hoodi testnet
     unhexlify("D523794C879D9eC028960a231F866758e405bE34"),  # mainnet
 )
 ADDRESSES_ACCOUNTING = (
-    unhexlify("624087DD1904ab122A32878Ce9e933C7071F53B9"),  # holesky testnet
+    unhexlify("624087DD1904ab122A32878Ce9e933C7071F53B9"),  # Hoodi testnet
     unhexlify("7a7f0b3c23C23a31cFcb0c44709be70d4D545c6e"),  # mainnet
 )
 
