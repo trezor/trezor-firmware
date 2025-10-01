33b09988691f860c97b9aa76d050ef7ed5774ee4
Ioan Bizău <i@ibz.me>
2025-10-01 15:38:47 +0200

    chore(tests): test Tropic authenticity check
    [no changelog]

--- tests/device_tests/test_authenticate_device.py
+++ tests/device_tests/test_authenticate_device.py
@@ -1,7 +1,7 @@
 import pytest
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric import ec, ed25519
 from cryptography.x509 import extensions as ext
 
 from trezorlib import device, models
@@ -11,7 +11,7 @@ from ..common import compact_size
 
 pytestmark = pytest.mark.models("safe")
 
-ROOT_PUBLIC_KEY = {
+OPTIGA_ROOT_PUBLIC_KEY = {
     models.T2B1: bytes.fromhex(
         "047f77368dea2d4d61e989f474a56723c3212dacf8a808d8795595ef38441427c4389bc454f02089d7f08b873005e4c28d432468997871c0bf286fd3861e21e96a"
     ),
@@ -25,6 +25,46 @@ ROOT_PUBLIC_KEY = {
         "04521192e173a9da4e3023f747d836563725372681eba3079c56ff11b2fc137ab189eb4155f371127651b5594f8c332fc1e9c0f3b80d4212822668b63189706578"
     ),
 }
+
+TROPIC_ROOT_PUBLIC_KEY = bytes.fromhex(
+    "fde32b1037a8d4c7d6db710fe73204d41ec8a733baff65f866f9104ae96355f1"
+)
+MODELS_WITHOUT_TROPIC = ["T2B1", "T3B1", "T3T1"]
+
+def verify_cert_chain(certs, model_name):
+    for cert, ca_cert in zip(certs, certs[1:]):
+        assert cert.issuer == ca_cert.subject
+
+        ca_basic_constraints = ca_cert.extensions.get_extension_for_class(
+            ext.BasicConstraints
+        ).value
+        assert ca_basic_constraints.ca is True
+
+        try:
+            basic_constraints = cert.extensions.get_extension_for_class(
+                ext.BasicConstraints
+            ).value
+            if basic_constraints.ca:
+                assert basic_constraints.path_length < ca_basic_constraints.path_length
+        except ext.ExtensionNotFound:
+            pass
+
+        ca_public_key = ca_cert.public_key()
+        if isinstance(ca_public_key, ed25519.Ed25519PublicKey):
+            ca_public_key.verify(
+                cert.signature,
+                cert.tbs_certificate_bytes,
+            )
+        else:
+            ca_public_key.verify(
+                cert.signature,
+                cert.tbs_certificate_bytes,
+                cert.signature_algorithm_parameters,
+            )
+
+    # Verify that the common name matches the Trezor model.
+    common_name = cert.subject.get_attributes_for_oid(x509.oid.NameOID.COMMON_NAME)[0]
+    assert common_name.value.startswith(model_name)
 
 
 @pytest.mark.parametrize(
@@ -46,11 +86,16 @@ def test_authenticate_device(session: Session, challenge: bytes) -> None:
 
     # Issue an AuthenticateDevice challenge to Trezor.
     proof = device.authenticate(session, challenge)
+
+    data = b"\x13AuthenticateDevice:" + compact_size(len(challenge)) + challenge
+
+    # Optiga
+
     certs = [x509.load_der_x509_certificate(cert) for cert in proof.optiga_certificates]
 
     # Verify the last certificate in the certificate chain against trust anchor.
     root_public_key = ec.EllipticCurvePublicKey.from_encoded_point(
-        ec.SECP256R1(), ROOT_PUBLIC_KEY[session.model]
+        ec.SECP256R1(), OPTIGA_ROOT_PUBLIC_KEY[session.model]
     )
     root_public_key.verify(
         certs[-1].signature,
@@ -58,36 +103,30 @@ def test_authenticate_device(session: Session, challenge: bytes) -> None:
         certs[-1].signature_algorithm_parameters,
     )
 
-    # Verify the certificate chain.
-    for cert, ca_cert in zip(certs, certs[1:]):
-        assert cert.issuer == ca_cert.subject
-
-        ca_basic_constraints = ca_cert.extensions.get_extension_for_class(
-            ext.BasicConstraints
-        ).value
-        assert ca_basic_constraints.ca is True
-
-        try:
-            basic_constraints = cert.extensions.get_extension_for_class(
-                ext.BasicConstraints
-            ).value
-            if basic_constraints.ca:
-                assert basic_constraints.path_length < ca_basic_constraints.path_length
-        except ext.ExtensionNotFound:
-            pass
-
-        ca_cert.public_key().verify(
-            cert.signature,
-            cert.tbs_certificate_bytes,
-            cert.signature_algorithm_parameters,
-        )
-
-    # Verify that the common name matches the Trezor model.
-    common_name = cert.subject.get_attributes_for_oid(x509.oid.NameOID.COMMON_NAME)[0]
-    assert common_name.value.startswith(session.model.internal_name)
+    verify_cert_chain(certs, session.model.internal_name)
 
     # Verify the signature of the challenge.
-    data = b"\x13AuthenticateDevice:" + compact_size(len(challenge)) + challenge
     certs[0].public_key().verify(
         proof.optiga_signature, data, ec.ECDSA(hashes.SHA256())
     )
+
+    # Tropic
+
+    certs = [x509.load_der_x509_certificate(cert) for cert in proof.tropic_certificates]
+
+    if session.model.internal_name in MODELS_WITHOUT_TROPIC:
+        pytest.xfail(f"{session.model.internal_name} does not support Tropic")
+
+    assert len(certs) >= 2  # at least one root and one device cert from Tropic
+
+    # Verify the last certificate in the certificate chain against trust anchor.
+    root_public_key = ed25519.Ed25519PublicKey.from_public_bytes(TROPIC_ROOT_PUBLIC_KEY)
+    root_public_key.verify(
+        certs[-1].signature,
+        certs[-1].tbs_certificate_bytes,
+    )
+
+    verify_cert_chain(certs, session.model.internal_name)
+
+    # Verify the signature of the challenge.
+    certs[0].public_key().verify(proof.tropic_signature, data)
