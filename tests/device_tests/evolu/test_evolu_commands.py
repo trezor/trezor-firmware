a20ee7d4552edddc569b45fbaf788fe07ac8fbb9
Martin Pastyřík <martin.pastyrik@seznam.cz>
2025-10-01 11:15:38 +0200

    fixup! Evolu apps

--- tests/device_tests/evolu/test_evolu_commands.py
+++ tests/device_tests/evolu/test_evolu_commands.py
@@ -8,19 +8,17 @@ pytestmark = pytest.mark.models("safe")
 
 @pytest.mark.protocol("protocol_v1")
 def test_evolu_get_delegated_identity_is_constant(session: "Session"):
-    response = evolu.get_delegated_identity_key(session)
-    private_key = response.private_key
+    private_key = evolu.get_delegated_identity_key(session)
     assert len(private_key) == 32
 
-    response_2 = evolu.get_delegated_identity_key(session)
-    assert response_2.private_key == private_key
+    private_key_2 = evolu.get_delegated_identity_key(session)
+    assert private_key_2 == private_key
 
 
 @pytest.mark.protocol("protocol_v1")
 def test_evolu_get_delegated_identity_test_vector(session: "Session"):
     # on emulator, the additional salt is all zeroes. So the delegated identity key is constant.
-    response = evolu.get_delegated_identity_key(session)
-    private_key = response.private_key
+    private_key = evolu.get_delegated_identity_key(session)
     assert private_key == bytes.fromhex(
         "0e71b5d486a6738f85db46d4dfc8446ccb4d9804aa48e7624620f7cea291f278"
     )
@@ -30,12 +28,12 @@ def test_evolu_get_node(session: "Session"):
     proof = bytes.fromhex(
         "1fc2775112bbaa1018b1211037a8100f8be4b338c8a58e24cb1f2e314f57a5f0443bf6813d9035e0e8390fef66eefb6cc6249439a24cfb5b50a4ff026e7aeebe92"
     )
-    response = evolu.get_evolu_node(session, proof=proof)
+    node = evolu.get_evolu_node(session, proof=proof)
 
     check_value = bytes.fromhex(
         "a81aaf51997b6ddfa33d11c038d6aba5f711754a2c823823ff8b777825cdbb32b0e71c301fa381c75081bd3bcc134b63306aa6fc9a9f52d835ad4df8cd507be6"
     )
-    assert response.data == check_value
+    assert node == check_value
 
 
 def test_evolu_sign_request(session: "Session"):
