0bb75bab4d84de126b434077cde7f37919895966
Roman Zeyde <roman.zeyde@satoshilabs.com>
2025-10-02 10:17:25 +0200

    feat(core+python): support device unlocking during THP handshake
    Add a flag to the first THP handshake message, so the device can:
    
    - fail the handshake if the device is locked (current behavior)
    - prompt the user to enter the PIN (similar to other non-pairing flows)
    
    NOTE: This is a backward-incompatible change, so Suite THP implementation MUST be adapted as well.
    
    Co-authored-by: M1nd3r <petrsedlacek.km@seznam.cz>

--- tests/device_tests/thp/test_handshake.py
+++ tests/device_tests/thp/test_handshake.py
@@ -4,6 +4,7 @@ import pytest
 
 from trezorlib.client import ProtocolV2Channel
 from trezorlib.debuglink import TrezorClientDebugLink as Client
+from trezorlib.exceptions import DeviceLocked
 
 from .connect import prepare_protocol_for_handshake
 
@@ -49,3 +50,97 @@ def test_handshake(client: Client) -> None:
     # so far no luck in solving it - it should be also tackled in FW, as it causes unexpected FW error
     client.protocol = protocol
     client.do_pairing()
+
+
+PIN4 = "1234"
+
+
+@pytest.mark.setup_client(pin=PIN4)
+def test_no_unlock(client: Client):
+    protocol = prepare_protocol_for_handshake(client)
+
+    randomness_static = os.urandom(32)
+
+    protocol._do_channel_allocation()
+    protocol._init_noise(
+        randomness_static=randomness_static,
+    )
+    # the handshake should fail since the device is locked
+    protocol._send_handshake_init_request(try_to_unlock=False)
+    protocol._read_ack()
+    with pytest.raises(DeviceLocked):
+        protocol._read_handshake_init_response()
+
+
+@pytest.mark.setup_client(pin=PIN4)
+def test_unlock_pin(client: Client):
+    protocol = prepare_protocol_for_handshake(client)
+    debug = client.debug
+
+    randomness_static = os.urandom(32)
+
+    protocol._do_channel_allocation()
+    protocol._init_noise(
+        randomness_static=randomness_static,
+    )
+    # the device should show the PIN keyboard
+    protocol._send_handshake_init_request(try_to_unlock=True)
+    protocol._read_ack()
+    debug.synchronize_at("PinKeyboard")
+    debug.input(PIN4)
+
+    protocol._read_handshake_init_response()
+    protocol._send_handshake_completion_request()
+    protocol._read_ack()
+    protocol._read_handshake_completion_response()
+    client.do_pairing()
+    client.get_seedless_session().ping("unlocked")
+
+
+@pytest.mark.setup_client(pin=PIN4)
+def test_unlock_pin_wrong(client: Client):
+    protocol = prepare_protocol_for_handshake(client)
+    debug = client.debug
+
+    randomness_static = os.urandom(32)
+
+    protocol._do_channel_allocation()
+    protocol._init_noise(
+        randomness_static=randomness_static,
+    )
+    # the device should show the PIN keyboard
+    protocol._send_handshake_init_request(try_to_unlock=True)
+    protocol._read_ack()
+    debug.synchronize_at("PinKeyboard")
+    # enter wrong PIN
+    debug.input(PIN4 + "0")
+
+    debug.synchronize_at("PinKeyboard")
+    debug.input(PIN4)
+
+    protocol._read_handshake_init_response()
+    protocol._send_handshake_completion_request()
+    protocol._read_ack()
+    protocol._read_handshake_completion_response()
+    client.do_pairing()
+    client.get_seedless_session().ping("unlocked")
+
+
+@pytest.mark.setup_client(pin=PIN4)
+def test_unlock_cancel(client: Client):
+    protocol = prepare_protocol_for_handshake(client)
+    debug = client.debug
+
+    randomness_static = os.urandom(32)
+
+    protocol._do_channel_allocation()
+    protocol._init_noise(
+        randomness_static=randomness_static,
+    )
+    # cancelling unlock should fail the handshake
+    protocol._send_handshake_init_request(try_to_unlock=True)
+    protocol._read_ack()
+    debug.synchronize_at("PinKeyboard")
+    debug.click(debug.screen_buttons.pin_passphrase_erase())
+    with pytest.raises(DeviceLocked):
+        protocol._read_handshake_init_response()
