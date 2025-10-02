ac20233d172c9df2fd82c86e23f6d072d519fa6b
Lukas Bielesch <bielesch.lukas@gmail.com>
2025-10-02 09:21:39 +0200

    fix(core): incomplete chunks

--- core/embed/rust/src/ui/layout_delizia/flow/util.rs
+++ core/embed/rust/src/ui/layout_delizia/flow/util.rs
@@ -231,13 +231,12 @@ impl ConfirmValue {
     pub fn into_layout(
         self,
     ) -> Result<impl Component<Msg = FlowMsg> + Swipable + MaybeTrace, Error> {
-        let value_len = self.value.as_str_offset(0).len();
         let paragraphs = ConfirmValueParams {
             description: self.description.unwrap_or("".into()),
             extra: self.extra.unwrap_or("".into()),
             value: self.value,
             font: if self.chunkify {
-                theme::get_chunkified_text_style(value_len)
+                &theme::TEXT_MONO_ADDRESS_CHUNKS
             } else if self.text_mono {
                 if self.classic_ellipsis {
                     &theme::TEXT_MONO_WITH_CLASSIC_ELLIPSIS
@@ -285,13 +284,12 @@ impl ConfirmValue {
     }
 
     pub fn into_flow(self) -> Result<SwipeFlow, Error> {
-        let value_len = self.value.as_str_offset(0).len();
         let paragraphs = ConfirmValueParams {
             description: self.description.unwrap_or("".into()),
             extra: self.extra.unwrap_or("".into()),
             value: self.value,
             font: if self.chunkify {
-                theme::get_chunkified_text_style(value_len)
+                &theme::TEXT_MONO_ADDRESS_CHUNKS
             } else if self.text_mono {
                 if self.classic_ellipsis {
                     &theme::TEXT_MONO_WITH_CLASSIC_ELLIPSIS
