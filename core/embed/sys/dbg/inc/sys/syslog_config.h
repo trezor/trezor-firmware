157a0940cd9b76cc9ff01ab0fde34796ae521a7e
tychovrahe <brunam@seznam.cz>
2025-10-30 10:37:29 +0100

    feat(core): implement touch event emulation on driver level
    [no changelog]

--- core/embed/sys/dbg/inc/sys/syslog_config.h
+++ core/embed/sys/dbg/inc/sys/syslog_config.h
@@ -48,6 +48,10 @@
 #define SYSLOG_touch_driver_MAX_LOG_LEVEL SYSLOG_DEFAULT_LOG_LEVEL
 #endif
 
+#ifndef SYSLOG_touch_debug_MAX_LOG_LEVEL
+#define SYSLOG_touch_debug_MAX_LOG_LEVEL SYSLOG_DEFAULT_LOG_LEVEL
+#endif
+
 #ifndef SYSLOG_display_driver_MAX_LOG_LEVEL
 #define SYSLOG_display_driver_MAX_LOG_LEVEL SYSLOG_DEFAULT_LOG_LEVEL
 #endif
