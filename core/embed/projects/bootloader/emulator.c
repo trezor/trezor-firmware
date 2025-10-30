2873511845d9b731255272f2b18691f64d066fb6
tychovrahe <brunam@seznam.cz>
2025-10-30 14:04:45 +0100

    feat(core/bootloader_emu): add option to preload firmware image
    [no changelog]

--- core/embed/projects/bootloader/emulator.c
+++ core/embed/projects/bootloader/emulator.c
@@ -43,6 +43,7 @@ void usage(void) {
   printf("  -e MESSAGE [TITLE [FOOTER]]  display error screen and stop\n");
   printf("  -c COLOR_VARIANT  set color variant\n");
   printf("  -b BITCOIN_ONLY  set bitcoin only flag\n");
+  printf("  -i IMAGE_FILE  path to firmware image file to be used\n");
   printf(
       "  -f FIRMWARE  run interaction-less update for the specified image\n");
 #ifdef LOCKABLE_BOOTLOADER
@@ -88,6 +89,23 @@ bool load_firmware(const char *filename, uint8_t *hash) {
   return true;
 }
 
+bool preload_firmware_image(const char *filename) {
+  static uint8_t fw_buffer[FIRMWARE_MAXSIZE];
+
+  FILE *file = fopen(filename, "rb");
+  if (!file) {
+    printf("Failed to open file '%s'\n", filename);
+    return false;
+  }
+  size_t read = fread(fw_buffer, 1, sizeof(fw_buffer), file);
+  fclose(file);
+
+  flash_area_erase(&FIRMWARE_AREA, NULL);
+
+  return sectrue == flash_area_write_data_padded(&FIRMWARE_AREA, 0, fw_buffer,
+                                                 read, 0x0, FIRMWARE_MAXSIZE);
+}
+
 static int sdl_event_filter(void *userdata, SDL_Event *event) {
   switch (event->type) {
     case SDL_QUIT:
@@ -130,7 +148,7 @@ int main(int argc, char **argv) {
   uint8_t set_variant = 0xff;
   uint8_t color_variant = 0;
   uint8_t bitcoin_only = 0;
-  while ((opt = getopt(argc, argv, "hslec:b:f:")) != -1) {
+  while ((opt = getopt(argc, argv, "hslec:b:f:i:")) != -1) {
     switch (opt) {
       case 's':
         bootargs_set(BOOT_COMMAND_STOP_AND_WAIT, NULL, 0);
@@ -146,6 +164,11 @@ int main(int argc, char **argv) {
         set_variant = 1;
         bitcoin_only = atoi(optarg);
         break;
+      case 'i': {
+        if (!preload_firmware_image(optarg)) {
+          exit(1);
+        }
+      } break;
       case 'f': {
         uint8_t hash[BLAKE2S_DIGEST_LENGTH];
         if (!load_firmware(optarg, hash)) {
