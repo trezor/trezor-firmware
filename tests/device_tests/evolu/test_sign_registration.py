eed32d72438dbd2b0fc8413451068d61fff27215
Martin Pastyřík <martin.pastyrik@seznam.cz>
2025-10-31 11:07:49 +0100

    fixup! fixup! fixup! fixup! feat(core): functions for Evolu spam protection

--- tests/device_tests/evolu/test_sign_registration.py
+++ tests/device_tests/evolu/test_sign_registration.py
@@ -4,51 +4,30 @@ from trezorlib import evolu
 from trezorlib.debuglink import SessionDebugWrapper as Session
 from trezorlib.exceptions import TrezorFailure
 
-pytestmark = pytest.mark.models("safe")
+pytestmark = pytest.mark.models("core")
 
 
-@pytest.mark.protocol("protocol_v1")
-def test_evolu_get_delegated_identity_is_constant(session: Session):
-    private_key = evolu.get_delegated_identity_key(session)
-    assert len(private_key) == 32
-
-    private_key_2 = evolu.get_delegated_identity_key(session)
-    assert private_key_2 == private_key
-
-
-@pytest.mark.protocol("protocol_v1")
-def test_evolu_get_delegated_identity_test_vector(session: Session):
-    # on emulator, the master key is all zeroes. So the delegated identity key is constant.
-    private_key = evolu.get_delegated_identity_key(session)
-    assert private_key == bytes.fromhex(
-        "10e39ed3a40dd63a47a14608d4bccd4501170cf9f2188223208084d39c37b369"
-    )
-
-
-def test_evolu_get_node(session: Session):
-    proof = bytes.fromhex(
-        "1fb521e8a4e4580377d530a9d6eb0a394ec8340fa42094d9f2e822bb944ce6a2074b81241b3b65dfa15d66e052f2504aba3ad1644844d695b181b3cdc9666cb66b"
-    )
-    node = evolu.get_node(session, proof=proof)
-
-    check_value = bytes.fromhex(
-        "a81aaf51997b6ddfa33d11c038d6aba5f711754a2c823823ff8b777825cdbb32b0e71c301fa381c75081bd3bcc134b63306aa6fc9a9f52d835ad4df8cd507be6"
-    )
-    assert node == check_value
-
-
-def test_evolu_get_node_invalid_proof(session: Session):
-    proof = bytes.fromhex(
-        "1f354fbb47b4679c1cb0c2c6b96a27f9a147c61ec5ef6f6c42491c839f4b7a95792d099be0f138274e5ef7896058b4de4f383f497792bb157b925e2644a79a0000"  # altered last 2 bytes
+@pytest.mark.models("t2t1")
+def test_evolu_sign_request_t2t1(session: Session):
+    challenge = "1234"
+    size = 10
+    proposed_value = bytes.fromhex(
+        "1b161be2bfc622b4ffd9943138ab5931e77b4c6835e29b1ac25221c74492495a912c00f488fd5f95b43085f721f36574813785c011c60cf81877ccd057df6bed0c"
     )
 
     with pytest.raises(
         TrezorFailure,
-        match="Invalid proof",
+        match="Optiga is not available",
     ):
-        evolu.get_node(session, proof=proof)
+        evolu.sign_registration_request(
+            session,
+            challenge=bytes.fromhex(challenge),
+            size=size,
+            proof=proposed_value,
+        )
 
 
+@pytest.mark.models("safe")
 def test_evolu_sign_request(session: Session):
     challenge = "1234"
     size = 10
@@ -69,6 +48,7 @@ def test_evolu_sign_request(session: Session):
     assert response.signature == check_signature
 
 
+@pytest.mark.models("safe")
 def test_evolu_sign_request_invalid_proof(session: Session):
     challenge = "1234"
     size = 10
@@ -88,6 +68,7 @@ def test_evolu_sign_request_invalid_proof(session: Session):
         )
 
 
+@pytest.mark.models("safe")
 def test_evolu_sign_request_challenge_too_long(session: Session):
     challenge = "01" * 300  # 300 bytes, max is 255
     size = 10
@@ -107,6 +88,7 @@ def test_evolu_sign_request_challenge_too_long(session: Session):
         )
 
 
+@pytest.mark.models("safe")
 def test_evolu_sign_request_challenge_too_short(session: Session):
     challenge = ""  # 0 bytes, min is 1
     size = 10
@@ -126,6 +108,7 @@ def test_evolu_sign_request_challenge_too_short(session: Session):
         )
 
 
+@pytest.mark.models("safe")
 def test_evolu_sign_request_size_too_small(session: Session):
     challenge = "1234"
     size = -10
@@ -145,6 +128,7 @@ def test_evolu_sign_request_size_too_small(session: Session):
         )
 
 
+@pytest.mark.models("safe")
 def test_evolu_sign_request_size_too_large(session: Session):
     challenge = "1234"
     size = 0xFFFFFFFF + 1
@@ -164,6 +148,7 @@ def test_evolu_sign_request_size_too_large(session: Session):
         )
 
 
+@pytest.mark.models("safe")
 def test_evolu_sign_request_data_higher_bound(session: Session):
     challenge = "12" * 255
     size = 0xFFFFFFFF
