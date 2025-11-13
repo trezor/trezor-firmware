8975962f2718e9f47beaddfe7e447af3b3905eef
cepetr <petr@cerninovi.cz>
2025-11-13 09:02:34 +0100

    refactor(core): allow running coreapp emulator as an applet
    [no changelog]

---
+++ core/embed/sys/task/unix/coreapp.c
@@ -0,0 +1,48 @@
+/*
+ * This file is part of the Trezor project, https://trezor.io/
+ *
+ * Copyright (c) SatoshiLabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef KERNEL
+
+#include <trezor_rtl.h>
+
+#include <sys/applet.h>
+#include <sys/coreapp.h>
+#include <sys/systask.h>
+
+extern int coreapp_emu(int argc, char** argv);
+
+bool coreapp_init(applet_t* applet, int argc, char** argv) {
+  const applet_layout_t coreapp_layout = {0};
+  const applet_privileges_t coreapp_privileges = {0};
+
+  applet_init(applet, &coreapp_layout, &coreapp_privileges);
+
+  if (!systask_init(&applet->task, 0, 0, 0, applet)) {
+    return false;
+  }
+
+  if (!systask_push_call(&applet->task, (void*)coreapp_emu, (uintptr_t)argc,
+                         (uintptr_t)argv, 0)) {
+    return false;
+  }
+
+  return true;
+}
+
+#endif  // KERNEL
