eaa1d9da3bfcf0b77bf9a31494ee353631bbf23b
cepetr <petr@cerninovi.cz>
2025-12-09 08:22:17 +0100

    TEMP - slh-dsa perf test

---
+++ core/embed/projects/prodtest/cmd/prodtest_slhdsa.c
@@ -0,0 +1,86 @@
+/*
+ * This file is part of the Trezor project, https://trezor.io/
+ *
+ * Copyright (c) SatoshiLabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <trezor_rtl.h>
+
+#include <rtl/cli.h>
+#include <sec/rng.h>
+#include <sys/systick.h>
+
+#include <../vendor/sphincsplus/ref/api.h>
+
+static void prodtest_test_slhdsa(cli_t* cli) {
+  if (cli_arg_count(cli) > 0) {
+    cli_error_arg_count(cli);
+    return;
+  }
+
+  cli_trace(cli, "crypto_sign_secretkeybytes() -> %d",
+            crypto_sign_secretkeybytes());
+  cli_trace(cli, "crypto_sign_publickeybytes() -> %d",
+            crypto_sign_publickeybytes());
+  cli_trace(cli, "crypto_sign_bytes() -> %d", crypto_sign_bytes());
+  cli_trace(cli, "crypto_sign_seedbytes() -> %d", crypto_sign_seedbytes());
+
+  unsigned char pub_key[CRYPTO_PUBLICKEYBYTES];
+  unsigned char sec_key[CRYPTO_SECRETKEYBYTES];
+
+  unsigned char msg[] = "Test message for SLHDSA signature";
+
+  unsigned char sig[CRYPTO_BYTES];
+  unsigned int sig_len = 0;
+
+  crypto_sign_keypair(pub_key, sec_key);
+
+  ticks_t start;
+  uint32_t duration;
+
+  cli_trace(cli, "Signing message using SLHDSA...");
+
+  start = systick_ms();
+  crypto_sign_signature(sig, &sig_len, msg, sizeof(msg), sec_key);
+  duration = systick_ms() - start;
+
+  cli_trace(cli, "Signed in %u.%u s", duration / 1000, duration % 1000);
+
+  cli_trace(cli, "Verifying signature using SLHDSA...");
+
+  start = systick_ms();
+  int rc = crypto_sign_verify(sig, sig_len, msg, sizeof(msg), pub_key);
+  duration = systick_ms() - start;
+
+  cli_trace(cli, "Verified in %u.%u s", duration / 1000, duration % 1000);
+
+  cli_trace(cli, "Signature verification result: %s",
+            (rc == 0) ? "OK" : "FAIL");
+
+  cli_ok(cli, "");
+}
+
+void randombytes(unsigned char* x, unsigned long long xlen) {
+  rng_fill_buffer(x, xlen);
+}
+
+// clang-format off
+PRODTEST_CLI_CMD(
+  .name = "test-slhdsa",
+  .func = prodtest_test_slhdsa,
+  .info = "Perform SLHDSA self-test",
+  .args = ""
+);
