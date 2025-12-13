bc9bf54be5b4dfa1479e340e039bb3d994cd475c
obrusvit <vit.obrusnik@gmail.com>
2025-12-13 22:53:24 +0100

    WIP: feat(core/eckhart): adjust Homescreen notifs

--- core/embed/rust/src/ui/layout_eckhart/firmware/homescreen.rs
+++ core/embed/rust/src/ui/layout_eckhart/firmware/homescreen.rs
@@ -2,12 +2,15 @@ use crate::{
     error::Error,
     io::BinaryData,
     strutil::TString,
+    time::{Duration, Stopwatch},
     translations::TR,
+    trezorhal::ble,
     ui::{
-        component::{text::TextStyle, Component, Event, EventCtx, Label, Never, Swipe},
+        component::{text::TextStyle, Component, Event, EventCtx, Label, Never, Swipe, Timer},
         display::{image::ImageInfo, Color},
         geometry::{Alignment, Direction, Offset, Rect},
         layout::util::get_user_custom_image,
+        lerp::Lerp,
         shape::{self, Renderer},
         util::animation_disabled,
     },
@@ -26,18 +29,20 @@ use super::{
 #[cfg(feature = "rgb_led")]
 use crate::ui::led::LedState;
 
+type Notif = (TString<'static>, u8);
+type NotifQueue = heapless::Vec<Notif, 4>;
+
 /// Full-screen component for the homescreen and lockscreen.
 pub struct Homescreen {
     /// Device name with shadow
     label: HomeLabel,
-    /// Notification
-    hint: Option<Hint<'static>>,
+    /// Notification and background component
+    notif_and_bg: NotifAndBgUI,
+    notif_queue: NotifQueue,
     /// Home action bar
     action_bar: ActionBar,
     /// Background image
     image: Option<BinaryData<'static>>,
-    /// LED color
-    led_color: Option<Color>,
     /// Whether the homescreen is locked
     locked: bool,
     /// Whether the homescreen is a boot screen
@@ -61,41 +66,30 @@ impl Homescreen {
         locked: bool,
         bootscreen: bool,
         coinjoin_authorized: bool,
-        notification: Option<(TString<'static>, u8)>,
+        notification: Option<Notif>,
     ) -> Result<Self, Error> {
-        let image = get_homescreen_image();
-        let shadow = image.is_some();
-
-        // Notification
-        let mut notification_level = 4;
-        let (led_color, hint) = match notification {
-            Some((text, level)) => {
-                notification_level = level;
-                let (led_color, hint) = Self::get_notification_display(level, text);
-                (Some(led_color), Some(hint))
-            }
-            None if locked && coinjoin_authorized => (
-                Some(theme::LED_GREEN_LIME),
-                Some(Hint::new_instruction_green(
-                    TR::coinjoin__do_not_disconnect,
-                    Some(theme::ICON_INFO),
-                )),
-            ),
-            None => (None, None),
-        };
-
         // Homebar
-        let (style_sheet, gradient) = button_homebar_style(notification_level);
+        let notif_level = notification.as_ref().map(|(_, l)| *l).unwrap_or(4);
+        let (style_sheet, gradient) = button_homebar_style(notif_level);
         let btn = Button::new(Self::homebar_content(bootscreen, locked))
             .styled(style_sheet)
             .with_gradient(gradient);
 
+        // Notification and Background
+        let image = get_homescreen_image();
+        let user_set_image = image.is_some();
+        let mut notif_queue = NotifQueue::new();
+        if let Some(notif) = notification {
+            let _ = notif_queue.push(notif);
+        }
+        let notif_and_bg = NotifAndBgUI::new(user_set_image);
+
         Ok(Self {
-            label: HomeLabel::new(label, shadow),
-            hint,
+            label: HomeLabel::new(label, user_set_image),
+            notif_and_bg,
+            notif_queue,
             action_bar: ActionBar::new_single(btn),
             image,
-            led_color,
             locked,
             bootscreen,
             fuel_gauge: FuelGauge::homescreen_bar()
@@ -108,22 +102,6 @@ impl Homescreen {
     fn homebar_content(bootscreen: bool, locked: bool) -> ButtonContent {
         let text = (bootscreen || locked).then_some(TR::lockscreen__unlock.into());
         ButtonContent::HomeBar(text)
-    }
-
-    fn get_notification_display(level: u8, text: TString<'static>) -> (Color, Hint<'static>) {
-        match level {
-            0 => (theme::LED_RED, Hint::new_warning_danger(text)),
-            1 => (theme::LED_YELLOW, Hint::new_warning_neutral(text)),
-            2 => (theme::LED_BLUE, Hint::new_instruction(text, None)),
-            3 => (
-                theme::LED_GREEN_LIGHT,
-                Hint::new_instruction_green(text, Some(theme::ICON_INFO)),
-            ),
-            _ => (
-                theme::LED_WHITE,
-                Hint::new_instruction(text, Some(theme::ICON_INFO)),
-            ),
-        }
     }
 
     fn event_fuel_gauge(&mut self, ctx: &mut EventCtx, event: Event) {
@@ -142,6 +120,22 @@ impl Homescreen {
             b.set_content(bar_content)
         }
     }
+
+    fn event_notification(&mut self, ctx: &mut EventCtx, event: Event) {
+        if matches!(event, Event::Attach(_)) {
+            if ble::is_connected() {
+                // TODO: level?
+                let _ = self.notif_queue.push((TR::words__connected.into(), 2));
+            }
+            self.notif_and_bg
+                .update_notification(self.notif_queue.pop());
+        }
+
+        if let Some(NotifAndBgUIMsg::Done) = self.notif_and_bg.event(ctx, event) {
+            self.notif_and_bg
+                .update_notification(self.notif_queue.pop());
+        }
+    }
 }
 
 impl Component for Homescreen {
@@ -153,13 +147,7 @@ impl Component for Homescreen {
         debug_assert_eq!(bounds.width(), SCREEN.width());
 
         let (rest, bar_area) = bounds.split_bottom(theme::ACTION_BAR_HEIGHT);
-        let rest = if let Some(hint) = &mut self.hint {
-            let (rest, hint_area) = rest.split_bottom(hint.height());
-            hint.place(hint_area);
-            rest
-        } else {
-            rest
-        };
+        let rest = self.notif_and_bg.place(rest);
         let label_area = rest.inset(theme::CONTENT_INSETS_NO_HEADER);
 
         self.label.place(label_area);
@@ -171,6 +159,7 @@ impl Component for Homescreen {
     }
 
     fn event(&mut self, ctx: &mut EventCtx, event: Event) -> Option<Self::Msg> {
+        self.event_notification(ctx, event);
         self.event_fuel_gauge(ctx, event);
 
         let swipe_up = matches!(self.swipe.event(ctx, event), Some(Direction::Up));
@@ -195,20 +184,13 @@ impl Component for Homescreen {
             if let ImageInfo::Jpeg(_) = ImageInfo::parse(image) {
                 shape::JpegImage::new_image(SCREEN.top_left(), image).render(target);
             }
-        } else {
-            ScreenBackground::new(self.led_color, None).render(target);
         }
+        self.notif_and_bg.render(target);
         self.label.render(target);
-        self.hint.render(target);
         self.action_bar.render(target);
         if self.fuel_gauge.should_be_shown() {
             self.fuel_gauge.render(target);
         }
-
-        #[cfg(feature = "rgb_led")]
-        target.set_led_state(LedState::Static(
-            self.led_color.unwrap_or_else(Color::black),
-        ));
     }
 }
 
@@ -259,6 +241,137 @@ impl Component for HomeLabel {
     fn render<'s>(&'s self, target: &mut impl Renderer<'s>) {
         self.label_shadow.render(target);
         self.label.render(target);
+    }
+}
+
+/// Component to manage Homescreen notification and background rendering.
+/// It handles notification appearance timing, LED color management, and
+/// background rendering. These functionalities are encapsulated in a single
+/// component to ensure synchronized behavior.
+struct NotifAndBgUI {
+    pub background: ScreenBackground,
+    pub user_set_image: bool,
+    pub state: NotifStatUI,
+    pub hint: Option<Hint<'static>>,
+    pub led_color: Option<Color>,
+    pub timer: Timer,
+}
+
+enum NotifStatUI {
+    Empty,
+    /// Initial state, waiting before showing notification
+    Initial,
+    /// Notification appeared
+    Appeared,
+}
+
+impl NotifAndBgUI {
+    fn new(user_set_image: bool) -> Self {
+        Self {
+            background: ScreenBackground::new(Some(4)),
+            user_set_image,
+            state: NotifStatUI::Initial,
+            hint: None,
+            led_color: None,
+            timer: Timer::new(),
+        }
+    }
+
+    fn update_notification(&mut self, notif: Option<Notif>) {
+        if let Some((text, level)) = notif {
+            let led_color = Self::get_led_color(level);
+            let hint = Self::get_hint(text, level);
+            self.led_color = Some(led_color);
+            self.hint = Some(hint);
+            self.state = NotifStatUI::Initial;
+        } else {
+            self.led_color = None;
+            self.hint = None;
+            self.state = NotifStatUI::Empty;
+        }
+    }
+
+    fn get_led_color(level: u8) -> Color {
+        match level {
+            0 => theme::LED_RED,
+            1 => theme::LED_YELLOW,
+            2 => theme::LED_BLUE,
+            3 => theme::LED_GREEN_LIGHT,
+            _ => theme::LED_WHITE,
+        }
+    }
+
+    fn get_hint(text: TString<'static>, level: u8) -> Hint<'static> {
+        match level {
+            0 => Hint::new_warning_danger(text),
+            1 => Hint::new_warning_neutral(text),
+            2 => Hint::new_instruction(text, None),
+            3 => Hint::new_instruction_green(text, Some(theme::ICON_INFO)),
+            _ => Hint::new_instruction(text, Some(theme::ICON_INFO)),
+        }
+    }
+
+    fn eval_led_sim_alpha(&self) -> u8 {
+        u8::MAX
+    }
+}
+
+enum NotifAndBgUIMsg {
+    Done,
+}
+
+impl Component for NotifAndBgUI {
+    type Msg = NotifAndBgUIMsg;
+
+    fn place(&mut self, bounds: Rect) -> Rect {
+        let rest = if let Some(hint) = &mut self.hint {
+            let (rest, hint_area) = bounds.split_bottom(hint.height());
+            self.hint.place(hint_area);
+            rest
+        } else {
+            bounds
+        };
+        rest
+    }
+
+    fn event(&mut self, ctx: &mut EventCtx, event: Event) -> Option<Self::Msg> {
+        match &mut self.state {
+            NotifStatUI::Empty => {}
+            NotifStatUI::Initial => {
+                if !self.timer.is_running() {
+                    self.timer.start(ctx, Duration::from_millis(500));
+                }
+                if self.timer.expire(event) {
+                    self.state = NotifStatUI::Appeared;
+                }
+            }
+            NotifStatUI::Appeared => {
+                if !self.timer.is_running() {
+                    self.timer.start(ctx, Duration::from_millis(3000));
+                }
+                if self.timer.expire(event) {
+                    return Some(NotifAndBgUIMsg::Done);
+                }
+            }
+        };
+        None
+    }
+
+    fn render<'s>(&'s self, target: &mut impl Renderer<'s>) {
+        if let Some(led_color) = self.led_color {
+            let led_alpha = self.eval_led_sim_alpha();
+            self.background.render(target, Some((led_color, led_alpha)));
+
+            #[cfg(feature = "rgb_led")]
+            if matches!(self.state, NotifStatUI::Appeared) {
+                target.set_led_state(LedState::Static(led_color));
+            } else {
+                target.set_led_state(LedState::Static(Color::black()));
+            }
+        } else {
+            self.background.render(target, None);
+        }
+        self.hint.render(target);
     }
 }
 
