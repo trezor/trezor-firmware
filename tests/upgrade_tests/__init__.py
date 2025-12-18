c8ec6c2fea78b1587503c02b309cc53fa746ed8b
M1nd3r <petrsedlacek.km@seznam.cz>
2025-12-18 23:01:14 +0100

    wip

--- tests/upgrade_tests/__init__.py
+++ tests/upgrade_tests/__init__.py
@@ -20,7 +20,7 @@ from typing import List, Tuple
 import pytest
 from _pytest.mark.structures import MarkDecorator
 
-from ..emulators import ALL_TAGS, CORE, LEGACY, LOCAL_BUILD_PATHS, gen_from_model
+from ..emulators import ALL_TAGS, CORE, LEGACY, LOCAL_BUILD_PATHS, T3W1, gen_from_model
 
 SELECTED_GENS = [
     gen.strip() for gen in os.environ.get("TREZOR_UPGRADE_TEST", "").split(",") if gen
@@ -61,6 +61,7 @@ def for_all(
     *args: str,
     legacy_minimum_version: Tuple[int, int, int] = (1, 0, 0),
     core_minimum_version: Tuple[int, int, int] = (2, 0, 0),
+    t3w1_minimum_version: Tuple[int, int, int] = (2, 9, 3),
 ) -> "MarkDecorator":
     """Parametrizing decorator for test cases.
 
@@ -71,7 +72,8 @@ def for_all(
     >>>     assert True
 
     Arguments can be trezor models (e.g."T1B1" and "T2T1") or aliases "core" and "legacy",
-    and you can specify core_minimum_version and legacy_minimum_version as triplets.
+    and you can specify core_minimum_version, legacy_minimum_version and t3w1_minimum_version
+    as triplets.
 
     The test function should have arguments `gen` ("core" or "legacy") and `tag`
     (version tag usable in EmulatorWrapper call)
@@ -95,7 +97,7 @@ def for_all(
 
     if not args:
         gens = ["core", "legacy"]
-        models = ["T1B1", "T2T1"]
+        models = ["T1B1", "T2T1", "T3W1"]
 
     # If any gens were selected, use them. If none, select all.
     enabled_gens = SELECTED_GENS or list(gens)
@@ -104,6 +106,8 @@ def for_all(
     for model in models:
         if model in LEGACY:
             minimum_version = legacy_minimum_version
+        elif model == T3W1:
+            minimum_version = t3w1_minimum_version
         elif model in CORE:
             minimum_version = core_minimum_version
         else:
