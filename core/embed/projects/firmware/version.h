25b05677cad6b1c3e17dc32576be8c14a064ad17
obrusvit <vit.obrusnik@gmail.com>
2026-01-05 16:11:50 +0100

    chore(core): bump VERSION & FIX_VERSION to 2.10.0
    - security fix forces us to bump FIX_VERSION
    - semantically, it means bumping VERSION_MINOR
    - also modify CHANGELOGs from 2.9.7 to 2.10.0
    
    [no changelog]

--- core/embed/projects/firmware/version.h
+++ core/embed/projects/firmware/version.h
@@ -3,11 +3,11 @@
 #include "model_version.h"
 
 #define VERSION_MAJOR 2
-#define VERSION_MINOR 9
-#define VERSION_PATCH 7
+#define VERSION_MINOR 10
+#define VERSION_PATCH 0
 #define VERSION_BUILD 0
 
 #define FIX_VERSION_MAJOR 2
-#define FIX_VERSION_MINOR 9
+#define FIX_VERSION_MINOR 10
 #define FIX_VERSION_PATCH 0
 #define FIX_VERSION_BUILD 0
