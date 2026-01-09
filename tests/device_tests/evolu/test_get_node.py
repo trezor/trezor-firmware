d1d28b86b76402753c53e2cae7209ea9a3344847
Martin Pastyřík <martin.pastyrik@seznam.cz>
2026-01-09 10:10:48 +0100

    fix(tests): fix Evolu tests on HW devices
    Evolu tests now do not depend on hardocded values but compute the expected value from the `delegated_idenetity_key` of the tested device. Therefore enabling testing on HW devices where the master key is not initialized to all zeroes.
    
    A check function was added to the `sign_registration` tests to determine if the device has Optiga available. If not, the test will mark itself as `xfail` - we expect Optiga signing to fail if there is no Optiga.
    
    All the Evolu test have been simplified so that the logic is the same for THP and non-THP devices.
    
    Lastly, the verification of certificate chain and Optiga and Tropic signatures and certificates has been refactored from `test_authenticate_device.py` as it is now used in other tests.
    
     [no changelog]

--- tests/device_tests/evolu/test_get_node.py
+++ tests/device_tests/evolu/test_get_node.py
@@ -1,47 +1,70 @@
 import pytest
 
 from trezorlib import evolu
-from trezorlib.debuglink import SessionDebugWrapper as Session
+from trezorlib.debuglink import TrezorClientDebugLink as Client
 from trezorlib.exceptions import TrezorFailure
 
-pytestmark = pytest.mark.models("core")
+from .common import get_invalid_proof, get_proof
+
+pytestmark = [
+    pytest.mark.models("core"),
+    # the tests vectors in this test are for the SLIP-14 seed. It should be initialized from `conftest.py` already but we set it explicitly to be sure
+    pytest.mark.setup_client(
+        mnemonic="all all all all all all all all all all all all", passphrase=False
+    ),
+]
 
 
-def test_evolu_get_node(session: Session):
-    proof = bytes.fromhex(
-        "1fb521e8a4e4580377d530a9d6eb0a394ec8340fa42094d9f2e822bb944ce6a2074b81241b3b65dfa15d66e052f2504aba3ad1644844d695b181b3cdc9666cb66b"
-    )
-    node = evolu.get_node(session, proof=proof)
+def test_evolu_get_node(client: Client):
+    proof = get_proof(client, b"EvoluGetNode", [])
+    node = evolu.get_node(client.get_session(), proof=proof)
 
+    # expected node for the SLIP-14 seed
     check_value = bytes.fromhex(
         "a81aaf51997b6ddfa33d11c038d6aba5f711754a2c823823ff8b777825cdbb32b0e71c301fa381c75081bd3bcc134b63306aa6fc9a9f52d835ad4df8cd507be6"
     )
     assert node == check_value
 
 
-def test_evolu_get_node_invalid_proof(session: Session):
-    proof = bytes.fromhex(
-        "1f354fbb47b4679c1cb0c2c6b96a27f9a147c61ec5ef6f6c42491c839f4b7a95792d099be0f138274e5ef7896058b4de4f383f497792bb157b925e2644a79a0000"  # altered last 2 bytes
+@pytest.mark.setup_client(
+    # a different seed
+    mnemonic="valve multiply shuffle venue then cruel genre venture fruit hammer sponsor luxury",
+    passphrase=False,
+)
+def test_evolu_get_node_different_seed(client: Client):
+    proof = get_proof(client, b"EvoluGetNode", [])
+    node = evolu.get_node(client.get_session(), proof=proof)
+
+    # expected node for the SLIP-14 seed
+    check_value = bytes.fromhex(
+        "a81aaf51997b6ddfa33d11c038d6aba5f711754a2c823823ff8b777825cdbb32b0e71c301fa381c75081bd3bcc134b63306aa6fc9a9f52d835ad4df8cd507be6"
     )
 
+    # check that the generated node is different
+    assert node != check_value
+
+
+def test_evolu_get_node_invalid_proof(client: Client):
+    invalid_proof = get_invalid_proof(client, b"EvoluGetNode", [])
+
     with pytest.raises(
         TrezorFailure,
         match="Invalid proof",
     ):
-        evolu.get_node(session, proof=proof)
+        evolu.get_node(client.get_session(), proof=invalid_proof)
 
 
-def test_evolu_get_node_no_proof(session: Session):
+def test_evolu_get_node_no_proof(client: Client):
     with pytest.raises(
         TrezorFailure,
         match="Invalid proof",
     ):
-        evolu.get_node(session, proof=b"")
+        evolu.get_node(client.get_session(), proof=b"")
 
 
-def test_evolu_get_node_none_proof(session: Session):
+def test_evolu_get_node_none_proof(client: Client):
     with pytest.raises(
         TrezorFailure,
         match="DataError: Failed to decode message: Missing required field. proof_of_delegated_identity",
     ):
-        evolu.get_node(session, proof=None)  # type: ignore
+        evolu.get_node(client.get_session(), proof=None)  # type: ignore
--- tests/device_tests/evolu/test_sign_registration.py
+++ tests/device_tests/evolu/test_sign_registration.py
@@ -1,18 +1,39 @@
 import pytest
+from ecdsa import NIST256p, SigningKey, VerifyingKey
 
 from trezorlib import evolu
-from trezorlib.debuglink import SessionDebugWrapper as Session
+from trezorlib.debuglink import TrezorClientDebugLink as Client
 from trezorlib.exceptions import TrezorFailure
+
+from ...common import compact_size
+from ..certificate import check_signature_optiga
+from .common import get_delegated_identity_key, get_invalid_proof, get_proof
 
 pytestmark = pytest.mark.models("core")
 
 
+def signing_buffer(private_key: bytes, challenge: bytes, size: int) -> bytes:
+    public_key: VerifyingKey = SigningKey.from_string(private_key, curve=NIST256p).get_verifying_key()  # type: ignore
+    components = [
+        b"EvoluSignRegistrationRequestV1:",
+        public_key.to_string("uncompressed"),
+        challenge,
+        size.to_bytes(4, "big"),
+    ]
+    return b"".join((compact_size(len(comp)) + comp) for comp in components)
+
+
+def optiga_unavailable(client: Client) -> bool:
+    """Check if Optiga is unavailable from the presence of its security counter."""
+    return client.features.optiga_sec is None
+
+
 @pytest.mark.models("t2t1")
-def test_evolu_sign_request_t2t1(session: Session):
-    challenge = "1234"
+def test_evolu_sign_request_t2t1(client: Client):
+    challenge = bytes.fromhex("1234")
     size = 10
-    proposed_value = bytes.fromhex(
-        "1b161be2bfc622b4ffd9943138ab5931e77b4c6835e29b1ac25221c74492495a912c00f488fd5f95b43085f721f36574813785c011c60cf81877ccd057df6bed0c"
+    proof = get_proof(
+        client, b"EvoluSignRegistrationRequest", [challenge, size.to_bytes(4, "big")]
     )
 
     with pytest.raises(
@@ -20,40 +41,47 @@ def test_evolu_sign_request_t2t1(session: Session):
         match="Optiga is not available",
     ):
         evolu.sign_registration_request(
-            session,
-            challenge=bytes.fromhex(challenge),
+            client.get_session(),
+            challenge=challenge,
             size=size,
-            proof=proposed_value,
+            proof=proof,
         )
 
 
 @pytest.mark.models("safe")
-def test_evolu_sign_request(session: Session):
-    challenge = "1234"
+def test_evolu_sign_request(client: Client):
+    if optiga_unavailable(client):
+        pytest.xfail("Optiga is not available on this device.")
+    delegated_identity_key = get_delegated_identity_key(client)
+    challenge = bytes.fromhex("1234")
     size = 10
-    proposed_value = bytes.fromhex(
-        "1fb4ca7b8d956cc50ac652e383691af8e59b200adedde3a898b86795fd94d49241559a1699de1110617a91c44c70c4b9509fdb36f5057a52c0ef28fce7afa10734"
+    proposed_value = get_proof(
+        client,
+        b"EvoluSignRegistrationRequest",
+        [challenge, size.to_bytes(4, "big")],
     )
+
     response = evolu.sign_registration_request(
-        session,
-        challenge=bytes.fromhex(challenge),
+        client.get_session(),
+        challenge=challenge,
         size=size,
         proof=proposed_value,
     )
 
-    check_signature = bytes.fromhex(
-        "30440220148c0a0026828532e5a2e7ce5cf2dcd2491e7eea5f5c6eafd49779d1502c5ba102204b1ca171045969e38ac815de09462d6c5b496d04851266fe71abcf55b9aee672"
+    data = signing_buffer(delegated_identity_key, challenge, size)
+    check_signature_optiga(
+        response.signature, response.certificate_chain, client.model, data
     )
-
-    assert response.signature == check_signature
 
 
 @pytest.mark.models("safe")
-def test_evolu_sign_request_invalid_proof(session: Session):
-    challenge = "1234"
+def test_evolu_sign_request_invalid_proof(client: Client):
+    if optiga_unavailable(client):
+        pytest.xfail("Optiga is not available on this device.")
+    challenge = bytes.fromhex("1234")
     size = 10
-    proposed_value = bytes.fromhex(
-        "20dc125b51c2f596df4a9ae9ef816353dcdbf068b91ac687962742b8bd434276f60258c337e0d03211e599701a87cae8d8ac3258ce01bd484921743c2a5e990000"  # altered last 2 bytes
+    invalid_proof = get_invalid_proof(
+        client, b"EvoluSignRegistrationRequest", [challenge, size.to_bytes(4, "big")]
     )
 
     with pytest.raises(
@@ -61,19 +89,21 @@ def test_evolu_sign_request_invalid_proof(session: Session):
         match="Invalid proof",
     ):
         evolu.sign_registration_request(
-            session,
-            challenge=bytes.fromhex(challenge),
+            client.get_session(),
+            challenge=challenge,
             size=size,
-            proof=proposed_value,
+            proof=invalid_proof,
         )
 
 
 @pytest.mark.models("safe")
-def test_evolu_sign_request_challenge_too_long(session: Session):
-    challenge = "01" * 300  # 300 bytes, max is 255
+def test_evolu_sign_request_challenge_too_long(client: Client):
+    if optiga_unavailable(client):
+        pytest.xfail("Optiga is not available on this device.")
+    challenge = b"\x01" * 300  # 300 bytes, max is 255
     size = 10
-    proposed_value = bytes.fromhex(
-        "1fd0b4cd0a04806eaa74ae59cc2f5a740680fc784b877deff6ffa6b9eda7d5a7d4207958c48e679b18c64d0e7fcd0e5be25eb27bcf186fbf9531eb20bce7234a23"
+    proof = get_proof(
+        client, b"EvoluSignRegistrationRequest", [challenge, size.to_bytes(4, "big")]
     )
 
     with pytest.raises(
@@ -81,19 +111,21 @@ def test_evolu_sign_request_challenge_too_long(session: Session):
         match="Invalid challenge length",
     ):
         evolu.sign_registration_request(
-            session,
-            challenge=bytes.fromhex(challenge),
+            client.get_session(),
+            challenge=challenge,
             size=size,
-            proof=proposed_value,
+            proof=proof,
         )
 
 
 @pytest.mark.models("safe")
-def test_evolu_sign_request_challenge_too_short(session: Session):
-    challenge = ""  # 0 bytes, min is 1
+def test_evolu_sign_request_challenge_too_short(client: Client):
+    if optiga_unavailable(client):
+        pytest.xfail("Optiga is not available on this device.")
+    challenge = b""  # 0 bytes, minimum is 1
     size = 10
-    proposed_value = bytes.fromhex(
-        "1fa386d20efb38dbb3f7ae0509651fa36c8128324ef89fa1cfd104e10dced08c594f0e8f0a525a839b4fbfaa92b8c2b51163cef593f5c14fc9f1c8c48d1192270d"
+    proof = get_proof(
+        client, b"EvoluSignRegistrationRequest", [challenge, size.to_bytes(4, "big")]
     )
 
     with pytest.raises(
@@ -101,19 +133,23 @@ def test_evolu_sign_request_challenge_too_short(session: Session):
         match="Invalid challenge length",
     ):
         evolu.sign_registration_request(
-            session,
-            challenge=bytes.fromhex(challenge),
+            client.get_session(),
+            challenge=challenge,
             size=size,
-            proof=proposed_value,
+            proof=proof,
         )
 
 
 @pytest.mark.models("safe")
-def test_evolu_sign_request_size_too_small(session: Session):
-    challenge = "1234"
+def test_evolu_sign_request_size_too_small(client: Client):
+    if optiga_unavailable(client):
+        pytest.xfail("Optiga is not available on this device.")
+    challenge = bytes.fromhex("1234")
     size = -10
-    proposed_value = bytes.fromhex(
-        "1fa386d20efb38dbb3f7ae0509651fa36c8128324ef89fa1cfd104e10dced08c594f0e8f0a525a839b4fbfaa92b8c2b51163cef593f5c14fc9f1c8c48d1192270d"
+    proof = get_proof(
+        client,
+        b"EvoluSignRegistrationRequest",
+        [challenge, size.to_bytes(4, "big", signed=True)],
     )
 
     with pytest.raises(
@@ -121,19 +157,21 @@ def test_evolu_sign_request_size_too_small(session: Session):
         match=f"Value {size} in field size_to_acquire does not fit into uint32",
     ):
         evolu.sign_registration_request(
-            session,
-            challenge=bytes.fromhex(challenge),
+            client.get_session(),
+            challenge=challenge,
             size=size,
-            proof=proposed_value,
+            proof=proof,
         )
 
 
 @pytest.mark.models("safe")
-def test_evolu_sign_request_size_too_large(session: Session):
-    challenge = "1234"
+def test_evolu_sign_request_size_too_large(client: Client):
+    if optiga_unavailable(client):
+        pytest.xfail("Optiga is not available on this device.")
+    challenge = bytes.fromhex("1234")
     size = 0xFFFFFFFF + 1
-    proposed_value = bytes.fromhex(
-        "1fa386d20efb38dbb3f7ae0509651fa36c8128324ef89fa1cfd104e10dced08c594f0e8f0a525a839b4fbfaa92b8c2b51163cef593f5c14fc9f1c8c48d1192270d"
+    proof = get_proof(
+        client, b"EvoluSignRegistrationRequest", [challenge, size.to_bytes(5, "big")]
     )
 
     with pytest.raises(
@@ -141,28 +179,34 @@ def test_evolu_sign_request_size_too_large(session: Session):
         match=f"Value {size} in field size_to_acquire does not fit into uint32",
     ):
         evolu.sign_registration_request(
-            session,
-            challenge=bytes.fromhex(challenge),
+            client.get_session(),
+            challenge=challenge,
             size=size,
-            proof=proposed_value,
+            proof=proof,
         )
 
 
 @pytest.mark.models("safe")
-def test_evolu_sign_request_data_higher_bound(session: Session):
-    challenge = "12" * 255
+def test_evolu_sign_request_data_higher_bound(client: Client):
+    if optiga_unavailable(client):
+        pytest.xfail("Optiga is not available on this device.")
+    delegated_identity_key = get_delegated_identity_key(client)
+    challenge = b"\x12" * 255
     size = 0xFFFFFFFF
-    proposed_value = bytes.fromhex(
-        "1f1971f6ce302562e737520c0de2338cdaaac4e676fa02ff857b3b6081ebde794545f25905128ae9c9e7861e2358fe2e94821dd9e902564ec11478e5c6b60527c8"
+    proof = get_proof(
+        client,
+        b"EvoluSignRegistrationRequest",
+        [challenge, size.to_bytes(4, "big")],
     )
 
     response = evolu.sign_registration_request(
-        session,
-        challenge=bytes.fromhex(challenge),
+        client.get_session(),
+        challenge=challenge,
         size=size,
-        proof=proposed_value,
+        proof=proof,
     )
-    check_signature = bytes.fromhex(
-        "304402202fedb9dee42c4cb19c27daab8c5f8cbfb74047fa65a5521e1f410a14cb0ab41502202d76b31fe1c97e4577191825bc39e0b01a3bafcba3615130175cbe11d5714832"
+
+    data = signing_buffer(delegated_identity_key, challenge, size)
+    check_signature_optiga(
+        response.signature, response.certificate_chain, client.model, data
     )
-    assert response.signature == check_signature
