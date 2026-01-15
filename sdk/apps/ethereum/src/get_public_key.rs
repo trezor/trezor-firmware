c46f7d6cb3769ca78b8297026aef5a3f2ebd39ab
Lukas Bielesch <bielesch.lukas@gmail.com>
2026-01-15 23:01:02 +0100

    buildable

--- sdk/apps/ethereum/src/get_public_key.rs
+++ sdk/apps/ethereum/src/get_public_key.rs
@@ -1,6 +1,14 @@
 use crate::proto::ethereum::{EthereumGetPublicKey, EthereumPublicKey};
-use trezor_app_sdk::Result;
+use trezor_app_sdk::{Result, ui};
 
-pub fn get_public_key(_msg: EthereumGetPublicKey) -> Result<EthereumPublicKey> {
-    Ok(EthereumPublicKey::default())
+pub fn get_public_key(msg: EthereumGetPublicKey) -> Result<EthereumPublicKey> {
+    // TODO: Implement Ethereum public key retrieval"
+    let public_key = EthereumPublicKey::default();
+
+    // TODO: hexlify(resp.node.public_key).decode()
+    if matches!(msg.show_display, Some(true)) {
+        ui::show_public_key("aa")?;
+    }
+
+    Ok(public_key)
 }
