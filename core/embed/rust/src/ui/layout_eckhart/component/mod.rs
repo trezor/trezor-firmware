cf3163e325107f5b1d64f7c348b4a27971ab02a9
obrusvit <vit.obrusnik@gmail.com>
2026-01-18 17:16:55 +0100

    feat(eckhart): introduce ConnectionIndicator
    [no changelog]

--- core/embed/rust/src/ui/layout_eckhart/component/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/component/mod.rs
@@ -1,4 +1,5 @@
 mod button;
+mod connection_indicator;
 mod error;
 mod fuel_gauge;
 mod update_screen;
@@ -7,6 +8,7 @@ mod welcome_screen;
 pub use button::{
     Button, ButtonContent, ButtonMsg, ButtonStyle, ButtonStyleSheet, HapticMode, IconText,
 };
+pub use connection_indicator::ConnectionIndicator;
 pub use error::ErrorScreen;
 pub use fuel_gauge::FuelGauge;
 pub use update_screen::UpdateScreen;
