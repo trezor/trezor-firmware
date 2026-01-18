ce6657ff690b2b32509a013378617d9cdb01165e
obrusvit <vit.obrusnik@gmail.com>
2026-01-18 17:16:55 +0100

    feat(core/eckhart): render connected indication
    [no changelog]

--- core/embed/rust/src/ui/layout_eckhart/component/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/component/mod.rs
@@ -1,10 +1,12 @@
 mod button;
+mod connection_indicator;
 mod error;
 mod fuel_gauge;
 mod update_screen;
 mod welcome_screen;
 
 pub use button::{Button, ButtonContent, ButtonMsg, ButtonStyle, ButtonStyleSheet, IconText};
+pub use connection_indicator::ConnectionIndicator;
 pub use error::ErrorScreen;
 pub use fuel_gauge::FuelGauge;
 pub use update_screen::UpdateScreen;
