0d0312b68de685670732c249b5d875a52b90f91e
obrusvit <vit.obrusnik@gmail.com>
2026-01-18 17:16:55 +0100

    feat(eckhart): introduce ConnectionIndicator
    [no changelog]

--- core/embed/rust/src/ui/layout_eckhart/cshape/mod.rs
+++ core/embed/rust/src/ui/layout_eckhart/cshape/mod.rs
@@ -1,5 +1,7 @@
+mod connected;
 mod loader;
 mod screen_border;
 
+pub use connected::{render_connected_indicator, INDICATOR_OUTER_RADIUS};
 pub use loader::{render_loader, render_loader_indeterminate};
 pub use screen_border::ScreenBorder;
