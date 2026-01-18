5e409127c1130aec4d7ad09f8ce6afdb53724498
obrusvit <vit.obrusnik@gmail.com>
2026-01-18 20:58:45 +0100

    feat(eckhart): Homescreen connected indicator

--- core/embed/rust/src/ui/layout_eckhart/firmware/homescreen.rs
+++ core/embed/rust/src/ui/layout_eckhart/firmware/homescreen.rs
@@ -4,9 +4,9 @@ use crate::{
     strutil::TString,
     translations::TR,
     ui::{
-        component::{text::TextStyle, Component, Event, EventCtx, Label, Swipe},
+        component::{text::TextStyle, Component, Event, EventCtx, Label, Never, Swipe},
         display::{image::ImageInfo, Color},
-        geometry::{Direction, Point, Rect},
+        geometry::{Alignment2D, Direction, Insets, Offset, Rect},
         layout::util::get_user_custom_image,
         notification::{Notification, NotificationLevel},
         shape::{self, Renderer},
@@ -14,7 +14,7 @@ use crate::{
 };
 
 use super::{
-    super::component::{Button, ButtonContent, FuelGauge},
+    super::component::{Button, ButtonContent, ConnectionIndicator, FuelGauge},
     constant::{HEIGHT, SCREEN, WIDTH},
     theme::{self, firmware::button_homebar_style, ScreenBackground},
     ActionBar, ActionBarMsg, Hint,
@@ -25,8 +25,8 @@ use crate::ui::led::LedState;
 
 /// Full-screen component for the homescreen and lockscreen.
 pub struct Homescreen {
-    /// Device name
-    label: Label<'static>,
+    /// Device name label, fuel gauge, and connection status
+    status: HomescreenStatus,
     /// Notification
     hint: Option<Hint<'static>>,
     /// Home action bar
@@ -39,8 +39,6 @@ pub struct Homescreen {
     locked: bool,
     /// Whether the homescreen is a boot screen
     bootscreen: bool,
-    /// Fuel gauge (battery status indicator) rendered in the `action_bar` area
-    fuel_gauge: FuelGauge,
     /// Swipe component for vertical swiping
     swipe: Swipe,
     // swipe_config: SwipeConfig,
@@ -52,7 +50,6 @@ pub enum HomescreenMsg {
 }
 
 impl Homescreen {
-    const LABEL_TEXT_STYLE: TextStyle = theme::firmware::TEXT_BIG;
     pub fn new(
         label: TString<'static>,
         _lockable: bool,
@@ -61,7 +58,6 @@ impl Homescreen {
         coinjoin_authorized: bool,
         notification: Option<Notification>,
     ) -> Result<Self, Error> {
-        let label = Label::left_aligned(label, Self::LABEL_TEXT_STYLE).top_aligned();
         let image = get_homescreen_image();
 
         // Notification
@@ -87,14 +83,13 @@ impl Homescreen {
             .with_gradient(gradient);
 
         Ok(Self {
-            label,
+            status: HomescreenStatus::new(label.clone(), image.is_some()),
             hint,
             action_bar: ActionBar::new_single(btn),
             image,
             led_color,
             locked,
             bootscreen,
-            fuel_gauge: FuelGauge::always_icon_only(),
             swipe: Swipe::new().up(),
         })
     }
@@ -126,19 +121,15 @@ impl Component for Homescreen {
         debug_assert_eq!(bounds.width(), SCREEN.width());
 
         let (rest, bar_area) = bounds.split_bottom(theme::ACTION_BAR_HEIGHT);
-        let (status_area, rest) = rest.split_top(theme::HEADER_HEIGHT);
-        let status_area = status_area.inset(theme::SIDE_INSETS);
-        let rest = if let Some(hint) = &mut self.hint {
+        let status_area = if let Some(hint) = &mut self.hint {
             let (rest, hint_area) = rest.split_bottom(hint.height());
             hint.place(hint_area);
             rest
         } else {
             rest
         };
-        let label_area = rest.inset(theme::SIDE_INSETS);
 
-        self.fuel_gauge.place(status_area);
-        self.label.place(label_area);
+        self.status.place(status_area.inset(theme::SIDE_INSETS));
         self.action_bar.place(bar_area);
         // Swipe component is placed in the action bar touch area
         self.swipe.place(self.action_bar.touch_area());
@@ -146,14 +137,12 @@ impl Component for Homescreen {
     }
 
     fn event(&mut self, ctx: &mut EventCtx, event: Event) -> Option<Self::Msg> {
-        self.fuel_gauge.event(ctx, event);
-
+        self.status.event(ctx, event);
         let swipe_up = matches!(self.swipe.event(ctx, event), Some(Direction::Up));
         let homebar_tap = matches!(
             self.action_bar.event(ctx, event),
             Some(ActionBarMsg::Confirmed)
         );
-
         if swipe_up || homebar_tap {
             return if self.locked {
                 Some(HomescreenMsg::Dismissed)
@@ -173,12 +162,9 @@ impl Component for Homescreen {
         } else {
             ScreenBackground::new(self.led_color, None).render(target);
         }
-        self.label.render(target);
+        self.status.render(target);
         self.hint.render(target);
         self.action_bar.render(target);
-        if self.fuel_gauge.should_be_shown() {
-            self.fuel_gauge.render(target);
-        }
 
         #[cfg(feature = "rgb_led")]
         target.set_led_state(LedState::Static(
@@ -186,6 +172,94 @@ impl Component for Homescreen {
         ));
     }
 }
+
+/// Helper component that displays device name label, battery status, and
+/// connection status indicator.
+/// It is combined because the label is shown together with the fuel gauge and
+/// connection indicator when a custom background image is used.
+struct HomescreenStatus {
+    area: Rect,
+    /// Device name
+    label: Label<'static>,
+    /// Fuel gauge (battery status indicator)
+    fuel_gauge: FuelGauge,
+    /// Whether the device is connected to Host (either via USB or BLE)
+    connection_indicator: ConnectionIndicator,
+    /// Whether a custom background image is used, which affects the layout and
+    /// styling of the label
+    background_image: bool,
+}
+
+impl HomescreenStatus {
+    const LABEL_TEXT_STYLE_DEFAULT: TextStyle = theme::firmware::TEXT_BIG;
+    const LABEL_TEXT_STYLE_WITH_IMG: TextStyle = theme::firmware::TEXT_SMALL;
+
+    pub fn new(label: TString<'static>, background_image: bool) -> Self {
+        let style = if background_image {
+            Self::LABEL_TEXT_STYLE_WITH_IMG
+        } else {
+            Self::LABEL_TEXT_STYLE_DEFAULT
+        };
+        Self {
+            area: Rect::zero(),
+            label: Label::left_aligned(label, style).top_aligned(),
+            fuel_gauge: FuelGauge::always_icon_only(),
+            connection_indicator: ConnectionIndicator::new(),
+            background_image,
+        }
+    }
+}
+
+impl Component for HomescreenStatus {
+    type Msg = Never;
+
+    fn place(&mut self, bounds: Rect) -> Rect {
+        const LABEL_INSETS_DEFAULT: Insets = Insets::new(83, 24, 24, 0);
+        const ICON_PERCENT_GAP: i16 = 16; // TODO: put to theme?
+        let (header_area, _) = bounds.split_top(theme::HEADER_HEIGHT);
+        let (fuel_gauge_area, rest_header_area) =
+            header_area.split_left(self.fuel_gauge.content_width());
+        let connection_indicator_area = Rect::snap(
+            fuel_gauge_area.right_center(),
+            Offset::uniform(ConnectionIndicator::AREA_SIZE_NEEDED),
+            Alignment2D::CENTER_LEFT,
+        )
+        .translate(Offset::x(ICON_PERCENT_GAP));
+
+        let label_area = if !self.background_image {
+            bounds.inset(LABEL_INSETS_DEFAULT)
+        } else {
+            rest_header_area
+                .split_left(self.connection_indicator.content_width())
+                .1
+        };
+
+        self.fuel_gauge.place(fuel_gauge_area);
+        self.connection_indicator.place(connection_indicator_area);
+        self.label.place(label_area);
+        self.area = bounds;
+        self.area
+    }
+
+    fn event(&mut self, ctx: &mut EventCtx, event: Event) -> Option<Self::Msg> {
+        self.fuel_gauge.event(ctx, event);
+        let connection_event = self.connection_indicator.event(ctx, event);
+        if matches!(event, Event::PM(_)) || connection_event.is_some() {
+            // TODO: could FuelGauge also return Some(()) on update?
+            self.place(self.area);
+            ctx.request_paint();
+        }
+        None
+    }
+
+    fn render<'s>(&'s self, target: &mut impl Renderer<'s>) {
+        self.label.render(target);
+        self.fuel_gauge.render(target);
+        self.connection_indicator.render(target);
+    }
+}
+
+struct HideLabelAnimation {}
 
 pub fn check_homescreen_format(image: BinaryData) -> bool {
     match ImageInfo::parse(image) {
@@ -209,8 +283,16 @@ fn get_homescreen_image() -> Option<BinaryData<'static>> {
 impl crate::trace::Trace for Homescreen {
     fn trace(&self, t: &mut dyn crate::trace::Tracer) {
         t.component("Homescreen");
-        t.child("label", &self.label);
+        t.child("status", &self.status);
         t.child("homebar", &self.action_bar);
+    }
+}
+#[cfg(feature = "ui_debug")]
+impl crate::trace::Trace for HomescreenStatus {
+    fn trace(&self, t: &mut dyn crate::trace::Tracer) {
+        t.component("HomescreenStatus");
+        t.child("label", &self.label);
         t.child("fuel_gauge", &self.fuel_gauge);
+        t.child("connection_indicator", &self.connection_indicator);
     }
 }
