8c5c7b56dc4bfe742c2a383eeafdefbb2ee5fd44
bleska <jiri.blecha@satoshilabs.com>
2026-01-20 18:04:59 +0100

    feat(core): TS7 display refresh rate scaling and configuration optimization
    In order to lower the power consumption in standard operation mode, the goal is to regulate the display refresh rate based on the current needs (animations - 60Hz, static images - 30 Hz).
     - Introduction of the on-the-fly refresh rate change functionality (interrupt based).
    
    Configuration optimizations' investigations:
     - Since the display visible dimensions are 520x380 and the display controller doesn't support this resolution directly, the closest variant 520x480 is used with 50px offset. Changing the resolution used to 520x430 and shortening the HACT region by 50px effectivly increasing the blanking period.
     - Using RGB565 DSI color format, changing pixel clock to 15.5 MHz, changing HFP to 20 to maintain the 60 Hz refresh rate for the new pixel clock.
     - DSI lane byte clock vs LTDC pixel clock ratio (max, min) tests.
     - Etc. - different configuration profiles have been tested and TS7 input power consumption measured.
    
    Formalizing the code to official one:
     - adding preprocessor definitions to panels' configuration header files
     - adding ifdefs to display_driver and display_fb source files to integrate the refresh rate scaling code based on panel's used
     - the refresh rate change is to be performed to high one as soon as new image is to be shown, and to the low one after a timeout expires
    
    [no changelog]

--- core/embed/io/display/ltdc_dsi/panels/stm32u5a9j-dk/stm32u5a9j-dk.h
+++ core/embed/io/display/ltdc_dsi/panels/stm32u5a9j-dk/stm32u5a9j-dk.h
@@ -19,16 +19,61 @@
 
 #pragma once
 
-#define DSI_LANE_BYTE_FREQ_HZ 62500000ULL
+#include <trezor_types.h>
 
-#define VSYNC 1
-#define VBP 12
-#define VFP 50
-#define VACT 481
-#define HSYNC 2
-#define HBP 1
-#define HFP 1
-#define HACT 480
+#define REFRESH_RATE_SCALING_SUPPORTED 0
+
+// DSI PLL configuration (lane byte clock, TX escape clock)
+// DSI_LANE_BYTE_CLOCK_HZ = (((HSE_VALUE / PLL_DSI_IDF) * 2 * PLL_DSI_NDIV) /
+// PLL_DSI_ODF) / 8
+#define DSI_LANE_BYTE_CLOCK_HZ 62000000UL  // PLL DSI
+#define PLL_DSI_IDF 4
+// PLL_DSI_NDIV = (DSI_LANE_BYTE_CLOCK_HZ * 8 * PLL_DSI_ODF * PLL_DSI_IDF) / (2
+// * HSE_VALUE)
+#define PLL_DSI_NDIV 62
+#define PLL_DSI_ODF 2
+#define DSI_DPHY_FRANGE DSI_DPHY_FRANGE_450MHZ_510MHZ
+#define DSI_TX_ESCAPE_CLK_DIV 4  // 15.5MHz, ~7.75MHz (in LP)
+
+// DSI PHY timing parameters configuration
+#define PHY_LP_OFFSET PHY_LP_OFFSSET_0_CLKP  // LPXO - no offset
+// RM0456 Table 445. HS2LP and LP2HS values vs. band frequency (MHz)
+#define PHY_TIMER_CLK_HS2LP 11
+#define PHY_TIMER_CLK_LP2HS 40
+#define PHY_TIMER_DATA_HS2LP 12
+#define PHY_TIMER_DATA_LP2HS 23
+
+// LTDC PLL3 configuration (pixel clock and lane byte clock at the beginning of
+// initialization)
+// LTDC_PIXEL_CLOCK_HZ = ((HSE_VALUE / PLL3_M) * PLL3_N) / PLL3_R
+#define LTDC_PIXEL_CLOCK_HZ 20833333UL  // Output of PLL3R
+// 4MHz is used as PLL3 block input clock
+#define PLL3_M (HSE_VALUE / 4000000UL)
+#define PLL3_N 125
+#define PLL3_P 8
+#define PLL3_Q 8  // Not used output clock branch
+#define PLL3_R 24
+
+// DSI lane byte clock to LTDC pixel clock ratio
+#define LANE_BYTE_2_PIXEL_CLK_RATIO 3
+
+// Display timing parameters
+#define HSYNC 2   // Horizontal Sync
+#define HBP 1     // Horizontal Back Porch
+#define HACT 480  // Horizontal Active Time
+#define HFP 1     // Horizontal Front Porch
+
+#define VSYNC 1   // Vertical Sync
+#define VBP 12    // Vertical Back Porch
+#define VACT 481  // Vertical Active Time
+#define VFP 50    // Vertical Front Porch
+
+#define PANEL_DSI_MODE DSI_VID_MODE_BURST
+#define PANEL_DSI_LANES DSI_TWO_DATA_LANES
+#define PANEL_DSI_COLOR_CODING DSI_RGB888
+
+#define PANEL_LTDC_PIXEL_FORMAT LTDC_PIXEL_FORMAT_ARGB8888
+
 #define LCD_WIDTH 480
 #define LCD_HEIGHT 480
 
@@ -39,10 +84,6 @@
 #define GFXMMU_LUT_FIRST 0
 #define GFXMMU_LUT_LAST 479
 #define GFXMMU_LUT_SIZE 480
-
-#define PANEL_DSI_MODE DSI_VID_MODE_BURST
-#define PANEL_DSI_LANES DSI_TWO_DATA_LANES
-#define PANEL_LTDC_PIXEL_FORMAT LTDC_PIXEL_FORMAT_ARGB8888
 
 // Size of the physical frame buffer in bytes
 #define PHYSICAL_FRAME_BUFFER_SIZE (240 * 240 * 4)
