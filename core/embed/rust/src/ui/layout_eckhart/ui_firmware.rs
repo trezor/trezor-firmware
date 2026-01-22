9ea179d42565d75fb4fcc30122fea9116a72dc98
Lukas Bielesch <bielesch.lukas@gmail.com>
2026-01-22 23:34:29 +0400

    chore(core): modular eth get public key implementation - new trezorcrtypto_api module in rust - rust implementation of get public key - new string type for long strings

--- core/embed/rust/src/ui/layout_eckhart/ui_firmware.rs
+++ core/embed/rust/src/ui/layout_eckhart/ui_firmware.rs
@@ -34,7 +34,7 @@ use crate::{
     util::interpolate,
 };
 
-use trezor_structs::{ArchivedShortString, ArchivedTrezorUiEnum};
+use trezor_structs::{ArchivedStringN, ArchivedTrezorUiEnum};
 
 #[cfg(feature = "ble")]
 use crate::ui::component::{BLEHandler, BLEHandlerMode};
@@ -1741,7 +1741,7 @@ impl FirmwareUI for UIEckhart {
 
     fn process_ipc_message(data: &[u8]) -> Result<Gc<LayoutObj>, Error> {
         // Safe helper to convert archived string to TString using Deref
-        fn tstr_from_archived(s: &ArchivedShortString) -> TString<'static> {
+        fn tstr_from_archived<const N: usize>(s: &ArchivedStringN<N>) -> TString<'static> {
             unsafe { StrBuffer::from_ptr_and_len(s.data.as_ptr(), s.len as usize) }.into()
         }
 
@@ -1834,7 +1834,7 @@ impl FirmwareUI for UIEckhart {
                     "account".into(),
                     None,
                     None,
-                    0,
+                    1,
                     "br_name".into(),
                 )?;
 
