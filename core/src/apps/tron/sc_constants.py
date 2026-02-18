90a4407af929a360db139fb9ccbc07312c2f2d90
PrisionMike <su.sh2396@gmail.com>
2026-02-19 01:18:09 +0530

    feat(tron): Unfreeze and claiming TRX - core logic - Refactored Unfreeze with freeze. - Simplified some definitions and label use. - Supports UnfreezeBalanceV2Contract and WithdrawExpireUnfreezeContract
    [no changelog]

--- core/src/apps/tron/sc_constants.py
+++ core/src/apps/tron/sc_constants.py
@@ -5,14 +5,10 @@ from ubinascii import unhexlify
 SC_FUNC_SIG_BYTES = const(4)
 SC_ARGUMENT_BYTES = const(32)
 SC_ARGUMENT_ADDRESS_BYTES = const(20)
-SC_FUNC_APPROVE_REVOKE_AMOUNT = const(0)
 
 assert SC_ARGUMENT_ADDRESS_BYTES <= SC_ARGUMENT_BYTES
 
-# Known ERC-20 functions
+# Known TRC-20/ERC-20 functions
 
 SC_FUNC_SIG_TRANSFER = unhexlify("a9059cbb")
 SC_FUNC_SIG_APPROVE = unhexlify("095ea7b3")
-SC_FUNC_SIG_STAKE = unhexlify("3a29dbae")
-SC_FUNC_SIG_UNSTAKE = unhexlify("76ec871c")
-SC_FUNC_SIG_CLAIM = unhexlify("33986ffa")
