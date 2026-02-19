41903a328150b99b4c3c96c59385622a2197177c
tychovrahe <brunam@seznam.cz>
2026-02-19 17:06:19 +0100

    D003

---
+++ core/embed/models/D003/model_D003.h
@@ -0,0 +1,65 @@
+/*
+ * This file is part of the Trezor project, https://trezor.io/
+ *
+ * Copyright (c) SatoshiLabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <rtl/sizedefs.h>
+// #include "bootloaders/bootloader_hashes.h"
+#include "secret_layout.h"
+
+#define MODEL_NAME "D003"
+#define MODEL_FULL_NAME "Trezor DIY 3"
+#define MODEL_INTERNAL_NAME "D003"
+#define MODEL_INTERNAL_NAME_TOKEN D003
+#define MODEL_NAME_QSTR MP_QSTR_D003
+#define MODEL_INTERNAL_NAME_QSTR MP_QSTR_D003
+#define MODEL_USB_MANUFACTURER "Trezor DIY"
+#define MODEL_USB_PRODUCT MODEL_FULL_NAME
+#define MODEL_HOMESCREEN_MAXSIZE 65536
+#define MODEL_BLE_CODE 7
+
+/*** Discovery uses DEV keys in any build variant ***/
+#define MODEL_BOARDLOADER_KEYS \
+  (const uint8_t *)"\xdb\x99\x5f\xe2\x51\x69\xd1\x41\xca\xb9\xbb\xba\x92\xba\xa0\x1f\x9f\x2e\x1e\xce\x7d\xf4\xcb\x2a\xc0\x51\x90\xf3\x7f\xcc\x1f\x9d", \
+  (const uint8_t *)"\x21\x52\xf8\xd1\x9b\x79\x1d\x24\x45\x32\x42\xe1\x5f\x2e\xab\x6c\xb7\xcf\xfa\x7b\x6a\x5e\xd3\x00\x97\x96\x0e\x06\x98\x81\xdb\x12", \
+  (const uint8_t *)"\x22\xfc\x29\x77\x92\xf0\xb6\xff\xc0\xbf\xcf\xdb\x7e\xdb\x0c\x0a\xa1\x4e\x02\x5a\x36\x5e\xc0\xe3\x42\xe8\x6e\x38\x29\xcb\x74\xb6",
+
+#define MODEL_BOOTLOADER_KEYS \
+  (const uint8_t *)"\xd7\x59\x79\x3b\xbc\x13\xa2\x81\x9a\x82\x7c\x76\xad\xb6\xfb\xa8\xa4\x9a\xee\x00\x7f\x49\xf2\xd0\x99\x2d\x99\xb8\x25\xad\x2c\x48", \
+  (const uint8_t *)"\x63\x55\x69\x1c\x17\x8a\x8f\xf9\x10\x07\xa7\x47\x8a\xfb\x95\x5e\xf7\x35\x2c\x63\xe7\xb2\x57\x03\x98\x4c\xf7\x8b\x26\xe2\x1a\x56", \
+  (const uint8_t *)"\xee\x93\xa4\xf6\x6f\x8d\x16\xb8\x19\xbb\x9b\xeb\x9f\xfc\xcd\xfc\xdc\x14\x12\xe8\x7f\xee\x6a\x32\x4c\x2a\x99\xa1\xe0\xe6\x71\x48",
+
+#define MODEL_SECMON_KEYS \
+  (const uint8_t *)"\xdb\x99\x5f\xe2\x51\x69\xd1\x41\xca\xb9\xbb\xba\x92\xba\xa0\x1f\x9f\x2e\x1e\xce\x7d\xf4\xcb\x2a\xc0\x51\x90\xf3\x7f\xcc\x1f\x9d", \
+  (const uint8_t *)"\x21\x52\xf8\xd1\x9b\x79\x1d\x24\x45\x32\x42\xe1\x5f\x2e\xab\x6c\xb7\xcf\xfa\x7b\x6a\x5e\xd3\x00\x97\x96\x0e\x06\x98\x81\xdb\x12", \
+  (const uint8_t *)"\x22\xfc\x29\x77\x92\xf0\xb6\xff\xc0\xbf\xcf\xdb\x7e\xdb\x0c\x0a\xa1\x4e\x02\x5a\x36\x5e\xc0\xe3\x42\xe8\x6e\x38\x29\xcb\x74\xb6",
+
+#define IMAGE_CHUNK_SIZE SIZE_128K
+#define IMAGE_HASH_SHA256
+
+#define DISPLAY_JUMP_BEHAVIOR DISPLAY_RESET_CONTENT
+
+#define NORCOW_SECTOR_SIZE (16 * 4 * 1024)  // 64 kB
+#define NORCOW_MIN_VERSION 0x00000006
+
+#ifdef USE_SECMON_LAYOUT
+#include "memory_secmon.h"
+#else
+#include "memory.h"
+#endif
---
+++ core/site_scons/models/D003/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from .discovery3 import configure
+
+
+def configure_board(
+    revision: Optional[str],
+    features_wanted: list[str],
+    env: dict,
+    defines: list[str | tuple[str, str]],
+    sources: list[str],
+    paths: list[str],
+):
+    defines += (("MODEL_HEADER", '"D003/model_D003.h"'),)
+    defines += (("VERSIONS_HEADER", '"D003/versions.h"'),)
+    defines += (("OTP_LAYOUT_HEADER", '"D003/otp_layout.h"'),)
+    defines += (("UNIT_PROPERTIES_CONTENT_HEADER", '"D003/unit_properties_content.h"'),)
+    return configure(env, features_wanted, defines, sources, paths)
+
+
+def get_model_ui() -> str:
+    return "caesar"
+
+
+def get_model_ui_conf() -> list[str]:
+    return []
