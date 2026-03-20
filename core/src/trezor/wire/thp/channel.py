a931069801235e829f8c733a82cffb92110e8c08
M1nd3r <petrsedlacek.km@seznam.cz>
2026-03-20 10:43:21 +0100

    chore(core): improve handling of large messages
    - Handle failure to read big messages in a more controlled way.
    - Raise `FirmwareError` when failing to get a write buffer.
    - Remove duplicated constant from `writer.py`.
    
    [no changelog]

--- core/src/trezor/wire/thp/channel.py
+++ core/src/trezor/wire/thp/channel.py
@@ -90,6 +90,9 @@ class Reassembler:
                 return False
 
             buffer = self.thp_read_buf.get(self.buffer_len)
+            if buffer is None:
+                # Failed to get the buffer
+                return False
             self._buffer_packet_data(buffer, packet, PacketHeader.CONT_LENGTH)
         else:
             self.reset()
@@ -97,6 +100,9 @@ class Reassembler:
             self.buffer_len = payload_length + PacketHeader.INIT_LENGTH
 
             buffer = self.thp_read_buf.get(self.buffer_len)
+            if buffer is None:
+                # Failed to get the buffer
+                return False
             self._buffer_packet_data(buffer, packet, 0)
 
         assert len(buffer) == self.buffer_len
@@ -415,6 +421,11 @@ class Channel:
         length = payload_size + CHECKSUM_LENGTH + TAG_LENGTH + PacketHeader.INIT_LENGTH
 
         buffer = self.write_buf.get(length)
+        if buffer is None:
+            from trezor import wire
+
+            raise wire.FirmwareError("Failed to get a sufficiently large write buffer.")
+
         noise_payload_len = memory_manager.encode_into_buffer(buffer, msg, session_id)
 
         self._encrypt(buffer, noise_payload_len)
