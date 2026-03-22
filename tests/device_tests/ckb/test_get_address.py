c7857c51b651d767174b64d3b9e760536697285e
DinhMaiMTD <142642094+DinhMaiMTD@users.noreply.github.com>
2026-03-23 00:49:20 +0700

    fix(ckb): test get address missing coin type "Mainnet"

--- tests/device_tests/ckb/test_get_address.py
+++ tests/device_tests/ckb/test_get_address.py
@@ -13,13 +13,13 @@ pytestmark = [pytest.mark.altcoin, pytest.mark.ckb, pytest.mark.models("core")]
 @parametrize_using_common_fixtures("ckb/get_address.json")
 def test_get_address(session: Session, parameters, result):
     address_n = parse_path(parameters["path"])
-    address = ckb.get_address(session, address_n, show_display=True)
+    address = ckb.get_address(session, address_n, show_display=True, network="Mainnet")
     assert address == result["address"]
 
 
 def test_invalid_path(session: Session):
     with pytest.raises(TrezorFailure, match="Forbidden key path"):
-        ckb.get_address(session, parse_path("m/44h/999h/0h/0/0"), show_display=True)
+        ckb.get_address(session, parse_path("m/44h/999h/0h/0/0"), show_display=True, network="Mainnet")
 
 
 def test_get_address_cancel_show(session: Session):
@@ -31,4 +31,4 @@ def test_get_address_cancel_show(session: Session):
 
     with pytest.raises(Cancelled), session.test_ctx as client:
         client.set_input_flow(input_flow)
-        ckb.get_address(session, address_n, show_display=True)
+        ckb.get_address(session, address_n, show_display=True, network="Mainnet")
