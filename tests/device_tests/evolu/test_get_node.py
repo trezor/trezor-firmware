80a023ae641781289b3fdd468323a20af42b8c74
Martin Pastyřík <martin.pastyrik@seznam.cz>
2026-03-25 13:56:00 +0100

    fix(tests): Fix Evolu nightly tests
    Compute the proofs of delegated identity dynamically so that it does not fail in hardware tests
    
    [no changelog]

--- tests/device_tests/evolu/test_get_node.py
+++ tests/device_tests/evolu/test_get_node.py
@@ -74,9 +74,7 @@ def test_evolu_get_node_none_proof(client: Client):
 
 
 def test_evolu_get_node_index_change(client: Client):
-    proof = bytes.fromhex(
-        "1fb521e8a4e4580377d530a9d6eb0a394ec8340fa42094d9f2e822bb944ce6a2074b81241b3b65dfa15d66e052f2504aba3ad1644844d695b181b3cdc9666cb66b"
-    )
+    proof = get_proof(client, b"EvoluGetNode", [])
     node_index_0 = evolu.get_node(
         client.get_session(), proof=proof, node_rotation_index=0
     )
@@ -88,9 +86,7 @@ def test_evolu_get_node_index_change(client: Client):
 
 
 def test_evolu_get_node_index_1(client: Client):
-    proof = bytes.fromhex(
-        "1fb521e8a4e4580377d530a9d6eb0a394ec8340fa42094d9f2e822bb944ce6a2074b81241b3b65dfa15d66e052f2504aba3ad1644844d695b181b3cdc9666cb66b"
-    )
+    proof = get_proof(client, b"EvoluGetNode", [])
     node = evolu.get_node(client.get_session(), proof=proof, node_rotation_index=1)
 
     check_value = bytes.fromhex(
