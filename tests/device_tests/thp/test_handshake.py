013743d6ff50d251798020ceba458534344cb1bb
M1nd3r <petrsedlacek.km@seznam.cz>
2026-03-30 18:02:15 +0200

    test(core): add test for THP handshake
    - Added test: host raises an exception on receipt of malformed `handshake_init_response`.
    
    [no changelog]

--- tests/device_tests/thp/test_handshake.py
+++ tests/device_tests/thp/test_handshake.py
@@ -1,8 +1,11 @@
 import pytest
+from cryptography import exceptions
 
 from trezorlib.debuglink import TrezorTestContext
 from trezorlib.exceptions import DeviceLockedError
+from trezorlib.thp.channel import Channel
 from trezorlib.thp.client import TrezorClientThp
+from trezorlib.thp.message import Message
 from trezorlib.thp.pairing import PairingController
 
 from .connect import prepare_channel_for_handshake
@@ -10,6 +13,34 @@ from .connect import prepare_channel_for_handshake
 PIN4 = "1234"
 
 pytestmark = [pytest.mark.protocol("thp"), pytest.mark.setup_client(pin=PIN4)]
+
+
+@pytest.mark.setup_client(pin=None)
+def test_read_malformed_response(test_ctx: TrezorTestContext) -> None:
+    channel = Channel.allocate(test_ctx.transport)
+    original_read = channel._read
+
+    def _patched_read(timeout: float | None = None) -> Message:
+        message = original_read(timeout)
+
+        if message.is_ack():
+            # do not modify ACK
+            return message
+
+        assert message.is_handshake_init_response()
+
+        # modify last byte of the noise AEAD tag
+        modified_data = bytearray(message.data)
+        modified_data[-1] = (message.data[-1] + 1) % 256
+        return Message(
+            ctrl_byte=message.ctrl_byte,
+            cid=message.cid,
+            data=bytes(modified_data),
+        )
+
+    channel._read = _patched_read
+    with pytest.raises(exceptions.InvalidTag):
+        channel.open([], force_unlock=True)
 
 
 def test_no_unlock(test_ctx: TrezorTestContext):
