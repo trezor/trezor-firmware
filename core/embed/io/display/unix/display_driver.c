3ad7491fac30d3d600e787e63c7cab8479bd705f
cepetr <petr@cerninovi.cz>
2026-04-02 16:35:09 +0200

    refactor(core): use correct  SDL includes
    [no changelog]

--- core/embed/io/display/unix/display_driver.c
+++ core/embed/io/display/unix/display_driver.c
@@ -17,12 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#include "SDL_blendmode.h"
-#include "SDL_render.h"
-#endif
-
 #include <trezor_bsp.h>
 #include <trezor_model.h>
 #include <trezor_rtl.h>
@@ -32,8 +26,14 @@
 #include <sys/logging.h>
 #include <sys/systask.h>
 
-#include <SDL.h>
-#include <SDL_image.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#include <SDL2/SDL_blendmode.h>
+#include <SDL2/SDL_render.h>
+#endif
 
 #include "profile.h"
 
