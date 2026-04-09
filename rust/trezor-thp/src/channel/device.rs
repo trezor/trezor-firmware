cbc91a606b1cbb385d211f63e5e107e3fe15723a
Martin Milata <martin@martinmilata.cz>
2026-04-09 23:19:32 +0200

    feat(rust/trezor-thp): ACK piggybacking

--- rust/trezor-thp/src/channel/device.rs
+++ rust/trezor-thp/src/channel/device.rs
@@ -132,7 +132,7 @@ where
 
     // Returns true if allocation request has been received.
     fn handle_broadcast(&mut self, packet: &[u8]) -> Result<bool, Error> {
-        let (header, payload) = Reassembler::<Device>::single_inplace(packet)?;
+        let (header, payload) = Reassembler::<Device>::single(packet)?;
         match header {
             Header::Ping if payload.len() == Nonce::LEN => {
                 let (nonce, _rest) = Nonce::parse(payload)?;
@@ -328,11 +328,18 @@ impl<C: CredentialVerifier, B: Backend> ChannelOpen<C, B> {
     }
 
     fn incoming_internal(&mut self) -> Result<(), Error> {
+        let ChannelState::Receiving { reassembler, .. } = &self.channel.state else {
+            return Err(Error::not_ready());
+        };
+        let sync_bits = reassembler.sync_bits();
+
         let (header, len) = self.channel.raw_out(&self.internal_buffer)?;
         self.internal_buffer.truncate(len);
 
         match (self.state, header.handshake_phase()) {
             (HandshakeState::SendingChannelResponse, Some(HandshakeMessage::InitiationRequest)) => {
+                // enable ACK piggybacking if requested
+                self.enable_ack_piggybacking_if_requested(sync_bits);
                 let try_to_unlock = self.noise.read_initiation_request(&self.internal_buffer)?;
                 self.state = HandshakeState::StaticKeyRequired { try_to_unlock };
             }
@@ -349,6 +356,12 @@ impl<C: CredentialVerifier, B: Backend> ChannelOpen<C, B> {
             }
         }
         Ok(())
+    }
+
+    fn enable_ack_piggybacking_if_requested(&mut self, sync_bits: SyncBits) {
+        if sync_bits.ack_bit() {
+            self.channel.sync.allow_ack_piggybacking();
+        }
     }
 
     fn send_initiation_response(
@@ -480,18 +493,19 @@ where
         let res = self
             .channel
             .packet_in(packet_buffer, &mut self.internal_buffer);
-        if let PacketInResult::EnlargeBuffer { buffer_size, .. } = res {
+        if let PacketInResult::Accepted {
+            buffer_size: Some(s),
+            ..
+        } = res
+        {
             log::error!(
                 "[{:04x}] Payload length {} exceeds handshake limit.",
                 self.channel_id(),
-                buffer_size
+                s
             );
             // Possibly damaged length field, ignore continuations.
             self.channel.state = ChannelState::Idle;
             return PacketInResult::ignore(Error::malformed_data());
-        }
-        if res.got_ack() {
-            prepare_zeroed(&mut self.internal_buffer);
         }
         if res.got_message() {
             let handled = self.incoming_internal();
@@ -506,6 +520,8 @@ where
             if let HandshakeState::StaticKeyRequired { try_to_unlock } = self.state {
                 return PacketInResult::HandshakeKeyRequired { try_to_unlock };
             }
+        } else if res.got_ack() {
+            prepare_zeroed(&mut self.internal_buffer);
         }
         res
     }
--- rust/trezor-thp/src/fragment.rs
+++ rust/trezor-thp/src/fragment.rs
@@ -109,6 +109,7 @@ impl<R: Role> Fragmenter<R> {
 
 pub struct Reassembler<R: Role> {
     header: Header<R>,
+    sync_bits: SyncBits,
     offset: usize,
     checksum: Crc32,
 }
@@ -116,6 +117,7 @@ pub struct Reassembler<R: Role> {
 impl<R: Role> Reassembler<R> {
     pub fn new(input: &[u8], buffer: &mut [u8]) -> Result<Self> {
         let (header, after_header) = Header::parse(input)?;
+        let sync_bits = SyncBits::try_from(input)?;
         if header.is_continuation() {
             return Err(Error::malformed_data());
         }
@@ -134,6 +136,7 @@ impl<R: Role> Reassembler<R> {
 
         Ok(Self {
             header,
+            sync_bits,
             offset: nbytes,
             checksum,
         })
@@ -200,19 +203,12 @@ impl<R: Role> Reassembler<R> {
         &self.header
     }
 
-    // Shortcut to deserialize single packet message.
-    pub fn single<'a>(buffer: &[u8], dest: &'a mut [u8]) -> Result<(Header<R>, &'a [u8])> {
-        let reassembler = Self::new(buffer, dest)?;
-        if !reassembler.is_done() {
-            log::error!("Single packet message expected.");
-            return Err(Error::malformed_data());
-        }
-        let reply_len = reassembler.verify(dest)?;
-        let header = reassembler.header;
-        Ok((header, &dest[..reply_len]))
+    pub fn sync_bits(&self) -> SyncBits {
+        self.sync_bits
     }
 
-    pub fn single_inplace(buffer: &[u8]) -> Result<(Header<R>, &[u8])> {
+    // Shortcut to deserialize single packet message.
+    pub fn single(buffer: &[u8]) -> Result<(Header<R>, &[u8])> {
         let (header, after_header) = Header::parse(buffer)?;
         if header.is_continuation() {
             return Err(Error::malformed_data());
@@ -318,7 +314,7 @@ mod test {
             let packets = fragment(header, SyncBits::new(), source, packet_size);
             assert_eq!(packets.len(), 1);
 
-            let res = Reassembler::<Device>::single_inplace(&packets[0]).unwrap();
+            let res = Reassembler::<Device>::single(&packets[0]).unwrap();
             let expected_hex = hex::encode(source);
             let received_hex = hex::encode(res.1);
             assert_eq!(received_hex, expected_hex);
@@ -328,15 +324,15 @@ mod test {
     #[test]
     fn test_reassemble_single_good() {
         let empty = hex::decode(EMPTY_PAYLOAD_EXPECTED).unwrap();
-        let res = Reassembler::<Device>::single_inplace(&empty).unwrap();
+        let res = Reassembler::<Device>::single(&empty).unwrap();
         assert_eq!(res.1, b"");
-        let res = Reassembler::<Host>::single_inplace(&empty).unwrap();
+        let res = Reassembler::<Host>::single(&empty).unwrap();
         assert_eq!(res.1, b"");
 
         let short = hex::decode(SHORT_PAYLOAD_EXPECTED).unwrap();
-        let res = Reassembler::<Device>::single_inplace(&short).unwrap();
+        let res = Reassembler::<Device>::single(&short).unwrap();
         assert_eq!(res.1, b"\x07");
-        let res = Reassembler::<Host>::single_inplace(&short).unwrap();
+        let res = Reassembler::<Host>::single(&short).unwrap();
         assert_eq!(res.1, b"\x07");
     }
 
@@ -344,16 +340,16 @@ mod test {
     fn test_reassemble_single_bad() {
         // failure expected when more data follows
         let incomplete = hex::decode(LONGER_PAYLOAD_EXPECTED[0]).unwrap();
-        let res = Reassembler::<Device>::single_inplace(&incomplete);
+        let res = Reassembler::<Device>::single(&incomplete);
         assert_eq!(res, Err(Error::MalformedData));
-        let res = Reassembler::<Host>::single_inplace(&incomplete);
+        let res = Reassembler::<Host>::single(&incomplete);
         assert_eq!(res, Err(Error::MalformedData));
 
         // failure expected on continuations
         let continuation = hex::decode(LONGER_PAYLOAD_EXPECTED[1]).unwrap();
-        let res = Reassembler::<Device>::single_inplace(&continuation);
+        let res = Reassembler::<Device>::single(&continuation);
         assert_eq!(res, Err(Error::MalformedData));
-        let res = Reassembler::<Host>::single_inplace(&continuation);
+        let res = Reassembler::<Host>::single(&continuation);
         assert_eq!(res, Err(Error::MalformedData));
     }
 
@@ -458,7 +454,7 @@ mod test {
         let reassembler = Reassembler::<Device>::new(packet, &mut received);
         assert!(matches!(reassembler, Err(Error::MalformedData)));
 
-        let inplace = Reassembler::<Device>::single_inplace(packet);
+        let inplace = Reassembler::<Device>::single(packet);
         assert!(matches!(inplace, Err(Error::MalformedData)));
     }
 }
