efe31e92cf6d2f4219ddd7cbcd842cad1dfdb6ff
obrusvit <vit.obrusnik@gmail.com>
2026-04-13 13:55:33 +0200

    chore(core): add TS7 foreground image for emulator
    - due to the distinct display shape, this emulator uses foreground image
    so that the shape is visible
    - for that reason, a PNG picture is used with a cutout of the display
    - LED mock position was moved slightly above the display
    
    [no changelog]

--- core/embed/models/T3W1/boards/t3w1-unix.h
+++ core/embed/models/T3W1/boards/t3w1-unix.h
@@ -1,16 +1,18 @@
 #ifndef BOARDS_T3W1_UNIX_H
 #define BOARDS_T3W1_UNIX_H
 
-#define WINDOW_WIDTH 400
-#define WINDOW_HEIGHT 600
-#define TOUCH_OFFSET_X 80
-#define TOUCH_OFFSET_Y 102
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 800
+#define TOUCH_OFFSET_X 107
+#define TOUCH_OFFSET_Y 53
 
 #define BTN_POWER_KEY SDLK_p
 
 #define ORIENTATION_NS 1
 
-// #define BACKGROUND_FILE "T3W1/background_T3W1.h"
-// #define BACKGROUND_NAME background_T3W1_jpg
+#define LED_OFFSET_Y -8
+
+#define FOREGROUND_FILE "T3W1/foreground_T3W1.h"
+#define FOREGROUND_NAME foreground_T3W1_png
 
 #endif  // BOARDS_T3W1_UNIX_H
