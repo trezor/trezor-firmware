1b051d71cdf1f6502144049d44e41b6924782dd5
M1nd3r <petrsedlacek.km@seznam.cz>
2026-04-18 01:42:20 +0200

    refactor(core, crypto): extract `consteq` for general use
    [no changelog]

---
+++ crypto/consteq.h
@@ -0,0 +1,15 @@
+#ifndef __CONSTEQ_H__
+#define __CONSTEQ_H__
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/**
+ * @brief Constant-time memory comparison.
+ * Compares 'n' bytes, but unlike memcmp, it does not short-circuit,
+ * thus preventing timing attacks.
+ * @return `true` if the memory areas are equal, `false` otherwise.
+ */
+bool consteq(const void *s1, const void *s2, size_t n);
+#endif
