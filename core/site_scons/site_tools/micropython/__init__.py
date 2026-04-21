4fe3e19a8080ceaedef2de0616cb3aede4539f47
obrusvit <vit.obrusnik@gmail.com>
2026-04-21 11:02:15 +0200

    build(core): exclude haptic & led code if possible
    - also sort the features in the relevant function for better readability
    
    [no changelog]

--- core/site_scons/site_tools/micropython/__init__.py
+++ core/site_scons/site_tools/micropython/__init__.py
@@ -47,41 +47,45 @@ def generate(env):
         target = str(target[0])
         source = str(source[0])
         source_name = source.replace(env["source_dir"], "")
-        # replace "utils.BITCOIN_ONLY" with literal constant (True/False)
+        # replace "utils.BITCOIN_ONLY" or "utils.USE_<FEATURE>" with literal constant (True/False)
         # so the compiler can optimize out the things we don't want
-        btc_only = env["bitcoin_only"] == "1"
         backlight = env["backlight"]
-        optiga = env["optiga"]
-        tropic = env["tropic"]
-        touch = env["use_touch"]
-        button = env["use_button"]
         ble = env["use_ble"]
+        btc_only = env["bitcoin_only"] == "1"
+        button = env["use_button"]
+        haptic = env["use_haptic"]
         layout_bolt = env["ui_layout"] == "UI_LAYOUT_BOLT"
         layout_caesar = env["ui_layout"] == "UI_LAYOUT_CAESAR"
         layout_delizia = env["ui_layout"] == "UI_LAYOUT_DELIZIA"
         layout_eckhart = env["ui_layout"] == "UI_LAYOUT_ECKHART"
-        thp = env["thp"]
-        power_manager = env["power_manager"]
-        telemetry = env["telemetry"]
         n4w1 = env["n4w1"]
+        optiga = env["optiga"]
+        power_manager = env["power_manager"]
+        rgb_led = env["use_rgb_led"]
+        telemetry = env["telemetry"]
+        thp = env["thp"]
+        touch = env["use_touch"]
+        tropic = env["tropic"]
         include_source_lines = env["include_source_lines"]
         interim = f"{target[:-4]}.i"  # replace .mpy with .i
         sed_scripts = [
-            rf"-e 's/utils\.BITCOIN_ONLY/{btc_only}/g'",
             rf"-e 's/utils\.USE_BACKLIGHT/{backlight}/g'",
-            rf"-e 's/utils\.USE_OPTIGA/{optiga}/g'",
-            rf"-e 's/utils\.USE_TROPIC/{tropic}/g'",
+            rf"-e 's/utils\.USE_BLE/{ble}/g'",
+            rf"-e 's/utils\.BITCOIN_ONLY/{btc_only}/g'",
+            rf"-e 's/utils\.USE_BUTTON/{button}/g'",
+            rf"-e 's/utils\.USE_HAPTIC/{haptic}/g'",
             rf"-e 's/utils\.UI_LAYOUT == \"BOLT\"/{layout_bolt}/g'",
             rf"-e 's/utils\.UI_LAYOUT == \"CAESAR\"/{layout_caesar}/g'",
             rf"-e 's/utils\.UI_LAYOUT == \"DELIZIA\"/{layout_delizia}/g'",
             rf"-e 's/utils\.UI_LAYOUT == \"ECKHART\"/{layout_eckhart}/g'",
-            rf"-e 's/utils\.USE_BLE/{ble}/g'",
-            rf"-e 's/utils\.USE_BUTTON/{button}/g'",
-            rf"-e 's/utils\.USE_TOUCH/{touch}/g'",
-            rf"-e 's/utils\.USE_THP/{thp}/g'",
-            rf"-e 's/utils\.USE_POWER_MANAGER/{power_manager}/g'",
-            rf"-e 's/utils\.USE_TELEMETRY/{telemetry}/g'",
             rf"-e 's/utils\.USE_N4W1/{n4w1}/g'",
+            rf"-e 's/utils\.USE_OPTIGA/{optiga}/g'",
+            rf"-e 's/utils\.USE_POWER_MANAGER/{power_manager}/g'",
+            rf"-e 's/utils\.USE_RGB_LED/{rgb_led}/g'",
+            rf"-e 's/utils\.USE_TELEMETRY/{telemetry}/g'",
+            rf"-e 's/utils\.USE_THP/{thp}/g'",
+            rf"-e 's/utils\.USE_TOUCH/{touch}/g'",
+            rf"-e 's/utils\.USE_TROPIC/{tropic}/g'",
             r"-e 's/if TYPE_CHECKING/if False/'",
             r"-e 's/import typing/# &/'",
             r"-e '/from typing import (/,/^[[:space:]]*)/ {s/^/# /; }'",
