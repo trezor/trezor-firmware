bc97a6b2b00068cb36a13da27ddaba8306314870
Martin Milata <martin@martinmilata.cz>
2026-05-04 16:20:24 +0200

    refactor(core/rust): trezor-crypto backend glue for noise-protocol
    [no changelog]

--- core/embed/rust/src/crypto/mod.rs
+++ core/embed/rust/src/crypto/mod.rs
@@ -1,3 +1,5 @@
+use core::hint::black_box;
+
 use crate::error::value_error;
 
 pub mod aesgcm;
@@ -7,7 +9,7 @@ pub mod curve25519;
 pub mod ed25519;
 mod ffi;
 pub mod hmac;
-mod memory;
+pub mod memory;
 pub mod merkle;
 pub mod sha256;
 pub mod sha512;
@@ -22,6 +24,8 @@ pub enum Error {
     InvalidParams,
     // State precondition check failed (possibly raised by C implementation)
     InvalidContext,
+    // Authentication failed (e.g. AEAD tag mismatch)
+    AuthenticationFailed,
 }
 
 impl From<Error> for crate::error::Error {
@@ -31,6 +35,34 @@ impl From<Error> for crate::error::Error {
             Error::InvalidEncoding => value_error!(c"Invalid key or signature encoding"),
             Error::InvalidParams => value_error!(c"Invalid cryptographic parameters"),
             Error::InvalidContext => value_error!(c"Invalid cryptographic context"),
+            Error::AuthenticationFailed => value_error!(c"Authentication failed"),
         }
+    }
+}
+
+/// Constant time bytestring comparison for two arrays of the same length.
+fn consteq<const N: usize>(a: &[u8; N], b: &[u8; N]) -> bool {
+    let mut diff: u8 = 0;
+    for i in 0..N {
+        diff |= a[i] ^ b[i];
+    }
+    black_box(black_box(diff) == 0)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_consteq() {
+        assert!(consteq(&[], &[]));
+        assert!(consteq(&[0u8; 256], &[0u8; 256]));
+        assert!(consteq(&[0xffu8; 256], &[0xffu8; 256]));
+        assert!(consteq(b"0123456789abcdef", b"0123456789abcdef"));
+
+        assert!(!consteq(&[0u8; 256], &[0xffu8; 256]));
+        assert!(!consteq(&[0xffu8; 256], &[0u8; 256]));
+        assert!(!consteq(b"0123456789abcdef", b"123456789abcdef0"));
+        assert!(!consteq(b"0000000000000000", b"0000000000000001"));
     }
 }
