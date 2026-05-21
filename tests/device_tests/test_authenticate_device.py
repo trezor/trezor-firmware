3d9da61d2510938dcbbd410dba808a7bf690913b
Roman Zeyde <roman.zeyde@satoshilabs.com>
2026-05-21 10:32:14 +0200

    test(core): test both `AuthenticateDevice` flows
    Also, de-duplicate `challenge` parametrization using a fixture.
    
    [no changelog]

--- tests/device_tests/test_authenticate_device.py
+++ tests/device_tests/test_authenticate_device.py
@@ -1,33 +1,48 @@
 import pytest
 
-from trezorlib import device
+from trezorlib import device, exceptions, messages
 from trezorlib.debuglink import DebugSession as Session
 
 from ..common import compact_size
 from .certificate import check_signature_optiga, check_signature_tropic
 
+# The tests below require Optiga (and some require Tropic)
 pytestmark = pytest.mark.models("safe")
 
 
-@pytest.mark.parametrize(
-    "challenge",
-    (
-        b"",
-        b"hello world",
-        b"\x00" * 1024,
-        bytes.fromhex(
-            "21f3d40e63c304d0312f62eb824113efd72ba1ee02bef6777e7f8a7b6f67ba16"
+@pytest.fixture(
+    scope="module",
+    params=[
+        pytest.param(b"", id="empty"),
+        pytest.param(b"hello world", id="hello_world"),
+        pytest.param(b"\x00" * 1024, id="1kB_zeroes"),
+        pytest.param(
+            bytes.fromhex(
+                "21f3d40e63c304d0312f62eb824113efd72ba1ee02bef6777e7f8a7b6f67ba16"
+            ),
+            id="32B_digest",
         ),
-    ),
+    ],
 )
-def test_authenticate_device_optiga(session: Session, challenge: bytes) -> None:
+def challenge(request: pytest.FixtureRequest) -> bytes:
+    return request.param
+
+
+@pytest.fixture(scope="module", params=[0, 16, 1024])
+def chunk_size(request: pytest.FixtureRequest) -> int:
+    return request.param
+
+
+def test_authenticate_device_optiga(
+    session: Session, challenge: bytes, chunk_size: int
+) -> None:
     # NOTE Applications must generate a random challenge for each request.
 
     if not session.features.bootloader_locked:
         pytest.xfail("unlocked bootloader")
 
     # Issue an AuthenticateDevice challenge to Trezor.
-    proof = device.authenticate(session, challenge)
+    proof = device.authenticate(session, challenge, chunk_size)
 
     data = b"\x13AuthenticateDevice:" + compact_size(len(challenge)) + challenge
     check_signature_optiga(
@@ -35,26 +50,17 @@ def test_authenticate_device_optiga(session: Session, challenge: bytes) -> None:
     )
 
 
-@pytest.mark.parametrize(
-    "challenge",
-    (
-        b"",
-        b"hello world",
-        b"\x00" * 1024,
-        bytes.fromhex(
-            "21f3d40e63c304d0312f62eb824113efd72ba1ee02bef6777e7f8a7b6f67ba16"
-        ),
-    ),
-)
-@pytest.mark.models("core", skip=["safe3", "safe5"], reason="Not using Tropic")
-def test_authenticate_device_tropic(session: Session, challenge: bytes) -> None:
+@pytest.mark.models(skip=["safe3", "safe5"], reason="Not using Tropic")
+def test_authenticate_device_tropic(
+    session: Session, challenge: bytes, chunk_size: int
+) -> None:
     # NOTE Applications must generate a random challenge for each request.
 
     if not session.features.bootloader_locked:
         pytest.xfail("unlocked bootloader")
 
     # Issue an AuthenticateDevice challenge to Trezor.
-    proof = device.authenticate(session, challenge)
+    proof = device.authenticate(session, challenge, chunk_size)
 
     data = b"\x13AuthenticateDevice:" + compact_size(len(challenge)) + challenge
     check_signature_tropic(
@@ -63,3 +69,101 @@ def test_authenticate_device_tropic(session: Session, challenge: bytes) -> None:
         session.model,
         data,
     )
+
+
+@pytest.fixture(scope="function")
+def proof_sizes(session: Session) -> messages.AuthenticityProofSizes:
+    """Fetch AuthenticityProof sizes, for testing invalid subsequent requests."""
+    if not session.features.bootloader_locked:
+        pytest.xfail("unlocked bootloader")
+
+    # Issue an AuthenticateDevice challenge to Trezor.
+    return session.call(
+        messages.AuthenticateDevice(challenge=b"", stream=True),
+        expect=messages.AuthenticityProofSizes,
+    )
+
+
+@pytest.mark.models(["safe3", "safe5"], reason="No tropic signature")
+def test_authenticate_device_no_signature(
+    session: Session, proof_sizes: messages.AuthenticityProofSizes
+) -> None:
+    assert proof_sizes.tropic_signature is None
+    assert proof_sizes.tropic_certificates == []
+
+    # fetch missing signature (no Tropic on this model)
+    with pytest.raises(exceptions.TrezorFailure, match="DataError: No signature"):
+        session.call(
+            messages.GetAuthenticityProofChunk(
+                proof_type=messages.AuthenticityProofType.TROPIC,
+                index=None,
+                offset=0,
+                size=0,
+            )
+        )
+
+
+def test_authenticate_device_no_certificate(
+    session: Session, proof_sizes: messages.AuthenticityProofSizes
+) -> None:
+    # use wrong index when requesting a certificate
+    with pytest.raises(exceptions.TrezorFailure, match="DataError: No certificate"):
+        session.call(
+            messages.GetAuthenticityProofChunk(
+                proof_type=messages.AuthenticityProofType.OPTIGA,
+                index=len(proof_sizes.optiga_certificates),
+                offset=0,
+                size=0,
+            )
+        )
+
+
+def test_authenticate_device_invalid_range_size(
+    session: Session, proof_sizes: messages.AuthenticityProofSizes
+) -> None:
+    # use wrong chunk range size
+    with pytest.raises(
+        exceptions.TrezorFailure, match="DataError: Invalid chunk range"
+    ):
+        session.call(
+            messages.GetAuthenticityProofChunk(
+                proof_type=messages.AuthenticityProofType.OPTIGA,
+                index=None,
+                offset=0,
+                size=proof_sizes.optiga_signature + 1,
+            )
+        )
+
+
+def test_authenticate_device_invalid_range_offset(
+    session: Session, proof_sizes: messages.AuthenticityProofSizes
+) -> None:
+    # use wrong chunk range offset
+    with pytest.raises(
+        exceptions.TrezorFailure, match="DataError: Invalid chunk range"
+    ):
+        session.call(
+            messages.GetAuthenticityProofChunk(
+                proof_type=messages.AuthenticityProofType.OPTIGA,
+                index=None,
+                offset=proof_sizes.optiga_signature + 1,
+                size=0,
+            )
+        )
+
+
+def test_authenticate_device_unexpected(
+    session: Session, proof_sizes: messages.AuthenticityProofSizes
+) -> None:
+    session.client.ping("Should stop AuthenticityProof streaming")
+
+    with pytest.raises(
+        exceptions.TrezorFailure, match="UnexpectedMessage: Unexpected message"
+    ):
+        session.call(
+            messages.GetAuthenticityProofChunk(
+                proof_type=None,
+                offset=0,
+                size=0,
+            )
+        )
