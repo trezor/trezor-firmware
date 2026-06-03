140ae0bceacbae6dc2c94143574b0b9cd8cae5c4
kopecdav <david.kopecky@satoshilabs.com>
2026-06-03 16:19:12 +0200

    feat: purge battery_tester. [no changelog]

--- tools/automatic_battery_tester/dut/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dut import Dut
-from .dut_controller import DutController
-
-__all__ = ["Dut", "DutController"]
--- tools/automatic_battery_tester/test_logic/__init__.py
+++
@@ -1,5 +0,0 @@
-from .linear_scenario import LinearScenario
-from .random_wonder_scenario import RandomWonderScenario
-from .switching_scenario import SwitchingScenario
-
-__all__ = ["LinearScenario", "RandomWonderScenario", "SwitchingScenario"]
