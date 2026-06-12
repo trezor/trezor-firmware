7235317bd49f08179ad9c24605cf6f8cc8979525
PrisionMike <su.sh2396@gmail.com>
2026-06-12 12:44:08 +0200

    chore(tron): boilerplate support for (un)delegate resource instructions.
    - Benign string refactorings with eos.
    [no changelog]

--- core/src/trezor/enums/__init__.py
+++ core/src/trezor/enums/__init__.py
@@ -470,6 +470,8 @@ if TYPE_CHECKING:
         FreezeBalanceV2Contract = 54
         UnfreezeBalanceV2Contract = 55
         WithdrawExpireUnfreezeContract = 56
+        DelegateResourceContract = 57
+        UnDelegateResourceContract = 58
 
     class MessageType(IntEnum):
         Initialize = 0
@@ -761,6 +763,8 @@ if TYPE_CHECKING:
         TronUnfreezeBalanceV2Contract = 2208
         TronWithdrawUnfreeze = 2209
         TronVoteWitnessContract = 2210
+        TronDelegateResourceContract = 2211
+        TronUnDelegateResourceContract = 2212
         TronWithdrawBalance = 2213
         BenchmarkListNames = 9100
         BenchmarkNames = 9101
