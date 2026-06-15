7ee98685d8ebab97cbe3542b34bdb4394de665f4
Roman Zeyde <roman.zeyde@satoshilabs.com>
2026-06-15 09:40:14 +0200

    build(core): exclude D001/D002-related bytecode
    [no changelog]

--- core/site_scons/site_tools/micropython/__init__.py
+++ core/site_scons/site_tools/micropython/__init__.py
@@ -98,7 +98,7 @@ def generate(env):
             r"-e 's/from typing import/# &/'",
         ]
 
-        MODELS = ["T2T1", "T2B1", "T3T1", "T3B1", "T3W1"]
+        MODELS = ["T2T1", "T2B1", "T3T1", "T3B1", "T3W1", "D001", "D002"]
 
         for internal_model in MODELS:
             model_matches = env["TREZOR_MODEL"] == internal_model
