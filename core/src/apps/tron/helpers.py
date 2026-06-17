1e31ac8903627bbb6bb0fb2b8c46df7b9e26c920
PrisionMike <su.sh2396@gmail.com>
2026-06-17 10:50:23 +0200

    chore(tron): use pubhash for address derivation.
    - avoids calling private key in python for public key derivation.
    - removing a function that was only used in tests.
    
    [no changelog]

--- core/src/apps/tron/helpers.py
+++ core/src/apps/tron/helpers.py
@@ -7,13 +7,6 @@ if TYPE_CHECKING:
     from buffer_types import AnyBytes
 
 
-def address_from_public_key(pubkey: bytes) -> str:
-    from trezor.crypto.hashlib import sha3_256
-
-    address_bytes = b"\x41" + sha3_256(pubkey[1:], keccak=True).digest()[12:]
-    return base58.encode_check(address_bytes)
-
-
 def get_encoded_address(address_bytes: AnyBytes) -> str:
     """Encodes raw address bytes into Tron format."""
     address = base58.encode_check(address_bytes)
