0e832621edb0b3103af09b74a5b316bc30efcadd
Ludek Farsky <ludek.farsky@satoshilabs.com>
2026-07-03 09:28:22 +0200

    feat(nfc): enable polling the NFC in the sysevent loop
    [no changelog]

---
+++ core/embed/io/nfc/st25/nfc_poll_internal.h
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the Trezor project, https://trezor.io/
+ *
+ * Copyright (c) SatoshiLabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <trezor_bsp.h>
+#include <trezor_types.h>
+
+bool nfc_identify(nfc_dev_info_t *dev_info);
+
+bool nfc_check_connection(nfc_dev_info_t *dev_info);
+
+ts_t nfc_restart_discovery(void);
