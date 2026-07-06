960f44b48f47f728c0de9519a5634dd1e4d8030e
Lukas Bielesch <bielesch.lukas@gmail.com>
2026-07-06 10:37:04 +0200

    chore(core): update trezor api

--- core/embed/api/trezor_api_v1_impl.c
+++ core/embed/api/trezor_api_v1_impl.c
@@ -19,12 +19,48 @@
 
 #include "trezor_api_v1.h"
 
-#ifndef USE_DBG_CONSOLE
+// temporary hack to avoid usage of enum
+bool syslog_start_record_(const log_source_t* source, uint32_t level) {
+#ifdef USE_DBG_CONSOLE
+  return syslog_start_record(source, level);
+#else
+  return true;
+#endif
+}
+
 // temporary hack to allow compilation when DBG console is disabled
+#ifndef USE_DBG_CONSOLE
 ssize_t dbg_console_write(const void* data, size_t data_size) {
   return data_size;
 }
+
+ssize_t syslog_write_chunk(const char* text, size_t text_len, bool end_record) {
+  return text_len;
+}
 #endif
+
+const trezor_crypto_v1_t trezor_crypto_v1 = {
+    .ed25519_cosi_combine_publickeys = ed25519_cosi_combine_publickeys,
+    .ed25519_sign_open = ed25519_sign_open,
+    .sha3_256_Init = sha3_256_Init,
+    .sha3_512_Init = sha3_512_Init,
+    .sha3_Update = sha3_Update,
+    .sha3_Final = sha3_Final,
+    .keccak_Final = keccak_Final,
+    .sha256_Init = sha256_Init,
+    .sha256_Update = sha256_Update,
+    .sha256_Final = sha256_Final,
+    .sha512_Init = sha512_Init,
+    .sha512_Update = sha512_Update,
+    .sha512_Final = sha512_Final,
+    .hmac_sha256_Init = hmac_sha256_Init,
+    .hmac_sha256_Update = hmac_sha256_Update,
+    .hmac_sha256_Final = hmac_sha256_Final,
+    .ecdsa_recover_pub_from_sig = ecdsa_recover_pub_from_sig,
+    .ecdsa_verify_digest = ecdsa_verify_digest,
+    .secp256k1 = &secp256k1,
+    .nist256p1 = &nist256p1,
+};
 
 const trezor_api_v1_t trezor_api_v1 = {
     .system_exit = system_exit,
@@ -34,13 +70,16 @@ const trezor_api_v1_t trezor_api_v1 = {
     .system_exit_fatal_ex = system_exit_fatal_ex,
     .systick_ms = systick_ms,
     .sysevents_poll = sysevents_poll,
-    .syshandle_read = syshandle_read,
     .dbg_console_write = dbg_console_write,
+    .syslog_start_record = syslog_start_record_,
+    .syslog_write_chunk = syslog_write_chunk,
     .ipc_register = ipc_register,
     .ipc_unregister = ipc_unregister,
     .ipc_try_receive = ipc_try_receive,
     .ipc_message_free = ipc_message_free,
     .ipc_send = ipc_send,
+    .app_get_heap = app_get_heap,
+    .trezor_crypto_v1 = &trezor_crypto_v1,
 };
 
 const void* coreapp_api_get(uint32_t version) {
