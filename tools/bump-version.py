6676b43209c02c08114ac861d5097ae9d17c4500
Martin Milata <martin@martinmilata.cz>
2026-07-15 21:27:28 +0200

    fix(tools): bump-version: support X.Y.Z.W, regen error_codes.json
    - add support for VERSION_BUILD/VERSION_TWEAK
    - regenerate error_codes.json after bumping prodtest
    - fix python path
    - cleanup
    
    [no changelog]

--- tools/bump-version.py
+++ tools/bump-version.py
@@ -8,20 +8,26 @@ from typing import Any
 
 import click
 
-VERSION_RE = re.compile(r"^(\d+)[.](\d+)[.](\d+)$")
+VERSION_RE = re.compile(r"^(\d+)[.](\d+)[.](\d+)(?:[.](\d+))?$")
 HEADER_LINE_RE = re.compile(r"^#define ([A-Z_]+) \S+$")
 VERSION_FILE_LINE_RE = re.compile(r"^([A-Z_]+) = \S+$")
 
+SHORT = {
+    "core": Path("embed/projects/firmware"),
+    "legacy": Path("firmware"),
+}
+NO_BUILD = ["python", "legacy/*"]
 
-def bump_header(filename: Path, **kwargs: Any) -> None:
+
+def bump_re(pattern: re.Pattern, fmt: str, filename: Path, **kwargs: Any) -> None:
     result_lines = []
 
     with open(filename, "r+") as fh:
         for line in fh:
-            m = HEADER_LINE_RE.match(line)
-            if m is not None and m[1] in kwargs:
+            m = pattern.match(line)
+            if m is not None and kwargs.get(m[1]) is not None:
                 symbol = m[1]
-                result_lines.append(f"#define {symbol} {kwargs[symbol]}\n")
+                result_lines.append(fmt.format(symbol=symbol, value=kwargs[symbol]))
             else:
                 result_lines.append(line)
 
@@ -29,32 +35,18 @@ def bump_header(filename: Path, **kwargs: Any) -> None:
         fh.truncate(0)
         for line in result_lines:
             fh.write(line)
+
+
+def bump_header(filename: Path, **kwargs: Any) -> None:
+    return bump_re(HEADER_LINE_RE, "#define {symbol} {value}\n", filename, **kwargs)
 
 
 def bump_version_file(filename: Path, **kwargs: Any) -> None:
-    result_lines = []
-
-    with open(filename, "r+") as fh:
-        for line in fh:
-            m = VERSION_FILE_LINE_RE.match(line)
-            if m is not None and m[1] in kwargs:
-                symbol = m[1]
-                result_lines.append(f"{symbol} = {kwargs[symbol]}\n")
-            else:
-                result_lines.append(line)
-
-        fh.seek(0)
-        fh.truncate(0)
-        for line in result_lines:
-            fh.write(line)
+    return bump_re(VERSION_FILE_LINE_RE, "{symbol} = {value}\n", filename, **kwargs)
 
 
 def bump_python(subdir: Path, new_version: str) -> None:
     subprocess.check_call(["uv", "version", new_version], cwd=subdir)
-
-
-def hex_lit(version: Any) -> str:
-    return rf'"\x{int(version):02X}"'
 
 
 @click.command()
@@ -67,24 +59,45 @@ def hex_lit(version: Any) -> str:
     type=str,
 )
 def cli(project: str | Path, version: str) -> None:
-    """Bump version for given project (core, python, legacy/firmware,
-    legacy/bootloader, core/embed/projects/prodtest, nordic/trezor/trezor-ble).
+    """\b
+    PROJECT must be a directory like:
+    - core/embed/projects/firmware (shortcut: core)
+    - core/embed/projects/bootloader
+    - core/embed/projects/prodtest
+    - python
+    - nordic/trezor/trezor-ble
+    - legacy/firmware (shortcut: legacy)
+    - legacy/bootloader
+    \b
+    VERSION must be formatted like:
+    - MAJOR.MINOR.PATCH (build is set to 0 if applicable)
+    - MAJOR.MINOR.PATCH.BUILD (unsupported for legacy, python)
     """
     project = Path(project)
+    if project.name in SHORT:
+        project = project / SHORT[project.name]
 
-    m = VERSION_RE.match(version)
-    if m is None:
-        raise click.ClickException("Version must be MAJOR.MINOR.PATCH")
+    if (m := VERSION_RE.match(version)) is None:
+        raise click.ClickException(
+            "Version must be MAJOR.MINOR.PATCH or MAJOR.MINOR.PATCH.BUILD"
+        )
+    major, minor, patch, build = m.groups()
 
-    major, minor, patch = m.group(1, 2, 3)
+    if any(project.match(pat) for pat in NO_BUILD):
+        if build is not None:
+            raise click.ClickException(
+                f"Version must be MAJOR.MINOR.PATCH for {project}."
+            )
+    elif build is None:
+        build = 0
 
-    parts = project.parts
     if (project / "VERSION").is_file():
         bump_version_file(
             project / "VERSION",
             VERSION_MAJOR=major,
             VERSION_MINOR=minor,
             PATCHLEVEL=patch,
+            VERSION_TWEAK=build,
         )
     elif (project / "version.h").is_file():
         bump_header(
@@ -92,25 +105,20 @@ def cli(project: str | Path, version: str) -> None:
             VERSION_MAJOR=major,
             VERSION_MINOR=minor,
             VERSION_PATCH=patch,
+            VERSION_BUILD=build,
         )
-    elif parts[-1] == "core":
-        bump_header(
-            project / "embed" / "projects" / "firmware" / "version.h",
-            VERSION_MAJOR=major,
-            VERSION_MINOR=minor,
-            VERSION_PATCH=patch,
-        )
-        # also bump language JSONs
-        subprocess.run(["python", project / "translations" / "cli.py", "gen"])
-    elif parts[-1] == "legacy":
-        bump_header(
-            project / "firmware" / "version.h",
-            VERSION_MAJOR=major,
-            VERSION_MINOR=minor,
-            VERSION_PATCH=patch,
-        )
-    elif parts[-1] == "python":
-        bump_python(project / "python", f"{major}.{minor}.{patch}")
+        if project.match("core/embed/projects/firmware"):
+            # also bump language JSONs
+            subprocess.check_call(
+                ["python", project.parents[2] / "translations" / "cli.py", "gen"]
+            )
+        if project.match("core/embed/projects/prodtest"):
+            # refresh error_codes.json
+            subprocess.check_call(
+                ["python", project.parents[2] / "tools" / "prodtest_error_codes.py"]
+            )
+    elif project.name == "python":
+        bump_python(project, f"{major}.{minor}.{patch}")
     else:
         raise click.ClickException(f"Unknown project {project}.")
 
