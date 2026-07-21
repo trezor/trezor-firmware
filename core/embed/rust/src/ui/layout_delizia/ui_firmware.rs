96ce74194c7ba8fafe9fbdbf5f04375c181e866d
Roman Zeyde <roman.zeyde@satoshilabs.com>
2026-07-21 11:59:23 +0200

    fix(core/delizia): warning title should be red if `danger=true`
    [no changelog]

--- core/embed/rust/src/ui/layout_delizia/ui_firmware.rs
+++ core/embed/rust/src/ui/layout_delizia/ui_firmware.rs
@@ -1252,7 +1252,7 @@ impl FirmwareUI for UIDelizia {
             Some(title) => {
                 let header = Header::left_aligned(title);
                 let header = if danger {
-                    header.with_danger_icon()
+                    header.with_danger().with_danger_icon()
                 } else {
                     header.with_warning_low_icon()
                 };
