a9d1f6dfbcf8ea718228176fa75bbedea6f8f82a
obrusvit <vit.obrusnik@gmail.com>
2026-07-24 11:07:23 +0200

    docs(core): fix UI_LAYOUT commentary
    [no changelog]

--- core/mocks/generated/trezorutils.pyi
+++ core/mocks/generated/trezorutils.pyi
@@ -307,7 +307,7 @@ EMULATOR: bool
 BITCOIN_ONLY: bool
 """Whether the firmware is Bitcoin-only."""
 UI_LAYOUT: str
-"""UI layout identifier ("BOLT"-T, "CAESAR"-TS3, "DELIZIA"-TS5)."""
+"""UI layout identifier (BOLT/CAESAR/DELIZIA/ECKHART)."""
 USE_THP: bool
 """Whether the firmware supports Trezor-Host Protocol (version 2)."""
 NOTIFY_BOOT: int
