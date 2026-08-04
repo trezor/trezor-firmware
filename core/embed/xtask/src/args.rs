18b14dc6aa3c14954bce2576a9ee27567c66a9a5
cepetr <petr@cerninovi.cz>
2026-08-04 14:59:57 +0200

    feat(xbuild): add api bindings check

--- core/embed/xtask/src/args.rs
+++ core/embed/xtask/src/args.rs
@@ -8,6 +8,12 @@ use serde::Deserialize;
 pub use crate::model::Model;
 use crate::options::BuildOptions;
 
+pub struct ResolvedBuild {
+    pub features: Vec<String>,
+    pub target_triple: Option<&'static str>,
+    pub board_header: String,
+}
+
 #[derive(ValueEnum, Debug, Clone, Copy, Default, PartialEq, Eq, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum Project {
@@ -148,6 +154,7 @@ pub enum Cmd {
     /// Print current version of specified project
     PrintVersion(PrintVersionArgs),
     Modular(ModularArgs),
+    ApiBindings(ApiArgs),
 }
 
 #[derive(Args, Debug)]
@@ -156,7 +163,7 @@ pub struct ModularArgs {
     pub command: ModularCmd,
 }
 
-#[derive(Args, Debug, Clone)]
+#[derive(Args, Debug, Clone, Default)]
 #[command(override_usage = "xtask build <PROJECT> --model <MODEL> [OPTIONS]")]
 pub struct BuildArgs {
     pub project: Project,
@@ -256,4 +263,10 @@ pub struct CombineArgs {
 #[command(hide = true)] // Should probably go under some kind of misc subcommand.
 pub struct PrintVersionArgs {
     pub project: Project,
+}
+
+#[derive(Args, Debug)]
+pub struct ApiArgs {
+    #[arg(long, default_value = "false")]
+    pub check_only: bool,
 }
