9f6a7fb0d66297f06e99ab581cedfdce299c9550
Martin Milata <martin@martinmilata.cz>
2026-08-06 01:25:48 +0200

    chore(tools): add script for unpacking firmware artifacts
    The directory structure is compatible with other release tooling. Can be
    run in CI on release branches, or locally. No T1B1 support.

---
+++ tools/unpack-artifacts.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+"""
+Unpacks artifact archives produced by `core_firmware` jobs in `core.yml` into directory structure used by release tooling.
+Only models in the latest entry in `common/releases.json` are copied.
+"""
+
+import json
+import shutil
+import zipfile
+from pathlib import Path
+
+import click
+
+
+@click.command()
+@click.argument(
+    "artifact_dir", type=click.Path(exists=True, file_okay=False, dir_okay=True)
+)
+@click.option(
+    "-o",
+    "--output",
+    "output_dir",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True),
+    default="firmware",
+)
+@click.option(
+    "-r",
+    "--releases",
+    "releases_json",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+    default="common/releases.json",
+)
+@click.option(
+    "-t",
+    "--translations",
+    "translations_dir",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True),
+    default="core/translations",
+)
+def main(
+    artifact_dir: str | Path,
+    output_dir: str | Path,
+    releases_json: str | Path,
+    translations_dir: str | Path,
+) -> None:
+    artifact_dir = Path(artifact_dir)
+    output_dir = Path(output_dir)
+    releases_json = Path(releases_json)
+    translations_dir = Path(translations_dir)
+
+    releases = json.loads(releases_json.read_text())["firmware"]
+    latest = max(map(lambda s: [int(n) for n in s.split(".")], releases.keys()))
+    latest = ".".join(str(n) for n in latest)
+    click.echo(f"Version: {latest}")
+    models = sorted(releases[latest])
+    click.echo(f"Models: {', '.join(models)}")
+
+    for model in models:
+        # firmware
+        model_dir = output_dir / "unsigned" / model.lower()
+        model_dir.mkdir(parents=True, exist_ok=True)
+
+        for coins in ("universal", "btconly"):
+            artifact_zip = artifact_dir / f"core-firmware-{model}-{coins}-normal.zip"
+            if not artifact_zip.exists():
+                # download-artifact adds another directory level
+                artifact_zip = (
+                    artifact_dir
+                    / f"core-firmware-{model}-{coins}-normal"
+                    / f"core-firmware-{model}-{coins}-normal.zip"
+                )
+
+            with zipfile.ZipFile(artifact_zip, "r") as z:
+                matching = [
+                    fn
+                    for fn in z.namelist()
+                    if fn.startswith("pub/firmware-") and fn.endswith(".bin")
+                ]
+                assert len(matching) == 1
+                fw_filename = matching[0]
+
+                with z.open(fw_filename, "r") as fw_fh:
+                    fw_filename = Path(fw_filename).name
+                    click.echo(
+                        f"{artifact_zip}[{fw_filename}] -> {model_dir / fw_filename}"
+                    )
+                    (model_dir / fw_filename).write_bytes(fw_fh.read())
+
+        # translations
+        model_dir = output_dir / "unsigned" / "translations" / model.lower()
+        model_dir.mkdir(parents=True, exist_ok=True)
+
+        for f in translations_dir.iterdir():
+            fname = f.name
+            if fname.startswith(f"translation-{model}-") and fname.endswith(
+                "-unsigned.bin"
+            ):
+                click.echo(f"{f} -> {model_dir / fname}")
+                shutil.copy(f, model_dir / fname)
+
+
+if __name__ == "__main__":
+    main()
