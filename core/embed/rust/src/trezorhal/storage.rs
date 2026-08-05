c707aad06bc95d76292d00ce67e51c3e13d7e880
cepetr <petr@cerninovi.cz>
2026-08-05 09:24:43 +0200

    fix(core): fix up clippy warnings in storage.rs
    [no changelog]

--- core/embed/rust/src/trezorhal/storage.rs
+++ core/embed/rust/src/trezorhal/storage.rs
@@ -128,7 +128,7 @@ pub fn lock() {
 /// Unlock storage with PIN and optional external salt.
 /// Returns true if the PIN + salt combination is correct.
 pub fn unlock(pin: &str, salt: Option<&ExternalSalt>) -> bool {
-    let salt = salt.map(|s| s.as_ptr()).unwrap_or(ptr::null());
+    let salt = salt.map(|s| s.as_ptr()).unwrap_or_default();
     let result = unsafe { ffi::storage_unlock(pin.as_ptr() as *const _, pin.len(), salt) };
     matches!(result, ffi::storage_unlock_result_t::UNLOCK_OK)
 }
@@ -141,7 +141,7 @@ pub fn change_pin(new_pin: &str, new_salt: Option<&ExternalSalt>) -> bool {
         ffi::storage_change_pin(
             new_pin.as_ptr() as *const _,
             new_pin.len(),
-            new_salt.map(|s| s.as_ptr()).unwrap_or(ptr::null()),
+            new_salt.map(|s| s.as_ptr()).unwrap_or_default(),
         )
     };
     matches!(result, ffi::storage_pin_change_result_t::PIN_CHANGE_OK)
