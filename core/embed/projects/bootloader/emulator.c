10320a8aae9219384da68eb94a5495c7ea6400c3
cepetr <petr@cerninovi.cz>
2026-08-07 09:23:15 +0200

    fixup! fix(emulator): initialize mono counters before running bootemu

--- core/embed/projects/bootloader/emulator.c
+++ core/embed/projects/bootloader/emulator.c
@@ -228,10 +228,13 @@ int main(int argc, char **argv) {
   (void)!flash_otp_write(FLASH_OTP_BLOCK_DEVICE_VARIANT, 0, otp_data,
                          sizeof(otp_data));
 
-  // initialize monotonic counters
-  monoctr_write(MONOCTR_FIRMWARE_VERSION, 0);
+  // Initialize monotonic counters
+
+  ensure(monoctr_write(MONOCTR_FIRMWARE_VERSION, 0),
+         "initialize firmware monotonic counter");
 #ifdef USE_SECMON_VERIFICATION
-  monoctr_write(MONOCTR_SECMON_VERSION, 0);
+  ensure(monoctr_write(MONOCTR_SECMON_VERSION, 0),
+         "initialize secure-monitor monotonic counter");
 #endif
 
   int exit_code = bootloader_main();
