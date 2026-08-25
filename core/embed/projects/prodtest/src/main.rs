97855ef3699b216b7e2716275ff824f24fb6b585
cepetr <petr@cerninovi.cz>
2026-08-25 09:30:38 +0200

    fix(core): fix build with --debug  option
    [no changelog]

--- core/embed/projects/prodtest/src/main.rs
+++ core/embed/projects/prodtest/src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
 
+// force pull in Rust generated symbols (incl. the panic handler)
 use io as _;
+use sys as _;
 use trezor_lib as _;
--- core/embed/projects/unix/src/main.rs
+++ core/embed/projects/unix/src/main.rs
@@ -1,4 +1,6 @@
 #![no_std]
 #![no_main]
 
+// force pull in Rust generated symbols (incl. the panic handler)
+use sys as _;
 use trezor_lib as _;
