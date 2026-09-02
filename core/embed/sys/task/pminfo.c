0e3463fd7c0b0f65b40d3d1889cf9fea22239cf0
cepetr <petr@cerninovi.cz>
2026-09-02 10:07:11 +0200

    feat(core): print systask pminfo into  dbg_console
    [no changelog]

---
+++ core/embed/sys/task/pminfo.c
@@ -0,0 +1,126 @@
+/*
+ * This file is part of the Trezor project, https://trezor.io/
+ *
+ * Copyright (c) SatoshiLabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef KERNEL_MODE
+
+#include <trezor_bsp.h>
+
+#include <sys/systask.h>
+
+#ifdef USE_DBG_CONSOLE
+#include <sys/dbg_console.h>
+#endif
+
+#include <inttypes.h>
+
+#if USE_DBG_CONSOLE && !TREZOR_EMULATOR
+static const char* irq_name(int irqn) {
+  switch (irqn) {
+    case NonMaskableInt_IRQn:
+      return "NMI";
+    case HardFault_IRQn:
+      return "HardFault";
+    case MemoryManagement_IRQn:
+      return "MemManage";
+    case BusFault_IRQn:
+      return "BusFault";
+    case UsageFault_IRQn:
+      return "UsageFault";
+#if defined(__ARM_FEATURE_CMSE)
+    case SecureFault_IRQn:
+      return "SecureFault";
+#endif
+    case SVCall_IRQn:
+      return "SVCall";
+    case DebugMonitor_IRQn:
+      return "DebugMonitor";
+    case PendSV_IRQn:
+      return "PendSV";
+    case SysTick_IRQn:
+      return "SysTick";
+  }
+
+  return "Unknown";
+}
+
+#ifndef TREZOR_EMULATOR
+static void print_fault(const system_fault_t* fault) {
+  dbg_printf("  PC: 0x%08" PRIx32 "\n", fault->pc);
+  dbg_printf("  SP: 0x%08" PRIx32 "\n", fault->sp);
+  dbg_printf("  IRQn: %d (%s)\n", (int)fault->irqn, irq_name(fault->irqn));
+  dbg_printf("  CFSR: 0x%08" PRIx32 "\n", fault->cfsr);
+  dbg_printf("  HFSR: 0x%08" PRIx32 "\n", fault->hfsr);
+  dbg_printf("  MMFAR: 0x%08" PRIx32 "\n", fault->mmfar);
+  dbg_printf("  BFAR: 0x%08" PRIx32 "\n", fault->bfar);
+#if defined(__ARM_FEATURE_CMSE)
+  dbg_printf("  SFSR: 0x%08" PRIx32 "\n", fault->sfsr);
+  dbg_printf("  SFAR: 0x%08" PRIx32 "\n", fault->sfar);
+#endif
+}
+#endif  // TREZOR_EMULATOR
+
+#endif  // USE_DBG_CONSOLE
+
+void systask_print_pminfo(systask_t* task) {
+#ifdef USE_DBG_CONSOLE
+
+  const systask_postmortem_t* pminfo = &task->pminfo;
+
+  if (pminfo->reason == TASK_TERM_REASON_EXIT && pminfo->exit.code == 0) {
+    dbg_printf("Task #%u terminated cleanly\n", task->id);
+    return;
+  }
+
+  dbg_printf("Task #%u terminated.\n", task->id);
+
+  switch (pminfo->reason) {
+    case TASK_TERM_REASON_EXIT:
+      dbg_printf("Exit code: %d\n", pminfo->exit.code);
+      break;
+
+    case TASK_TERM_REASON_ERROR:
+      dbg_printf("Error: %s\n", pminfo->error.message);
+      if (pminfo->error.title[0] != '\0') {
+        dbg_printf("Title: %s\n", pminfo->error.title);
+      }
+      if (pminfo->error.footer[0] != '\0') {
+        dbg_printf("Footer: %s\n", pminfo->error.footer);
+      }
+      break;
+
+    case TASK_TERM_REASON_FATAL:
+      dbg_printf("Fatal: %s", pminfo->fatal.expr);
+      if (pminfo->fatal.file[0] != '\0') {
+        dbg_printf(" at %s:%" PRId32, pminfo->fatal.file, pminfo->fatal.line);
+      }
+      dbg_printf("\n");
+      break;
+
+    case TASK_TERM_REASON_FAULT:
+      dbg_printf("Fault in %s code\n",
+                 pminfo->privileged ? "privileged" : "unprivileged");
+#ifndef TREZOR_EMULATOR
+      print_fault(&pminfo->fault);
+#endif
+      break;
+  }
+#endif  // USE_DBG_CONSOLE
+}
+
+#endif  // KERNEL_MODE
