9570365b3eccb9a283595a76939397accc31f8e8
Roman Zeyde <roman.zeyde@satoshilabs.com>
2026-09-08 09:33:47 +0200

    chore(core): fix a small comment typo
    [no changelog]

--- core/embed/rust/src/ui/api/firmware_micropython.rs
+++ core/embed/rust/src/ui/api/firmware_micropython.rs
@@ -1389,7 +1389,7 @@ pub static mp_module_trezorui_api: Module = obj_module! {
     ///
     ///     if utils.USE_BLE:
     ///         def ble_event(self, event: int, data: bytes) -> LayoutState | None:
-    ///             """Receive a BLE events."""
+    ///             """Receive a BLE event."""
     ///
     ///     if utils.USE_POWER_MANAGER:
     ///         def pm_event(self, flags: int) -> LayoutState | None:
