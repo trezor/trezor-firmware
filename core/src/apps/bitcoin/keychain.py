66aebfbddf8a60c1dc2107ab2e6ce5bebdd42cd9
Roman Zeyde <roman.zeyde@satoshilabs.com>
2026-09-09 12:19:25 +0200

    fixup! [WIP] feat(core): add Miniscript support

--- core/src/apps/bitcoin/keychain.py
+++ core/src/apps/bitcoin/keychain.py
@@ -148,10 +148,10 @@ def validate_path_against_script_type(
         if coin.coin_name in BITCOIN_NAMES:
             append(PATTERN_CASA_UNHARDENED)
 
-    elif coin.segwit and script_type in (
-        InputScriptType.SPENDWITNESS,
-        InputScriptType.SPENDMINISCRIPT,
-    ):
+    elif coin.segwit and script_type == InputScriptType.SPENDMINISCRIPT:
+        append(PATTERN_BIP48_SEGWIT)  # Liana
+
+    elif coin.segwit and script_type == InputScriptType.SPENDWITNESS:
         append(PATTERN_BIP84)
         if multisig:
             append(PATTERN_BIP48_SEGWIT)
