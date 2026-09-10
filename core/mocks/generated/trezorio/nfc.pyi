d1b969984cfed828416411993c42e98ae8245aa8
Roman Zeyde <roman.zeyde@satoshilabs.com>
2026-09-10 09:59:09 +0200

    feat(core): support NFC discovery in MicroPython
    [no changelog]

---
+++ core/mocks/generated/trezorio/nfc.pyi
@@ -0,0 +1,14 @@
+from typing import *
+from buffer_types import *
+
+
+
+# upymod/modtrezorio/modtrezorio-nfc.h
+def start() -> None:
+    """Start NFC discovery."""
+
+
+
+# upymod/modtrezorio/modtrezorio-nfc.h
+def stop() -> None:
+    """Stop NFC discovery."""
