cd29104c219d17e185d3f152e52e2934cba7749e
cepetr <petr@cerninovi.cz>
2026-09-11 13:14:20 +0200

    feat(core): introduce app_arena
    [no changelog]

--- core/embed/upymod/build.rs
+++ core/embed/upymod/build.rs
@@ -109,7 +109,6 @@ fn main() -> Result<()> {
             "modutime.c",
             "rustmods.c",
             "trezorobj.c",
-            "modtrezorapp/modtrezorapp.c",
             "modtrezorconfig/modtrezorconfig.c",
             "modtrezorcrypto/modtrezorcrypto.c",
             "modtrezorcrypto/crc.c",
@@ -117,6 +116,10 @@ fn main() -> Result<()> {
             "modtrezorui/modtrezorui.c",
             "modtrezorutils/modtrezorutils.c",
         ]);
+
+        if cfg!(feature = "app_loading") {
+            lib.add_sources(["modtrezorapp/modtrezorapp.c"]);
+        }
 
         if cfg!(feature = "sd_card") {
             lib.add_sources(["modtrezorio/ff.c", "modtrezorio/ffunicode.c"]);
@@ -1196,6 +1199,10 @@ impl<'a> MpyBuilder<'a> {
 
         if cfg!(feature = "thp") {
             files.add(src, "apps/thp/*.py")?;
+        }
+
+        if cfg!(feature = "app_loading") {
+            files.add(src, "apps/trezorapp/*.py")?;
         }
 
         if cfg!(feature = "universal_fw") {
