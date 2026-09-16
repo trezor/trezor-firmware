aa22809b59c9966a45a02367b6031e7fea7c16c7
obrusvit <vit.obrusnik@gmail.com>
2026-09-16 12:05:36 +0200

    feat(core/delizia): use Marquee in Footer
    [no changelog]

--- core/embed/rust/src/ui/layout_delizia/component/frame.rs
+++ core/embed/rust/src/ui/layout_delizia/component/frame.rs
@@ -79,8 +79,8 @@ pub struct Frame<T> {
     content: T,
     header: Option<Header>,
     header_update_fn: Option<fn(&T, &mut EventCtx, &mut Header)>,
-    footer: Option<Footer<'static>>,
-    footer_update_fn: Option<fn(&T, &mut EventCtx, &mut Footer<'static>)>,
+    footer: Option<Footer>,
+    footer_update_fn: Option<fn(&T, &mut EventCtx, &mut Footer)>,
     swipe: SwipeConfig,
     horizontal_swipe: HorizontalSwipe,
     margin: u8,
@@ -208,10 +208,7 @@ where
         self
     }
 
-    pub fn register_footer_update_fn(
-        mut self,
-        f: fn(&T, &mut EventCtx, &mut Footer<'static>),
-    ) -> Self {
+    pub fn register_footer_update_fn(mut self, f: fn(&T, &mut EventCtx, &mut Footer)) -> Self {
         self.footer_update_fn = Some(f);
         self
     }
