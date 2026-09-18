68ea80dc6d13dda15b509e8d29dd3d1cb366df52
cepetr <petr@cerninovi.cz>
2026-09-18 18:23:52 +0200

    feat(core): app root packet downgrade protection
    [no changelog]

--- core/embed/io/app_arena/inc/io/app_root.h
+++ core/embed/io/app_arena/inc/io/app_root.h
@@ -32,6 +32,13 @@ typedef enum {
 } app_ring_t;
 
 /**
+ * @brief Hold persisted timestamps for each application ring
+ */
+typedef struct {
+  uint32_t ring_timestamp[APP_RING_COUNT];
+} app_root_state_t;
+
+/**
  * @brief Initializes the root-of-trust storage.
  *
  * @return TS_OK on success, or an error code on failure.
@@ -44,12 +51,19 @@ ts_t app_root_init(void);
  * Before storing, the function checks the integrity and validity of the
  * root packet, including its signature.
  *
+ * Root packets timestamps are compared against the provided state structure
+ * to ensure that the new root packet is more recent than the stored timestamps.
+ * state structure is updated with new values and caller is responsible
+ * for storing it for future reference.
+ *
  * @param root_packet Pointer to the root packet to store.
  * @param root_packet_size Size of the root packet in bytes.
+ * @param state Pointer to an app_root_state_t structure
  *
  * @return TS_OK on success, or an error code on failure.
  */
-ts_t app_root_update(const void* root_packet, size_t root_packet_size);
+ts_t app_root_update(const void* root_packet, size_t root_packet_size,
+                     app_root_state_t* state);
 
 /**
  * @brief Deletes all stored root packets
