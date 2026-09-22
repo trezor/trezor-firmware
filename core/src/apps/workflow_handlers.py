9b8365e8272d3ab7bd6f969f2be64eb045e83f2d
Raghav <raghavawasthi2005@gmail.com>
2026-09-22 01:39:36 +0100

    Update core/src/apps/workflow_handlers.py
    Co-authored-by: Vít Obrusník <vit.obrusnik@gmail.com>

--- core/src/apps/workflow_handlers.py
+++ core/src/apps/workflow_handlers.py
@@ -61,7 +61,7 @@ def _find_message_handler_module(msg_type: int) -> str:
     if msg_type == MessageType.RebootToBootloader:
         return "apps.management.reboot_to_bootloader"
 
-    if utils.UI_LAYOUT != "BOLT" and msg_type == MessageType.ShowDeviceTutorial:
+    if utils.INTERNAL_MODEL != "T2T1" and msg_type == MessageType.ShowDeviceTutorial:
         return "apps.management.show_tutorial"
 
     if utils.USE_TELEMETRY and msg_type == MessageType.TelemetryGet:
